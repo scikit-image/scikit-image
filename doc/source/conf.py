35a94b760e627ed44b858e0f9b7851cbbfd22f24
Stefan van der Walt <stefan@sun.ac.za>
2009-09-24 19:24:36 +0200

    Docs: add inheritance diagram extension.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,7 +26,7 @@ sys.path.append(os.path.join(curpath, '../ext'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath', 'numpydoc',
-              'sphinx.ext.autosummary']
+              'sphinx.ext.autosummary', 'sphinx.ext.inheritance_diagram']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
