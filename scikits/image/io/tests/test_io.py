33afe601aaccce33ca58edc7ee86d9b89a276ea1
Stefan van der Walt <stefan@sun.ac.za>
2009-10-12 08:51:18 +0200

    Move test data to global data dir and fix test.

--- scikits/image/io/tests/test_io.py
+++ scikits/image/io/tests/test_io.py
@@ -49,7 +49,7 @@ class TestMultiImage():
     def setUp(self):
         # This multipage TIF file was created with imagemagick:
         # convert im1.tif im2.tif -adjoin multipage.tif
-        self.img = io.MultiImage(os.path.join('data', 'multipage.tif'))
+        self.img = io.MultiImage(os.path.join(data_dir, 'multipage.tif'))
 
     def test_len(self):
         assert len(self.img) == 2
