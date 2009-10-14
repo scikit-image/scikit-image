d28e3e8555098f8cb1576a4bf622b0c29ba3d0d4
sccolbert <sccolbert@gmail.com>
2009-10-14 17:38:50 +0200

    fixed the setup.py, it works now.

--- scikits/image/opencv/setup.py
+++ scikits/image/opencv/setup.py
@@ -3,55 +3,47 @@
 import os
 import shutil
 
-current_dir = os.path.dirname(__file__)
-
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
+    
     config = Configuration('opencv', parent_package, top_path)
     
-    config.add_data_dir(os.path.join(current_dir, 'tests'))
+    config.add_data_dir('tests')
     
     # since distutils/cython has problems, we'll check to see if cython is 
     # installed and use that to rebuild the .c files, if not, we'll just build 
-    # direct from the included .c files
+    # directly from the included .c files
 
-    # if the cython compilation fails, it will overwrite the good .c files
-    # so we back them up first
-    
-    src = os.path.join(current_dir, 'opencv_cv.c')
-    dst = os.path.join(current_dir, 'opencv_cv.c.bak')
-    shutil.copy(src, dst)
-    src = os.path.join(current_dir, 'opencv_backend.c')
-    dst = os.path.join(current_dir, 'opencv_backend.c.bak')
-    shutil.copy(src, dst)    
-    
+    cython_files = ['opencv_backend.pyx', 'opencv_cv.pyx']
+       
     try:
-        import cython  
-        # no way to check the cython version number????
-        os.system('cython opencv_backend.pyx')
-        os.system('cython opencv_cv.pyx')
-        
-        # if the cython compilation failed, the resulting file 
-        # size will be very small ~ 78 bytes
-        f1 = os.path.join(current_dir, 'opencv_cv.c')
-        f2 = os.path.join(current_dir, 'opencv_backend.c')
-        size1 = os.path.getsize(f1)
-        size2 = os.path.getsize(f2)
-        assert size1 > 100 and size2 > 100
-    except:
-        print 'Cython compilation failed. Building from backups.'
-        src = os.path.join(current_dir, 'opencv_cv.c.bak')
-        dst = os.path.join(current_dir, 'opencv_cv.c')
-        shutil.copy(src, dst)
-        src = os.path.join(current_dir, 'opencv_backend.c.bak')
-        dst = os.path.join(current_dir, 'opencv_backend.c')
-        shutil.copy(src, dst)
-        
-    config.add_extension('opencv_backend', sources=['opencv_backend.c'],
-                         include_dirs=[get_numpy_include_dirs()])
+        import Cython        
+        for pyxfile in cython_files:
+            # make a backup of the good c files
+            c_file = pyxfile.rstrip('pyx') + 'c'
+            src = c_file
+            dst = c_file + '.bak'
+            shutil.copy(src, dst)
+            
+            # run cython compiler
+            os.system('cython ' + pyxfile)
+            
+            # if the file is small, cython compilation failed
+            size = os.path.getsize(c_file)
+            if size < 100:
+                print 'Cython compilation failed. Restoring from backup.'
+                # restore from backup
+                shutil.copy(dst, src)                
+                       
+    except ImportError:
+        # if cython is not found, we just build from the include .c files
+        pass
     
-    config.add_extension('opencv_cv', sources=['opencv_cv.c'],
-                         include_dirs=[get_numpy_include_dirs()])
+    for pyxfile in cython_files:
+        c_file = pyxfile.rstrip('pyx') + 'c'
+        config.add_extension(pyxfile.rstrip('.pyx'), 
+                                 sources=[c_file],
+                                 include_dirs=[get_numpy_include_dirs()])             
         
     return config
 
