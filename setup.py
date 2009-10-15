340ec9b1c3c7b437bfd8e3288401afeab5196925
Stefan van der Walt <stefan@sun.ac.za>
2009-10-15 08:36:19 +0200

    Clean up and fix setup.

--- setup.py
+++ setup.py
@@ -9,9 +9,6 @@ Please refer to the online documentation at
 http://stefanv.github.com/scikits.image
 """
 
-import os
-import sys
-
 DISTNAME            = 'scikits.image'
 DESCRIPTION         = 'Image processing routines for SciPy'
 LONG_DESCRIPTION    = descr
@@ -22,39 +19,47 @@ LICENSE             = 'Modified BSD'
 DOWNLOAD_URL        = 'http://github.com/stefanv/scikits.image'
 VERSION             = '0.2dev'
 
+import os
 import setuptools
 from numpy.distutils.core import setup
 
-def configuration(parent_package='', top_path=None, package_name=DISTNAME):
+def configuration(parent_package='', top_path=None):
     if os.path.exists('MANIFEST'): os.remove('MANIFEST')
 
     from numpy.distutils.misc_util import Configuration
-    config = Configuration(package_name, parent_package, top_path,
-                           version = VERSION,
-                           maintainer  = MAINTAINER,
-                           maintainer_email = MAINTAINER_EMAIL,
-                           description = DESCRIPTION,
-                           license = LICENSE,
-                           url = URL,
-                           download_url = DOWNLOAD_URL,
-                           long_description = LONG_DESCRIPTION)
+    config = Configuration(None, parent_package, top_path,
+                           namespace_packages=['scikits'])
 
-    config.add_subpackage('scikits.image.opencv')
+    config.add_subpackage('scikits')
+    config.add_subpackage(DISTNAME)
+    config.add_data_files('scikits/__init__.py')
 
     return config
 
 if __name__ == "__main__":
-    setup(configuration = configuration,
-        install_requires = [],
-        namespace_packages = ['scikits'],
-        packages = setuptools.find_packages(),
-        include_package_data = True,
-        zip_safe = False, # the package can run out of an .egg file
+    setup(
+        name=DISTNAME,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        url=URL,
+        license=LICENSE,
+        download_url=DOWNLOAD_URL,
+        version=VERSION,
+
         classifiers =
             [ 'Development Status :: 1 - Planning',
               'Environment :: Console',
               'Intended Audience :: Developers',
               'Intended Audience :: Science/Research',
               'License :: OSI Approved :: BSD License',
-              'Topic :: Scientific/Engineering'])
+              'Topic :: Scientific/Engineering'],
 
+        configuration=configuration,
+        install_requires=[],
+        namespace_packages=['scikits'],
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        zip_safe=False, # the package can run out of an .egg file
+        )
