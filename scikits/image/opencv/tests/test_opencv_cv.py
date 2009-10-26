3bf33a0a46a2713b0384631b8c954d575ac968b0
Stefan van der Walt <stefan@sun.ac.za>
2009-10-26 13:34:13 +0200

    Fix opencv for systems without libraries installed.

--- scikits/image/opencv/tests/test_opencv_cv.py
+++ scikits/image/opencv/tests/test_opencv_cv.py
@@ -12,8 +12,7 @@ with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     from scikits.image.opencv import *
 
-opencv_skip = dec.skipif(cv is None,
-                         'OpenCV libraries not found')
+opencv_skip = dec.skipif(not loaded, 'OpenCV libraries not found')
 
 class OpenCVTest(object):
     lena_RGB_U8 = np.load(os.path.join(data_dir, 'lena_RGB_U8.npy'))
