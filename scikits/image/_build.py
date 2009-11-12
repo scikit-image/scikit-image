9d64d8d0dc223d522fe91ba55d7fba17a272354a
Stefan van der Walt <stefan@sun.ac.za>
2009-11-12 14:21:36 +0200

    setup: Fix Cython build.

--- scikits/image/_build.py
+++ scikits/image/_build.py
@@ -29,7 +29,7 @@ def cython(pyx_files, working_path=''):
             status = os.system(cmd)
 
             # if the resulting file is small, cython compilation failed
-            if status != 0 or os.path.getsize(c_file_new):
+            if status != 0 or os.path.getsize(c_file_new) < 100:
                 print "Cython compilation of %s failed. Falling back " \
                       "on pre-generated file." % os.path.basename(pyxfile)
                 continue
