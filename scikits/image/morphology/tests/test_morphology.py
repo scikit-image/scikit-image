870d2efc922f3da1cdf6afc553ba2c4e58dd4562
Stefan van der Walt <stefan@sun.ac.za>
2009-11-20 11:31:34 +0200

    morphology: Wrap long lines.

--- scikits/image/morphology/tests/test_morphology.py
+++ scikits/image/morphology/tests/test_morphology.py
@@ -22,31 +22,41 @@ class TestMorphology():
             k = k + 1
 
     def test_erode_diamond(self):
-        self.morph_worker(lena, "diamond-erode-matlab-output.npy", greyscale_erode, diamond)
+        self.morph_worker(lena, "diamond-erode-matlab-output.npy",
+                          greyscale_erode, diamond)
 
     def test_dilate_diamond(self):
-        self.morph_worker(lena, "diamond-dilate-matlab-output.npy", greyscale_dilate, diamond)
+        self.morph_worker(lena, "diamond-dilate-matlab-output.npy",
+                          greyscale_dilate, diamond)
 
     def test_open_diamond(self):
-        self.morph_worker(lena, "diamond-open-matlab-output.npy", greyscale_open, diamond)
+        self.morph_worker(lena, "diamond-open-matlab-output.npy",
+                          greyscale_open, diamond)
 
     def test_close_diamond(self):
-        self.morph_worker(lena, "diamond-close-matlab-output.npy", greyscale_close, diamond)
+        self.morph_worker(lena, "diamond-close-matlab-output.npy",
+                          greyscale_close, diamond)
 
     def test_tophat_diamond(self):
-        self.morph_worker(lena, "diamond-tophat-matlab-output.npy", greyscale_white_top_hat, diamond)
-        
+        self.morph_worker(lena, "diamond-tophat-matlab-output.npy",
+                          greyscale_white_top_hat, diamond)
+
     def test_bothat_diamond(self):
-        self.morph_worker(lena, "diamond-bothat-matlab-output.npy", greyscale_black_top_hat, diamond)
+        self.morph_worker(lena, "diamond-bothat-matlab-output.npy",
+                          greyscale_black_top_hat, diamond)
 
     def test_erode_disk(self):
-        self.morph_worker(lena, "disk-erode-matlab-output.npy", greyscale_erode, disk)
+        self.morph_worker(lena, "disk-erode-matlab-output.npy",
+                          greyscale_erode, disk)
 
     def test_dilate_disk(self):
-        self.morph_worker(lena, "disk-dilate-matlab-output.npy", greyscale_dilate, disk)
+        self.morph_worker(lena, "disk-dilate-matlab-output.npy",
+                          greyscale_dilate, disk)
 
     def test_open_disk(self):
-        self.morph_worker(lena, "disk-open-matlab-output.npy", greyscale_open, disk)
+        self.morph_worker(lena, "disk-open-matlab-output.npy",
+                          greyscale_open, disk)
 
     def test_close_disk(self):
-        self.morph_worker(lena, "disk-close-matlab-output.npy", greyscale_close, disk)
+        self.morph_worker(lena, "disk-close-matlab-output.npy",
+                          greyscale_close, disk)
