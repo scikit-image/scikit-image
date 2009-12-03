ec70fdad4835d73bac9907e9ba82da571a1ed10e
Stefan van der Walt <stefan@sun.ac.za>
2009-12-03 09:56:12 +0200

    docs: Add project title to header.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -116,7 +116,7 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'v%s documentation'%version
+html_title = 'scikits.image v%s docs' % version
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
