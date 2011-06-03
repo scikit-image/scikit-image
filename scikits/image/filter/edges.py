cab37488c8166f3ab494affca7fe4f53e48a5cc4
Pieter Holtzhausen <holtzhau@gmail.com>
2011-06-03 23:57:27 +0200

    Backend decorator implemented

--- scikits/image/filter/edges.py
+++ scikits/image/filter/edges.py
@@ -9,18 +9,42 @@ Original author: Lee Kamentsky
 
 """
 import numpy as np
-from scipy.ndimage import convolve, binary_erosion, generate_binary_structure
+from scipy.ndimage import convolve
+import sys
 
-def sobel(image, mask=None):
-    """Calculate the absolute magnitude Sobel to find edges.
+imports = {}
+def get_backend(backend):
+    submodule = __file__.split(".")[0] + "_%s" % backend
+    name = "backend.%s" % submodule
+    if name not in imports:
+        module = __import__(name, fromlist=[submodule])
+        imports[name] = module
+        return module
+    else:
+        return imports[name]
+    
+def add_backends(function):
+    def new_function(*args, **kwargs):
+        backend = kwargs.get("backend")
+        if "backend" in kwargs:
+            del kwargs["backend"]
+        if not backend:
+            return function(*args, **kwargs)
+        else:
+            return getattr(get_backend(backend), function.__name__)(*args, **kwargs)
+    return new_function
+    
+@add_backends
+def sobel(image, axis=None, output=None):
+    """Calculate the absolute magnitude Sobel to find the edges.
 
     Parameters
     ----------
     image : array_like, dtype=float
-        Image to process.
+        Image to process
     mask : array_like, dtype=bool, optional
-        An optional mask to limit the application to a certain area.
-
+        An optional mask to limit the application to a certain area
+    
     Returns
     -------
     output : ndarray
@@ -31,178 +55,68 @@ def sobel(image, mask=None):
     Take the square root of the sum of the squares of the horizontal and
     vertical Sobels to get a magnitude that's somewhat insensitive to
     direction.
-
-    Note that ``scipy.ndimage.sobel`` returns a directional Sobel which
-    has to be further processed to perform edge detection.
+    
+    Note that scipy's Sobel returns a directional Sobel which isn't
+    useful for edge detection in its raw form.
     """
-    return np.sqrt(hsobel(image, mask)**2 + vsobel(image, mask)**2)
+    if image.dtype == np.uint8:
+        output_type = np.int16
+    elif image.dtype == np.float32:
+        output_type = np.float32
+    if axis is None:
+        dx = np.empty(image.shape, dtype=np.float32)
+        dy = np.empty(image.shape, dtype=np.float32)
+        convolve(image, np.array([[ 1, 2, 1],
+                                  [ 0, 0, 0],
+                                  [-1,-2,-1]]), output=dx)
+        convolve(image, np.array([[ 1, 0,-1],
+                                  [ 2, 0,-2],
+                                  [ 1, 0,-1]]), output=dy)
+        if output:
+            output[:] = np.sqrt(dx ** 2 + dy ** 2)
+            return output
+        else:
+            return np.sqrt(dx ** 2 + dy ** 2)
+    elif axis == 0:
+        dx = np.empty(image.shape, dtype=output_type)
+        convolve(image, np.array([[ 1, 2, 1],
+                                  [ 0, 0, 0],
+                                  [-1,-2,-1]]), output=dx)
+        return dx
+    elif axis == 1:
+        dy = np.empty(image.shape, dtype=output_type)
+        convolve(image, np.array([[ 1, 0,-1],
+                                  [ 2, 0,-2],
+                                  [ 1, 0,-1]]), output=dy)
+        return dy
 
-def hsobel(image, mask=None):
-    """Find the horizontal edges of an image using the Sobel transform.
 
-    Parameters
-    ----------
-    image : array_like, dtype=float
-        Image to process.
-    mask : array_like, dtype=bool, optional
-        An optional mask to limit the application to a certain area.
+if __name__ == "__main__":
+    import os, time
+    from scikits.image import data_dir
+    from scikits.image import io
+    from scikits.image.color import rgb2gray
+    io.use_plugin("gtk")
+    #image = rgb2gray(io.imread(os.path.join(data_dir,"lena512.png"))).astype(np.uint8)
+    image = rgb2gray(io.imread(os.path.join(data_dir,"lena512.png"))).astype(np.float32)
+    #image = np.zeros((2000, 2000), dtype=np.float32)
+    t = time.time()
+    # imports the opencv backend making subsequential calls faster
+    get_backend("opencv")
+    backends = [None, "opencv", "opencl"]
+    mod =__import__("backend.edges_opencv", fromlist=["edges_opencv"])
+    for backend in backends:
+        t = time.time()
+        output0 = sobel(image, axis=None, backend=backend)
+        print backend, time.time() - t
+        io.imshow(output0.astype(np.uint8))
+       
+    io.show()
+    
+#    hprewitt = np.abs(convolve(image, np.array([[ 1, 1, 1],
+#                                              [ 0, 0, 0],
+#                                              [-1,-1,-1]]).astype(float) / 3.0))                              
+#    vprewitt = np.abs(convolve(image, np.array([[ 1, 0,-1],
+#                                              [ 1, 0,-1],
+#                                              [ 1, 0,-1]]).astype(float) / 3.0))
 
-    Returns
-    -------
-    output : ndarray
-      The Sobel edge map.
-
-    Notes
-    -----
-    We use the following kernel and return the absolute value of the
-    result at each point::
-
-      1   2   1
-      0   0   0
-     -1  -2  -1
-
-    """
-    if mask is None:
-        mask = np.ones(image.shape, bool)
-    big_mask = binary_erosion(mask,
-                              generate_binary_structure(2, 2),
-                              border_value = 0)
-    result = np.abs(convolve(image.astype(float),
-                             np.array([[ 1, 2, 1],
-                                       [ 0, 0, 0],
-                                       [-1,-2,-1]]).astype(float) / 4.0))
-    result[big_mask == False] = 0
-    return result
-
-def vsobel(image, mask=None):
-    """Find the vertical edges of an image using the Sobel transform.
-
-    Parameters
-    ----------
-    image : array_like, dtype=float
-        Image to process
-    mask : array_like, dtype=bool, optional
-        An optional mask to limit the application to a certain area
-
-    Returns
-    -------
-    output : ndarray
-      The Sobel edge map.
-
-    Notes
-    -----
-    We use the following kernel and return the absolute value of the
-    result at each point::
-
-      1   0  -1
-      2   0  -2
-      1   0  -1
-
-    """
-    if mask is None:
-        mask = np.ones(image.shape, bool)
-    big_mask = binary_erosion(mask,
-                              generate_binary_structure(2, 2),
-                              border_value=0)
-    result = np.abs(convolve(image.astype(float),
-                             np.array([[1, 0, -1],
-                                       [2, 0, -2],
-                                       [1, 0, -1]]).astype(float) / 4.0))
-    result[big_mask == False] = 0
-    return result
-
-def prewitt(image, mask=None):
-    """Find the edge magnitude using the Prewitt transform.
-
-    Parameters
-    ----------
-    image : array_like, dtype=float
-        Image to process.
-    mask : array_like, dtype=bool, optional
-        An optional mask to limit the application to a certain area.
-
-    Returns
-    -------
-    output : ndarray
-      The Prewitt edge map.
-
-    Notes
-    -----
-    Return the square root of the sum of squares of the horizontal
-    and vertical Prewitt transforms.
-    """
-    return np.sqrt(hprewitt(image, mask) ** 2 + vprewitt(image, mask) ** 2)
-
-def hprewitt(image, mask=None):
-    """Find the horizontal edges of an image using the Prewitt transform.
-
-    Parameters
-    ----------
-    image : array_like, dtype=float
-        Image to process.
-    mask : array_like, dtype=bool, optional
-        An optional mask to limit the application to a certain area.
-
-    Returns
-    -------
-    output : ndarray
-      The Prewitt edge map.
-
-    Notes
-    -----
-    We use the following kernel and return the absolute value of the
-    result at each point::
-
-      1   1   1
-      0   0   0
-     -1  -1  -1
-
-    """
-    if mask is None:
-        mask = np.ones(image.shape, bool)
-    big_mask = binary_erosion(mask,
-                              generate_binary_structure(2, 2),
-                              border_value=0)
-    result = np.abs(convolve(image.astype(float),
-                             np.array([[ 1, 1, 1],
-                                       [ 0, 0, 0],
-                                       [-1,-1,-1]]).astype(float) / 3.0))
-    result[big_mask == False] = 0
-    return result
-
-def vprewitt(image, mask=None):
-    """Find the vertical edges of an image using the Prewitt transform.
-
-    Parameters
-    ----------
-    image : array_like, dtype=float
-        Image to process.
-    mask : array_like, dtype=bool, optional
-        An optional mask to limit the application to a certain area.
-
-    Returns
-    -------
-    output : ndarray
-      The Prewitt edge map.
-
-    Notes
-    -----
-    We use the following kernel and return the absolute value of the
-    result at each point::
-
-      1   0  -1
-      1   0  -1
-      1   0  -1
-
-    """
-    if mask is None:
-        mask = np.ones(image.shape, bool)
-    big_mask = binary_erosion(mask,
-                              generate_binary_structure(2, 2),
-                              border_value=0)
-    result = np.abs(convolve(image.astype(float),
-                             np.array([[1, 0, -1],
-                                       [1, 0, -1],
-                                       [1, 0, -1]]).astype(float) / 3.0))
-    result[big_mask == False] = 0
-    return result
