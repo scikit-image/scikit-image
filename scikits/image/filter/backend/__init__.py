cc8f993d23feb4b575a1f7795170b23a4bff983e
Pieter Holtzhausen <holtzhau@gmail.com>
2011-07-08 23:01:31 +0200

    init.py file for backend

---
+++ scikits/image/filter/backend/__init__.py
@@ -0,0 +1,6 @@
+from scikits.image.backend.backend import register_backend
+register_backend("opencl", "scikits.image.filter", "scikits.image.filter.backend.edges_opencl.sobel")
+register_backend("opencl", "scikits.image.filter", "scikits.image.filter.backend.edges_opencl.test")
+
+register_backend("opencv", "scikits.image.filter", "scikits.image.filter.backend.edges_opencv.sobel")
+register_backend("opencv", "scikits.image.filter", "scikits.image.filter.backend.edges_opencv.test")
