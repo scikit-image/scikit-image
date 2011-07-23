4daac0dc1b86331243be3179abf22a316a0e578b
Pieter Holtzhausen <holtzhau@gmail.com>
2011-07-23 17:56:34 +0200

    Namespace correction

--- scikits/image/filter/backend/__init__.py
+++ scikits/image/filter/backend/__init__.py
@@ -1,3 +1,3 @@
-from scikits.image.backend.backend import register
+from scikits.image.backend import register
 register(backend="opencl", module="scikits.image.filter.backend.edges_opencl", functions=["sobel"])
 register(backend="opencv", module="scikits.image.filter.backend.edges_opencv", functions=["sobel", "test"])
