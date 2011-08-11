662789463a4995a0a843aa78fb96d0b53abb5077
Pieter Holtzhausen <holtzhau@gmail.com>
2011-08-11 16:25:00 +0200

    Implemented cython version of integral images.

--- scikits/image/detection/_template.pyx
+++ scikits/image/detection/_template.pyx
@@ -9,142 +9,83 @@ from scipy.signal import fftconvolve
 cdef extern from "math.h":
     double sqrt(double x)
     double fabs(double x)
-    
-cdef extern from "template.h":
-    void iimg2(float *image, double *ii2, int imH, int imW)
-
-cdef void integral_image2(float *image, double *ii2, int imH, int imW):
-	double *prev_line = ii2;
-	double s
-	int x, y
-	*ii2 = (*image)*(*image); 
-	ii2 += 1
-	image += 1;
-	for (x = 1; x < imH; x++):
-		*ii2 = (*image)*(*image) + *(ii2-1);
-		ii2+=1; image+=1;
-	for (y = 1; y < imW; y++):
-		s = 0;
-		for (x = 0; x < imH; x++):
-			s += *image * *image;
-			*ii2 = s + *prev_line;
-			ii2+=1
-			image+=1
-			prev_line+=1
 
 @cython.boundscheck(False)
-cpdef np.ndarray integral_image(np.ndarray X, with_squared=False, squared=False):
-    """Summed area table / integral image.
+cdef void integral_image(float *image, double *ii, int width, int height):
+    cdef double *prev_line = ii
+    cdef double s
+    cdef int x, y
+    ii[0] = image[0]
+    ii += 1
+    image += 1
+    for x in range(1, height):
+        ii[0] = (image[0])
+        ii[0] += (ii-1)[0]
+        ii += 1
+        image += 1
+    for y in range(1, width):
+        s = 0
+        for x in range(0, height):
+            s += image[0]
+            ii[0] = s
+            ii[0] += prev_line[0]
+            ii += 1
+            image += 1
+            prev_line += 1
 
-    The integral image contains the sum of all elements above and to the
-    left of it, i.e.:
-
-    .. math::
-
-       S[m, n] = \sum_{i \leq m} \sum_{j \leq n} X[i, j]
-
-    Parameters
-    ----------
-    X : ndarray of uint8
-        Input image.
-
-    Returns
-    -------
-    S : ndarray
-        Summed area table.
-
-    References
-    ----------
-    .. [1] F.C. Crow, "Summed-area tables for texture mapping,"
-           ACM SIGGRAPH Computer Graphics, vol. 18, 1984, pp. 207-212.
-
-    """
-    cdef int s_rows, s_cols
-    s_rows = X.shape[0]
-    s_cols = X.shape[1]
-
-    cdef np.ndarray[np.uint64_t, ndim=2] S
-    cdef np.ndarray[np.uint64_t, ndim=2] S2
-    
-    if squared:
-        S2 = np.zeros((s_rows, s_cols), dtype=np.uint64)  
-        # First column
-        S2[0, 0] = X[0, 0] * X[0, 0]
-        for m in range(1, s_rows):
-            S2[m, 0] = X[m, 0] * X[m, 0] + S2[m - 1, 0]
-        # First row
-        for n in range(1, s_cols):
-            S2[0, n] = X[0, n] * X[0, n] + S2[0, n - 1]
-        for m in range(1, s_rows):
-            for n in range(1, s_cols):
-                    S2[m, n] = S2[m - 1, n] + S2[m, n - 1] \
-                                  - S2[m - 1, n - 1] + X[m, n] * X[m, n]
-        return S2
-    elif not with_squared:
-        S = np.zeros((s_rows, s_cols), dtype=np.uint64)
-        # First column
-        S[0, 0] = X[0, 0]
-        for m in range(1, s_rows):
-            S[m, 0] = X[m, 0] + S[m - 1, 0]
-        # First row
-        for n in range(1, s_cols):
-            S[0, n] = X[0, n] + S[0, n - 1]
-        for m in range(1, s_rows):
-            for n in range(1, s_cols):
-                    S[m, n] = S[m - 1, n] + S[m, n - 1] \
-                                  - S[m - 1, n - 1] + X[m, n]
-        return S
-    else:
-        S = np.zeros((s_rows, s_cols), dtype=np.uint64)
-        S2 = np.zeros((s_rows, s_cols), dtype=np.uint64)  
-        # First column
-        S[0, 0] = X[0, 0]
-        S2[0, 0] = X[0, 0] ** 2
-        for m in range(1, s_rows):
-            S[m, 0] = X[m, 0] + S[m - 1, 0]
-            S2[m, 0] = X[m, 0] ** 2 + S2[m - 1, 0]
-        # First row
-        for n in range(1, s_cols):
-            S[0, n] = X[0, n] + S[0, n - 1]
-            S2[0, n] = X[0, n] ** 2 + S2[0, n - 1]
-        for m in range(1, s_rows):
-            for n in range(1, s_cols):
-                    S[m, n] = S[m - 1, n] + S[m, n - 1] \
-                                  - S[m - 1, n - 1] + X[m, n] ** 2
-                    S2[m, n] = S2[m - 1, n] + S2[m, n - 1] \
-                                  - S2[m - 1, n - 1] + X[m, n] ** 2                                  
-        return (S, S2)
-
+@cython.boundscheck(False)
+cdef void integral_image2(float *image, double *ii2, int width, int height):
+    cdef double *prev_line = ii2
+    cdef double s
+    cdef int x, y
+    ii2[0] = (image[0])*(image[0])
+    ii2 += 1
+    image += 1
+    for x in range(1, height):
+        ii2[0] = (image[0]) * (image[0])
+        ii2[0] += (ii2-1)[0]
+        ii2 += 1
+        image += 1
+    for y in range(1, width):
+        s = 0
+        for x in range(0, height):
+            s += (image[0]) * (image[0])
+            ii2[0] = s
+            ii2[0] += prev_line[0]
+            ii2 += 1
+            image += 1
+            prev_line += 1
 
 @cython.boundscheck(False)
 def match_template(np.ndarray [float, ndim=2, mode="c"] image, np.ndarray[float, ndim=2, mode="c"] template):
-    #out = np.empty((image.shape[0] - template.shape[0] + 1,image.shape[1] - template.shape[0] + 1), dtype=np.float32)
+    # convolve the image with template by frequency domain multiplication
     cdef np.ndarray result = np.ascontiguousarray(fftconvolve(image, template, mode="valid"))
-    cdef np.ndarray integral = np.empty((image.shape[0]+1, image.shape[1]+1))
+    # calculate squared integral images used for normalization
     cdef np.ndarray integral_sqr = np.empty((image.shape[0], image.shape[1]))
-    size = image.shape
-    template_size = template.shape
-    #cv.Integral(image, integral, integral_sqr)
-    #integral_sqr = integral_image(image, squared=True)
-    
     cdef np.ndarray integral_sqr2 = np.empty((image.shape[0], image.shape[1]))
-    iimg2(<float*>image.data, <double*>integral_sqr.data, image.shape[1], image.shape[0])
-    
+    integral_image2(<float*>image.data, <double*>integral_sqr.data, image.shape[0], image.shape[1])
+    # use inversed area for accuracy
     cdef double inv_area = 1.0 / (template.shape[0] * template.shape[1])
+    # calculate template norm according to the following:
+    # variance ** 2 = 1/K Sigma[(x_k - mean) ** 2] = 1/K Sigma[x_k ** 2] - mean ** 2
     cdef double template_norm = sqrt((np.std(template) ** 2 + np.mean(template) ** 2)) / sqrt(inv_area)
     cdef int sqstep = integral_sqr.strides[0] / sizeof(double)
+    # define window of template size in squared integral image
     cdef double* q0 = <double*> integral_sqr.data
     cdef double* q1 = q0 + template.shape[0] 
     cdef double* q2 = <double*>integral_sqr.data + template.shape[1] * sqstep
     cdef double* q3 = q2 + template.shape[0]
-    cdef int i, j, k, index
+    cdef int i, j, index
     cdef double num, window_sum2, normed
+    # move window through convolution results, normalizing in the process
     for i in range(result.shape[0]):
         index = i * sqstep;
         for j in range(result.shape[1]):
+            # calculate squared template window sum in the image
             window_sum2 = q0[index] - q1[index] - q2[index] + q3[index]
             normed = sqrt(window_sum2) * template_norm
             num = result[i, j]
+            # enforce some limits
             if fabs(num) < normed:
                 num /= normed
             elif fabs(num) < normed*1.125:
--- scikits/image/detection/template.py
+++ scikits/image/detection/template.py
@@ -2,77 +2,60 @@
 """
 import numpy as np
 import cv
+import _template
 
 def match_template_cv(image, template, out=None):
-    """Calculate the absolute magnitude Sobel to find edges.
+    """Finds a template in an image using normalized correlation.
 
     Parameters
     ----------
     image : array_like, dtype=float
         Image to process.
-    mask : array_like, dtype=bool, optional
-        An optional mask to limit the application to a certain area.
-
+    template : array_like, dtype=float
+        Template to locate.
+    out: array_like, dtype=float, optional
+        Optional destination.
     Returns
     -------
-    output : ndarray
-      The Sobel edge map.
+    output : ndarray, dtype=float
+        Correlation results between 0.0 and 1.0. Maximum indicating most probable match.
     """
     if out == None:
         size = image.shape
         template_size = template.shape
         out = np.empty((size[0] - template_size[0] + 1,size[1] - template_size[0] + 1), dtype=np.float32)
-
-    cv.MatchTemplate(image, template, out, cv.CV_TM_CCORR)
-    
-    index = np.argmin(out)
-    print "min cv", np.min(out), index 
     cv.MatchTemplate(image, template, out, cv.CV_TM_CCORR_NORMED)
-    index = np.argmax(out)
-    print "max cv", np.max(out), index 
-#    y, x = np.unravel_index(index, out.shape)
-#    print "y x", y, x
-#    print out[y, x]
-#    print out[y-2:y+2, x-2:x+2]
-#    result = np.array(fftconvolve(image, template, mode="valid"))
-#    print "max sc", np.min(result), np.argmin(result)
-#    print result[y, x]
-#    print "delta", np.sum(result-out)
     return out
 
-from scipy.signal import fftconvolve
-import _template
 
-def match_template(image, template, out=None):
+def match_template(image, template):
+    """Finds a template in an image using normalized correlation.
+
+    Parameters
+    ----------
+    image : array_like, dtype=float
+        Image to process.
+    template : array_like, dtype=float
+        Template to locate.
+    Returns
+    -------
+    output : ndarray, dtype=float
+        Correlation results between 0.0 and 1.0. Maximum indicating most probable match.
+    """
     return _template.match_template(image, template)
-#    if out == None:
-#        size = image.shape
-#        template_size = template.shape
-##        out = np.empty((size[0] - template_size[0] + 1,size[1] - template_size[0] + 1), dtype=np.float32)
-#    result = np.array(fftconvolve(image, template, mode="valid"))
-#    integral = np.empty((result.shape[0]+1, result.shape[1]+1))
-#    integral_sqr = np.empty((result.shape[0]+1, result.shape[1]+1))
-#    cv.Integral(result, integral, integral_sqr)
-#    template_sum2 = np.std(template) ** 2
-#    template_norm = np.mean(template) ** 2
-#    print integral.strides
-    
-#    return result
+
 
 if __name__ == "__main__":
     import scikits.image.io as io
     import time
-        
     template = io.imread("../../../target.bmp").astype(np.float32)
     image = io.imread("../../../source.bmp").astype(np.float32)
     r = match_template_cv(image, template)
-#    print r[1, 1]
-#    print np.argmax(r)
-#    y, x = np.unravel_index(np.argmax(r), r.shape)
-#    print x, y
-#    print np.max(r)
+
     t = time.time()
     result = match_template(image, template)
     print time.time() - t
     index = np.argmax(result)
     print "max sc", np.max(result), index 
+    y, x = np.unravel_index(np.argmax(r), r.shape)
+    print x, y
