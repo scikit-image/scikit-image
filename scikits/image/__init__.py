ae2b253289191b41b602f6d31865b2f8806b6eb3
Stefan van der Walt <stefan@sun.ac.za>
2011-08-20 21:34:54 -0700

    Use absolute path for scikits.image.data_dir.

--- scikits/image/__init__.py
+++ scikits/image/__init__.py
@@ -3,7 +3,7 @@
 
 import os.path as _osp
 
-data_dir = _osp.join(_osp.dirname(__file__), 'data')
+data_dir = _osp.abspath(_osp.join(_osp.dirname(__file__), 'data'))
 
 from version import version as __version__
 
