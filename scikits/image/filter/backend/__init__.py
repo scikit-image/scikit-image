5c05a54b94b562e529e45bfffe8d63ccbf08329e
Pieter Holtzhausen <holtzhau@gmail.com>
2011-08-22 10:47:59 +0200

    Removed test function remnants

--- scikits/image/filter/backend/__init__.py
+++ scikits/image/filter/backend/__init__.py
@@ -1,3 +1,3 @@
 from scikits.image.backend import register
 register(backend="opencl", module="scikits.image.filter", functions=["edges_opencl.sobel"])
-register(backend="opencv", module="scikits.image.filter", source="edges_opencv", functions=["sobel", "test"])
+register(backend="opencv", module="scikits.image.filter", source="edges_opencv", functions=["sobel"])
