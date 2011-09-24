1822365a2bd71d84be427b7b6454f78bfff5cd14
Tony S Yu <tsyu80@gmail.com>
2011-09-24 00:23:43 -0400

    DOC: fix copyright

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'scikits.image'
-copyright = u'2009, SciPy Developers'
+copyright = u'2011, scikits-image team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
