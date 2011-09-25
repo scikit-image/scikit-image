38aa44765d6fd683972a178f827ccf53ff1473e5
andy <andy@marvin>
2011-09-25 22:38:58 +0200

    Convenience functions to load example images.

--- scikits/image/data/__init__.py
+++ scikits/image/data/__init__.py
@@ -1,4 +1,24 @@
-import scipy.misc as _m
+"""Convinience functions to get sample data"""
 
-lena = _m.lena
+import os
 
+from ..io import imread
+from ...image import data_dir
+
+def camera():
+    """Example gray "camera" image, often used for segmentation
+    and denoising examples."""
+
+    return imread(os.path.join(data_dir, "camera.png"))
+
+def lena():
+    """Example "Lena" image. """
+    return imread(os.path.join(data_dir, "lena.png"))
+
+def checkerboard():
+    """Checkerboard image"""
+    return imread(os.path.join(data_dir, "chessboard_RGB.png"))
+
+def checkerboard_gray():
+    """Checkerboard image, only gray channel"""
+    return imread(os.path.join(data_dir, "chessboard_GRAY.png"))
