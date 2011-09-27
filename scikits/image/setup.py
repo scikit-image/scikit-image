604084231c396d0b0a681c93542f4a3c0afa991c
Stefan van der Walt <stefan@sun.ac.za>
2011-09-27 12:50:06 -0700

    BUG: Add 'util' subpackage.

--- scikits/image/setup.py
+++ scikits/image/setup.py
@@ -12,6 +12,7 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('filter')
     config.add_subpackage('transform')
     config.add_subpackage('data')
+    config.add_subpackage('util')
 
     def add_test_directories(arg, dirname, fnames):
         if dirname.split(os.path.sep)[-1] == 'tests':
