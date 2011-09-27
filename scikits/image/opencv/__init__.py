34b2000e459f36e64e87ac20ad1d7328ffaa2051
Stefan van der Walt <stefan@sun.ac.za>
2011-09-26 20:28:31 -0700

    DOC: Warn about OpenCV deprecation.

--- scikits/image/opencv/__init__.py
+++ scikits/image/opencv/__init__.py
@@ -1,5 +1,17 @@
 from opencv_constants import *
 
+from .. import get_log as _get_log
+
+_log = _get_log("scikits.image.opencv")
+_log.warn("""
+The scikits.image OpenCV wrappers will be removed in the next release.
+
+These wrappers were written before OpenCV's own allowed manipulation
+of NumPy arrays without copying.  Since they now do, please switch to
+the official bindings::
+
+  http://opencv.willowgarage.com/wiki/""")
+
 # Note: users should be able to import this module even if
 # the extensions are uncompiled or the opencv libraries unavailable.
 # In that case, the opencv functionality is simply unavailable.
