9da0a6509bcb1d5157f82463705a65b07dfeb497
Stefan van der Walt <stefan@sun.ac.za>
2011-09-28 10:41:05 -0700

    BUG: Add 'color' subpackage.

--- scikits/image/setup.py
+++ scikits/image/setup.py
@@ -13,6 +13,7 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('transform')
     config.add_subpackage('data')
     config.add_subpackage('util')
+    config.add_subpackage('color')
 
     def add_test_directories(arg, dirname, fnames):
         if dirname.split(os.path.sep)[-1] == 'tests':
