022aac779f0f52c4e25c0a7994f6a0237e475dc0
Riaan van den Dool <riaanvddool@gmail.com>
2011-10-10 11:52:07 +0200

    GDAL plugin added.

---
+++ scikits/image/io/_plugins/gdal_plugin.py
@@ -0,0 +1,19 @@
+__all__ = ['imread']
+
+import numpy as np
+
+try:
+    import osgeo.gdal as gdal
+except ImportError:
+    raise ImportError("The GDAL Library could not be found. "
+                      "Please refer to http://www.gdal.org/ "
+                      "for further instructions.")
+
+def imread(fname, dtype=None):
+    """Load an image from file.
+
+    """
+    ds = gdal.Open(fname)
+
+    return ds.ReadAsArray().astype(dtype)
+
