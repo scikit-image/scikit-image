bbe88734f6c900c27c2d8f0f593fcee0b35d0e6f
Andreas Mueller <amueller@ais.uni-bonn.de>
2011-10-18 16:55:56 +0200

    add qt plugin again.

--- scikits/image/io/__init__.py
+++ scikits/image/io/__init__.py
@@ -9,7 +9,7 @@ from _plugins import available as plugins
 from _plugins import info as plugin_info
 available_plugins = plugins()
 
-for preferred_plugin in ['pil', 'matplotlib', 'null']:
+for preferred_plugin in ['pil', 'matplotlib', 'qt', 'null']:
     if preferred_plugin in available_plugins:
         try:
             use_plugin(preferred_plugin)
