1c9c35af2fbe22298fdfc5337f340239d518a864
emmanuelle <emmanuelle.gouillart@nsup.org>
2011-10-23 11:06:56 +0200

    Medial axis transform (skeletonization) from cell profiler
     * medial_axis function in morphology.skeletonize
     * plot_medial_axis.py example for the gallery
    
    Compared to the skeletonize algorithm in morphology.skeletonize,
    medial_axis is faster because it processes all pixel in one pass.
    However, the resulting skeleton has more branches, that may be unwanted
    depending on the application.

--- scikits/image/morphology/_cpmorphology2.pyx
+++ scikits/image/morphology/_cpmorphology2.pyx
@@ -38,30 +38,49 @@ def skeletonize_loop(np.ndarray[dtype=np.uint8_t, ndim=2,
                                 negative_indices=False, mode='c'] order,
                      np.ndarray[dtype=np.uint8_t, ndim=1,
                                 negative_indices=False, mode='c'] table):
-    '''Inner loop of skeletonize function
+    """
+    Inner loop of skeletonize function
 
-    result - on input, the image to be skeletonized, on output the skeletonized
-             image
-    i,j    - the coordinates of each foreground pixel in the image
-    order  - the index of each pixel, in the order of processing
-    table  - the 512-element lookup table of values after transformation
+    Parameters
+    ----------
+
+    result: ndarray of uint8
+        On input, the image to be skeletonized, on output the skeletonized
+        image.
+    i, j: ndarrays
+        The coordinates of each foreground pixel in the image
+    order: ndarray
+        The index of each pixel, in the order of processing (order[0] is
+        the first pixel to process, etc.)
+    table: ndarray
+        The 512-element lookup table of values after transformation 
+        (whether to keep or not each configuration in a binary 3x3 array)
+
+    Notes
+    -----
 
     The loop determines whether each pixel in the image can be removed without
     changing the Euler number of the image. The pixels are ordered by
     increasing distance from the background which means a point nearer to
     the quench-line of the brushfire will be evaluated later than a
     point closer to the edge.
-    '''
+
+    Note that the neighbourhood of a pixel may evolve before the loop 
+    arrives at this pixel. This is why it is possible to compute the 
+    skeleton in only one pass, thanks to an adapted ordering of the
+    pixels.
+    """
     cdef:
         np.int32_t accumulator
-        np.int32_t index,order_index
-        np.int32_t ii,jj
+        np.int32_t index, order_index
+        np.int32_t ii, jj
 
     for index in range(order.shape[0]):
         accumulator = 16
         order_index = order[index]
         ii = i[order_index]
         jj = j[order_index]
+        # Compute the configuration around the pixel
         if ii > 0:
             if jj > 0 and result[ii - 1, jj - 1]:
                 accumulator += 1
@@ -74,13 +93,14 @@ def skeletonize_loop(np.ndarray[dtype=np.uint8_t, ndim=2,
             if jj < result.shape[1] - 1 and result[ii, jj + 1]:
                 accumulator += 32
             if ii < result.shape[0]-1:
-                if jj > 0 and result[ii+1,jj-1]:
+                if jj > 0 and result[ii + 1, jj - 1]:
                     accumulator += 64
-                if result[ii+1,jj]:
+                if result[ii + 1, jj]:
                     accumulator += 128
-                if jj < result.shape[1]-1 and result[ii+1,jj+1]:
+                if jj < result.shape[1] - 1 and result[ii + 1, jj + 1]:
                     accumulator += 256
-            result[ii,jj] = table[accumulator]
+            # Assign the value of table corresponding to the configuration
+            result[ii, jj] = table[accumulator]
 
 @cython.boundscheck(False)
 def table_lookup_index(np.ndarray[dtype=np.uint8_t, ndim=2,
@@ -198,112 +218,7 @@ def table_lookup_index(np.ndarray[dtype=np.uint8_t, ndim=2,
             indexer[i + 1, j_shape - 1] += 2
     return indexer
 
-@cython.boundscheck(False)
-def index_lookup(np.ndarray[dtype=np.int32_t, ndim=1, 
-                                negative_indices=False] index_i, 
-                 np.ndarray[dtype=np.int32_t, ndim=1, 
-                                negative_indices=False] index_j, 
-                 np.ndarray[dtype=np.uint32_t, ndim=2, 
-                                negative_indices=False] image,
-                 table_in, 
-                 iterations=None):
-    """
-    Perform a table lookup for only the indexed pixels
-    
-    For morphological operations that only convert 1 to 0, the set of
-    resulting pixels is always a subset of the input set. Therefore, when
-    repeating, it will be faster to operate only on the subsets especially
-    when the results are 1-d or 0-d objects.
-    
-    This function returns a new index_i and index_j array of the pixels
-    that survive the operation. The image is modified in-place to remove
-    the pixels that did not survive.
-    
-    index_i - an array of row indexes into the image.
-    index_j - a similarly-shaped array of column indexes.
-    image - the binary image: *NOTE* add a row and column of border values
-            to the original image to account for pixels on the edge of the
-            image.
-    iterations - # of iterations to do, default is "forever"
-    
-    The idea of index_lookup was taken from
-    http://blogs.mathworks.com/steve/2008/06/13/performance-optimization-for-applylut/
-    which, apparently, is how Matlab achieved its bwmorph speedup.
-    """
-    cdef:
-        np.ndarray[dtype=np.uint8_t, ndim=1, 
-            negative_indices=False] table = table_in.astype(np.uint8)
-        np.uint32_t center, hit_count, idx, indexer
-        np.int32_t idxi, idxj
-
-    if iterations == None:
-        # Worst case - remove one per iteration
-        iterations = len(index_i)
-
-    for i in range(iterations):
-        hit_count = len(index_i)
-        with nogil:
-            #
-            # For integer images (i.e., labels), a neighbor point is
-            # "background" if it doesn't match the central value. This
-            # lets adjacent labeled objects shrink independently of each
-            # other.
-            #
-            for 0 <= idx < hit_count:
-                idxi, idxj = index_i[idx], index_j[idx]
-                center = image[idxi, idxj]
-                indexer =   ((image[idxi - 1, idxj - 1] == center) * 1 +
-                             (image[idxi - 1, idxj]     == center) * 2 +
-                             (image[idxi - 1, idxj + 1] == center) * 4 +
-                             (image[idxi,     idxj - 1] == center) * 8 +
-                                    16 +
-                             (image[idxi,     idxj + 1] == center) * 32 +
-                             (image[idxi + 1, idxj - 1] == center) * 64 +
-                             (image[idxi + 1, idxj]     == center) * 128 +
-                             (image[idxi + 1, idxj + 1] == center) * 256)
-                if table[indexer] == 0:
-                    # mark for deletion
-                    index_i[idx] = -index_i[idx]
-
-            # remove marked pixels
-            for 0 <= idx < hit_count:
-                idxi, idxj = index_i[idx], index_j[idx]
-                if idxi < 0:
-                    image[-idxi, idxj] = 0
-
-        index_j = index_j[index_i >= 0]
-        index_i = index_i[index_i >= 0]
-        if len(index_i) == hit_count:
-            break
-
-    return (index_i, index_j)
-
-def prepare_for_index_lookup(image, border_value):
-    """
-    Return the index arrays of "1" pixels and an image with an added border
-    
-    The routine, index_lookup takes an array of i indexes, an array of
-    j indexes and an image guaranteed to be indexed successfully by 
-    index_<i,j>[:] +/- 1. This routine constructs an image with added border
-    pixels... evilly, the index, 0 - 1, lands on the border because of Python's
-    negative indexing convention.
-    """
-    if np.issubdtype(image.dtype, float):
-        image = image.astype(bool)
-    image_i, image_j = np.argwhere(image.astype(bool)).transpose().\
-                                    astype(np.int32) + 1
-    output_image = (np.ones(np.array(image.shape) + 2, image.dtype) \
-                    if border_value
-                    else np.zeros(np.array(image.shape) + 2, image.dtype))
-    output_image[1:image.shape[0] + 1, 1:image.shape[1] + 1] = image
-    return (image_i, image_j, output_image.astype(np.uint32))
 
 
-def extract_from_image_lookup(orig_image, index_i, index_j):
-    """
-    Extract only one pixel
-    """
-    output = np.zeros(orig_image.shape, orig_image.dtype)
-    output[index_i - 1, index_j - 1] = orig_image[index_i - 1, index_j - 1]
-    return output
+
 
