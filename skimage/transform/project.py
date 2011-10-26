5633b56fd5fb5c4aca08ebe13303ca313f518ce4
Neil Yager <neil_yager@yahoo.com>
2011-10-26 11:56:49 +0100

    Added examples for homography docstring

--- skimage/transform/project.py
+++ skimage/transform/project.py
@@ -23,21 +23,6 @@ def homography(image, H, output_shape=None, order=1,
     For each pixel, given its homogeneous coordinate :math:`\mathbf{x}
     = [x, y, 1]^T`, its target position is calculated by multiplying
     with the given matrix, :math:`H`, to give :math:`H \mathbf{x}`.
-    E.g., to rotate by theta degrees clockwise, the matrix should be
-
-    ::
-
-      [[cos(theta) -sin(theta) 0]
-       [sin(theta)  cos(theta) 0]
-       [0            0         1]]
-
-    or, to translate x by 10 and y by 20,
-
-    ::
-
-      [[1 0 10]
-       [0 1 20]
-       [0 0 1 ]].
 
     Parameters
     ----------
@@ -56,11 +41,49 @@ def homography(image, H, output_shape=None, order=1,
         Used in conjunction with mode 'constant', the value outside
         the image boundaries.
 
+    Examples
+    --------
+    >>> # rotate by 90 degrees
+    >>> x = np.arange(9, dtype=np.uint8).reshape((3, 3)) + 1
+    >>> x
+    array([[1, 2, 3],
+           [4, 5, 6],
+           [7, 8, 9]], dtype=uint8)
+    >>> theta = -np.pi/2
+    >>> M = np.array([[np.cos(theta),-np.sin(theta),0],
+    ...               [np.sin(theta), np.cos(theta),2],
+    ...               [0,             0,            1]])
+    >>> x90 = homography(x, M, order=1)
+    >>> x90
+    array([[3, 6, 9],
+           [2, 5, 8],
+           [1, 4, 7]], dtype=uint8)
+    >>> # translate right by 2 and down by 1
+    >>> y = np.zeros((5,5), dtype=np.uint8)
+    >>> y[1, 1] = 255
+    >>> y
+    array([[  0,   0,   0,   0,   0],
+           [  0, 255,   0,   0,   0],
+           [  0,   0,   0,   0,   0],
+           [  0,   0,   0,   0,   0],
+           [  0,   0,   0,   0,   0]], dtype=uint8)
+    >>> M = np.array([[ 1.,  0.,  2.],
+    ...               [ 0.,  1.,  1.],
+    ...               [ 0.,  0.,  1.]])
+    >>> y21 = homography(y, M, order=1)
+    >>> y21
+    array([[  0,   0,   0,   0,   0],
+           [  0,   0,   0,   0,   0],
+           [  0,   0,   0, 255,   0],
+           [  0,   0,   0,   0,   0],
+           [  0,   0,   0,   0,   0]], dtype=uint8)
+
+    
     """
     if image.ndim < 2:
         raise ValueError("Input must have more than 1 dimension.")
 
-    image = np.atleast_3d(image).astype(np.float64)
+    image = np.atleast_3d(image)
     ishape = np.array(image.shape)
     bands = ishape[2]
 
