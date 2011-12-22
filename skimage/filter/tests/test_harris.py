7ff98597e2cf1a92e4a1b957926a43fdfbb29e1e
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2011-12-22 22:08:27 +0100

    Small fixes on the Harris corner detector - documentation, method renaming etc

--- skimage/filter/tests/test_harris.py
+++ skimage/filter/tests/test_harris.py
@@ -1,23 +1,28 @@
 import numpy as np
 
-import unittest
+from skimage.filter import harris
+from skimage import img_as_float
 
-from skimage.filter import harris_corner_detector
 
-
-class TestHarris(unittest.TestCase):
-
+class TestHarris():
     def test_square_image(self):
         im = np.zeros((50, 50)).astype(float)
         im[:25, :25] = 1.
-        results = harris_corner_detector(im)
-        self.assertTrue(results.any())
-        self.assertTrue(len(results) == 1)
+        results = harris(im)
+        assert results.any()
+        assert len(results) == 1
 
     def test_noisy_square_image(self):
         im = np.zeros((50, 50)).astype(float)
         im[:25, :25] = 1.
         im = im + np.random.uniform(size=im.shape) * .5
-        results = harris_corner_detector(im)
+        results = harris(im)
         assert results.any()
         assert len(results) == 1
+
+    def test_squared_dot(self):
+        im = np.zeros((50, 50))
+        im[4:8, 4:8] = 1
+        im = img_as_float(im)
+        results = harris(im)
+        assert results == np.array([6, 6])
