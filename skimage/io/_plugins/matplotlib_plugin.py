f9907ffd21d99036c2de42fc97c886b8520cab96
Stefan van der Walt <stefan@sun.ac.za>
2012-01-27 17:02:21 -0800

    ENH: Use nearest neighbor interpolation by default in matplotlib plugin.

--- skimage/io/_plugins/matplotlib_plugin.py
+++ skimage/io/_plugins/matplotlib_plugin.py
@@ -1,4 +1,11 @@
-from matplotlib.pyplot import imshow, show, imread
+import matplotlib.pyplot as plt
+
+def imshow(*args, **kwargs):
+    kwargs.setdefault('interpolation', 'nearest')
+    plt.imshow(*args, **kwargs)
+
+imread = plt.imread
+show = plt.show
 
 def _app_show():
     show()
