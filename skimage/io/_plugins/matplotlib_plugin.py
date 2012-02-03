bcfb8d372751b552e3e3d6b6315db75c310e7801
Stefan van der Walt <stefan@sun.ac.za>
2012-02-02 17:06:06 -0800

    ENH: When displaying images using matplotlib, use the grey colormap by default.

--- skimage/io/_plugins/matplotlib_plugin.py
+++ skimage/io/_plugins/matplotlib_plugin.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 def imshow(*args, **kwargs):
     kwargs.setdefault('interpolation', 'nearest')
+    kwargs.setdefault('cmap', 'gray')
     plt.imshow(*args, **kwargs)
 
 imread = plt.imread
