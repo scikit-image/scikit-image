7d6bf0c6d9db4fd5d94ccbfb85f655985fa9f123
Tony S Yu <tsyu80@gmail.com>
2012-02-09 00:02:57 -0500

    DOC: Use CSS to style versions sidebar.
    Summary:
    
    * Label current version with an html id so it can be highlighted in CSS.
    * Note: Switched out arrow indicator for a disc b/c it's easier (unicode bullets are not well-supported).
    * Move script file to theme and use standard Sphinx theming to include javascript file.

--- doc/source/themes/agogo/static/docversions.js
+++ doc/source/themes/agogo/static/docversions.js
@@ -4,13 +4,14 @@ function insert_version_links() {
     document.write('<ul class="versions">\n');
 
     for (i = 0; i < labels.length; i++){
-        document.write('<li>');
+        open_list = '<li>'
         if (typeof(DOCUMENTATION_OPTIONS) !== 'undefined') {
             if ((DOCUMENTATION_OPTIONS['VERSION'] == labels[i]) ||
                 (DOCUMENTATION_OPTIONS['VERSION'].match(/dev$/) && (i == 0))) {
-                document.write('&#9656;&nbsp;');
+                open_list = '<li id="current">'
             }
         }
+        document.write(open_list);
         document.write('<a href="URL">skimage VERSION</a> </li>\n'
                         .replace('VERSION', labels[i])
                         .replace('URL', 'http://scikits-image.org/docs/' + labels[i]));
