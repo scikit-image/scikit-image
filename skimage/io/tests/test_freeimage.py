5d276146ee8762569888976145cefc0360cd79a9
Stefan van der Walt <stefan@sun.ac.za>
2012-02-10 15:34:26 -0800

    BUG: Fix FreeImage tests.

--- skimage/io/tests/test_freeimage.py
+++ skimage/io/tests/test_freeimage.py
@@ -1,25 +1,32 @@
 import os
 import skimage as si
 import skimage.io as sio
-import skimage.io._plugins.freeimage_plugin as fi
 
 from numpy.testing import *
+from numpy.testing.decorators import skipif
 
+try:
+    import skimage.io._plugins.freeimage_plugin as fi
+    FI_available = True
+except OSError:
+    FI_available = False
 
+@skipif(not FI_available)
 def test_read():
     sio.use_plugin('freeimage', 'imread')
     img = sio.imread(os.path.join(si.data_dir, 'color.png'))
     assert img.shape == (370, 371, 3)
     assert all(img[274,135] == [0, 130, 253])
 
+@skipif(not FI_available)
 def test_metadata():
     meta = fi.read_metadata(os.path.join(si.data_dir, 'multipage.tif'))
-    assert meta[('EXIF_MAIN', 'BitsPerSample')] == 8
+    assert meta[('EXIF_MAIN', 'Orientation')] == 1
     assert meta[('EXIF_MAIN', 'Software')].startswith('ImageMagick')
 
     meta = fi.read_multipage_metadata(os.path.join(si.data_dir, 'multipage.tif'))
     assert len(meta) == 2
-    assert meta[0][('EXIF_MAIN', 'BitsPerSample')] == 8
+    assert meta[0][('EXIF_MAIN', 'Orientation')] == 1
     assert meta[1][('EXIF_MAIN', 'Software')].startswith('ImageMagick')
 
 
