60906a0b4786f6ef62f9a874a64e5722b6821323
Nicolas Pinto <pinto@alum.mit.edu>
2012-02-14 19:45:38 -0500

    DOC: update view_as_blocks and gabors_from_lena example using comments from @stevanv (PR #138)

--- doc/examples/plot_gabors_from_lena.py
+++ doc/examples/plot_gabors_from_lena.py
@@ -7,20 +7,20 @@ Gabors / Primary Visual Cortex "Simple Cells" from Lena
 
 How to build a (bio-plausible) "sparse" dictionary (or 'codebook', or
 'filterbank') for e.g. image classification without any fancy math and
-with just standard python scientific librairies?
+with just standard python scientific libraries?
 
 Please find below a short answer ;-)
 
 This simple example shows how to get Gabor-like filters [1]_ using just
 the famous Lena image. Gabor filters are good approximations of the
 "Simple Cells" [2]_ receptive fields [3]_ found in the mammalian primary
-visual cortex (V1) (for details, see e.g. the Nobel-prize winning work of Hubel
-& Wiesel done in the 60s).
+visual cortex (V1) (for details, see e.g. the Nobel-prize winning work
+of Hubel & Wiesel done in the 60s [4]_ [5]_).
 
-Here we use McQueen's 'kmeans' algorithm [4]_, as a simple bio-plausible
-hebbian-like learning rule and we apply it (a) to patches of the
-original Lena image (retinal projection), and (b) to patches of an
-LGN-like [5]_ Lena image using a simple difference of gaussians (DoG)
+Here we use McQueen's 'kmeans' algorithm [6]_, as a simple biologically
+plausible hebbian-like learning rule and we apply it (a) to patches of
+the original Lena image (retinal projection), and (b) to patches of an
+LGN-like [7]_ Lena image using a simple difference of gaussians (DoG)
 approximation.
 
 Enjoy ;-) And keep in mind that getting Gabors on natural image patches
@@ -31,39 +31,36 @@ is not rocket science.
 .. [3] http://en.wikipedia.org/wiki/Receptive_field
 .. [4] http://en.wikipedia.org/wiki/K-means_clustering
 .. [5] http://en.wikipedia.org/wiki/Lateral_geniculate_nucleus
-
-References
-----------
-D. H. Hubel and T. N. Wiesel Receptive Fields of Single Neurones in the
-Cat's Striate Cortex J. Physiol. pp. 574-591 (148) 1959
-
-D. H. Hubel and T. N. Wiesel Receptive Fields, Binocular Interaction and
-Functional Architecture in the Cat's Visual Cortex J. Physiol. 160 pp.
-106-154 1962
+.. [6] D. H. Hubel and T. N. Wiesel Receptive Fields of Single Neurones
+in the Cat's Striate Cortex J. Physiol. pp. 574-591 (148) 1959
+.. [7] D. H. Hubel and T. N. Wiesel Receptive Fields, Binocular
+Interaction and Functional Architecture in the Cat's Visual Cortex J.
+Physiol. 160 pp.  106-154 1962
 """
 
 import numpy as np
-from scipy import misc
 from scipy.cluster.vq import kmeans2
+from scipy import ndimage as ndi
 import matplotlib.pyplot as plt
 
+from skimage import data
+from skimage import color
 from skimage.util.shape import view_as_windows
 from skimage.util.montage import montage2d
-from scipy import ndimage as ndi
 
 np.random.seed(42)
 
 patch_shape = 8, 8
 n_filters = 49
 
-lena = misc.lena() / 255.
+lena = color.rgb2gray(data.lena()) / 255.
 
 # -- filterbank1 on original Lena
 patches1 = view_as_windows(lena, patch_shape)
 patches1 = patches1.reshape(-1, patch_shape[0] * patch_shape[1])[::8]
 fb1, _ = kmeans2(patches1, n_filters, minit='points')
 fb1 = fb1.reshape((-1,) + patch_shape)
-fb1_montage = montage2d(fb1)
+fb1_montage = montage2d(fb1, rescale_intensity=True)
 
 # -- filterbank2 LGN-like Lena
 lena_dog = ndi.gaussian_filter(lena, .5) - ndi.gaussian_filter(lena, 1)
@@ -71,7 +68,7 @@ patches2 = view_as_windows(lena_dog, patch_shape)
 patches2 = patches2.reshape(-1, patch_shape[0] * patch_shape[1])[::8]
 fb2, _ = kmeans2(patches2, n_filters, minit='points')
 fb2 = fb2.reshape((-1,) + patch_shape)
-fb2_montage = montage2d(fb2)
+fb2_montage = montage2d(fb2, rescale_intensity=True)
 
 # --
 plt.figure(figsize=(9, 3))
