d86dc4eed886ed7709996b3f2eb763158a3bece1
Stefan van der Walt <stefan@sun.ac.za>
2012-02-26 20:56:53 -0800

    PKG: Update version to 0.5.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ MAINTAINER_EMAIL    = 'stefan@sun.ac.za'
 URL                 = 'http://scikits-image.org'
 LICENSE             = 'Modified BSD'
 DOWNLOAD_URL        = 'http://github.com/scikits-image/scikits-image'
-VERSION             = '0.5dev'
+VERSION             = '0.5'
 
 import os
 import setuptools
