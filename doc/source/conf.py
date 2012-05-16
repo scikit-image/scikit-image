ca25314310d5c58d136d6d86e968248c13331927
Tony S Yu <tsyu80@gmail.com>
2012-05-15 22:47:30 -0400

    Remove gen_rst

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,12 +23,6 @@ sys.path.append(os.path.join(curpath, '..', 'ext'))
 
 # -- General configuration -----------------------------------------------------
 
-try:
-    import gen_rst
-except:
-    pass
-
-
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath', 'numpydoc',
