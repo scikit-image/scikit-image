4d999c313f0a78bf585e494b9f22c082c1a3fbab
tangofoxtrotmike <tangofoxtrotmike@github.com>
2012-05-22 17:35:11 +1000

    feature.hog(): use y,x coords consistently, now works on non-square images

--- skimage/feature/tests/test_hog.py
+++ skimage/feature/tests/test_hog.py
@@ -5,12 +5,12 @@ from skimage.feature import hog
 
 def test_histogram_of_oriented_gradients():
     # Replace with skimage.data.lena() after merge
-    img = scipy.misc.lena().astype(np.int8) 
+    img = scipy.misc.lena()[:256,:].astype(np.int8) 
     
     fd = hog(img, orientations=9, pixels_per_cell=(8, 8), 
              cells_per_block=(1, 1))
 
-    assert len(fd) == 9 * (512//8) ** 2
+    assert len(fd) == 9 * (256//8) * (512//8)
     
 if __name__ == '__main__':
     from numpy.testing import run_module_suite
