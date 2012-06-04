da3239d0ff879ad4823e3fcb468a34cf215551cc
Johannes Schönberger <jschoenberger@demuc.de>
2012-06-04 15:27:11 +0200

    remove old import

--- skimage/transform/__init__.py
+++ skimage/transform/__init__.py
@@ -1,7 +1,6 @@
 from .hough_transform import *
 from .radon_transform import *
 from .finite_radon_transform import *
-from .project import *
 from ._project import homography as fast_homography
 from .integral import *
 from .geometric import warp, make_tform, swirl, homography
