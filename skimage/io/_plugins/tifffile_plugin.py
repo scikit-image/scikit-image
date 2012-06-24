37d1fd47233241e682123538af3df5965e618d6c
Stefan van der Walt <stefan@sun.ac.za>
2012-06-24 16:52:22 -0700

    DOC: Provide correct link for tifffile plugin.

--- skimage/io/_plugins/tifffile_plugin.py
+++ skimage/io/_plugins/tifffile_plugin.py
@@ -3,4 +3,4 @@ try:
 except ImportError:
     raise ImportError("The tifffile module could not be found.\n"
         "It can be obtained at "
-        "<http://www.lfd.uci.edu/~gohlke/code/tifffile.py.html>\n")
+        "<http://www.lfd.uci.edu/~gohlke/code/tifffile.py>\n")
