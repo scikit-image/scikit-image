249fa149ef11e81d8a82af54d94a49e724cef3af
Stefan van der Walt <stefan@sun.ac.za>
2012-06-24 21:46:12 -0700

    PKG: Start 0.7 development cycle.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ MAINTAINER_EMAIL    = 'stefan@sun.ac.za'
 URL                 = 'http://scikits-image.org'
 LICENSE             = 'Modified BSD'
 DOWNLOAD_URL        = 'http://github.com/scikits-image/scikits-image'
-VERSION             = '0.6'
+VERSION             = '0.7dev'
 
 import os
 import setuptools
