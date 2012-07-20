5101aa2a389c005e3be0eba22c5c5c168493fcf4
Jonathan J. Helmus <jjhelmus@gmail.com>
2012-07-20 15:23:25 -0400

    added test for ImageCollection slicing

--- skimage/io/tests/test_collection.py
+++ skimage/io/tests/test_collection.py
@@ -43,6 +43,12 @@ class TestImageCollection():
         assert_raises(IndexError, return_img, num)
         assert_raises(IndexError, return_img, -num - 1)
 
+    def test_slicing(self):
+        assert type(self.collection[:] is ImageCollection)
+        assert len(self.collection[:]) == 2
+        assert_array_almost_equal(self.collection[0], self.collection[:1])
+        assert_array_almost_equal(self.collection[1], self.collection[1:])
+
     def test_files_property(self):
         assert isinstance(self.collection.files, list)
 
