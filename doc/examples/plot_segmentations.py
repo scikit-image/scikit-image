ea02bc6170d96e2bd5b610ae24c73dd77b400efa
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-08-05 23:11:50 +0100

    make figure smaller again.

--- doc/examples/plot_segmentations.py
+++ doc/examples/plot_segmentations.py
@@ -77,7 +77,7 @@ print("Slic number of segments: %d" % len(np.unique(segments_slic)))
 print("Quickshift number of segments: %d" % len(np.unique(segments_quick)))
 
 fig, ax = plt.subplots(1, 3)
-fig.set_size_inches(15, 6, forward=True)
+fig.set_size_inches(8, 3, forward=True)
 plt.subplots_adjust(0.05, 0.05, 0.95, 0.95, 0.05, 0.05)
 
 ax[0].imshow(visualize_boundaries(img, segments_fz))
