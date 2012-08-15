40c58f33334698d92bcc42e42225ed8c8e6034c5
Johannes Schönberger <hannesschoenberger@gmail.com>
2012-08-15 08:48:40 +0200

    add subdivision of polygonal curves using B-Splines

--- skimage/measure/__init__.py
+++ skimage/measure/__init__.py
@@ -1,4 +1,4 @@
 from .find_contours import find_contours
 from ._regionprops import regionprops, perimeter
 from ._structural_similarity import structural_similarity
-from ._polygon import approximate_polygon
+from ._polygon import approximate_polygon, subdivide_polygon
