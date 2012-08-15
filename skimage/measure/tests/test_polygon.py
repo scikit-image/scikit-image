8296f857ee21c7f50aec2534a2ac63e2e1053136
Johannes Schönberger <hannesschoenberger@gmail.com>
2012-08-15 09:47:34 +0200

    add test case for non-circular subdivision of polygons

--- skimage/measure/tests/test_polygon.py
+++ skimage/measure/tests/test_polygon.py
@@ -1,6 +1,6 @@
 import numpy as np
 from skimage.measure import approximate_polygon, subdivide_polygon
-
+from skimage.measure._polygon import _SUBDIVISION_MASKS
 
 square = np.array([
     [0, 0], [0, 1], [0, 2], [0, 3],
@@ -26,9 +26,14 @@ def test_approximate_polygon():
 
 def test_subdivide_polygon():
     for degree in range(1, 7):
+        # test circular
         out = subdivide_polygon(square, degree)
         np.testing.assert_array_equal(out[-1], out[0])
         np.testing.assert_equal(out.shape[0], 2 * square.shape[0] - 1)
+        # test non-circular
+        out = subdivide_polygon(square[:-1], degree)
+        mask_len = len(_SUBDIVISION_MASKS[degree][0])
+        np.testing.assert_equal(out.shape[0], 2 * (square.shape[0] - mask_len))
 
 
 if __name__ == "__main__":
