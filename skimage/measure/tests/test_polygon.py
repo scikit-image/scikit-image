f267f656664ea4040cfe78de7c2da90487ef10c0
Johannes Schönberger <hannesschoenberger@gmail.com>
2012-08-15 09:04:06 +0200

    add test case for subdivision of polygons

--- skimage/measure/tests/test_polygon.py
+++ skimage/measure/tests/test_polygon.py
@@ -1,15 +1,16 @@
 import numpy as np
-from skimage.measure import approximate_polygon
+from skimage.measure import approximate_polygon, subdivide_polygon
+
+
+square = np.array([
+    [0, 0], [0, 1], [0, 2], [0, 3],
+    [1, 3], [2, 3], [3, 3],
+    [3, 2], [3, 1], [3, 0],
+    [2, 0], [1, 0], [0, 0]
+])
 
 
 def test_approximate_polygon():
-    square = np.array([
-        [0, 0], [0, 1], [0, 2], [0, 3],
-        [1, 3], [2, 3], [3, 3],
-        [3, 2], [3, 1], [3, 0],
-        [2, 0], [1, 0], [0, 0]
-    ])
-
     out = approximate_polygon(square, 0.1)
     np.testing.assert_array_equal(out, square[(0, 3, 6, 9, 12), :])
 
@@ -21,6 +22,13 @@ def test_approximate_polygon():
 
     out = approximate_polygon(square, -1)
     np.testing.assert_array_equal(out, square)
+
+
+def test_subdivide_polygon():
+    for degree in range(1, 7):
+        out = subdivide_polygon(square, degree)
+        np.testing.assert_array_equal(out[-1], out[0])
+        np.testing.assert_equal(out.shape[0], 2 * square.shape[0] - 1)
 
 
 if __name__ == "__main__":
