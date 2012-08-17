1ea41173b821124e2836e6731ff3d32f98c6e542
Tony S Yu <tsyu80@gmail.com>
2012-08-16 22:42:27 -0400

    BUG: Rename files with similar function, python module, and cython extension names.

--- skimage/feature/__init__.py
+++ skimage/feature/__init__.py
@@ -1,5 +1,5 @@
 from ._hog import hog
-from .greycomatrix import greycomatrix, greycoprops
+from ._greycomatrix import greycomatrix, greycoprops
 from .peak import peak_local_max
 from ._harris import harris
 from .template import match_template
