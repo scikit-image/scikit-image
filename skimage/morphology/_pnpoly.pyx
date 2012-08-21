878554ac35ab5cb8013c7b0b02971a9381a03c79
Johannes Schönberger <hannesschoenberger@gmail.com>
2012-08-21 15:45:18 +0200

    Fix indentation

--- skimage/morphology/_pnpoly.pyx
+++ skimage/morphology/_pnpoly.pyx
@@ -48,39 +48,39 @@ def grid_points_inside_poly(shape, verts):
 
 
 def points_inside_poly(points, verts):
-     """Test whether points lie inside a polygon.
+    """Test whether points lie inside a polygon.
 
-     Parameters
-     ----------
-     points : (N, 2) array
-         Input points, ``(x, y)``.
-     verts : (M, 2) array
-         Vertices of the polygon, sorted either clockwise or anti-clockwise.
-         The first point may (but does not need to be) duplicated.
+    Parameters
+    ----------
+    points : (N, 2) array
+     Input points, ``(x, y)``.
+    verts : (M, 2) array
+     Vertices of the polygon, sorted either clockwise or anti-clockwise.
+     The first point may (but does not need to be) duplicated.
 
-     Returns
-     -------
-     mask : (N,) array of bool
-         True if corresponding point is inside the polygon.
+    Returns
+    -------
+    mask : (N,) array of bool
+     True if corresponding point is inside the polygon.
 
-     """
-     cdef np.ndarray[np.double_t, ndim=1, mode="c"] x, y, vx, vy
+    """
+    cdef np.ndarray[np.double_t, ndim=1, mode="c"] x, y, vx, vy
 
-     points = np.asarray(points)
-     verts = np.asarray(verts)
+    points = np.asarray(points)
+    verts = np.asarray(verts)
 
-     x = points[:, 0].astype(np.double)
-     y = points[:, 1].astype(np.double)
+    x = points[:, 0].astype(np.double)
+    y = points[:, 1].astype(np.double)
 
-     vx = verts[:, 0].astype(np.double)
-     vy = verts[:, 1].astype(np.double)
+    vx = verts[:, 0].astype(np.double)
+    vy = verts[:, 1].astype(np.double)
 
-     cdef np.ndarray[np.uint8_t, ndim=1] out = \
-          np.zeros(x.shape[0], dtype=np.uint8)
+    cdef np.ndarray[np.uint8_t, ndim=1] out = \
+      np.zeros(x.shape[0], dtype=np.uint8)
 
-     points_in_polygon(vx.shape[0], <double*>vx.data, <double*>vy.data,
-             x.shape[0], <double*>x.data, <double*>y.data,
-             <unsigned char*>out.data)
+    points_in_polygon(vx.shape[0], <double*>vx.data, <double*>vy.data,
+         x.shape[0], <double*>x.data, <double*>y.data,
+         <unsigned char*>out.data)
 
-     return out.astype(bool)
+    return out.astype(bool)
 
