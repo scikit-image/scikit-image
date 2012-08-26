6ec4c21cf7af09401aabadff79898fe783efe9bd
Johannes Schönberger <hannesschoenberger@gmail.com>
2012-08-26 22:55:35 +0200

    Fix import of rotate function

--- skimage/transform/__init__.py
+++ skimage/transform/__init__.py
@@ -5,4 +5,4 @@ from .integral import *
 from ._geometric import (estimate_transform,
                          SimilarityTransform, AffineTransform,
                          ProjectiveTransform, PolynomialTransform)
-from ._warps import warp, warp_coords, swirl, homography
+from ._warps import warp, warp_coords, rotate, swirl, homography
