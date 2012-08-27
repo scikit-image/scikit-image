501c401ddad3b2614abd99d3513a8c19fea25eca
Johannes Schönberger <hannesschoenberger@gmail.com>
2012-08-27 18:05:18 +0200

    Move image transform functions to _geometric file

--- skimage/transform/__init__.py
+++ skimage/transform/__init__.py
@@ -2,7 +2,7 @@ from .hough_transform import *
 from .radon_transform import *
 from .finite_radon_transform import *
 from .integral import *
-from ._geometric import (estimate_transform,
+from ._geometric import (warp, warp_coords, estimate_transform,
                          SimilarityTransform, AffineTransform,
                          ProjectiveTransform, PolynomialTransform)
-from ._warps import warp, warp_coords, rotate, swirl, homography
+from ._warps import rotate, swirl, homography
