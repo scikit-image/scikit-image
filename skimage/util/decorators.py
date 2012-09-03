3c2349b7db637b7e4048bca528d953beed63cc5a
Johannes Schönberger <hannesschoenberger@gmail.com>
2012-09-03 14:29:57 +0200

    Add support for array conversion

--- skimage/util/decorators.py
+++ skimage/util/decorators.py
@@ -1,7 +1,8 @@
 import numpy as np
+from skimage.util.dtype import convert
 
 
-def check_array(arg_pos, kwarg_name, channels=None, dtype=None):
+def check_array(arg_pos, kwarg_name, channels=None, dtype=None, convert=False):
     """Decorator to check input parameters of a function.
 
     If the input matches the specified conditions the decorated function is
@@ -17,6 +18,8 @@ def check_array(arg_pos, kwarg_name, channels=None, dtype=None):
         Check array for number of channels. Default is None.
     dtype : str or ``numpy.dtype``
         Check array for specific dtype. Default is None.
+    convert : bool
+        Automatically convert image to specified `dtype`. Default is False.
 
     """
 
@@ -33,19 +36,31 @@ def check_array(arg_pos, kwarg_name, channels=None, dtype=None):
             error_msg = None
 
             if channels is not None:
-                if (
-                    (channels > 1 and array.ndim == 2)
-                    or (array.ndim == 3 and array.shape[2] != channels)
-                ):
+                if array.ndim > 2:
+                    array_channels = array.shape[2]
+                else:
+                    array_channels = 1
+                if array_channels != channels or array.ndim not in (2, 3):
                     error_msg = 'invalid number of channels'
 
             if dtype is not None:
                 if np.dtype(dtype) != array.dtype:
-                    error_msg = 'invalid dtype'
+                    if convert:
+                        array = convert(array, dtype)
+                    else:
+                        error_msg = 'invalid dtype'
 
             if error_msg is not None:
                 raise ValueError(error_msg + ' for %s' % image_desc)
 
+            # pass updated array
+            if kwarg_name in kwargs:
+                kwargs[kwarg_name] = array
+            else:
+                args = list(args)
+                args[arg_pos] = array
+
             return func(*args, **kwargs)
+
         return inner
     return wrapper
