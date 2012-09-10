8c4ba00b12133494dc7bca45d5275ee9ca770342
Johannes Schönberger <hannesschoenberger@gmail.com>
2012-09-10 20:59:19 +0200

    Move harris file to a combined interest point file

--- skimage/feature/__init__.py
+++ skimage/feature/__init__.py
@@ -1,5 +1,5 @@
 from ._hog import hog
 from .texture import greycomatrix, greycoprops, local_binary_pattern
 from .peak import peak_local_max
-from ._harris import harris
+from .interest import harris
 from .template import match_template
