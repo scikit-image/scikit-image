f2d5b109e90dde5ba50a5717428d91a85d85003f
Tony S Yu <tsyu80@gmail.com>
2012-09-13 22:15:14 -0400

    Add `assert_greater` compatibility function.
    Fix tests to work with nose < 1.1.3. Compatibility functions borrowed from scikit-learn.

---
+++ skimage/_shared/testing.py
@@ -0,0 +1,28 @@
+"""Testing utilities."""
+
+# Copyright (c) 2011 Pietro Berkes
+# License: Simplified BSD
+
+def _assert_less(a, b, msg=None):
+    message = "%r is not lower than %r" % (a, b)
+    if msg is not None:
+        message += ": " + msg
+    assert a < b, message
+
+
+def _assert_greater(a, b, msg=None):
+    message = "%r is not greater than %r" % (a, b)
+    if msg is not None:
+        message += ": " + msg
+    assert a > b, message
+
+
+try:
+    from nose.tools import assert_less
+except ImportError:
+    assert_less = _assert_less
+
+try:
+    from nose.tools import assert_greater
+except ImportError:
+    assert_greater = _assert_greater
