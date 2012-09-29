37d9f566216b3936a96d9cec878d5eaa44fc14ab
Tony S Yu <tsyu80@gmail.com>
2012-09-29 14:42:59 -0400

    PKG: Update version to 0.7.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ MAINTAINER_EMAIL    = 'stefan@sun.ac.za'
 URL                 = 'http://scikits-image.org'
 LICENSE             = 'Modified BSD'
 DOWNLOAD_URL        = 'http://github.com/scikits-image/scikits-image'
-VERSION             = '0.7dev'
+VERSION             = '0.7.0'
 PYTHON_VERSION      = (2, 5)
 DEPENDENCIES        = {
                         'numpy': (1, 6),
