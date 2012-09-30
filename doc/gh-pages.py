0250bb15f6acf6593ef63af0274ec921e16357f2
Tony S Yu <tsyu80@gmail.com>
2012-09-30 11:01:36 -0400

    PKG: Clarify instructions for gh-pages.
    Also, clean up some whitespace.

--- doc/gh-pages.py
+++ doc/gh-pages.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
     shutil.copytree(html_dir, dest)
     # copy pdf file into tree
     #shutil.copy(pjoin(pdf_dir, 'scikits.image.pdf'), pjoin(dest, 'scikits.image.pdf'))
-    
+
     try:
         cd(pages_dir)
         status = sh2('git status | head -1')
@@ -117,7 +117,7 @@ if __name__ == '__main__':
             e = 'On %r, git branch is %r, MUST be "gh-pages"' % (pages_dir,
                                                                  branch)
             raise RuntimeError(e)
-        sh("touch .nojekyll")        
+        sh("touch .nojekyll")
         sh('git add .nojekyll')
         sh('git add index.html')
         sh('git add %s' % tag)
@@ -131,4 +131,4 @@ if __name__ == '__main__':
 
     print
     print 'Now verify the build in: %r' % dest
-    print "If everything looks good, 'git push'"
+    print "If everything looks good, run 'git push' inside doc/gh-pages."
