483507f4dfa02a9c0d43b1c4f1cabb12e225c5b7
Olivier Debeir <odebeir@ulb.ac.be>
2012-10-04 17:15:10 +0200

    try to fix rank setup

--- skimage/setup.py
+++ skimage/setup.py
@@ -16,6 +16,7 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('io')
     config.add_subpackage('measure')
     config.add_subpackage('morphology')
+    config.add_subpackage('rank')
     config.add_subpackage('transform')
     config.add_subpackage('util')
     config.add_subpackage('segmentation')
