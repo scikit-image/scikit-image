b0807739efaa87e412d2c0d4a6efe3652e7acb13
Johannes Schönberger <hannesschoenberger@gmail.com>
2012-10-05 11:51:08 +0200

    Use common file for denoising filters

--- skimage/filter/__init__.py
+++ skimage/filter/__init__.py
@@ -3,6 +3,6 @@ from .ctmf import median_filter
 from ._canny import canny
 from .edges import (sobel, hsobel, vsobel, scharr, hscharr, vscharr, prewitt,
                     hprewitt, vprewitt)
-from ._tv_denoise import tv_denoise
+from .denoise import tv_denoise
 from ._rank_order import rank_order
 from .thresholding import threshold_otsu, threshold_adaptive
