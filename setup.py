b5d4409b9b6d0eaeeb358c59a40c1e87156fb7bc
Stefan van der Walt <stefan@sun.ac.za>
2012-10-13 23:29:26 -0700

    PKG: Set version to 0.7.2.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ MAINTAINER_EMAIL    = 'stefan@sun.ac.za'
 URL                 = 'http://scikit-image.org'
 LICENSE             = 'Modified BSD'
 DOWNLOAD_URL        = 'http://github.com/scikit-image/scikit-image'
-VERSION             = '0.8dev'
+VERSION             = '0.7.2'
 PYTHON_VERSION      = (2, 5)
 DEPENDENCIES        = {
                         'numpy': (1, 6),
