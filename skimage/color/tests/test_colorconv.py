4a0564a7697e6a0ff81163338ab3a28bdd6bddf2
Stefan van der Walt <stefan@sun.ac.za>
2012-10-16 23:33:29 -0700

    Add is_rgb and is_gray.

--- skimage/color/tests/test_colorconv.py
+++ skimage/color/tests/test_colorconv.py
@@ -25,10 +25,11 @@ from skimage.color import (
     convert_colorspace,
     rgb2grey, gray2rgb,
     xyz2lab, lab2xyz,
-    lab2rgb, rgb2lab
+    lab2rgb, rgb2lab,
+    is_rgb, is_gray
     )
 
-from skimage import data_dir
+from skimage import data_dir, data
 
 import colorsys
 
@@ -201,6 +202,17 @@ def test_gray2rgb_rgb():
     x = np.random.random((5, 5, 4))
     y = gray2rgb(x)
     assert_equal(x, y)
+
+
+def test_is_rgb():
+    color = data.lena()
+    gray = data.camera()
+
+    assert is_rgb(color)
+    assert not is_gray(color)
+
+    assert is_gray(gray)
+    assert not is_gray(color)
 
 
 if __name__ == "__main__":
