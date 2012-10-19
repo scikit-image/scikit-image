9f7a376e4dcf19886c6f7f9cea806ffafc8bf36d
Tony S Yu <tsyu80@gmail.com>
2012-10-19 15:21:31 -0400

    ENH: Add test and support for reading file URLs

--- skimage/io/tests/test_io.py
+++ skimage/io/tests/test_io.py
@@ -2,6 +2,7 @@ from numpy.testing import *
 import numpy as np
 
 import skimage.io as io
+from skimage import data_dir
 
 
 def test_stack_basic():
@@ -14,6 +15,13 @@ def test_stack_basic():
 @raises(ValueError)
 def test_stack_non_array():
     io.push([[1, 2, 3]])
+
+
+def test_imread_url():
+    image_url = 'file://%s/camera.png' % data_dir
+    image = io.imread(image_url)
+    assert image.shape == (512, 512)
+
 
 if __name__ == "__main__":
     run_module_suite()
