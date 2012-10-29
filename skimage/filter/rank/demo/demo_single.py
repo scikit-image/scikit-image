262e7f78f4ffe9145be2e1fcd8717cd4dde8da5d
Olivier Debeir <odebeir@ulb.ac.be>
2012-10-29 18:29:48 +0100

    small comparison with denoise_bilateral

--- skimage/filter/rank/demo/demo_single.py
+++ skimage/filter/rank/demo/demo_single.py
@@ -5,6 +5,7 @@ from skimage import data
 from skimage.morphology.selem import disk
 import skimage.filter.rank as rank
 
+from skimage.filter import denoise_bilateral
 
 if __name__ == '__main__':
     a8 = data.camera()
@@ -14,6 +15,16 @@ if __name__ == '__main__':
     f8= rank.percentile_autolevel(a8,selem,p0=.0,p1=1.)
     f16= rank.autolevel(a16,selem)
     f16p= rank.percentile_autolevel(a16,selem,p0=.0,p1=1.)
+
+    den = denoise_bilateral(a8,win_size=10,sigma_range=10,sigma_spatial=2)[:,:,0]
+    f16b= rank.bilateral_mean(a8.astype(np.uint16),disk(10),s0=10,s1=10)
+
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(den)
+    plt.subplot(1,2,2)
+    plt.imshow(f16b)
+    plt.show()
 
     print f16==f16p
 
