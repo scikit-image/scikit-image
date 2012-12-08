979a15cd9a417b030ef42b386646b12b8db218f5
Anders Boesen Lindbo Larsen <abll@imm.dtu.dk>
2012-12-08 22:37:36 +0100

    PEP8 style corrections.

--- skimage/feature/tests/test_daisy.py
+++ skimage/feature/tests/test_daisy.py
@@ -17,14 +17,16 @@ def test_daisy_desc_dims():
     rings = 2
     histograms = 4
     orientations = 3
-    descs = daisy(img, rings=rings, histograms=histograms, orientations=orientations)
-    assert(descs.shape[2] == (rings*histograms + 1)*orientations)
+    descs = daisy(img, rings=rings, histograms=histograms,
+                  orientations=orientations)
+    assert(descs.shape[2] == (rings * histograms + 1) * orientations)
 
     rings = 4
     histograms = 5
     orientations = 13
-    descs = daisy(img, rings=rings, histograms=histograms, orientations=orientations)
-    assert(descs.shape[2] == (rings*histograms + 1)*orientations)
+    descs = daisy(img, rings=rings, histograms=histograms,
+                  orientations=orientations)
+    assert(descs.shape[2] == (rings * histograms + 1) * orientations)
 
 
 def test_descs_shape():
@@ -32,15 +34,15 @@ def test_descs_shape():
     radius = 20
     step = 8
     descs = daisy(img, radius=radius, step=step)
-    assert(descs.shape[0] == ceil((img.shape[0]-radius*2)/float(step)))
-    assert(descs.shape[1] == ceil((img.shape[1]-radius*2)/float(step)))
+    assert(descs.shape[0] == ceil((img.shape[0] - radius * 2) / float(step)))
+    assert(descs.shape[1] == ceil((img.shape[1] - radius * 2) / float(step)))
 
-    img = img[:-1,:-2]
+    img = img[:-1, :-2]
     radius = 5
     step = 3
     descs = daisy(img, radius=radius, step=step)
-    assert(descs.shape[0] == ceil((img.shape[0]-radius*2)/float(step)))
-    assert(descs.shape[1] == ceil((img.shape[1]-radius*2)/float(step)))
+    assert(descs.shape[0] == ceil((img.shape[0] - radius * 2) / float(step)))
+    assert(descs.shape[1] == ceil((img.shape[1] - radius * 2) / float(step)))
 
 
 def test_daisy_incompatible_sigmas_and_radii():
@@ -56,14 +58,14 @@ def test_daisy_normalization():
     descs = daisy(img, normalization='l1')
     for i in range(descs.shape[0]):
         for j in range(descs.shape[1]):
-            assert_almost_equal(np.sum(descs[i,j,:]), 1)
+            assert_almost_equal(np.sum(descs[i, j, :]), 1)
     descs_ = daisy(img)
     assert_almost_equal(descs, descs_)
 
     descs = daisy(img, normalization='l2')
     for i in range(descs.shape[0]):
         for j in range(descs.shape[1]):
-            assert_almost_equal(sqrt(np.sum(descs[i,j,:]**2)), 1)
+            assert_almost_equal(sqrt(np.sum(descs[i, j, :] ** 2)), 1)
 
     orientations = 8
     descs = daisy(img, orientations=orientations, normalization='daisy')
@@ -72,13 +74,13 @@ def test_daisy_normalization():
         for j in range(descs.shape[1]):
             for k in range(0, desc_dims, orientations):
                 assert_almost_equal(sqrt(np.sum(
-                        descs[i,j,k:k+orientations]**2)), 1)
+                    descs[i, j, k:k + orientations] ** 2)), 1)
 
     img = np.zeros((50, 50))
     descs = daisy(img, normalization='off')
     for i in range(descs.shape[0]):
         for j in range(descs.shape[1]):
-            assert_almost_equal(np.sum(descs[i,j,:]), 0)
+            assert_almost_equal(np.sum(descs[i, j, :]), 0)
 
     assert_raises(ValueError, daisy, img, normalization='does_not_exist')
 
