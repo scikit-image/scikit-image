b3e8a25e933052313f96f5a1dd095c2213dfbe68
kuantkid <kuantkid@gmail.com>
2012-12-20 15:20:30 +0800

    COSMIT

--- skimage/segmentation/tests/test_graph_cut.py
+++ skimage/segmentation/tests/test_graph_cut.py
@@ -9,7 +9,7 @@ def test_all_finish_with_good_shape():
     img = img_as_float(lena()[::4, ::4])
     segments_slic = slic(img, ratio=10, n_segments=100, sigma=1)
     label_slic = normalized_cut(img, n_segments=5,
-                                mask=segments_slic,random_state=0)
+                                mask=segments_slic, random_state=0)
     # test callable mask
     label_callable = normalized_cut(img, n_segments=5,
                                     mask=lambda(image): slic(image, ratio=10,
@@ -19,7 +19,7 @@ def test_all_finish_with_good_shape():
     # test string mask
     label_string = normalized_cut(img, n_segments=5,
                                   mask="slic",
-                                  n_init_cluster=100,random_state=0)
+                                  n_init_cluster=100, random_state=0)
     assert (label_slic.shape == img.shape[0:2])
     assert (label_callable.shape == img.shape[0:2])
     assert (label_string.shape == img.shape[0:2])
@@ -27,7 +27,7 @@ def test_all_finish_with_good_shape():
     # test no mask is given
     label_no_mask = normalized_cut(img[::8, ::8],
                                    n_segments=5,
-                                   mask=None,random_state=0)
+                                   mask=None, random_state=0)
     assert (label_no_mask.shape == img[::8, ::8].shape[0:2])
 
 
