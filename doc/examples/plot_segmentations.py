5b91d70b547e19eba9868ae5bf34e4adc584f104
kuantkid <kuantkid@gmail.com>
2012-12-20 14:18:29 +0800

    ADD: add normalized cut to the __init__

--- doc/examples/plot_segmentations.py
+++ doc/examples/plot_segmentations.py
@@ -66,13 +66,13 @@ from skimage.data import lena
 from skimage.segmentation import felzenszwalb, slic, quickshift
 from skimage.segmentation import mark_boundaries
 from skimage.util import img_as_float
-import skimage.segmentation._graph_cut as gc
-reload(gc)
+import skimage.segmentation import normalized_cut
+
 img = img_as_float(lena()[::2, ::2])
 segments_fz = felzenszwalb(img, scale=100, sigma=0.5, min_size=50)
 segments_slic = slic(img, ratio=10, n_segments=250, sigma=1)
 segments_quick = quickshift(img, kernel_size=3, max_dist=6, ratio=0.5)
-segments_graph_cut = gc.normalized_cut(img, mask="slic", n_cluster = 10, n_init_cluster=100)
+segments_graph_cut = normalized_cut(img, mask="slic", n_cluster = 10, n_init_cluster=100)
 
 print("Felzenszwalb's number of segments: %d" % len(np.unique(segments_fz)))
 print("Slic number of segments: %d" % len(np.unique(segments_slic)))
--- skimage/segmentation/__init__.py
+++ skimage/segmentation/__init__.py
@@ -5,3 +5,4 @@ from ._quickshift import quickshift
 from .boundaries import find_boundaries, visualize_boundaries, mark_boundaries
 from ._clear_border import clear_border
 from ._join import join_segmentations, relabel_from_one
+from ._graph_cut import normalized_cut
