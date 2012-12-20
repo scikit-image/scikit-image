ee2b275703dfcb2fdb60cc4c2cda842696379cfa
kuantkid <kuantkid@gmail.com>
2012-12-20 14:55:42 +0800

    COSMIT

--- skimage/segmentation/tests/test_graph_cut.py
+++ skimage/segmentation/tests/test_graph_cut.py
@@ -11,24 +11,24 @@ def test_all_finish_with_good_shape():
     label_slic = normalized_cut(img, n_segments=5,
                                 mask=segments_slic)
     # test callable mask
-    label_callable = normalized_cut(img, n_segments = 5,
-                   mask=lambda(image):slic(image, ratio=10,
-                                           n_segments=100,
-                                           sigma=1))
+    label_callable = normalized_cut(img, n_segments=5,
+                                    mask=lambda(image): slic(image, ratio=10,
+                                                             n_segments=100,
+                                                             sigma=1))
     # test string mask
-    label_string = normalized_cut(img, n_segments = 10,
-                   mask = "slic",
-                   n_init_cluster = 100)
+    label_string = normalized_cut(img, n_segments=10,
+                                  mask="slic",
+                                  n_init_cluster=100)
     assert (label_slic.shape == img.shape[0:2])
     assert (label_callable.shape == img.shape[0:2])
     assert (label_string.shape == img.shape[0:2])
-    
+
     # test no mask is given
-    label_no_mask = normalized_cut(img[::8,::8],
-                                   n_segments = 5,
+    label_no_mask = normalized_cut(img[::8, ::8],
+                                   n_segments=5,
                                    mask=None)
-    assert (label_no_mask.shape == img[::8,::8].shape[0:2])
-    
+    assert (label_no_mask.shape == img[::8, ::8].shape[0:2])
+
 
 if __name__ == '__main__':
     from numpy import testing
