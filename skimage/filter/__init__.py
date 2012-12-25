236ef48ec97fe389aeaf5bfeb195863d59dd928f
Johannes Schönberger <hannesschoenberger@gmail.com>
2012-12-25 11:15:41 +0100

    Add original (Chambolle) implementation of TV denoising

--- skimage/filter/__init__.py
+++ skimage/filter/__init__.py
@@ -3,6 +3,7 @@ from .ctmf import median_filter
 from ._canny import canny
 from .edges import (sobel, hsobel, vsobel, scharr, hscharr, vscharr, prewitt,
                     hprewitt, vprewitt)
+from ._denoise import denoise_tv_chambolle, tv_denoise
 from ._denoise_cy import denoise_bilateral, denoise_tv_bregman
 from ._rank_order import rank_order
 from .thresholding import threshold_otsu, threshold_adaptive
