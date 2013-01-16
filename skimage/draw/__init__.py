ebc020fe01d38c50ad4485b76adb882c25e78c93
Johannes Schönberger <hannesschoenberger@gmail.com>
2013-01-16 20:01:41 +0100

    Wrap line and group imports

--- skimage/draw/__init__.py
+++ skimage/draw/__init__.py
@@ -1,2 +1,5 @@
-from ._draw import line, polygon, ellipse, circle, circle_perimeter, ellipse_perimeter, set_color
+from ._draw import line, polygon, ellipse, ellipse_perimeter, \
+                   circle, circle_perimeter, set_color
+
+
 bresenham = line
