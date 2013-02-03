3e5a210834f0330513d58814d55004abf7b069df
Michael Hansen <hansen.mike@gmail.com>
2013-02-02 19:25:54 -0500

    Made API match Greg's

--- skimage/novice/example.py
+++ skimage/novice/example.py
@@ -1,20 +1,20 @@
-from skimage.novice import picture  # special submodule for beginners
+from skimage import novice            # special submodule for beginners
 
-pic = picture.open('sample.png')    # create a picture object from a file
-print pic.format                    # pictures know their format...
-print pic.path                      # ...and where they came from...
-print pic.size                      # ...and their size
-print pic.width                     # 'width' and 'height' also exposed
-pic.size = (200, 250)               # changing size automatically resizes
-for pixel in pic:                   # can iterate over pixels
-    if ((pixel.red > 128) and           # pixels have RGB (values are 0-255)...
-        (pixel.x < pic.width)):     # ...and know where they are
-        pixel.red /= 2                  # pixel is an alias into the pic
+picture = novice.open('sample.png')   # create a picture object from a file
+print picture.format                  # pictures know their format...
+print picture.path                    # ...and where they came from...
+print picture.size                    # ...and their size
+print picture.width                   # 'width' and 'height' also exposed
+picture.size = (200, 250)             # changing size automatically resizes
+for pixel in picture:                 # can iterate over pixels
+    if ((pixel.red > 128) and         # pixels have RGB (values are 0-255)...
+        (pixel.x < picture.width)):   # ...and know where they are
+        pixel.red /= 2                # pixel is an alias into the picture
    
-print pic.modified                  # pictures know if their pixels are dirty
-print pic.path                      # picture no longer corresponds to file
-pic[0:20, 0:20] = (0, 0, 0)         # overwrite lower-left rectangle with black
-pic.save('sample-bluegreen.jpg')    # guess file type from suffix
-print pic.path                      # picture now corresponds to file
-print pic.format                    # ...has a different format
-print pic.modified                  # ...and is now in sync
+print picture.modified                # pictures know if their pixels are dirty
+print picture.path                    # picture no longer corresponds to file
+picture[0:20, 0:20] = (0, 0, 0)       # overwrite lower-left rectangle with black
+picture.save('sample-bluegreen.jpg')  # guess file type from suffix
+print picture.path                    # picture now corresponds to file
+print picture.format                  # ...has a different format
+print picture.modified                # ...and is now in sync
