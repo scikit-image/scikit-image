169b86ac6fe7abcfe40d4476fc38ab87f65236ef
Michael Hansen <hansen.mike@gmail.com>
2013-02-02 19:25:54 -0500

    Made API match Greg's

--- skimage/novice/__init__.py
+++ skimage/novice/__init__.py
@@ -1,1 +1,8 @@
-from .novice import picture, pixel, pixelgroup
+import os
+from .novice import picture
+
+def open(path):
+    """
+    Creates a new picture object from the given image path
+    """
+    return picture(os.path.abspath(path))
