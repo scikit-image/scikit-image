7bcc0e59d2ade5a30c6113a98c42006201e348c0
Michael Hansen <hansen.mike@gmail.com>
2013-02-05 21:00:45 -0500

    Implementing review changes

--- skimage/novice/novice.py
+++ skimage/novice/novice.py
@@ -1,59 +1,58 @@
-import os
-from PIL import Image
+import os, skimage, numpy as np
+import skimage.io, skimage.transform
 from itertools import islice, product
-from io import BytesIO
 
 # ================================================== 
 
-class pixel(object):
-    def __init__(self, pic, data, x, y, rgb):
-        self.__picture = pic
-        self.__data = data
-        self.__x = x
-        self.__y = y
-        self.__red = self.__validate(rgb[0])
-        self.__green = self.__validate(rgb[1])
-        self.__blue = self.__validate(rgb[2])
+class Pixel(object):
+    def __init__(self, pic, image, x, y, rgb):
+        self._picture = pic
+        self._image = image
+        self._x = x
+        self._y = y
+        self._red = self._validate(rgb[0])
+        self._green = self._validate(rgb[1])
+        self._blue = self._validate(rgb[2])
 
     @property
     def x(self):
         """Gets the horizontal location (left = 0)"""
-        return self.__x
+        return self._x
 
     @property
     def y(self):
         """Gets the vertical location (bottom = 0)"""
-        return self.__y
+        return self._y
 
     @property
     def red(self):
         """Gets or sets the red component"""
-        return self.__red
+        return self._red
 
     @red.setter
     def red(self, value):
-        self.__red = self.__validate(value)
-        self.__setpixel()
+        self._red = self._validate(value)
+        self._setpixel()
 
     @property
     def green(self):
         """Gets or sets the green component"""
-        return self.__green
+        return self._green
 
     @green.setter
     def green(self, value):
-        self.__green = self.__validate(value)
-        self.__setpixel()
+        self._green = self._validate(value)
+        self._setpixel()
 
     @property
     def blue(self):
         """Gets or sets the blue component"""
-        return self.__blue
+        return self._blue
 
     @blue.setter
     def blue(self, value):
-        self.__blue = self.__validate(value)
-        self.__setpixel()
+        self._blue = self._validate(value)
+        self._setpixel()
 
     @property
     def rgb(self):
@@ -63,14 +62,14 @@ class pixel(object):
     def rgb(self, value):
         """Gets or sets the color with an (r, g, b) tuple"""
         for v in value:
-            self.__validate(v)
+            self._validate(v)
 
-        self.__red = value[0]
-        self.__green = value[1]
-        self.__blue = value[2]
-        self.__setpixel()
+        self._red = value[0]
+        self._green = value[1]
+        self._blue = value[2]
+        self._setpixel()
 
-    def __validate(self, value):
+    def _validate(self, value):
         """Verifies that the pixel value is in [0, 255]"""
         try:
             value = int(value)
@@ -81,26 +80,26 @@ class pixel(object):
 
         return value
 
-    def __setpixel(self):
+    def _setpixel(self):
         """
         Sets the actual pixel value in the picture.
         NOTE: Using Cartesian coordinate system!
         """
-        self.__data[self.__x, self.__picture.height - self.__y - 1] = \
+        self._image[self._x, self._picture.height - self._y - 1] = \
                 (self.red, self.green, self.blue)
 
         # Modified pictures lose their paths
-        self.__picture._picture__path = None
-        self.__picture._picture__modified = True
+        self._picture._picture_path = None
+        self._picture._picture_modified = True
 
     def __repr__(self):
-        return "pixel (red: {0}, green: {1}, blue: {2})".format(self.red, self.green, self.blue)
+        return "Pixel (red: {0}, green: {1}, blue: {2})".format(self.red, self.green, self.blue)
 
 # ================================================== 
 
-class pixelgroup(object):
+class PixelGroup(object):
     def __init__(self, pixels):
-        self.__pixels = pixels
+        self._pixels = pixels
 
     @property
     def x(self):
@@ -153,66 +152,91 @@ class pixelgroup(object):
             p.rgb = value
 
     def __iter__(self):
-        return iter(self.__pixels)
+        return iter(self._pixels)
 
     def __repr__(self):
-        return "pixelgroup ({0} pixels)".format(len(self.__pixels))
+        return "PixelGroup ({0} pixels)".format(len(self._pixels))
 
 # ================================================== 
 
-class picture(object):
-    def __init__(self, path):
-        self.__path = path
-        image = Image.open(path)
-        self.__format = image.format
+class Picture(object):
+    def __init__(self, path=None, size=None, color=None):
+        """
+        If 'path' is provided, open that file (the normal case).
+        If 'size' is provided instead, create an image of that size.
+        If 'color' is provided as well as 'size', initialize the
+        created image to that color; otherwise, initialize to black.
+        Cannot provide both 'path' and 'size'.
+        Can only provide 'color' if 'size' provided.
+        """
 
-        # We convert the image to RGB automatically so
-        # (r, g, b) tuples can be used everywhere.
-        self.__image = image.convert("RGB")
-        self.__data = self.__image.load()
-        self.__modified = False
+        # Can only provide either path or size, but not both.
+        if path and size:
+            assert False, "Can only provide either path or size, not both."
 
-        self.__inflation = 1
+        # Opening a particular file.  Convert the image to RGB
+        # automatically so (r, g, b) tuples can be used
+        # everywhere.
+        elif path is not None:
+            self._image = skimage.img_as_ubyte(skimage.io.imread(path))
+            self._path = path
+            self._format = None
+
+        # Creating a particular size of image.
+        elif size is not None:
+            if color is None:
+                color = (0, 0, 0)
+            self._image = np.zeros((size[0], size[1], 3), "uint8")
+            self._image[:, :] = color
+            self._path = None
+            self._format = None
+
+        # Must have provided either 'path' or 'size'.
+        else:
+            assert False, "Must provide one of path or size."
+
+        # Common setup.
+        self._modified = False
+        self._inflation = 1
 
     def save(self, path):
         """Saves the picture to the given path."""
-        self.__inflate(self.__image).save(path)
-        self.__modified = False
-        self.__path = os.path.abspath(path)
+        skimage.io.imsave(path, self._inflate(self._image))
+        self._modified = False
+        self._path = os.path.abspath(path)
 
         # Need to re-open the image to get the format
         # for some reason (likely because we converted to RGB).
-        self.__format = Image.open(path).format
+        #self._format = Image.open(path).format
 
     @property
     def path(self):
         """Gets the path of the picture"""
-        return self.__path
+        return self._path
 
     @property
     def modified(self):
         """Gets a value indicating if the picture has changed"""
-        return self.__modified
+        return self._modified
 
     @property
     def format(self):
         """Gets the format of the picture (e.g., PNG)"""
-        return self.__format
+        return self._format
 
     @property
     def size(self):
         """Gets or sets the size of the picture with a (width, height) tuple"""
-        return self.__image.size
+        return self._image.shape[:2]
 
     @size.setter
     def size(self, value):
         try:
             # Don't resize if no change in size
             if (value[0] != self.width) or (value[1] != self.height):
-                self.__image = self.__image.resize(value)
-                self.__data = self.__image.load()
-                self.__modified = True
-                self.__path = None
+                self._image = skimage.img_as_ubyte(skimage.transform.resize(self._image, value, order=0))
+                self._modified = True
+                self._path = None
         except TypeError:
             raise TypeError("Expected (width, height), but got {0} instead!".format(value))
 
@@ -237,7 +261,7 @@ class picture(object):
     @property
     def inflation(self):
         """Gets or sets the inflation factor (each pixel will be an NxN block for factor N)"""
-        return self.__inflation
+        return self._inflation
 
     @inflation.setter
     def inflation(self, value):
@@ -245,39 +269,30 @@ class picture(object):
             value = int(value)
             if value < 0:
                 raise ValueError()
-            self.__inflation = value
+            self._inflation = value
         except ValueError:
             raise ValueError("Expected inflation factor to be an integer greater than zero")
 
     def show(self):
         """Returns an IPython image of the picture for display in an IPython notebook"""
-        from IPython.core.display import Image as IPImage
+        return skimage.io.Image(self._image)
 
-        if self.path and self.inflation == 1:
-            # Picture hasn't been modified, just use the file directly
-            return IPImage(filename=self.path)
-        else:
-            # Convert picture to in-memory PNG
-            data = BytesIO()
-            self.__inflate(self.__image).save(data, format="png")
-            data.seek(0)
-            return IPImage(data=data.getvalue(), format="png", embed=True)
-
-    def __makepixel(self, xy):
+    def _makepixel(self, xy):
         """
-        Creates a pixel object for a given x, y location.
+        Creates a Pixel object for a given x, y location.
         NOTE: Using Cartesian coordinate system!
         """
         (x,y) = xy
-        rgb = self.__data[x, self.height - y - 1]
-        return pixel(self, self.__data, x, y, rgb)
+        rgb = self._image[x, self.height - y - 1]
+        return Pixel(self, self._image, x, y, rgb)
 
-    def __inflate(self, img):
+    def _inflate(self, img):
         """Returns resized image using inflation factor (nearest neighbor)"""
-        return img.resize((self.width * self.__inflation,
-            self.height * self.__inflation), Image.NEAREST)
+        return skimage.img_as_ubyte(skimage.transform.resize(self._image, 
+                (self.width * self._inflation,
+                 self.height * self._inflation), order=0))
 
-    def __negidx(self, idx, bound):
+    def _negidx(self, idx, bound):
         """Handles negative indices by wrapping around bound"""
         if (idx is None) or idx >= 0:
             return idx
@@ -288,9 +303,9 @@ class picture(object):
         """Iterates over all pixels in the image"""
         for x in xrange(self.width):
             for y in xrange(self.height):
-                yield self.__makepixel((x, y))
+                yield self._makepixel((x, y))
 
-    def __keys(self, key):
+    def _keys(self, key):
         """
         Takes a key for __getitem__ or __setitem__ and
         validates it.  If valid, returns either a pair of ints
@@ -303,12 +318,12 @@ class picture(object):
             if ((isinstance(slx, int) or isinstance(slx, slice)) and
                 (isinstance(sly, int) or isinstance(sly, slice))):
                 if isinstance(slx, int):
-                    slx = self.__negidx(slx, self.width)
+                    slx = self._negidx(slx, self.width)
                     if (slx < 0) or (slx >= self.width):
                         raise IndexError("Index out of range")
 
                 if isinstance(sly, int):
-                    sly = self.__negidx(sly, self.height)
+                    sly = self._negidx(sly, self.height)
                     if (sly < 0) or (sly >= self.height):
                         raise IndexError("Index out of range")
 
@@ -319,15 +334,15 @@ class picture(object):
                 if isinstance(slx, int):
                     slx = [slx]
                 else: # slice (allow negative indices)
-                    start = self.__negidx(slx.start, self.width)
-                    stop  = self.__negidx(slx.stop,  self.width)
+                    start = self._negidx(slx.start, self.width)
+                    stop  = self._negidx(slx.stop,  self.width)
                     slx = islice(xrange(self.width), start, stop, slx.step)
                     
                 if isinstance(sly, int):
                     sly = [sly]
                 else: # slice (allow negative indices)
-                    start = self.__negidx(sly.start, self.height)
-                    stop  = self.__negidx(sly.stop,  self.height)
+                    start = self._negidx(sly.start, self.height)
+                    stop  = self._negidx(sly.stop,  self.height)
                     sly = islice(xrange(self.height), start, stop, sly.step)
 
                 return product(slx, sly)
@@ -337,16 +352,28 @@ class picture(object):
         raise TypeError("Invalid key type")
 
     def __getitem__(self, key):
-        """Gets pixels using 2D int or slice notations"""
-        keys = self.__keys(key)
+        """
+        Gets pixels using 2D int or slice notations.
+        Examples:
+            pic[0, 0]      # Bottom-left pixel
+            pic[:, -1]     # Top row
+            pic[::2, ::2]  # Every other pixel
+        """
+        keys = self._keys(key)
         if isinstance(keys, tuple):
-            return self.__makepixel(keys)
+            return self._makepixel(keys)
         else:
-            return pixelgroup(map(self.__makepixel, keys))
+            return PixelGroup(map(self._makepixel, keys))
 
     def __setitem__(self, key, value):
-        """Sets pixelvalues using 2D int or slice notations"""
-        keys = self.__keys(key)
+        """
+        Sets pixel values using 2D int or slice notations.
+        Examples:
+            pic[0, 0] = (0, 0, 0)            # Make bottom-left pixel black
+            pic[:, -1] = (255, 0, 0)         # Make top row red
+            pic[::2, ::2] = (255, 255, 255)  # Make every other pixel white
+        """
+        keys = self._keys(key)
         if isinstance(keys, tuple):
             pixel = self[keys[0], keys[1]]
             pixel.rgb = value
@@ -356,5 +383,5 @@ class picture(object):
                 pixel.rgb = value
 
     def __repr__(self):
-        return "picture (format: {0}, path: {1}, modified: {2})".format(self.format, self.path, self.modified)
+        return "Picture (format: {0}, path: {1}, modified: {2})".format(self.format, self.path, self.modified)
 
