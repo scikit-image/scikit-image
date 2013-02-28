74b37dbb055a13416f9b2732cdb318a854bd66ed
spotter <xmoleslo@gmail.com>
2013-02-28 16:26:57 +0100

    ENH: Added rgb2hed and hed2rgb to __all__ list

--- skimage/color/colorconv.py
+++ skimage/color/colorconv.py
@@ -45,7 +45,8 @@ from __future__ import division
 
 __all__ = ['convert_colorspace', 'rgb2hsv', 'hsv2rgb', 'rgb2xyz', 'xyz2rgb',
            'rgb2rgbcie', 'rgbcie2rgb', 'rgb2grey', 'rgb2gray', 'gray2rgb',
-           'xyz2lab', 'lab2xyz', 'lab2rgb', 'rgb2lab', 'is_rgb', 'is_gray'
+           'xyz2lab', 'lab2xyz', 'lab2rgb', 'rgb2lab', 'rgb2hed', 'hed2rgb',
+           'is_rgb', 'is_gray'
            ]
 
 __docformat__ = "restructuredtext en"
