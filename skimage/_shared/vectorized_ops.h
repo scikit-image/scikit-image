3869d1ef8a10f31ba0c345f80cc76bec5e8b3bb8
Jan Margeta <jmargeta@gmail.com>
2013-03-03 22:45:03 +0100

    Removing unnecessary typedefs for uint16_t in MSVC build

--- skimage/_shared/vectorized_ops.h
+++ skimage/_shared/vectorized_ops.h
@@ -12,11 +12,7 @@
 #include <stdint.h>
 #elif defined(_MSC_VER)
 #define inline __inline
-#if (_MSC_VER < 1300)
-typedef unsigned short uint16_t;
-#else
 typedef unsigned __int16 uint16_t;
-#endif
 #endif
 
 /**
