96e5c1b1b8b1d78881ca860aea882c3e793d5bde
Johannes Schönberger <jschoenberger@demuc.de>
2013-03-04 20:41:19 +0100

    Update version number for 0.8

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ MAINTAINER_EMAIL    = 'stefan@sun.ac.za'
 URL                 = 'http://scikit-image.org'
 LICENSE             = 'Modified BSD'
 DOWNLOAD_URL        = 'http://github.com/scikit-image/scikit-image'
-VERSION             = '0.8dev'
+VERSION             = '0.8.0'
 PYTHON_VERSION      = (2, 5)
 DEPENDENCIES        = {
                         'numpy': (1, 6),
