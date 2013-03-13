5e67572ac7aaa7a5bbfa22c4c4dd35b251ba8f16
Johannes Schönberger <jschoenberger@demuc.de>
2013-03-13 12:16:58 +0100

    Do not set paper size to allow manual setting through make

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -199,7 +199,7 @@ htmlhelp_basename = 'scikitimagedoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 # The paper size ('letter' or 'a4').
-latex_paper_size = 'a4'
+#latex_paper_size = 'letter'
 
 # The font size ('10pt', '11pt' or '12pt').
 latex_font_size = '10pt'
