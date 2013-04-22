32c571cf82bd00b2557296c2abd49635be0552cb
Chintak Sheth <chintaksheth@gmail.com>
2013-04-22 08:02:11 +0530

    Updated the return type for convex_hull_image in documentation to bool rather than incorrect unit8

--- skimage/morphology/convex_hull.py
+++ skimage/morphology/convex_hull.py
@@ -19,64 +19,100 @@ def convex_hull_image(image):
 	
 	Returns
     -------
-    hull : ndarray of uint8
-        Binary image with pixels in convex hull set to 255.
+    hull : ndarray of bool
+        Binary image with pixels in convex hull set to True.
     
 	References
     ----------
     .. [1] http://blogs.mathworks.com/steve/2011/10/04/binary-image-convex-hull-algorithm-notes/
 	"""
-			
-    image = image.astype(bool)
-
+	
+	image = image.astype(bool)
+	
     # Here we do an optimisation by choosing only pixels that are
     # the starting or ending pixel of a row or column.  This vastly
     # limits the number of coordinates to examine for the virtual
     # hull.
-    coords = possible_hull(image.astype(np.uint8))
-    N = len(coords)
-
+	coords = possible_hull(image.astype(np.uint8))
+	N = len(coords)
+	
     # Add a vertex for the middle of each pixel edge
-    coords_corners = np.empty((N * 4, 2))
-    for i, (x_offset, y_offset) in enumerate(zip((0, 0, -0.5, 0.5),
-                                                 (-0.5, 0.5, 0, 0))):
-        coords_corners[i * N:(i + 1) * N] = coords + [x_offset, y_offset]
-
-    coords = coords_corners
-
-    try:
+	coords_corners = np.empty((N * 4, 2))
+	for i, (x_offset, y_offset) in enumerate(zip((0, 0, -0.5, 0.5), 
+												 (-0.5, 0.5, 0, 0))):
+		coords_corners[i * N:(i + 1) * N] = coords + [x_offset, y_offset]
+	coords = coords_corners
+	
+	try:
         from scipy.spatial import Delaunay
     except ImportError:
         raise ImportError('Could not import scipy.spatial, only available in '
                           'scipy >= 0.9.')
-
-    # Find the convex hull
+	# Find the convex hull
     chull = Delaunay(coords).convex_hull
     v = coords[np.unique(chull)]
-
-    # Sort vertices clock-wise
+	
+	# Sort vertices clock-wise
     v_centred = v - v.mean(axis=0)
     angles = np.arctan2(v_centred[:, 0], v_centred[:, 1])
     v = v[np.argsort(angles)]
-
-    # For each pixel coordinate, check whether that pixel
+	
+	# For each pixel coordinate, check whether that pixel
     # lies inside the convex hull
     mask = grid_points_inside_poly(image.shape[:2], v)
-
-    return mask
+	
+	return mask
 
 def connected_component(image, start_pixel_tuple):
+	"""Compute the connected object to a given starting pixel with 
+	8-connectivity for a binary image
+	
+	The convex hull is the set of pixels included in the smallest convex
+	polygon that surround all white pixels in the input image.
+	
+    Parameters
+    ----------
+    image : ndarray
+        Binary input image.  
+	
+	start_pixel_tuple : tuple of int
+		Of the form (x, y) which represents the index of the starting pixel
+	
+	Returns
+    -------
+    obj : ndarray of uint8
+        Binary image with pixels in convex hull set to True.
+	"""
 	next_im = np.zeros(image.shape, dtype=np.uint8)
 	next_im[start_pixel_tuple] = 1
 	start_im = np.zeros(image.shape)
 	while not np.array_equal(start_im, next_im):
 		start_im = next_im.copy()
-		dilated_im = dilation(start_im, sq(8))
+		dilated_im = dilation(start_im, sq(5))
 		next_im = dilated_st & image
 	
 	return next_im
 
-def convex_hull_object(image, output_type=None):
+def convex_hull_object(image, output_form=None):
+	"""Compute the convex hull image of individual objects in a binary image.
+	
+	The convex hull is the set of pixels included in the smallest convex
+	polygon that surround all white pixels in the input image.
+	
+    Parameters
+    ----------
+    image : ndarray
+        Binary input image.  
+	
+	output_form : string
+		if 'single' then outputs a 3D array with all convex hull computed for 
+		individual objects, where the 3rd index is used to change the object
+	
+	Returns
+    -------
+    hull : ndarray of bool
+        Binary image with pixels in convex hull set to True.
+	"""
 	# We add 1 to the output of label() so as to make the 
 	# background 0 rather than -1
 	segmented_objs = np.zeros((m, n, labeled_st.max()))
@@ -89,20 +125,7 @@ def convex_hull_object(image, output_type=None):
 		convex_objs[:, :, i] = convex_hull_image(segmented_objs[:, :, i])
 		convex_out |= convex_objs[:, :, i]
 	
-	if output_type is 'single':
+	if output_form is 'single':
 		return convex_objs
-	else if output_type is None:
-		return convex_out
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	else if output_form is None:
+		return convex_out	
