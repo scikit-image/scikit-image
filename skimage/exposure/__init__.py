36a915d5c116ac9c1067ba1cdd079d0c27054b7e
Johannes Schönberger <jschoenberger@demuc.de>
2013-04-27 15:41:50 +0200

    Add __all__ to exposure package

--- skimage/exposure/__init__.py
+++ skimage/exposure/__init__.py
@@ -1,3 +1,10 @@
 from .exposure import histogram, equalize, equalize_hist, \
                       rescale_intensity, cumulative_distribution
 from ._adapthist import equalize_adapthist
+
+__all__ = ['histogram',
+           'equalize',
+           'equalize_hist',
+           'equalize_adapthist',
+           'rescale_intensity',
+           'cumulative_distribution']
