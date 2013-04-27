702c1773b76e082854f660687111ce45341823eb
Johannes Schönberger <jschoenberger@demuc.de>
2013-04-27 17:22:32 +0200

    Add __all__ to filter package

--- skimage/filter/__init__.py
+++ skimage/filter/__init__.py
@@ -1,4 +1,4 @@
-from .lpi_filter import *
+from .lpi_filter import inverse, wiener, LPIFilter2D
 from .ctmf import median_filter
 from ._canny import canny
 from .edges import (sobel, hsobel, vsobel, scharr, hscharr, vscharr, prewitt,
@@ -9,3 +9,31 @@ from ._denoise_cy import denoise_bilateral, denoise_tv_bregman
 from ._rank_order import rank_order
 from ._gabor import gabor_kernel, gabor_filter
 from .thresholding import threshold_otsu, threshold_adaptive
+
+
+__all__ = ['inverse',
+           'wiener',
+           'LPIFilter2D',
+           'median_filter',
+           'canny',
+           'sobel',
+           'hsobel',
+           'vsobel',
+           'scharr',
+           'hscharr',
+           'vscharr',
+           'prewitt',
+           'hprewitt',
+           'vprewitt',
+           'roberts',
+           'roberts_positive_diagonal',
+           'roberts_negative_diagonal',
+           'denoise_tv_chambolle',
+           'tv_denoise',
+           'denoise_bilateral',
+           'denoise_tv_bregman',
+           'rank_order',
+           'gabor_kernel',
+           'gabor_filter',
+           'threshold_otsu',
+           'threshold_adaptive']
