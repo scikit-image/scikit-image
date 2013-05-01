db496d0af9dbcdff22d74372beba0919161e9ce4
Johannes Schönberger <jschoenberger@demuc.de>
2013-05-01 14:22:07 +0200

    Remove trailing brackets

--- skimage/measure/fit.py
+++ skimage/measure/fit.py
@@ -598,10 +598,10 @@ def ransac(data, model_class, min_samples, residual_threshold,
 
     Should give the correct result estimated without the faulty data:
 
-        [ 20.12762373, 29.73563061, 4.81499637, 10.4743584, 0.05217117])
+        [ 20.12762373, 29.73563061, 4.81499637, 10.4743584, 0.05217117]
         [ 4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-         38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49])
+         38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]
 
     Robustly estimate geometric transformation:
 
