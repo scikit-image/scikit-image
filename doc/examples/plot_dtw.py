2d7ddf0c66bc66f2c4a854f223e95c62271d299d
Marc de Klerk <mdeklerk@ml.sun.ac.za>
2013-05-16 09:18:18 +0200

    - appended to docstring - set v_offset to 10 - cleanup / refactor

--- doc/examples/plot_dtw.py
+++ doc/examples/plot_dtw.py
@@ -2,6 +2,8 @@
 ===================
 Dynamic Time Warping
 ===================
+DTW is an algorithm for measuring similarity between two sequences which
+may vary in time or speed
 
 This example shows how to match data point in time varying sequences.
 
@@ -13,7 +15,7 @@ import matplotlib.pyplot as plt
 from skimage.graph import dtw
 
 stretch = True
-v_offset = 100
+v_offset = 10
 axis_padding = 0.05
 
 vec1 = [71, 73, 75, 80, 80, 80, 78, 76, 75, 73, 71, 71, 71, 73, 75, 76, 76, 68,
@@ -25,11 +27,9 @@ vec2 = [69, 69, 73, 75, 79, 80, 79, 78, 76, 73, 72, 71, 70, 70, 69, 69, 69, 71,
 t = np.array(vec1, np.double)
 r = np.array(vec2, np.double)
 
-f = plt.figure()
-
 m = np.zeros((len(t), len(r)))
 
-path, distance = dtw(t, r)
+path, distance = dtw(t, r, case=2)
 
 for point in path:
     m[point[0], point[1]] = 1
@@ -39,23 +39,15 @@ if stretch:
 else:
     stretch = 1
 
-ax = f.add_subplot(111)
+f, ax = plt.subplots()
 
-ax.plot(np.arange(len(t)), t+v_offset, 'b')
-ax.plot(np.arange(len(t)), t+v_offset, 'b.')
-ax.plot(stretch * np.arange(len(r)), r, 'r')
-ax.plot(stretch * np.arange(len(r)), r, 'r.')
+ax.plot(np.arange(len(t)), t+v_offset, 'b.-')
+ax.plot(stretch * np.arange(len(r)), r, 'r.-')
 
 for edge in reversed(path):
     ax.plot([edge[0], stretch * edge[1]], [v_offset+t[edge[0]], r[edge[1]]],
         color="gray")
 
-ax.autoscale_view(True,True,True)
-
-x1, x2, y1, y2 = plt.axis()
-x_pad = axis_padding*(x2-x1)
-y_pad = axis_padding*(y2-y1)
-
-plt.axis((x1-x_pad, x2+x_pad, y1-y_pad, y2+y_pad))
+plt.margins(0.1)
 
 plt.show()
