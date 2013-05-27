ae8b5d443906e200c01ed393580bf83b18ea56c2
Olivier Debeir <odebeir@ulb.ac.be>
2013-05-27 11:35:03 +0200

    pep8: example

--- doc/examples/plot_percentile.py
+++ doc/examples/plot_percentile.py
@@ -14,23 +14,23 @@ from skimage.morphology import disk
 from skimage.filter import rank
 from skimage import color
 
-lena = np.array(256*color.rgb2gray(data.lena()),dtype=np.uint8)
+lena = np.array(256 * color.rgb2gray(data.lena()), dtype=np.uint8)
 selem = disk(5)
 
 # display results
-fig, axes = plt.subplots(ncols=4,nrows=4, figsize=(15, 10))
+fig, axes = plt.subplots(ncols=4, nrows=4, figsize=(15, 10))
 
 max_row = 4
 max_col = 4
-p0 = np.linspace(0,1,max_col*max_row,endpoint=True)
-i=0
+p0 = np.linspace(0, 1, max_col * max_row, endpoint=True)
+i = 0
 for row in axes:
     for ax in row:
-        perc = rank.percentile(lena,selem=selem,p0=p0[i])
-        ax.imshow(perc,cmap=plt.cm.gray)
-        ax.set_title('perc. : %.2f'%p0[i])
+        perc = rank.percentile(lena, selem=selem, p0=p0[i])
+        ax.imshow(perc, cmap=plt.cm.gray)
+        ax.set_title('perc. : %.2f' % p0[i])
         ax.xaxis.set_visible(False)
         ax.yaxis.set_visible(False)
-        i+=1
+        i += 1
 
 plt.show()
