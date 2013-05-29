817c9634e89be79c5ca4e08ce48c1eb1dd173f46
Johannes Schönberger <jschoenberger@demuc.de>
2013-05-29 20:36:08 +0200

    Fix setting of QT_API environment variable

--- skimage/viewer/qt/__init__.py
+++ skimage/viewer/qt/__init__.py
@@ -16,4 +16,7 @@ if qt_api is None:
             # Note that we don't want to raise an error because that would
             # cause the TravisCI build to fail.
             warnings.warn("Could not import PyQt4: ImageViewer not available!")
+
+
+if qt_api is not None:
     os.environ['QT_API'] = qt_api
