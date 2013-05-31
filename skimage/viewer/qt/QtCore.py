484e5693b2f3e0bc8c238cd64afeaad17bfa6673
Tony S Yu <tsyu80@gmail.com>
2013-05-31 08:15:26 -0500

    Add attributes to Mock object to fix Travis build

--- skimage/viewer/qt/QtCore.py
+++ skimage/viewer/qt/QtCore.py
@@ -5,7 +5,12 @@ if qt_api == 'pyside':
 elif qt_api == 'pyqt':
     from PyQt4.QtCore import *
 else:
-    # Mock objects
-    Qt = None
+    # Mock objects for buildbot (which doesn't have Qt, but imports viewer).
+    class Qt(object):
+        TopDockWidgetArea = None
+        BottomDockWidgetArea = None
+        LeftDockWidgetArea = None
+        RightDockWidgetArea = None
+
     def pyqtSignal(*args, **kwargs):
         pass
