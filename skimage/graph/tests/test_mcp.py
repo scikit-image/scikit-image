0a4fcdad0a46387cb5c6324dd82034d978c6e613
François Boulogne <fboulogne sciunto org>
2013-06-20 22:46:37 +0200

    fix imports

--- skimage/graph/tests/test_mcp.py
+++ skimage/graph/tests/test_mcp.py
@@ -1,5 +1,8 @@
 import numpy as np
-from numpy.testing import *
+from numpy.testing import (assert_array_equal,
+                           assert_almost_equal,
+                           )
+
 
 import skimage.graph.mcp as mcp
 
