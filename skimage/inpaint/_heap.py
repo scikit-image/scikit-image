dd98e921ad15ac522684f8f5cdaa7f3de7d4e180
Chintak Sheth <chintaksheth@gmail.com>
2013-06-26 10:54:18 +0530

    Implemented the heap structure using the in-built heapq function

--- skimage/inpaint/_heap.py
+++ skimage/inpaint/_heap.py
@@ -1,61 +1,92 @@
-__all__ = ['initialise']
+__all__ = ['generate_heap', 'display_heap']
 
 import numpy as np
 import heapq
-from skimage.morphology import dilation, disk
+from functools import total_ordering
+from skimage.util import img_as_ubyte, img_as_bool
+from skimage.morphology import erosion, disk
 
 
-KNOWN = 0
-BAND = 1
-INSIDE = 2
+BAND = 0
 
 
-def initialise(_mask):
-    """Initialisation:
-    Each pixel has 2 new values assigned to it stored in `flag` and `u` arrays.
-
-    `flag` Initialisation:
+def generate_flags(mask):
+    """Initialization:
     All pixels are classified into 1 of the following flags:
-    # KNOWN - denoted by 0 - intensity and u values are known.
-    # BAND - denoted by 1 - u value undergoes an update.
-    # INSIDE - denoted by 2 - intensity and u values unkown
+    # BAND - denoted by an integer value of 0
+    # KNOWN - denoted by an integer value of 1
+    # INSIDE - denoted by an integer value of 2
 
-    `u` Initialisation:
-    u <- 0 : `flag` equal to BAND or KNOWN
-    u <- 1.0e6 (arbitrarily large value) : `flag` equal to INSIDE
-
-    `heap` Initialisation:
-    Contains all the pixels marked as BAND in `flag`. The heap element is
-    a tuple with 2 elements, first being the `u` value corresponding to the
-    tuple of index which is stored as the second element.
-    Heap Element : (u[(i, j)], (i, j))
+    Depending on the flag value, that is, if flag is BAND or KNOWN
+    T is set to 0 and for flag equal to INSIDE, T is set to 1.0e6,
+    arbitrarily large value.
 
     Parameters
     ----------
-    _mask : ndarray of bool
-        This array is cast to uint8. Suppose the size is (m, n)
+    mask : ndarray
+        This array is cast to uint8 and normalized to 1 before processing.
 
     Returns
-    ------
-    flag, u : ndarray of zeros of size (m+2, n+2)
-        They contain the results after the initialisation as above.
-
-    heap : Empty list
-        Contains the BAND points with heap element as mentioned above
-
+    -------
+    flag : ndarray of uint8
+        It cosists of either 0, 1 or 2 according to conditions above.
     """
 
-    mask = _mask.astype(np.uint8)
-    outside = dilation(mask, disk(1))
-    band = np.logical_xor(mask, outside).astype(np.uint8)
+    mask = np.where(mask, 1, 0).astype(np.uint8)
+    inside = erosion(mask, disk(1))
+    border = img_as_ubyte(np.logical_xor(mask, inside)) / 255
 
-    flag = (2 * outside) - band
+    flag = 1 - border + inside
+    T = inside * 1.0e6
 
-    u = np.where(flag == INSIDE, 1.0e6, 0)
+    return flag, T
+
+
+def generate_heap(flag, T):
+    """Initialization:
+    All pixels are classified into 1 of the following flags:
+    # BAND - denoted by an integer value of 0
+    # KNOWN - denoted by an integer value of 1
+    # INSIDE - denoted by an integer value of 2
+
+    Depending on the flag value, that is, if flag is BAND or KNOWN
+    T is set to 0 and for flag equal to INSIDE, T is set to 1.0e6,
+    arbitrarily large value.
+
+    Parameters
+    ----------
+    mask : ndarray
+        Binary input image. This array is cast to uint8 and normalized to 1
+        before processing.
+
+    Returns
+    -------
+    heap : list of HeapData objects
+        It consists of the 'T' values and the index.
+    """
 
     heap = []
+
     indices = np.transpose(np.where(flag == BAND))
     for z in indices:
-        heapq.heappush(heap, (u[tuple(z)], tuple(z)))
+        heapq.heappush(heap, HeapElem(T[tuple(z)], z))
 
-    return flag, u, heap
+    return heap
+
+
+def display_heap(heap):
+    for i in heap:
+        print i.t, i.index
+
+
+@total_ordering
+class HeapElem(object):
+
+    def __init__(self, t, index):
+        self.data = (t, tuple(index))
+        self.t, self.index = self.data[0], self.data[1]
+
+    def __le__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return self.data <= other.data
