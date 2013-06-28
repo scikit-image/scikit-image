57c4472183241546dee63922c7555708bd222006
Chintak Sheth <chintaksheth@gmail.com>
2013-06-28 15:37:32 +0530

    Denoting KNOWN as 0 and BAND by 1

--- skimage/inpaint/_heap.py
+++ skimage/inpaint/_heap.py
@@ -7,22 +7,23 @@ from skimage.util import img_as_ubyte, img_as_bool
 from skimage.morphology import erosion, disk
 
 
-BAND = 0
+BAND = 1
+
 
 def generate_flags(mask):
     """Initialization:
     All pixels are classified into 1 of the following flags:
-    # BAND - denoted by an integer value of 0
-    # KNOWN - denoted by an integer value of 1
+    # KNOWN - denoted by an integer value of 0
+    # BAND - denoted by an integer value of 1
     # INSIDE - denoted by an integer value of 2
 
     Depending on the flag value, that is, if flag is BAND or KNOWN
-    T is set to 0 and for flag equal to INSIDE, T is set to 1.0e6,
+    u is set to 0 and for flag equal to INSIDE, u is set to 1.0e6,
     arbitrarily large value.
 
     Parameters
     ----------
-    mask : ndarray
+    mask : ndarray of bool
         This array is cast to uint8 and normalized to 1 before processing.
 
     Returns
@@ -31,12 +32,12 @@ def generate_flags(mask):
         It cosists of either 0, 1 or 2 according to conditions above.
     """
 
-    mask = np.where(mask, 1, 0).astype(np.uint8)
+    mask = _mask.astype(np.uint8)
     inside = erosion(mask, disk(1))
     border = img_as_ubyte(np.logical_xor(mask, inside))/255
 
-    flag = 1 - border + inside
-    T = inside * 1.0e6
+    flag = border + (2 * inside)
+    u = inside * 1.0e6
 
     return flag, T
 
