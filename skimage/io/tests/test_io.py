362d915399cc9c77d9f8223004d0a5cc1b9cf295
Adam Wisniewski <adamw@tbcn.ca>
2013-06-28 16:18:01 -0400

    support for saving to file-like object in imsave

--- skimage/io/tests/test_io.py
+++ skimage/io/tests/test_io.py
@@ -2,6 +2,7 @@ import os
 
 from numpy.testing import *
 import numpy as np
+from StringIO import StringIO
 
 import skimage.io as io
 from skimage import data_dir
@@ -27,6 +28,17 @@ def test_imread_url():
     image = io.imread(image_url)
     assert image.shape == (512, 512)
 
+def test_imsave_filelike():
+    image = np.array([[0, 0], [0, 0]], dtype=float)
+    s = StringIO()
+
+    # save to file-like object
+    io.imsave(s, image)
+
+    # read from file-like object
+    s.seek(0)
+    image = io.imread(s)
+    assert image.shape == (2, 2)
 
 if __name__ == "__main__":
     run_module_suite()
