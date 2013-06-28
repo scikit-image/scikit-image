c80a422681d5d769dc14766e14062f0cfeb56cbb
Chintak Sheth <chintaksheth@gmail.com>
2013-06-28 06:28:43 +0530

    Fixed PEP8 issues

--- skimage/inpaint/fmm.py
+++ skimage/inpaint/fmm.py
@@ -17,39 +17,38 @@ def fast_marching_method(image, mask, epsilon):
 
     heap = []
 
-    flag, T = _heap.generate_flags(mask)
-    heap = _heap.generate_heap(flag, T)
+    flag, u = _heap.generate_flags(mask)
+    heap = _heap.generate_heap(flag, u)
 
     while len(heap):
         item = heappop(heap)
         i, j = item.index
 
-        if ((i <= 1) or (j <= 1) or (i > mask.shape[0]-1)
-                or (j > mask.shape[1]-1)):
+        if ((i <= 1) or (j <= 1) or (i > mask.shape[0] - 1)
+                or (j > mask.shape[1] - 1)):
             continue
-        for (k, l) in (i-1, j), (i, j-1), (i+1, j), (i, j+1):
-            if flag[k, l] is not KNOWN:
-                if flag[k, l] is INSIDE:
-                    _inpaint.inpaint_point(k, l, image, flag, T, epsilon)
-                    flag[k, l] = BAND
-                    #below indent-1
-                    T[k, l] = min(eikonal_solve(k-1, l, k, l-1, flag, T),
-                                  eikonal_solve(k+1, l, k, l-1, flag, T),
-                                  eikonal_solve(k-1, l, k, l+1, flag, T),
-                                  eikonal_solve(k+1, l, k, l+1, flag, T))
-                    heappush(_heap.HeapElem(T[k, l], (k, l)))
+        for (k, l) in (i - 1, j), (i, j - 1), (i + 1, j), (i, j + 1):
+            if flag[k, l] is not KNOWN and flag[k, l] is INSIDE:
+                _inpaint.inpaint_point(k, l, image, flag, u, epsilon)
+                flag[k, l] = BAND
+                #below indent-1
+                u[k, l] = min(eikonal_solve(k - 1, l, k, l - 1, flag, u),
+                              eikonal_solve(k + 1, l, k, l - 1, flag, u),
+                              eikonal_solve(k - 1, l, k, l + 1, flag, u),
+                              eikonal_solve(k + 1, l, k, l + 1, flag, u))
+                heappush(heap, [u[k, l], (k, l)])
 
         flag[i, j] = KNOWN
-        T[i, j] = -T[i, j]
+        u[i, j] = -u[i, j]
 
 
-def eikonal_solve(i1, j1, i2, j2, flag, T):
+def eikonal_solve(i1, j1, i2, j2, flag, u):
     """This function provides the solution for the Eikonal equation.
     """
 
     sol = 1.0e6
-    t11 = T[i1, j1]
-    t22 = T[i2, j2]
+    t11 = u[i1, j1]
+    t22 = u[i2, j2]
     mint = min(t11, t22)
     if flag[i1, j1] is not INSIDE:
         if flag[i2, j2] is not INSIDE:
