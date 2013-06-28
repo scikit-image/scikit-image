050e6c46ee6a4e0878f18bef3bc6e803879ea5e6
Chintak Sheth <chintaksheth@gmail.com>
2013-06-28 15:12:52 +0530

    Simplified eikonal_solve

--- skimage/inpaint/fmm.py
+++ skimage/inpaint/fmm.py
@@ -39,7 +39,7 @@ def fast_marching_method(image, mask, epsilon):
                 heappush(heap, [u[k, l], (k, l)])
 
         flag[i, j] = KNOWN
-        u[i, j] = -u[i, j]
+        # u[i, j] = -u[i, j]
 
 
 def eikonal_solve(i1, j1, i2, j2, flag, u):
@@ -50,15 +50,14 @@ def eikonal_solve(i1, j1, i2, j2, flag, u):
     t11 = u[i1, j1]
     t22 = u[i2, j2]
     mint = min(t11, t22)
-    if flag[i1, j1] is not INSIDE:
-        if flag[i2, j2] is not INSIDE:
-            if abs(t11 - t22) >= 1.0:
-                sol = 1 + mint
-            else:
-                sol = (t11 + t22 + np.sqrt(2 - (t11 - t22) * (t11 - t22))) * .5
+    if flag[i1, j1] is not INSIDE and flag[i2, j2] is not INSIDE:
+        if abs(t11 - t22) >= 1.0:
+            sol = 1 + mint
         else:
-            sol = 1 + t11
-    elif flag[i2, j2] is not INSIDE:
+            sol = (t11 + t22 + np.sqrt(2 - (t11 - t22) * (t11 - t22))) * .5
+    elif flag[i1, j1] is not INSIDE and flag[i2, j2] is INSIDE:
+        sol = 1 + t11
+    elif flag[i1, j1] is INSIDE and flag[i2, j2] is not INSIDE:
         sol = 1 + t22
     else:
         sol = 1 + mint
