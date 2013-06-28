afa8c58fae066fdedfab11e922c437ce8268f2a9
Chintak Sheth <chintaksheth@gmail.com>
2013-06-28 16:34:16 +0530

    Added negate parameter in fast_marching_method to work for FMM outside and inside the boundary

--- skimage/inpaint/fmm.py
+++ skimage/inpaint/fmm.py
@@ -1,16 +1,16 @@
-__all__ = ['fast_marching_method', 'eikonal_solve']
+__all__ = ['fast_marching_method', 'eikonal']
 
 import numpy as np
 import _heap
 import _inpaint
 from heapq import heappop, heappush
 
-BAND = 0
-KNOWN = 1
+KNOWN = 0
+BAND = 1
 INSIDE = 2
 
 
-def fast_marching_method(image, mask, epsilon):
+def fast_marching_method(image, mask, epsilon, negate):
     """Fast Marching Method implementation based on the algorithm outlined in
     the paper by Telea.
     """
@@ -21,46 +21,51 @@ def fast_marching_method(image, mask, epsilon):
     heap = _heap.generate_heap(flag, u)
 
     while len(heap):
-        item = heappop(heap)
-        i, j = item.index
+        i, j = heappop(heap)[1]
+        flag[i, j] = KNOWN
 
         if ((i <= 1) or (j <= 1) or (i > mask.shape[0] - 1)
                 or (j > mask.shape[1] - 1)):
             continue
 
-        for (k, l) in (i - 1, j), (i, j - 1), (i + 1, j), (i, j + 1):
-            if flag[k, l] is not KNOWN and flag[k, l] is INSIDE:
-                _inpaint.inpaint_point(k, l, image, flag, u, epsilon)
-                flag[k, l] = BAND
-                #below indent-1
-                u[k, l] = min(eikonal_solve(k - 1, l, k, l - 1, flag, u),
-                              eikonal_solve(k + 1, l, k, l - 1, flag, u),
-                              eikonal_solve(k - 1, l, k, l + 1, flag, u),
-                              eikonal_solve(k + 1, l, k, l + 1, flag, u))
-                heappush(heap, [u[k, l], (k, l)])
+        for (i_nb, j_nb) in (i - 1, j), (i, j - 1), (i + 1, j), (i, j + 1):
+            if flag[i_nb, j_nb] is INSIDE:
+                u[i_nb, j_nb] = min(eikonal(i_nb - 1, j_nb,
+                                            i_nb, j_nb - 1, flag, u),
+                                    eikonal(i_nb + 1, j_nb,
+                                            i_nb, j_nb - 1, flag, u),
+                                    eikonal(i_nb - 1, j_nb,
+                                            i_nb, j_nb + 1, flag, u),
+                                    eikonal(i_nb + 1, j_nb,
+                                            i_nb, j_nb + 1, flag, u))
+                if negate is False:
+                    _inpaint.inpaint_point(i_nb, j_nb, image, flag, u, epsilon)
 
-        flag[i, j] = KNOWN
-        # u[i, j] = -u[i, j]
+                flag[i_nb, j_nb] = BAND
+                heappush(heap, [u[i_nb, j_nb], (i_nb, j_nb)])
+
+        if negate is True:
+            u[i, j] = -u[i, j]
 
 
-def eikonal_solve(i1, j1, i2, j2, flag, u):
+def eikonal(i1, j1, i2, j2, flag, u):
     """This function provides the solution for the Eikonal equation.
     """
 
     sol = 1.0e6
-    t11 = u[i1, j1]
-    t22 = u[i2, j2]
-    mint = min(t11, t22)
+    u11 = u[i1, j1]
+    u22 = u[i2, j2]
+    min_u = min(u11, u22)
     if flag[i1, j1] is not INSIDE and flag[i2, j2] is not INSIDE:
-        if abs(t11 - t22) >= 1.0:
-            sol = 1 + mint
+        if abs(u11 - u22) >= 1.0:
+            sol = 1 + min_u
         else:
-            sol = (t11 + t22 + np.sqrt(2 - (t11 - t22) * (t11 - t22))) * .5
+            sol = (u11 + u22 + np.sqrt(2 - (u11 - u22) * (u11 - u22))) * .5
     elif flag[i1, j1] is not INSIDE and flag[i2, j2] is INSIDE:
-        sol = 1 + t11
+        sol = 1 + u11
     elif flag[i1, j1] is INSIDE and flag[i2, j2] is not INSIDE:
-        sol = 1 + t22
+        sol = 1 + u22
     else:
-        sol = 1 + mint
+        sol = 1 + min_u
 
     return sol
