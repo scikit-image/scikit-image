5f7fc5283094dd74e8385a8df7757241ed8dd017
Josh Warner (Mac) <warner.joshua@mayo.edu>
2013-06-29 18:00:26 -0500

    FEAT: generator to add various types of random noise to images

--- skimage/util/__init__.py
+++ skimage/util/__init__.py
@@ -1,6 +1,7 @@
 from .dtype import (img_as_float, img_as_int, img_as_uint, img_as_ubyte,
                     img_as_bool, dtype_limits)
 from .shape import view_as_blocks, view_as_windows
+from .noise import random_noise
 
 import numpy
 ver = numpy.__version__.split('.')
@@ -20,4 +21,5 @@ __all__ = ['img_as_float',
            'dtype_limits',
            'view_as_blocks',
            'view_as_windows',
-           'pad']
+           'pad',
+           'random_noise']
