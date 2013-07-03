d3dd7626a181559df0e77eef4f2dfca2b1f8d5f3
Chintak Sheth <chintaksheth@gmail.com>
2013-07-03 14:23:50 +0530

    Minor fixes

--- skimage/inpaint/fmm.py
+++ skimage/inpaint/fmm.py
@@ -2,7 +2,7 @@ __all__ = ['inpaint', 'fast_marching_method', 'eikonal']
 
 import numpy as np
 import _heap
-import _inpaint
+from _inpaint import inpaint_point as inp_point
 from heapq import heappop, heappush
 from skimage.morphology import dilation, square
 
@@ -23,7 +23,7 @@ def eikonal(i1, j1, i2, j2, flag, u):
         if abs(u11 - u22) >= 1.0:
             sol = 1 + min_u
         else:
-            sol = (u11 + u22 + np.sqrt(2 - (u11 - u22) * (u11 - u22))) * .5
+            sol = (u11 + u22 - np.sqrt(2 - (u11 - u22) * (u11 - u22))) * .5
     elif flag[i1, j1] is not INSIDE and flag[i2, j2] is INSIDE:
         sol = 1 + u11
     elif flag[i1, j1] is INSIDE and flag[i2, j2] is not INSIDE:
@@ -43,8 +43,8 @@ def fast_marching_method(image, flag, u, heap, negate, epsilon=5):
         i, j = heappop(heap)[1]
         flag[i, j] = KNOWN
 
-        if ((i <= 1) or (j <= 1) or (i > image.shape[0] - 1)
-                or (j > image.shape[1] - 1)):
+        if ((i <= 1) or (j <= 1) or (i >= image.shape[0] - 1)
+                or (j >= image.shape[1] - 1)):
             continue
 
         for (i_nb, j_nb) in (i - 1, j), (i, j - 1), (i + 1, j), (i, j + 1):
@@ -58,8 +58,8 @@ def fast_marching_method(image, flag, u, heap, negate, epsilon=5):
                                     eikonal(i_nb + 1, j_nb,
                                             i_nb, j_nb + 1, flag, u))
                 if negate is False:
-                    image[i_nb - 1, j_nb - 1, :] = _inpaint.inpaint_point(i_nb,
-                                                            j_nb, image, flag, u, epsilon)
+                    image[i_nb - 1, j_nb - 1, :] = inp_point(i_nb, j_nb, image,
+                                                             flag, u, epsilon)
 
                 flag[i_nb, j_nb] = BAND
                 heappush(heap, [u[i_nb, j_nb], (i_nb, j_nb)])
@@ -77,12 +77,14 @@ def inpaint(input_image, inpaint_mask, epsilon=5):
     """
     """
     # TODO: Error checks. Image either 3 or 1 channel. All dims same
-    if input_image.ndim == 3:
-        m, n, channel = input_image.shape
-        image = np.zeros((m + 2, n + 2, channel), np.uint8)
-    else:
-        m, n = input_image.shape
-        image = np.zeros((m + 2, n + 2), np.uint8)
+    # if input_image.ndim == 3:
+    #     m, n, channel = input_image.shape
+    #     image = np.zeros((m + 2, n + 2, channel), np.uint8)
+    # else:
+    #     m, n = input_image.shape
+    #     image = np.zeros((m + 2, n + 2), np.uint8)
+    m, n = input_image.shape
+    image = np.zeros((m + 2, n + 2), np.uint8)
     mask = np.zeros((m + 2, n + 2), bool)
     image[1: -1, 1: -1] = input_image
     mask[1: -1, 1: -1] = inpaint_mask
@@ -93,10 +95,14 @@ def inpaint(input_image, inpaint_mask, epsilon=5):
     outside_band = np.logical_xor(outside, mask).astype(np.uint8)
     out_flag = _heap.init_flag(mask)
     u = _heap.init_u(flag)
-    out_heap = _heap.generate_heap(out_flag, u)
+
+    out_heap = []
+    _heap.generate_heap(out_heap, out_flag, u)
     u = fast_marching_method(outside_band, out_flag, u, out_heap, negate=True)
 
-    heap = _heap.generate_heap(flag, u)
-    output = fast_marching_method(image, flag, u, heap, negate=False, epsilon)
+    heap = []
+    _heap.generate_heap(heap, flag, u)
+    output = fast_marching_method(image, flag, u, heap, negate=False,
+                                  epsilon=epsilon)
 
     return output
