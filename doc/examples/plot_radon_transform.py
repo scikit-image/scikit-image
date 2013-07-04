1d611878a6c3cbd56cbc55a7abf6220643d3459a
Jostein Bø Fløystad <jostein.floystad@gmail.com>
2013-07-04 15:24:44 +0200

    Radon example: Style fixes.

--- doc/examples/plot_radon_transform.py
+++ doc/examples/plot_radon_transform.py
@@ -67,9 +67,9 @@ plt.imshow(image, cmap=plt.cm.Greys_r)
 theta = np.linspace(0., 180., max(image.shape), endpoint=True)
 sinogram = radon(image, theta=theta, circle=True)
 plt.subplot(122)
-plt.title("Radon transform\n(Sinogram)");
-plt.xlabel("Projection angle (deg)");
-plt.ylabel("Projection position (pixels)");
+plt.title("Radon transform\n(Sinogram)")
+plt.xlabel("Projection angle (deg)")
+plt.ylabel("Projection position (pixels)")
 plt.imshow(sinogram, cmap=plt.cm.Greys_r,
            extent=(0, 180, 0, sinogram.shape[0]), aspect='auto')
 
