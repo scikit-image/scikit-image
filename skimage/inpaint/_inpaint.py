4132929b0e6e306264a364394877075f92df9bef
Chintak Sheth <chintaksheth@gmail.com>
2013-07-04 13:58:50 +0530

    Use np.subtract for subtraction to avoid ubyte overflow

--- skimage/inpaint/_inpaint.py
+++ skimage/inpaint/_inpaint.py
@@ -14,7 +14,8 @@ def grad_func(i, j, flag, array, channel=-1):
     """
 
     if channel == 0 or channel == 1 or channel == 2:
-        u = array[:, :, channel]
+        #u = array[:, :, channel]
+        u = array
         i_nbl = i - 1 + (i == 1)
         i_nbh = i - 1 - (i == u.shape[0] - 2)
         j_nbl = j - 1 + (j == 1)
@@ -26,20 +27,20 @@ def grad_func(i, j, flag, array, channel=-1):
         i_nbl, i_nbh, j_nbl, j_nbh = i, i, j, j
 # TODO: Try dict implementation instead of if...elif
     if flag[i, j + 1] != INSIDE and flag[i, j - 1] != INSIDE:
-        gradUx = (u[i_nbl, j_nbh + 1] - u[i_nbl, j_nbl - 1]) * factor
+        gradUx = np.subtract(u[i_nbl, j_nbh + 1], u[i_nbl, j_nbl - 1]) * factor
     elif flag[i, j + 1] != INSIDE and flag[i, j - 1] == INSIDE:
-        gradUx = u[i_nbl, j_nbh + 1] - u[i_nbl, j_nbl]
+        gradUx = np.subtract(u[i_nbl, j_nbh + 1], u[i_nbl, j_nbl])
     elif flag[i, j + 1] == INSIDE and flag[i, j - 1] != INSIDE:
-        gradUx = u[i_nbl, j_nbh] - u[i_nbl, j_nbl - 1]
+        gradUx = np.subtract(u[i_nbl, j_nbh], u[i_nbl, j_nbl - 1])
     elif flag[i, j + 1] == INSIDE and flag[i, j - 1] == INSIDE:
         gradUx = 0
 
     if flag[i + 1, j] != INSIDE and flag[i - 1, j] != INSIDE:
-        gradUy = (u[i_nbh + 1, j_nbl] - u[i_nbl - 1, j_nbl]) * factor
+        gradUy = np.subtract(u[i_nbh + 1, j_nbl], u[i_nbl - 1, j_nbl]) * factor
     elif flag[i + 1, j] != INSIDE and flag[i - 1, j] == INSIDE:
-        gradUy = u[i_nbh + 1, j_nbl] - u[i_nbl, j_nbl]
+        gradUy = np.subtract(u[i_nbh + 1, j_nbl], u[i_nbl, j_nbl])
     elif flag[i + 1, j] == INSIDE and flag[i - 1, j] != INSIDE:
-        gradUy = u[i_nbh, j_nbl] - u[i_nbl - 1, j_nbl]
+        gradUy = np.subtract(u[i_nbh, j_nbl], u[i_nbl - 1, j_nbl])
     elif flag[i + 1, j] == INSIDE and flag[i - 1, j] == INSIDE:
         gradUy = 0
 
@@ -49,7 +50,8 @@ def grad_func(i, j, flag, array, channel=-1):
 def inpaint_point(i, j, image, flag, u, epsilon):
     Ia, Jx, Jy, norm = 0, 0, 0, 0
     #If the input image is 3 channel. TODO: support for a single channel
-    for color in 0, 1, 2:
+    for color in [0]:
+        #Compute the gradient of the u or speed at (i, j)
         gradUx, gradUy = grad_func(i, j, flag, u, channel=-1)
         for i_nb in xrange(i - epsilon, i + epsilon):
             for j_nb in xrange(j - epsilon, j + epsilon):
@@ -70,17 +72,18 @@ def inpaint_point(i, j, image, flag, u, epsilon):
                             dirc = 1.0e-6
                         weight = abs(dst * lev * dirc)
 
+                        #Comput the gradient of image intensity at (i_nb, j_nb)
                         gradIx, gradIy = grad_func(i_nb, j_nb, flag, image,
                                                    channel=color)
 
                         Ia += weight * image[i_nb - 1 + (i_nb == 1),
-                                             j_nb - 1 + (j_nb == 1), color]
+                                             j_nb - 1 + (j_nb == 1)]
                         Jx -= weight * gradIx * rx
                         Jy -= weight * gradIy * ry
                         norm += weight
 
-        sat = (Ia / norm + (Jx + Jy) /
-              (np.sqrt(Jx * Jx + Jy * Jy) + 1.0e-20) + 0.5)
-        image[i - 1, j - 1, color] = int(round(sat))
+        sat = (Ia / norm + (Jx + Jy) / (np.sqrt(Jx * Jx + Jy * Jy) + 1.0e-20)
+               + 0.5)
+        image[i - 1, j - 1] = int(round(sat))
 
-    return image[i - 1, j - 1, :]
+    return image[i - 1, j - 1]
