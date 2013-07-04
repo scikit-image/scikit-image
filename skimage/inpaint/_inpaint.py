73482d02293cf7acee012474fd4b882a4a7acae8
Chintak Sheth <chintaksheth@gmail.com>
2013-07-04 19:36:58 +0530

    Convert input array to `int` type to avoid ubyte subtraction overflow

--- skimage/inpaint/_inpaint.py
+++ skimage/inpaint/_inpaint.py
@@ -2,6 +2,10 @@ __all__ = ['grad_func', 'inpaint_point']
 
 import numpy as np
 
+KNOWN = 0
+BAND = 1
+INSIDE = 2
+
 
 def grad_func(i, j, flag, array, channel=-1):
     """This function calculates the gradient of the speed/image of a pixel
@@ -11,14 +15,14 @@ def grad_func(i, j, flag, array, channel=-1):
 
     if channel == 0 or channel == 1 or channel == 2:
         #u = array[:, :, channel]
-        u = array
+        u = np.array(array, int)
         i_nbl = i - 1 + (i == 1)
         i_nbh = i - 1 - (i == u.shape[0] - 2)
         j_nbl = j - 1 + (j == 1)
         j_nbh = j - 1 - (j == u.shape[1] - 2)
         factor = 2.0
     elif channel is -1:
-        u = array
+        u = np.array(array, int)
         factor = 0.5
         i_nbl, i_nbh, j_nbl, j_nbh = i, i, j, j
 # TODO: Try dict implementation instead of if...elif
@@ -54,8 +58,10 @@ def inpaint_point(i, j, image, flag, u, epsilon):
                 if (i_nb > 0 and j_nb > 0 and i_nb < (u.shape[0] - 1)
                         and j_nb < (u.shape[1] - 1)):
                     cart_d = (j_nb - j) * (j_nb - j) + (i_nb - i) * (i_nb - i)
-                    if (flag[i_nb, j_nb] != INSIDE and
+                    if (flag[i_nb, j_nb] == KNOWN and
                             cart_d <= epsilon ** 2):
+                        gradUx, gradUy = grad_func(i_nb, j_nb, flag, u,
+                                                   channel=-1)
                         rx = i - i_nb
                         ry = j - j_nb
 
@@ -69,8 +75,8 @@ def inpaint_point(i, j, image, flag, u, epsilon):
                         weight = abs(dst * lev * dirc)
 
                         #Comput the gradient of image intensity at (i_nb, j_nb)
-                        gradIx, gradIy = grad_func(i_nb, j_nb, flag, image,
-                                                   channel=color)
+                        # gradIx, gradIy = grad_func(i_nb, j_nb, flag, image,
+                        #                            channel=color)
 
                         Ia += weight * image[i_nb - 1 + (i_nb == 1),
                                              j_nb - 1 + (j_nb == 1)]
@@ -80,6 +86,7 @@ def inpaint_point(i, j, image, flag, u, epsilon):
 
         sat = (Ia / norm + (Jx + Jy) / (np.sqrt(Jx * Jx + Jy * Jy) + 1.0e-20)
                + 0.5)
-        image[i - 1, j - 1] = int(round(sat))
+#        image[i - 1, j - 1] = int(round(sat))
+        image[i, j] = int(round(sat))
 
-    return image[i - 1, j - 1]
+#    return image[i - 1, j - 1]
