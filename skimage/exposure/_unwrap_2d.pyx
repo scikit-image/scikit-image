5022cfb699ca3e49a5ab1a9a9d0c48852e86c708
Jostein Bø Fløystad <jostein.floystad@gmail.com>
2013-07-11 13:04:38 +0200

    unwrap: PEP8 fixes for cython extensions.

--- skimage/exposure/_unwrap_2d.pyx
+++ skimage/exposure/_unwrap_2d.pyx
@@ -4,13 +4,13 @@ cdef extern void unwrap2D(float* wrapped_image,
                      int image_width, int image_height,
                      int wrap_around_x, int wrap_around_y)
 
-def unwrap_2d(float[:,::1] array,
-              unsigned char[:,::1] mask,
-              float[:,::1] unwrapped_array,
+def unwrap_2d(float[:, ::1] array,
+              unsigned char[:, ::1] mask,
+              float[:, ::1] unwrapped_array,
               wrap_around):
-    unwrap2D(&array[0,0],
-             &unwrapped_array[0,0],
-             &mask[0,0],
+    unwrap2D(&array[0, 0],
+             &unwrapped_array[0, 0],
+             &mask[0, 0],
              array.shape[1], array.shape[0],
              wrap_around[1], wrap_around[0],
              )
