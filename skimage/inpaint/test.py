3ca551c3e8cbcebe1a5957cc100088ba15f23b4f
Chintak Sheth <chintaksheth@gmail.com>
2013-07-12 16:34:49 +0530

    Configure to work with `fmm.inpaint`

--- skimage/inpaint/test.py
+++ skimage/inpaint/test.py
@@ -6,17 +6,6 @@ import _heap
 import fmm
 
 
-def inpaint(image, mask, epsilon=3):
-    image = image.copy()
-
-    flag, u, heap = _heap.initialise(mask)
-
-    painted = fmm.fast_marching_method(image, flag, u, heap, epsilon=epsilon)
-    plt.imshow(u)
-    plt.show()
-    return painted
-
-
 def demo_inpaint():
     image = data.camera()[80:180, 200:300]
     paint_region = (slice(65, 75), slice(55, 75))
@@ -25,7 +14,7 @@ def demo_inpaint():
     mask = np.zeros_like(image, dtype=np.uint8)
     mask[paint_region] = 1
 
-    painted = inpaint(image, mask)
+    painted = fmm.inpaint(image, mask)
 
     fig, (ax0, ax1) = plt.subplots(ncols=2)
     plt.gray()
