1c8b7188761fc17ac5a407821d44dcf5743ec6f8
Chintak Sheth <chintaksheth@gmail.com>
2013-07-23 11:48:56 +0530

    Added `inpaint_fmm` and `fast_marching_method` to __init__.py

--- skimage/filter/__init__.py
+++ skimage/filter/__init__.py
@@ -2,7 +2,7 @@ from .lpi_filter import inverse, wiener, LPIFilter2D
 from .ctmf import median_filter
 from ._canny import canny
 from .edges import (sobel, hsobel, vsobel, scharr, hscharr, vscharr, prewitt,
-                    hprewitt, vprewitt, roberts , roberts_positive_diagonal,
+                    hprewitt, vprewitt, roberts, roberts_positive_diagonal,
                     roberts_negative_diagonal)
 from ._denoise import denoise_tv_chambolle, tv_denoise
 from ._denoise_cy import denoise_bilateral, denoise_tv_bregman
@@ -10,6 +10,8 @@ from ._rank_order import rank_order
 from ._gabor import gabor_kernel, gabor_filter
 from .thresholding import threshold_otsu, threshold_adaptive
 from . import rank
+from .inpaint import inpaint_fmm
+from ._inpaint import fast_marching_method
 
 
 __all__ = ['inverse',
@@ -38,4 +40,6 @@ __all__ = ['inverse',
            'gabor_filter',
            'threshold_otsu',
            'threshold_adaptive',
-           'rank']
+           'rank',
+           'inpaint_fmm',
+           'fast_marching_method']
