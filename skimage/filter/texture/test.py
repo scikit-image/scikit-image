5c13e2992064c166e9a4139d14e5f35e9f3d57c5
Chintak Sheth <chintaksheth@gmail.com>
2013-07-28 08:55:26 +0530

    larger test area

--- skimage/filter/texture/test.py
+++ skimage/filter/texture/test.py
@@ -14,7 +14,7 @@ def demo_syn():
     # paint_region = (slice(20, 25), slice(15, 35))
 
     image = data.camera()[400:500, 250:350]
-    paint_region = (slice(20, 25), slice(40, 50))
+    paint_region = (slice(20, 45), slice(40, 60))
 
     mask = np.zeros_like(image, dtype=np.uint8)
     image[paint_region] = 0
@@ -28,9 +28,9 @@ def start():
 
     image, painted = demo_syn()
 
-    fig, (ax0, ax1) = plt.subplots(ncols=2)
-    ax0.imshow(image)
-    ax1.imshow(painted)
-    plt.show()
+    # fig, (ax0, ax1) = plt.subplots(ncols=2)
+    # ax0.imshow(image)
+    # ax1.imshow(painted)
+    # plt.show()
 
 start()
