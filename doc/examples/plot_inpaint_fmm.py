ebe95c236900a32bd6e5fe93dbc5bbe9fb150c45
Chintak Sheth <chintaksheth@gmail.com>
2013-07-31 12:57:06 +0530

    Remove start function and demo_time_fill

--- doc/examples/plot_inpaint_fmm.py
+++ doc/examples/plot_inpaint_fmm.py
@@ -5,9 +5,6 @@ from skimage.filter import inpaint
 from skimage.filter import _inpaint
 
 
-__all__ = ['demo_inpaint', 'demo_time_fill']
-
-
 def demo_inpaint():
     image = data.camera()
     paint_region = (slice(120, 130), slice(440, 470))
@@ -22,29 +19,9 @@ def demo_inpaint():
     return image, inpaint.inpaint_fmm(image, mask)
 
 
-def demo_time_fill():
-    image = np.ones((15, 15), np.float)
-    fill_region = (slice(3, 12), slice(3, 12))
-    image[fill_region] = 0
+image, painted = demo_inpaint()
 
-    mask = np.zeros_like(image, dtype=np.uint8)
-    mask[fill_region] = 1
-
-    flag, u, heap = inpaint._init_fmm(mask)
-    _inpaint.fast_marching_method(image, flag, u, heap, _run_inpaint=False,
-                                  radius=5)
-
-    return image, np.round(u, 1)
-
-
-def start():
-
-    image, painted = demo_inpaint()
-    # image, painted = demo_time_fill()
-
-    fig, (ax0, ax1) = plt.subplots(ncols=2)
-    ax0.imshow(image)
-    ax1.imshow(painted)
-    plt.show()
-
-start()
+fig, (ax0, ax1) = plt.subplots(ncols=2)
+ax0.imshow(image)
+ax1.imshow(painted)
+plt.show()
