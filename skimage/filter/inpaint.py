1c5d14e6a403c2128ca49cb5e9015b9947576209
Chintak Sheth <chintaksheth@gmail.com>
2013-07-31 11:59:27 +0530

    Update docs to incorporate Tony's suggestion

--- skimage/filter/inpaint.py
+++ skimage/filter/inpaint.py
@@ -19,7 +19,7 @@ def _init_fmm(mask):
 
     Parameters
     ----------
-    mask : 2D array of bool
+    mask : array, bool
         ``True`` values are to be inpainted.
 
     Returns
@@ -36,9 +36,9 @@ def _init_fmm(mask):
     -----
     ``flag`` Initialisation:
     All pixels are classified into 1 of the following flags:
-    # 0 = KNOWN - intensity and u values are known.
-    # 1 = BAND - u value undergoes an update.
-    # 2 = INSIDE - intensity and u values unkown
+    - 0 = KNOWN - intensity and u values are known.
+    - 1 = BAND - u value undergoes an update.
+    - 2 = INSIDE - intensity and u values unkown
 
     ``u`` Initialisation:
     u <- 0 : ``flag`` equal to BAND or KNOWN
@@ -53,8 +53,8 @@ def _init_fmm(mask):
     References
     ----------
     .. [1] Telea, A., "An Image Inpainting Technique based on the Fast Marching
-            Method", Journal of Graphic Tools (2004).
-            http://iwi.eldoc.ub.rug.nl/FILES/root/2004/JGraphToolsTelea/2004JGraphToolsTelea.pdf
+           Method", Journal of Graphic Tools (2004).
+           http://iwi.eldoc.ub.rug.nl/FILES/root/2004/JGraphToolsTelea/2004JGraphToolsTelea.pdf
 
     """
 
@@ -81,17 +81,17 @@ def inpaint_fmm(input_image, inpaint_mask, radius=5):
 
     Parameters
     ---------
-    input_image : (M, N) ndarray
-        Grayscale image to be inpainted of type ``np.uint8``
-    inpaint_mask : (M, N) ndarray
-        Mask containing pixels to be inpainted. ``True`` values are inpainted
+    input_image : (M, N) array, unit8
+        Grayscale image to be inpainted.
+    inpaint_mask : (M, N) array, bool
+        Mask containing pixels to be inpainted. ``True`` values are inpainted.
     radius : int
         Determining the range of the neighbourhood for inpainting a pixel
 
     Returns
     ------
-    painted : (M, N) ndarray
-        The inpainted grayscale image of same dimensions but ytpe ``np.float``
+    painted : (M, N) array, float
+        The inpainted grayscale image.
 
     Notes
     -----
@@ -103,9 +103,9 @@ def inpaint_fmm(input_image, inpaint_mask, radius=5):
     Implementaiton under ``skimage.filter.inpaint._init_fmm``.
     - ``flag`` Initialisation:
         All pixels are classified into 1 of the following flags:
-        # 0 = KNOWN - intensity and u values are known.
-        # 1 = BAND - u value undergoes an update.
-        # 2 = INSIDE - intensity and u values unkown
+        - 0 = KNOWN - intensity and u values are known.
+        - 1 = BAND - u value undergoes an update.
+        - 2 = INSIDE - intensity and u values unkown
 
     - ``u`` Initialisation:
         u <- 0 : ``flag`` equal to BAND or KNOWN
@@ -136,35 +136,25 @@ def inpaint_fmm(input_image, inpaint_mask, radius=5):
     References
     ---------
     .. [1] Telea, A., "An Image Inpainting Technique based on the Fast Marching
-            Method", Journal of Graphic Tools (2004).
-            http://iwi.eldoc.ub.rug.nl/FILES/root/2004/JGraphToolsTelea/2004JGraphToolsTelea.pdf
+           Method", Journal of Graphic Tools (2004).
+           http://iwi.eldoc.ub.rug.nl/FILES/root/2004/JGraphToolsTelea/2004JGraphToolsTelea.pdf
 
     Examples
     --------
     >>> import numpy as np
     >>> import matplotlib.pyplot as plt
+    >>> mask = np.zeros((8,8), np.uint8)
+    >>> mask[2:-2, 2:-2] = 1
     >>> image = np.arange(64).reshape(8,8)
-    >>> image[2:-2, 2:-2] = 0
     >>> image
     array([[ 0,  1,  2,  3,  4,  5,  6,  7],
            [ 8,  9, 10, 11, 12, 13, 14, 15],
-           [16, 17,  0,  0,  0,  0, 22, 23],
-           [24, 25,  0,  0,  0,  0, 30, 31],
-           [32, 33,  0,  0,  0,  0, 38, 39],
-           [40, 41,  0,  0,  0,  0, 46, 47],
+           [16, 17, 18, 19, 20, 21, 22, 23],
+           [24, 25, 26, 27, 28, 29, 30, 31],
+           [32, 33, 34, 35, 36, 37, 38, 39],
+           [40, 41, 42, 43, 44, 45, 46, 47],
            [48, 49, 50, 51, 52, 53, 54, 55],
            [56, 57, 58, 59, 60, 61, 62, 63]])
-    >>> mask = np.zeros((8,8), np.uint8)
-    >>> mask[2:-2, 2:-2] = 1
-    >>> mask
-    array([[0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 1, 1, 1, 1, 0, 0],
-           [0, 0, 1, 1, 1, 1, 0, 0],
-           [0, 0, 1, 1, 1, 1, 0, 0],
-           [0, 0, 1, 1, 1, 1, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0]], dtype=uint8)
     >>> from skimage.filter.inpaint import inpaint_fmm
     >>> painted = inpaint_fmm(image, mask)
     >>> np.round(painted)
@@ -188,6 +178,8 @@ def inpaint_fmm(input_image, inpaint_mask, radius=5):
         if input_image.shape[1] != inpaint_mask.shape[1]:
             raise TypeError("The second dimension of 'inpaint_mask' and "
                             "'input_image' do not match. ")
+
+    input_image[inpaint_mask == 1] = 0
 
     h, w = input_image.shape
     painted = np.zeros((h + 2, w + 2), np.float)
