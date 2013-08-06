909d4cb5f6ac2d051091c43c849475f1d1ae4aa1
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2013-08-06 22:45:21 +1000

    Add test to unique_rows for discontiguous arrays

--- skimage/util/tests/test_unique_rows.py
+++ skimage/util/tests/test_unique_rows.py
@@ -3,6 +3,14 @@ from numpy.testing import assert_equal, assert_raises
 from skimage.util import unique_rows
 
 
+def test_discontiguous_array():
+    ar = np.array([[1, 0, 1], [0, 1, 0], [1, 0, 1]], np.uint8)
+    ar = ar[::2]
+    ar_out = unique_rows(ar)
+    desired_ar_out = np.array([[1, 0, 1]], np.uint8)
+    assert_equal(ar_out, desired_ar_out)
+
+
 def test_uint8_array():
     ar = np.array([[1, 0, 1], [0, 1, 0], [1, 0, 1]], np.uint8)
     ar_out = unique_rows(ar)
