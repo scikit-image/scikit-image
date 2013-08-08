eb8ce0d8820d600c9ed0d1dc2c161fd6c702007b
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2013-08-08 22:22:19 +1000

    Complete grid should have stepsize of 1, not None
    Some functions might expect an integer value out of regular_grid,
    rather than None. This allows that.

--- skimage/util/tests/test_regular_grid.py
+++ skimage/util/tests/test_regular_grid.py
@@ -6,7 +6,7 @@ from skimage.util import regular_grid
 def test_regular_grid_full():
     ar = np.zeros((2, 2))
     g = regular_grid(ar, 25)
-    assert_equal(g, [slice(None, None, None), slice(None, None, None)])
+    assert_equal(g, [slice(None, None, 1), slice(None, None, 1)])
     ar[g] = 1
     assert_equal(ar.size, ar.sum())
 
