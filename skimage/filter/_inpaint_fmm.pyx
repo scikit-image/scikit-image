ba80f9908ba63b9e0b0ae52743d8a7a3caba17f1
Chintak Sheth <chintaksheth@gmail.com>
2013-08-09 01:39:25 +0530

    Move _init_fmm to the cython file

--- skimage/filter/_inpaint_fmm.pyx
+++ skimage/filter/_inpaint_fmm.pyx
@@ -8,7 +8,7 @@ from libc.math cimport sqrt
 
 import numpy as np
 from heapq import heappop, heappush
-from skimage.morphology import disk
+from skimage.morphology import disk, dilation
 
 
 LARGE_VALUE = 1.0e6
@@ -17,48 +17,209 @@ BAND = 1
 INSIDE = 2
 
 
-cdef cnp.float_t _grad_func(cnp.float_t[:, :] array,
-                            Py_ssize_t i, Py_ssize_t j,
-                            cnp.uint8_t[:, :] flag):
-    """Return the x-gradient of the input array at a pixel.
+cpdef _fast_marching_method(cnp.float_t[:, ::1] image, mask,
+                            Py_ssize_t radius=5):
+    """Inpaint an image using the Fast Marching Method (FMM).
 
-    This gradient is structured to ignore inner, unknown regions as specified
-    by the `flag` array. By default, this returns the gradient in the
-    x-direction. To get the y-gradient, switch the order of `i`, `j`, and
-    transpose `flag` and `array`.
+    Image Inpainting technique based on the Fast Marching Method implementation
+    as described in [1]_. FMM is used for computing the evolution of
+    boundary moving in a direction *normal* to itself.
 
     Parameters
-    ----------
-    array : array, float
-        Either ``image`` or ``u``, whose gradient is to be computed
-    i, j : int
-        Row and column index of the pixel whose gradient is to be calculated.
-    flag : array, uint8
-        Array marking pixels as known, along the boundary to be solved, or
-        inside the unknown region: 0 = KNOWN, 1 = BAND, 2 = INSIDE
+    ---------
+    image : array, float
+        Initial input image padded by a single row/column on all sides
+    mask : array, bool
+        Initial mask padded as the image, True values are inpainted
+    radius : int
+        Neighbourhood of the pixel of interest
 
     Returns
-    -------
-    grad : float
-        The local gradient of array.
+    ------
+    painted : array, float
+        The inpainted grayscale image
+
+    Notes
+    -----
+    The steps of the algorithm are as follows:
+    - Extract the pixel with the smallest ``u`` value in the BAND pixels
+    - Update its ``flag`` value as KNOWN
+    - March the boundary inwards by adding new points.
+        - If they are either INSIDE or BAND, compute its ``u`` value using the
+          ``eikonal`` function for all the 4 quadrants
+        - If ``flag`` is INSIDE
+            - Change it to BAND
+            - Inpaint the pixel
+        - Select the ``min`` value and assign it as ``u`` value of the pixel
+        - Insert this new value in the ``heap``
+
+    For further details, see [1]_
+
+    References
+    ----------
+    .. [1] Telea, A., "An Image Inpainting Technique based on the Fast Marching
+           Method", Journal of Graphic Tools (2004).
+           http://iwi.eldoc.ub.rug.nl/FILES/root/2004/JGraphToolsTelea/2004JGraphToolsTelea.pdf
 
     """
 
     cdef:
-        cnp.float_t grad
+        Py_ssize_t i, j,
+        cnp.int16_t i_nb, j_nb
+        cnp.int16_t[:, ::1] shifted_indices
+        cnp.uint8_t[:, ::1] flag
+        cnp.float_t[:, ::1] u
+        list heap = list()
 
-    if flag[i, j + 1] != INSIDE:
-        if flag[i, j - 1] != INSIDE:
-            grad = (array[i, j + 1] - array[i, j - 1]) * 0.5
-        else:
-            grad = (array[i, j + 1] - array[i, j])
-    else:
-        if flag[i, j - 1] != INSIDE:
-            grad = (array[i, j] - array[i, j - 1])
-        else:
-            grad = 0
+    flag, u, heap = _init_fmm(mask)
 
-    return grad
+    indices = np.transpose(np.where(disk(radius)))
+    indices_centered = np.ascontiguousarray((indices - radius), np.int16)
+
+    while len(heap):
+        i, j = heappop(heap)[1]
+        flag[i, j] = KNOWN
+
+        if ((i <= 1) or (j <= 1) or (i >= image.shape[0] - 2)
+                or (j >= image.shape[1] - 2)):
+            continue
+
+        for (i_nb, j_nb) in (i - 1, j), (i, j - 1), (i + 1, j), (i, j + 1):
+
+            if not flag[i_nb, j_nb] == KNOWN:
+                u[i_nb, j_nb] = min(_eikonal(i_nb - 1, j_nb, i_nb,
+                                             j_nb - 1, flag, u),
+                                    _eikonal(i_nb + 1, j_nb, i_nb,
+                                             j_nb - 1, flag, u),
+                                    _eikonal(i_nb - 1, j_nb, i_nb,
+                                             j_nb + 1, flag, u),
+                                    _eikonal(i_nb + 1, j_nb, i_nb,
+                                             j_nb + 1, flag, u))
+
+                if flag[i_nb, j_nb] == INSIDE:
+                    flag[i_nb, j_nb] = BAND
+                    heappush(heap, (u[i_nb, j_nb], (i_nb, j_nb)))
+
+                    shifted_indices = (indices_centered +
+                                       np.array([i_nb, j_nb], np.int16))
+
+                    _inpaint_point(i_nb, j_nb, image, flag,
+                                   u, shifted_indices, radius)
+
+
+cdef _init_fmm(mask):
+    """Initialisation for Image Inpainting technique based on Fast Marching
+    Method as outined in [1]_. Each pixel has 2 new values assigned to it
+    stored in ``flag`` and ``u`` arrays.
+
+    Parameters
+    ----------
+    mask : array, bool
+        True values are to be inpainted
+
+    Returns
+    ------
+    flag : array, uint8
+        Array marking pixels as known, along the boundary to be solved, or
+        inside the unknown region: 0 = KNOWN, 1 = BAND, 2 = INSIDE
+    u : array, float
+        The distance/time map from the boundary to each pixel
+    heap : list of tuples
+        Priority heap with ``u`` and indices of BAND points
+
+    Notes
+    -----
+    ``flag`` Initialisation:
+    All pixels are classified into 1 of the following flags:
+    - 0 = KNOWN - intensity and u values are known
+    - 1 = BAND - u value undergoes an update
+    - 2 = INSIDE - intensity and u values unkown
+
+    References
+    ----------
+    .. [1] Telea, A., "An Image Inpainting Technique based on the Fast Marching
+           Method", Journal of Graphic Tools (2004).
+           http://iwi.eldoc.ub.rug.nl/FILES/root/2004/JGraphToolsTelea/2004JGraphToolsTelea.pdf
+
+    """
+
+    outside = dilation(mask, disk(1))
+    band = np.logical_xor(mask, outside).astype(np.uint8)
+
+    flag = (2 * outside) - band
+
+    u = np.where(flag == INSIDE, LARGE_VALUE, 0)
+
+    heap = []
+    # Store the ``u`` and indices of pixels marked as BAND points
+    indices = np.transpose(np.where(flag == BAND))
+    for z in indices:
+        heappush(heap, (u[tuple(z)], tuple(z)))
+
+    return flag, u, heap
+
+
+cdef cnp.float_t _eikonal(Py_ssize_t i1, Py_ssize_t j1, Py_ssize_t i2,
+                          Py_ssize_t j2, cnp.uint8_t[:, ::1] flag,
+                          cnp.float_t[:, ::1] u):
+    """Solve a step of the Eikonal equation.
+
+    The``u`` values of known pixels (marked by``flag``) are considered for
+    computing the``u`` value of the neighbouring pixel.
+
+    See Equation 4 and Figure 4 in [1]_ for implementation details.
+
+    Parameters
+    ----------
+    i1, j1, i2, j2 : int
+        Row and column indices of two diagonally-adjacent pixels
+    flag : array
+        Array marking pixels as known, along the boundary to be solved, or
+        inside the unknown region: 0 = KNOWN, 1 = BAND, 2 = INSIDE
+    u : array, float
+        The distance/time map from the boundary to each pixel
+
+    Returns
+    -------
+    u_out : float
+        The``u`` value for the pixel``(i2, j1)``
+
+    Notes
+    -----
+    The boundary is assumed to move with a constant speed in a direction normal
+    to the boundary at all pixels, such that the time of arrival``u`` must be
+    monotonically increasing. Note that``u`` is often denoted``T``.
+
+    References
+    ----------
+    .. [1] Telea, A., "An Image Inpainting Technique based on the Fast Marching
+           Method", Journal of Graphic Tools (2004).
+           http://iwi.eldoc.ub.rug.nl/FILES/root/2004/JGraphToolsTelea/2004JGraphToolsTelea.pdf
+
+    """
+
+    cdef cnp.float_t u_out, u1, u2, r, s
+
+    u_out = LARGE_VALUE
+    u1 = u[i1, j1]
+    u2 = u[i2, j2]
+
+    if flag[i1, j1] == KNOWN:
+        if flag[i2, j2] == KNOWN:
+            r = sqrt(2 - (u1 - u2) ** 2)
+            s = (u1 + u2 - r) * 0.5
+            if s >= u1 and s >= u2:
+                u_out = s
+            else:
+                s += r
+                if s >= u1 and s >= u2:
+                    u_out = s
+        else:
+            u_out = 1 + u1
+    elif flag[i2, j2] == KNOWN:
+        u_out = 1 + u2
+
+    return u_out
 
 
 cdef _inpaint_point(cnp.int16_t i, cnp.int16_t j, cnp.float_t[:, ::1] image,
@@ -80,7 +241,7 @@ cdef _inpaint_point(cnp.int16_t i, cnp.int16_t j, cnp.float_t[:, ::1] image,
         Array marking pixels as known, along the boundary to be solved, or
         inside the unknown region: 0 = KNOWN, 1 = BAND, 2 = INSIDE
     u : array, float
-        The distance/time map from the boundary to each pixel.
+        The distance/time map from the boundary to each pixel
     radius : int
         Neighbourhood of (i, j) to be considered for inpainting
 
@@ -147,155 +308,45 @@ cdef _inpaint_point(cnp.int16_t i, cnp.int16_t j, cnp.float_t[:, ::1] image,
     image[i, j] = (Ia / norm + (Jx + Jy) / sqrt(Jx * Jx + Jy * Jy))
 
 
-cdef cnp.float_t _eikonal(Py_ssize_t i1, Py_ssize_t j1, Py_ssize_t i2,
-                          Py_ssize_t j2, cnp.uint8_t[:, ::1] flag,
-                          cnp.float_t[:, ::1] u):
-    """Solve a step of the Eikonal equation.
+cdef cnp.float_t _grad_func(cnp.float_t[:, :] array,
+                            Py_ssize_t i, Py_ssize_t j,
+                            cnp.uint8_t[:, :] flag):
+    """Return the x-gradient of the input array at a pixel.
 
-    The``u`` values of known pixels (marked by``flag``) are considered for
-    computing the``u`` value of the neighbouring pixel.
-
-    See Equation 4 and Figure 4 in [1]_ for implementation details.
+    This gradient is structured to ignore inner, unknown regions as specified
+    by the `flag` array. By default, this returns the gradient in the
+    x-direction. To get the y-gradient, switch the order of `i`, `j`, and
+    transpose `flag` and `array`.
 
     Parameters
     ----------
-    i1, j1, i2, j2 : int
-        Row and column indices of two diagonally-adjacent pixels.
-    flag : array
-        Array marking pixels as known, along the boundary to be solved, or
-        inside the unknown region: 0 = KNOWN, 1 = BAND, 2 = INSIDE
-    u : array, float
-        The distance/time map from the boundary to each pixel.
-
-    Returns
-    -------
-    u_out : float
-        The``u`` value for the pixel``(i2, j1)``.
-
-    Notes
-    -----
-    The boundary is assumed to move with a constant speed in a direction normal
-    to the boundary at all pixels, such that the time of arrival``u`` must be
-    monotonically increasing. Note that``u`` is often denoted``T``.
-
-    References
-    ----------
-    .. [1] Telea, A., "An Image Inpainting Technique based on the Fast Marching
-           Method", Journal of Graphic Tools (2004).
-           http://iwi.eldoc.ub.rug.nl/FILES/root/2004/JGraphToolsTelea/2004JGraphToolsTelea.pdf
-
-    """
-
-    cdef cnp.float_t u_out, u1, u2, r, s
-
-    u_out = LARGE_VALUE
-    u1 = u[i1, j1]
-    u2 = u[i2, j2]
-
-    if flag[i1, j1] == KNOWN:
-        if flag[i2, j2] == KNOWN:
-            r = sqrt(2 - (u1 - u2) ** 2)
-            s = (u1 + u2 - r) * 0.5
-            if s >= u1 and s >= u2:
-                u_out = s
-            else:
-                s += r
-                if s >= u1 and s >= u2:
-                    u_out = s
-        else:
-            u_out = 1 + u1
-    elif flag[i2, j2] == KNOWN:
-        u_out = 1 + u2
-
-    return u_out
-
-
-cpdef fast_marching_method(cnp.float_t[:, ::1] image,
-                           cnp.uint8_t[:, ::1] flag,
-                           cnp.float_t[:, ::1] u, heap,
-                           _run_inpaint=True, Py_ssize_t radius=5):
-    """Inpaint an image using the Fast Marching Method (FMM).
-
-    Image Inpainting technique based on the Fast Marching Method implementation
-    as described in [1]_. FMM is used for computing the evolution of
-    boundary moving in a direction *normal* to itself.
-
-    Parameters
-    ---------
-    image : array, float
-        Initial input image padded by a single row/column on all sides
+    array : array, float
+        Either ``image`` or ``u``, whose gradient is to be computed
+    i, j : int
+        Row and column index of the pixel whose gradient is to be calculated
     flag : array, uint8
         Array marking pixels as known, along the boundary to be solved, or
         inside the unknown region: 0 = KNOWN, 1 = BAND, 2 = INSIDE
-    u : array, float
-        The distance/time map from the boundary to each pixel.
-    heap : list of tuples
-        Priority heap which stores pixels for processing.
-    _run_inpaint : bool
-        If``True`` then inpaint the image
-        If``False`` then only compute the distance/time map,``u``
-    radius : int
-        Neighbourhood of the pixel of interest
 
-    Notes
-    -----
-    The steps of the algorithm are as follows:
-    - Extract the pixel with the smallest ``u`` value in the BAND pixels
-    - Update its ``flag`` value as KNOWN
-    - March the boundary inwards by adding new points.
-        - If they are either INSIDE or BAND, compute its ``u`` value using the
-          ``eikonal`` function for all the 4 quadrants
-        - If ``flag`` is INSIDE
-            - Change it to BAND
-            - Inpaint the pixel
-        - Select the ``min`` value and assign it as ``u`` value of the pixel
-        - Insert this new value in the ``heap``
-
-    For further details, see [1]_
-
-    References
-    ----------
-    .. [1] Telea, A., "An Image Inpainting Technique based on the Fast Marching
-           Method", Journal of Graphic Tools (2004).
-           http://iwi.eldoc.ub.rug.nl/FILES/root/2004/JGraphToolsTelea/2004JGraphToolsTelea.pdf
+    Returns
+    -------
+    grad : float
+        The local gradient of array
 
     """
 
     cdef:
-        Py_ssize_t i, j,
-        cnp.int16_t i_nb, j_nb
-        cnp.int16_t[:, ::1] shifted_indices
+        cnp.float_t grad
 
-    indices = np.transpose(np.where(disk(radius)))
-    indices_centered = np.ascontiguousarray((indices - radius), np.int16)
+    if flag[i, j + 1] != INSIDE:
+        if flag[i, j - 1] != INSIDE:
+            grad = (array[i, j + 1] - array[i, j - 1]) * 0.5
+        else:
+            grad = (array[i, j + 1] - array[i, j])
+    else:
+        if flag[i, j - 1] != INSIDE:
+            grad = (array[i, j] - array[i, j - 1])
+        else:
+            grad = 0
 
-    while len(heap):
-        i, j = heappop(heap)[1]
-        flag[i, j] = KNOWN
-
-        if ((i <= 1) or (j <= 1) or (i >= image.shape[0] - 2)
-                or (j >= image.shape[1] - 2)):
-            continue
-
-        for (i_nb, j_nb) in (i - 1, j), (i, j - 1), (i + 1, j), (i, j + 1):
-
-            if not flag[i_nb, j_nb] == KNOWN:
-                u[i_nb, j_nb] = min(_eikonal(i_nb - 1, j_nb, i_nb,
-                                             j_nb - 1, flag, u),
-                                    _eikonal(i_nb + 1, j_nb, i_nb,
-                                             j_nb - 1, flag, u),
-                                    _eikonal(i_nb - 1, j_nb, i_nb,
-                                             j_nb + 1, flag, u),
-                                    _eikonal(i_nb + 1, j_nb, i_nb,
-                                             j_nb + 1, flag, u))
-
-                if flag[i_nb, j_nb] == INSIDE:
-                    flag[i_nb, j_nb] = BAND
-                    heappush(heap, (u[i_nb, j_nb], (i_nb, j_nb)))
-
-                    if _run_inpaint:
-                        shifted_indices = (indices_centered +
-                                           np.array([i_nb, j_nb], np.int16))
-
-                        _inpaint_point(i_nb, j_nb, image, flag,
-                                       u, shifted_indices, radius)
+    return grad
