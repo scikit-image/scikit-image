5a7111d9f63e01eb057be4ee3e154ef231b3418f
Chintak Sheth <chintaksheth@gmail.com>
2013-08-13 00:40:46 +0530

    Fix setup.py file; Remove the additional files while fixing merge conflicts

--- skimage/filter/setup.py
+++ skimage/filter/setup.py
@@ -12,39 +12,26 @@ def configuration(parent_package='', top_path=None):
     config = Configuration('filter', parent_package, top_path)
     config.add_data_dir('tests')
 
-    cython(['_inpaint_fmm.pyx'], working_path=base_path)
     cython(['_ctmf.pyx'], working_path=base_path)
     cython(['_denoise_cy.pyx'], working_path=base_path)
+    cython(['_inpaint_fmm.pyx'], working_path=base_path)
     cython(['rank/core_cy.pyx'], working_path=base_path)
     cython(['rank/generic_cy.pyx'], working_path=base_path)
     cython(['rank/percentile_cy.pyx'], working_path=base_path)
     cython(['rank/bilateral_cy.pyx'], working_path=base_path)
 
-    config.add_extension('_inpaint_fmm', sources=['_inpaint_fmm.c'],
-                         include_dirs=[get_numpy_include_dirs()])
     config.add_extension('_ctmf', sources=['_ctmf.c'],
                          include_dirs=[get_numpy_include_dirs()])
     config.add_extension('_denoise_cy', sources=['_denoise_cy.c'],
                          include_dirs=[get_numpy_include_dirs(), '../_shared'])
-    config.add_extension('rank._core8', sources=['rank/_core8.c'],
+    config.add_extension('_inpaint_fmm', sources=['_inpaint_fmm.c'],
                          include_dirs=[get_numpy_include_dirs()])
-    config.add_extension('rank._core16', sources=['rank/_core16.c'],
+    config.add_extension('rank.core_cy', sources=['rank/core_cy.c'],
                          include_dirs=[get_numpy_include_dirs()])
-    config.add_extension('rank._crank8', sources=['rank/_crank8.c'],
-                         include_dirs=[get_numpy_include_dirs()])
-    config.add_extension(
-        'rank._crank8_percentiles', sources=['rank/_crank8_percentiles.c'],
-        include_dirs=[get_numpy_include_dirs()])
-    config.add_extension('rank._crank16', sources=['rank/_crank16.c'],
+    config.add_extension('rank.generic_cy', sources=['rank/generic_cy.c'],
                          include_dirs=[get_numpy_include_dirs()])
     config.add_extension(
-        'rank._crank16_percentiles', sources=['rank/_crank16_percentiles.c'],
-        include_dirs=[get_numpy_include_dirs()])
-    config.add_extension(
-        'rank._crank16_bilateral', sources=['rank/_crank16_bilateral.c'],
-        include_dirs=[get_numpy_include_dirs()])
-    config.add_extension(
-        'rank.percentile_rank', sources=['rank/percentile_rank.c'],
+        'rank.percentile_cy', sources=['rank/percentile_cy.c'],
         include_dirs=[get_numpy_include_dirs()])
     config.add_extension(
         'rank.bilateral_cy', sources=['rank/bilateral_cy.c'],
