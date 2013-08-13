c2f55d4a0717dae1370b92b5dc6056625e647c3d
Chintak Sheth <chintaksheth@gmail.com>
2013-08-13 12:42:09 +0530

    Clean plot_inpaint_efros.py and fix the name

--- doc/examples/plot_inpaint_efros.py
+++ doc/examples/plot_inpaint_efros.py
@@ -1,31 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import data
-from skimage.filter.inpaint_texture import inpaint_efros
-# plt.gray()
+from skimage.filter.inpaint_texture import inpaint_texture
 
 
-__all__ = ['demo_syn']
+image = data.camera()[400:500, 250:350]
+mask = np.zeros_like(image, dtype=np.uint8)
+paint_region = (slice(20, 45), slice(40, 60))
 
+image[paint_region] = 0
+mask[paint_region] = 1
 
-def demo_syn():
-    image = data.camera()[400:500, 250:350]
-    paint_region = (slice(20, 45), slice(40, 60))
+painted = inpaint_texture(image, mask, 5)
 
-    mask = np.zeros_like(image, dtype=np.uint8)
-    image[paint_region] = 0
-    mask[paint_region] = 1
-
-    return image, inpaint_efros(image, mask, 9)
-
-
-def start():
-
-    image, painted = demo_syn()
-
-    fig, (ax0, ax1) = plt.subplots(ncols=2)
-    ax0.imshow(image)
-    ax1.imshow(painted)
-    plt.show()
-
-start()
+fig, (ax0, ax1) = plt.subplots(ncols=2)
+ax0.imshow(image, cmap=plt.cm.gray)
+ax1.imshow(painted, cmap=plt.cm.gray)
+plt.show()
--- viewer_examples/plugins/demo_inpaint_texture.py
+++ viewer_examples/plugins/demo_inpaint_texture.py
@@ -3,7 +3,7 @@ from skimage import data
 from skimage.viewer import ImageViewer
 from skimage.viewer.widgets import history
 from skimage.viewer.plugins.labelplugin import LabelPainter
-from skimage.filter.inpaint_texture import inpaint_efros
+from skimage.filter.inpaint_texture import inpaint_texture
 
 
 class OKCancelButtons(history.OKCancelButtons):
@@ -26,12 +26,12 @@ class InpaintPlugin(LabelPainter):
 
     def _show_inpaint(self):
         viewer = self.image_viewer
-        painted = inpaint_efros(viewer.image, self.paint_tool.overlay)
+        painted = inpaint_texture(viewer.image, self.paint_tool.overlay)
         viewer.ax.imshow(painted)
         viewer.redraw()
 
 
-image = data.camera()
+image = data.checkerboard()
 plugin = InpaintPlugin()
 plugin += OKCancelButtons()
 plt.gray()
