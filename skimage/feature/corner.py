b8958ccee08403abf48da76f8cca9831eaccc6da
Ankit Agrawal <aaaagrawal@iitb.ac.in>
2013-08-21 01:44:04 +0530

    Transferring all the FAST code to corner.py and corner_cy.pyx

--- skimage/feature/corner.py
+++ skimage/feature/corner.py
@@ -1,9 +1,12 @@
 import numpy as np
 from scipy import ndimage
 from scipy import stats
+from scipy.ndimage.filters import maximum_filter
 from skimage.color import rgb2grey
 from skimage.util import img_as_float, pad
 from skimage.feature import peak_local_max
+
+from corner_cy import _corner_fast
 
 
 def _compute_derivatives(image):
@@ -542,3 +545,49 @@ def corner_peaks(image, min_distance=10, threshold_abs=0, threshold_rel=0.1,
         return np.transpose(peaks.nonzero())
     else:
         return peaks
+
+
+def corner_fast(image, n=12, threshold=0.15):
+
+    """Extract FAST corners for a given image.
+
+    Parameters
+    ----------
+    image : 2D ndarray
+        Input image.
+    n : int
+        Number of consecutive pixels out of 16 pixels on the circle that
+        should be brighter or darker with respect to test pixel above the
+        `threshold` so as to classify the test pixel as a FAST corner. Also
+        stands for the n in `FAST-n` corner detector.
+    threshold : float
+        Threshold used in deciding whether the pixels on the circle are
+        brighter, darker or similar w.r.t. the test pixel. Decrease the
+        threshold when more corners are desired and vice-versa.
+
+    Returns
+    -------
+    corners : (N, 2) ndarray
+        Location i.e. (row, col) of extracted FAST corners.
+
+    References
+    ----------
+    .. [1] Edward Rosten and Tom Drummond
+           "Machine Learning for high-speed corner detection",
+           http://www.edwardrosten.com/work/rosten_2006_machine.pdf
+
+    """
+    image = np.squeeze(image)
+    if image.ndim != 2:
+        raise ValueError("Only 2-D gray-scale images supported.")
+
+    image = np.ascontiguousarray(image, dtype=np.double)
+    corner_response = _corner_fast(image, n, threshold)
+
+    # Non-maximal Suppression
+    corner_zero_mask = corner_response != 0
+    maximas = (maximum_filter(corner_response, (3, 3)) == corner_response) & corner_zero_mask
+    x, y = np.where(maximas == True)
+
+    corners = np.squeeze(np.dstack((x, y)))
+    return corners
