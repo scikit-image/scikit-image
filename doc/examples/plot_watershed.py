c68dab766e21f3583100d94a98a9a5cec08955ab
François Boulogne <fboulogne sciunto org>
2013-08-25 14:52:30 +0200

    DOC: add titles to plots

--- doc/examples/plot_watershed.py
+++ doc/examples/plot_watershed.py
@@ -52,8 +52,11 @@ fig, axes = plt.subplots(ncols=3, figsize=(8, 2.7))
 ax0, ax1, ax2 = axes
 
 ax0.imshow(image, cmap=plt.cm.gray, interpolation='nearest')
+ax0.set_title('Overlapping objects')
 ax1.imshow(-distance, cmap=plt.cm.jet, interpolation='nearest')
+ax1.set_title('Distances')
 ax2.imshow(labels, cmap=plt.cm.spectral, interpolation='nearest')
+ax2.set_title('Separated objects')
 
 for ax in axes:
     ax.axis('off')
