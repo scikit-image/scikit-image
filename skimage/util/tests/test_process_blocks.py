9f7e63a027b27c176550352d983b946fc56a82b2
Riaan van den Dool <rvddool@csir.co.za>
2013-09-10 22:18:55 +0200

    Adding some more tests.

--- skimage/util/tests/test_process_blocks.py
+++ skimage/util/tests/test_process_blocks.py
@@ -22,6 +22,35 @@ def test_process_windows_2D_array_args():
     assert_equal(B, np.array([[[1, 9], [5, 13]],
                               [[17, 25], [21, 29]]]))
 
+def test_process_windows_2D_array_args_with_overlap():
+    A = np.arange(5 * 6).reshape((5, 6))
+    B = process_blocks(A, (3, 4), np.sum, overlap=2)
+    """ Currently:
+        >>> A
+        array([[ 0,  1,  2,  3,  4,  5],
+               [ 6,  7,  8,  9, 10, 11],
+               [12, 13, 14, 15, 16, 17],
+               [18, 19, 20, 21, 22, 23],
+               [24, 25, 26, 27, 28, 29]])
+        >>> B.shape
+        (2,2)
+        >>> B
+        array([[ 90, 114],
+               [234, 258]])
+        Expected:
+        >>> B.shape
+        (3,2)
+        
+    """
+    
+    
+
+def test_process_windows_2D_array_args_with_2D_overlap():
+    A = np.arange(5 * 6).reshape((5, 6))
+    print "Allow n-dimensional overlap specifications?"
+    B = process_blocks(A, (3, 4), np.sum, overlap=(2,2))
+    
+
 
 if __name__ == '__main__':
     np.testing.run_module_suite()
