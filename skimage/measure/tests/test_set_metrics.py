2089df3cc2ba93b0ce1805e03b870594a3f041dd
Jostein Bø Fløystad <jostein.floystad@gmail.com>
2013-09-30 09:01:47 +0200

    hausdorff: Test images with single-region pixels.

--- skimage/measure/tests/test_set_metrics.py
+++ skimage/measure/tests/test_set_metrics.py
@@ -1,7 +1,9 @@
 from __future__ import print_function, division
 
 import numpy as np
+from numpy.testing import assert_almost_equal
 import math
+import itertools
 
 from skimage.measure import hausdorff_distance, hausdorff_distance_region
 
@@ -12,3 +14,21 @@ def test_hausdorff_empty():
     assert math.isinf(hausdorff_distance(empty, non_empty))
     assert math.isinf(hausdorff_distance(non_empty, empty))
     assert hausdorff_distance(empty, empty) == 0.
+
+
+def check_hausdorff_region_single(shape, coords_a, coords_b, distance):
+    a = np.zeros(shape, dtype=np.bool)
+    b = np.zeros(shape, dtype=np.bool)
+    a[coords_a] = True
+    b[coords_b] = True
+    assert_almost_equal(hausdorff_distance_region(a, b), distance)
+
+
+def test_hausdorff_region_single():
+    shape = (5, 5)
+    coords = [(0, 0), (3, 0), (1, 4), (4, 1)]
+    for coords_a, coords_b in itertools.product(coords, repeat=2):
+        distance = np.sqrt(sum((ca - cb)**2
+                               for ca, cb in zip(coords_a, coords_b)))
+        yield (check_hausdorff_region_single,
+               shape, coords_a, coords_b, distance)
