ce6b1d0951bd470d03a29095c3f97e39d7e0e8fd
Stefan van der Walt <stefan@sun.ac.za>
2013-10-19 14:20:54 +0200

    Contrib script now shows PRs and merges.

--- doc/release/contribs.py
+++ doc/release/contribs.py
@@ -13,10 +13,27 @@ tag = sys.argv[1]
 def call(cmd):
     return subprocess.check_output(shlex.split(cmd)).split('\n')
 
-
 tag_date = call("git show --format='%%ci' %s" % tag)[0]
 print "Release %s was on %s" % (tag, tag_date)
-print "\nCommitters since, alphabetical by last name:\n"
+
+merges = call("git log --since='%s' --merges --format='>>>%%B' --reverse" % tag_date)
+merges = [m for m in merges if m.strip()]
+merges = '\n'.join(merges).split('>>>')
+merges = [m.split('\n')[:2] for m in merges]
+merges = [m for m in merges if len(m) == 2 and m[1].strip()]
+
+print "\nIt contained the following %d merges:" % len(merges)
+print
+for (merge, message) in merges:
+    if merge.startswith('Merge pull request #'):
+        PR = ' (%s)' % merge.split()[3]
+    else:
+        PR = ''
+
+    print '- ' + message + PR
+
+
+print "\nMade by the following committers [alphabetical by last name]:\n"
 
 authors = call("git log --since='%s' --format=%%aN" % tag_date)
 authors = [a.strip() for a in authors if a.strip()]
