a23e31b5539d6d8db268451600ebf71293c02d15
Tony S Yu <tsyu80@gmail.com>
2013-11-18 22:33:42 -0600

    Factor out table formatting code.

--- skimage/io/__init__.py
+++ skimage/io/__init__.py
@@ -22,6 +22,14 @@ def _separator(char, lengths):
     return [char * separator_length for separator_length in lengths]
 
 
+def _format_plugin_info_table(info_table, column_lengths):
+    """Add separators and column titles to plugin info table."""
+    info_table.insert(0, _separator('=', column_lengths))
+    info_table.insert(1, ('Plugin', 'Description'))
+    info_table.insert(2, _separator('-', column_lengths))
+    info_table.append(_separator('-', column_lengths))
+
+
 def _update_doc(doc):
     """Add a list of plugins to the module docstring, formatted as
     a ReStructuredText table.
@@ -29,21 +37,17 @@ def _update_doc(doc):
     """
     from textwrap import wrap
 
-    info = [(p, plugin_info(p).get('description', 'no description'))
-            for p in available_plugins if not p == 'test']
+    info_table = [(p, plugin_info(p).get('description', 'no description'))
+                  for p in available_plugins if not p == 'test']
 
-    col_1_len = max([len(n) for (n, _) in info])
-    col_2_len = WRAP_LEN - 1 - col_1_len
+    name_length = max([len(n) for (n, _) in info_table])
+    description_length = WRAP_LEN - 1 - name_length
+    column_lengths = [name_length, description_length]
+    _format_plugin_info_table(info_table, column_lengths)
 
-    # Insert table header
-    info.insert(0, _separator('=', (col_1_len, col_2_len)))
-    info.insert(1, ('Plugin', 'Description'))
-    info.insert(2, _separator('-', (col_1_len, col_2_len)))
-    info.append(_separator('-', (col_1_len, col_2_len)))
-
-    for (name, plugin_description) in info:
-        wrapped_descr = wrap(plugin_description, col_2_len)
-        doc += "%s %s\n" % (name.ljust(col_1_len), '\n'.join(wrapped_descr))
+    for (name, plugin_description) in info_table:
+        wrapped_descr = wrap(plugin_description, description_length)
+        doc += "%s %s\n" % (name.ljust(name_length), '\n'.join(wrapped_descr))
     doc = doc.strip()
 
     return doc
