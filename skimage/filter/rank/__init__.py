cb310feab111e2dafc95360eca4c34cf3e9c7388
Olivier Debeir <odebeir@ulb.ac.be>
2013-12-06 13:42:06 +0100

    fix:rtank.__init__

--- skimage/filter/rank/__init__.py
+++ skimage/filter/rank/__init__.py
@@ -4,7 +4,7 @@ from .generic import (autolevel, bottomhat, equalize, gradient, maximum, mean,
 from ._percentile import (autolevel_percentile, gradient_percentile,
                           mean_percentile, subtract_mean_percentile,
                           enhance_contrast_percentile, percentile,
-                          pop_percentile,sum_percentile, threshold_percentile)
+                          pop_percentile, sum_percentile, threshold_percentile)
 from .bilateral import mean_bilateral, pop_bilateral, sum_bilateral
 
 from skimage._shared.utils import deprecated
@@ -52,6 +52,8 @@ __all__ = ['autolevel',
            'pop_percentile',
            'pop_bilateral',
            'sum',
+           'sum_bilateral',
+           'sum_percentile',
            'threshold',
            'threshold_percentile',
            'tophat',
