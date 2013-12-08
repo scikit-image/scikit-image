688778b572527f7b6f5126d735501480db58652c
Johannes Schönberger <jsch@demuc.de>
2013-12-09 00:57:07 +0100

    Move inlined function definitions to header for actual inlining

--- skimage/_shared/setup.py
+++ skimage/_shared/setup.py
@@ -14,12 +14,9 @@ def configuration(parent_package='', top_path=None):
     config.add_data_dir('tests')
 
     cython(['geometry.pyx'], working_path=base_path)
-    cython(['interpolation.pyx'], working_path=base_path)
     cython(['transform.pyx'], working_path=base_path)
 
     config.add_extension('geometry', sources=['geometry.c'])
-    config.add_extension('interpolation', sources=['interpolation.c'],
-                         include_dirs=[get_numpy_include_dirs()])
     config.add_extension('transform', sources=['transform.c'],
                          include_dirs=[get_numpy_include_dirs()])
 
