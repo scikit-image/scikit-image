fdd880aa7f8e3595dc281cf6aa0b4c3cbfdfaa8b
Almar Klein <almar.klein@gmail.com>
2013-12-14 23:58:45 +0100

    update-mcp: add new MCP classes to skimage.graph namespace

--- skimage/graph/__init__.py
+++ skimage/graph/__init__.py
@@ -1,7 +1,9 @@
 from .spath import shortest_path
-from .mcp import MCP, MCP_Geometric, route_through_array
+from .mcp import MCP, MCP_Geometric, MCP_Connect, MCP_Flexible, route_through_array
 
 __all__ = ['shortest_path',
            'MCP',
            'MCP_Geometric',
+           'MCP_Connect',
+           'MCP_Flexible',
            'route_through_array']
