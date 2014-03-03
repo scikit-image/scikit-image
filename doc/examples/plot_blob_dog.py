5ca75a166331f393a254259f9345126984eea514
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2014-03-03 18:02:59 +0530

    added hubble deep field image

--- doc/examples/plot_blob_dog.py
+++ doc/examples/plot_blob_dog.py
@@ -10,22 +10,38 @@ plot their position on the image.
 
 """
 from skimage.feature import blob_dog
-from skimage import data
+from skimage import data,color
 from matplotlib import pyplot as plt
 import math
 
-img = data.coins()
-blobs = blob_dog(img)
+print """
+Between December 18 - December 28, 1995 the Hubble Space telescopse took a 
+series of photographs of a seemingly empty empty patch which covered about 
+one 24-millionth of the whole sky. The results were astonishing.The example
+used about 1/4th of the original picture.
+
+Source
+------
+en.wikipedia.org/wiki/Hubble_Deep_Field
+"""
+
+img = data.hubble_deep_field()[0:300,0:300]
+gray_img = color.rgb2gray(img)
+blobs = blob_dog(gray_img,threshold = .5,min_sigma = 0.1,max_sigma = 10)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 plt.imshow(img, cmap='gray')
-
+num = len(blobs)
 
 for b in blobs:
     y, x = b[0], b[1]
     r = math.sqrt(b[2] / math.pi)
     c = plt.Circle((x, y), r, color='#ff0000', lw=2, fill=False)
     ax.add_patch(c)
+
+print """This picture contains atleast %d detected stellar objects
+Yet another reminder of the extraordinaty vastness of our universe.
+""" % blobs.shape[0]
 
 plt.show()
