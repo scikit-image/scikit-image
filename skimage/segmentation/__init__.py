44f3e1b1365cbcabafae129354203161cd3e5fec
Johannes Schönberger <jsch@demuc.de>
2014-04-09 09:31:20 -0400

    Remove deprecated skimage.segmentation.visualize_boundaries

--- skimage/segmentation/__init__.py
+++ skimage/segmentation/__init__.py
@@ -2,7 +2,7 @@ from .random_walker_segmentation import random_walker
 from ._felzenszwalb import felzenszwalb
 from .slic_superpixels import slic
 from ._quickshift import quickshift
-from .boundaries import find_boundaries, visualize_boundaries, mark_boundaries
+from .boundaries import find_boundaries, mark_boundaries
 from ._clear_border import clear_border
 from ._join import join_segmentations, relabel_from_one, relabel_sequential
 
@@ -12,7 +12,6 @@ __all__ = ['random_walker',
            'slic',
            'quickshift',
            'find_boundaries',
-           'visualize_boundaries',
            'mark_boundaries',
            'clear_border',
            'join_segmentations',
