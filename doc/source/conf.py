8d2d7e6bd8fb2b138ea33115fb0d543d7cb6fc46
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2014-05-03 02:11:39 +0530

    Added files and required code for data images gallery.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -145,7 +145,7 @@ html_title = 'skimage v%s docs' % version
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static','data_gallery/images']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
