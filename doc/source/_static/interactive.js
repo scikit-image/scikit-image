30fcbadd014a05ffb6cf3fae054043ad2e71ad1a
Rishabh Raj <rishabhr123@gmail.com>
2014-07-02 14:03:52 -0400

    edit code only when fetch is successful

--- doc/source/_static/interactive.js
+++ doc/source/_static/interactive.js
@@ -8,22 +8,25 @@ $(document).ready(function() {
 		code_url = $('a.download.internal:first').attr('href');
 		// fetch height of div which showed the code
 		var code_height = $('.highlight-python').height();
-		// replace div with editor
-		$('.highlight-python').replaceWith('<div id="editor"></div>');
 
 		// fetch code and insert into editor
-		$.get(code_url, function(data){
-			var editor = ace.edit("editor");
-			
-			$('#editor').height(code_height);
-			// place curson at end to prevent entire code being selected 
-			// after using setValue (which is a feature)
-			editor.setValue(data, 1);
-    		
-    		// editor.setTheme("ace/theme/monokai");
-    		editor.getSession().setMode("ace/mode/python");
-		});
-	});
+		$.get(code_url, function(data, status){
+			if (status === "success"){
+				// replace div with editor
+				$('.highlight-python').replaceWith('<div id="editor"></div>');
+
+				var editor = ace.edit("editor");
+
+				$('#editor').height(code_height);
+				// place curson at end to prevent entire code being selected 
+				// after using setValue (which is a feature)
+				editor.setValue(data, 1);
+
+                // editor.setTheme("ace/theme/monokai");
+                editor.getSession().setMode("ace/mode/python");
+            }
+        });
+    });
 
 	// revert back to example inside div
 	$('#reload').bind('click', function(){
