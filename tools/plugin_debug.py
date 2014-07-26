46e3f804579df97e01fa9f391e8a31f8be28b0b1
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2014-07-26 14:13:11 +0530

    Added plugin debug script

---
+++ tools/plugin_debug.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from skimage import io
+
+for key in (io.manage_plugins.plugin_store):
+    print(key,"->",io.manage_plugins.plugin_store[key])
