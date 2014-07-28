78caebf6d25b6bbba070b0d3b74abc26800dfa36
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2014-07-29 03:31:12 +0530

    string wrap

--- doc/examples/plot_rag_draw.py
+++ doc/examples/plot_rag_draw.py
@@ -21,8 +21,8 @@ plt.imshow(out)
 
 out = graph.rag_draw(labels, g, img, high_color=(1, 0, 0), thresh=30)
 plt.figure()
-plt.title("RAG with edge weights less than 30,\
-          color mapped between green and red.")
+plt.title("RAG with edge weights less than 30, color "
+          "mapped between green and red.")
 
 plt.imshow(out)
 plt.show()
