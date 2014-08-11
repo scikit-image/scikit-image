535a508879473106036c8b826641ceaf981429bc
Rishabh Raj <rishabhr123@gmail.com>
2014-08-11 11:40:59 +0530

    add server response servertime

--- doc/source/_static/interactive.js
+++ doc/source/_static/interactive.js
@@ -210,7 +210,7 @@ $(document).ready(function () {
                     if (e.result.hasOwnProperty('busy')) {
                         $('#success-message').html("Server Busy, try again later!").show();
                     } else {
-                        $('#success-message').html("Success: Received " + num_images + " image(s)").show();
+                        $('#success-message').html("Success: Received " + num_images + " image(s) at " + e.timestamp + " UTC -5").show();
                     }
                     code_running = false;
                 },
