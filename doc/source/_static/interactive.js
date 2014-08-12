2e40287632523677f766f7eee8b4ec04a2b67101
Rishabh Raj <rishabhr123@gmail.com>
2014-08-12 18:14:51 +0530

    enable execution for multiple code snippets

--- doc/source/_static/interactive.js
+++ doc/source/_static/interactive.js
@@ -5,10 +5,14 @@ $(document).ready(function () {
     var backup,
         encodedcode = $(".tobehidden"),
         code_running = false,
-        editor;
+        editor,
+        snippet,
+        snippet_index;
 
     // Create Base64 Object
     var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+    var clear_images = "\nplt.close()\n";
 
     // hide Run, only visible when code edited
     // hide loading animation, visible, when code running
@@ -150,6 +154,19 @@ $(document).ready(function () {
         $('.all-output').show();
     }
 
+    function getcode() {
+        var resulting_code = '',
+            enc_snippet;
+        for(var i=0; i<snippet_index; i++) {
+            enc_snippet = encodedcode.eq(i).html();
+            enc_snippet = Base64.decode(enc_snippet);
+            resulting_code = resulting_code + enc_snippet;
+            resulting_code = resulting_code + clear_images;
+        }
+        resulting_code = resulting_code + editor.getValue();
+        return resulting_code;
+    }
+
     function runcode() {
         if (!code_running) {
             code_running = true;
@@ -167,7 +184,7 @@ $(document).ready(function () {
 
             $('#runcode').hide();
 
-            var code = editor.getValue(),
+            var code = getcode(),
             // console.log(code);
                 jcode = codetoJSON(code);
                 // get editor-bg
@@ -241,8 +258,8 @@ $(document).ready(function () {
             // replace div with editor
             backup = $(this);
 
-            var snippet_index = $(this).closest('div.highlight-python').index('div.highlight-python'),
-                snippet = encodedcode.eq(snippet_index).html();
+            snippet_index = $(this).closest('div.highlight-python').index('div.highlight-python'),
+            snippet = encodedcode.eq(snippet_index).html();
 
             $(this).replaceWith('<div id="editor"></div>');
 
@@ -274,8 +291,8 @@ $(document).ready(function () {
         // replace div with editor
         backup = $(this);
 
-        var snippet_index = $(this).closest('div.highlight-python').index('div.highlight-python'),
-            snippet = encodedcode.eq(snippet_index).html();
+        snippet_index = $(this).closest('div.highlight-python').index('div.highlight-python'),
+        snippet = encodedcode.eq(snippet_index).html();
 
         $(this).replaceWith('<div id="editor"></div>');
         console.log(snippet_index);
