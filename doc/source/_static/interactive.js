9388d5d385374d03fb2cd1ccb66297d4c4ec7ed9
Rishabh Raj <rishabhr123@gmail.com>
2014-08-16 13:22:51 +0530

    fix the reload button, remove use for Esc

--- doc/source/_static/interactive.js
+++ doc/source/_static/interactive.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     $.support.cors = true;
 
     // create backup of div containing code
-    var backup,
+    var backup = [],
+        editor = [],
         encodedcode = $(".tobehidden"),
-        code_running = false,
-        editor = [];
+        code_running = false;
 
     // Create Base64 Object
     var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
@@ -23,12 +23,14 @@ $(document).ready(function () {
 
     $('div.highlight-python').each(function (index) {
         $(this).data('index', index);
+        backup[index] = $(this);
     });
 
     $('.editcode').each(function (index) {
         $(this).data('index', index);
     });
 
+    // inspired from a JSfiddle
     String.format = function() {
         // The string containing the format items (e.g. "{0}")
         // will and always has to be the first argument.
@@ -211,6 +213,7 @@ $(document).ready(function () {
             $('img.output_image').remove();
 
             $('#runcode').hide();
+            $('#reload').hide();
 
             var code = getcode(snippet_index),
             // console.log(code);
@@ -242,6 +245,7 @@ $(document).ready(function () {
                     // TODO: Refactor to something like reset
                     $('#loading').hide();
                     $('#runcode').show();
+                    $('#reload').show();
                     handleoutput(e, snippet_index);
                     // suggest number of images received
                     if ($.isEmptyObject(e.result)) {
@@ -265,6 +269,7 @@ $(document).ready(function () {
                     // TODO: Refactor to something like reset
                     $('#loading').hide();
                     $('#runcode').show();
+                    $('#reload').show();
 
                     error_code = jqxhr.status;
                     error_text = jqxhr.statusText;
@@ -279,29 +284,35 @@ $(document).ready(function () {
     }
 
     function reload () {
-        $('div#editor').replaceWith(backup);
+        // replace all code snippets (in edit mode or otherwise) with default value
+        console.log(backup.length);
+        for(var i=0; i<backup.length; i++){
+            if (editor[i]) {
+                $('#editor' + i).replaceWith(backup[i]);
+            }
+        }
+        
+        $('div.highlight-python').each(function (index) {
+            $(this).data('index', index);
+        });
+
         $('div.highlight-python').unbind('click');
         // replacing messes up event handlers so need to bind again
         $('div.highlight-python').bind('click', function (){
-            // if one editor is already active, do nothing
-            if ($('#editor').length > 0) {
-                return;
-            }
-            // replace div with editor
-            backup = $(this);
+            // fetch height of div which showed the code
+            var code_height = $(this).height(),
+                snippet_index = $(this).data('index'),
+                snippet = encodedcode.eq(snippet_index).html();
 
-            snippet_index = $(this).data('index'),
-            snippet = encodedcode.eq(snippet_index).html();
-
-            $(this).replaceWith('<div id="editor"></div>');
+            $(this).replaceWith(String.format('<div id="editor{0}"></editor>', snippet_index));
 
             snippet = Base64.decode(snippet);
-            editcode(snippet);
+            editcode(snippet, snippet_index, code_height);
         });
         // hide Run, only visible when code edited
         $('#runcode').hide();
         $('#loading').hide();
-        $('.editcode').eq(snippet_index).show();
+        $('.editcode').show();
         $('.all-output').hide();
     }
 
@@ -312,9 +323,6 @@ $(document).ready(function () {
         var edit_btn_index = $(this).data('index'),
             snippet_index = edit_btn_index,
             snippet = encodedcode.eq(snippet_index).html();
-
-        // replace div with editor
-        backup = $('div.highlight-python').eq(edit_btn_index);
 
         $('div.highlight-python').each(function (){
             if ($(this).data('index') === snippet_index) {
@@ -333,23 +341,15 @@ $(document).ready(function () {
             snippet_index = $(this).data('index'),
             snippet = encodedcode.eq(snippet_index).html();
 
-        // replace div with editor
-        backup = $(this);
-
         $(this).replaceWith(String.format('<div id="editor{0}"></editor>', snippet_index));
 
         snippet = Base64.decode(snippet);
         editcode(snippet, snippet_index, code_height);
     });
 
-    $('#runcode').bind('click', runcode);
-    // WIP: DISABLE RELOAD FOR NOW
-    // revert back to example inside div
-    //$('#reload').bind('click', reload);
+    // TODO: Finalize what this button is for
+    //$('#runcode').bind('click', runcode);
 
-    $(document).keyup(function(e) {
-        if (e.keyCode == 27) {
-            //reload();
-        }   // esc
-    })
+    // revert back to example inside div
+    $('#reload').bind('click', reload);
 });
