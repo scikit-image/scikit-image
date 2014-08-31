f86b6212bc733afe189b26d6fc87be07f1a06c44
Julien Coste <julien.coste@inria.fr>
2014-08-31 12:27:02 +0100

    Canny is now deprecated and will be in api_changes in v0.13

--- skimage/filter/__init__.py
+++ skimage/filter/__init__.py
@@ -1,6 +1,5 @@
 from .lpi_filter import inverse, wiener, LPIFilter2D
 from ._gaussian import gaussian_filter
-# Backward compatibility v<0.11
 from .edges import (sobel, hsobel, vsobel, scharr, hscharr, vscharr, prewitt,
                     hprewitt, vprewitt, roberts, roberts_positive_diagonal,
                     roberts_negative_diagonal)
@@ -9,8 +8,6 @@ from ._gabor import gabor_kernel, gabor_filter
 from .thresholding import (threshold_adaptive, threshold_otsu, threshold_yen,
                            threshold_isodata)
 from . import rank
-from ..feature import canny
-
 
 from skimage._shared.utils import deprecated
 from skimage import restoration
@@ -20,6 +17,10 @@ denoise_tv_bregman = deprecated('skimage.restoration.denoise_tv_bregman')\
                         (restoration.denoise_tv_bregman)
 denoise_tv_chambolle = deprecated('skimage.restoration.denoise_tv_chambolle')\
                         (restoration.denoise_tv_chambolle)
+
+# Backward compatibility v<0.11
+from ..feature import canny
+canny = deprecated('skimage.feature.canny')(canny)
 
 
 __all__ = ['inverse',
