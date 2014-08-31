Fixed some issues with the new windowed_histogram function in filter.rank. It used to be able to output uint8 histogram, whose max pixel counts of 255 could easily overflow. Addressed by limiting the output type to float. Normalized histograms are now generated, otherwise behaviour is unpredictable at boundaries or when pixels are not permitted by a mask. The new optional n_bins parameter allows the caller to specify the size of the histogram generated. Having it fixed to image.max()+1 could result in feature vectors being shorter than desired just due to a value not being used in an image. An example has been added to the docs, that demonstrates the application of windows histograms in object matching; a single coin is extracted and found by chi squared histogram matching.

skimage/filter/rank/generic.py
@@ -868,8 +868,8 @@ def otsu(image, selem, out=None, mask=None, shift_x=False, shift_y=False):
                   mask=mask, shift_x=shift_x, shift_y=shift_y)
 
 
-def windowed_histogram(image, selem, out=None, mask=None, shift_x=False, shift_y=False):
-    """Sliding window histogram
+def windowed_histogram(image, selem, out=None, mask=None, shift_x=False, shift_y=False, n_bins=None):
+    """Normalized sliding window histogram
 
     Parameters
     ----------
@@ -886,15 +886,19 @@ def windowed_histogram(image, selem, out=None, mask=None, shift_x=False, shift_y
         Offset added to the structuring element center point. Shift is bounded
         to the structuring element sizes (center must be inside the given
         structuring element).
+    n_bins : int or None
+        The number of histogram bins. Will default to image.max() + 1 if None
+        is passed.
 
     Returns
     -------
-    out : 3-D array (same dtype as input image) whose extra dimension
-        Output image.
-
-    References
-    ----------
-    .. [otsu] http://en.wikipedia.org/wiki/Otsu's_method
+    out : 3-D array with float dtype of dimensions (H,W,N), where (H,W) are
+        the dimensions of the input image and N is n_bins or image.max()+1
+        if no value is provided as a parameter. Effectively, each pixel
+        is an N-dimensional feature vector that is the histogram.
+        The sum of the elements in the feature vector will be 1, unless
+        no pixels in the window were covered by both selem and mask, in which
+        case all elements will be 0.
 
     Examples
     --------
@@ -903,9 +907,14 @@ def windowed_histogram(image, selem, out=None, mask=None, shift_x=False, shift_y
     >>> from skimage.morphology import disk
     >>> img = data.camera()
     >>> hist_img = windowed_histogram(img, disk(5))
-    >>> thresh_image = img >= local_otsu
 
     """
 
-    return _apply_vector_per_pixel(generic_cy._windowed_hist, image, selem, out=out,
-                  mask=mask, shift_x=shift_x, shift_y=shift_y, pixel_size=image.max()+1)
+    if n_bins is None:
+        n_bins = image.max() + 1
+
+    return _apply_vector_per_pixel(generic_cy._windowed_hist, image, selem,
+                                   out=out, mask=mask,
+                                   shift_x=shift_x, shift_y=shift_y,
+                                   out_dtype=np.double,
+                                   pixel_size=n_bins)

skimage/filter/rank/tests/test_rank.py
@@ -551,22 +551,35 @@ def test_windowed_histogram():
                        [0, 1, 1, 1, 0],
                        [0, 0, 0, 0, 0]], dtype=np.uint8)
     elem = np.ones((3, 3), dtype=np.uint8)
-    out8 = np.empty(image8.shape+(2,), dtype=np.uint8)
+    outf = np.empty(image8.shape+(2,), dtype=float)
     mask = np.ones(image8.shape, dtype=np.uint8)
 
+    # Population so we can normalize the expected output while maintaining
+    # code readability
+    pop = np.array([[4, 6, 6, 6, 4],
+                    [6, 9, 9, 9, 6],
+                    [6, 9, 9, 9, 6],
+                    [6, 9, 9, 9, 6],
+                    [4, 6, 6, 6, 4]], dtype=float)
+
     r0 =  np.array([[3, 4, 3, 4, 3],
-           [4, 5, 3, 5, 4],
-           [3, 3, 0, 3, 3],
-           [4, 5, 3, 5, 4],
-           [3, 4, 3, 4, 3]], dtype=np.uint8)
+                    [4, 5, 3, 5, 4],
+                    [3, 3, 0, 3, 3],
+                    [4, 5, 3, 5, 4],
+                    [3, 4, 3, 4, 3]], dtype=float) / pop
     r1 =  np.array([[1, 2, 3, 2, 1],
-           [2, 4, 6, 4, 2],
-           [3, 6, 9, 6, 3],
-           [2, 4, 6, 4, 2],
-           [1, 2, 3, 2, 1]], dtype=np.uint8)
-    rank.windowed_histogram(image=image8, selem=elem, out=out8, mask=mask)
-    assert_array_equal(r0, out8[:,:,0])
-    assert_array_equal(r1, out8[:,:,1])
+                    [2, 4, 6, 4, 2],
+                    [3, 6, 9, 6, 3],
+                    [2, 4, 6, 4, 2],
+                    [1, 2, 3, 2, 1]], dtype=float) / pop
+    rank.windowed_histogram(image=image8, selem=elem, out=outf, mask=mask)
+    assert_array_equal(r0, outf[:,:,0])
+    assert_array_equal(r1, outf[:,:,1])
+
+    # Test n_bins parameter
+    larger_output = rank.windowed_histogram(image=image8, selem=elem,
+                                            mask=mask, n_bins=5)
+    assert larger_output.shape[2] == 5
 
 
 if __name__ == "__main__":