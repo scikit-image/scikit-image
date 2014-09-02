fc18fb7ffa3987d3bd258d1162aa8a1886e9e5cc
Rebecca <rebeccaroisin@gmail.com>
2014-09-02 08:54:49 +0100

    removed lena reference

--- skimage/data/__init__.py
+++ skimage/data/__init__.py
@@ -71,7 +71,7 @@ def astronaut():
     Photograph of Eileen Collins, an American astronaut. She was selected 
     as an astronaut in 1992 and first piloted the space shuttle STS-63 in
     1995. She retired in 2006 after spending a total of 38 days, 8 hours 
-    and 10 minutes in outer space. Possible replacement for Lena. 
+    and 10 minutes in outer space.
 
     This image was downloaded from the NASA Great Images database
     <http://grin.hq.nasa.gov/ABSTRACTS/GPN-2000-001177.html>`__.
