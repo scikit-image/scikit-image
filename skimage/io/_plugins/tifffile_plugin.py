c5b2834084519aa06353a85758f444064f1d6875
Steven Silvester <steven.silvester@ieee.org>
2014-10-05 20:53:01 -0500

    Remove old tifffile plugin in favor of using it in PIL plugin.

--- skimage/io/_plugins/tifffile_plugin.py
+++
@@ -1,6 +0,0 @@
-try:
-    from tifffile import imread, imsave
-except ImportError:
-    raise ImportError("The tifffile module could not be found.\n"
-        "It can be obtained at "
-        "<http://www.lfd.uci.edu/~gohlke/code/tifffile.py>\n")
