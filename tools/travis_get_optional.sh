87d3a07b71003f37ab31a22b9cc51e991e3999a9
Steven Silvester <steven.silvester@ieee.org>
2014-10-09 20:01:43 -0500

    Reorganize files

---
+++ tools/travis_get_optional.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/sh
+./header.py "Install optional dependencies"
+
+# Install Qt and then update the Matplotlib settings
+if [[ $TRAVIS_PYTHON_VERSION == 2.7* ]]; then
+    sudo apt-get install -q python-qt4
+    SCI_QT_API=PyQt4
+
+else
+    sudo apt-get install -q libqt4-dev
+    pip install PySide $WHEELHOUSE
+    python ~/virtualenv/python${TRAVIS_PYTHON_VERSION}/bin/pyside_postinstall.py -install
+    SCI_QT_API=PySide
+fi
+
+# Matplotlib settings - must be after we install Pyside
+MPL_DIR=$HOME/.config/matplotlib
+mkdir -p $MPL_DIR
+touch $MPL_DIR/matplotlibrc
+echo 'backend : Agg' > $MPL_DIR/matplotlibrc
+echo 'backend.qt4 : '$SCI_QT_API >> $MPL_DIR/matplotlibrc
+
+# imread does NOT support py3.2
+if [[  $TRAVIS_PYTHON_VERSION != 3.2 ]]; then
+    sudo apt-get install -q libtiff4-dev libwebp-dev libpng12-dev xcftools
+    pip install imread
+fi
+
+# TODO: update when SimpleITK become available on py34 or hopefully pip
+if [[  $TRAVIS_PYTHON_VERSION != 3.4 ]]; then
+    easy_install SimpleITK
+fi
+
+sudo apt-get install libfreeimage3
+pip install astropy
+
+if [[ $TRAVIS_PYTHON_VERSION == 2.* ]]; then
+    pip install pyamg
+fi
--- tools/travis_setup.sh
+++ tools/travis_setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/sh
-travis_retry pip install wheel flake8 coveralls nose
+pip install wheel flake8 coveralls nose
 
 # on Python 2.7, use the system versions of numpy, scipy, and matplotlib
 # and the minimum version of cython and networkx
 if [[  $TRAVIS_PYTHON_VERSION == 2.7* ]]; then
-    travis_retry sudo apt-get install python-scipy python-matplotlib
+    sudo apt-get install python-scipy python-matplotlib
     pip install https://github.com/cython/cython/archive/0.19.2.tar.gz
     pip install https://github.com/networkx/networkx/archive/networkx-1.8.tar.gz
 fi
 
-travis_retry pip install  -r requirements.txt $WHEELHOUSE
+pip install  -r requirements.txt $WHEELHOUSE
 python check_bento_build.py
