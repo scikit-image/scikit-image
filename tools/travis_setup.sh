e146c3eea5ea9449a1601a7d36eba20e9c60b86e
Steven Silvester <steven.silvester@ieee.org>
2014-10-10 06:11:26 -0500

    Clean up formatting and a comment

--- tools/travis_setup.sh
+++ tools/travis_setup.sh
@@ -6,11 +6,11 @@ pip uninstall -y numpy
 
 # on Python 2.7, use the system versions of numpy, scipy, and matplotlib
 # and the minimum version of cython and networkx
-if [[  $TRAVIS_PYTHON_VERSION == 2.7* ]]; then
+if [[ $TRAVIS_PYTHON_VERSION == 2.7* ]]; then
     sudo apt-get install python-scipy python-matplotlib
     pip install https://github.com/cython/cython/archive/0.19.2.tar.gz
     pip install https://github.com/networkx/networkx/archive/networkx-1.8.tar.gz
 fi
 
-pip install  -r requirements.txt $WHEELHOUSE
+pip install -r requirements.txt $WHEELHOUSE
 python check_bento_build.py
