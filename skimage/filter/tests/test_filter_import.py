1bfb10cb88bd026a99283b5bda267266fe32a835
Stefan van der Walt <stefan@sun.ac.za>
2014-11-06 13:22:32 +0200

    Ensure import of old filter namespace raises warning

--- skimage/filter/tests/test_filter_import.py
+++
@@ -1,8 +0,0 @@
-from warnings import catch_warnings, simplefilter
-
-def test_import_filter():
-    with catch_warnings():
-        simplefilter('ignore')
-        from skimage import filter as F
-
-    assert('sobel' in dir(F))
