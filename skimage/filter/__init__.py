2e90f4b15d9f7f8789cf1bc30cdb7d14af65dbed
Stefan van der Walt <stefan@sun.ac.za>
2014-11-10 14:51:28 +0200

    For `filter` submodule, explicitly set warning registry location

--- skimage/filter/__init__.py
+++ skimage/filter/__init__.py
@@ -1,9 +1,12 @@
 from skimage._shared.utils import skimage_deprecation
-from warnings import warn
-warn(skimage_deprecation('The `skimage.filter` module has been renamed to '
-                         '`skimage.filters`.  This placeholder module '
-                         'will be removed in v0.13.'))
-del warn
+from warnings import warn_explicit
+__warningregistry__ = {}
+warn_explicit(skimage_deprecation('The `skimage.filter` module has been renamed to '
+                                  '`skimage.filters`.  This placeholder module '
+                                  'will be removed in v0.13.'), category=None,
+                                  filename=__file__, lineno=4,
+                                  registry=__warningregistry__)
+del warn_explicit
 del skimage_deprecation
 
 from ..filters import *
