046fe99e4e2de0503c44555287eedaedc56ef280
Stefan van der Walt <stefan@sun.ac.za>
2014-11-10 16:56:46 +0200

    Make sure warning is raised upon import

--- skimage/filters/tests/test_filter_import.py
+++ skimage/filters/tests/test_filter_import.py
@@ -3,9 +3,9 @@ from warnings import catch_warnings, simplefilter
 
 def test_filter_import():
     with catch_warnings():
-        simplefilter('ignore')
+        simplefilter('always')
         from skimage import filter as F
 
     assert('sobel' in dir(F))
     assert any(['has been renamed' in w
-                for (w, _, _) in F.__warningregistry__]), F.__warningregistry__
+                for (w, _, _) in F.__warningregistry__])
