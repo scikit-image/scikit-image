e03638a9d2e41d68861a29cd48771cd6afe60691
Stefan van der Walt <stefan@sun.ac.za>
2014-11-10 13:41:26 +0200

    Filter won't be imported twice--check for its deprecation correctly

--- skimage/filters/tests/test_filter_import.py
+++ skimage/filters/tests/test_filter_import.py
@@ -1,11 +1,11 @@
-from skimage._shared.utils import all_warnings, skimage_deprecation
 from warnings import catch_warnings, simplefilter
-from numpy.testing import assert_warns
 
-def import_filter():
-    from skimage import filter as F
-    assert('sobel' in dir(F))
 
 def test_filter_import():
-    with all_warnings():
-        assert_warns(skimage_deprecation, import_filter)
+    with catch_warnings():
+        simplefilter('ignore')
+        from skimage import filter as F
+
+    assert('sobel' in dir(F))
+    assert any(['has been renamed' in w
+                for (w, _, _) in F.__warningregistry__])
