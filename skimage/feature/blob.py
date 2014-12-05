833edc17ef749054ed629936b2143aef8a8465c8
Eric <elubeck@caltech.edu>
2014-12-04 16:22:58 -0800

    Fixed typo

--- skimage/feature/blob.py
+++ skimage/feature/blob.py
@@ -200,7 +200,7 @@ def blob_dog(image, min_sigma=1, max_sigma=50, sigma_ratio=1.6, threshold=2.0,
                                   footprint=np.ones((3, 3, 3)),
                                   threshold_rel=0.0,
                                   exclude_border=False)
-    # Convert array to float64
+    # Convert local_maxima to float64
     lm = local_maxima.astype('float64')
     # Convert the last index to its corresponding scale value
     lm[:,2] = sigma_list[local_maxima[:,2]]
@@ -302,7 +302,7 @@ def blob_log(image, min_sigma=1, max_sigma=50, num_sigma=10, threshold=.2,
                                   threshold_rel=0.0,
                                   exclude_border=False)
 
-    # Convert array to float64
+    # Convert local_maxima to float64
     lm = local_maxima.astype('float64')
     # Convert the last index to its corresponding scale value
     lm[:,2] = sigma_list[local_maxima[:,2]]
@@ -413,7 +413,7 @@ def blob_doh(image, min_sigma=1, max_sigma=30, num_sigma=10, threshold=0.01,
                                   threshold_rel=0.0,
                                   exclude_border=False)
 
-    # Convert array to float64
+    # Convert local_maxima to float64
     lm = local_maxima.astype('float64')
     # Convert the last index to its corresponding scale value
     lm[:,2] = sigma_list[local_maxima[:,2]]
