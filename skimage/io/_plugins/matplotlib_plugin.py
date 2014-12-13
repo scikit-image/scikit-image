bc78bf85442b0ffb7962a1c9c4a3560a0fd1960d
Steven Silvester <steven.silvester@ieee.org>
2014-12-13 08:20:51 -0600

    Create a new figure for imshow if there is already data

--- skimage/io/_plugins/matplotlib_plugin.py
+++ skimage/io/_plugins/matplotlib_plugin.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 
 
 def imshow(*args, **kwargs):
+    if plt.gca().has_data():
+        plt.figure()
     kwargs.setdefault('interpolation', 'nearest')
     kwargs.setdefault('cmap', 'gray')
     plt.imshow(*args, **kwargs)
