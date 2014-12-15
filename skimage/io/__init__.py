7f8cd65675180c253bdd8fb2327f3f929de9fb23
Stefan van der Walt <stefan@sun.ac.za>
2014-12-15 02:21:34 +0200

    Fix docstring markup errors

--- skimage/io/__init__.py
+++ skimage/io/__init__.py
@@ -26,7 +26,7 @@ def _format_plugin_info_table(info_table, column_lengths):
     info_table.insert(0, _separator('=', column_lengths))
     info_table.insert(1, ('Plugin', 'Description'))
     info_table.insert(2, _separator('-', column_lengths))
-    info_table.append(_separator('-', column_lengths))
+    info_table.append(_separator('=', column_lengths))
 
 
 def _update_doc(doc):
