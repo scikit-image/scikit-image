3829401e9f1d9503f874d13eb77ee4ec9e73ed70
Steven Silvester <steven.silvester@ieee.org>
2014-12-23 21:20:23 -0600

    Fix label wrapper function

--- skimage/measure/_label.py
+++ skimage/measure/_label.py
@@ -1,6 +1,7 @@
 from ._ccomp import label as _label
 
-def label(input, neighbors=8, background=None, return_num=False):
-    return _label(input, neighbors, background, return_num)
+def label(input, neighbors=8, background=None, return_num=False,
+        connectivity=None):
+    return _label(input, neighbors, background, return_num, connectivity)
 
 label.__doc__ = _label.__doc__
