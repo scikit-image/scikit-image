2e0deb9595cd80b33ef53833773fb6d9380793b0
Steven Silvester <steven.silvester@ieee.org>
2014-12-24 07:47:05 -0600

    Fix syntax error and missing | operator

--- skimage/morphology/tests/test_grey.py
+++ skimage/morphology/tests/test_grey.py
@@ -175,7 +175,7 @@ def test_3d_fallback_white_tophat():
     with expected_warnings(['operator.*deprecated|\A\Z']):
         new_image = grey.white_tophat(image)
     footprint = ndimage.generate_binary_structure(3,1)
-    with expected_warnings(['operator.*deprecated\A\Z']):
+    with expected_warnings(['operator.*deprecated|\A\Z']):
         image_expected = ndimage.white_tophat(image,footprint=footprint)
     testing.assert_array_equal(new_image, image_expected)
 
