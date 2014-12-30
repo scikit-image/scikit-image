14a2b5d8f3f021219ef3ea09c90eeda78f539476
Phil Schaf <flying-sheep@web.de>
2014-12-30 14:20:21 +0100

    fixed Qt5 compat

--- skimage/viewer/qt.py
+++ skimage/viewer/qt.py
@@ -1,11 +1,10 @@
-has_qt = True
-
 try:
-    from matplotlib.backends.qt_compat import QtGui, QtCore, QtWidgets
+    from matplotlib.backends.qt_compat import QtGui, QtCore, QtWidgets, QT_RC_MAJOR_VERSION as has_qt
 except ImportError:
     try:
         from matplotlib.backends.qt4_compat import QtGui, QtCore
         QtWidgets = QtGui
+        has_qt = 4
     except ImportError:
         # Mock objects
         class QtGui_cls(object):
@@ -27,6 +26,17 @@ except ImportError:
         QtCore = QtCore_cls()
 
         has_qt = False
+
+if has_qt == 5:
+    from matplotlib.backends.backend_qt5 import FigureManagerQT
+    from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+elif has_qt == 4:
+    from matplotlib.backends.backend_qt4 import FigureManagerQT
+    from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
+else:
+    assert not has_qt, 'Unsupported Qt version {0}'.format(has_qt)
+    FigureManagerQT = object
+    FigureCanvasQTAgg = object
 
 Qt = QtCore.Qt
 Signal = QtCore.Signal
