43bd8242ff6df1502d4d2e246023796e859c8d40
Blake Griffith <blake.a.griffith@gmail.com>
2015-04-24 17:37:41 -0500

    load process chunks into namespace

--- skimage/util/__init__.py
+++ skimage/util/__init__.py
@@ -2,6 +2,7 @@ from .dtype import (img_as_float, img_as_int, img_as_uint, img_as_ubyte,
                     img_as_bool, dtype_limits)
 from .shape import view_as_blocks, view_as_windows
 from .noise import random_noise
+from .process import process_chunks
 
 from .arraypad import pad, crop
 from ._regular_grid import regular_grid
@@ -20,4 +21,5 @@ __all__ = ['img_as_float',
            'crop',
            'random_noise',
            'regular_grid',
+           'process_chunks',
            'unique_rows']
