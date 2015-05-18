0e573f71af8de5a265f3a8ef06df6f8fd29b15e8
Blake Griffith <blake.a.griffith@gmail.com>
2015-05-18 14:27:55 -0500

    Use absolute import

--- skimage/util/tests/test_apply_parallel.py
+++ skimage/util/tests/test_apply_parallel.py
@@ -1,8 +1,10 @@
+from __future__ import absolute_import
+
 import numpy as np
 from numpy.testing import assert_array_almost_equal
 
 from skimage.filters import threshold_adaptive, gaussian_filter
-from skimage.util import apply_parallel
+from skimage.util.apply_parallel import apply_parallel
 
 
 def test_apply_parallel():
