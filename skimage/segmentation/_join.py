6c108b14c11299e8efb0dc29aa0d5eb1f4cb6822
Ajay Bhat <a.ajay.bhat@gmail.com>
2015-06-01 09:35:25 +0530

    Simplify

--- skimage/segmentation/_join.py
+++ skimage/segmentation/_join.py
@@ -122,17 +122,14 @@ def relabel_sequential(label_field, offset=1):
         label_field = label_field.astype(new_type)
         m = m.astype(new_type)  # Ensures m is an integer
     labels = np.unique(label_field)
-    has_zero = (labels == 0).any()
-    labels = labels.take(labels.nonzero())[0]
+    has_zero, labels = (labels == 0).any(), labels.take(labels.nonzero())[0]
     relabeled = np.digitize(label_field.ravel(), labels).reshape(label_field.shape)
     relabeled = np.array([np.add(labeled, offset - 1) for labeled in relabeled if
                           (labeled.ndim == 0 and labeled != 0) or labeled.ndim != 0])
     relabeled = np.concatenate((relabeled, [0])) if has_zero and relabeled.ndim == labels.ndim else relabeled
     forward_map = np.zeros(m + 1, int)
     forward_map[label_field] = relabeled
-    if not has_zero:
-        labels = np.concatenate(([0], labels))
-    inverse_map = np.zeros(offset - 1 + len(labels), dtype=np.intp)
-    inverse_map[(offset - 1):] = labels
+    labels = np.concatenate(([0], labels)) if not has_zero else labels
+    inverse_map = np.concatenate((np.zeros(offset - 1, dtype=np.intp), labels))
     inverse_map = np.concatenate(([0], inverse_map)) if has_zero else inverse_map
     return relabeled, forward_map, inverse_map
