40fd7916e8470b1608e496352dc97ac9ce03fe37
Geoffrey French <brittix1023@googlemail.com>
2015-06-14 18:31:55 +0100

    Added SEEDS example to plot_segmentation.

--- doc/examples/plot_segmentations.py
+++ doc/examples/plot_segmentations.py
@@ -57,6 +57,20 @@ of Quickshift, while ``n_segments`` chooses the number of centers for kmeans.
 .. [3] Radhakrishna Achanta, Appu Shaji, Kevin Smith, Aurelien Lucchi,
     Pascal Fua, and Sabine Suesstrunk, SLIC Superpixels Compared to
     State-of-the-art Superpixel Methods, TPAMI, May 2012.
+
+
+SEEDS super-pixel segmentation
+------------------------------
+This algorithm initialises the segmentation by dividing the image into
+evenly space square blocks, each of which is assigned a label. It then
+swaps smaller blocks (recursively reducing the block size as the image is
+refined) between neighbouring labels in order to refine segmentation.
+See [4] for details.
+
+.. [4] M. van den Bergh, X. Boix, G. Roig, B. de Capitani, L. van Gool..
+       "SEEDS: Superpixels extracted via energy-driven sampling."
+       Proceedings of the European Conference on Computer Vision, pages 13-26,
+       2012.
 """
 from __future__ import print_function
 
@@ -64,30 +78,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from skimage.data import astronaut
-from skimage.segmentation import felzenszwalb, slic, quickshift
+from skimage.segmentation import felzenszwalb, slic, seeds, quickshift
 from skimage.segmentation import mark_boundaries
 from skimage.util import img_as_float
 
 img = img_as_float(astronaut()[::2, ::2])
 segments_fz = felzenszwalb(img, scale=100, sigma=0.5, min_size=50)
 segments_slic = slic(img, n_segments=250, compactness=10, sigma=1)
+segments_seeds = seeds(img, num_superpixels=250)
 segments_quick = quickshift(img, kernel_size=3, max_dist=6, ratio=0.5)
 
 print("Felzenszwalb's number of segments: %d" % len(np.unique(segments_fz)))
 print("Slic number of segments: %d" % len(np.unique(segments_slic)))
+print("SEEDS number of segments: %d" % len(np.unique(segments_seeds)))
 print("Quickshift number of segments: %d" % len(np.unique(segments_quick)))
 
-fig, ax = plt.subplots(1, 3)
-fig.set_size_inches(8, 3, forward=True)
+fig, ax = plt.subplots(2, 2)
+fig.set_size_inches(6, 7, forward=True)
 fig.subplots_adjust(0.05, 0.05, 0.95, 0.95, 0.05, 0.05)
 
-ax[0].imshow(mark_boundaries(img, segments_fz))
-ax[0].set_title("Felzenszwalbs's method")
-ax[1].imshow(mark_boundaries(img, segments_slic))
-ax[1].set_title("SLIC")
-ax[2].imshow(mark_boundaries(img, segments_quick))
-ax[2].set_title("Quickshift")
-for a in ax:
+ax[0,0].imshow(mark_boundaries(img, segments_fz))
+ax[0,0].set_title("Felzenszwalbs's method")
+ax[0,1].imshow(mark_boundaries(img, segments_slic))
+ax[0,1].set_title("SLIC")
+ax[1,0].imshow(mark_boundaries(img, segments_quick))
+ax[1,0].set_title("Quickshift")
+ax[1,1].imshow(mark_boundaries(img, segments_seeds))
+ax[1,1].set_title("SEEDS")
+for a in ax.flatten():
     a.set_xticks(())
     a.set_yticks(())
 plt.show()
