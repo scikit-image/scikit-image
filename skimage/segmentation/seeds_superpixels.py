89249470560188a452e3c9faa85f89da8cc3b07b
Geoffrey French <frondit1985@gmail.com>
2015-06-21 08:58:12 +0100

    Python 3 fix; xrange -> range.

--- skimage/segmentation/seeds_superpixels.py
+++ skimage/segmentation/seeds_superpixels.py
@@ -174,7 +174,7 @@ def seeds(input_2d, hist_size=None, num_superpixels=200, n_levels=5,
     hist_2ds_by_level.append(hist_2d)
     metrics_by_level.append(seed_metrics)
     l_metrics_prev = seed_metrics
-    for i in xrange(1, n_levels):
+    for i in range(1, n_levels):
         hist_2d = _seeds.downscale_histogram(hist_2d)
         hist_2ds_by_level.append(hist_2d)
         l_metrics = _seeds.LevelMetrics()
@@ -191,8 +191,8 @@ def seeds(input_2d, hist_size=None, num_superpixels=200, n_levels=5,
     
     label_hists = np.array(hist_2d).reshape((n_labels, hist_size))
     label_areas = []
-    for y in xrange(l_metrics.n_blocks_y):
-        for x in xrange(l_metrics.n_blocks_x):
+    for y in range(l_metrics.n_blocks_y):
+        for x in range(l_metrics.n_blocks_x):
             label_areas.append(l_metrics.block_area(y, x))
     label_areas = np.array(label_areas, dtype=np.int32)
     
@@ -202,7 +202,7 @@ def seeds(input_2d, hist_size=None, num_superpixels=200, n_levels=5,
 
     # For each level above the pixel level
     labels_prev_level = labels_by_level[-1]
-    for level in xrange(n_levels-2, -1, -1):
+    for level in range(n_levels-2, -1, -1):
         labels = _seeds.upscale_labels_by_2(labels_prev_level,
                                             metrics_by_level[level].n_blocks_y,
                                             metrics_by_level[level].n_blocks_x)
@@ -226,7 +226,7 @@ def seeds(input_2d, hist_size=None, num_superpixels=200, n_levels=5,
 
 
     if return_steps:
-        for lvl in xrange(len(labels_by_level)):
+        for lvl in range(len(labels_by_level)):
             met = metrics_by_level[lvl]
             labels_by_level[lvl] = np.array(_seeds.upscale_labels(labels_by_level[lvl], met))
         return np.array(pixel_labels), labels_by_level, index_2d, quantized_colours
