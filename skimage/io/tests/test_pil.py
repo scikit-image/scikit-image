80d4e813f970864e473ead71258892c5a748345f
Jonathan Helmus <jjhelmus@gmail.com>
2015-07-11 14:48:12 -0500

    TST: Use smaller gif file in test_pil tests

--- skimage/io/tests/test_pil.py
+++ skimage/io/tests/test_pil.py
@@ -191,11 +191,11 @@ def test_all_mono():
 
 
 def test_multi_page_gif():
-    img = imread(os.path.join(data_dir, 'no_time_for_that.gif'))
-    assert img.shape == (24, 280, 500, 3), img.shape
-    img2 = imread(os.path.join(data_dir, 'no_time_for_that.gif'),
+    img = imread(os.path.join(data_dir, 'no_time_for_that_tiny.gif'))
+    assert img.shape == (24, 25, 14, 3), img.shape
+    img2 = imread(os.path.join(data_dir, 'no_time_for_that_tiny.gif'),
                   img_num=5)
-    assert img2.shape == (280, 500, 3)
+    assert img2.shape == (25, 14, 3)
     assert_allclose(img[5], img2)
 
 
