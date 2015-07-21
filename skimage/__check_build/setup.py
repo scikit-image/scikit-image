93366d1b572aecc942391502d749419c847552d2
Jonathan Helmus <jjhelmus@gmail.com>
2015-07-21 13:08:47 -0500

    MAINT: Fixed __check_build setup.py file

--- skimage/__check_build/setup.py
+++ skimage/__check_build/setup.py
@@ -1,8 +1,11 @@
-# Author: Virgile Fritsch <virgile.fritsch@inria.fr>
-# License: BSD 3 clause
+#!/usr/bin/env python
 
-import numpy
+import os
+
 from skimage._build import cython
+
+base_path = os.path.abspath(os.path.dirname(__file__))
+
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
@@ -16,4 +19,11 @@ def configuration(parent_package='', top_path=None):
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
-    setup(**configuration(top_path='').todict())
+    setup(maintainer='scikit-image Developers',
+          author='scikit-image Developers',
+          maintainer_email='scikit-image@googlegroups.com',
+          description='Build Check',
+          url='https://github.com/scikit-image/scikit-image',
+          license='SciPy License (BSD Style)',
+          **(configuration(top_path='').todict())
+          )
