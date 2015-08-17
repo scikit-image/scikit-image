8e839ae240b8766703b65a9d170f52da0a1f516d
dan <warmspringwinds@gmail.com>
2015-08-17 10:45:47 +0200

    gallery example was added and more information about xml specified

--- skimage/data/detect.py
+++ skimage/data/detect.py
@@ -4,7 +4,13 @@ from .. import data_dir
 
 def frontal_face_cascade_xml():
     """
-    Returns the file's path to the trained xml file.
+    Returns the file's path to the trained xml file for frontal face detection
+    that was taken from OpenCV repository [1]_.
+
+    References
+    ----------
+    .. [1] OpenCV lbpcascade trained files
+           https://github.com/Itseez/opencv/tree/master/data/lbpcascades
     """
 
     return _os.path.join(data_dir, 'lbpcascade_frontalface_opencv.xml')
