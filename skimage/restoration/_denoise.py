76285f59862dd20c83164d9aea734035be7e4bf9
odebeir <odebeir@ulb.ac.be>
2015-08-28 21:33:58 +0200

    add exemple to  denoise_bilateral() in docstring

--- skimage/restoration/_denoise.py
+++ skimage/restoration/_denoise.py
@@ -54,6 +54,18 @@ def denoise_bilateral(image, win_size=5, sigma_range=None, sigma_spatial=1,
     ----------
     .. [1] http://users.soe.ucsc.edu/~manduchi/Papers/ICCV98.pdf
 
+    Examples
+    --------
+    >>> from skimage import data, img_as_float
+    >>> from skimage.restoration import denoise_bilateral
+    >>> import numpy as np
+    >>>
+    >>> astro = img_as_float(data.astronaut())
+    >>> astro = astro[220:300, 220:320]
+    >>>
+    >>> noisy = astro + 0.6 * astro.std() * np.random.random(astro.shape)
+    >>> noisy = np.clip(noisy, 0, 1)
+    >>> denoised = denoise_bilateral(noisy, sigma_range=0.05, sigma_spatial=15)
     """
     mode = _mode_deprecations(mode)
     return _denoise_bilateral(image, win_size, sigma_range, sigma_spatial,
