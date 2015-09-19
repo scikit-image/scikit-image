e1c57be1c409486f4c6c62e43afef9e1f08c389a
Steven Silvester <steven.silvester@ieee.org>
2015-09-18 21:28:49 -0500

    Encrypt token for target repo

--- tools/deploy_docs.sh
+++ tools/deploy_docs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PYTHON_VERSION == 3.4 ]]
+if [[ $TRAVIS_PULL_REQUEST == false && $TRAVIS_BRANCH == "master" && 
+      $TRAVIS_PYTHON_VERSION == 3.4 ]]
 then
     # See https://help.github.com/articles/creating-an-access-token-for-command-line-use/ for how to generate a token
     # See http://docs.travis-ci.com/user/encryption-keys/ for how to generate 
@@ -17,7 +18,7 @@ then
 
     git add dev
     git commit -m "Deployed to GitHub Pages"
-    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages
+    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
     )
 else
     echo "-- will only push docs from master --"
