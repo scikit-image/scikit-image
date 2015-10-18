7212fe3e82a19a3dea7c29a77726cf0c9f76512f
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2015-10-18 11:41:54 +1100

    Replace semicolon with full stop

--- skimage/segmentation/slic_superpixels.py
+++ skimage/segmentation/slic_superpixels.py
@@ -29,7 +29,7 @@ def slic(image, n_segments=100, compactness=10., max_iter=10, sigma=0,
         more weight to space proximity, making superpixel shapes more
         square/cubic. In SLICO mode, this is the initial compactness.
         This parameter depends strongly on image contrast and on the
-        shapes of objects in the image; we recommend exploring possible
+        shapes of objects in the image. We recommend exploring possible
         values on a log scale, e.g., 0.01, 0.1, 1, 10, 100, before
         refining around a chosen value.
     max_iter : int, optional
