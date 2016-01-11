84e3af9ec2568315b5ff7b7f87a35c16239e340f
Vighnesh Birodkar <vighneshbirodkar@nyu.edu>
2016-01-10 20:24:19 -0500

    improved drawing in draw_rag example

--- doc/examples/segmentation/plot_rag_draw.py
+++ doc/examples/segmentation/plot_rag_draw.py
@@ -14,18 +14,19 @@ from matplotlib import pyplot as plt, colors
 
 img = data.coffee()
 labels = segmentation.slic(img, compactness=30, n_segments=400)
+img_marked = segmentation.mark_boundaries(img, labels, color=(0, 0, 0))
 g = graph.rag_mean_color(img, labels)
 
-out = graph.draw_rag(labels, g, img)
+out = graph.draw_rag(labels, g, img_marked)
 plt.figure()
 plt.title("RAG with all edges shown in green.")
 plt.imshow(out)
 
 # The color palette used was taken from
 # http://www.colorcombos.com/color-schemes/2/ColorCombo2.html
-cmap = colors.ListedColormap(['#6599FF', '#ff9900'])
-out = graph.draw_rag(labels, g, img, node_color="#ffde00", colormap=cmap,
-                     thresh=30, desaturate=True)
+cmap = colors.LinearSegmentedColormap.from_list('new', ['#6599FF', '#ff9900'])
+out = graph.draw_rag(labels, g, img_marked, node_color="#ffde00",
+                     colormap=cmap, thresh=30, desaturate=True)
 plt.figure()
 plt.title("RAG with edge weights less than 30, color "
           "mapped between blue and orange.")
@@ -33,7 +34,7 @@ plt.imshow(out)
 
 plt.figure()
 plt.title("All edges drawn with viridis colormap")
-out = graph.draw_rag(labels, g, img, colormap=viridis,
+out = graph.draw_rag(labels, g, img_marked, colormap=viridis,
                      desaturate=True)
 
 plt.imshow(out)
