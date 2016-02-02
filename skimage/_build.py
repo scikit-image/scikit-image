3cd4d03cb2de99da4a2a4592f3541074335ad6e9
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2016-02-02 16:35:20 +0000

    ENH: handle tempita templated Cython source files, *.pyx.in

--- skimage/_build.py
+++ skimage/_build.py
@@ -30,13 +30,18 @@ def cython(pyx_files, working_path=''):
         # If cython is not found, we do nothing -- the build will make use of
         # the distributed .c files
         print("Cython not found; falling back to pre-built %s" \
-              % " ".join([f.replace('.pyx', '.c') for f in pyx_files]))
+              % " ".join([f.replace('.pyx.in', 'c').replace('.pyx', '.c')
+                          for f in pyx_files]))
     else:
         for pyxfile in [os.path.join(working_path, f) for f in pyx_files]:
 
             # if the .pyx file stayed the same, we don't need to recompile
             if not _changed(pyxfile):
                 continue
+
+            if pyxfile.endswith('.pyx.in'):
+                process_tempita_pyx(pyxfile)
+                pyxfile = pyxfile.replace('.pyx.in', '.pyx')
 
             cythonize(pyxfile)
 
@@ -69,3 +74,21 @@ def _changed(filename):
             cf.write(md5_new.encode('utf-8'))
 
     return md5_cached != md5_new.encode('utf-8')
+
+
+def process_tempita_pyx(fromfile):
+    try:
+        try:
+            from Cython import Tempita as tempita
+        except ImportError:
+            import tempita
+    except ImportError:
+        raise Exception('Building requires Tempita: '
+                        'pip install --user Tempita')
+    from_filename = tempita.Template.from_filename
+    template = from_filename(fromfile) #, encoding=sys.getdefaultencoding())
+    pyxcontent = template.substitute()
+    assert fromfile.endswith('.pyx.in')
+    pyxfile = fromfile[:-len('.pyx.in')] + '.pyx'
+    with open(pyxfile, "w") as f:
+        f.write(pyxcontent)
