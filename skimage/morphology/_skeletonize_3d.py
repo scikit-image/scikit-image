81d47328ea1af345416e54aa6f9ba254c40f6e82
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2016-02-03 21:14:18 +0000

    DOC: spell out the names of [Lee94], add the Notes section

--- skimage/morphology/_skeletonize_3d.py
+++ skimage/morphology/_skeletonize_3d.py
@@ -26,11 +26,24 @@ def skeletonize_3d(img):
     --------
     skeletonize, medial_axis
 
+    Notes
+    -----
+    The method of [Lee94]_ uses an octree data structure to examine a 3x3x3
+    neighborhood of a pixel. The algorithm proceeds by iteratively sweeping
+    over the image, and removing pixels at each iteration until the image
+    stops changing. Each iteration consists of two steps: first, a list of
+    candidates for removal is assembled; then pixels from this list are
+    rechecked sequentially, to better preserve connectivity of the image.
+
+    The algorithm this function implements is different from the algorithms
+    used by either `skeletonize` or `medial_axis`, thus for 2D images the
+    results produced by this function are generally different.
+
     References
     ----------
-    .. [Lee94] Lee et al, Building skeleton models via 3-D medial surface/axis
-           thinning algorithms. Computer Vision, Graphics, and Image Processing,
-           56(6):462-478, 1994.
+    .. [Lee94] T.-C. Lee, R.L. Kashyap and C.-N. Chu, Building skeleton models
+           via 3-D medial surface/axis thinning algorithms.
+           Computer Vision, Graphics, and Image Processing, 56(6):462-478, 1994.
 
     """
     # make sure the image is 3D or 2D (if it is, temporarily upcast to 3D)
