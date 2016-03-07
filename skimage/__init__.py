d8c18a9f1547d8c921e21654bd119c40fd6ffaec
Stefan van der Walt <stefanv@berkeley.edu>
2016-03-06 18:25:28 -0800

    Update version to 0.13dev

--- skimage/__init__.py
+++ skimage/__init__.py
@@ -65,7 +65,7 @@ import sys
 pkg_dir = osp.abspath(osp.dirname(__file__))
 data_dir = osp.join(pkg_dir, 'data')
 
-__version__ = '0.12.0'
+__version__ = '0.13dev'
 
 try:
     imp.find_module('nose')
