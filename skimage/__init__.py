feda03618eee64a1aecb17c5cceb534e1d2538a1
Stefan van der Walt <stefanv@berkeley.edu>
2016-03-08 01:31:11 -0800

    Release 0.12.3

--- skimage/__init__.py
+++ skimage/__init__.py
@@ -65,7 +65,7 @@ import sys
 pkg_dir = osp.abspath(osp.dirname(__file__))
 data_dir = osp.join(pkg_dir, 'data')
 
-__version__ = '0.12.2'
+__version__ = '0.12.3'
 
 try:
     imp.find_module('nose')
