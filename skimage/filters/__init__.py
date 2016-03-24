2b1a88dfb5b4525e4e4d02ddcbaeee93747469a9
Egor Panfilov <multicolor.mood@gmail.com>
2016-03-24 21:40:19 +0300

    Removed deprecated filters.canny links

--- skimage/filters/__init__.py
+++ skimage/filters/__init__.py
@@ -14,29 +14,18 @@ from . import rank
 from .rank import median
 
 from .._shared.utils import deprecated, copy_func
-from .. import restoration
+
 
 gaussian_filter = copy_func(gaussian, name='gaussian_filter')
 gaussian_filter = deprecated('skimage.filters.gaussian')(gaussian_filter)
 gabor_filter = copy_func(gabor, name='gabor_filter')
 gabor_filter = deprecated('skimage.filters.gabor')(gabor_filter)
 
-# Backward compatibility v<0.11
-
-
-@deprecated('skimage.feature.canny')
-def canny(*args, **kwargs):
-    # Hack to avoid circular import
-    from ..feature._canny import canny as canny_
-    return canny_(*args, **kwargs)
-
-
 __all__ = ['inverse',
            'wiener',
            'LPIFilter2D',
            'gaussian',
            'median',
-           'canny',
            'sobel',
            'hsobel',
            'vsobel',
--- skimage/filters/tests/test_deprecated_imports.py
+++
@@ -1,9 +0,0 @@
-from ..._shared._warnings import expected_warnings
-from ...data import moon
-
-
-def test_canny_import():
-    data = moon()
-    with expected_warnings(['skimage.feature.canny']):
-        from skimage.filters import canny
-        canny(data)
