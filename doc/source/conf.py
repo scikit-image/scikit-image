35b35c5139dfd1663d932593f62948c7e00ee3c3
Shaun Singh <shaunsingh@airbears2-10-142-58-134.airbears2.1918.berkeley.edu>
2016-03-30 17:02:19 -0700

    Fix broken Sphinx

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,7 +28,7 @@ sys.path.append(os.path.join(curpath, '..', 'ext'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.pngmath',
+              'sphinx.ext.imgmath',
               'numpydoc',
               'sphinx.ext.autosummary',
               'plot2rst',
