1bda5f91791c4f8a1713bf375f9ccbd0e2929f79
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-04-14 18:47:16 +1000

    Fix incorrect call in doctest

--- skimage/draw/draw_nd.py
+++ skimage/draw/draw_nd.py
@@ -39,7 +39,7 @@ def line_nd(start, stop, endpoint=False, round=True):
            [0, 0, 1, 0, 0],
            [0, 0, 1, 0, 0],
            [0, 0, 0, 0, 0]])
-    >>> geom.line_nd([2, 1, 1], [5, 5, 2.5], endpoint=True)
+    >>> line_nd([2, 1, 1], [5, 5, 2.5], endpoint=True)
     [array([2, 3, 4, 4, 5]), array([1, 2, 3, 4, 5]), array([1, 1, 2, 2, 2])]
     """
     start = np.asarray(start)
