2b7eb5a0a18a581b4f7c5e1acfdf21afc3f80f13
Jungle <wwjd228@gmail.com>
2016-04-19 18:00:18 +0800

    Add a new parameter "aspectratio" for aspect ratio.
    Aspect ratio is the spatial aspect ratio, and specifies the ellipticity of the support of the Gabor function.

--- doc/examples/filters/plot_gaborfilter.py
+++
@@ -1,9 +0,0 @@
-from skimage.filters import gabor_kernel
-from skimage import io
-from matplotlib import pyplot as plt  # doctest: +SKIP
-
-gk = gabor_kernel(frequency=0.6, aspectratio=0.5)
-plt.figure()        # doctest: +SKIP
-io.imshow(gk.real)  # doctest: +SKIP
-io.show()           # doctest: +SKIP
-
