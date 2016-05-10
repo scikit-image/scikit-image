2ddf2ef01daf533c176d18a3663988d42d77af2f
emmanuelle <emmanuelle.gouillart@nsup.org>
2016-05-10 21:46:48 +0200

    Removed spurious import in conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,7 +14,7 @@
 import sys
 import os
 import skimage
-import sphinx_gallery
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
