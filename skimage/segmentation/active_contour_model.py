84efa08186f5327eac7ee29f497fa22ed2674308
Noah Stier <noah.stier@gmail.com>
2016-05-31 16:30:55 -0700

    skip doctests for interpreter example

--- skimage/segmentation/active_contour_model.py
+++ skimage/segmentation/active_contour_model.py
@@ -79,14 +79,14 @@ def active_contour(image, snake, alpha=0.01, beta=0.1,
     Examples
     --------
     >>> from skimage.draw import circle_perimeter
-    >>> from skimage.filters import gaussian_filter
+    >>> from skimage.filters import gaussian
 
     Create and smooth image:
 
     >>> img = np.zeros((100, 100))
     >>> rr, cc = circle_perimeter(35, 45, 25)
     >>> img[rr, cc] = 1
-    >>> img = gaussian_filter(img, 2)
+    >>> img = gaussian(img, 2)
 
     Initialize spline:
 
@@ -102,13 +102,14 @@ def active_contour(image, snake, alpha=0.01, beta=0.1,
 
     Watch the snake progress:
 
+    >>> import matplotlib.pyplot as plt
     >>> def cb(snake, it, dist):
-    >>>     pts = plt.scatter(snake[:, 0], snake[:, 1])
-    >>>     plt.title('it = %i, dist=%f' % (it, dist))
-    >>>     plt.pause(.05)
-    >>>     pts.remove()
-    >>> plt.imshow(img)
-    >>> snake = active_contour(img, init, callback=cb)
+    ...     pts = plt.scatter(snake[:, 0], snake[:, 1], figure=fig)
+    ...     plt.title('it = %i, dist=%f' % (it, dist))
+    ...     plt.pause(.05)
+    ...     pts.remove()
+    >>> plt.imshow(img, figure=fig)  #doctest: +SKIP
+    >>> snake = active_contour(img, init, callback=cb)  #doctest: +SKIP
 
     """
     scipy_version = list(map(int, scipy.__version__.split('.')))
