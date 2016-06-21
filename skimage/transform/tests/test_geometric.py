d666667a2e4f940c46918b394dc19e31260e62f8
Johannes Schönberger <jsch@demuc.de>
2016-06-21 22:36:37 +0200

    Increase unit test coverage for EuclideanTransform

--- skimage/transform/tests/test_geometric.py
+++ skimage/transform/tests/test_geometric.py
@@ -288,11 +288,14 @@ def test_invalid_input():
     assert_raises(ValueError, ProjectiveTransform, np.zeros((2, 3)))
     assert_raises(ValueError, AffineTransform, np.zeros((2, 3)))
     assert_raises(ValueError, SimilarityTransform, np.zeros((2, 3)))
+    assert_raises(ValueError, EuclideanTransform, np.zeros((2, 3)))
 
     assert_raises(ValueError, AffineTransform,
                   matrix=np.zeros((2, 3)), scale=1)
     assert_raises(ValueError, SimilarityTransform,
                   matrix=np.zeros((2, 3)), scale=1)
+    assert_raises(ValueError, EuclideanTransform,
+                  matrix=np.zeros((2, 3)), translation=(0, 0))
 
     assert_raises(ValueError, PolynomialTransform, np.zeros((3, 3)))
 
