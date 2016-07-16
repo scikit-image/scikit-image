830f6fe4775fe426fa651c8631e1be43d8110a86
Todd V. Rovito <todd.rovito@us.af.mil>
2016-07-16 11:22:34 -0500

    Added titles to the subplots for the transformed image added text at the bottom that includes the AffineTransform parameters.  Trying to make it clear for new comers issue #1967.

--- doc/examples/features_detection/plot_censure.py
+++ doc/examples/features_detection/plot_censure.py
@@ -31,6 +31,7 @@ ax[0].imshow(img_orig, cmap=plt.cm.gray)
 ax[0].axis('off')
 ax[0].scatter(detector.keypoints[:, 1], detector.keypoints[:, 0],
               2 ** detector.scales, facecolors='none', edgecolors='r')
+ax[0].set_title("Original Image")
 
 detector.detect(img_warp)
 
@@ -38,5 +39,10 @@ ax[1].imshow(img_warp, cmap=plt.cm.gray)
 ax[1].axis('off')
 ax[1].scatter(detector.keypoints[:, 1], detector.keypoints[:, 0],
               2 ** detector.scales, facecolors='none', edgecolors='r')
+ax[1].set_title("Transformed Image")
+
+ax[1].text(372, 450, 'AffineTransform scale = 1.5', ha='center', va='center', color='yellow')
+ax[1].text(325, 475, 'AffineTransform rotation 28.6 degrees', ha='center', va='center', color='yellow')
+ax[1].text(290, 500, 'AffineTransform translation X = 150  Y = -200', ha='center', va='center', color='yellow')
 
 plt.show()
