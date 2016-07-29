4f467271d7c72aa515bee2769aa54e54daefd560
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-07-29 14:47:31 +1000

    Add watershed to plot_segmentations

--- doc/examples/segmentation/plot_segmentations.py
+++ doc/examples/segmentation/plot_segmentations.py
@@ -59,6 +59,29 @@ kmeans.
 .. [3] Radhakrishna Achanta, Appu Shaji, Kevin Smith, Aurelien Lucchi,
     Pascal Fua, and Sabine Suesstrunk, SLIC Superpixels Compared to
     State-of-the-art Superpixel Methods, TPAMI, May 2012.
+
+
+Compact watershed segmentation of gradient images
+-------------------------------------------------
+
+Instead of taking a color image as input, watershed requires a grayscale
+*gradient* image, where bright pixels denote a boundary between regions.
+The algorithm views the image as a landscape, with bright pixels forming high
+peaks. This landscape is then flooded from the given *markers*, until separate
+flood basins meet at the peaks. Each distinct basin then forms a different
+image segment. [4]_
+
+As with SLIC, there is an additional *compactness* argument that makes it
+harder for markers to flood faraway pixels. This makes the watershed regions
+more regularly shaped. [5]_
+
+
+.. [4] http://en.wikipedia.org/wiki/Watershed_%28image_processing%29
+
+.. [5] Peer Neubert & Peter Protzel (2014). Compact Watershed and
+       Preemptive SLIC: On Improving Trade-offs of Superpixel Segmentation
+       Algorithms. ICPR 2014, pp 996-1001. DOI:10.1109/ICPR.2014.181
+       https://www.tu-chemnitz.de/etit/proaut/forschung/rsrc/cws_pSLIC_ICPR.pdf
 """
 from __future__ import print_function
 
@@ -66,7 +89,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from skimage.data import astronaut
-from skimage.segmentation import felzenszwalb, slic, quickshift
+from skimage.color import rgb2gray
+from skimage.filters import sobel
+from skimage.segmentation import felzenszwalb, slic, quickshift, watershed
 from skimage.segmentation import mark_boundaries
 from skimage.util import img_as_float
 
@@ -74,23 +99,27 @@ img = img_as_float(astronaut()[::2, ::2])
 segments_fz = felzenszwalb(img, scale=100, sigma=0.5, min_size=50)
 segments_slic = slic(img, n_segments=250, compactness=10, sigma=1)
 segments_quick = quickshift(img, kernel_size=3, max_dist=6, ratio=0.5)
+gradient = sobel(rgb2gray(img))
+segments_watershed = watershed(gradient, markers=250, compactness=0.001)
 
 print("Felzenszwalb's number of segments: %d" % len(np.unique(segments_fz)))
-print("Slic number of segments: %d" % len(np.unique(segments_slic)))
-print("Quickshift number of segments: %d" % len(np.unique(segments_quick)))
+print('SLIC number of segments: %d' % len(np.unique(segments_slic)))
+print('Quickshift number of segments: %d' % len(np.unique(segments_quick)))
 
-fig, ax = plt.subplots(1, 3, sharex=True, sharey=True,
+fig, ax = plt.subplots(2, 2, sharex=True, sharey=True,
                        subplot_kw={'adjustable': 'box-forced'})
 fig.set_size_inches(8, 3, forward=True)
 fig.tight_layout()
 
-ax[0].imshow(mark_boundaries(img, segments_fz))
-ax[0].set_title("Felzenszwalbs's method")
-ax[1].imshow(mark_boundaries(img, segments_slic))
-ax[1].set_title("SLIC")
-ax[2].imshow(mark_boundaries(img, segments_quick))
-ax[2].set_title("Quickshift")
-for a in ax:
+ax[0, 0].imshow(mark_boundaries(img, segments_fz))
+ax[0, 0].set_title("Felzenszwalbs's method")
+ax[0, 1].imshow(mark_boundaries(img, segments_slic))
+ax[0, 1].set_title('SLIC')
+ax[1, 0].imshow(mark_boundaries(img, segments_quick))
+ax[1, 0].set_title('Quickshift')
+ax[1, 1].imshow(mark_boundaries(img, segments_watershed))
+ax[1, 1].set_title('Compact watershed')
+for a in ax.ravel():
     a.set_xticks(())
     a.set_yticks(())
 plt.show()
