e32b5bc97c057b0bfa4a882974f1124eba8188d0
François Boulogne <devel@sciunto.org>
2016-08-01 10:42:47 +0200

    DOC: use horse to illustrate skeleton

--- doc/examples/edges/plot_skeleton.py
+++ doc/examples/edges/plot_skeleton.py
@@ -11,32 +11,12 @@ border pixels are identified and removed on the condition that they do not
 break the connectivity of the corresponding object.
 """
 from skimage.morphology import skeletonize
-from skimage import draw
+from skimage import data
 import numpy as np
 import matplotlib.pyplot as plt
 
-# an empty image
-image = np.zeros((400, 400))
-
-# foreground object 1
-image[10:-10, 10:100] = 1
-image[-100:-10, 10:-10] = 1
-image[10:-10, -100:-10] = 1
-
-# foreground object 2
-rs, cs = draw.line(250, 150, 10, 280)
-for i in range(10):
-    image[rs + i, cs] = 1
-rs, cs = draw.line(10, 150, 250, 280)
-for i in range(20):
-    image[rs + i, cs] = 1
-
-# foreground object 3
-ir, ic = np.indices(image.shape)
-circle1 = (ic - 135)**2 + (ir - 150)**2 < 30**2
-circle2 = (ic - 135)**2 + (ir - 150)**2 < 20**2
-image[circle1] = 1
-image[circle2] = 0
+# Invert the horse image
+image = ~data.horse()
 
 # perform skeletonization
 skeleton = skeletonize(image)
