efbfd7957fe31c9c4b98c332d25ee1095c9694c6
Scott Sievert <me@scottsievert.com>
2016-08-10 00:23:59 -0500

    DOC: shows wavelet denoising in YCbCr

--- doc/examples/filters/plot_denoise.py
+++ doc/examples/filters/plot_denoise.py
@@ -24,6 +24,16 @@ Bilateral filter
 A bilateral filter is an edge-preserving and noise reducing filter. It averages
 pixels based on their spatial closeness and radiometric similarity.
 
+Wavelet denoising filter
+------------------------
+
+A wavelet denoising filter relies on the wavelet representation of the image.
+The noise is represented by small values in the wavelet domain which are set to
+0.
+
+In color images, wavelet denoising is typically done in the YCbCr color space
+as denoising in separate color channels may lead to more apparent noise.
+
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,6 +41,7 @@ import matplotlib.pyplot as plt
 from skimage import data, img_as_float
 from skimage.restoration import (denoise_tv_chambolle, denoise_bilateral,
                                  denoise_wavelet, estimate_sigma)
+from skimage import data, img_as_float, color
 from skimage.util import random_noise
 
 
@@ -60,10 +71,9 @@ ax[0, 1].set_title('TV')
 ax[0, 2].imshow(denoise_bilateral(noisy, sigma_color=0.05, sigma_spatial=15))
 ax[0, 2].axis('off')
 ax[0, 2].set_title('Bilateral')
-ax[0, 3].imshow(denoise_wavelet(noisy, sigma=0.85*sigma_est,
-                                multichannel=True))
+ax[0, 3].imshow(denoise_wavelet(noisy))
 ax[0, 3].axis('off')
-ax[0, 3].set_title('Wavelet')
+ax[0, 3].set_title('Wavelet in RGB')
 
 ax[1, 1].imshow(denoise_tv_chambolle(noisy, weight=0.2, multichannel=True))
 ax[1, 1].axis('off')
@@ -71,10 +81,17 @@ ax[1, 1].set_title('(more) TV')
 ax[1, 2].imshow(denoise_bilateral(noisy, sigma_color=0.1, sigma_spatial=15))
 ax[1, 2].axis('off')
 ax[1, 2].set_title('(more) Bilateral')
-ax[1, 3].imshow(denoise_wavelet(noisy, sigma=1.25*sigma_est,
-                                multichannel=True))
+
+noisy_ycbcr = color.rgb2ycbcr(noisy)
+denoised_y_channel = denoise_wavelet((noisy_ycbcr[..., 0] - 16) / (235 - 16))
+denoised_ycbcr = noisy_ycbcr.copy()
+denoised_ycbcr[..., 0] = (denoised_y_channel * (235 - 16)) + 16
+denoised_in_ycbcr = color.ycbcr2rgb(denoised_ycbcr)
+denoised_in_ycbcr = np.clip(denoised_in_ycbcr, 0, 1)
+
+ax[1, 3].imshow(denoised_in_ycbcr)
 ax[1, 3].axis('off')
-ax[1, 3].set_title('(more) Wavelet')
+ax[1, 3].set_title('Wavelet denoising in YCbCr')
 ax[1, 0].imshow(astro)
 ax[1, 0].axis('off')
 ax[1, 0].set_title('original')
