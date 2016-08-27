8806be90b584e2661f7f9767ba3036043c1d78d2
Joshua Warner <silvertrumpet999@gmail.com>
2016-08-27 13:11:40 -0500

    Default to mode='reflect' to agree with scipy

--- skimage/filters/_gaussian.py
+++ skimage/filters/_gaussian.py
@@ -11,7 +11,7 @@ from .._shared.utils import warn
 __all__ = ['gaussian']
 
 
-def gaussian(image, sigma=1, output=None, mode='nearest', cval=0,
+def gaussian(image, sigma=1, output=None, mode='reflect', cval=0,
              multichannel=None):
     """Multi-dimensional Gaussian filter
 
@@ -30,7 +30,7 @@ def gaussian(image, sigma=1, output=None, mode='nearest', cval=0,
     mode : {'reflect', 'constant', 'nearest', 'mirror', 'wrap'}, optional
         The `mode` parameter determines how the array borders are
         handled, where `cval` is the value when mode is equal to
-        'constant'. Default is 'nearest'.
+        'constant'. Default is 'reflect'.
     cval : scalar, optional
         Value to fill past edges of input if `mode` is 'constant'. Default
         is 0.0
