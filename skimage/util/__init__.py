11718ae1551551d4758cd0856e115fa9cbb3f0b3
Joshua Warner <silvertrumpet999@gmail.com>
2016-08-27 12:09:31 -0500

    STY: Use copy_func instead of wrapper for np.pad.

--- skimage/util/__init__.py
+++ skimage/util/__init__.py
@@ -5,10 +5,14 @@ from .noise import random_noise
 from .apply_parallel import apply_parallel
 
 from .arraycrop import crop
-from .arraypad import pad
 from ._regular_grid import regular_grid, regular_seeds
 from .unique import unique_rows
 from ._invert import invert
+
+from .._shared.utils import copy_func
+
+from numpy import pad as numpy_pad
+pad = copy_func(numpy_pad, name='pad')
 
 
 __all__ = ['img_as_float',
