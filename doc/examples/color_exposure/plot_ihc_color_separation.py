9b11337673c0ac6c6c4b369d29c1b4f4b6f1db01
cespenel <cedric.espenel@gmail.com>
2016-09-01 07:24:16 -0700

    PEP8 correction

--- doc/examples/color_exposure/plot_ihc_color_separation.py
+++ doc/examples/color_exposure/plot_ihc_color_separation.py
@@ -26,11 +26,11 @@ from skimage.color import rgb2hed
 from matplotlib.colors import LinearSegmentedColormap
 
 # Create an artificial color close to the orginal one
-cmap_Hema = LinearSegmentedColormap.from_list('mycmap', ['white', 'navy'])
-cmap_DAB = LinearSegmentedColormap.from_list('mycmap', ['white',
-'saddlebrown'])
-cmap_Eosin = LinearSegmentedColormap.from_list('mycmap', ['darkviolet',
-'white'])
+cmap_hema = LinearSegmentedColormap.from_list('mycmap', ['white', 'navy'])
+cmap_dab = LinearSegmentedColormap.from_list('mycmap', ['white',
+                                             'saddlebrown'])
+cmap_eosin = LinearSegmentedColormap.from_list('mycmap', ['darkviolet',
+                                               'white'])
 
 ihc_rgb = data.immunohistochemistry()
 ihc_hed = rgb2hed(ihc_rgb)
@@ -42,13 +42,13 @@ ax = axes.ravel()
 ax[0].imshow(ihc_rgb)
 ax[0].set_title("Original image")
 
-ax[1].imshow(ihc_hed[:, :, 0], cmap=cmap_Hema)
+ax[1].imshow(ihc_hed[:, :, 0], cmap=cmap_hema)
 ax[1].set_title("Hematoxylin")
 
-ax[2].imshow(ihc_hed[:, :, 1], cmap=cmap_Eosin)
+ax[2].imshow(ihc_hed[:, :, 1], cmap=cmap_eosin)
 ax[2].set_title("Eosin")
 
-ax[3].imshow(ihc_hed[:, :, 2], cmap=cmap_DAB)
+ax[3].imshow(ihc_hed[:, :, 2], cmap=cmap_dab)
 ax[3].set_title("DAB")
 
 for a in ax.ravel():
