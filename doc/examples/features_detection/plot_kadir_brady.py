ec9f3b27fb1f748721966e0848881429396629c3
Parul Sethi <parul1sethi@gmail.com>
2016-09-23 23:06:58 +0530

    added doc example

---
+++ doc/examples/features_detection/plot_kadir_brady.py
@@ -0,0 +1,31 @@
+"""
+==============
+Saliency Detection
+==============
+
+Detect salient regions by assigning a saliency score to them which is
+measured by the entropy of a region's descriptor (to find locally complex regions)
+weighed by the difference over multiple scales to select features
+that are globally discriminative.
+"""
+
+from skimage import data
+from skimage.feature import saliency_kadir_brady
+from skimage.color import rgb2gray
+
+import matplotlib.pyplot as plt
+
+image = data.astronaut()[0:300, 0:300]
+image_gray = rgb2gray(image)
+
+regions = saliency_kadir_brady(image_gray, min_scale=10, max_scale=25,
+                               saliency_threshold=0.6, clustering_threshold=7)
+
+fig,ax = plt.subplots()
+ax.axis('off')
+for y,x,r in regions:
+    c = plt.Circle((x,y), r, color='red', linewidth=2, fill=False)
+    ax.add_patch(c)
+
+ax.imshow(image, interpolation='nearest')
+plt.show()
