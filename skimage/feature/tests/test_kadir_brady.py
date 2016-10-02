7f5fb49af5fe5ada095472c71fe58dd64bb85c69
Parul Sethi <parul1sethi@gmail.com>
2016-10-02 22:40:36 +0530

    fixed doc syntax error

--- skimage/feature/tests/test_kadir_brady.py
+++ skimage/feature/tests/test_kadir_brady.py
@@ -12,15 +12,15 @@ def test_kadir_brady():
 
     regions = saliency_kadir_brady(rgb2gray(image))
 
-    scale_range = [(5,7),(7,9),(9,11),(11,13)]
+    scale_range = [(5, 7),(7, 9),(9, 11),(11, 13)]
 
     # regions categorized under different scale ranges
     s_regions = [[r[0] for r in regions if int(r[2]) in range(i,j)] for i,j in scale_range]
 
-    assert_array_equal(len(s_regions[0]),0)
-    assert_array_equal(len(s_regions[1]),57)
-    assert_array_equal(len(s_regions[2]),32)
-    assert_array_equal(len(s_regions[3]),0)
+    assert_array_equal(len(s_regions[0]), 0)
+    assert_array_equal(len(s_regions[1]), 57)
+    assert_array_equal(len(s_regions[2]), 32)
+    assert_array_equal(len(s_regions[3]), 0)
 
     assert_raises(ValueError, saliency_kadir_brady, img3)
 
