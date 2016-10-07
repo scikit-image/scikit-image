49d8b3f8725f1d0486c29896c4fdc39d9c3ad55d
PengchengAi <657904384@qq.com>
2016-10-07 08:51:41 +0800

    Update freeimage_plugin.py

--- skimage/io/_plugins/freeimage_plugin.py
+++ skimage/io/_plugins/freeimage_plugin.py
@@ -94,12 +94,12 @@ def load_freeimage():
             raise RuntimeError('One or more FreeImage libraries were found, '
                                'but could not be loaded due to the following '
                                'errors:\n',
-                               '\n'.join(err_txt), sep='')
+                               '\n'.join(err_txt))
         else:
             # No errors, because no potential libraries found at all!
             raise RuntimeError('Could not find a FreeImage library in any of:'
                                '\n', 
-                               '\n'.join(lib_dirs), sep='')
+                               '\n'.join(lib_dirs))
 
     # FreeImage found
     freeimage.FreeImage_SetOutputMessage(c_error_handler)
