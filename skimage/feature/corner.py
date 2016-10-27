17acae82a591733fe8d04403e5e4561376683375
JohnnyTeutonic <jonathanreich100@gmail.com>
2016-10-27 22:00:49 +1100

    made the deprecation warning for the order more verbose

--- skimage/feature/corner.py
+++ skimage/feature/corner.py
@@ -162,7 +162,9 @@ def hessian_matrix(image, sigma=1, mode='constant', cval=0, order=None):
         if image.ndim == 2:
             # The legacy 2D code followed (x, y) convention, so we swap the axis
             # order to maintain compatibility with old code
-            warn('deprecation warning: the default value will be changed to ''xy'' in version 0.15')
+            warn('deprecation warning: the default order of the hessian matrix values '
+            'will be ''row-column'' instead of ''xy'' starting in skimage version 0.15'
+             'Use order=''rc'' or order=''xy'' to set this explicitly')
             order = 'rc'
         else:
             order = 'xy'
