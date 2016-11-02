e1815e66c4f9c1ce717d7e6c8ed95bc5c15d28c8
JohnnyTeutonic <jonathanreich100@gmail.com>
2016-11-02 13:26:10 +1100

    changed order to 'rc' in frangi hessian matrix

--- skimage/filters/_frangi.py
+++ skimage/filters/_frangi.py
@@ -44,7 +44,7 @@ def _frangi_hessian_common_filter(image, scale_range, scale_step,
     # Filtering for all sigmas
     for i, sigma in enumerate(sigmas):
         # Make 2D hessian
-        (Drr, Drc, Dcc) = hessian_matrix(image, sigma, order='C')
+        (Drr, Drc, Dcc) = hessian_matrix(image, sigma, order='rc')
 
         # Correct for scale
         Drr = (sigma ** 2) * Drr
@@ -52,7 +52,7 @@ def _frangi_hessian_common_filter(image, scale_range, scale_step,
         Dcc = (sigma ** 2) * Dcc
 
         # Calculate (abs sorted) eigenvalues and vectors
-        (lambda1, lambda2) = hessian_matrix_eigvals(Dyy, Dxy, Dxx)
+        (lambda1, lambda2) = hessian_matrix_eigvals(Drr, Drc, Dcc)
 
         # Compute some similarity measures
         lambda1[lambda1 == 0] = 1e-10
