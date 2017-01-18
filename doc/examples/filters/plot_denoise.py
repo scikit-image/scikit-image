79f2a5d005021843d16134a6e71e1d178f83509c
François Boulogne <devel@sciunto.org>
2017-01-18 21:49:57 +0100

    Various enhancements in example

--- doc/examples/filters/plot_denoise.py
+++ doc/examples/filters/plot_denoise.py
@@ -3,13 +3,13 @@
 Denoising a picture
 ====================
 
-In this example, we denoise a noisy version of the picture of the astronaut
-Eileen Collins using the total variation and bilateral denoising filter.
+In this example, we denoise a noisy version of a picture using the total
+variation, bilateral, and wavelet denoising filters.
 
-These algorithms typically produce "posterized" images with flat domains
-separated by sharp edges. It is possible to change the degree of posterization
-by controlling the tradeoff between denoising and faithfulness to the original
-image.
+Total variation and bilateral algorithms typically produce "posterized" images
+with flat domains separated by sharp edges. It is possible to change the degree
+of posterization by controlling the tradeoff between denoising and faithfulness
+to the original image.
 
 Total variation filter
 ----------------------
@@ -41,18 +41,16 @@ noise.
 import numpy as np
 import matplotlib.pyplot as plt
 
-from skimage import data, img_as_float
 from skimage.restoration import (denoise_tv_chambolle, denoise_bilateral,
                                  denoise_wavelet, estimate_sigma)
 from skimage import data, img_as_float, color
 from skimage.util import random_noise
 
 
-astro = img_as_float(data.astronaut())
-astro = astro[220:300, 220:320]
+original = img_as_float(data.chelsea()[100:250, 50:300])
 
 sigma = 0.155
-noisy = random_noise(astro, var=sigma**2)
+noisy = random_noise(original, var=sigma**2)
 
 fig, ax = plt.subplots(nrows=2, ncols=4, figsize=(8, 5), sharex=True,
                        sharey=True, subplot_kw={'adjustable': 'box-forced'})
@@ -67,14 +65,14 @@ print("Estimated Gaussian noise standard deviation = {}".format(sigma_est))
 
 ax[0, 0].imshow(noisy)
 ax[0, 0].axis('off')
-ax[0, 0].set_title('noisy')
+ax[0, 0].set_title('Noisy')
 ax[0, 1].imshow(denoise_tv_chambolle(noisy, weight=0.1, multichannel=True))
 ax[0, 1].axis('off')
 ax[0, 1].set_title('TV')
 ax[0, 2].imshow(denoise_bilateral(noisy, sigma_color=0.05, sigma_spatial=15))
 ax[0, 2].axis('off')
 ax[0, 2].set_title('Bilateral')
-ax[0, 3].imshow(denoise_wavelet(noisy))
+ax[0, 3].imshow(denoise_wavelet(noisy, multichannel=True))
 ax[0, 3].axis('off')
 ax[0, 3].set_title('Wavelet denoising')
 
@@ -84,12 +82,12 @@ ax[1, 1].set_title('(more) TV')
 ax[1, 2].imshow(denoise_bilateral(noisy, sigma_color=0.1, sigma_spatial=15))
 ax[1, 2].axis('off')
 ax[1, 2].set_title('(more) Bilateral')
-ax[1, 3].imshow(denoise_wavelet(noisy, convert2ycbcr=True))
+ax[1, 3].imshow(denoise_wavelet(noisy, multichannel=True, convert2ycbcr=True))
 ax[1, 3].axis('off')
-ax[1, 3].set_title('Wavelet denoising in YCbCr colorspace')
-ax[1, 0].imshow(astro)
+ax[1, 3].set_title('Wavelet denoising\nin YCbCr colorspace')
+ax[1, 0].imshow(original)
 ax[1, 0].axis('off')
-ax[1, 0].set_title('original')
+ax[1, 0].set_title('Original')
 
 fig.tight_layout()
 
