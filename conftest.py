192831a9dc404ec73a749f6cd140466e31de916a
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-01-20 22:51:10 -0800

    MAINT ignore visual_test.py if visvis not installed

--- conftest.py
+++ conftest.py
@@ -1,2 +1,9 @@
+# List of files that pytest should ignore
+
 collect_ignore = ["setup.py",
                   "skimage/io/_plugins"]
+try:
+    import visvis
+except ImportError:
+    collect_ignore.append("skimage/measure/mc_meta/visual_test.py")
+
