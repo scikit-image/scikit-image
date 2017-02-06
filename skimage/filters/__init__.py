15719d1ba6e227b3e2a8e3f6fde444b4df811d50
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-02-06 14:53:55 +1100

    Add threshold_local to filters/__init__.py

--- skimage/filters/__init__.py
+++ skimage/filters/__init__.py
@@ -8,7 +8,8 @@ from .edges import (sobel, sobel_h, sobel_v,
 from ._rank_order import rank_order
 from ._gabor import gabor_kernel, gabor
 from ._frangi import frangi, hessian
-from .thresholding import (threshold_adaptive, threshold_otsu, threshold_yen,
+from .thresholding import (threshold_local,
+                           threshold_adaptive, threshold_otsu, threshold_yen,
                            threshold_isodata, threshold_li, threshold_minimum,
                            threshold_mean, threshold_triangle,
                            threshold_niblack, threshold_sauvola,
