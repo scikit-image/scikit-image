1082ec3fe64186267d434bb40dfb64847a96e58b
Steven Silvester <steven.silvester@ieee.org>
2017-02-10 07:16:30 -0600

    Try putting the files in the tifffile directory

--- skimage/external/setup.py
+++ skimage/external/setup.py
@@ -12,8 +12,7 @@ def configuration(parent_package='', top_path=None):
 
     config.add_extension('tifffile._tifffile',
                          sources=['tifffile/tifffile.c'],
-                         include_dirs=([get_numpy_include_dirs()] +
-                                       [os.path.dirname(__file__)]))
+                         include_dirs=[get_numpy_include_dirs()])
     return config
 
 if __name__ == '__main__':
