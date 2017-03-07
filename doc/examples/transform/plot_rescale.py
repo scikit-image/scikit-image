3f0c23b1608206d4ca862b34f3f1e995a982d89d
Sourav Singh <souravsingh@users.noreply.github.com>
2017-03-08 00:02:41 +0530

    Make fixes to the example

--- doc/examples/transform/plot_rescale.py
+++ doc/examples/transform/plot_rescale.py
@@ -2,11 +2,9 @@
 =======
 Rescale
 =======
-
-The Rescale operation performs interpolation to 
-upscale or downscale images by a certain decimal 
-factor.
-
+The rescale operation resizes an image by a given scaling factor.
+The scaling factor can either be a single floating point value,
+or multiple values---one along each axis.
 """
 
 
@@ -23,9 +21,12 @@ fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, figsize=(8, 3),
                                sharex=True, sharey=True,
                                subplot_kw={'adjustable':'box-forced'})
 
-ax0.imshow(image, cmap=plt.cm.gray, interpolation='none')
+ax0.imshow(image, cmap=plt.cm.gray)
 ax0.axis('off')
-ax1.imshow(rescale_image, cmap=plt.cm.gray, interpolation='none')
+ax0.set_title("Original Image")
+
+ax1.imshow(rescale_image, cmap=plt.cm.gray)
 ax1.axis('off')
+ax1.set_title("Rescaled Image")
 
 plt.show()
