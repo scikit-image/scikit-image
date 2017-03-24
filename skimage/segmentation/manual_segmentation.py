677deb0b0e5d6dcb05432ae1e227bdc672d81766
Kesavan PS <pskesavan@tifrh.res.in>
2017-03-24 17:54:28 +0530

    fixed PEP8 issues

--- skimage/segmentation/manual_segmentation.py
+++ skimage/segmentation/manual_segmentation.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+
 def manual(image):
     """Return a binary image based on the selections made with mouse clicks.
 
@@ -30,7 +31,7 @@ def manual(image):
 
     """
 
-    image = np.squeeze(image) 
+    image = np.squeeze(image)
 
     if image.ndim is not 2:
         raise TypeError('Only 2-D images supported.')
@@ -38,28 +39,30 @@ def manual(image):
     manual.list_of_verts = []
 
     _select_lasso(image)
-    
+
     mask = np.zeros_like(image)
-    
+
     yshape, xshape = image.shape
     y_grid, x_grid = np.mgrid[:yshape, :xshape]
     all_pixels = np.vstack((x_grid.ravel(), y_grid.ravel())).T
-    
+
     for verts in manual.list_of_verts:
         _path = matplotlib.path.Path(verts)
         _mask = _path.contains_points(all_pixels)
         _mask = _mask.reshape(image.shape)
         mask += _mask
-    
+
     return mask
 
+
 def _select_lasso(image):
-    """Uses the LassoSelector widget from matplotlib 
+    """Uses the LassoSelector widget from matplotlib
     to crop freehand."""
     fig, ax = plt.subplots()
     ax.imshow(image)
     lasso = matplotlib.widgets.LassoSelector(ax, _onselect)
     plt.show()
+
 
 def _onselect(verts):
     manual.list_of_verts.append(verts)
