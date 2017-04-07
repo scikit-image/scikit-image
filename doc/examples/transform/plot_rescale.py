b1ff99d0a655d94e67609e7cb94cce9aba59efcf
François Boulogne <devel@sciunto.org>
2017-04-07 21:16:18 +0200

    Cosmetic edits

--- doc/examples/transform/plot_rescale.py
+++ doc/examples/transform/plot_rescale.py
@@ -1,7 +1,7 @@
 """
-==============================
-Rescale,resize and Downscaling
-==============================
+=============================
+Rescale, resize and downscale
+=============================
 
 `Rescale` operation resizes an image by a given scaling factor.
 The scaling factor can either be a single floating point value,
@@ -10,9 +10,9 @@ or multiple values - one along each axis.
 `Resize` serves the same purpose, but allows to specify an output
 image shape instead of a scaling factor.
 
-`Downscale` operation serves the purpose of downsampling an 
-n-dimensional image by calculating local mean on the elements of 
-each block of the size factors given as a parameter to the function. 
+`Downscale` operation serves the purpose of downsampling an
+n-dimensional image by calculating local mean on the elements of
+each block of the size factors given as a parameter to the function.
 """
 
 import matplotlib.pyplot as plt
@@ -31,6 +31,9 @@ fig, axes = plt.subplots(nrows=2, ncols=2,
 
 ax = axes.ravel()
 
+ax[0].imshow(image, cmap='gray')
+ax[0].set_title("Original image")
+
 ax[1].imshow(image_rescaled, cmap='gray')
 ax[1].set_title("Rescaled image")
 
@@ -39,9 +42,6 @@ ax[2].set_title("Resized image")
 
 ax[3].imshow(image_downscaled, cmap='gray')
 ax[3].set_title("Image downscaled using local averaging")
-
-ax[0].imshow(image, cmap='gray')
-ax[0].set_title("Original image")
 
 plt.tight_layout()
 plt.show()
