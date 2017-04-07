8cd11e8079f26fa7012240524f9b7bf1858ec276
Egor Panfilov <multicolor.mood@gmail.com>
2017-04-07 21:37:06 +0300

    Enhanced the rescale example

--- doc/examples/transform/plot_rescale.py
+++ doc/examples/transform/plot_rescale.py
@@ -1,15 +1,15 @@
 """
-=======
-Rescale
-=======
-The rescale operation resizes an image by a given scaling factor.
+==================
+Rescale and resize
+==================
+
+`Rescale` operation resizes an image by a given scaling factor.
 The scaling factor can either be a single floating point value,
-or multiple values---one along each axis.
+or multiple values - one along each axis.
 
-The Example takes the 'camera' image as input and rescales it by
-a factor of 0.5.
+`Resize` serves the same purpose, but allows to specify an output
+image shape instead of a scaling factor.
 """
-
 
 import matplotlib.pyplot as plt
 
@@ -18,30 +18,26 @@ from skimage.transform import rescale, resize, downscale_local_mean
 
 image = data.camera()
 
-rescale_image = rescale(image, 0.5)
-resize_image = resize(image, (100, 100), mode='reflect')
-image_downscale = downscale_local_mean(image, (3, 4))
+image_rescaled = rescale(image, 0.5)
+image_resized = resize(image, (400, 400), mode='reflect')
+image_downscaled = downscale_local_mean(image, (2, 3))
 
-fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(8, 3),
-                         sharex=True, sharey=True,
-                         subplot_kw={'adjustable': 'box-forced'})
+fig, axes = plt.subplots(nrows=2, ncols=2,
+                         sharex=True, sharey=True)
 
 ax = axes.ravel()
 
+ax[1].imshow(image_rescaled, cmap='gray')
+ax[1].set_title("Rescaled image")
+
+ax[2].imshow(image_resized, cmap='gray')
+ax[2].set_title("Resized image")
+
+ax[3].imshow(image_downscaled, cmap='gray')
+ax[3].set_title("Image downscaled using local averaging")
+
 ax[0].imshow(image, cmap='gray')
-ax[0].axis('off')
-ax[0].set_title("Original Image")
+ax[0].set_title("Original image")
 
-ax[1].imshow(rescale_image, cmap='gray')
-ax[1].axis('off')
-ax[1].set_title("Rescaled Image")
-
-ax[2].imshow(resize_image, cmap='gray')
-ax[2].axis('off')
-ax[2].set_title("Resized Image")
-
-ax[3].imshow(image_downscale, cmap='gray')
-ax[3].axis('off')
-ax[3].set_title("Downscale Image using local averaging")
-
+plt.tight_layout()
 plt.show()
