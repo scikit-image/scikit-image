432b76683aa50b5d05466d14acb362ebeb69b2d3
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-04-12 17:08:59 +1000

    Move manual segmentation to the future package
    This will enable figuring out the API over one or two releases.

--- skimage/segmentation/__init__.py
+++ skimage/segmentation/__init__.py
@@ -7,7 +7,6 @@ from .boundaries import find_boundaries, mark_boundaries
 from ._clear_border import clear_border
 from ._join import join_segmentations, relabel_from_one, relabel_sequential
 from ..morphology import watershed
-from .manual_segmentation import manual
 
 
 __all__ = ['random_walker',
@@ -21,5 +20,4 @@ __all__ = ['random_walker',
            'join_segmentations',
            'relabel_from_one',
            'relabel_sequential',
-           'watershed',
-           'manual']
+           'watershed']
