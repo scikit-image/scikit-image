6cdacb16e6f32797f63ef9f28727b60ce1f81364
Kesavan PS <pskesavan@tifrh.res.in>
2017-04-12 18:09:13 +0530

    fixed incorrect function names

--- skimage/future/__init__.py
+++ skimage/future/__init__.py
@@ -6,6 +6,7 @@ production code that will depend on updated skimage versions.
 """
 
 from . import graph
-from .manual_segmentation import manual
+from .manual_segmentation import manual_polygon_segmentation, manual_lasso_segmentation
 
-__all__ = ['graph', 'manual']
+
+__all__ = ['graph', 'manual_lasso_segmentation', 'manual_polygon_segmentation']
