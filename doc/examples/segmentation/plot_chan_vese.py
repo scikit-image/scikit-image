80b9050ce5fbfcd463fa86d2c927ad993bc682fe
Egor Panfilov <multicolor.mood@gmail.com>
2017-04-13 13:19:41 +0300

    Fixed chan_vese arguments name in gallery example

--- doc/examples/segmentation/plot_chan_vese.py
+++ doc/examples/segmentation/plot_chan_vese.py
@@ -3,8 +3,8 @@
 Chan-Vese Segmentation
 ======================
 
-The Chan-Vese Algorithm is designed to segment objects without clearly
-defined boundaries. This algorithm is based on level sets that are
+The Chan-Vese segmentation algorithm is designed to segment objects without
+clearly defined boundaries. This algorithm is based on level sets that are
 evolved iteratively to minimize an energy, which is defined by
 weighted values corresponding to the sum of differences intensity
 from the average value outside the segmented region, the sum of
@@ -48,26 +48,28 @@ from skimage import data
 from skimage.segmentation import chan_vese
 
 image = data.camera().astype(np.float)
-# Feel free to play around with these parameters to see how they impact the
-# result
+# Feel free to play around with the parameters to see how they impact the result
 cv = chan_vese(image, mu=0.25, lambda1=1, lambda2=1, tol=1e-3, max_iter=200,
-               dt=0.5, starting_level_set="checkerboard", extended_output=True)
+               dt=0.5, init_level_set="checkerboard", extended_output=True)
 
-fig, ax = plt.subplots(2, 2, figsize=(8, 8))
-ax[0, 0].imshow(image, cmap="gray", interpolation="nearest")
-ax[0, 0].set_axis_off()
-ax[0, 0].set_title("Original Image", fontsize=12)
+fig, axes = plt.subplots(2, 2, figsize=(8, 8))
+ax = axes.flatten()
 
-ax[0, 1].imshow(cv[0], cmap="gray", interpolation="nearest")
-ax[0, 1].set_axis_off()
-title = "Chan-Vese segmentation - {0} - iterations".format(len(cv[2]))
-ax[0, 1].set_title(title, fontsize=12)
+ax[0].imshow(image, cmap="gray")
+ax[0].set_axis_off()
+ax[0].set_title("Original Image", fontsize=12)
 
-ax[1, 0].imshow(cv[1], cmap="gray", interpolation="nearest")
-ax[1, 0].set_axis_off()
-ax[1, 0].set_title("Final Level Set", fontsize=12)
+ax[1].imshow(cv[0], cmap="gray")
+ax[1].set_axis_off()
+title = "Chan-Vese segmentation - {} iterations".format(len(cv[2]))
+ax[1].set_title(title, fontsize=12)
 
-ax[1, 1].plot(cv[2])
-ax[1, 1].set_title("Evolution of energy for each iteration", fontsize=12)
+ax[2].imshow(cv[1], cmap="gray")
+ax[2].set_axis_off()
+ax[2].set_title("Final Level Set", fontsize=12)
+
+ax[3].plot(cv[2])
+ax[3].set_title("Evolution of energy over iterations", fontsize=12)
+
 fig.tight_layout()
 plt.show()
