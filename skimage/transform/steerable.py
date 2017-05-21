9d21c37d5570998a040e35da1b4e063426fd5529
Dzung Nguyen <dzungng89@gmail.com>
2017-05-20 20:37:09 -0500

    fix dtype error

--- skimage/transform/steerable.py
+++ skimage/transform/steerable.py
@@ -1,30 +1,27 @@
 from __future__ import division
 import numpy as np
-import scipy.misc as sc
 from .._shared.utils import assert_nD
 from ..util import img_as_float
 
 
-def build_steerable(image, height=5, sampling=True):
+def build_steerable(image, height=5):
     """
     Parameters
     ----------
     image : 2-D array
-        image to process
+        input image
     height : integer, optional
         Height of the steerable decomposition. This includes the counting of
-        low pass and high pass.
-    sampling : booelan, optional
-        If sampling is true, then use subsampling when generating the subbands.
-        If sampling is false, only filters are applied.
-
+        low pass and high pass subbands.
 
     Returns
     -------
-    coeff : is a list containing subbands, which has the following structure
-            first element is high pass subband
-            last element is low pass subband
-            the elements in between are a list  containing subbands from different orientations
+    coeff : subbands of Steerable decomposition,
+            stored as a list of 'height' sublists,
+            Sublists corresponds to decreasing radius level in Steerable pyramid
+            The first sublist contains the high pass subband.
+            The last sublist contains the low pass subband
+            Intermediate sublists is list of subbands coming from different orientations.
 
     References
     ----------
@@ -39,10 +36,12 @@ def recon_steerable(coeff):
     """
     Parameters
     ----------
-    coeff : is a list containing subbands, which has the following structure
-            first element is high pass subband
-            last element is low pass subband
-            the elements in between are a list  containing subbands from different orientations
+    coeff : subbands of Steerable decomposition,
+            stored as a list containing 'height' sublists,
+            Sublists corresponds to decreasing radius level in Steerable pyramid
+            The first sublist contains the high pass subband.
+            The last sublist contains the low pass subband
+            Intermediate sublists contains subbands coming from different orientations.
 
     Returns
     -------
@@ -82,13 +81,13 @@ class Steerable:
         im = img_as_float(im)
 
         M, N = im.shape
-        log_rad, angle = self.base(M, N)
-        Xrcos, Yrcos = self.rcos_curve(1, -0.5)
+        log_rad, angle = base(M, N)
+        Xrcos, Yrcos = rcos_curve(1, -0.5)
         Yrcos = np.sqrt(Yrcos)
         YIrcos = np.sqrt(1 - Yrcos * Yrcos)
 
-        lo0mask = self.point_op(log_rad, YIrcos, Xrcos)
-        hi0mask = self.point_op(log_rad, Yrcos, Xrcos)
+        lo0mask = point_op(log_rad, YIrcos, Xrcos)
+        hi0mask = point_op(log_rad, Yrcos, Xrcos)
 
         imdft = np.fft.fftshift(np.fft.fft2(im))
         lo0dft = imdft * lo0mask
@@ -124,14 +123,14 @@ class Steerable:
             Xrcos = Xrcos - 1
 
             # ==================== Orientation bandpass =======================
-            himask = self.point_op(log_rad, Yrcos, Xrcos)
+            himask = point_op(log_rad, Yrcos, Xrcos)
 
             lutsize = 1024
             Xcosn = np.pi * \
                 np.array(range(-(2 * lutsize + 1), (lutsize + 2))) / lutsize
             order = self.nbands - 1
-            const = np.power(2, 2 * order) * np.square(sc.factorial(order)
-                                                       ) / (self.nbands * sc.factorial(2 * order))
+            const = np.power(2, 2 * order) * np.square(np.math.factorial(order)
+                                                       ) / (self.nbands * np.math.factorial(2 * order))
 
             alpha = (Xcosn + np.pi) % (2 * np.pi) - np.pi
             Ycosn = 2 * np.sqrt(const) * np.power(np.cos(Xcosn),
@@ -140,7 +139,7 @@ class Steerable:
             orients = []
 
             for b in range(self.nbands):
-                anglemask = self.point_op(
+                anglemask = point_op(
                     angle, Ycosn, Xcosn + np.pi * b / self.nbands)
                 banddft = np.power(np.complex(
                     0, -1), self.nbands - 1) * lodft * anglemask * himask
@@ -161,7 +160,7 @@ class Steerable:
             angle = angle[lostart[0]:loend[0], lostart[1]:loend[1]]
             lodft = lodft[lostart[0]:loend[0], lostart[1]:loend[1]]
             YIrcos = np.abs(np.sqrt(1 - Yrcos * Yrcos))
-            lomask = self.point_op(log_rad, YIrcos, Xrcos)
+            lomask = point_op(log_rad, YIrcos, Xrcos)
 
             lodft = lomask * lodft
 
@@ -193,20 +192,20 @@ class Steerable:
             Xrcos = Xrcos - 1
 
             # ========================== Orientation residue===================
-            himask = self.point_op(log_rad, Yrcos, Xrcos)
+            himask = point_op(log_rad, Yrcos, Xrcos)
 
             lutsize = 1024
             Xcosn = np.pi * \
                 np.array(range(-(2 * lutsize + 1), (lutsize + 2))) / lutsize
             order = self.nbands - 1
-            const = np.power(2, 2 * order) * np.square(sc.factorial(order)
-                                                       ) / (self.nbands * sc.factorial(2 * order))
+            const = np.power(2, 2 * order) * np.square(np.math.factorial(order)
+                                                       ) / (self.nbands * np.math.factorial(2 * order))
             Ycosn = np.sqrt(const) * np.power(np.cos(Xcosn), order)
 
             orientdft = np.zeros(coeff[0][0].shape)
 
             for b in range(self.nbands):
-                anglemask = self.point_op(
+                anglemask = point_op(
                     angle, Ycosn, Xcosn + np.pi * b / self.nbands)
                 banddft = np.fft.fftshift(np.fft.fft2(coeff[0][b]))
                 orientdft = orientdft + \
@@ -223,7 +222,7 @@ class Steerable:
             nlog_rad = log_rad[lostart[0]:loend[0], lostart[1]:loend[1]]
             nangle = angle[lostart[0]:loend[0], lostart[1]:loend[1]]
             YIrcos = np.sqrt(np.abs(1 - Yrcos * Yrcos))
-            lomask = self.point_op(nlog_rad, YIrcos, Xrcos)
+            lomask = point_op(nlog_rad, YIrcos, Xrcos)
 
             nresdft = self.recon_pyr_level(
                 coeff[1:], nlog_rad, Xrcos, Yrcos, nangle)
@@ -245,86 +244,83 @@ class Steerable:
         the elements in between are a list  containing subbands from different orientations
 
         """
-        if (self.nbands != len(coeff[1])):
-            raise ValueError(
-                "The number of orientations of subband and steerable pyramid does not match")
+        if (self.height != len(coeff)):
+            raise ValueError("Height of coeff should be %d" % self.height)
+
+        for i in range(1, self.height - 1):
+            if (self.nbands != len(coeff[1])):
+                raise ValueError("Size of intermediate sublist should be %d" % self.nbands)
 
         M, N = coeff[0].shape
-        log_rad, angle = self.base(M, N)
+        log_rad, angle = base(M, N)
 
-        Xrcos, Yrcos = self.rcos_curve(1, -0.5)
+        Xrcos, Yrcos = rcos_curve(1, -0.5)
         Yrcos = np.sqrt(Yrcos)
         YIrcos = np.sqrt(np.abs(1 - Yrcos * Yrcos))
 
-        lo0mask = self.point_op(log_rad, YIrcos, Xrcos)
-        hi0mask = self.point_op(log_rad, Yrcos, Xrcos)
+        lo0mask = point_op(log_rad, YIrcos, Xrcos)
+        hi0mask = point_op(log_rad, Yrcos, Xrcos)
 
         tempdft = self.recon_pyr_level(coeff[1:], log_rad, Xrcos, Yrcos, angle)
 
         hidft = np.fft.fftshift(np.fft.fft2(coeff[0]))
         outdft = tempdft * lo0mask + hidft * hi0mask
 
-        return np.fft.ifft2(np.fft.ifftshift(outdft)).real.astype(np.int32)
+        return np.fft.ifft2(np.fft.ifftshift(outdft)).real
 
-    def base(self, m, n):
-        """
-        Parameters
-        ----------
-        m, n : size of the desired
+def base(m, n):
+    """
+    Helper function that create a grid containing radius from center and angle
 
-        Returns
-        -------
-        log_rad : [mxn] array: log of the radius to the center of the matrix
-        angle : [mxn] array: the angle of the line start from the center
+    Parameters
+    ----------
+    m, n : size of the desired grid
 
-        Notes
-        -----
-        Helper function that create a grid containing radius from center and angle
-        """
+    Returns
+    -------
+    log_rad : [mxn] array: log of the radius with respect to the center of the matrix
+    angle : [mxn] array: the angle of the line originated from center of the matrix
+    """
 
-        ctrm = np.ceil((m + 0.5) / 2).astype(int)
-        ctrn = np.ceil((n + 0.5) / 2).astype(int)
-        xv, yv = np.meshgrid((np.array(range(n)) + 1 - ctrn) / (n / 2),
-                             (np.array(range(m)) + 1 - ctrm) / (m / 2))
+    ctrm = np.ceil((m + 0.5) / 2).astype(int)
+    ctrn = np.ceil((n + 0.5) / 2).astype(int)
+    xv, yv = np.meshgrid((np.array(range(n)) + 1 - ctrn) / (n / 2),
+                         (np.array(range(m)) + 1 - ctrm) / (m / 2))
 
-        rad = np.sqrt(xv**2 + yv**2)
-        rad[ctrm - 1, ctrn - 1] = rad[ctrm - 1, ctrn - 2]
-        log_rad = np.log2(rad)
+    rad = np.sqrt(xv**2 + yv**2)
+    rad[ctrm - 1, ctrn - 1] = rad[ctrm - 1, ctrn - 2]
+    log_rad = np.log2(rad)
+    angle = np.arctan2(yv, xv)
 
-        angle = np.arctan2(yv, xv)
+    return log_rad, angle
 
-        return log_rad, angle
+def rcos_curve(width, position):
+    """
+    Raised cosine 1D curve
 
-    def rcos_curve(self, width, position):
-        """
-        Parameters
-        ----------
-        width: width of the transition
-        position: where the cut is
+    Parameters
+    ----------
+    width: width of the transition
+    position: where the cut is
 
-        Returns
-        -------
-        X, Y: define a 1D curve, that look similar to a tanh
+    Returns
+    -------
+    X, Y: define a 1D raised cosine curve
 
-        Notes
-        -----
-        Calculate the curve of the filter
-        """
-        N = 256
-        X = np.pi * np.array(range(-N - 1, 2)) / 2 / N
+    """
+    N = 256
+    X = np.pi * np.array(range(-N - 1, 2)) / 2 / N
 
-        Y = np.cos(X)**2
-        Y[0] = Y[1]
-        Y[N + 2] = Y[N + 1]
+    Y = np.cos(X)**2
+    Y[0] = Y[1]
+    Y[N + 2] = Y[N + 1]
 
-        X = position + 2 * width / np.pi * (X + np.pi / 4)
-        return X, Y
+    X = position + 2 * width / np.pi * (X + np.pi / 4)
+    return X, Y
 
-    def point_op(self, im, Y, X):
-        """
-        Notes
-        -----
-        Given a 1D curve defined by X, Y, convert it to a 2D mask
-        """
-        out = np.interp(im.flatten(), X, Y)
-        return np.reshape(out, im.shape)
+def point_op(mask, Y, X):
+    """
+    Given a 1D curve defined by X, Y, convert it to a 2D mask
+    """
+    out = np.interp(mask.flatten(), X, Y)
+    return np.reshape(out, mask.shape)
--- skimage/transform/tests/test_steerable.py
+++ skimage/transform/tests/test_steerable.py
@@ -1,12 +1,33 @@
 from numpy.testing import assert_array_almost_equal, assert_raises, run_module_suite
 from skimage.transform import steerable
+import numpy as np
+from skimage import img_as_float
 
 def test_steerable_reconstruction():
-	im = np.random.randint(0, 255, (128, 128))
+	im = np.random.randint(0, 255, (128, 128), dtype=np.uint8)
 	coeff = steerable.build_steerable(im)
-	out = recon_steerable(coeff)
+	out = steerable.recon_steerable(coeff)
 
-	assert_array_almost_equal(im, out, decimal = 0)
+	assert_array_almost_equal(img_as_float(im), out, decimal = 2)
+
+def test_steerable_reconstruction_shape():
+	im = np.random.randint(0, 255, (200, 200), dtype=np.uint8)
+	coeff = steerable.build_steerable(im)
+	out = steerable.recon_steerable(coeff)
+
+	assert_array_almost_equal(img_as_float(im), out, decimal = 2)
+
+# def test_steerable_reconstruction():
+# 	im = np.random.randint(0, 255, (113, 29), dtype=np.uint8)
+# 	coeff = steerable.build_steerable(im)
+# 	out = steerable.recon_steerable(coeff)
+
+# 	print(coeff[0].shape)
+# 	print(coeff[1][0].shape)
+# 	print(coeff[2][0].shape)
+
+# 	assert_array_almost_equal(im, out, decimal = 0)
+
 
 if __name__ == "__main__":
 	run_module_suite()
