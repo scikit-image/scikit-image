4c9fcca1d929e7539201d712aa67e929d70ec45b
Dzung Nguyen <dzungng89@gmail.com>
2017-05-20 21:13:40 -0500

    fix typos in example

--- doc/examples/plot_steerable.py
+++ doc/examples/plot_steerable.py
@@ -24,11 +24,11 @@ coeff = steerable.build_steerable(image)
 
 height = len(coeff)
 fig, axes = plt.subplots(height, 4, figsize=(8, 7))
-axes[0, 0].imshow(image)
-axes[height - 1, 0].imshow(image)
+axes[0, 0].imshow(coeff[0])
+axes[height - 1, 0].imshow(coeff[-1])
 
 for i in range(1, height - 1):
     for j in range(4):
-        axes[i][j].imshow(image)
+        axes[i][j].imshow(coeff[i][j])
 
 plt.show()
