7029a4b7e7464ec3a869081fbd384afc3a5b5750
Pablo Márquez Neila <pmarquezneila@gmail.com>
2017-08-15 05:22:54 +0200

    PEP8 compliant

--- skimage/segmentation/morphsnakes.py
+++ skimage/segmentation/morphsnakes.py
@@ -3,38 +3,42 @@
 from itertools import cycle
 
 import numpy as np
-from scipy.ndimage import binary_dilation, binary_erosion, \
-                        gaussian_filter, gaussian_gradient_magnitude
+from scipy import ndimage as ndi
 
 __all__ = ['morph_acwe', 'morph_gac', 'SIoIS', 'ISoSI', 'gborders']
 
 
 class fcycle(object):
-    
+
     def __init__(self, iterable):
         """Call functions from the iterable each time it is called."""
         self.funcs = cycle(iterable)
-    
+
     def __call__(self, *args, **kwargs):
         f = next(self.funcs)
         return f(*args, **kwargs)
-    
+
 
 # SI and IS operators for 2D and 3D.
-_P2 = [np.eye(3), np.array([[0,1,0]]*3), np.flipud(np.eye(3)), np.rot90([[0,1,0]]*3)]
-_P3 = [np.zeros((3,3,3)) for i in range(9)]
+_P2 = [np.eye(3),
+       np.array([[0, 1, 0]] * 3),
+       np.flipud(np.eye(3)),
+       np.rot90([[0, 1, 0]] * 3)]
+_P3 = [np.zeros((3, 3, 3)) for i in range(9)]
 
-_P3[0][:,:,1] = 1
-_P3[1][:,1,:] = 1
-_P3[2][1,:,:] = 1
-_P3[3][:,[0,1,2],[0,1,2]] = 1
-_P3[4][:,[0,1,2],[2,1,0]] = 1
-_P3[5][[0,1,2],:,[0,1,2]] = 1
-_P3[6][[0,1,2],:,[2,1,0]] = 1
-_P3[7][[0,1,2],[0,1,2],:] = 1
-_P3[8][[0,1,2],[2,1,0],:] = 1
+_P3[0][:, :, 1] = 1
+_P3[1][:, 1, :] = 1
+_P3[2][1, :, :] = 1
+_P3[3][:, [0, 1, 2], [0, 1, 2]] = 1
+_P3[4][:, [0, 1, 2], [2, 1, 0]] = 1
+_P3[5][[0, 1, 2], :, [0, 1, 2]] = 1
+_P3[6][[0, 1, 2], :, [2, 1, 0]] = 1
+_P3[7][[0, 1, 2], [0, 1, 2], :] = 1
+_P3[8][[0, 1, 2], [2, 1, 0], :] = 1
 
 _opbuffer = np.zeros((0))
+
+
 def SI(u):
     """SI operator."""
     global _opbuffer
@@ -43,15 +47,17 @@ def SI(u):
     elif np.ndim(u) == 3:
         P = _P3
     else:
-        raise ValueError("u has an invalid number of dimensions (should be 2 or 3)")
-    
+        raise ValueError("u has an invalid number of dimensions "
+                         "(should be 2 or 3)")
+
     if u.shape != _opbuffer.shape[1:]:
         _opbuffer = np.zeros((len(P),) + u.shape)
-    
+
     for _opbuffer_i, P_i in zip(_opbuffer, P):
-        _opbuffer_i[:] = binary_erosion(u, P_i)
-    
+        _opbuffer_i[:] = ndi.binary_erosion(u, P_i)
+
     return _opbuffer.max(0)
+
 
 def IS(u):
     """IS operator."""
@@ -61,51 +67,63 @@ def IS(u):
     elif np.ndim(u) == 3:
         P = _P3
     else:
-        raise ValueError("u has an invalid number of dimensions (should be 2 or 3)")
-    
+        raise ValueError("u has an invalid number of dimensions "
+                         "(should be 2 or 3)")
+
     if u.shape != _opbuffer.shape[1:]:
         _opbuffer = np.zeros((len(P),) + u.shape)
-    
+
     for _opbuffer_i, P_i in zip(_opbuffer, P):
-        _opbuffer_i[:] = binary_dilation(u, P_i)
-    
+        _opbuffer_i[:] = ndi.binary_dilation(u, P_i)
+
     return _opbuffer.min(0)
 
+
 # SIoIS operator.
-SIoIS = lambda u: SI(IS(u))
-ISoSI = lambda u: IS(SI(u))
+def SIoIS(u):
+    return SI(IS(u))
+
+
+def ISoSI(u):
+    return IS(SI(u))
+
+
 curvop = fcycle([SIoIS, ISoSI])
 
+
 def _check_input(image, init_level_set):
-    
+
     if len(image.shape) not in [2, 3]:
         raise ValueError("Input image is not a 2D or 3D array.")
-    
+
     if len(image.shape) != len(init_level_set.shape):
         raise ValueError("The dimensions of the initial level set do not "
                          "match the dimensions of the image.")
 
+
 def _find_threshold(image, percentile=10):
-    
+
     return np.percentile(image, percentile)
+
 
 def gborders(img, alpha=1.0, sigma=1.0):
     """Stopping criterion for image borders."""
     # The norm of the gradient.
-    gradnorm = gaussian_gradient_magnitude(img, sigma, mode='constant')
+    gradnorm = ndi.gaussian_gradient_magnitude(img, sigma, mode='constant')
     return 1.0/np.sqrt(1.0 + alpha*gradnorm)
 
+
 def morph_acwe(image, init_level_set, iterations,
-              smoothing=1, lambda1=1, lambda2=1,
-              iter_callback=lambda x: None):
-    """Morphological active contours without edges (aka Morphological Chan-Vese).
-    
+               smoothing=1, lambda1=1, lambda2=1,
+               iter_callback=lambda x: None):
+    """Morphological active contours without edges (aka Morphological Chan-Vese)
+
     Active contours without edges implemented with morphological operators. It
-    can be used to segment objects in images without well defined borders. It is
-    required that the inside of the object looks different on average than the
-    outside (i.e., the inner area of the object should be darker or lighter than
-    the outer area on average).
-    
+    can be used to segment objects in images without well defined borders. It
+    is required that the inside of the object looks different on average than
+    the outside (i.e., the inner area of the object should be darker or lighter
+    than the outer area on average).
+
     Parameters
     ----------
     image : (M, N) or (L, M, N) array
@@ -130,25 +148,26 @@ def morph_acwe(image, init_level_set, iterations,
         If given, this function is called once per iteration with the current
         level set as the only argument. This is useful for debugging or for
         plotting intermediate results during the evolution.
-    
+
     Returns
     -------
     segmentation : (M, N) or (L, M, N) array
         Final segmentation (i.e., the final level set)
-    
+
     Notes
     -----
-    
+
     This is a version of the Chan-Vese algorithm that uses morphological
     operators instead of solving a partial differential equation (PDE) for the
     evolution of the contour. The set of morphological operators used in this
     algorithm are proved to be infinitesimally equivalent to the Chan-Vese PDE
     (see [1]_). However, morphological operators are do not suffer from the
     numerical stability issues typically found in PDEs (it is not necessary to
-    find the right time step for the evolution), and are computationally faster.
-    
+    find the right time step for the evolution), and are computationally
+    faster.
+
     The algorithm and its theoretical derivation are described in [1]_.
-    
+
     References
     ----------
     .. [1] A Morphological Approach to Curvature-based Evolution of Curves and
@@ -156,47 +175,49 @@ def morph_acwe(image, init_level_set, iterations,
            Transactions on Pattern Analysis and Machine Intelligence (PAMI),
            2014, DOI 10.1109/TPAMI.2013.106
     """
-    
+
     # TODO: Make it work with "color" (or multi-channel) images.
-    
+
     _check_input(image, init_level_set)
-    
+
     u = np.int8(init_level_set > 0)
-    
+
     iter_callback(u)
-    
+
     for _ in range(iterations):
-        
+
         # TODO: test speed
         # inside = u > 0
         # outside = u <= 0
         c0 = (image * (1 - u)).sum() / float((1 - u).sum())
         c1 = (image * u).sum() / float(u.sum())
-        
+
         # Image attachment
         du = np.gradient(u)
         abs_du = np.abs(du).sum(0)
         aux = abs_du * (lambda1*(image - c1)**2 - lambda2*(image - c0)**2)
-        
+
         u[aux < 0] = 1
         u[aux > 0] = 0
-        
+
         # Smoothing
         for _ in range(smoothing):
             u = curvop(u)
-        
+
         iter_callback(u)
-    
+
     return u
 
+
 def morph_gac(image, init_level_set, iterations,
-             smoothing=1, threshold='auto', balloon=0,
-             iter_callback=lambda x: None):
+              smoothing=1, threshold='auto', balloon=0,
+              iter_callback=lambda x: None):
     """Morphological geodesic active contours.
-    
-    Geodesic active contours implemented with morphological operators. It can be
-    used to segment objects with visible but noisy, cluttered, broken borders.
-    
+
+    Geodesic active contours implemented with morphological operators. It can
+    be used to segment objects with visible but noisy, cluttered, broken
+    borders.
+
     Parameters
     ----------
     image : (M, N) or (L, M, N) array
@@ -226,44 +247,44 @@ def morph_gac(image, init_level_set, iterations,
         If given, this function is called once per iteration with the current
         level set as the only argument. This is useful for debugging or for
         plotting intermediate results during the evolution.
-    
+
     Returns
     -------
     segmentation : (M, N) or (L, M, N) array
         Final segmentation (i.e., the final level set)
-    
+
     References
     ----------
     .. [1] A Morphological Approach to Curvature-based Evolution of Curves and
            Surfaces, Pablo Márquez-Neila, Luis Baumela, Luis Álvarez. In IEEE
            Transactions on Pattern Analysis and Machine Intelligence (PAMI),
-           2014, DOI 10.1109/TPAMI.2013.106    
+           2014, DOI 10.1109/TPAMI.2013.106
     """
-    
+
     _check_input(image, init_level_set)
-    
+
     if threshold == 'auto':
         threshold = _find_threshold(image)
-    
+
     structure = np.ones((3,) * len(image.shape), dtype=np.int8)
     dimage = np.gradient(image)
-    threshold_mask = image > threshold
+    # threshold_mask = image > threshold
     threshold_mask_balloon = image > threshold / np.abs(balloon)
-    
+
     u = np.int8(init_level_set > 0)
-    
+
     iter_callback(u)
-    
+
     for _ in range(iterations):
-        
+
         # Balloon
         if balloon > 0:
-            aux = binary_dilation(u, structure)
+            aux = ndi.binary_dilation(u, structure)
         elif balloon < 0:
-            aux = binary_erosion(u, structure)
+            aux = ndi.binary_erosion(u, structure)
         if balloon != 0:
             u[threshold_mask_balloon] = aux[threshold_mask_balloon]
-        
+
         # Image attachment
         aux = np.zeros_like(image)
         du = np.gradient(u)
@@ -271,11 +292,11 @@ def morph_gac(image, init_level_set, iterations,
             aux += el1 * el2
         u[aux > 0] = 1
         u[aux < 0] = 0
-        
+
         # Smoothing
         for _ in range(smoothing):
             u = curvop(u)
-        
+
         iter_callback(u)
-    
+
     return u
--- skimage/segmentation/tests/test_morphsnakes.py
+++ skimage/segmentation/tests/test_morphsnakes.py
@@ -4,9 +4,11 @@ from skimage.segmentation import morph_acwe, morph_gac
 # from numpy.testing import assert_array_equal
 import pytest
 
+
 def rgb2gray(img):
     """Convert a RGB image to gray scale."""
-    return 0.2989*img[:,:,0] + 0.587*img[:,:,1] + 0.114*img[:,:,2]
+    return 0.2989*img[:, :, 0] + 0.587*img[:, :, 1] + 0.114*img[:, :, 2]
+
 
 def circle_levelset(shape, center, sqradius, scalerow=1.0):
     """Build a binary function with a circle as the 0.5-levelset."""
@@ -15,6 +17,7 @@ def circle_levelset(shape, center, sqradius, scalerow=1.0):
     u = np.float_(phi > 0)
     return u
 
+
 def test_morphsnakes_incorrect_image_shape():
     img = np.zeros((10, 10, 3))
     ls = np.zeros((10, 9))
@@ -22,6 +25,7 @@ def test_morphsnakes_incorrect_image_shape():
         morph_acwe(img, init_level_set=ls, iterations=1)
     with pytest.raises(ValueError):
         morph_gac(img, init_level_set=ls, iterations=1)
+
 
 if __name__ == "__main__":
     np.testing.run_module_suite()
