69155b1ebd76597f34c82ad5f989cb3fec8809be
DavidBreuer <DavidBreuer@users.noreply.github.com>
2017-08-20 18:13:15 +0200

    Rename internal functions and remove unnecessary files

--- doc/examples/ridges/plot_ridge_filter.py
+++ doc/examples/ridges/plot_ridge_filter.py
@@ -3,11 +3,29 @@
 Ridge operators
 ===============
 
-Ridge filters can be used to detect continuous edges, such as vessels,
-neurites, wrinkles, rivers, and other tube-like structures. The present
-class of ridge filters relies on the eigenvalues of the Hessian matrix of
-image intensities to detect ridge structures where the intensity changes
-perpendicular but not along the structure.
+Ridge filters can be used to detect continuous edges, such as neurites 
+_[1], tubes _[2], vessels _[3], wrinkles _[4], rivers, and other rigdge-like 
+structures. The present class of ridge filters relies on the eigenvalues of 
+the Hessian matrix of image intensities to detect ridge structures where the 
+intensity changes perpendicular but not along the structure.
+
+.. [1] Meijering, E., Jacob, M., Sarria, J. C., Steiner, P., Hirling, H., 
+Unser, M. (2004). Design and validation of a tool for neurite tracing and 
+analysis in fluorescence microscopy images. Cytometry Part A, 58(2), 167-176.
+
+.. [2] Sato, Y., Nakajima, S., Shiraga, N., Atsumi, H., Yoshida, S., Koller, 
+T., ..., Kikinis, R. (1998). Three-dimensional multi-scale line filter for
+segmentation and visualization of curvilinear structures in medical images.
+Medical image analysis, 2(2), 143-168.
+
+.. [3] Frangi, A. F., Niessen, W. J., Vincken, K. L., & Viergever, M. A. (1998,
+October). Multiscale vessel enhancement filtering. In International Conference
+on Medical Image Computing and Computer-Assisted Intervention (pp. 130-137).
+Springer Berlin Heidelberg.
+
+.. [4] Ng, C. C., Yap, M. H., Costen, N., & Li, B. (2014, November). Automatic
+wrinkle detection using hybrid Hessian filter. In Asian Conference on Computer
+Vision (pp. 609-622). Springer International Publishing.
 """
 
 from skimage.data import page
