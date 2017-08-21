de9b392e94b25d81f774e1b9c8391edcf6e80c98
DavidBreuer <DavidBreuer@users.noreply.github.com>
2017-08-21 22:49:23 +0200

    Improved plot example

--- doc/examples/ridges/plot_ridge_filter.py
+++ doc/examples/ridges/plot_ridge_filter.py
@@ -3,17 +3,17 @@
 Ridge operators
 ===============
 
-Ridge filters can be used to detect continuous edges, such as neurites 
-_[1], tubes _[2], vessels _[3], wrinkles _[4], rivers, and other rigdge-like 
-structures. The present class of ridge filters relies on the eigenvalues of 
-the Hessian matrix of image intensities to detect ridge structures where the 
+Ridge filters can be used to detect continuous edges, such as neurites
+_[1], tubes _[2], vessels _[3], wrinkles _[4], rivers, and other rigdge-like
+structures. The present class of ridge filters relies on the eigenvalues of
+the Hessian matrix of image intensities to detect ridge structures where the
 intensity changes perpendicular but not along the structure.
 
-.. [1] Meijering, E., Jacob, M., Sarria, J. C., Steiner, P., Hirling, H., 
-Unser, M. (2004). Design and validation of a tool for neurite tracing and 
+.. [1] Meijering, E., Jacob, M., Sarria, J. C., Steiner, P., Hirling, H.,
+Unser, M. (2004). Design and validation of a tool for neurite tracing and
 analysis in fluorescence microscopy images. Cytometry Part A, 58(2), 167-176.
 
-.. [2] Sato, Y., Nakajima, S., Shiraga, N., Atsumi, H., Yoshida, S., Koller, 
+.. [2] Sato, Y., Nakajima, S., Shiraga, N., Atsumi, H., Yoshida, S., Koller,
 T., ..., Kikinis, R. (1998). Three-dimensional multi-scale line filter for
 segmentation and visualization of curvilinear structures in medical images.
 Medical image analysis, 2(2), 143-168.
@@ -51,12 +51,15 @@ for i, black_ridges in enumerate([1, 0]):
         plt.imshow(func(image, **kwargs), cmap=cmap, aspect='auto')
 
         if i == 0:
-            plt.title(['Original image', 'Meijering neuriteness\n',
-                       'Sato tubeness', 'Frangi vesselness\n',
-                       'Hessian vesselness'][j])
+            plt.title(['Original\nimage', 'Meijering\nneuriteness',
+                       'Sato\ntubeness', 'Frangi\nvesselness',
+                       'Hessian\nvesselness'][j])
 
         if j == 0:
             plt.ylabel('black_ridges = ' + str(bool(black_ridges)))
 
         plt.xticks([])
         plt.yticks([])
+
+plt.tight_layout()
+plt.show()
