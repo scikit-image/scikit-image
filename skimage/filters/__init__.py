a477c988fb10ed8cadbf17b080e9aa247240dc1c
DavidBreuer <DavidBreuer@users.noreply.github.com>
2017-08-21 12:44:44 +0200

    Add missing bracket in filters/__init__.py

--- skimage/filters/__init__.py
+++ skimage/filters/__init__.py
@@ -14,8 +14,8 @@ from .thresholding import (threshold_local, threshold_otsu, threshold_yen,
                            threshold_mean, threshold_triangle,
                            threshold_niblack, threshold_sauvola,
                            try_all_threshold, apply_hysteresis_threshold)
-from .ridges import (compute_hessian_eigenvalues, meijering, sato, frangi, 
-                     hessian
+from .ridges import (compute_hessian_eigenvalues, meijering, sato, frangi,
+                     hessian)
 from . import rank
 from .rank import median
 from ._unsharp_mask import unsharp_mask
