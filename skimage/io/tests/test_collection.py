8bd32d0e257aa2ccb9a3755d5575d8df1a8a7f6a
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2017-10-13 13:52:39 +0300

    Add test for matching imagecollection and framecollection on single-frame images

--- skimage/io/tests/test_collection.py
+++ skimage/io/tests/test_collection.py
@@ -2,7 +2,7 @@ import os
 
 import numpy as np
 from skimage import data_dir
-from skimage.io.collection import ImageCollection, alphanumeric_key
+from skimage.io.collection import ImageCollection, FrameCollection, alphanumeric_key
 from skimage.io import reset_plugins
 
 from skimage._shared import testing
@@ -56,6 +56,8 @@ class TestImageCollection(TestCase):
         self.images = ImageCollection(self.pattern)
         # Image collection with images having shapes that match.
         self.images_matched = ImageCollection(self.pattern_matched)
+        # Same images as a collection of frames
+        self.frames_matched = FrameCollection(self.pattern_matched)
 
     def test_len(self):
         assert len(self.images) == 2
@@ -141,3 +143,7 @@ class TestImageCollection(TestCase):
     def test_concatenate_mismatched_image_shapes(self):
         with testing.raises(ValueError):
             self.images.concatenate()
+
+    def test_framecollection_imagecollection(self):
+        assert_equal(self.images_matched[0], self.frames_matched[0])
+        assert_equal(self.images_matched[1], self.frames_matched[1])
