81ba02bc6c9832457496dc58440b934c65ce4727
Matt Agee <magee@uci.edu>
2017-10-26 14:08:18 -0700

    Update "Normalized Cuts" gallery example to use normalized_cut_gen

--- doc/examples/segmentation/plot_ncut.py
+++ doc/examples/segmentation/plot_ncut.py
@@ -20,12 +20,26 @@ from matplotlib import pyplot as plt
 
 img = data.coffee()
 
-labels1 = segmentation.slic(img, compactness=30, n_segments=400)
-out1 = color.label2rgb(labels1, img, kind='avg')
+super_pixels = segmentation.slic(img, compactness=30, n_segments=400)
+out1 = color.label2rgb(super_pixels, img, kind='avg')
 
-g = graph.rag_mean_color(img, labels1, mode='similarity')
-labels2 = graph.cut_normalized(labels1, g)
-out2 = color.label2rgb(labels2, img, kind='avg')
+g = graph.rag_mean_color(img, super_pixels, mode='similarity')
+
+# Two ways to apply Normalized Cuts:
+
+# Getting the value for a single threshold
+labels1 = graph.cut_normalized(super_pixels, g, thresh=1e-3)
+
+# Or using a generator to try several
+label_gen = graph.cut_normalized_gen(super_pixels, g, thresh=1e-4)
+labels2 = next(label_gen)
+labels2 = label_gen.send(1e-3)
+
+# Either method yields the same labels
+(labels1 == labels2).all() # Evaluates to True
+
+
+out2 = color.label2rgb(labels1, img, kind='avg')
 
 fig, ax = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(6, 8))
 
