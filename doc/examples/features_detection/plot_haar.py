97a6a1664a0c8a49c855ab6b545699b656463ec7
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-11-13 21:28:01 +0100

    EXA improve example code

--- doc/examples/features_detection/plot_haar.py
+++ doc/examples/features_detection/plot_haar.py
@@ -23,9 +23,12 @@ References
 import numpy as np
 import matplotlib.pyplot as plt
 
-from skimage.data import astronaut
-from skimage.color import rgb2gray
+from joblib import Parallel, delayed
 
+from sklearn.ensemble import RandomForestClassifier
+
+from skimage.data import cbcl_database
+from skimage.transform import integral_image
 from skimage.feature import haar_like_feature
 from skimage.feature import haar_like_feature_coord
 from skimage.feature import draw_haar_like_feature
@@ -45,16 +48,18 @@ feature_types = ['type-2-x', 'type-2-y',
                  'type-3-x', 'type-3-y',
                  'type-4']
 
-feature = [draw_haar_like_feature(img, 0, 0, img.shape[1], img.shape[0],
-                                  feat_t, max_n_features=1)
-           for img, feat_t in zip(images, feature_types)]
-
 fig, axs = plt.subplots(3, 2)
 for ax, img, feat_t in zip(np.ravel(axs), images, feature_types):
-    ax.imshow(draw_haar_like_feature(img, 0, 0, img.shape[1], img.shape[0],
-                                     feat_t, max_n_features=1,
-                                     random_state=0))
+    coord, _ = haar_like_feature_coord(img.shape[0], img.shape[1], feat_t)
+    haar_feature = draw_haar_like_feature(img, 0, 0,
+                                          img.shape[0],
+                                          img.shape[1],
+                                          coord,
+                                          max_n_features=1,
+                                          random_state=0)
+    ax.imshow(haar_feature)
     ax.set_title(feat_t)
+
 fig.suptitle('The different Haar-like feature descriptors')
 plt.axis('off')
 plt.tight_layout()
@@ -68,30 +73,72 @@ plt.tight_layout()
 # be provided. Typically, for a ROI of 24 by 24 pixels, the total amount of
 # features which can be extracted is 160,000.
 
-image = rgb2gray(astronaut())
-plt.figure()
-plt.imshow(draw_haar_like_feature(image, 125, 200, 50, 50,
-                                  'type-4', max_n_features=3,
-                                  random_state=0))
-plt.plot([200, 200 + 50, 200 + 50, 200, 200],
-         [125, 125, 125 + 50, 125 + 50, 125],
-         label='ROI')
-plt.axis([100, 300, 100, 300])
-plt.gca().invert_yaxis()
-plt.axis('off')
-plt.title('Example of few Haar-like feature extracted in ROI area')
-plt.legend()
 
-###############################################################################
-# The descriptor
-###############################################################################
-# As previously mentioned, the actual descriptor corresponds to the subtraction
-# between the rectangle from the integral of the original image. The
-# :func:`haar_like_feature` allows to extract those features for a specific ROI
-
-haar_features = haar_like_feature(image, 125, 200, 24, 24)
-print('{} Haar-like features have been extracted: {}'.format(
-    haar_features.shape[0], haar_features))
+def extract_feature_image(img, feature_type):
+    """Extract the haar feature for the current image"""
+    ii = integral_image(img)
+    return haar_like_feature(ii, 0, 0, ii.shape[0], ii.shape[1],
+                             feature_type=feature_type)
 
 
+images = cbcl_database()
+
+feature_types = ['type-2-x', 'type-2-y']
+
+X = np.array(Parallel(n_jobs=-1)(
+    delayed(extract_feature_image)(img, feature_types)
+    for img in images))
+y = np.array([1] * 100 + [0] * 100)
+
+feature_coord, feature_type = haar_like_feature_coord(images.shape[2],
+                                                      images.shape[1],
+                                                      feature_types)
+
+clf = RandomForestClassifier(n_estimators=1000, max_depth=None,
+                             max_features=100,
+                             n_jobs=-1, random_state=0)
+clf.fit(X, y)
+
+idx_sorted = np.argsort(clf.feature_importances_)[::-1]
+
+fig, axs = plt.subplots(3, 2)
+for idx, ax in enumerate(axs.ravel()):
+    image = images[0]
+    image = draw_haar_like_feature(image, 0, 0,
+                                   images.shape[2],
+                                   images.shape[1],
+                                   [feature_coord[idx_sorted[idx]]])
+    ax.imshow(image)
+
+fig.suptitle('The most important features')
+plt.tight_layout()
 plt.show()
+
+# image = rgb2gray(astronaut())
+# plt.figure()
+# coord, _ = haar_like_feature_coord(50, 50, 'type-4')
+# plt.imshow(draw_haar_like_feature(image, 125, 200, 50, 50,
+#                                   coord, max_n_features=3,
+#                                   random_state=0))
+# plt.plot([200, 200 + 50, 200 + 50, 200, 200],
+#          [125, 125, 125 + 50, 125 + 50, 125],
+#          label='ROI')
+# plt.axis([100, 300, 100, 300])
+# plt.gca().invert_yaxis()
+# plt.axis('off')
+# plt.title('Example of few Haar-like feature extracted in ROI area')
+# plt.legend()
+
+# ###############################################################################
+# # The descriptor
+# ###############################################################################
+# # As previously mentioned, the actual descriptor corresponds to the subtraction
+# # between the rectangle from the integral of the original image. The
+# # :func:`haar_like_feature` allows to extract those features for a specific ROI
+
+# haar_features = haar_like_feature(image, 125, 200, 24, 24)
+# print('{} Haar-like features have been extracted: {}'.format(
+#     haar_features.shape[0], haar_features))
+
+
+# plt.show()
