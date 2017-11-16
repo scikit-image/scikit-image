c892b92802fa3817e1306d2acec0e1f93fbbe33b
Kira Evans <contact@kne42.me>
2017-11-16 12:47:09 -0500

    Fix PEP8 E501.

--- skimage/draw/tests/test_random_shapes.py
+++ skimage/draw/tests/test_random_shapes.py
@@ -102,7 +102,10 @@ def test_can_generate_one_by_one_rectangle():
     assert len(labels) == 1
     _, bbox = labels[0]
     crop = image[bbox[0][0]:bbox[0][1], bbox[1][0]:bbox[1][1]]
-    assert np.shape(crop) == (1, 1, 3) and np.any(crop >= 1) and np.any(crop <= 255)  # rgb
+
+    # rgb
+    assert (np.shape(crop) == (1, 1, 3) and np.any(crop >= 1)
+            and np.any(crop <= 255))
 
 
 def test_throws_when_min_pixel_intensity_out_of_range():
