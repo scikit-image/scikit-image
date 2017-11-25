3dff8cbd4683913822538f8516eee50a00a7df0a
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-11-25 13:36:47 +0100

    EXA make specific extraction/selection/classification example

--- doc/examples/xx_applications/plot_haar_extraction_selection_classification.py
+++ doc/examples/xx_applications/plot_haar_extraction_selection_classification.py
@@ -1,13 +1,18 @@
 """
-============================
-Haar-like feature descriptor
-============================
+======================================================
+Face classification using Haar-like feature descriptor
+======================================================
 
-Haar-like feature descriptors are a set of simple features which was used in
-the first real-time face detector [1]_. These features can be efficiently
-computed on any scale in constant time, using an integral image [1]_ and by
-subtracting rectangle area. The following example will show the mechanism to
-build this family of descriptors.
+Haar-like feature descriptors were successfully used to implement the first
+real-time face detector [1]_. Inspired by this application, we propose an
+example illustrating the extraction, selection, and classification of Haar-like
+features to detect faces vs. non-faces.
+
+Notes
+-----
+
+This example relies on scikit-learn regarding the feature selection and
+classification.
 
 References
 ----------
@@ -19,6 +24,8 @@ References
        DOI: 10.1109/CVPR.2001.990517
 
 """
+from __future__ import print_function
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,38 +41,7 @@ from skimage.feature import haar_like_feature
 from skimage.feature import haar_like_feature_coord
 from skimage.feature import draw_haar_like_feature
 
-
-###############################################################################
-# The different Haar-like feature descriptors
-###############################################################################
-# The Haar-like feature descriptors come into 5 different types as illustrated
-# in the figure below. The value of the descriptor consists in the subtraction
-# between the green and red rectangles.
-
-images = [np.zeros((2, 2)), np.zeros((2, 2)),
-          np.zeros((3, 3)), np.zeros((3, 3)),
-          np.zeros((2, 2))]
-
-feature_types = ['type-2-x', 'type-2-y',
-                 'type-3-x', 'type-3-y',
-                 'type-4']
-
-fig, axs = plt.subplots(3, 2)
-for ax, img, feat_t in zip(np.ravel(axs), images, feature_types):
-    coord, _ = haar_like_feature_coord(img.shape[0], img.shape[1], feat_t)
-    haar_feature = draw_haar_like_feature(img, 0, 0,
-                                          img.shape[0],
-                                          img.shape[1],
-                                          coord,
-                                          max_n_features=1,
-                                          random_state=0)
-    ax.imshow(haar_feature)
-    ax.set_title(feat_t)
-    ax.set_xticks([])
-    ax.set_yticks([])
-
-fig.suptitle('The different Haar-like feature descriptors')
-plt.axis('off')
+print(__doc__)
 
 ###############################################################################
 # The usual feature extraction scheme
