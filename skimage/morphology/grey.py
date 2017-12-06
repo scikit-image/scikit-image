a2fa2069b38bc5b0e02fa36748924e876c01c27d
nrweir <nicholas.r.weir@gmail.com>
2017-12-05 23:13:06 -0500

    adding new args to white tophat, black tophat, opening, and closing

--- skimage/morphology/grey.py
+++ skimage/morphology/grey.py
@@ -141,7 +141,7 @@ def erosion(image, selem=None, out=None, shift_x=False, shift_y=False,
     enlarges dark regions.
 
     After optional shifting of selem this function is a wrapper for
-    scipy.ndimage.grey_erosion.
+    scipy.ndimage.morphology.grey_erosion.
 
     Parameters
     ----------
@@ -197,8 +197,8 @@ def erosion(image, selem=None, out=None, shift_x=False, shift_y=False,
     selem = _shift_selem(selem, shift_x, shift_y)
     if out is None:
         out = np.empty_like(image)
-    ndi.grey_erosion(image, footprint=selem, output=out, mode=mode, cval=cval,
-                     origin=origin)
+    ndi.morphology.grey_erosion(image, footprint=selem, output=out, mode=mode,
+                                cval=cval, origin=origin)
     return out
 
 
@@ -212,7 +212,7 @@ def dilation(image, selem=None, out=None, shift_x=False, shift_y=False,
     and shrinks dark regions.
 
     After optional shifting of selem this function is a wrapper for
-    scipy.ndimage.grey_dilation.
+    scipy.ndimage.morphology.grey_dilation.
 
     Parameters
     ----------
@@ -281,13 +281,19 @@ def dilation(image, selem=None, out=None, shift_x=False, shift_y=False,
 
 @default_selem
 @pad_for_eccentric_selems
-def opening(image, selem=None, out=None):
+def opening(image, selem=None, out=None, mode='reflect', cval=0.0, origin=0):
     """Return greyscale morphological opening of an image.
 
     The morphological opening on an image is defined as an erosion followed by
     a dilation. Opening can remove small bright spots (i.e. "salt") and connect
     small dark cracks. This tends to "open" up (dark) gaps between (bright)
     features.
+
+    Note that this function does not directly utilize
+    scipy.ndimage.morphology.grey_opening, but instead manually performs
+    opening through skimage's erosion/dilation (which are wrappers for
+    scipy.ndimage.morphology.grey_erosion and
+    scipy.ndimage.morphology.grey_dilation, respectively)
 
     Parameters
     ----------
@@ -299,6 +305,13 @@ def opening(image, selem=None, out=None):
     out : ndarray, optional
         The array to store the result of the morphology. If None
         is passed, a new array will be allocated.
+    mode : {'reflect', 'constant', 'nearest', 'mirror', 'wrap'}, optional
+        Determines how array borders are handled. If using 'constant', `cval`
+        is the value borders are set to. Default is 'reflect'.
+    cval : scalar, optional
+        Value to fill past edges with if `mode`='constant'. Defaults to 0.0.
+    origin : scalar, optional
+        Controls placement of the filter. Defaults to 0.
 
     Returns
     -------
@@ -323,15 +336,16 @@ def opening(image, selem=None, out=None):
            [0, 0, 0, 0, 0]], dtype=uint8)
 
     """
-    eroded = erosion(image, selem)
+    eroded = erosion(image, selem, mode=mode, cval=cval, origin=origin)
     # note: shift_x, shift_y do nothing if selem side length is odd
-    out = dilation(eroded, selem, out=out, shift_x=True, shift_y=True)
+    out = dilation(eroded, selem, out=out, shift_x=True, shift_y=True,
+                   mode=mode, cval=cval, origin=origin)
     return out
 
 
 @default_selem
 @pad_for_eccentric_selems
-def closing(image, selem=None, out=None):
+def closing(image, selem=None, out=None, mode='reflect', cval=0.0, origin=0):
     """Return greyscale morphological closing of an image.
 
     The morphological closing on an image is defined as a dilation followed by
@@ -339,6 +353,10 @@ def closing(image, selem=None, out=None):
     small bright cracks. This tends to "close" up (dark) gaps between (bright)
     features.
 
+    Note that this function does not directly utilize ndi.grey_closing, but
+    instead manually performs opening through skimage's dilation/erosion
+    (which are wrappers for ndi.grey_dilation and ndi.grey_erosion,
+    respectively)
     Parameters
     ----------
     image : ndarray
@@ -349,6 +367,13 @@ def closing(image, selem=None, out=None):
     out : ndarray, optional
         The array to store the result of the morphology. If None,
         is passed, a new array will be allocated.
+    mode : {'reflect', 'constant', 'nearest', 'mirror', 'wrap'}, optional
+        Determines how array borders are handled. If using 'constant', `cval`
+        is the value borders are set to. Default is 'reflect'.
+    cval : scalar, optional
+        Value to fill past edges with if `mode`='constant'. Defaults to 0.0.
+    origin : scalar, optional
+        Controls placement of the filter. Defaults to 0.
 
     Returns
     -------
@@ -373,19 +398,25 @@ def closing(image, selem=None, out=None):
            [0, 0, 0, 0, 0]], dtype=uint8)
 
     """
-    dilated = dilation(image, selem)
+    dilated = dilation(image, selem, mode=mode, cval=cval, origin=origin)
     # note: shift_x, shift_y do nothing if selem side length is odd
-    out = erosion(dilated, selem, out=out, shift_x=True, shift_y=True)
+    out = erosion(dilated, selem, out=out, shift_x=True, shift_y=True,
+                  mode=mode, cval=cval, origin=origin)
     return out
 
 
 @default_selem
-def white_tophat(image, selem=None, out=None):
+def white_tophat(image, selem=None, out=None, mode='reflect', cval=0.0,
+                 origin=0):
     """Return white top hat of an image.
 
     The white top hat of an image is defined as the image minus its
     morphological opening. This operation returns the bright spots of the image
     that are smaller than the structuring element.
+
+    When `out` is provided, this function directly performs opening as
+    implemented in skimage's opening (a wrapper for ndi.grey_opening). If `out`
+    is not provided, ndi.white_tophat is called directly.
 
     Parameters
     ----------
@@ -397,6 +428,13 @@ def white_tophat(image, selem=None, out=None):
     out : ndarray, optional
         The array to store the result of the morphology. If None
         is passed, a new array will be allocated.
+    mode : {'reflect', 'constant', 'nearest', 'mirror', 'wrap'}, optional
+        Determines how array borders are handled. If using 'constant', `cval`
+        is the value borders are set to. Default is 'reflect'.
+    cval : scalar, optional
+        Value to fill past edges with if `mode`='constant'. Defaults to 0.0.
+    origin : scalar, optional
+        Controls placement of the filter. Defaults to 0.
 
     Returns
     -------
@@ -423,23 +461,30 @@ def white_tophat(image, selem=None, out=None):
     """
     selem = np.array(selem)
     if out is image:
-        opened = opening(image, selem)
+        opened = opening(image, selem, mode=mode, cval=cval, origin=origin)
         out -= opened
         return out
     elif out is None:
         out = np.empty_like(image)
-    out = ndi.white_tophat(image, footprint=selem, output=out)
+    out = ndi.morphology.white_tophat(image, footprint=selem, output=out,
+                                      mode=mode, cval=cval, origin=origin)
     return out
 
 
 @default_selem
-def black_tophat(image, selem=None, out=None):
+def black_tophat(image, selem=None, out=None, mode='reflect', cval=0.0,
+                 origin=0):
     """Return black top hat of an image.
 
     The black top hat of an image is defined as its morphological closing minus
     the original image. This operation returns the dark spots of the image that
     are smaller than the structuring element. Note that dark spots in the
     original image are bright spots after the black top hat.
+
+    When `out` is provided, this function directly performs opening as
+    implemented in skimage's closing (a wrapper for
+    scipy.ndimage.morphology.grey_closing). If `out` is not provided,
+    scipy.ndimage.morphology.black_tophat is called directly.
 
     Parameters
     ----------
@@ -451,6 +496,13 @@ def black_tophat(image, selem=None, out=None):
     out : ndarray, optional
         The array to store the result of the morphology. If None
         is passed, a new array will be allocated.
+    mode : {'reflect', 'constant', 'nearest', 'mirror', 'wrap'}, optional
+        Determines how array borders are handled. If using 'constant', `cval`
+        is the value borders are set to. Default is 'reflect'.
+    cval : scalar, optional
+        Value to fill past edges with if `mode`='constant'. Defaults to 0.0.
+    origin : scalar, optional
+        Controls placement of the filter. Defaults to 0.
 
     Returns
     -------
@@ -476,9 +528,9 @@ def black_tophat(image, selem=None, out=None):
 
     """
     if out is image:
-        original = image.copy()
+        closed = closing(image, selem, mode=mode, cval=cval, origin=origin)
+        out = closed - out
     else:
-        original = image
-    out = closing(image, selem, out=out)
-    out -= original
-    return out
+        out = ndi.morphology.black_tophat(image, footprint=selem, out=out,
+                                          mode=mode, cval=cval, origin=origin)
+        return out
