1c5d1ab5f190c16779cafdb6279742dde1b6de6a
nrweir <nicholas.r.weir@gmail.com>
2017-12-05 20:22:28 -0500

    final cleanup

--- skimage/morphology/binary.py
+++ skimage/morphology/binary.py
@@ -49,11 +49,10 @@ def binary_erosion(image, selem=None, out=None, iterations=1, mask=None,
         ``[False, True]``.
 
     """
-    if out is None:
-        out = np.empty(image.shape, dtype=np.bool)
-    ndi.binary_erosion(image, structure=selem, output=out,
-                       iterations=iterations, mask=mask, origin=origin,
-                       border_value=border_value, brute_force=brute_force)
+    out = ndi.binary_erosion(image, structure=selem, output=out,
+                             iterations=iterations, mask=mask, origin=origin,
+                             border_value=border_value,
+                             brute_force=brute_force)
     return out
 
 
@@ -63,7 +62,8 @@ def binary_dilation(image, selem=None, out=None, iterations=1, mask=None,
     """Return fast binary morphological dilation of an image.
 
     This function returns the same result as greyscale dilation but performs
-    faster for binary images.
+    faster for binary images. It is a wrapper for the
+    scipy.ndimage.binary_dilation function.
 
     Morphological dilation sets a pixel at ``(i,j)`` to the maximum over all
     pixels in the neighborhood centered at ``(i,j)``. Dilation enlarges bright
@@ -71,7 +71,6 @@ def binary_dilation(image, selem=None, out=None, iterations=1, mask=None,
 
     Parameters
     ----------
-
     image : ndarray
         Binary input image.
     selem : ndarray, optional
@@ -81,7 +80,7 @@ def binary_dilation(image, selem=None, out=None, iterations=1, mask=None,
         The array to store the result of the morphology. If None, is
         passed, a new array will be allocated.
     iterations : {int, float}, optional
-        The number of times dilation is repeated (defaults to 1). If <1,
+        The number of times dilation is repeated (defaults to 1). If < 1,
         dilation is repeated until the result no longer changes.
     mask : array-like, optional
         If provided, dilation is only performed on elements of `image` with
@@ -96,21 +95,21 @@ def binary_dilation(image, selem=None, out=None, iterations=1, mask=None,
     dilated : ndarray of bool or uint
         The result of the morphological dilation with values in
         ``[False, True]``.
+
     """
-    if out is None:
-        out = np.empty(image.shape, dtype=np.bool)
-    ndi.binary_dilation(image, structure=selem, output=out,
-                        iterations=iterations, mask=mask, origin=origin,
-                        border_value=border_value, brute_force=False)
+    out = ndi.binary_dilation(image, structure=selem, output=out,
+                              iterations=iterations, mask=mask, origin=origin,
+                              border_value=border_value, brute_force=False)
     return out
 
 
 @default_selem
-def binary_opening(image, selem=None, out=None):
+def binary_opening(image, selem=None, out=None, iterations=1, origin=0):
     """Return fast binary morphological opening of an image.
 
     This function returns the same result as greyscale opening but performs
-    faster for binary images.
+    faster for binary images. It is a wrapper for the
+    scipy.ndimage.binary_opening function.
 
     The morphological opening on an image is defined as an erosion followed by
     a dilation. Opening can remove small bright spots (i.e. "salt") and connect
@@ -127,6 +126,12 @@ def binary_opening(image, selem=None, out=None):
     out : ndarray of bool, optional
         The array to store the result of the morphology. If None
         is passed, a new array will be allocated.
+    iterations : {int, float}, optional
+        The number of times binary closing (dilation followed by erosion = 1)
+        is repeated. Defaults to 1. If < 1, closing is repeated until the
+        output no longer changes.
+    origin : int or tuple of ints, optional
+        Placement of the filter, by default 0.
 
     Returns
     -------
@@ -134,17 +139,18 @@ def binary_opening(image, selem=None, out=None):
         The result of the morphological opening.
 
     """
-    eroded = binary_erosion(image, selem)
-    out = binary_dilation(eroded, selem, out=out)
+    out = ndi.binary_opening(image, structure=selem, iterations=iterations,
+                             output=out, origin=origin)
     return out
 
 
 @default_selem
-def binary_closing(image, selem=None, out=None):
+def binary_closing(image, selem=None, out=None, iterations=1, origin=0):
     """Return fast binary morphological closing of an image.
 
-    This function returns the same result as greyscale closing but performs
-    faster for binary images.
+    This function acts as a wrapper for scipy.ndimage.binary_closing and
+    returns the same result as greyscale closing but performs faster for
+    binary images.
 
     The morphological closing on an image is defined as a dilation followed by
     an erosion. Closing can remove small dark spots (i.e. "pepper") and connect
@@ -161,6 +167,12 @@ def binary_closing(image, selem=None, out=None):
     out : ndarray of bool, optional
         The array to store the result of the morphology. If None,
         is passed, a new array will be allocated.
+    iterations : {int, float}, optional
+        The number of times binary closing (dilation followed by erosion = 1)
+        is repeated. Defaults to 1. If < 1, closing is repeated until the
+        output no longer changes.
+    origin : int or tuple of ints, optional
+        Placement of the filter, by default 0.
 
     Returns
     -------
@@ -168,6 +180,6 @@ def binary_closing(image, selem=None, out=None):
         The result of the morphological closing.
 
     """
-    dilated = binary_dilation(image, selem)
-    out = binary_erosion(dilated, selem, out=out)
+    out = ndi.binary_closing(image, structure=selem, iterations=iterations,
+                             output=out, origin=origin)
     return out
--- skimage/morphology/grey.py
+++ skimage/morphology/grey.py
@@ -28,6 +28,7 @@ def _shift_selem(selem, shift_x, shift_y):
     -------
     out : 2D array, shape (M + int(shift_x), N + int(shift_y))
         The shifted structuring element.
+
     """
     if selem.ndim != 2:
         # do nothing for 1D or 3D or higher structuring elements
@@ -76,6 +77,7 @@ def _invert_selem(selem):
     References
     ----------
     .. [1] https://github.com/scipy/scipy/blob/ec20ababa400e39ac3ffc9148c01ef86d5349332/scipy/ndimage/morphology.py#L1285
+
     """
     inverted = selem[(slice(None, None, -1),) * selem.ndim]
     return inverted
@@ -83,6 +85,11 @@ def _invert_selem(selem):
 
 def pad_for_eccentric_selems(func):
     """Pad input images for certain morphological operations.
+
+    As of 0.14, only used to allow morphological functions to reproduce
+    behavior from previous versions of `skimage.morphology`, which rather than
+    using ndi.morphology edge handling, padded the image border with extra
+    pixels of the same values. Now has no effect unless `mode` = 'pad'.
 
     Parameters
     ----------
@@ -100,26 +107,30 @@ def pad_for_eccentric_selems(func):
     See Also
     --------
     opening, closing.
+
     """
     @functools.wraps(func)
-    def func_out(image, selem, out=None, *args, **kwargs):
+    def func_out(image, selem, out=None, mode='reflect', cval=0.0, origin=0,
+                 *args, **kwargs):
         pad_widths = []
         padding = False
-        if out is None:
-            out = np.empty_like(image)
-        for axis_len in selem.shape:
-            if axis_len % 2 == 0:
-                axis_pad_width = axis_len - 1
-                padding = True
-            else:
-                axis_pad_width = 0
-            pad_widths.append((axis_pad_width,) * 2)
+        mode_temp = mode
+        if mode == 'pad':
+            mode_temp = 'reflect'
+            for axis_len in selem.shape:
+                if axis_len % 2 == 0:
+                    axis_pad_width = axis_len - 1
+                    padding = True
+                else:
+                    axis_pad_width = 0
+                pad_widths.append((axis_pad_width,) * 2)
         if padding:
             image = np.pad(image, pad_widths, mode='edge')
-            out_temp = np.empty_like(image)
+            out_temp = None
         else:
             out_temp = out
-        out_temp = func(image, selem, out=out_temp, *args, **kwargs)
+        out_temp = func(image, selem, out=out_temp, mode=mode_temp, cval=0.0,
+                        origin=0, *args, **kwargs)
         if padding:
             out[:] = crop(out_temp, pad_widths)
         else:
@@ -127,13 +138,18 @@ def pad_for_eccentric_selems(func):
         return out
     return func_out
 
+
 @default_selem
-def erosion(image, selem=None, out=None, shift_x=False, shift_y=False):
+def erosion(image, selem=None, out=None, shift_x=False, shift_y=False,
+            mode='reflect', cval=0.0, origin=0):
     """Return greyscale morphological erosion of an image.
 
     Morphological erosion sets a pixel at (i,j) to the minimum over all pixels
     in the neighborhood centered at (i,j). Erosion shrinks bright regions and
     enlarges dark regions.
+
+    After optional shifting of selem this function is a wrapper for
+    scipy.ndimage.morphology.grey_erosion.
 
     Parameters
     ----------
@@ -148,6 +164,13 @@ def erosion(image, selem=None, out=None, shift_x=False, shift_y=False):
     shift_x, shift_y : bool, optional
         shift structuring element about center point. This only affects
         eccentric structuring elements (i.e. selem with even numbered sides).
+    mode : {'reflect', 'constant', 'nearest', 'mirror', 'wrap'}, optional
+        Determines how array borders are handled. If using 'constant', `cval`
+        is the value borders are set to. Default is 'reflect'.
+    cval : scalar, optional
+        Value to fill past edges with if `mode` = 'constant'. Defaults to 0.0.
+    origin : scalar, optional
+        Controls placement of the filter. Defaults to 0.
 
     Returns
     -------
@@ -156,7 +179,7 @@ def erosion(image, selem=None, out=None, shift_x=False, shift_y=False):
 
     Notes
     -----
-    For ``uint8`` (and ``uint16`` up to a certain bit-depth) data, the
+    For `uint8` (and `uint16` up to a certain bit-depth) data, the
     lower algorithm complexity makes the `skimage.filters.rank.minimum`
     function more efficient for larger images and structuring elements.
 
@@ -182,21 +205,25 @@ def erosion(image, selem=None, out=None, shift_x=False, shift_y=False):
     selem = _shift_selem(selem, shift_x, shift_y)
     if out is None:
         out = np.empty_like(image)
-    ndi.grey_erosion(image, footprint=selem, output=out)
+    ndi.morphology.grey_erosion(image, footprint=selem, output=out, mode=mode,
+                                cval=cval, origin=origin)
     return out
 
 
 @default_selem
-def dilation(image, selem=None, out=None, shift_x=False, shift_y=False):
+def dilation(image, selem=None, out=None, shift_x=False, shift_y=False,
+             mode='reflect', cval=0.0, origin=0):
     """Return greyscale morphological dilation of an image.
 
     Morphological dilation sets a pixel at (i,j) to the maximum over all pixels
     in the neighborhood centered at (i,j). Dilation enlarges bright regions
     and shrinks dark regions.
 
+    After optional shifting of selem this function is a wrapper for
+    scipy.ndimage.morphology.grey_dilation.
+
     Parameters
     ----------
-
     image : ndarray
         Image array.
     selem : ndarray, optional
@@ -208,6 +235,13 @@ def dilation(image, selem=None, out=None, shift_x=False, shift_y=False):
     shift_x, shift_y : bool, optional
         shift structuring element about center point. This only affects
         eccentric structuring elements (i.e. selem with even numbered sides).
+    mode : {'reflect', 'constant', 'nearest', 'mirror', 'wrap'}, optional
+        Determines how array borders are handled. If using 'constant', `cval`
+        is the value borders are set to. Default is 'reflect'.
+    cval : scalar, optional
+        Value to fill past edges with if `mode` = 'constant'. Defaults to 0.0.
+    origin : scalar, optional
+        Controls placement of the filter. Defaults to 0.
 
     Returns
     -------
@@ -245,22 +279,25 @@ def dilation(image, selem=None, out=None, shift_x=False, shift_y=False):
     # this author (@jni). To "patch" this behaviour, we invert our own
     # selem before passing it to `ndi.grey_dilation`.
     # [1] https://github.com/scipy/scipy/blob/ec20ababa400e39ac3ffc9148c01ef86d5349332/scipy/ndimage/morphology.py#L1285
+    # added 20171207 @nrweir: This is still the case, so keeping _invert_selem.
     selem = _invert_selem(selem)
     if out is None:
         out = np.empty_like(image)
-    ndi.grey_dilation(image, footprint=selem, output=out)
+    ndi.grey_dilation(image, footprint=selem, output=out, mode=mode,
+                      cval=cval, origin=origin)
     return out
 
 
 @default_selem
-@pad_for_eccentric_selems
-def opening(image, selem=None, out=None):
+def opening(image, selem=None, out=None, mode='reflect', cval=0.0, origin=0):
     """Return greyscale morphological opening of an image.
 
     The morphological opening on an image is defined as an erosion followed by
     a dilation. Opening can remove small bright spots (i.e. "salt") and connect
     small dark cracks. This tends to "open" up (dark) gaps between (bright)
     features.
+
+    This function is a wrapper to ndi.morphology.grey_opening.
 
     Parameters
     ----------
@@ -272,6 +309,16 @@ def opening(image, selem=None, out=None):
     out : ndarray, optional
         The array to store the result of the morphology. If None
         is passed, a new array will be allocated.
+    mode : {'reflect', 'constant', 'nearest', 'mirror', 'wrap', 'pad'},
+           optional
+        Determines how array borders are handled. If using 'constant', `cval`
+        is the value borders are set to. Default is 'reflect'. If using 'pad',
+        will reproduce behavior displayed in scikit-imgae < 0.14 and pad edges
+        prior to processing with eccentrically shaped structuring elements.
+    cval : scalar, optional
+        Value to fill past edges with if `mode` = 'constant'. Defaults to 0.0.
+    origin : scalar, optional
+        Controls placement of the filter. Defaults to 0.
 
     Returns
     -------
@@ -296,21 +343,24 @@ def opening(image, selem=None, out=None):
            [0, 0, 0, 0, 0]], dtype=uint8)
 
     """
-    eroded = erosion(image, selem)
-    # note: shift_x, shift_y do nothing if selem side length is odd
-    out = dilation(eroded, selem, out=out, shift_x=True, shift_y=True)
+    out = ndi.grey_opening(image, footprint=selem, output=out, mode=mode,
+                           cval=cval, origin=origin)
     return out
 
 
 @default_selem
-@pad_for_eccentric_selems
-def closing(image, selem=None, out=None):
+def closing(image, selem=None, out=None, mode='reflect', cval=0.0, origin=0):
     """Return greyscale morphological closing of an image.
 
     The morphological closing on an image is defined as a dilation followed by
     an erosion. Closing can remove small dark spots (i.e. "pepper") and connect
     small bright cracks. This tends to "close" up (dark) gaps between (bright)
     features.
+
+    Note that this function does not directly utilize ndi.grey_closing, but
+    instead manually performs opening through skimage's dilation/erosion
+    (which are wrappers for ndi.grey_dilation and ndi.grey_erosion,
+    respectively)
 
     Parameters
     ----------
@@ -322,6 +372,16 @@ def closing(image, selem=None, out=None):
     out : ndarray, optional
         The array to store the result of the morphology. If None,
         is passed, a new array will be allocated.
+    mode : {'reflect', 'constant', 'nearest', 'mirror', 'wrap', 'pad'},
+           optional
+        Determines how array borders are handled. If using 'constant', `cval`
+        is the value borders are set to. Default is 'reflect'. If using 'pad',
+        will reproduce behavior displayed in scikit-imgae < 0.14 and pad edges
+        prior to processing with eccentrically shaped structuring elements.
+    cval : scalar, optional
+        Value to fill past edges with if `mode` = 'constant'. Defaults to 0.0.
+    origin : scalar, optional
+        Controls placement of the filter. Defaults to 0.
 
     Returns
     -------
@@ -346,19 +406,22 @@ def closing(image, selem=None, out=None):
            [0, 0, 0, 0, 0]], dtype=uint8)
 
     """
-    dilated = dilation(image, selem)
-    # note: shift_x, shift_y do nothing if selem side length is odd
-    out = erosion(dilated, selem, out=out, shift_x=True, shift_y=True)
+    out = ndi.grey_closing(image, footprint=selem, output=out, mode=mode,
+                           cval=cval, origin=origin)
     return out
 
 
 @default_selem
-def white_tophat(image, selem=None, out=None):
+@pad_for_eccentric_selems
+def white_tophat(image, selem=None, out=None, mode='reflect', cval=0.0,
+                 origin=0):
     """Return white top hat of an image.
 
     The white top hat of an image is defined as the image minus its
     morphological opening. This operation returns the bright spots of the image
     that are smaller than the structuring element.
+
+    This function is a wrapper to ndi.morphology.white_tophat.
 
     Parameters
     ----------
@@ -370,6 +433,16 @@ def white_tophat(image, selem=None, out=None):
     out : ndarray, optional
         The array to store the result of the morphology. If None
         is passed, a new array will be allocated.
+    mode : {'reflect', 'constant', 'nearest', 'mirror', 'wrap', 'pad'},
+           optional
+        Determines how array borders are handled. If using 'constant', `cval`
+        is the value borders are set to. Default is 'reflect'. If using 'pad',
+        will reproduce behavior displayed in scikit-imgae < 0.14 and pad edges
+        prior to processing with eccentrically shaped structuring elements.
+    cval : scalar, optional
+        Value to fill past edges with if `mode` = 'constant'. Defaults to 0.0.
+    origin : scalar, optional
+        Controls placement of the filter. Defaults to 0.
 
     Returns
     -------
@@ -394,25 +467,23 @@ def white_tophat(image, selem=None, out=None):
            [0, 0, 0, 0, 0]], dtype=uint8)
 
     """
-    selem = np.array(selem)
-    if out is image:
-        opened = opening(image, selem)
-        out -= opened
-        return out
-    elif out is None:
-        out = np.empty_like(image)
-    out = ndi.white_tophat(image, footprint=selem, output=out)
+    out = ndi.white_tophat(image, footprint=selem, output=out, mode=mode,
+                           cval=cval, origin=origin)
     return out
 
 
 @default_selem
-def black_tophat(image, selem=None, out=None):
+@pad_for_eccentric_selems
+def black_tophat(image, selem=None, out=None, mode='reflect', cval=0.0,
+                 origin=0):
     """Return black top hat of an image.
 
     The black top hat of an image is defined as its morphological closing minus
     the original image. This operation returns the dark spots of the image that
     are smaller than the structuring element. Note that dark spots in the
     original image are bright spots after the black top hat.
+
+    This function is a wrapper to ndi.morphology.black_tophat.
 
     Parameters
     ----------
@@ -424,6 +495,16 @@ def black_tophat(image, selem=None, out=None):
     out : ndarray, optional
         The array to store the result of the morphology. If None
         is passed, a new array will be allocated.
+    mode : {'reflect', 'constant', 'nearest', 'mirror', 'wrap', 'pad'},
+           optional
+        Determines how array borders are handled. If using 'constant', `cval`
+        is the value borders are set to. Default is 'reflect'. If using 'pad',
+        will reproduce behavior displayed in scikit-imgae < 0.14 and pad edges
+        prior to processing with eccentrically shaped structuring elements.
+    cval : scalar, optional
+        Value to fill past edges with if `mode` = 'constant'. Defaults to 0.0.
+    origin : scalar, optional
+        Controls placement of the filter. Defaults to 0.
 
     Returns
     -------
@@ -448,10 +529,6 @@ def black_tophat(image, selem=None, out=None):
            [0, 0, 0, 0, 0]], dtype=uint8)
 
     """
-    if out is image:
-        original = image.copy()
-    else:
-        original = image
-    out = closing(image, selem, out=out)
-    out -= original
+    out = ndi.morphology.black_tophat(image, footprint=selem, output=out,
+                                      mode=mode, cval=cval, origin=origin)
     return out
