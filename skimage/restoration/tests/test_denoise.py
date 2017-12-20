13ced196a401d755860ff7f7410e1d7795b996c8
Egor Panfilov <egor.v.panfilov@gmail.com>
2017-12-20 12:45:52 +0300

    Removed debug information, cosmetic changes in test_denoise

--- skimage/restoration/tests/test_denoise.py
+++ skimage/restoration/tests/test_denoise.py
@@ -256,17 +256,16 @@ def test_denoise_nl_means_2d_multichannel():
     imgn = np.clip(imgn, 0, 1)
     for fast_mode in [True, False]:
         for s in [sigma, 0]:
-            for n_chan in [2, 3, 6]:
-                psnr_noisy = compare_psnr(img[..., :n_chan],
-                                          imgn[..., :n_chan])
-                denoised = restoration.denoise_nl_means(imgn[..., :n_chan],
+            for n_channels in [2, 3, 6]:
+                psnr_noisy = compare_psnr(img[..., :n_channels],
+                                          imgn[..., :n_channels])
+                denoised = restoration.denoise_nl_means(imgn[..., :n_channels],
                                                         3, 5, h=0.75 * sigma,
                                                         fast_mode=fast_mode,
                                                         multichannel=True,
                                                         sigma=s)
-                psnr_denoised = compare_psnr(denoised[..., :n_chan],
-                                             img[..., :n_chan])
-                print(psnr_noisy, psnr_denoised)
+                psnr_denoised = compare_psnr(denoised[..., :n_channels],
+                                             img[..., :n_channels])
                 # make sure noise is reduced
                 assert_(psnr_denoised > psnr_noisy)
 
