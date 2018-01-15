2a6bb56959bf6b80b12cdcc639a77682382fcadd
Emmanuelle Gouillart <emmanuelle.gouillart@nsup.org>
2018-01-15 22:07:41 +0100

    pep8-ification

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -30,7 +30,7 @@ sys.path.append(os.path.join(curpath, '..', 'ext'))
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'numpydoc',
-	      'sphinx.ext.autodoc',
+              'sphinx.ext.autodoc',
               'sphinx.ext.autosummary',
               'sphinx.ext.intersphinx',
               'sphinx.ext.linkcode',
