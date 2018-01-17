48ef5db81c8f0940ee4f9b4b050462d067b2769c
Egor Panfilov <egor.v.panfilov@gmail.com>
2018-01-17 19:59:20 +0300

    Use legacy numpy printing in pytest

--- conftest.py
+++ conftest.py
@@ -1,5 +1,12 @@
-# List of files that pytest should ignore
+# Use legacy numpy printing. This fix is made to keep doctests functional.
+# For more info, see https://github.com/scikit-image/scikit-image/pull/2935 .
+# TODO: remove this workaround once minimal required numpy is set to 1.14.0
+import numpy as np
 
+if np.version.full_version >= '1.14.0':
+    np.set_printoptions(legacy='1.13')
+
+# List of files that pytest should ignore
 collect_ignore = ["setup.py",
                   "skimage/io/_plugins",
                   "doc/",
