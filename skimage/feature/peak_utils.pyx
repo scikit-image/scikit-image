27c4c7dcc99c63464d4ee298a01625c2f514e531
Lars G <lagru@mailbox.org>
2018-04-23 12:10:05 +0200

    Implement buffer as class-like queue

--- skimage/feature/peak_utils.pyx
+++ skimage/feature/peak_utils.pyx
@@ -14,6 +14,70 @@ ctypedef fused dtype_t:
     cnp.uint16_t
     cnp.double_t
 
+ctypedef struct QueueItem:
+    Py_ssize_t r
+    Py_ssize_t c
+
+
+cdef:
+    struct Queue:
+        # A queue whose items can be restored after consumption.
+        QueueItem* _buffer_ptr
+        Py_ssize_t _buffer_size, _index_valid, _index_consumed
+
+    void q_init(Queue* self, Py_ssize_t buffer_size) nogil:
+        """Initialize the queue and its buffer."""
+        self._buffer_ptr = <QueueItem*>malloc(buffer_size * sizeof(QueueItem))
+        if not self._buffer_ptr:
+            with gil:
+                raise MemoryError("couldn't allocate buffer")
+        self._buffer_size = buffer_size
+        self._index_consumed = -1
+        self._index_valid = -1
+
+    void q_free_buffer(Queue* self) nogil:
+        """Free the buffer of the queue."""
+        free(self._buffer_ptr)
+
+    inline void q_restore(Queue* self) nogil:
+        """Restore all consumed items to the queue."""
+        self._index_consumed = -1
+
+    inline void q_clear(Queue* self) nogil:#
+        """Remove all items in the queue."""
+        self._index_consumed = -1
+        self._index_valid = -1
+
+    inline void q_push(Queue* self, QueueItem* item_ptr) nogil:
+        """Enqueue a new item."""
+        self._index_valid += 1
+        if self._buffer_size <= self._index_valid:
+            _q_grow_buffer(self)
+        self._buffer_ptr[self._index_valid] = item_ptr[0]
+
+    inline unsigned char q_pop(Queue* self, QueueItem* item_ptr) nogil:
+        """Dequeue / consume an item."""
+        if 0 <= self._index_consumed + 1 <= self._index_valid:
+            self._index_consumed += 1
+            item_ptr[0] = self._buffer_ptr[self._index_consumed]
+            return 1
+        return 0
+
+    void _q_grow_buffer(Queue* self) nogil:
+        """Double the memory used for the buffer."""
+        cdef QueueItem* new_buffer
+
+        # TODO prevent integer overflow!
+        self._buffer_size *= 2
+        new_buffer_ptr = <QueueItem*>realloc(
+            self._buffer_ptr,
+            self._buffer_size * sizeof(QueueItem)
+        )
+        if not new_buffer_ptr:
+            with gil:
+                raise MemoryError("couldn't reallocate buffer")
+        self._buffer_ptr = new_buffer_ptr
+
 
 # -- Algorithm A ---------------------------------------------------------------
 
@@ -33,16 +97,14 @@ def maxima2d_A(dtype_t[:, ::1] image not None):
     3. Return array with flags (only 0 and 1 remain).
     """
     cdef:
+        Queue queue
         unsigned char[:, ::1] flags
-        Py_ssize_t* r_buffer
-        Py_ssize_t* c_buffer
-        Py_ssize_t candidate_count, buffer_size, r_max, c_max, r, c, c_ahead
+        Py_ssize_t r_max, c_max, r, c, c_ahead
 
     # Flags: 0 - no maximum, 2 - potential maximum, 1 - definite maximum
     flags = np.zeros_like(image, dtype=np.uint8)
 
     with nogil:
-        candidate_count = 0  # Number of potential maxima
         r_max = image.shape[0] - 1
         c_max = image.shape[1] - 1
 
@@ -61,7 +123,6 @@ def maxima2d_A(dtype_t[:, ::1] image not None):
                     if image[r, c_ahead] <= image[r, c]:
                         # Mark samples as potential maximum
                         flags[r, c:c_ahead] = 2
-                        candidate_count += c_ahead - c
                     # Skip already evaluated samples
                     c = c_ahead
                 else:
@@ -76,24 +137,17 @@ def maxima2d_A(dtype_t[:, ::1] image not None):
                 image[r, 1] < image[r, 0]
             ):
                 flags[r, 0] = 2
-                candidate_count += 1
             if (
                 flags[r, c_max - 1] == 2 and
                 image[r, c_max - 1] == image[r, c_max] or
                 image[r, c_max - 1] < image[r, c_max]
             ):
                 flags[r, c_max] = 2
-                candidate_count += 1
 
         # Initialize a buffer used to queue positions while evaluating each
         # potential maximum (flagged with 2); the initial size is the number
-        # of potential maxima / candidates.
-        buffer_size = candidate_count
-        r_buffer = <Py_ssize_t*>malloc(buffer_size * sizeof(Py_ssize_t))
-        c_buffer = <Py_ssize_t*>malloc(buffer_size * sizeof(Py_ssize_t))
-        if not r_buffer or not c_buffer:
-            with gil:
-                raise MemoryError()
+        # of potential maxima
+        q_init(&queue, 64)
         try:
             for r in range(r_max + 1):
                 c = 0
@@ -102,12 +156,10 @@ def maxima2d_A(dtype_t[:, ::1] image not None):
                     if flags[r, c] == 2:
                         # Find all samples part of the plateau and fill with 0
                         # or 1 depending on whether it's a true maximum
-                        _fill_plateau_A(image, flags, &r_buffer, &c_buffer,
-                                        &buffer_size, r, c, r_max, c_max)
+                        _fill_plateau_A(image, flags, &queue, r, c, r_max, c_max)
                     c += 1
         finally:
-            free(r_buffer)
-            free(c_buffer)
+            q_free_buffer(&queue)
 
     # Return original python object which is base of memoryview
     return flags.base
@@ -115,68 +167,59 @@ def maxima2d_A(dtype_t[:, ::1] image not None):
 
 cdef inline void \
     _fill_plateau_A(dtype_t[:, ::1] image, unsigned char[:, ::1] flags,
-                    Py_ssize_t** r_buffer, Py_ssize_t** c_buffer,
-                    Py_ssize_t* buffer_size, Py_ssize_t r, Py_ssize_t c,
+                    Queue* queue_ptr, Py_ssize_t r, Py_ssize_t c,
                     Py_ssize_t r_max, Py_ssize_t c_max) nogil:
     """Fill with 1 if plateau is local maximum else with 0."""
     cdef:
         dtype_t h
-        Py_ssize_t p_next, p_valid
         unsigned char true_maximum
         Py_ssize_t steps_r[8]
         Py_ssize_t steps_c[8]
+        QueueItem item
 
     h = image[r, c]
     true_maximum = 1 # Boolean flag
     # Steps that shift a position to all 8 neighbouring samples
     steps_r = ( 0, -1, 0, 0, 1, 1,  0,  0)
     steps_c = (-1,  0, 1, 1, 0, 0, -1, -1)
+    item.r = r
+    item.c = c
+    q_clear(queue_ptr)
 
     # Push starting position to buffer
-    p_next = 0  # Pointer to next evaluated position
-    p_valid = 0  # Pointer to most recent position in buffer (end of valid area)
-    r_buffer[0][p_valid] = r  # Dereference pointer with "[0]"
-    c_buffer[0][p_valid] = c
+    q_push(queue_ptr, &item)
     # Mark as true maximum according to initial guess, also works as a flag that
     # a sample is queued / in the buffer
     flags[r, c] = 1
 
     # Break loop if all queued positions were evaluated
-    while p_next <= p_valid:
-        # Load next position in buffer and shift pointer to next element
-        r = r_buffer[0][p_next]
-        c = c_buffer[0][p_next]
-        p_next += 1
+    while q_pop(queue_ptr, &item):
 
         # Look at all  8 neighbouring samples
         for i in range(8):
-            r += steps_r[i]
-            c += steps_c[i]
-            if not (0 <= r <= r_max and 0 <= c <= c_max):
+            item.r += steps_r[i]
+            item.c += steps_c[i]
+            if not (0 <= item.r <= r_max and 0 <= item.c <= c_max):
                 continue  # Skip "neighbours" outside image
 
             # If neighbour wasn't queued already (!= 1) and is part of the
             # current plateau (== h)
-            if flags[r, c] != 1 and image[r, c] == h:
+            if flags[item.r, item.c] != 1 and image[item.r, item.c] == h:
                 # Push neighbour to buffer
-                p_valid += 1
-                if p_valid >= buffer_size[0]:
-                    _double_buffer(r_buffer, c_buffer, buffer_size)
-                r_buffer[0][p_valid] = r
-                c_buffer[0][p_valid] = c
+                q_push(queue_ptr, &item)
                 # Flag as true maximum (initial guess) so that it isn't queued
                 # multiple times
-                flags[r, c] = 1
+                flags[item.r, item.c] = 1
 
             # If neighbouring sample is larger -> plateau isn't a maximum
-            elif image[r, c] > h:
+            elif image[item.r, item.c] > h:
                 true_maximum = 0
 
     if not true_maximum:
+        q_restore(queue_ptr)
         # Initial guess was wrong -> replace 1 with 0 for plateau
-        while p_valid >= 0:
-            flags[r_buffer[0][p_valid], c_buffer[0][p_valid]] = 0
-            p_valid -= 1
+        while q_pop(queue_ptr, &item):
+            flags[item.r, item.c] = 0
 
 
 
@@ -197,9 +240,7 @@ def maxima2d_B(dtype_t[:, ::1] image not None):
     """
     cdef:
         unsigned char[:, ::1] flags
-        Py_ssize_t* r_buffer
-        Py_ssize_t* c_buffer
-        Py_ssize_t buffer_size
+        Queue queue
         Py_ssize_t r_max, c_max, r, c, c_ahead
 
     # Flags: 0 - no maximum, 2 - potential maximum, 1 - definite maximum
@@ -211,13 +252,7 @@ def maxima2d_B(dtype_t[:, ::1] image not None):
 
         # Initialize a buffer used to queue positions while evaluating each
         # sample; the initial size is just a guess
-        buffer_size = 64
-        r_buffer = <Py_ssize_t *>malloc(buffer_size * sizeof(Py_ssize_t))
-        c_buffer = <Py_ssize_t *>malloc(buffer_size * sizeof(Py_ssize_t))
-        if not r_buffer or not c_buffer:
-            with gil:
-                raise MemoryError()
-
+        q_init(&queue, 64)
         try:
             for r in range(r_max + 1):
                 c = 0
@@ -226,20 +261,17 @@ def maxima2d_B(dtype_t[:, ::1] image not None):
                     if flags[r, c] == 2:
                         # Find all samples part of the plateau and fill with 0
                         # or 1 depending on whether it's a true maximum
-                        _fill_plateau_B(image, flags, &r_buffer, &c_buffer,
-                                        &buffer_size, r, c, r_max, c_max)
+                        _fill_plateau_B(image, flags, &queue, r, c, r_max, c_max)
                     c += 1
         finally:
-            free(r_buffer)
-            free(c_buffer)
+            q_free_buffer(&queue)
     # Return original python object which is base of memoryview
     return flags.base
 
 
 cdef inline void \
     _fill_plateau_B(dtype_t[:, ::1] image, unsigned char[:, ::1] flags,
-                    Py_ssize_t** r_buffer, Py_ssize_t** c_buffer,
-                    Py_ssize_t* buffer_size, Py_ssize_t r, Py_ssize_t c,
+                    Queue* queue_ptr, Py_ssize_t r, Py_ssize_t c,
                     Py_ssize_t r_max, Py_ssize_t c_max) nogil:
     """Fill with 1 if plateau is local maximum else with 0."""
     cdef:
@@ -247,75 +279,47 @@ cdef inline void \
         unsigned char true_maximum
         Py_ssize_t steps_r[8]
         Py_ssize_t steps_c[8]
-        Py_ssize_t p_next, p_valid
+        QueueItem item
 
     h = image[r, c]
     true_maximum = 1  # Boolean flag
     # Steps that shift a position to all 8 neighbouring samples
     steps_r = ( 0, -1, 0, 0, 1, 1,  0,  0)
     steps_c = (-1,  0, 1, 1, 0, 0, -1, -1)
+    q_clear(queue_ptr)
 
     # Push starting position to buffer
-    p_next = 0  # Pointer to next evaluated position
-    p_valid = 0  # Pointer to most recent position in buffer (end of valid area)
-    r_buffer[0][p_valid] = r  # Dereference pointer with "[0]"
-    c_buffer[0][p_valid] = c
+    item.r = r
+    item.c = c
+    q_push(queue_ptr, &item)
     # Assume that majority of samples isn't part of a maximum, thus mark
     # initially with 0, also works as a flag that a sample is queued already
     flags[r, c] = 0
 
     # Break loop if all pending positions were evaluated
-    while p_next <= p_valid:
-        # Load next position in buffer and shift pointer to next element
-        r = r_buffer[0][p_next]
-        c = c_buffer[0][p_next]
-        p_next += 1
-
+    while q_pop(queue_ptr, &item):
         # Look at all  8 neighbouring samples
         for i in range(8):
-            r += steps_r[i]
-            c += steps_c[i]
-            if not (0 <= r <= r_max and 0 <= c <= c_max):
+            item.r += steps_r[i]
+            item.c += steps_c[i]
+            if not (0 <= item.r <= r_max and 0 <= item.c <= c_max):
                 continue  # Skip "neighbours" outside image
 
             # If neighbour wasn't queued already (== 2) and is part of the
             # current plateau (== h)
-            if flags[r, c] == 2 and image[r, c] == h:
+            if flags[item.r, item.c] == 2 and image[item.r, item.c] == h:
                 # Push neighbour to buffer
-                p_valid += 1
-                if p_valid >= buffer_size[0]:
-                    _double_buffer(r_buffer, c_buffer, buffer_size)
-                r_buffer[0][p_valid] = r
-                c_buffer[0][p_valid] = c
+                q_push(queue_ptr, &item)
                 # Flag as "not maximum" (initial guess) so that it isn't queued
                 # multiple times
-                flags[r, c] = 0
+                flags[item.r, item.c] = 0
 
             # If neighbouring sample is larger -> plateau isn't a maximum
-            elif image[r, c] > h:
+            elif image[item.r, item.c] > h:
                 true_maximum = 0
 
     if true_maximum:
+        q_restore(queue_ptr)
         # Initial guess was wrong -> replace 0 with 1 for plateau
-        while p_valid >= 0:
-            flags[r_buffer[0][p_valid], c_buffer[0][p_valid]] = 1
-            p_valid -= 1
-
-
-# -- Helper --------------------------------------------------------------------
-
-cdef void _double_buffer(Py_ssize_t** r_buffer, Py_ssize_t** c_buffer,
-                         Py_ssize_t* buffer_size) nogil:
-    """Safely double buffer size."""
-    cdef:
-        Py_ssize_t* new_r_buffer
-        Py_ssize_t* new_c_buffer
-    buffer_size[0] *= 2
-    new_r_buffer = <Py_ssize_t *>realloc(r_buffer[0], buffer_size[0] * sizeof(Py_ssize_t))
-    new_c_buffer = <Py_ssize_t *>realloc(c_buffer[0], buffer_size[0] * sizeof(Py_ssize_t))
-    if not new_r_buffer or not new_c_buffer:
-        with gil:
-            raise MemoryError()
-    # Only replace pointer to buffer if reallocation was successful
-    r_buffer[0] = new_r_buffer
-    c_buffer[0] = new_c_buffer
+        while q_pop(queue_ptr, &item):
+            flags[item.r, item.c] = 1
