7c0282bcce1765e8f3008c8b5791c2c5eda3038d
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2018-04-29 10:50:51 +1000

    Refine docstring of util.invert

--- skimage/util/_invert.py
+++ skimage/util/_invert.py
@@ -5,37 +5,59 @@ from .dtype import dtype_limits
 def invert(image, signed_float=False):
     """Invert an image.
 
-    Image inversion. If the image is of type unsignedinteger, the image
-    is subtracted from the maximum value allowed by the dtype maximum.
-    If the image is of type signedinteger, the image is subtracted from -1.
-    If the image is of float type and signed_float is true, the result is
-    -image. If the image is of float type and signed_float is false (default)
-    the result is 1.0 - image.
+    Invert the intensity range of the input image, so that the maximum value is
+    now the minimum for the input dtype, and vice-versa. This operation is
+    slightly different depending on the input dtype:
+
+    - unsigned integers: subtract the image from the dtype maximum
+    - signed integers: subtract the image from -1 (see Notes)
+    - floats: subtract the image from 1 (if signed_float is False, so we
+      assume the image is unsigned), or from 0 (if signed_float is True).
+
+    See the examples for clarification.
 
     Parameters
     ----------
     image : ndarray
-        The input image.
-    signed_float : bool
+        Input image.
+    signed_float : bool, optional
         If True and the image is of type float, the range is assumed to
         be [-1, 1]. If False and the image is of type float, the range is
-        assumed to be [0, 1]. Default value is False (i.e. the assumed range is
-        [0, 1].
+        assumed to be [0, 1].
 
     Returns
     -------
-    invert : ndarray
+    inverted : ndarray
         Inverted image.
+
+    Notes
+    -----
+    Ideally, for signed integers we would simply multiply by -1. However,
+    signed integer ranges are asymmetric. For example, for np.int8, the range
+    of possible values is [-128, 127], so that -128 * -1 equals -128! By
+    subtracting from -1, we correctly map the maximum dtype value to the
+    minimum.
 
     Examples
     --------
-    >>> img = np.array([[100, 0, 200],
-    ...                 [0,  50, 0],
-    ...                 [30,  0, 255]], np.uint8)
+    >>> img = np.array([[100,  0, 200],
+    ...                 [  0, 50,   0],
+    ...                 [ 30,  0, 255]], np.uint8)
     >>> invert(img)
     array([[155, 255,  55],
            [255, 205, 255],
            [225, 255,   0]], dtype=uint8)
+    >>> img2 = np.array([[ -2, 0, -128],
+    ...                  [127, 0,    5]], np.int8)
+    >>> invert(img2)
+    array([[   1,   -1,  127],
+           [-128,   -1,   -6]], dtype=int8)
+    >>> img3 = np.array([[ 0., 1., 0.5, 0.75]])
+    >>> invert(img3)
+    array([[ 1.  ,  0.  ,  0.5 ,  0.25]])
+    >>> img4 = np.array([[ 0., 1., -1., -0.25]])
+    >>> invert(img4, signed_float=True)
+    array([[-0.  , -1.  ,  1.  ,  0.25]])
     """
     if image.dtype == 'bool':
         return(~image)
