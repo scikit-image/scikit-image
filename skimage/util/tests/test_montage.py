487d6859a97f447487cf29a0c9f1d7ab14fc98aa
Mark Harfouche <mark.harfouche@gmail.com>
2018-05-12 20:14:17 +0200

    utils: no more warnings

--- skimage/util/tests/test_montage.py
+++ skimage/util/tests/test_montage.py
@@ -1,8 +1,14 @@
 from skimage._shared import testing
-from skimage._shared.testing import assert_equal, assert_array_equal
+from skimage._shared.testing import (assert_equal, assert_array_equal,
+                                     expected_warnings)
 
 import numpy as np
-from skimage.util import montage2d, montage
+from skimage.util import montage
+from skimage.util import montage2d as montage2d_deprecated
+
+def montage2d(*args, **kwargs):
+    with expected_warnings(['deprecated']):
+        return montage2d_deprecated(*args, **kwargs)
 
 
 def test_montage2d_simple():
