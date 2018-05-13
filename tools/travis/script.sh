193c06425ab875447bd64c7871dc6887b988be65
Mark Harfouche <mark.harfouche@gmail.com>
2018-05-13 08:40:27 +0200

    Use travis for a sperate build for minimum_requirements

--- tools/travis/script.sh
+++ tools/travis/script.sh
@@ -17,10 +17,6 @@ if [[ $TRAVIS_OS_NAME == "osx" ]]; then
     echo 'backend : Template' > $MPL_DIR/matplotlibrc
 fi
 
-section "Test.with.min.requirements"
-pytest $TEST_ARGS skimage
-section_end "Test.with.min.requirements"
-
 section "Flake8.test"
 flake8 --exit-zero --exclude=test_*,six.py skimage doc/examples viewer_examples
 section_end "Flake8.test"
@@ -34,7 +30,7 @@ if [[ $OPTIONAL_DEPS == 1 ]]; then
 fi
 
 # Install Qt and then update the Matplotlib settings
-if [[ $WITH_QT == 1 ]]; then
+if [[ $QT == "PyQt4" ]]; then
     # http://stackoverflow.com/a/9716100
     LIBS=( PyQt4 sip.so )
 
@@ -49,7 +45,7 @@ if [[ $WITH_QT == 1 ]]; then
         ln -sf $LIB_SYSTEM_PATH/$LIB $LIB_VIRTUALENV_PATH/$LIB
     done
 
-elif [ "$WITH_PYSIDE" == 1 ] && [ -e ~/venv/bin/pyside_postinstall.py ]; then
+elif [ "$QT" == "PySide" ] && [ -e ~/venv/bin/pyside_postinstall.py ]; then
     python ~/venv/bin/pyside_postinstall.py -install
 fi
 
@@ -57,22 +53,17 @@ if [[ $WITH_PYAMG == 1 ]]; then
     pip install --retries 3 -q pyamg
 fi
 
-# Show what's installed
-pip list
-
 section_end "Install.optional.dependencies"
 
 
 section "Build.docs.or.run.examples"
 
-# This fails on Travis OSX
-if [[ $TRAVIS_OS_NAME != "osx" ]]; then
-    pip install --retries 3 -q -r ./requirements/docs.txt
-fi
-
 if [[ $BUILD_DOCS == 1 ]]; then
+    # This fails on Travis OSX
+    pip install --retries 3 -q -r ./requirements/docs.txt
     export SPHINXCACHE=$HOME/.cache/sphinx; make html
 else
+    # Why is this optional?
     echo 'backend : Template' > $MPL_DIR/matplotlibrc
 
     for f in doc/examples/*/*.py; do
@@ -95,29 +86,42 @@ for f in doc/examples/xx_applications/*.py; do
     fi
 done
 
-# Now configure Matplotlib to use Qt4
-if [[ $WITH_QT == 1 ]]; then
-    MPL_QT_API=PyQt4
-    export QT_API=pyqt
-elif [[ $WITH_PYSIDE == 1 ]]; then
-    MPL_QT_API=PySide
-    export QT_API=pyside
-fi
-if [[ $WITH_QT == 1 || $WITH_PYSIDE == 1 ]]; then
-    echo 'backend: Qt4Agg' > $MPL_DIR/matplotlibrc
-    echo 'backend.qt4 : '$MPL_QT_API >> $MPL_DIR/matplotlibrc
-fi
-
 section_end "Run.doc.applications"
 
 
 section "Test.with.optional.dependencies"
 
+# Now configure Matplotlib to use Qt4
+if [[ $QT == "PyQt4" ]]; then
+    MPL_QT_API=PyQt4
+    export QT_API=pyqt
+elif [[ $QT == "PyQt4" ]]; then
+        MPL_QT_API=PyQt5
+        export QT_API=pyqt5
+elif [[ $QT == "PySide" ]]; then
+    MPL_QT_API=PySide
+    export QT_API=pyside
+elif [[ $QT == "PySide2" ]]; then
+    MPL_QT_API=PySide2
+    export QT_API=pyside2
+fi
+if [[ $QT == "PyQt4" || $QT == "PySide" ]]; then
+    echo 'backend: Qt4Agg' > $MPL_DIR/matplotlibrc
+    echo 'backend.qt4 : '$MPL_QT_API >> $MPL_DIR/matplotlibrc
+elif [[ $QT == "PyQt5" || $QT == "PySide2" ]]; then
+    # Is this correct for PySide2?
+    echo 'backend: Qt5Agg' > $MPL_DIR/matplotlibrc
+    echo 'backend.qt5 : '$MPL_QT_API >> $MPL_DIR/matplotlibrc
+fi
+
 # run tests again with optional dependencies to get more coverage
 if [[ $OPTIONAL_DEPS == 1 ]]; then
     TEST_ARGS="$TEST_ARGS --cov=skimage"
 fi
-pytest $TEST_ARGS
+
+# Show what's installed
+pip list
+pytest $TEST_ARGS skimage
 
 section_end "Test.with.optional.dependencies"
 
