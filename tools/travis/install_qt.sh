474809c40074a4d34391ca0c3839cc201fe6057f
Mark Harfouche <mark.harfouche@gmail.com>
2018-05-13 23:43:54 +0200

    Fixed a bug in install_qt.sh

--- tools/travis/install_qt.sh
+++ tools/travis/install_qt.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
-if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+set -ex
+
+if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
     echo "backend : Template" > $MPL_DIR/matplotlibrc
 fi
 # Now configure Matplotlib to use Qt4
-if [[ $QT == "PyQt4" ]]; then
+if [[ "${QT}" == "PyQt4" ]]; then
     pip install pyqt4
     MPL_QT_API=PyQt4
     export QT_API=pyqt
-elif [[ $QT == "PyQt4" ]]; then
+elif [[ "${QT}" == "PyQt5" ]]; then
     pip install pyqt5
     MPL_QT_API=PyQt5
     export QT_API=pyqt5
-elif [[ $QT == "PySide" ]]; then
+elif [[ "${QT}" == "PySide" ]]; then
     pip install pyside
     MPL_QT_API=PySide
     export QT_API=pyside
-elif [[ $QT == "PySide2" ]]; then
+elif [[ "${QT}" == "PySide2" ]]; then
     pip install pyside2
     MPL_QT_API=PySide2
     export QT_API=pyside2
 fi
-if [[ $QT == "PyQt4" || $QT == "PySide" ]]; then
+if [[ "${QT}" == "PyQt4" || "${QT}" == "PySide" ]]; then
     echo 'backend: Qt4Agg' > $MPL_DIR/matplotlibrc
     echo 'backend.qt4 : '$MPL_QT_API >> $MPL_DIR/matplotlibrc
-elif [[ $QT == "PyQt5" || $QT == "PySide2" ]]; then
+elif [[ "${QT}" == "PyQt5" || "${QT}" == "PySide2" ]]; then
     # Is this correct for PySide2?
     echo 'backend: Qt5Agg' > $MPL_DIR/matplotlibrc
     echo 'backend.qt5 : '$MPL_QT_API >> $MPL_DIR/matplotlibrc
 fi
+
+set -ex
