c7a10491f20ea38b3c47fdda7e65f44085b4f674
Kevin Mader <kmader@users.noreply.github.com>
2018-05-14 13:51:46 +0200

    Update convex_hull.py
    changing convex_hull_image behavior to show a warning and return an empty image if given an empty image

--- skimage/morphology/convex_hull.py
+++ skimage/morphology/convex_hull.py
@@ -6,6 +6,7 @@ from ..measure.pnpoly import grid_points_in_poly
 from ._convex_hull import possible_hull
 from ..measure._label import label
 from ..util import unique_rows
+from .._shared.utils import warn
 
 __all__ = ['convex_hull_image', 'convex_hull_object']
 
@@ -47,7 +48,11 @@ def convex_hull_image(image, offset_coordinates=True, tolerance=1e-10):
 
     """
     ndim = image.ndim
-
+    if not np.max(image):
+        # if there are no non-zero points print a warning and return an empty image
+        warn("Input image is entirely zero and does not have a valid convex hull"
+             "Returning empty image", UserWarning)
+        return np.zeros(image.shape, dtype = bool)
     # In 2D, we do an optimisation by choosing only pixels that are
     # the starting or ending pixel of a row or column.  This vastly
     # limits the number of coordinates to examine for the virtual hull.
