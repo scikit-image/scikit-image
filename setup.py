33cfbca6246edc632b4d97b9a8659431479c2a50
Mark Harfouche <mark.harfouche@gmail.com>
2018-05-15 19:55:30 -0700

    Accidentally added some things to Manifest and setup.py

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
         ],
         install_requires=INSTALL_REQUIRES,
         requires=REQUIRES,
-        packages=setuptools.find_packages(exclude=['doc', 'tests']),
+        packages=setuptools.find_packages(exclude=['doc']),
         include_package_data=True,
         zip_safe=False,  # the package can run out of an .egg file
 
