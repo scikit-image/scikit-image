cff66591b39fc42f18d96e1f6902bd734b2abef2
Mark Harfouche <mark.harfouche@gmail.com>
2018-06-02 12:20:50 -0700

    Fix the combine_stains to use powers of 10 as well

--- skimage/color/colorconv.py
+++ skimage/color/colorconv.py
@@ -1474,7 +1474,7 @@ def combine_stains(stains, conv_matrix):
 
     stains = dtype.img_as_float(stains)
     logrgb2 = -np.reshape(stains, (-1, 3)) @ conv_matrix
-    rgb2 = np.exp(logrgb2)
+    rgb2 = np.power(10, logrgb2)
     return rescale_intensity(np.reshape(rgb2 - 2, stains.shape),
                              in_range=(-1, 1))
 
