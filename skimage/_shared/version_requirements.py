ea94b34aaf6cc2f3ee32a9e833065ef9abfdc5f9
Mark Harfouche <mark.harfouche@gmail.com>
2018-06-08 19:54:19 -0700

    Included the user's python version.

--- skimage/_shared/version_requirements.py
+++ skimage/_shared/version_requirements.py
@@ -1,4 +1,5 @@
 from distutils.version import LooseVersion
+from platform import python_version
 import functools
 import re
 import sys
@@ -13,9 +14,11 @@ def check_python_version(min_version):
 You are running scikit-image on an unsupported version of Python.
 Likely Python 2.
 
-Unfortunately, scikit-image 0.15 and above no longer work on this
-version of Python.  You therefore have two options: either upgrade to
-Python %s, or install an older version of scikit-image. For Python 2.7, use
+Unfortunately, scikit-image 0.15 and above no longer work with your installed
+version of Python (%s).  You therefore have two options: either upgrade to
+Python %s, or install an older version of scikit-image.
+
+For Python 2.7 or Python 3.4, use
 
  $ pip install 'scikit-image<0.15'
 
@@ -25,7 +28,7 @@ Please also consider updating `pip` and `setuptools`:
 
 Newer versions of these tools avoid installing packages incompatible
 with your version of Python.
-""" % '.'.join([str(v) for v in min_version]))
+""" % (python_version(), '.'.join([str(v) for v in min_version])))
 
 
 def _check_version(actver, version, cmp_op):
