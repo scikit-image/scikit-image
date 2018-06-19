c7076bfa4bd081d38f87ec6d018e78272ba781c7
Kim Newell <Kim.Newell@csiro.au>
2018-06-19 16:27:03 +1000

    Add benchmark for filters.sobel

---
+++ benchmarks/benchmark_filters.py
@@ -0,0 +1,16 @@
+# See "Writing benchmarks" in the asv docs for more information.
+import numpy as np
+from skimage import filters
+
+
+class FiltersSuite:
+    """Benchmark for filter routines in scikit-image."""
+    def setup(self):
+        self.image = np.random.random((4000, 4000))
+        self.image[:2000, :2000] += 1
+        self.image[3000:, 3000] += 0.5
+
+    def time_sobel(self):
+        result = filters.sobel(self.image)
+
+
