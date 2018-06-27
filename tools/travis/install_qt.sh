0374a6ae80fd4a20eb604a51290bec056f550fdf
Mark Harfouche <mark.harfouche@gmail.com>
2018-06-26 20:15:07 -0700

    Does the install qt script even work?

--- tools/travis/install_qt.sh
+++ tools/travis/install_qt.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -ev
-
+section "install.qt"
 if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
     echo "backend : Template" > $MPL_DIR/matplotlibrc
 fi
+
+echo ${QT}
 # Now configure Matplotlib to use Qt5
-if [[ "${QT}" == "PyQt5" ]]; then
+if [[ "${QT}" = "PyQt5" ]]; then
+    echo got here
     pip install --retries 3 -q $PIP_FLAGS pyqt5
     MPL_QT_API=PyQt5
     export QT_API=pyqt5
@@ -18,8 +21,10 @@ else
 fi
 if [[ "${QT}" == "PyQt5" || "${QT}" == "PySide2" ]]; then
     # Is this correct for PySide2?
+    echo got there
     echo 'backend: Qt5Agg' > $MPL_DIR/matplotlibrc
     echo 'backend.qt5 : '$MPL_QT_API >> $MPL_DIR/matplotlibrc
 fi
+section_end "install.qt"
 
 set +ev
