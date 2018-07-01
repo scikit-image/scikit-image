5eeae5c651ab135a95ce37f733be325a2d3bd0a5
Lars G <lagru@mailbox.org>
2018-07-01 13:17:45 +0200

    BENCH: Add benchmarks for morphology.watershed

---
+++ benchmarks/benchmark_morphology.py
@@ -0,0 +1,38 @@
+"""Benchmarks for `skimage.morphology`."""
+
+
+import numpy as np
+from skimage import data, filters, morphology
+
+
+class Watershed(object):
+
+    param_names = ["seed_count", "connectivity", "compactness"]
+    params = [(5, 500), (1, 2), (0, 0.01)]
+
+    def setup(self, *args):
+        self.image = filters.sobel(data.coins())
+
+    def time_watershed(self, seed_count, connectivity, compactness):
+        morphology.watershed(self.image, seed_count, connectivity,
+                             compactness=compactness)
+
+    def peakmem_reference(self, *args):
+        """Provide reference for memory measurement with empty benchmark.
+
+        Peakmem benchmarks measure the maximum amount of RAM used by a
+        function. However, this maximum also includes the memory used
+        during the setup routine (as of asv 0.2.1; see [1]_).
+        Measuring an empty peakmem function might allow us to disambiguate
+        between the memory used by setup and the memory used by target (see
+        other ``peakmem_`` functions below).
+
+        References
+        ----------
+        .. [1]: http://asv.readthedocs.io/en/stable/writing_benchmarks.html#peak-memory
+        """
+        pass
+
+    def peakmem_watershed(self, seed_count, connectivity, compactness):
+        morphology.watershed(self.image, seed_count, connectivity,
+                             compactness=compactness)
