ad93f97709f2ab3a4985aea3eaf4bcbb8f60162e
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2018-07-21 22:24:26 -0500

    Use versionchanged tag inside output argument docstring

--- skimage/util/_regular_grid.py
+++ skimage/util/_regular_grid.py
@@ -24,6 +24,16 @@ def regular_grid(ar_shape, n_points):
         A slice along each dimension of `ar_shape`, such that the intersection
         of all the slices give the coordinates of regularly spaced points.
 
+        .. versionchanged:: 0.14.1
+            In scikit-image 0.14.1 and 0.15, the return type was changed from a
+            list to a tuple to ensure `compatibility with Numpy 1.15`_ and
+            higher. If your code requires the returned result to be a list, you
+            may convert the output of this function to a list with:
+
+            >>> result = list(regular_grid(ar_shape=(3, 20, 40), n_points=8))
+
+            .. _compatibility with NumPy 1.15: https://github.com/numpy/numpy/blob/master/doc/release/1.15.0-notes.rst#deprecations
+
     Examples
     --------
     >>> ar = np.zeros((20, 40))
@@ -47,18 +57,6 @@ def regular_grid(ar_shape, n_points):
     >>> ar[g] = 1
     >>> ar.sum()
     8.0
-
-    Note
-    ----
-    In scikit-image 0.15 the return type was changed from a list to a tuple to
-    ensure compatibility with Numpy 1.15:
-    https://github.com/numpy/numpy/blob/master/doc/release/1.15.0-notes.rst#deprecations
-
-    If your code requires the returned result to be a list, you may convert the
-    output of this function to a list with:
-
-    >>> result = list(regular_grid(ar_shape=(3, 20, 40), n_points=8))
-
     """
     ar_shape = np.asanyarray(ar_shape)
     ndim = len(ar_shape)
