09851812e54a0f7690281fbedd055ef5fed4bcbe
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2018-07-22 03:17:23 -0500

    Add note about changed squeeze behavior in regionprops docstring

--- skimage/measure/_regionprops.py
+++ skimage/measure/_regionprops.py
@@ -365,6 +365,13 @@ def regionprops(label_image, intensity_image=None, cache=True,
     ----------
     label_image : (N, M) ndarray
         Labeled input image. Labels with value 0 are ignored.
+
+        .. versionchanged:: 0.14.1
+            Previously, ``label_image`` was processed by ``numpy.squeeze`` and
+            so any number of singleton dimensions was allowed. This resulted in
+            inconsistent handling of images with singleton dimensions. To
+            recover the old behaviour, use
+            ``regionprops(np.squeeze(label_image), ...)``.
     intensity_image : (N, M) ndarray, optional
         Intensity (i.e., input) image with same size as labeled image.
         Default is None.
