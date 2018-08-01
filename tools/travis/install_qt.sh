36f146626a0d9de6f72348a6d0efa22cbfc69cf0
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2018-08-01 10:30:36 +1000

    Backport #3242: handle NumPy 1.15 warnings in dependencies

--- tools/travis/install_qt.sh
+++ tools/travis/install_qt.sh
@@ -27,8 +27,12 @@ elif [[ "${QT}" == "PySide" ]]; then
     python ~/venv/bin/pyside_postinstall.py -install
     MPL_QT_API=PySide
     export QT_API=pyside
+# Now configure Matplotlib to use Qt5
 elif [[ "${QT}" == "PyQt5" ]]; then
-    pip install --retries 3 -q $PIP_FLAGS pyqt5
+    # Matplotlib 2.2.2 doesn't support pyqt5 5.11 until this commit
+    # https://github.com/matplotlib/matplotlib/commit/260e6d611afae9adaac528c28a4879097c357b74#diff-025508b6963efcfa97ba27389f2d2b86
+    # becomes released
+    pip install --retries 3 -q $PIP_FLAGS 'pyqt5<5.11'
     MPL_QT_API=PyQt5
     export QT_API=pyqt5
 elif [[ "${QT}" == "PySide2" ]]; then
