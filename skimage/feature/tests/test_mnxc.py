0ca63b845b9e2d33e4659f06ac89a146a39bd0b7
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-08-09 12:18:11 -0400

    Added test comparing mnsc with autocorrelation, despite random masks

--- skimage/feature/tests/test_mnxc.py
+++ skimage/feature/tests/test_mnxc.py
@@ -3,6 +3,7 @@ from skimage._shared.testing import assert_equal
 from skimage._shared import testing
 
 from skimage.feature import mnxc
+from skimage.data import camera
 
 def test_mnxc_output_shape():
     """Masked normalized cross-correlation should return a shape of N + M + 1 for each transform axis."""
@@ -100,3 +101,22 @@ def test_mnxc_over_axes():
     over_axes = mnxc(arr1, arr2, m1, m2, axes = (0, 1), mode = 'same')
 
     testing.assert_array_almost_equal(with_loop, over_axes)
+
+def test_mnxc_autocorrelation_trivial_masks():
+    """Masked normalized cross-correlation between identical arrays should reduce to an autocorrelation even with random masks."""
+    # See random number generator for reproducible results
+    np.random.seed(23)
+
+    arr1 = camera()
+    arr2 = camera()
+    
+    # Random masks with 75% of pixels being valid
+    m1 = np.random.choice([True, False], arr1.shape, p = [3/4, 1/4])
+    m2 = np.random.choice([True, False], arr2.shape, p = [3/4, 1/4])
+
+    xcorr = mnxc(arr1, arr1, m1, m1, axes = (0, 1), mode = 'same', overlap_ratio = 0).real
+    max_index = np.unravel_index(np.argmax(xcorr), xcorr.shape)
+
+    # Autocorrelation should have maximum in center of array
+    testing.assert_almost_equal(xcorr.max(), 1)
+    testing.assert_array_equal(max_index, np.array(arr1.shape) / 2)
