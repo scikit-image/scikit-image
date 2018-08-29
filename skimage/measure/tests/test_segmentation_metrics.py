40b1c087c7317cb7880f177c77d84443a03effc8
Sean Budd <seanbudd@me.com>
2018-08-29 14:45:48 +1000

    improvements to segmentation metrics testing

--- skimage/measure/tests/test_segmentation_metrics.py
+++ skimage/measure/tests/test_segmentation_metrics.py
@@ -8,16 +8,16 @@ from skimage.measure import (compare_adapted_rand_error,
 from skimage._shared.testing import assert_equal, assert_almost_equal
 
 def test_red():
-    seg = np.array([[2, 1], [1, 2]])
-    gt = np.array([[1, 2],[3, 1]])
-    assert_equal(compare_raw_edit_distance(seg, gt), (-3.0, 0.0))
+    im_true = np.array([[2, 1], [1, 2]])
+    im_test = np.array([[1, 2],[3, 1]])
+    assert_equal(compare_raw_edit_distance(im_true, im_test), (-3.0, 0.0))
 
 def test_vi():
-    seg = np.array([1, 2, 3, 4])
-    gt = np.array([1, 1, 8, 8])
-    assert_equal(compare_variation_of_information(seg, gt), 1)
+    im_true = np.array([1, 2, 3, 4])
+    im_test = np.array([1, 1, 8, 8])
+    assert_equal(compare_variation_of_information(im_true, im_test), -1)
 
 def test_are():
-    seg = np.array([[2, 1], [1, 2]])
-    gt = np.array([[1, 2],[3, 1]])
-    assert_almost_equal((compare_adapted_rand_error(seg, gt)), 0.3333333)
+    im_true = np.array([[2, 1], [1, 2]])
+    im_test = np.array([[1, 2],[3, 1]])
+    assert_almost_equal(compare_adapted_rand_error(im_true, im_test), (0.3333333, 0.5, 1.0))
