e01d43cf5a77c9a76130890fd3231115de1ae28b
Alexandre de Siqueira <siqueiraaf@gmail.com>
2018-09-01 15:10:38 +0200

    Removing duplicated imports.

--- skimage/filters/tests/test_thresholding.py
+++ skimage/filters/tests/test_thresholding.py
@@ -20,8 +20,6 @@ from skimage.filters.thresholding import (threshold_local,
                                           _mean_std)
 from skimage._shared import testing
 from skimage._shared.testing import assert_equal, assert_almost_equal
-from skimage._shared import testing
-from skimage._shared.testing import assert_equal
 
 
 class TestSimpleImage():
