0b0d285db301564a1bf10e420f817c434d33d04e
François Boulogne <devel@sciunto.org>
2018-09-03 10:25:43 +0200

    Show our data to gallery

---
+++ doc/examples/data/plot_general.py
@@ -0,0 +1,42 @@
+"""
+======================
+General purpose images
+======================
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from skimage import data
+
+
+images = (
+           'astronaut',
+           'binary_blobs',
+           'camera',
+           'checkerboard',
+           'chelsea',
+           'clock',
+           'coffee',
+           'coins',
+           'horse',
+           'logo',
+           'page',
+           'text',
+           'rocket',
+           )
+
+#'stereo_motorcycle'
+#'lfw_subset',
+
+fig, axes = plt.subplots(len(images), 1, figsize=(8, 4 * len(images)),
+                         sharex=False, sharey=False)
+ax = axes.ravel()
+
+for i, image in enumerate(images):
+    caller = getattr(data, image)
+    ax[i].imshow(caller())
+    ax[i].set_title(image)
+
+fig.tight_layout()
+plt.show()
---
+++ doc/examples/data/plot_scientific.py
@@ -0,0 +1,27 @@
+"""
+=================
+Scientific images
+=================
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from skimage import data
+
+
+images = ('hubble_deep_field',
+        'immunohistochemistry',
+        'moon',
+        )
+
+fig, axes = plt.subplots(len(images), 1, figsize=(8, 4 * len(images)),
+ax = axes.ravel()
+
+for i, image in enumerate(images):
+    caller = getattr(data, image)
+    ax[i].imshow(caller())
+    ax[i].set_title(image)
+
+fig.tight_layout()
+plt.show()
