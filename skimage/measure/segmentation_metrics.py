fix log calculation for variation of information

skimage/measure/segmentation_metrics.py
@@ -269,8 +269,8 @@ def _vi_tables(im_true, im_test):
     py_inv = sparse.diags(_invert_nonzero(py))
 
     # then, compute the entropies
-    hygx = px @ _xlogx(px_inv @ pxy).sum(axis=1)
-    hxgy = _xlogx(pxy @ py_inv).sum(axis=0) @ py
+    hygx = -px @ _xlogx(px_inv @ pxy).sum(axis=1)
+    hxgy = -_xlogx(pxy @ py_inv).sum(axis=0) @ py
 
     return list(map(np.asarray, [hxgy, hygx]))
 

skimage/measure/tests/test_segmentation_metrics.py
@@ -9,7 +9,7 @@ from skimage._shared.testing import assert_equal, assert_almost_equal
 def test_vi():
     im_true = np.array([1, 2, 3, 4])
     im_test = np.array([1, 1, 8, 8])
-    assert_equal(compare_variation_of_information(im_true, im_test), -1)
+    assert_equal(compare_variation_of_information(im_true, im_test), 1)
 
 def test_are():
     im_true = np.array([[2, 1], [1, 2]])