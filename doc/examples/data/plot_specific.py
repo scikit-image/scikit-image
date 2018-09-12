b1554a3be29045f1d602cd19256cad6301b1e056
François Boulogne <devel@sciunto.org>
2018-09-12 09:00:15 +0200

    Integrate lwm dataset

--- doc/examples/data/plot_specific.py
+++ doc/examples/data/plot_specific.py
@@ -9,8 +9,11 @@ import matplotlib.pyplot as plt
 
 from skimage import data
 
-# TODO
-#'lfw_subset',
+######################################################################
+#
+# Stereo images
+# =============
+
 
 fig, axes = plt.subplots(1, 2, figsize=(8, 4))
 ax = axes.ravel()
@@ -19,7 +22,22 @@ images = data.stereo_motorcycle()
 ax[0].imshow(images[0])
 ax[1].imshow(images[1])
 
-#ax[i].set_title(image)
+fig.tight_layout()
+plt.show()
 
+######################################################################
+#
+# Faces and non-faces dataset
+# ===========================
+#
+# A sample of 20 over 200 images is displayed.
+
+
+fig, axes = plt.subplots(4, 5, figsize=(20, 20))
+ax = axes.ravel()
+images = data.lfw_subset()
+for i in range(20):
+    ax[i].imshow(images[90+i], cmap=plt.cm.gray)
+    ax[i].axis('off')
 fig.tight_layout()
 plt.show()
