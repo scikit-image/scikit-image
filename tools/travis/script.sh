da3fd48c38574b2c8a40a12239a8ecfc5087c809
Mark Harfouche <mark.harfouche@gmail.com>
2018-09-29 23:53:45 -0400

    Build tools: cleanup doc dependecy install since OSX can now install sphinx gallery

--- tools/travis/script.sh
+++ tools/travis/script.sh
@@ -22,27 +22,16 @@ section_end "Tests.pytest"
 section "Tests.examples"
 # Run example applications
 echo Build or run examples
+pip install --retries 3 -q -r ./requirements/docs.txt
+echo 'backend : Template' > $MPL_DIR/matplotlibrc
 if [[ "${BUILD_DOCS}" == "1" ]]; then
-  # requirements/docs.txt fails on Travis OSX
-  pip install --retries 3 -q -r ./requirements/docs.txt
   export SPHINXCACHE=${HOME}/.cache/sphinx; make html
 elif [[ "${TEST_EXAMPLES}" != "0" ]]; then
-  # OSX Can't install sphinx-gallery.
-  # I think all it needs is scikit-learn from that requirements doc
-  # to run the tests. See Issue #3084
-  if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-    pip install --retries 3 -q scikit-learn
-  else
-    pip install --retries 3 -q -r ./requirements/docs.txt
-  fi
-  cp $MPL_DIR/matplotlibrc $MPL_DIR/matplotlibrc_backup
-  echo 'backend : Template' > $MPL_DIR/matplotlibrc
   for f in doc/examples/*/*.py; do
     python "${f}"
     if [ $? -ne 0 ]; then
       exit 1
     fi
   done
-  mv $MPL_DIR/matplotlibrc_backup $MPL_DIR/matplotlibrc
 fi
 section_end "Tests.examples"
