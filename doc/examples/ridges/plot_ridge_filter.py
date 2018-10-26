c3505c7382710eccd63f96057eb4d9e5b0bc8d5c
David Breuer <david.breuer@daqana.com>
2018-10-27 01:35:06 +0200

    Finish rebase of ridge-detection branch

--- doc/examples/ridges/plot_ridge_filter.py
+++ doc/examples/ridges/plot_ridge_filter.py
@@ -12,25 +12,34 @@ intensity changes perpendicular but not along the structure.
 .. [1] Meijering, E., Jacob, M., Sarria, J. C., Steiner, P., Hirling, H.,
 Unser, M. (2004). Design and validation of a tool for neurite tracing and
 analysis in fluorescence microscopy images. Cytometry Part A, 58(2), 167-176.
+https://imagescience.org/meijering/publications/download/cyto2004.pdf
 
 .. [2] Sato, Y., Nakajima, S., Shiraga, N., Atsumi, H., Yoshida, S., Koller,
 T., ..., Kikinis, R. (1998). Three-dimensional multi-scale line filter for
 segmentation and visualization of curvilinear structures in medical images.
 Medical image analysis, 2(2), 143-168.
+https://pdfs.semanticscholar.org/6964/59e0c67f729a05a819699adae5d64aaab4b3.pdf
 
 .. [3] Frangi, A. F., Niessen, W. J., Vincken, K. L., & Viergever, M. A. (1998,
 October). Multiscale vessel enhancement filtering. In International Conference
 on Medical Image Computing and Computer-Assisted Intervention (pp. 130-137).
 Springer Berlin Heidelberg.
+http://www.tecn.upf.es/~afrangi/articles/miccai1998.pdf
 
 .. [4] Ng, C. C., Yap, M. H., Costen, N., & Li, B. (2014, November). Automatic
 wrinkle detection using hybrid Hessian filter. In Asian Conference on Computer
 Vision (pp. 609-622). Springer International Publishing.
+(https://dspace.lboro.ac.uk/dspace-jspui/bitstream/ \
+ 2134/20252/1/Choon-accv2014final-604.pdf)
 """
 
 from skimage.data import page
-from skimage.filters import identity, meijering, sato, frangi, hessian
+from skimage.filters import meijering, sato, frangi, hessian
 import matplotlib.pyplot as plt
+
+def identity(image, **kwargs):
+    """Return the original image, ignoring any kwargs."""
+    return image
 
 image = page()
 
