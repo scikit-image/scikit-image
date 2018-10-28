c26f3db4eb2a7b4ee44e5a09e6bc8100f44c4845
David Breuer <david.breuer@daqana.com>
2018-10-28 16:51:20 +0100

    Fix rst formatting in ridge filter gallery example

--- doc/examples/edges/plot_ridge_filter.py
+++ doc/examples/edges/plot_ridge_filter.py
@@ -3,34 +3,43 @@
 Ridge operators
 ===============
 
-Ridge filters can be used to detect continuous edges, such as neurites
-_[1], tubes _[2], vessels _[3], wrinkles _[4], rivers, and other rigdge-like
-structures. The present class of ridge filters relies on the eigenvalues of
+Ridge filters can be used to detect ridge-like structures, such as neurites
+[1]_, tubes [2]_, vessels [3]_, wrinkles [4]_ or rivers.
+
+Different ridge filters may be suited for detecting different structures,
+e.g., depending on contrast or noise level.
+
+The present class of ridge filters relies on the eigenvalues of
 the Hessian matrix of image intensities to detect ridge structures where the
 intensity changes perpendicular but not along the structure.
 
-.. [1] Meijering, E., Jacob, M., Sarria, J. C., Steiner, P., Hirling, H.,
-Unser, M. (2004). Design and validation of a tool for neurite tracing and
-analysis in fluorescence microscopy images. Cytometry Part A, 58(2), 167-176.
-https://imagescience.org/meijering/publications/download/cyto2004.pdf
+References
+----------
 
-.. [2] Sato, Y., Nakajima, S., Shiraga, N., Atsumi, H., Yoshida, S., Koller,
-T., ..., Kikinis, R. (1998). Three-dimensional multi-scale line filter for
-segmentation and visualization of curvilinear structures in medical images.
-Medical image analysis, 2(2), 143-168.
-https://pdfs.semanticscholar.org/6964/59e0c67f729a05a819699adae5d64aaab4b3.pdf
+.. [1] Meijering, E., Jacob, M., Sarria, J. C., Steiner, P., Hirling, H.,
+       Unser, M. (2004). Design and validation of a tool for neurite tracing
+       and analysis in fluorescence microscopy images. Cytometry Part A, 58(2),
+       167-176.
+       https://imagescience.org/meijering/publications/download/cyto2004.pdf
+
+.. [2] Sato, Y., Nakajima, S., Shiraga, N., Atsumi, H., Yoshida, S.,
+       Koller, T., ..., Kikinis, R. (1998). Three-dimensional multi-scale line
+       filter for segmentation and visualization of curvilinear structures in
+       medical images. Medical image analysis, 2(2), 143-168.
+       https://pdfs.semanticscholar.org/6964/\
+59e0c67f729a05a819699adae5d64aaab4b3.pdf
 
 .. [3] Frangi, A. F., Niessen, W. J., Vincken, K. L., & Viergever, M. A. (1998,
-October). Multiscale vessel enhancement filtering. In International Conference
-on Medical Image Computing and Computer-Assisted Intervention (pp. 130-137).
-Springer Berlin Heidelberg.
-http://www.tecn.upf.es/~afrangi/articles/miccai1998.pdf
+       October). Multiscale vessel enhancement filtering. In International
+       Conference on Medical Image Computing and Computer-Assisted Intervention
+       (pp. 130-137). Springer Berlin Heidelberg.
+       http://www.tecn.upf.es/~afrangi/articles/miccai1998.pdf
 
 .. [4] Ng, C. C., Yap, M. H., Costen, N., & Li, B. (2014, November). Automatic
-wrinkle detection using hybrid Hessian filter. In Asian Conference on Computer
-Vision (pp. 609-622). Springer International Publishing.
-(https://dspace.lboro.ac.uk/dspace-jspui/bitstream/ \
- 2134/20252/1/Choon-accv2014final-604.pdf)
+       wrinkle detection using hybrid Hessian filter. In Asian Conference on
+       Computer Vision (pp. 609-622). Springer International Publishing.
+       https://dspace.lboro.ac.uk/dspace-jspui/bitstream/\
+2134/20252/1/Choon-accv2014final-604.pdf
 """
 
 from skimage.data import page
