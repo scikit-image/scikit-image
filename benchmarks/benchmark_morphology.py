5c8b795dbd6b61e0c26f78d9e4d4eeec752ec913
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-11-01 16:46:23 +0100

    Add skeletonize_3d benchmark

--- benchmarks/benchmark_morphology.py
+++ benchmarks/benchmark_morphology.py
@@ -36,3 +36,33 @@ class Watershed(object):
     def peakmem_watershed(self, seed_count, connectivity, compactness):
         morphology.watershed(self.image, seed_count, connectivity,
                              compactness=compactness)
+
+
+
+class Skeletonize3d(object):
+
+    def setup(self, *args):
+        # we stack the horse data 5 times to get an example image
+        self.image = np.stack(5 * [data.horse()])
+
+    def time_skeletonize_3d(self):
+        morphology.skeletonize_3d(self.image)
+
+    def peakmem_reference(self, *args):
+        """Provide reference for memory measurement with empty benchmark.
+
+        Peakmem benchmarks measure the maximum amount of RAM used by a
+        function. However, this maximum also includes the memory used
+        during the setup routine (as of asv 0.2.1; see [1]_).
+        Measuring an empty peakmem function might allow us to disambiguate
+        between the memory used by setup and the memory used by target (see
+        other ``peakmem_`` functions below).
+
+        References
+        ----------
+        .. [1]: https://asv.readthedocs.io/en/stable/writing_benchmarks.html#peak-memory
+        """
+        pass
+
+    def peakmem_skeletonize_3d(self):
+        morphology.skeletonize_3d(self.image)
