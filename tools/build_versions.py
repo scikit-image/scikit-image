d0a8f6525c8d8df190cc6338384aa8d095948a2c
Mark Harfouche <mark.harfouche@gmail.com>
2018-11-03 12:36:07 -0400

    Get rid of the requirements-parser dependency

--- tools/build_versions.py
+++ tools/build_versions.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-
-import numpy as np
-import scipy as sp
-import matplotlib as mpl
-import six
-from PIL import Image
-import Cython
-import networkx
+import pkg_resources
+import re
+from os.path import dirname, abspath, basename
+from glob import glob
 
 
-for m in (np, sp, mpl, six, Image, networkx, Cython):
-    if m is Image:
-        version = m.VERSION
-    else:
-        version = m.__version__
-    print(m.__name__.rjust(10), ' ', version)
+def main():
+    requirements_dir = dirname(abspath(__file__)) + '/../requirements'
+    for requirement_file in glob(requirements_dir + '/*.txt'):
+        print(basename(requirement_file))
+        with open(str(requirement_file), 'r') as f:
+            for req in f:
+                req = req.strip()
+                if req[0] == '#':
+                    continue
+                req = re.split('<|>|=|!|;', req)[0]
+                try:
+                    version = pkg_resources.get_distribution(req).version
+                    print(req.rjust(20), version)
+                except pkg_resources.DistributionNotFound:
+                    print(req.rjust(20), 'is not installed')
+
+
+if __name__ == '__main__':
+    main()
