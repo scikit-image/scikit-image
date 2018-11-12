8a297162ffe45fe66e336d71036e5a57fb0d1c38
Sean Budd <seanbudd@me.com>
2018-11-12 18:08:13 +1100

    minor fixes to registration py

--- skimage/transform/registration.py
+++ skimage/transform/registration.py
@@ -8,17 +8,17 @@ from ..measure import compare_mse
 __all__ = ['register_affine']
 
 
-def _argmin_to_matrix(argmin):
+def _parameter_vector_to_matrix(parameter_vector):
     """
     Converts the optimisation parameters to a 3x3 transformation matrix
 
-    The optimisation paramters are known as the argmin and are composed of
+    The optimisation paramters are known as the parameter_vector and are composed of
     the first 2 rows of the transformation matrix, as that is all that
     is used in an affine transformation.
 
     Parameters
     ----------
-    argmin : (6) array
+    parameter_vector : (6) array
         Input array giving the argument of the minimum function to optimise against
 
     Returns
@@ -27,26 +27,14 @@ def _argmin_to_matrix(argmin):
         A transformation matrix used to obtain a new image
     """
 
-    matrix = np.empty((3, 3))
-
-    matrix[0][0] = argmin[0]
-    matrix[0][1] = argmin[1]
-    matrix[0][2] = argmin[2]
-    matrix[1][0] = argmin[3]
-    matrix[1][1] = argmin[4]
-    matrix[1][2] = argmin[5]
-    matrix[2][0] = 0
-    matrix[2][1] = 0
-    matrix[2][2] = 1
-
-    return matrix
+    return np.concatenate((np.reshape(parameter_vector, (2, 3)), [[0, 0, 1]]), axis=0)
 
 
-def _matrix_to_argmin(matrix):
+def _matrix_to_parameter_vector(matrix):
     """
     Converts a 3x3 transformation matrix to the optimisation parameters
 
-    See the inverse function `_argmin_to_matrix`.
+    See the inverse function `_parameter_vector_to_matrix`.
 
     Parameters
     ----------
@@ -55,12 +43,12 @@ def _matrix_to_argmin(matrix):
 
     Returns
     -------
-    argmin : (6) array
+    parameter_vector : (6) array
         Output array giving the argument of the minimum function to optimise against
 
     """
 
-    return (matrix[0][0], matrix[0][1], matrix[0][2], matrix[1][0], matrix[1][1], matrix[1][2])
+    return matrix[:2, :].ravel()
 
 
 def register_affine(reference, target, *, cost=compare_mse, nlevels=None,
@@ -68,7 +56,7 @@ def register_affine(reference, target, *, cost=compare_mse, nlevels=None,
     """
     Returns a matrix which registers the target image to the reference image
 
-    Uses image registration techniques, and only optimises with affine transformations.
+    Will only return an affine transformation matrix.
 
     Parameters
     ----------
@@ -100,6 +88,16 @@ def register_affine(reference, target, *, cost=compare_mse, nlevels=None,
         A transformation matrix used to obtain a new image.
         ndi.affine_transform(target, matrix) will align your target image.
 
+    Example
+    -------
+    >>> reference_image = camera()
+    >>> matrix_transform = [
+            [np.cos(0.12), -np.sin(0.12), 0.2],
+            [np.sin(0.12),  np.cos(0.12), 0.1],
+            [0,             0,            1.0]]
+    >>> target_image = ndi.affine_transform(reference_image, matrix_transform)
+    >>> output_image = register_affine(reference_image, target_image)
+
     """
 
     if nlevels is None:
@@ -110,20 +108,20 @@ def register_affine(reference, target, *, cost=compare_mse, nlevels=None,
     pyramid_ref = pyramid_gaussian(reference, max_layer=nlevels - 1)
     pyramid_tgt = pyramid_gaussian(target, max_layer=nlevels - 1)
     image_pairs = reversed(list(zip(pyramid_ref, pyramid_tgt)))
-    argmin = np.zeros(6)
-    argmin[0] = 1
-    argmin[4] = 1
+    parameter_vector = np.zeros(6)
+    parameter_vector[0] = 1
+    parameter_vector[4] = 1
 
     for (ref, tgt) in image_pairs:
         def _cost(param):
-            transformation = _argmin_to_matrix(param)
+            transformation = _parameter_vector_to_matrix(param)
             transformed = ndi.affine_transform(tgt, transformation, order=1)
             return cost(ref, transformed)
 
-        result = minimize(_cost, argmin, method='Powell')
-        argmin = result.x
-        iter_callback(tgt, _argmin_to_matrix(argmin))
+        result = minimize(_cost, parameter_vector, method='Powell')
+        parameter_vector = result.x
+        iter_callback(tgt, _parameter_vector_to_matrix(parameter_vector))
 
-    matrix = _argmin_to_matrix(argmin)
+    matrix = _parameter_vector_to_matrix(parameter_vector)
 
     return matrix
