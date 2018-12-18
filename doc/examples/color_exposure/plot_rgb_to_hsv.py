3c7cd501906371d2bbe58515816775cc6396375b
Alexis CVETKOV-ILIEV <alexis.cvetkov-iliev@institutoptique.fr>
2018-12-18 17:58:59 +0100

    Changed syntax according to the PEP8 issues

--- doc/examples/color_exposure/plot_rgb_to_hsv.py
+++ doc/examples/color_exposure/plot_rgb_to_hsv.py
@@ -21,7 +21,7 @@ from skimage.color import rgb2hsv
 
 original = data.coffee()
 hsv_img = rgb2hsv(original)
-hue_img = hsv_img[:,:,0]
+hue_img = hsv_img[:, :, 0]
 
 global_thresh = 0.04
 binary_global = hue_img > global_thresh
@@ -33,10 +33,10 @@ ax[0].imshow(original)
 ax[0].set_title("Original")
 ax[1].imshow(hsv_img)
 ax[1].set_title("HSV image")
-ax[2].hist(hue_img.ravel(),512)
+ax[2].hist(hue_img.ravel(), 512)
 ax[2].set_title("Histogram of Hue values with threshold")
 ax[2].axvline(x=global_thresh, color='r', linestyle='dashed', linewidth=2)
-ax[2].set_xbound(0,0.12)
+ax[2].set_xbound(0, 0.12)
 ax[3].imshow(binary_global)
 ax[3].set_title("Binary image")
 
