b0a53e86442819404e7a63f34ee439271da380ee
shcrela <shcrela@gmail.com>
2018-12-18 15:11:40 +0100

    put the phrase in plural

--- skimage/feature/peak.py
+++ skimage/feature/peak.py
@@ -56,8 +56,8 @@ def peak_local_max(image, min_distance=1, threshold_abs=None,
         If nonzero int, `exclude_border` excludes peaks from
         within `exclude_border`-pixels of the border of the image.
 	If True, takes the `min_distance` parametar as value.
-	If zero or False, the peak is identified regardless of its'
-	distance from the border.
+	If zero or False, peaks are identified regardless of their
+	distance from the border
     indices : bool, optional
         If True, the output will be an array representing peak
         coordinates.  If False, the output will be a boolean array shaped as
