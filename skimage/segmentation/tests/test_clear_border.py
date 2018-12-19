475457fb0ccecbad12cb67f058b3dcb1e7329b14
pnaylor <peter.naylor@mines-paristech.fr>
2018-12-19 10:48:05 +0100

    added an usage example in the docstring and also a small test for the mask

--- skimage/segmentation/tests/test_clear_border.py
+++ skimage/segmentation/tests/test_clear_border.py
@@ -28,6 +28,17 @@ def test_clear_border():
     result = clear_border(image.copy(), buffer_size=1, bgval=2)
     assert_array_equal(result, 2 * np.ones_like(image))
 
+    # test mask
+    mask = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [1, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [1, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [1, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [1, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [1, 1, 1, 1, 1, 1, 1, 1, 1]]).astype(np.bool)
+    result = clear_border(image.copy(), mask=mask)
+    ref = image.copy()
+    ref[2, 0] = 0
+    assert_array_equal(result, ref)
 
 def test_clear_border_3d():
     image = np.array([
