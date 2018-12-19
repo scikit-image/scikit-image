840a0fbc44e4aa7e7353e92944ec60525468cd00
François Boulogne <devel@sciunto.org>
2018-12-19 10:10:28 +0100

    Add top hat example to gallery

---
+++ doc/examples/filters/plot_tophat.py
@@ -0,0 +1,28 @@
+"""
+=======
+Top hat
+=======
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from skimage import data
+from skimage import morphology
+
+image = color.rgb2gray(data.hubble_deep_field())[:500, :500]
+
+selem =  morphology.disk(1)
+res = morphology.white_tophat(image, selem)
+
+fig, ax = plt.subplots(ncols=3, figsize=(20, 8))
+ax[0].set_title('Original')
+ax[0].imshow(image, cmap='gray')
+ax[1].set_title('White tophat')
+ax[1].imshow(res, cmap='gray')
+ax[2].set_title('Complementary')
+ax[2].imshow(image - res, cmap='gray')
+
+plt.show()
+
