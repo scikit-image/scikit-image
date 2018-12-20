bfcf98411933690f774c5f7b951c462c0f45ce62
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-12-20 11:13:51 +0100

    address stefan comments

--- skimage/filters/tests/test_median.py
+++ skimage/filters/tests/test_median.py
@@ -20,14 +20,14 @@ def image():
 
 @pytest.mark.parametrize(
     "mask, shift_x, shift_y, mode, cval, behavior, n_warning, warning_type",
-    [(True, None, None, 'nearest', 0.0, 'new', 1, (UserWarning,)),
-     (None, 1, None, 'nearest', 0.0, 'new', 1, (UserWarning,)),
-     (None, None, 1, 'nearest', 0.0, 'new', 1, (UserWarning,)),
-     (True, 1, 1, 'nearest', 0.0, 'new', 1, (UserWarning,)),
-     (None, False, False, 'constant', 0.0, 'old', 2, (DeprecationWarning,
+    [(True, None, None, 'nearest', 0.0, 'ndimage', 1, (UserWarning,)),
+     (None, 1, None, 'nearest', 0.0, 'ndimage', 1, (UserWarning,)),
+     (None, None, 1, 'nearest', 0.0, 'ndimage', 1, (UserWarning,)),
+     (True, 1, 1, 'nearest', 0.0, 'ndimage', 1, (UserWarning,)),
+     (None, False, False, 'constant', 0.0, 'rank', 2, (DeprecationWarning,
                                                       UserWarning,)),
-     (None, False, False, 'nearest', 0.0, 'old', 1, (DeprecationWarning,)),
-     (None, False, False, 'nearest', 0.0, 'new', 0, [])]
+     (None, False, False, 'nearest', 0.0, 'rank', 1, (DeprecationWarning,)),
+     (None, False, False, 'nearest', 0.0, 'ndimage', 0, [])]
 )
 def test_median_warning(image, mask, shift_x, shift_y, mode, cval, behavior,
                         n_warning, warning_type):
@@ -45,8 +45,8 @@ def test_median_warning(image, mask, shift_x, shift_y, mode, cval, behavior,
 
 @pytest.mark.parametrize(
     "behavior, func, params",
-    [('new', ndimage.median_filter, {'size': (3, 3)}),
-     ('old', rank.median, {'selem': np.ones((3, 3), dtype=np.uint8)})]
+    [('ndimage', ndimage.median_filter, {'size': (3, 3)}),
+     ('rank', rank.median, {'selem': np.ones((3, 3), dtype=np.uint8)})]
 )
 @pytest.mark.filterwarnings("ignore:Default 'behavior' will change")
 def test_median_behavior(image, behavior, func, params):
