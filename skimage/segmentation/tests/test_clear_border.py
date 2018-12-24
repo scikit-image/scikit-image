c7658c8a9418c97e94b5c3008707f2dd01305461
pnaylor <peter.naylor@mines-paristech.fr>
2018-12-24 18:51:01 +0100

    small tweak to docstring to show that it is just not a 1-pixel wide removal along the borders

--- skimage/segmentation/tests/test_clear_border.py
+++ skimage/segmentation/tests/test_clear_border.py
@@ -7,17 +7,17 @@ from skimage._shared.testing import assert_array_equal, assert_
 def test_clear_border():
     image = np.array(
         [[0, 0, 0, 0, 0, 0, 0, 1, 0],
-         [0, 0, 0, 0, 1, 0, 0, 0, 0],
-         [1, 0, 0, 1, 0, 1, 0, 0, 0],
-         [0, 0, 1, 1, 1, 1, 1, 0, 0],
+         [1, 1, 0, 0, 1, 0, 0, 1, 0],
+         [1, 1, 0, 1, 0, 1, 0, 0, 0],
+         [0, 0, 0, 1, 1, 1, 1, 0, 0],
          [0, 1, 1, 1, 1, 1, 1, 1, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
     # test default case
     result = clear_border(image.copy())
     ref = image.copy()
-    ref[2, 0] = 0
-    ref[0, -2] = 0
+    ref[1:3, 0:2] = 0
+    ref[0:2, -2] = 0
     assert_array_equal(result, ref)
 
     # test buffer
@@ -29,15 +29,15 @@ def test_clear_border():
     assert_array_equal(result, 2 * np.ones_like(image))
 
     # test mask
-    mask = np.array([[0, 1, 1, 1, 1, 1, 1, 1, 1],
-                     [0, 1, 1, 1, 1, 1, 1, 1, 1],
-                     [0, 1, 1, 1, 1, 1, 1, 1, 1],
-                     [0, 1, 1, 1, 1, 1, 1, 1, 1],
-                     [0, 1, 1, 1, 1, 1, 1, 1, 1],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0]]).astype(np.bool)
+    mask = np.array([[0, 0, 1, 1, 1, 1, 1, 1, 1],
+                     [0, 0, 1, 1, 1, 1, 1, 1, 1],
+                     [1, 1, 1, 1, 1, 1, 1, 1, 1],
+                     [1, 1, 1, 1, 1, 1, 1, 1, 1],
+                     [1, 1, 1, 1, 1, 1, 1, 1, 1],
+                     [1, 1, 1, 1, 1, 1, 1, 1, 1]]).astype(np.bool)
     result = clear_border(image.copy(), mask=mask)
     ref = image.copy()
-    ref[2, 0] = 0
+    ref[1:3, 0:2] = 0
     assert_array_equal(result, ref)
 
 def test_clear_border_3d():
