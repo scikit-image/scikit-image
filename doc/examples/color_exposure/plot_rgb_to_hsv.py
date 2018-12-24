b83c0131c83704a549a69f19079377263e90aa38
Alexis CVETKOV-ILIEV <alexis.cvetkov-iliev@institutoptique.fr>
2018-12-24 17:42:27 +0100

    Changed value threshold, resized the figures to see the titles, added a plt.show() command

--- doc/examples/color_exposure/plot_rgb_to_hsv.py
+++ doc/examples/color_exposure/plot_rgb_to_hsv.py
@@ -21,20 +21,17 @@ channels.
 ##############################################################################
 # We first load the RGB image and extract the Hue and Value channels:
 
-import matplotlib
 import matplotlib.pyplot as plt
 
 from skimage import data
 from skimage.color import rgb2hsv
-
-matplotlib.rcParams['font.size'] = 12
 
 rgb_img = data.coffee()
 hsv_img = rgb2hsv(rgb_img)
 hue_img = hsv_img[:, :, 0]
 value_img = hsv_img[:, :, 2]
 
-fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, figsize=(12, 4))
+fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, figsize=(8, 2))
 
 ax0.imshow(rgb_img)
 ax0.set_title("RGB image")
@@ -55,7 +52,7 @@ fig.tight_layout()
 hue_threshold = 0.04
 binary_img = hue_img > hue_threshold
 
-fig, (ax0, ax1) = plt.subplots(ncols=2, figsize=(12, 4))
+fig, (ax0, ax1) = plt.subplots(ncols=2, figsize=(8, 3))
 
 ax0.hist(hue_img.ravel(), 512)
 ax0.set_title("Histogram of the Hue channel with threshold")
@@ -71,9 +68,9 @@ fig.tight_layout()
 # We finally perform an additional thresholding on the Value channel to partly
 # remove the shadow of the cup:
 
-fig, ax0 = plt.subplots(figsize=(6, 4))
+fig, ax0 = plt.subplots(figsize=(4, 3))
 
-value_threshold = 0.09
+value_threshold = 0.10
 binary_img = (hue_img > hue_threshold) | (value_img < value_threshold)
 
 ax0.imshow(binary_img)
@@ -81,3 +78,4 @@ ax0.set_title("Hue and value thresholded image")
 ax0.axis('off')
 
 fig.tight_layout()
+plt.show()
