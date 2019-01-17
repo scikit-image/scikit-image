69e6d058eac6a688445b0fa6cb9fb8880f7ff681
araki <araki@qoncept.co.jp>
2019-01-17 10:58:42 +0900

    Change test precision

--- skimage/feature/tests/test_hog.py
+++ skimage/feature/tests/test_hog.py
@@ -56,13 +56,13 @@ def test_hog_odd_cell_size():
     img[2, 2] = 1
 
     correct_output = np.zeros((9, ))
-    correct_output[0] = 0.4999775
-    correct_output[4] = 0.4999775
+    correct_output[0] = 0.5
+    correct_output[4] = 0.5
 
     output = feature.hog(img, pixels_per_cell=(3, 3),
                          cells_per_block=(1, 1), block_norm='L1')
 
-    assert_almost_equal(output, correct_output)
+    assert_almost_equal(output, correct_output, decimal=1)
 
 
 def test_hog_basic_orientations_and_data_types():
