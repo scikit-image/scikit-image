82f12b8a15d65f322fbb466e4a5c7e3e24e94c8b
François Boulogne <fboulogne@sciunto.org>
2019-01-24 08:31:18 +0100

    Add multichannel=True to match_histogram example (#3672)

--- doc/examples/transform/plot_histogram_matching.py
+++ doc/examples/transform/plot_histogram_matching.py
@@ -24,7 +24,7 @@ from skimage.transform import match_histograms
 reference = data.coffee()
 image = data.chelsea()
 
-matched = match_histograms(image, reference)
+matched = match_histograms(image, reference, multichannel=True)
 
 fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3),
                                     sharex=True, sharey=True)
