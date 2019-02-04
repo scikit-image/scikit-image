a0eb87d389aee164091bcc7c9aa7fbcfd90f49ff
Mark Harfouche <mark.harfouche@gmail.com>
2019-02-04 13:52:12 -0500

    Change Cython `language_level` to 3 (#3707)

--- skimage/feature/_texture.pyx
+++ skimage/feature/_texture.pyx
@@ -93,7 +93,7 @@ cdef inline int _bit_rotate_right(int value, int length) nogil:
 
 
 def _local_binary_pattern(double[:, ::1] image,
-                          int P, float R, char method='D'):
+                          int P, float R, char method=b'D'):
     """Gray scale and rotation invariant LBP (Local Binary Patterns).
 
     LBP is an invariant descriptor that can be used for texture classification.
@@ -155,7 +155,7 @@ def _local_binary_pattern(double[:, ::1] image,
                 for i in range(P):
                     texture[i] = bilinear_interpolation(&image[0, 0], rows, cols,
                                                         r + rp[i], c + cp[i],
-                                                        'C', 0)
+                                                        b'C', 0)
                 # signed / thresholded texture
                 for i in range(P):
                     if texture[i] - image[r, c] >= 0:
@@ -165,8 +165,8 @@ def _local_binary_pattern(double[:, ::1] image,
 
                 lbp = 0
 
-                # if method == 'var':
-                if method == 'V':
+                # if method == b'var':
+                if method == b'V':
                     # Compute the variance without passing from numpy.
                     # Following the LBP paper, we're taking a biased estimate
                     # of the variance (ddof=0)
@@ -181,14 +181,14 @@ def _local_binary_pattern(double[:, ::1] image,
                         lbp = var_
                     else:
                         lbp = NAN
-                # if method == 'uniform':
-                elif method == 'U' or method == 'N':
+                # if method == b'uniform':
+                elif method == b'U' or method == b'N':
                     # determine number of 0 - 1 changes
                     changes = 0
                     for i in range(P - 1):
                         changes += (signed_texture[i]
                                     - signed_texture[i + 1]) != 0
-                    if method == 'N':
+                    if method == b'N':
                         # Uniform local binary patterns are defined as patterns
                         # with at most 2 value changes (from 0 to 1 or from 1 to
                         # 0). Uniform patterns can be characterized by their
@@ -258,12 +258,12 @@ def _local_binary_pattern(double[:, ::1] image,
                         else:
                             lbp = P + 1
                 else:
-                    # method == 'default'
+                    # method == b'default'
                     for i in range(P):
                         lbp += signed_texture[i] * weights[i]
 
-                    # method == 'ror'
-                    if method == 'R':
+                    # method == b'ror'
+                    if method == b'R':
                         # shift LBP P times to the right and get minimum value
                         rotation_chain[0] = <int>lbp
                         for i in range(1, P):
