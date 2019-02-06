5c5a501cbda6875f303162414d7bc8c14fe2ad1d
Taylor D. Scott <tscott5@wisc.edu>
2019-02-05 21:23:52 -0600

    PEP8 formatting

--- skimage/feature/register_translation.py
+++ skimage/feature/register_translation.py
@@ -63,9 +63,10 @@ def _upsampled_dft(data, upsampled_region_size,
     dim_properties = list(zip(data.shape, upsampled_region_size, axis_offsets))
 
     for (n_items, ups_size, ax_offset) in dim_properties[::-1]:
-        kernel = ((np.arange(upsampled_region_size[0]) - ax_offset)[:, None] *
-                  np.fft.fftfreq(n_items, upsample_factor))
+        kernel = ((np.arange(upsampled_region_size[0]) - ax_offset)[:, None]
+                  * np.fft.fftfreq(n_items, upsample_factor))
         kernel = np.exp(-im2pi * kernel)
+
         # Equivalent to:
         #   data[i, j, k] = kernel[i, :] @ data[j, k].T
         data = np.tensordot(kernel, data, axes=(1, -1))
