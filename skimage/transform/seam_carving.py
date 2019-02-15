719506e7c3f8fdcf438048bf5e9b7b8bef8469c3
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-02-15 13:54:37 +1100

    PEP8 fix

--- skimage/transform/seam_carving.py
+++ skimage/transform/seam_carving.py
@@ -4,10 +4,13 @@ def seam_carve(*args, **kwargs):
     See https://github.com/scikit-image/scikit-image/issues/3646
     """
 
-    message = """Seam carving has been removed because it is a patented algorithm.
+    message = (
+    """Seam carving has been removed because it is a patented algorithm.
 
     See https://github.com/scikit-image/scikit-image/issues/3646
 
     This message will be removed completely in scikit-image version 0.16.
     """
+    )
+
     raise NotImplementedError(message)
