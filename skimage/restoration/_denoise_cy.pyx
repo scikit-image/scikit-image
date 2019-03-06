8bd2c973fcd169f29586f7d68cdb1ac595ce7ccf
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-03-06 12:31:13 +1100

    Backport: use fused types in denoise, warp (#3486)
    * MNT: Add a fused numeric type to make fused_types more constent.
    
    * MNT: make the interpolation use fused types for any->any type interpolation of images.
    
    * MNT: Move even more numpy function calls from Cython to Python.
    
    * MNT: More explicit type specifying
    
    * BUG: Use fused floats in denoise
    
    Pull all denoise array allocation out into python
    Function used in python should be def not cdef
    Have the correct number of arguments
    
    * MNT: Add additional type check for denoise, more docs
    
    * PEP8 fixes
    
    * Remove redundant array u and view cu
    
    * Mutate output array in Cython and trim in Python
    
    * Replace height and width with existing rows and cols
    
    * Change iteration order to match array order
    
    * Move range_lut and color_lut properly from Cy to Py
    
    * Ravel range LUT which is expected to be 1D
    
    * Update comment in denoise bilateral tests
    
    * Fix relative import in interpolation.pyx
    
    * BENCH: Benchmark for warping with many types
    
    * Rename warp benchmark file
    
    * Update pointer syntax
    
    Co-Authored-By: Mark Harfouche <mark.harfouche@gmail.com>

--- skimage/restoration/_denoise_cy.pyx
+++ skimage/restoration/_denoise_cy.pyx
@@ -8,6 +8,7 @@ import numpy as np
 from libc.math cimport exp, fabs, sqrt
 from libc.float cimport DBL_MAX
 from .._shared.interpolation cimport get_pixel3d
+from .._shared.fused_numerics cimport np_floats
 from ..util import img_as_float
 
 
@@ -57,29 +58,12 @@ cdef inline Py_ssize_t Py_ssize_t_min(Py_ssize_t value1, Py_ssize_t value2):
         return value2
 
 
-def _denoise_bilateral(image, Py_ssize_t win_size, sigma_color,
-                      double sigma_spatial, Py_ssize_t bins,
-                      mode, double cval):
-    cdef:
-        double min_value, max_value
-
-    min_value = image.min()
-    max_value = image.max()
-
-    if min_value == max_value:
-        return image
-
-    # if image.max() is 0, then dist_scale can have an unverified value
-    # and color_lut[<int>(dist * dist_scale)] may cause a segmentation fault
-    # so we verify we have a positive image and that the max is not 0.0.
-    if min_value < 0.0:
-        raise ValueError("Image must contain only positive values")
-
-    if max_value == 0.0:
-        raise ValueError("The maximum value found in the image was 0.")
-
-    image = np.atleast_3d(img_as_float(image))
-
+def _denoise_bilateral(np_floats[:, :, ::1] image, np_floats max_value,
+                       Py_ssize_t win_size, np_floats sigma_color,
+                       np_floats sigma_spatial, Py_ssize_t bins, mode,
+                       np_floats cval, np_floats[::1] color_lut,
+                       np_floats[::1] range_lut, np_floats[::1] empty_dims,
+                       np_floats[:, :, ::1] out):
     cdef:
         Py_ssize_t rows = image.shape[0]
         Py_ssize_t cols = image.shape[1]
@@ -87,19 +71,13 @@ def _denoise_bilateral(image, Py_ssize_t win_size, sigma_color,
         Py_ssize_t window_ext = (win_size - 1) / 2
         Py_ssize_t max_color_lut_bin = bins - 1
 
-        double[:, :, ::1] cimage
-        double[:, :, ::1] out
-
-        double[:] color_lut
-        double[:] range_lut
-
         Py_ssize_t r, c, d, wr, wc, kr, kc, rr, cc, pixel_addr, color_lut_bin
-        double value, weight, dist, total_weight, csigma_color, color_weight, \
+        np_floats value, weight, dist, total_weight, csigma_color, color_weight, \
                range_weight, t
-        double dist_scale
-        double[:] values
-        double[:] centres
-        double[:] total_values
+        np_floats dist_scale
+        np_floats[:] values
+        np_floats[:] centres
+        np_floats[:] total_values
 
     if sigma_color is None:
         csigma_color = image.std()
@@ -111,22 +89,18 @@ def _denoise_bilateral(image, Py_ssize_t win_size, sigma_color,
                          "`edge`, `wrap`, `symmetric` or `reflect`.")
     cdef char cmode = ord(mode[0].upper())
 
-    cimage = np.ascontiguousarray(image)
 
-    out = np.zeros((rows, cols, dims), dtype=np.double)
-    color_lut = _compute_color_lut(bins, csigma_color, max_value)
-    range_lut = _compute_range_lut(win_size, sigma_spatial)
     dist_scale = bins / dims / max_value
-    values = np.empty(dims, dtype=np.double)
-    centres = np.empty(dims, dtype=np.double)
-    total_values = np.empty(dims, dtype=np.double)
+    values = empty_dims.copy()
+    centres = empty_dims.copy()
+    total_values = empty_dims.copy()
 
     for r in range(rows):
         for c in range(cols):
             total_weight = 0
             for d in range(dims):
                 total_values[d] = 0
-                centres[d] = cimage[r, c, d]
+                centres[d] = image[r, c, d]
             for wr in range(-window_ext, window_ext + 1):
                 rr = wr + r
                 kr = wr + window_ext
@@ -138,7 +112,7 @@ def _denoise_bilateral(image, Py_ssize_t win_size, sigma_color,
                     # distance between centre stack and current position
                     dist = 0
                     for d in range(dims):
-                        value = get_pixel3d(&cimage[0, 0, 0], rows, cols, dims,
+                        value = get_pixel3d(&image[0, 0, 0], rows, cols, dims,
                                             rr, cc, d, cmode, cval)
                         values[d] = value
                         t = centres[d] - value
@@ -161,10 +135,9 @@ def _denoise_bilateral(image, Py_ssize_t win_size, sigma_color,
     return np.squeeze(np.asarray(out))
 
 
-def _denoise_tv_bregman(image, double weight, int max_iter, double eps,
-                       char isotropic):
-    image = np.atleast_3d(img_as_float(image))
-
+def _denoise_tv_bregman(np_floats[:, :, ::1] image, np_floats weight,
+                        int max_iter, np_floats eps,
+                        char isotropic, np_floats[:, :, ::1] out):
     cdef:
         Py_ssize_t rows = image.shape[0]
         Py_ssize_t cols = image.shape[1]
@@ -176,52 +149,49 @@ def _denoise_tv_bregman(image, double weight, int max_iter, double eps,
         Py_ssize_t total = rows * cols * dims
 
     shape_ext = (rows2, cols2, dims)
-    u = np.zeros(shape_ext, dtype=np.double)
 
     cdef:
-        double[:, :, ::1] cimage = np.ascontiguousarray(image)
-        double[:, :, ::1] cu = u
+        np_floats[:, :, ::1] dx = out.copy()
+        np_floats[:, :, ::1] dy = out.copy()
+        np_floats[:, :, ::1] bx = out.copy()
+        np_floats[:, :, ::1] by = out.copy()
 
-        double[:, :, ::1] dx = np.zeros(shape_ext, dtype=np.double)
-        double[:, :, ::1] dy = np.zeros(shape_ext, dtype=np.double)
-        double[:, :, ::1] bx = np.zeros(shape_ext, dtype=np.double)
-        double[:, :, ::1] by = np.zeros(shape_ext, dtype=np.double)
-
-        double ux, uy, uprev, unew, bxx, byy, dxx, dyy, s, tx, ty
+        np_floats ux, uy, uprev, unew, bxx, byy, dxx, dyy, s, tx, ty
         int i = 0
-        double lam = 2 * weight
-        double rmse = DBL_MAX
-        double norm = (weight + 4 * lam)
+        np_floats lam = 2 * weight
+        np_floats rmse = DBL_MAX
+        np_floats norm = (weight + 4 * lam)
 
-    u[1:-1, 1:-1] = image
+    out_rows, out_cols = out.shape[:2]
+    out[1:out_rows-1, 1:out_cols-1] = image
 
     # reflect image
-    u[0, 1:-1] = image[1, :]
-    u[1:-1, 0] = image[:, 1]
-    u[-1, 1:-1] = image[-2, :]
-    u[1:-1, -1] = image[:, -2]
+    out[0, 1:out_cols-1] = image[1, :]
+    out[1:out_rows-1, 0] = image[:, 1]
+    out[out_rows-1, 1:out_cols-1] = image[rows-1, :]
+    out[1:out_rows-1, out_cols-1] = image[:, cols-1]
 
     while i < max_iter and rmse > eps:
 
         rmse = 0
 
-        for k in range(dims):
-            for r in range(1, rows + 1):
-                for c in range(1, cols + 1):
+        for r in range(1, rows + 1):
+            for c in range(1, cols + 1):
+                for k in range(dims):
 
-                    uprev = cu[r, c, k]
+                    uprev = out[r, c, k]
 
                     # forward derivatives
-                    ux = cu[r, c + 1, k] - uprev
-                    uy = cu[r + 1, c, k] - uprev
+                    ux = out[r, c + 1, k] - uprev
+                    uy = out[r + 1, c, k] - uprev
 
                     # Gauss-Seidel method
                     unew = (
                         lam * (
-                            + cu[r + 1, c, k]
-                            + cu[r - 1, c, k]
-                            + cu[r, c + 1, k]
-                            + cu[r, c - 1, k]
+                            + out[r + 1, c, k]
+                            + out[r - 1, c, k]
+                            + out[r, c + 1, k]
+                            + out[r, c - 1, k]
 
                             + dx[r, c - 1, k]
                             - dx[r, c, k]
@@ -232,9 +202,9 @@ def _denoise_tv_bregman(image, double weight, int max_iter, double eps,
                             + bx[r, c, k]
                             - by[r - 1, c, k]
                             + by[r, c, k]
-                        ) + weight * cimage[r - 1, c - 1, k]
+                        ) + weight * image[r - 1, c - 1, k]
                     ) / norm
-                    cu[r, c, k] = unew
+                    out[r, c, k] = unew
 
                     # update root mean square error
                     tx = unew - uprev
@@ -275,5 +245,3 @@ def _denoise_tv_bregman(image, double weight, int max_iter, double eps,
 
         rmse = sqrt(rmse / total)
         i += 1
-
-    return np.squeeze(np.asarray(u[1:-1, 1:-1]))
