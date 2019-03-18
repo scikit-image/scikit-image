7e8b3efb563952128f4f501d36ff644a0797a341
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-03-18 18:21:54 +1100

    Fix segmentation metrics test

--- skimage/measure/tests/test_segmentation_metrics.py
+++ skimage/measure/tests/test_segmentation_metrics.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 from skimage.measure import (compare_adapted_rand_error,
-                             compare_split_variation_of_information,
                              compare_variation_of_information)
 
 from skimage._shared.testing import assert_equal, assert_almost_equal
@@ -9,9 +8,10 @@ from skimage._shared.testing import assert_equal, assert_almost_equal
 def test_vi():
     im_true = np.array([1, 2, 3, 4])
     im_test = np.array([1, 1, 8, 8])
-    assert_equal(compare_variation_of_information(im_true, im_test), 1)
+    assert_equal(np.sum(compare_variation_of_information(im_true, im_test)), 1)
 
 def test_are():
     im_true = np.array([[2, 1], [1, 2]])
     im_test = np.array([[1, 2],[3, 1]])
-    assert_almost_equal(compare_adapted_rand_error(im_true, im_test), (0.3333333, 0.5, 1.0))
+    assert_almost_equal(compare_adapted_rand_error(im_true, im_test),
+                        (0.3333333, 0.5, 1.0))
