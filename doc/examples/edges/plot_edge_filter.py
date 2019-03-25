ec4be5e8f61c0fb152a99d153e102e21137c48e3
Dominik Straub <dominikstraub@yahoo.com>
2019-03-25 23:46:45 +0100

    Add gallery example (rotational invariance)

--- doc/examples/edges/plot_edge_filter.py
+++ doc/examples/edges/plot_edge_filter.py
@@ -12,8 +12,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from skimage.data import camera
-from skimage.filters import roberts, sobel, scharr, prewitt
-
+from skimage.filters import roberts, sobel, sobel_h, sobel_v, scharr, \
+    scharr_h, scharr_v, prewitt, prewitt_v, prewitt_h, farid_v, farid_h
 
 image = camera()
 edge_roberts = roberts(image)
@@ -57,7 +57,7 @@ plt.show()
 
 x, y = np.ogrid[:100, :100]
 # Rotation-invariant image with different spatial frequencies
-img = np.exp(1j * np.hypot(x, y)**1.3 / 20.).real
+img = np.exp(1j * np.hypot(x, y) ** 1.3 / 20.).real
 
 edge_sobel = sobel(img)
 edge_scharr = scharr(img)
@@ -82,6 +82,76 @@ ax[2].set_title('Scharr - Prewitt')
 
 ax[3].imshow(diff_scharr_sobel, cmap=plt.cm.gray, vmax=max_diff)
 ax[3].set_title('Scharr - Sobel')
+
+for a in ax:
+    a.axis('off')
+
+plt.tight_layout()
+plt.show()
+
+######################################################################
+# As in the example above, we illustrate the rotational invariance
+# of the filters in this example. We see an rotationally invariant image
+# along with the angle of its analytical gradient in the top row.
+# The other two rows contain the difference between the different gradient
+# approximations (Sobel, Prewitt, Scharr & Farid) and the analytical gradient
+# Farid & Simoncelli derivative filters are most rotationally invariant, but
+# require a 5x5 kernel for this advantage to unfold.
+#
+# .. [1] https://en.wikipedia.org/wiki/Sobel_operator#Alternative_operators
+#
+# .. [2] Farid, Hany, and Eero P. Simoncelli. "Differentiation of discrete
+#     multidimensional signals."
+#     IEEE Transactions on image processing 13.4 (2004): 496-508.
+#
+# .. [3] https://en.wikipedia.org/wiki/Image_derivatives
+# #Farid_and_Simoncelli_Derivatives
+
+
+x, y = np.mgrid[-10:10:255j, -10:10:255j]
+img = np.sin(x ** 2 + y ** 2)
+
+imgx = 2 * x * np.cos(x ** 2 + y ** 2)
+imgy = 2 * y * np.cos(x ** 2 + y ** 2)
+
+angle = lambda dx, dy: np.mod(np.arctan2(dy, dx), np.pi)
+true_angle = angle(imgx, imgy)
+
+angle_farid = angle(farid_h(img), farid_v(img))
+angle_sobel = angle(sobel_h(img), sobel_v(img))
+angle_scharr = angle(scharr_h(img), scharr_v(img))
+angle_prewitt = angle(prewitt_h(img), prewitt_v(img))
+
+diff_farid = np.abs(true_angle - angle_farid)
+diff_sobel = np.abs(true_angle - angle_sobel)
+diff_scharr = np.abs(true_angle - angle_scharr)
+diff_prewitt = np.abs(true_angle - angle_prewitt)
+
+fig, axes = plt.subplots(nrows=3, ncols=2, sharex=True, sharey=True,
+                         figsize=(8, 8))
+ax = axes.ravel()
+
+ax[0].imshow(img, cmap=plt.cm.gray)
+ax[0].set_title('Original image')
+
+ax[1].imshow(true_angle, cmap=plt.cm.hsv)
+ax[1].set_title('Analytical gradient angle')
+
+ax[2].imshow(diff_sobel, cmap=plt.cm.jet, vmin=0, vmax=0.02)
+ax[2].set_title('Sobel')
+
+ax[3].imshow(diff_prewitt, cmap=plt.cm.jet, vmin=0, vmax=0.02)
+ax[3].set_title('Prewitt')
+
+ax[4].imshow(diff_scharr, cmap=plt.cm.jet, vmin=0, vmax=0.02)
+ax[4].set_title('Scharr')
+
+cax = ax[5].imshow(diff_farid, cmap=plt.cm.jet, vmin=0, vmax=0.02)
+ax[5].set_title('Farid')
+
+fig.subplots_adjust(right=0.8)
+cbar_ax = fig.add_axes([0.85, 0.10, 0.05, 0.50])
+fig.colorbar(cax, cax=cbar_ax)
 
 for a in ax:
     a.axis('off')
