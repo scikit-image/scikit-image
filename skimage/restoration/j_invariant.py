163b2c090582a1cf47d278909c18443f2531fc9b
Joshua Batson <joshua.batson@czbiohub.org>
2019-03-29 16:08:41 -0700

    Keep dimensions.

--- skimage/restoration/j_invariant.py
+++ skimage/restoration/j_invariant.py
@@ -5,46 +5,45 @@ from ..measure import compare_mse
 from ..util import img_as_float
 
 
-def interpolate_image(x, multichannel=False):
-    spatialdims = x.ndim if not multichannel else x.ndim - 1
+def interpolate_image(image, multichannel=False):
+    spatialdims = image.ndim if not multichannel else image.ndim - 1
     conv_filter = ndi.generate_binary_structure(spatialdims, 1)
     conv_filter.ravel()[conv_filter.size // 2] = 0
     conv_filter /= conv_filter.sum()
 
     if multichannel:
-        x_interp = np.zeros(x.shape)
-        for i in range(x.shape[-1]):
-            x_interp[..., i] = ndi.convolve(x[..., i], conv_filter, mode='reflect')
+        interp = np.zeros(image.shape)
+        for i in range(image.shape[-1]):
+            interp[..., i] = ndi.convolve(image[..., i], conv_filter, mode='reflect')
     else:
-        x_interp = ndi.convolve(x, conv_filter, mode='reflect')
-    return x_interp
+        interp = ndi.convolve(image, conv_filter, mode='reflect')
+    return interp
 
 
 def generate_mask(shape, idx, stride=3):
-    m = np.zeros(shape)
+    phases = np.unravel_index(idx, (stride,) * len(shape))
+    mask = tuple(slice(p, None, stride) for p in phases)
 
-    phasex = idx % stride
-    phasey = (idx // stride) % stride
-
-    m[phasex::stride, phasey::stride] = 1
-    return m
+    return mask
 
 
-def invariant_denoise(image, denoise_function, denoiser_kwargs, stride):
-    n_masks = stride * stride
-
-    interp = interpolate_image(image)
-
-    output = np.zeros(image.shape)
-
+def invariant_denoise(image, denoise_function, *, stride=4, multichannel=False, masks=None, denoiser_kwargs=None):
+    image = img_as_float(image)
     if denoiser_kwargs is None:
         denoiser_kwargs = {}
 
-    for i in range(n_masks):
-        m = generate_mask(image.shape, i, stride=stride)
-        input_image = m*interp + (1 - m)*image
-        input_image = input_image.astype(image.dtype)
-        output += m*denoise_function(input_image, **denoiser_kwargs)
+    interp = interpolate_image(image, multichannel=multichannel)
+    output = np.zeros(image.shape)
+
+    if masks is None:
+        spatialdims = image.ndim if not multichannel else image.ndim - 1
+        n_masks = stride ** spatialdims
+        masks = (generate_mask(image.shape[:spatialdims], idx, stride=stride) for idx in range(n_masks))
+
+    for mask in masks:
+        input_image = image
+        input_image[mask] = interp[mask]
+        output[mask] = denoise_function(input_image, **denoiser_kwargs)[mask]
     return output
 
 
@@ -54,26 +53,28 @@ def selections_from_dict(dictionary):
         yield dict(zip(keys, element))
 
 
-def calibrate_denoiser(image, denoising_function, parameter_ranges,
-                       stride=4, full_loss=False):
+def calibrate_denoiser(image, denoise_function, parameter_ranges, *,
+                       stride=4, multichannel=False, approximate_loss=True):
     image = img_as_float(image)
     parameters_tested = list(selections_from_dict(parameter_ranges))
     losses = []
 
-    for parameters in parameters_tested:
-        if full_loss:
-            denoised = invariant_denoise(image, denoising_function, parameters, stride)
+    for denoiser_kwargs in parameters_tested:
+        if not approximate_loss:
+            denoised = invariant_denoise(image, denoise_function, 
+                                         stride=stride, multichannel=multichannel,
+                                         denoiser_kwargs=denoiser_kwargs)
             loss = compare_mse(denoised, image)
         else:
-            n_masks = stride * stride
+            spatialdims = image.ndim if not multichannel else image.ndim - 1
+            n_masks = stride ** spatialdims
+            mask = generate_mask(image.shape[:spatialdims], n_masks//2, stride=stride)
 
-            m = generate_mask(image.shape, n_masks // 2, stride=stride)
-            interp = interpolate_image(image)
+            masked_denoised = invariant_denoise(image, denoise_function,
+                                         masks=[mask], multichannel=multichannel,
+                                         denoiser_kwargs=denoiser_kwargs)
 
-            input_image = m*interp + (1 - m)*image
-            input_image = input_image.astype(image.dtype)
-            masked_denoised = m*denoising_function(input_image, **parameters)
-            loss = n_masks * compare_mse(masked_denoised, m*image)
+            loss = compare_mse(masked_denoised[mask], image[mask])
 
         losses.append(loss)
 
@@ -81,6 +82,8 @@ def calibrate_denoiser(image, denoising_function, parameter_ranges,
     best_loss = losses[idx]
     best_parameters = parameters_tested[idx]
 
-    denoised = invariant_denoise(image, denoising_function, best_parameters, stride)
+    denoised = invariant_denoise(image, denoise_function,
+                                 stride=stride, multichannel=multichannel,
+                                 denoiser_kwargs=best_parameters)
 
     return denoised, best_parameters, best_loss, parameters_tested, losses
