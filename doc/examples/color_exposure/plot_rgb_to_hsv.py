30d5f978f0b63e9eb488dc801e2ed4755ca0791c
Emmanuelle Gouillart <emmanuelle.gouillart@nsup.org>
2019-03-30 15:13:40 +0100

    "Removed colon"

--- doc/examples/color_exposure/plot_rgb_to_hsv.py
+++ doc/examples/color_exposure/plot_rgb_to_hsv.py
@@ -16,7 +16,7 @@ channels.
 
 .. [1] https://en.wikipedia.org/wiki/HSL_and_HSV
 
-We first load the RGB image and extract the Hue and Value channels:
+We first load the RGB image and extract the Hue and Value channels.
 """
 import matplotlib.pyplot as plt
 from skimage import data
