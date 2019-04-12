417b8d612cd68ee5aac8d767be0ce1342a6a62ee
Stefan van der Walt <stefanv@berkeley.edu>
2019-04-12 09:55:05 -0700

    Force imageio reader to return NumPy arrays

--- skimage/io/_plugins/imageio_plugin.py
+++ skimage/io/_plugins/imageio_plugin.py
@@ -1,3 +1,15 @@
 __all__ = ['imread', 'imsave']
 
-from imageio import imread, imsave
+from functools import wraps
+import numpy as np
+from imageio import imread as imageio_imread, imsave
+
+
+def array_return(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return np.asarray(f(*args, **kwargs))
+    return wrapper
+
+
+imread = array_return(imageio_imread)
