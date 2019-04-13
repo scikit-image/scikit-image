30c84636e32d1c8a1102ba7d337a3fbf143b064b
Mark Harfouche <mark.harfouche@gmail.com>
2019-04-13 00:11:54 -0400

    simplify the implementation of imageio_imread as np.array

--- skimage/io/_plugins/imageio_plugin.py
+++ skimage/io/_plugins/imageio_plugin.py
@@ -4,12 +4,6 @@ from functools import wraps
 import numpy as np
 from imageio import imread as imageio_imread, imsave
 
-
-def array_return(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        return np.asarray(f(*args, **kwargs))
-    return wrapper
-
-
-imread = array_return(imageio_imread)
+@wraps(imageio_imread)
+def imread(*args, **kwargs):
+    return np.asarray(imageio_imread(*args, **kwargs))
