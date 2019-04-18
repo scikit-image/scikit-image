9453e9d80bd611a5c955ac5759e26cb39d0610af
Riadh <r.fezzani@vitadx.com>
2019-04-18 15:28:52 +0200

    Add tests for SLIC mask option

--- skimage/segmentation/tests/test_slic.py
+++ skimage/segmentation/tests/test_slic.py
@@ -1,6 +1,7 @@
 from itertools import product
 
 import numpy as np
+import pytest
 from skimage.segmentation import slic
 
 from skimage._shared import testing
@@ -210,3 +211,248 @@ def test_more_segments_than_pixels():
     seg = slic(img, sigma=0, n_segments=500, compactness=1,
                multichannel=False, convert2lab=False)
     assert np.all(seg.ravel() == np.arange(seg.size))
+
+
+def test_color_2d_mask():
+    rnd = np.random.RandomState(0)
+    msk = np.zeros((20, 21))
+    msk[2:-2, 2:-2] = 1
+    img = np.zeros((20, 21, 3))
+    img[:10, :10, 0] = 1
+    img[10:, :10, 1] = 1
+    img[10:, 10:, 2] = 1
+    img += 0.01 * rnd.normal(size=img.shape)
+    np.clip(img, 0, 1, out=img)
+    seg = slic(img, n_segments=4, sigma=0, enforce_connectivity=False,
+               mask=msk)
+
+    # we expect 4 segments + non masked area
+    assert_equal(len(np.unique(seg)), 5)
+    assert_equal(seg.shape, img.shape[:-1])
+    # segments
+    assert_equal(seg[2:10, 2:10], 1)
+    assert_equal(seg[10:-2, 2:10], 3)
+    assert_equal(seg[2:10, 10:-2], 2)
+    assert_equal(seg[10:-2, 10:-2], 0)
+    # non masked area
+    assert_equal(seg[:2, :], -1)
+    assert_equal(seg[-2:, :], -1)
+    assert_equal(seg[:, :2], -1)
+    assert_equal(seg[:, -2:], -1)
+
+
+def test_multichannel_2d_mask():
+    rnd = np.random.RandomState(0)
+    msk = np.zeros((20, 20))
+    msk[2:-2, 2:-2] = 1
+    img = np.zeros((20, 20, 8))
+    img[:10, :10, 0:2] = 1
+    img[:10, 10:, 2:4] = 1
+    img[10:, :10, 4:6] = 1
+    img[10:, 10:, 6:8] = 1
+    img += 0.01 * rnd.normal(size=img.shape)
+    np.clip(img, 0, 1, out=img)
+    seg = slic(img, n_segments=4, enforce_connectivity=False,
+               mask=msk)
+
+    # we expect 4 segments + non masked area
+    assert_equal(len(np.unique(seg)), 5)
+    assert_equal(seg.shape, img.shape[:-1])
+    # segments
+    assert_equal(seg[2:10, 2:10], 1)
+    assert_equal(seg[10:-2, 2:10], 3)
+    assert_equal(seg[2:10, 10:-2], 2)
+    assert_equal(seg[10:-2, 10:-2], 0)
+    # non masked area
+    assert_equal(seg[:2, :], -1)
+    assert_equal(seg[-2:, :], -1)
+    assert_equal(seg[:, :2], -1)
+    assert_equal(seg[:, -2:], -1)
+
+
+def test_gray_2d_mask():
+    rnd = np.random.RandomState(0)
+    msk = np.zeros((20, 21))
+    msk[2:-2, 2:-2] = 1
+    img = np.zeros((20, 21))
+    img[:10, :10] = 0.33
+    img[10:, :10] = 0.67
+    img[10:, 10:] = 1.00
+    img += 0.0033 * rnd.normal(size=img.shape)
+    np.clip(img, 0, 1, out=img)
+    seg = slic(img, sigma=0, n_segments=4, compactness=1,
+               multichannel=False, convert2lab=False, mask=msk)
+
+    assert_equal(len(np.unique(seg)), 5)
+    assert_equal(seg.shape, img.shape)
+    # segments
+    assert_equal(seg[2:10, 2:10], 0)
+    assert_equal(seg[10:-2, 2:10], 2)
+    assert_equal(seg[2:10, 10:-2], 1)
+    assert_equal(seg[10:-2, 10:-2], 3)
+    # non masked area
+    assert_equal(seg[:2, :], -1)
+    assert_equal(seg[-2:, :], -1)
+    assert_equal(seg[:, :2], -1)
+    assert_equal(seg[:, -2:], -1)
+
+
+def test_list_sigma_mask():
+    rnd = np.random.RandomState(0)
+    msk = np.zeros((2, 6))
+    msk[:, 1:-1] = 1
+    img = np.array([[1, 1, 1, 0, 0, 0],
+                    [0, 0, 0, 1, 1, 1]], np.float)
+    img += 0.1 * rnd.normal(size=img.shape)
+    result_sigma = np.array([[-1, 0, 0, 1, 1, -1],
+                             [-1, 0, 0, 1, 1, -1]], np.int)
+    seg_sigma = slic(img, n_segments=2, sigma=[1, 50, 1],
+                     multichannel=False, mask=msk)
+    assert_equal(seg_sigma, result_sigma)
+
+
+def test_spacing_mask():
+    rnd = np.random.RandomState(0)
+    msk = np.zeros((2, 5))
+    msk[:, 1:-1] = 1
+    img = np.array([[1, 1, 1, 0, 0],
+                    [1, 1, 0, 0, 0]], np.float)
+    result_non_spaced = np.array([[-1, 0, 0, 1, -1],
+                                  [-1, 0, 1, 1, -1]], np.int)
+    result_spaced = np.array([[-1, 0, 0, 0, -1],
+                              [-1, 1, 1, 1, -1]], np.int)
+    img += 0.1 * rnd.normal(size=img.shape)
+    seg_non_spaced = slic(img, n_segments=2, sigma=0, multichannel=False,
+                          compactness=1.0, mask=msk)
+    seg_spaced = slic(img, n_segments=2, sigma=0, spacing=[1, 500, 1],
+                      compactness=1.0, multichannel=False, mask=msk)
+    assert_equal(seg_non_spaced, result_non_spaced)
+    assert_equal(seg_spaced, result_spaced)
+
+
+def test_enforce_connectivity_mask():
+    msk = np.zeros((3, 6))
+    msk[:, 1:-1] = 1
+    img = np.array([[0, 0, 0, 1, 1, 1],
+                    [1, 0, 0, 1, 1, 0],
+                    [0, 0, 0, 1, 1, 0]], np.float)
+
+    segments_connected = slic(img, 2, compactness=0.0001,
+                              enforce_connectivity=True,
+                              convert2lab=False, mask=msk)
+    segments_disconnected = slic(img, 2, compactness=0.0001,
+                                 enforce_connectivity=False,
+                                 convert2lab=False, mask=msk)
+
+    # Make sure nothing fatal occurs (e.g. buffer overflow) at low values of
+    # max_size_factor
+    segments_connected_low_max = slic(img, 2, compactness=0.0001,
+                                      enforce_connectivity=True,
+                                      convert2lab=False,
+                                      max_size_factor=0.8, mask=msk)
+
+    result_connected = np.array([[-1, 0, 0, 1, 1, -1],
+                                 [-1, 0, 0, 1, 1, -1],
+                                 [-1, 0, 0, 1, 1, -1]], np.float)
+
+    result_disconnected = np.array([[-1, 1, 1, 0, 0, -1],
+                                    [-1, 1, 1, 0, 0, -1],
+                                    [-1, 1, 1, 0, 0, -1]], np.float)
+
+    assert_equal(segments_connected, result_connected)
+    assert_equal(segments_disconnected, result_disconnected)
+    assert_equal(segments_connected_low_max, result_connected)
+
+
+def test_slic_zero_mask():
+
+    rnd = np.random.RandomState(0)
+    msk = np.zeros((20, 21))
+    msk[2:-2, 2:-2] = 1
+    img = np.zeros((20, 21, 3))
+    img[:10, :10, 0] = 1
+    img[10:, :10, 1] = 1
+    img[10:, 10:, 2] = 1
+    img += 0.01 * rnd.normal(size=img.shape)
+    np.clip(img, 0, 1, out=img)
+    seg = slic(img, n_segments=4, sigma=0, slic_zero=True,
+               mask=msk)
+
+    # we expect 4 segments + non masked area
+    assert_equal(len(np.unique(seg)), 5)
+    assert_equal(seg.shape, img.shape[:-1])
+    # segments
+    assert_equal(seg[2:10, 2:10], 0)
+    assert_equal(seg[10:-2, 2:10], 2)
+    assert_equal(seg[2:10, 10:-2], 1)
+    assert_equal(seg[10:-2, 10:-2], 3)
+    # non masked area
+    assert_equal(seg[:2, :], -1)
+    assert_equal(seg[-2:, :], -1)
+    assert_equal(seg[:, :2], -1)
+    assert_equal(seg[:, -2:], -1)
+
+
+def test_more_segments_than_pixels_mask():
+    rnd = np.random.RandomState(0)
+    msk = np.zeros((20, 21))
+    msk[2:-2, 2:-2] = 1
+    img = np.zeros((20, 21))
+    img[:10, :10] = 0.33
+    img[10:, :10] = 0.67
+    img[10:, 10:] = 1.00
+    img += 0.0033 * rnd.normal(size=img.shape)
+    np.clip(img, 0, 1, out=img)
+    seg = slic(img, sigma=0, n_segments=500, compactness=1,
+               multichannel=False, convert2lab=False, mask=msk)
+
+    assert np.all(seg[2:-2, 2:-2].ravel() == np.arange(seg[2:-2, 2:-2].size))
+
+
+@pytest.mark.filterwarnings("ignore:SLIC")
+def test_3d_mask():
+    # The feature is not supported: user is warned and mask is ignored
+
+    msk = np.zeros((21, 22))
+    msk[2:-2, 2:-2] = 1
+
+    # test_color_3d():
+    rnd = np.random.RandomState(0)
+    img = np.zeros((20, 21, 22, 3))
+    slices = []
+    for dim_size in img.shape[:-1]:
+        midpoint = dim_size // 2
+        slices.append((slice(None, midpoint), slice(midpoint, None)))
+    slices = list(product(*slices))
+    colors = list(product(*(([0, 1],) * 3)))
+    for s, c in zip(slices, colors):
+        img[s] = c
+    img += 0.01 * rnd.normal(size=img.shape)
+    img[img > 1] = 1
+    img[img < 0] = 0
+    seg = slic(img, sigma=0, n_segments=8, mask=msk)
+
+    assert_equal(len(np.unique(seg)), 8)
+    for s, c in zip(slices, range(8)):
+        assert_equal(seg[s], c)
+
+    # test_gray_3d():
+    rnd = np.random.RandomState(0)
+    img = np.zeros((20, 21, 22))
+    slices = []
+    for dim_size in img.shape:
+        midpoint = dim_size // 2
+        slices.append((slice(None, midpoint), slice(midpoint, None)))
+    slices = list(product(*slices))
+    shades = np.arange(0, 1.000001, 1.0 / 7)
+    for s, sh in zip(slices, shades):
+        img[s] = sh
+    img += 0.001 * rnd.normal(size=img.shape)
+    img[img > 1] = 1
+    img[img < 0] = 0
+    seg = slic(img, sigma=0, n_segments=8, compactness=1,
+               multichannel=False, convert2lab=False, mask=msk)
+
+    assert_equal(len(np.unique(seg)), 8)
+    for s, c in zip(slices, range(8)):
+        assert_equal(seg[s], c)
