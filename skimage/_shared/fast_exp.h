889868db66c2a902780815485c18f156e12c1db8
Mark Harfouche <mark.harfouche@gmail.com>
2019-04-27 18:52:43 -0400

    remove one of the inline directives

--- skimage/_shared/fast_exp.h
+++ skimage/_shared/fast_exp.h
@@ -15,7 +15,7 @@
 // All these inline directives are necessary for us to stay compatible with
 // as many compilers as possible.
 // See discussion https://github.com/scikit-image/scikit-image/issues/3866
-__inline double inline __attribute__((always_inline)) fast_exp (double y)
+double inline __attribute__((always_inline)) fast_exp (double y)
 {
     union
     {
