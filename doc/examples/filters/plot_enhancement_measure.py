b6c78e4c038e02ee05cee345a8155314cbc42d26
Stefan van der Walt <sjvdwalt@gmail.com>
2019-04-29 09:01:35 -0400

    Update doc/examples/filters/plot_enhancement_measure.py
    Co-Authored-By: iliailmer <i.ilmer@icloud.com>

--- doc/examples/filters/plot_enhancement_measure.py
+++ doc/examples/filters/plot_enhancement_measure.py
@@ -6,7 +6,7 @@ Quantitative Measure of Image Enhancement
 In this example, we will quantify the effect of applying contrast enhancement
 to an image using a measure called EME [1]_.
 
-It is defined as a log of the ratio of local maximum to local minimum
+It is defined as the log of the ratio of local maximum to minimum
 <<<<<<< HEAD
 within a sliding window. The code allows the user to choose the window size.
 This function was defined to quantify improvement of the image
