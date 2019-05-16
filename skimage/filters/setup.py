10fcd0a27b94e576256bb8f7befb553ab977ad8e
Alexandre de Siqueira <alex.desiqueira@igdore.org>
2019-05-16 16:52:39 -0700

    Fixing include_dirs argument.

--- skimage/filters/setup.py
+++ skimage/filters/setup.py
@@ -22,7 +22,7 @@ def configuration(parent_package='', top_path=None):
     config.add_extension('rank.core_cy', sources=['rank/core_cy.c'],
                          include_dirs=[get_numpy_include_dirs()])
     config.add_extension('_multiotsu', sources=['_multiotsu.c'],
-                         include_dirs=[get_numpy_include_dirs])
+                         include_dirs=[get_numpy_include_dirs()])
     config.add_extension('rank.generic_cy', sources=['rank/generic_cy.c'],
                          include_dirs=[get_numpy_include_dirs()])
     config.add_extension(
