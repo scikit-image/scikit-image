90b4146556206d2f7bc8111d83d77c742d4ddf71
Alexandre de Siqueira <alex.desiqueira@igdore.org>
2019-05-17 14:25:04 -0700

    Some changes based on help comments

--- skimage/filters/_multiotsu.pyx
+++ skimage/filters/_multiotsu.pyx
@@ -36,19 +36,20 @@ def _find_threshold_multiotsu(double [:, ::1] var_btwcls,
                                 combinations(range(1, bins - 2), classes - 1)))
     sh0 = _tmp.shape[0]
     sh1 = _tmp.shape[1]
-    cdef Py_ssize_t [:, :] idx_tuples = np.zeros((sh0, sh1 + 2), 
-                                                  dtype=np.int)
+    cdef Py_ssize_t [:, ::1] idx_tuples = np.zeros((sh0, sh1 + 2),
+                                                   dtype=np.intp)
     idx_tuples[:, 1:sh1 + 1] = _tmp[:]
     idx_tuples[:, sh1 + 1] = bins - 1
-    cdef Py_ssize_t [::1] idx_tuple
+    cdef cnp.int64_t [::1] idx_tuple = np.zeros(classes-1, dtype=np.int64)
+
     for idx_tuple in idx_tuples:
         part_sigma = 0
-        for idd in range(0, classes):
+        for idd in range(classes):
             part_sigma += var_btwcls[1 + idx_tuple[idd], idx_tuple[idd+1]]
         # checking if partial sigma is higher than maximum sigma
         if max_sigma < part_sigma:
-            for idd in range(0, classes - 1):
+            for idd in range(classes - 1):
                 aux_thresh[idd] = idx_tuple[idd + 1]
-                max_sigma = part_sigma
+            max_sigma = part_sigma
 
     return aux_thresh
