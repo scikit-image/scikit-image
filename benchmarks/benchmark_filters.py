abb731b41e6d28096c71e620b059edfc0710da9e
Alexandre de Siqueira <alex.desiqueira@igdore.org>
2019-05-24 16:45:33 -0700

    Adding MultiOtsu benchmarks

--- benchmarks/benchmark_filters.py
+++ benchmarks/benchmark_filters.py
@@ -1,7 +1,7 @@
 # See "Writing benchmarks" in the asv docs for more information.
 # https://asv.readthedocs.io/en/latest/writing_benchmarks.html
 import numpy as np
-from skimage import filters
+from skimage import data, filters
 
 
 class FiltersSuite:
@@ -12,6 +12,35 @@ class FiltersSuite:
         self.image[3000:, 3000] += 0.5
 
     def time_sobel(self):
-        result = filters.sobel(self.image)
+        filters.sobel(self.image)
 
 
+class MultiOtsu(object):
+    """Benchmarks for MultiOtsu threshold."""
+    param_names = ['classes']
+    params = [(3, 5)]
+
+    def setup(self, *args):
+        self.image = data.camera()
+
+    def time_threshold_multiotsu(self, classes):
+        filters.threshold_multiotsu(self.image, classes=classes)
+
+    def peakmem_reference(self, *args):
+        """Provide reference for memory measurement with empty benchmark.
+
+        Peakmem benchmarks measure the maximum amount of RAM used by a
+        function. However, this maximum also includes the memory used
+        during the setup routine (as of asv 0.2.1; see [1]_).
+        Measuring an empty peakmem function might allow us to disambiguate
+        between the memory used by setup and the memory used by target (see
+        other ``peakmem_`` functions below).
+
+        References
+        ----------
+        .. [1]: https://asv.readthedocs.io/en/stable/writing_benchmarks.html#peak-memory
+        """
+        pass
+
+    def peakmem_threshold_multiotsu(self, classes):
+        filters.threshold_multiotsu(self.image, classes=classes)
