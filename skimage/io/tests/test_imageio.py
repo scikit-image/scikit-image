5a50a9e8de189a7ceb5cc49e115b058edcc27bc6
Mark Harfouche <mark.harfouche@gmail.com>
2019-06-12 22:51:10 -0400

    Don't import or skip imageio becaue we depend on it

--- skimage/io/tests/test_imageio.py
+++ skimage/io/tests/test_imageio.py
@@ -9,10 +9,6 @@ from skimage._shared import testing
 from skimage._shared.testing import (assert_array_almost_equal, TestCase,
                                      expected_warnings)
 
-from pytest import importorskip
-
-importorskip('imageio')
-
 
 def setup():
     use_plugin('imageio')
