c9f049ba15ad8a7a9a237c622617f8831a566b16
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-06-14 01:21:25 +1000

    Update developer example

--- doc/examples/developers/plot_threshold_li.py
+++ doc/examples/developers/plot_threshold_li.py
@@ -92,13 +92,82 @@ plt.show()
 
 ###############################################################################
 # This is clearly much more efficient than the brute force approach. However,
-# in some images, the gradient of the cross-entropy becomes very shallow, and
-# the optimization can progress much more slowly.
+# in some images, the cross-entropy is not *convex*, meaning having a single
+# optimum. In this case, gradient descent could yield a threshold that is not
+# optimal. In this example, we see how a bad initial guess for the optimization
+# results in a poor threshold selection.
 
 iter_thresholds2 = []
 
+opt_threshold2 = filters.threshold_li(cell, initial_guess=64,
+                                      iter_callback=iter_thresholds2.append)
 
-def grad(image, t):
+thresholds2 = np.arange(np.min(cell) + 1.5, np.max(cell) - 1.5)
+entropies2 = [_cross_entropy(cell, t) for t in thresholds]
+iter_entropies2 = [_cross_entropy(cell, t) for t in iter_thresholds2]
+
+fig, ax = plt.subplots(1, 3, figsize=(8, 3))
+
+ax[0].imshow(cell, cmap='magma')
+ax[0].set_title('image')
+ax[0].set_axis_off()
+
+ax[1].imshow(cell > opt_threshold2, cmap='gray')
+ax[1].set_title('thresholded')
+ax[1].set_axis_off()
+
+ax[2].plot(thresholds2, entropies2, label='all threshold entropies')
+ax[2].plot(iter_thresholds2, iter_entropies2, label='optimization path')
+ax[2].scatter(iter_thresholds2, iter_entropies2, c='C1')
+ax[2].legend(loc='upper right')
+
+plt.show()
+
+###############################################################################
+# In this image, amazingly, the *default* initial guess, the mean image value,
+# actually lies *right* on top of the peak between the two "valleys" of the
+# objective function. Without supplying an initial guess, Li's thresholding
+# method does nothing at all!
+
+iter_thresholds3 = []
+
+opt_threshold3 = filters.threshold_li(cell,
+                                      iter_callback=iter_thresholds3.append)
+
+iter_entropies3 = [_cross_entropy(cell, t) for t in iter_thresholds3]
+
+fig, ax = plt.subplots(1, 3, figsize=(8, 3))
+
+ax[0].imshow(cell, cmap='magma')
+ax[0].set_title('image')
+ax[0].set_axis_off()
+
+ax[1].imshow(cell > opt_threshold3, cmap='gray')
+ax[1].set_title('thresholded')
+ax[1].set_axis_off()
+
+ax[2].plot(thresholds2, entropies2, label='all threshold entropies')
+ax[2].plot(iter_thresholds3, iter_entropies3, label='optimization path')
+ax[2].scatter(iter_thresholds3, iter_entropies3, c='C1')
+ax[2].legend(loc='upper right')
+
+plt.show()
+
+###############################################################################
+# To see what is going on, let's define a function, ``li_gradient``, that
+# replicates the inner loop of the Li method and returns the *change* from the
+# current threshold value to the next one. When this gradient is 0, we are at
+# a so-called *stationary point* and Li returns this value. When it is
+# negative, the next threshold guess will be lower, and when it is positive,
+# the next guess will be higher.
+#
+# In the plot below, we show the cross-entropy and the Li update path when the
+# initial guess is on the *right* side of that entropy peak. We overlay the
+# threshold update gradient, marking the 0 gradient line and the default
+# initial guess by ``threshold_li``.
+
+def li_gradient(image, t):
+    """Find the threshold update at a given threshold."""
     foreground = image > t
     mean_fore = np.mean(image[foreground])
     mean_back = np.mean(image[~foreground])
@@ -108,19 +177,18 @@ def grad(image, t):
     return dt
 
 
-opt_threshold2 = filters.threshold_li(cell, tolerance=0.25,
-                                      iter_callback=iter_thresholds2.append)
-iter_entropies2 = [_cross_entropy(cell, t) for t in iter_thresholds2]
-print(len(iter_thresholds2), 'examined, optimum:', opt_threshold2)
+iter_thresholds4 = []
+opt_threshold4 = filters.threshold_li(cell, initial_guess=68,
+                                      iter_callback=iter_thresholds4.append)
+iter_entropies4 = [_cross_entropy(cell, t) for t in iter_thresholds4]
+print(len(iter_thresholds4), 'examined, optimum:', opt_threshold4)
 
-thresholds2 = np.arange(np.min(cell) + 1.5, np.max(cell) - 1.5)
-entropies2 = [_cross_entropy(cell, t) for t in thresholds2]
-gradients = [grad(cell, t) for t in thresholds2]
+gradients = [li_gradient(cell, t) for t in thresholds2]
 
 fig, ax1 = plt.subplots()
 ax1.plot(thresholds2, entropies2)
-ax1.plot(iter_thresholds2, iter_entropies2)
-ax1.scatter(iter_thresholds2, iter_entropies2, c='C1')
+ax1.plot(iter_thresholds4, iter_entropies4)
+ax1.scatter(iter_thresholds4, iter_entropies4, c='C1')
 ax1.set_xlabel('threshold')
 ax1.set_ylabel('cross entropy', color='C0')
 ax1.tick_params(axis='y', labelcolor='C0')
@@ -135,5 +203,31 @@ ax2.set_ylabel('$\Delta$(threshold)', color='C3')
 ax2.tick_params(axis='y', labelcolor='C3')
 
 fig.tight_layout()
+
+plt.show()
+
+###############################################################################
+# In addition to allowing users to provide a number as an initial guess,
+# :func:`skimage.filters.threshold_li` can receive a function that makes a
+# guess from the image intensities, just like :func:`numpy.mean` does by
+# default. This might be a good option when many images with different ranges
+# need to be processed.
+
+def quantile_95(image):
+    return np.quantile(image, 0.95)
+
+iter_thresholds5 = []
+opt_threshold5 = filters.threshold_li(cell, initial_guess=quantile_95,
+                                      iter_callback=iter_thresholds5.append)
+iter_entropies5 = [_cross_entropy(cell, t) for t in iter_thresholds5]
+print(len(iter_thresholds5), 'examined, optimum:', opt_threshold5)
+
+fig, ax1 = plt.subplots()
+ax1.plot(thresholds2, entropies2)
+ax1.plot(iter_thresholds5, iter_entropies5)
+ax1.scatter(iter_thresholds5, iter_entropies5, c='C1')
+ax1.set_xlabel('threshold')
+ax1.set_ylabel('cross entropy', color='C0')
+ax1.tick_params(axis='y', labelcolor='C0')
 
 plt.show()
