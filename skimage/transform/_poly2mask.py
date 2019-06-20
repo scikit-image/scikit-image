e838e32f6ee3798f0f82ede73bcde54af7a74606
Hadrien Mary <hadrien.mary@gmail.com>
2019-06-20 16:26:22 -0400

    Remove matplotlib backend.

--- skimage/transform/_poly2mask.py
+++ skimage/transform/_poly2mask.py
@@ -1,10 +1,9 @@
 import numpy as np
-import matplotlib
 
 from .. import draw
 
 
-def poly2mask(image_shape, polygon, backend='default'):
+def poly2mask(image_shape, polygon):
     """Compute a mask from polygon.
 
     Parameters
@@ -14,40 +13,16 @@ def poly2mask(image_shape, polygon, backend='default'):
     polygon : array_like.
         The polygon coordinates of shape (N, 2) where N is
         the number of points.
-    backend : 'default' or 'matplotlib'.
-        The default implementation uses `skimage.draw`, while the
-        'matplotlib' implementtion uses `maplotlib.path.Path`.
-        While note exactly the same both methods give near-identical results.
 
     Returns
     -------
     mask : 2-D ndarray of type 'bool'.
         The mask that corresponds to the input polygon.
     """
-    if backend == 'default':
-        return _poly2mask_skimage(image_shape, polygon)
-    elif backend == 'matplotlib':
-        return _poly2mask_mpl(image_shape, polygon)
-
-    raise ValueError(
-        "Wrong backend selected. Choose either 'default' or 'matplotlib'.")
-
-
-def _poly2mask_skimage(image_shape, polygon):
-    polygon = np.array(polygon)
+    polygon = np.asarray(polygon)
+    vertex_row_coords, vertex_col_coords = polygon.T
     fill_row_coords, fill_col_coords = draw.polygon(
-        polygon[:, 1], polygon[:, 0], image_shape)
+        vertex_row_coords, vertex_col_coords, image_shape)
     mask = np.zeros(image_shape, dtype=np.bool)
     mask[fill_row_coords, fill_col_coords] = True
-    return mask
-
-
-def _poly2mask_mpl(image_shape, polygon):
-    polygon = np.array(polygon)
-    xx, yy = np.meshgrid(np.arange(image_shape[1]), np.arange(image_shape[0]))
-    xx, yy = xx.flatten(), yy.flatten()
-    indices = np.vstack((xx, yy)).T
-    mask = matplotlib.path.Path(polygon).contains_points(indices)
-    mask = mask.reshape(image_shape)
-    mask = mask.astype(np.bool)
     return mask
--- skimage/transform/tests/test_poly2mask.py
+++ skimage/transform/tests/test_poly2mask.py
@@ -8,13 +8,7 @@ polygon = np.array([[80, 111, 146, 234, 407, 300, 187, 45],
                     [465, 438, 499, 380, 450, 287, 210, 167]]).T
 
 
-def test_poly2mask_default():
-    mask = transform.poly2mask(image_shape, polygon, backend='default')
+def test_poly2mask():
+    mask = transform.poly2mask(image_shape, polygon)
     assert mask.shape == image_shape
     assert mask.sum() == 57647
-
-
-def test_poly2mask_matplotlib():
-    mask = transform.poly2mask(image_shape, polygon, backend='matplotlib')
-    assert mask.shape == image_shape
-    assert mask.sum() == 57650
