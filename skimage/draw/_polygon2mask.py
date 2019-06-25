7014141ebd77690acea37423e95cf5d811563de7
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-06-25 16:07:09 +1000

    Minor fixes to new polygon2mask module
    - import from local module instead of loading full skimage.draw module.
    - remove unnecessary imports from doctests, which are executed in local
      namespace.

--- skimage/draw/_polygon2mask.py
+++ skimage/draw/_polygon2mask.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from .. import draw
+from . import draw
 
 
 def polygon2mask(image_shape, polygon):
@@ -26,8 +26,6 @@ def polygon2mask(image_shape, polygon):
 
     Examples
     --------
-    >>> import numpy as np
-    >>> from skimage.draw import polygon2mask
     >>> image_shape = (128, 128)
     >>> polygon = np.array([[60, 100], [100, 40], [40, 40]])
     >>> mask = polygon2mask(image_shape, polygon)
