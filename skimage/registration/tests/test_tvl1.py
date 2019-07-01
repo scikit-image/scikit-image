1fb3d983be2b5f6a7cd152728502cc788d132fee
Riadh <r.fezzani@vitadx.com>
2019-07-01 10:29:52 +0200

    Add error handeling UT

--- skimage/registration/tests/test_tvl1.py
+++ skimage/registration/tests/test_tvl1.py
@@ -1,4 +1,5 @@
 import numpy as np
+from skimage._shared import testing
 from skimage.registration import tvl1
 
 
@@ -9,4 +10,20 @@ def test_no_motion():
     u, v = tvl1(img, img)
 
     assert np.all(u == 0)
-    assert np.all(u == 0)
+    assert np.all(v == 0)
+
+
+def test_wrong_ndim():
+    rnd = np.random.RandomState(0)
+    I0 = rnd.normal(size=(256, 256, 3))
+    I1 = rnd.normal(size=(256, 256, 3))
+    with testing.raises(ValueError):
+        u, v = tvl1(I0, I1)
+
+
+def test_incompatible_shapes():
+    rnd = np.random.RandomState(0)
+    I0 = rnd.normal(size=(256, 256))
+    I1 = rnd.normal(size=(255, 256))
+    with testing.raises(ValueError):
+        u, v = tvl1(I0, I1)
