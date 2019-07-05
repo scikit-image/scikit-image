6a38880e32c0f3e63253148806ec56857b84c472
Clement Ng <clementdng@gmail.com>
2019-07-05 12:10:30 -0700

    Fixed PEP8 issues in benchmark

--- benchmarks/benchmark_peak_local_max.py
+++ benchmarks/benchmark_peak_local_max.py
@@ -3,7 +3,9 @@ import numpy as np
 from scipy import ndimage as ndi
 from skimage.feature import peak_local_max
 
+
 class PeakLocalMaxSuite(object):
+
     def setup(self):
         mask = np.zeros([500, 500], dtype=bool)
         x, y = np.indices((500, 500))
@@ -11,7 +13,8 @@ class PeakLocalMaxSuite(object):
         y_c = y // 20 * 20 + 10
         mask[(x - x_c)**2 + (y - y_c)**2 < 8**2] = True
 
-        # create a mask, label each disk, create distance image for peak searching
+        # create a mask, label each disk, create distance image for peak
+        # searching
         self.labels, num_objs = ndi.label(mask)
         self.dist = ndi.distance_transform_edt(mask)
 
