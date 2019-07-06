6e8d10517c22d423a17322445edbb36fcede21ea
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-07-07 03:44:04 +1000

    Restore 10x loop for benchmark continuity

--- benchmarks/benchmark_exposure.py
+++ benchmarks/benchmark_exposure.py
@@ -16,7 +16,11 @@ class ExposureSuite:
         self.p2, self.p98 = np.percentile(self.image, (2, 98))
 
     def time_equalize_hist(self):
-        result = exposure.equalize_hist(self.image)
+        # Run 10x to average out performance
+        # note that this is not needed as asv does this kind of averaging by
+        # default, but this loop remains here to maintain benchmark continuity
+        for i in range(10):
+            result = exposure.equalize_hist(self.image)
 
     def time_equalize_adapthist(self):
         result = exposure.equalize_adapthist(self.image, clip_limit=0.03)
