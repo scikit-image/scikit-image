3bb78d25b141b710807b49a0857fc5e484d014f3
François Boulogne <fboulogne@sciunto.org>
2019-07-10 16:29:26 +0200

    matplotlib: remove interpolation=nearest, none in our examples (#4002)
    * mpl use interpolation=nearest by default. Remove this option for the sake of clarity
    
    * replace interpolation=none by nearest (default) in gallery

--- doc/examples/segmentation/plot_floodfill.py
+++ doc/examples/segmentation/plot_floodfill.py
@@ -29,11 +29,11 @@ filled_checkers = flood_fill(checkers, (76, 76), 127)
 
 fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
 
-ax[0].imshow(checkers, cmap=plt.cm.gray, interpolation='none')
+ax[0].imshow(checkers, cmap=plt.cm.gray)
 ax[0].set_title('Original')
 ax[0].axis('off')
 
-ax[1].imshow(filled_checkers, cmap=plt.cm.gray, interpolation='none')
+ax[1].imshow(filled_checkers, cmap=plt.cm.gray)
 ax[1].plot(76, 76, 'wo')  # seed point
 ax[1].set_title('After flood fill')
 ax[1].axis('off')
--- doc/examples/segmentation/plot_watershed.py
+++ doc/examples/segmentation/plot_watershed.py
@@ -51,11 +51,11 @@ labels = watershed(-distance, markers, mask=image)
 fig, axes = plt.subplots(ncols=3, figsize=(9, 3), sharex=True, sharey=True)
 ax = axes.ravel()
 
-ax[0].imshow(image, cmap=plt.cm.gray, interpolation='nearest')
+ax[0].imshow(image, cmap=plt.cm.gray)
 ax[0].set_title('Overlapping objects')
-ax[1].imshow(-distance, cmap=plt.cm.gray, interpolation='nearest')
+ax[1].imshow(-distance, cmap=plt.cm.gray)
 ax[1].set_title('Distances')
-ax[2].imshow(labels, cmap=plt.cm.nipy_spectral, interpolation='nearest')
+ax[2].imshow(labels, cmap=plt.cm.nipy_spectral)
 ax[2].set_title('Separated objects')
 
 for a in ax:
