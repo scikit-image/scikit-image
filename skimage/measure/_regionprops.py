74762c3f04216fe8eae700e53e85af2434080ac4
Holly Gibbs <hgibbs@tamu.edu>
2019-07-14 17:23:27 -0500

    pep8 fixes

--- skimage/measure/_regionprops.py
+++ skimage/measure/_regionprops.py
@@ -195,9 +195,9 @@ class RegionProperties:
                           for i in range(self._ndim)]).T
 
     @only2d
-    def eccentricity(self): 
+    def eccentricity(self):
         with warnings.catch_warnings():
-            warnings.filterwarnings("ignore","regionprops and image moments")
+            warnings.filterwarnings('ignore', 'regionprops and image moments')
             l1, l2 = self.inertia_tensor_eigvals
         if l1 == 0:
             return 0
@@ -265,13 +265,13 @@ class RegionProperties:
 
     def major_axis_length(self):
         with warnings.catch_warnings():
-            warnings.filterwarnings("ignore","regionprops and image moments")
+            warnings.filterwarnings('ignore', 'regionprops and image moments')
             l1 = self.inertia_tensor_eigvals[0]
         return 4 * sqrt(l1)
 
     def minor_axis_length(self):
         with warnings.catch_warnings():
-            warnings.filterwarnings("ignore","regionprops and image moments")
+            warnings.filterwarnings('ignore', 'regionprops and image moments')
             l2 = self.inertia_tensor_eigvals[-1]
         return 4 * sqrt(l2)
 
