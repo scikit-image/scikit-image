065a5d42959c1a9534f401a24deeb3f954447584
Holly Gibbs <hgibbs@tamu.edu>
2019-07-14 17:19:30 -0500

    add final warnings filter

--- skimage/measure/_regionprops.py
+++ skimage/measure/_regionprops.py
@@ -195,8 +195,10 @@ class RegionProperties:
                           for i in range(self._ndim)]).T
 
     @only2d
-    def eccentricity(self): #suppress warning
-        l1, l2 = self.inertia_tensor_eigvals
+    def eccentricity(self): 
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore","regionprops and image moments")
+            l1, l2 = self.inertia_tensor_eigvals
         if l1 == 0:
             return 0
         return sqrt(1 - l2 / l1)
