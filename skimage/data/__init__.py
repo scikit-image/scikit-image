067d1a854eccd01e97bd32e5063fede541528632
François Boulogne <devel@sciunto.org>
2019-07-29 07:13:15 +0200

    Remove old import of internal warn

--- skimage/data/__init__.py
+++ skimage/data/__init__.py
@@ -11,7 +11,6 @@ import os as _os
 import numpy as _np
 from warnings import warn
 
-from .._shared._warnings import expected_warnings, warn
 from ..util.dtype import img_as_bool
 from ._binary_blobs import binary_blobs
 from ._detect import lbp_frontal_face_cascade_filename
