b8182cea80980dc88d96b97066e962fd55e297c5
François Boulogne <devel@sciunto.org>
2019-07-29 07:34:03 +0200

    More explicit api for mpl

--- doc/examples/features_detection/plot_corner.py
+++ doc/examples/features_detection/plot_corner.py
@@ -34,7 +34,8 @@ coords_subpix = corner_subpix(image, coords, window_size=13)
 
 fig, ax = plt.subplots()
 ax.imshow(image, cmap=plt.cm.gray)
-ax.plot(coords[:, 1], coords[:, 0], 'oc', markersize=6)
+ax.plot(coords[:, 1], coords[:, 0], color='cyan', marker='o',
+        linestyle='None', markersize=6)
 ax.plot(coords_subpix[:, 1], coords_subpix[:, 0], '+r', markersize=15)
 ax.axis((0, 310, 200, 0))
 plt.show()
