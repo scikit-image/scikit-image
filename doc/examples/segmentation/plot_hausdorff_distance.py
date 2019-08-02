9eec3a9e3dd34753fa6332381332b799e562e669
Clement Ng <clementdng@gmail.com>
2019-08-02 00:11:57 -0700

    Updated gallery example

--- doc/examples/segmentation/plot_hausdorff_distance.py
+++ doc/examples/segmentation/plot_hausdorff_distance.py
@@ -15,31 +15,31 @@ import numpy as np
 
 from skimage import metrics
 
-shape = (600, 600)
+shape = (60, 60)
 image = np.zeros(shape)
 
 # Create a diamond-like shape where the four corners form the 1st set of points
-x_diamond = 300
-y_diamond = 300
-r = 100
+x_diamond = 30
+y_diamond = 30
+r = 10
 
 fig, ax = plt.subplots()
-plt_x = [0, 1, 0, -1, 0]
-plt_y = [1, 0, -1, 0, 1]
+plt_x = [0, 1, 0, -1]
+plt_y = [1, 0, -1, 0]
 
 set_ax = [(x_diamond + r * x) for x in plt_x]
 set_ay = [(y_diamond + r * y) for y in plt_y]
-plt.plot(set_ax, set_ay, 'r')
+plt.plot(set_ax, set_ay, 'or')
 
 # Create a kite-like shape where the four corners form the 2nd set of points
-x_kite = 300
-y_kite = 300
-x_r = 150
-y_r = 200
+x_kite = 30
+y_kite = 30
+x_r = 15
+y_r = 20
 
 set_bx = [(x_kite + x_r * x) for x in plt_x]
 set_by = [(y_kite + y_r * y) for y in plt_y]
-plt.plot(set_bx, set_by, 'b')
+plt.plot(set_bx, set_by, 'og')
 
 # Set up the data to compute the hausdorff distance
 coords_a = np.zeros(shape, dtype=np.bool)
@@ -53,11 +53,22 @@ for x, y in zip(set_bx, set_by):
 # Call the hausdorff function on the coordinates
 metrics.hausdorff_distance(coords_a, coords_b)
 
-# Plot (one of) the line(s) that shows the length of the hausdorff distance
-x_line = [200, 300]
-y_line = [300, 100]
-plt.plot(x_line, y_line, 'g')
+# Plot the lines that shows the length of the hausdorff distance
+x_line = [30, 30]
+y_line = [20, 10]
+plt.plot(x_line, y_line, 'y')
+
+x_line = [30, 30]
+y_line = [40, 50]
+plt.plot(x_line, y_line, 'y')
+
+# Plot circles to show that at this distance, the hausdorff distance can
+# travel to its nearest neighbor (in this case, from the kite to diamond)
+ax.add_artist(plt.Circle((30, 10), 10, color='y', fill=None))
+ax.add_artist(plt.Circle((30, 50), 10, color='y', fill=None))
+ax.add_artist(plt.Circle((15, 30), 10, color='y', fill=None))
+ax.add_artist(plt.Circle((45, 30), 10, color='y', fill=None))
 
 ax.imshow(image, cmap=plt.cm.gray)
-ax.axis((0, 600, 600, 0))
+ax.axis((0, 60, 60, 0))
 plt.show()
