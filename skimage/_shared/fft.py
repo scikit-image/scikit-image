9f0726e7c3001817069b5412e2fac4a5d30d5a36
Mark Harfouche <mark.harfouche@gmail.com>
2019-08-14 22:18:01 -0400

    trigger build

--- skimage/_shared/fft.py
+++ skimage/_shared/fft.py
@@ -1,6 +1,9 @@
 """Prefer FFTs via the new scipy.fft module when available (SciPy 1.4+)
 
 Otherwise fall back to numpy.fft.
+
+Like numpy 1.15+ scipy 1.3+ is also using pocketfft, but a newer 
+C++/pybind11 version called pypocketfft
 """
 try:
     import scipy.fft
