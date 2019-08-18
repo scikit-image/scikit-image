3d1f3bbf4b6053bf401c6e38dd7d072b0108119b
Richard Graham <richard.graham@cbbp-fpm.com>
2019-08-17 17:28:39 -0700

    Changed incorrect assertion in test_regionprops_table. Had assumed area of SAMPLE incorrect. This was because of a bug with the old test_cache method which happened to swap out the global variable SAMPLE with all ones

--- skimage/measure/tests/test_regionprops.py
+++ skimage/measure/tests/test_regionprops.py
@@ -492,7 +492,7 @@ def test_props_to_dict():
     regions = regionprops(SAMPLE)
     out = _props_to_dict(regions, properties=('label', 'area', 'bbox'),
                          separator='+')
-    assert out == {'label': array([1]), 'area': array([180]),
+    assert out == {'label': array([1]), 'area': array([72]),
                    'bbox+0': array([0]), 'bbox+1': array([0]),
                    'bbox+2': array([10]), 'bbox+3': array([18])}
 
@@ -505,7 +505,7 @@ def test_regionprops_table():
 
     out = regionprops_table(SAMPLE, properties=('label', 'area', 'bbox'),
                             separator='+')
-    assert out == {'label': array([1]), 'area': array([180]),
+    assert out == {'label': array([1]), 'area': array([72]),
                    'bbox+0': array([0]), 'bbox+1': array([0]),
                    'bbox+2': array([10]), 'bbox+3': array([18])}
 
