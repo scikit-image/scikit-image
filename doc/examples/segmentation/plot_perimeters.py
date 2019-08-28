9f32e5e451e1cef7974389460558683414987994
yann gavet <gavet@emse.fr>
2019-08-28 16:31:00 +0200

    PEP8 conformity
    mainly removed trailing whitespace and ; at ends of lines

--- doc/examples/segmentation/plot_perimeters.py
+++ doc/examples/segmentation/plot_perimeters.py
@@ -14,28 +14,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-square = np.zeros((100, 100));
-square [40:60, 40:60] = 1;
+square = np.zeros((100, 100))
+square[40:60, 40:60] = 1
 
-for n in [4,6]:        
-    p =[];
-    angles = range(90);
+for n in [4, 6]:
+    p = []
+    angles = range(90)
+    for i in angles:
+        rotated_square = rotate(square, i, order=0)  # nearest neighbor
+        p.append(perimeter(rotated_square, n))
+    plt.plot(angles, p)
+
+for d in [2, 4]:
+    p = []
+    angles = range(90)
     for i in angles:
 
-        rotated_square=rotate(square, i, order=0); # nearest neighbor
-        p.append(perimeter(rotated_square, n));
-    plt.plot(angles, p);
+        rotated_square = rotate(square, i, order=0)  # nearest neighbor
+        p.append(crofton_perimeter(rotated_square, d))
+    plt.plot(angles, p)
 
-for d in [2,4]:
-    p =[];
-    angles = range(90);
-    for i in angles:
-
-        rotated_square=rotate(square, i, order=0); # nearest neighbor
-        p.append(crofton_perimeter(rotated_square, d));
-    plt.plot(angles, p);
-        
-plt.xlabel('Rotation angle');
+plt.xlabel('Rotation angle')
 plt.ylabel('Perimeter of the rotated square')
-plt.legend(['N4 perimeter', 'N8 perimeter', 'Crofton 2 directions', 'Crofton 4 directions'])
-plt.show();
+plt.legend(['N4 perimeter', 'N8 perimeter',
+            'Crofton 2 directions', 'Crofton 4 directions'])
+plt.show()
