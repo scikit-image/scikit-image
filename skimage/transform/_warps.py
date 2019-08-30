975c3c3a9259e596bf8632bcb791d57c205c9e75
Riadh <r.fezzani@vitadx.com>
2019-08-30 15:28:59 +0200

    Fix test_resize_dtype failure

--- skimage/transform/_warps.py
+++ skimage/transform/_warps.py
@@ -842,7 +842,7 @@ def warp(image, inverse_map, map_args={}, output_shape=None, order=1,
 
         if matrix is not None:
             matrix = matrix.astype(image.dtype)
-            ctype = 'float32_t' if image.dtype is np.float32 else 'float64_t'
+            ctype = 'float32_t' if image.dtype == np.float32 else 'float64_t'
             if image.ndim == 2:
                 warped = _warp_fast[ctype](image, matrix,
                                            output_shape=output_shape,
