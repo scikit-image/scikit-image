d233a411a2711957c3e9cbd1ac7d2d744517694f
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-09-06 05:35:24 -0500

    Add tests for pathological arrays in threshold_li
    Checks for https://github.com/scikit-image/scikit-image/issues/4140

--- skimage/filters/tests/test_thresholding.py
+++ skimage/filters/tests/test_thresholding.py
@@ -316,6 +316,20 @@ def test_li_negative_inital_guess():
         result = threshold_li(coins, initial_guess=-5)
 
 
+def test_li_pathological_arrays():
+    # See https://github.com/scikit-image/scikit-image/issues/4140
+    a = np.array([0, 0, 1, 0, 0, 1, 0, 1])
+    b = np.array([0, 0, 0.1, 0, 0, 0.1, 0, 0.1])
+    c = np.array([0, 0, 0.1, 0, 0, 0.1, 0.01, 0.1])
+    d = np.array([0, 0, 1, 0, 0, 1, 0.5, 1])
+    e = np.array([1, 1])
+    f = np.array([1, 2])
+    arrays = [a, b, c, d, e, f]
+    thresholds = [threshold_li(arr) for arr in arrays]
+    print(thresholds)
+    assert np.all(np.isfinite(thresholds))
+
+
 def test_yen_camera_image():
     camera = util.img_as_ubyte(data.camera())
     assert 197 < threshold_yen(camera) < 199
