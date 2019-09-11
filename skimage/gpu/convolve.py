f1749da4241f5e2cb397f8d3df0b312fbf00fb2b
Dalordish <dalordish@gmail.com>
2019-09-12 00:54:46 +1000

    add support for reflected mode with latest version of gputools

--- skimage/gpu/convolve.py
+++ skimage/gpu/convolve.py
@@ -2,6 +2,15 @@ from scipy import ndimage as ndi
 import numpy as np
 
 #setup the imports, attempt gputools, if not, fallback instantly
+#
+
+'''
+  mode_str = {"constant":"CLK_ADDRESS_CLAMP",
+                "wrap":"CLK_ADDRESS_REPEAT"}
+                "wrap":"CLK_ADDRESS_REPEAT",
+                "nearest":"CLK_ADDRESS_CLAMP_TO_EDGE",
+                "reflect":"CLK_ADDRESS_MIRRORED_REPEAT"}
+'''
 try:
     import gputools
 except ImportError:
@@ -9,9 +18,9 @@ except ImportError:
         ndi.convolve(input,weights,output=output,mode=mode,cval=cval,origin=origin)
 
 def convolve(input,weights,output=None,mode='reflect',cval=0.0,origin=0):
-    if mode == 'constant' and cval == 0.0: #These are the only two conditions that gputools supports
+    if (mode == 'reflect') or (mode == 'constant' and cval == 0.0): #These are the only two conditions that gputools supports
         print('using gpu!') #print for debugging
-        out = gputools.convolve(np.array(input),np.array(weights)) 
+        out = gputools.convolve(np.array(input),np.array(weights),mode='mode') 
         # we need to do some type conversion here in order to maintain full compatibility
         # as ndi autocasts to np arrays, but gputools does not
         if output != None:
