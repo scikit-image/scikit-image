da8f3df4a60c8360a6b8414c0dfbb991c195be39
Dalordish <dalordish@gmail.com>
2019-09-12 00:55:05 +1000

    make fft copy file to support out of place editing

--- skimage/gpu/fft.py
+++ skimage/gpu/fft.py
@@ -1,3 +1,4 @@
+import numpy as np
 def fft(x,n=None,axis=1,overwrite_x=False):
     from .._shared.fft import fftmodule #shared fft /module/ for np and scipy
                                         #likely a better way to just hijack the existing failover
@@ -6,6 +7,9 @@ def fft(x,n=None,axis=1,overwrite_x=False):
         if (overwrite_x == True and n == None): # the only conditions that gputools supports
             print("using gpu")
             return gputools.fft(x)
+        elif n == None:
+            out = np.copy(x)
+            return gputools.fft(out)
         else:
             print("falling back to fftmodule") 
             return fftmodule.fftn(x,n,axis,overwrite_x)
