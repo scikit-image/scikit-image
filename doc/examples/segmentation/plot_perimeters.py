df2242f917d550b0f4da8238594b1d93877548c9
yann gavet <gavet@emse.fr>
2019-09-12 13:55:05 +0200

    Euler number and Crofton perimeter
    Improvements of the documentation and comments in code

--- doc/examples/segmentation/plot_perimeters.py
+++ doc/examples/segmentation/plot_perimeters.py
@@ -3,8 +3,9 @@
 Different perimeters
 =========================
 
-This example shows the uncertainty in evaluating perimeters.
-Take a square, rotate it, and evaluate the perimeters.
+In this example we show the uncertainty on calculating perimeters, comparing 
+classic and Crofton ones. For that, we evaluate the perimeters of a square and 
+its rotated version.
 
 """
 from skimage.measure import perimeter
@@ -14,28 +15,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-square = np.zeros((100, 100))
-square[40:60, 40:60] = 1
+# scale parameter can be used to increase the grid size. The resulting curves
+# should be smoothed with higer scales
+scale = 10
 
-for n in [4, 6]:
-    p = []
-    angles = range(90)
-    for i in angles:
-        rotated_square = rotate(square, i, order=0)  # nearest neighbor
-        p.append(perimeter(rotated_square, n))
-    plt.plot(angles, p)
+# Construct 2 figures, square and disks
+square = np.zeros((100*scale, 100*scale))
+square[40*scale:60*scale, 40*scale:60*scale] = 1
 
-for d in [2, 4]:
-    p = []
-    angles = range(90)
-    for i in angles:
+[X, Y] = np.meshgrid(np.linspace(0, 100*scale), np.linspace(0, 100*scale))
+R = 20 * scale
+disk = (X-50*scale)**2+(Y-50*scale)**2 <= R**2
 
-        rotated_square = rotate(square, i, order=0)  # nearest neighbor
-        p.append(crofton_perimeter(rotated_square, d))
-    plt.plot(angles, p)
+fig, axes = plt.subplots(1, 2, figsize=(16, 8))
+ax = axes.flatten()
 
-plt.xlabel('Rotation angle')
-plt.ylabel('Perimeter of the rotated square')
-plt.legend(['N4 perimeter', 'N8 perimeter',
-            'Crofton 2 directions', 'Crofton 4 directions'])
+# for each type of objects, the different perimeters are evaluated
+for index, obj in enumerate([square, disk]):
+
+    # 2 neighbourhoud configurations for measure.perimeter
+    for n in [4, 6]:
+        p = []
+        angles = range(90)
+        for i in angles:
+            # rotation and perimeter evaluation
+            rotated = rotate(obj, i, order=0)
+            p.append(perimeter(rotated, n))
+        ax[index].plot(angles, p)
+
+    # 2 or 4 directions can be used by measure.crofton_perimeter
+    for d in [2, 4]:
+        p = []
+        angles = range(90)
+        for i in angles:
+            # rotation and perimeter evaluation
+            rotated = rotate(obj, i, order=0)
+            p.append(crofton_perimeter(rotated, d))
+        ax[index].plot(angles, p)
+
+    ax[index].set_xlabel('Rotation angle')
+    ax[index].legend(['N4 perimeter', 'N8 perimeter',
+                      'Crofton 2 directions', 'Crofton 4 directions'])
+    ax[index].set_ylabel('Perimeter of the rotated object')
+
+ax[0].set_title('Square')
+ax[1].set_title('Disk')
 plt.show()
