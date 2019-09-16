0f389728e51521b95f73134303cf1153dfbf4863
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-09-15 20:34:12 -0500

    Add test for line crossing 0

--- skimage/draw/tests/test_draw_nd.py
+++ skimage/draw/tests/test_draw_nd.py
@@ -1,7 +1,13 @@
 from skimage.draw import line_nd
+from skimage._shared.testing import assert_equal
 
 
 def test_empty_line():
     coords = line_nd((1, 1, 1), (1, 1, 1))
     assert len(coords) == 3
     assert all(len(c) == 0 for c in coords)
+
+
+def test_zero_line():
+    coords = line_nd((-1, -1), (2, 2))
+    assert_equal(coords, [[-1, 0, 1], [-1, 0, 1]])
