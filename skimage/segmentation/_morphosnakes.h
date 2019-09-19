9f7372c67f6bada77a6482082d5d6cb1f92ea33c
Axel Ekman <axel.ekman@iki.fi>
2019-09-19 08:36:50 -0700

    fix conditional omp include

--- skimage/segmentation/_morphosnakes.h
+++ skimage/segmentation/_morphosnakes.h
@@ -10,7 +10,14 @@
 #include <vector>
 
 #include <algorithm>
+
+#if defined(_OPENMP)
 #include <omp.h>
+#else
+typedef int omp_int_t;
+inline omp_int_t omp_get_thread_num() { return 0; }
+inline omp_int_t omp_get_max_threads() { return 1; }
+#endif
 
 typedef struct point3d
 {
