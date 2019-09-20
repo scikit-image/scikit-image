489ae845c5fcda0344e9bf6decf3a0784991b257
Axel Ekman <axel.ekman@iki.fi>
2019-09-19 21:58:53 -0700

    define omp_get_num_threads

--- skimage/segmentation/_morphosnakes.h
+++ skimage/segmentation/_morphosnakes.h
@@ -15,7 +15,7 @@
 #else
 typedef int omp_int_t;
 inline omp_int_t omp_get_thread_num() { return 0; }
-inline omp_int_t omp_get_max_threads() { return 1; }
+inline omp_int_t omp_get_num_threads() { return 1; }
 #endif
 
 typedef struct point3d
