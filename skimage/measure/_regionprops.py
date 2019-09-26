b2a7bca72f856fa0e497f6f64c6df4610b6d3fa3
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-09-25 23:54:45 -0500

    Update regionprops_table signature

--- skimage/measure/_regionprops.py
+++ skimage/measure/_regionprops.py
@@ -525,8 +525,10 @@ def _props_to_dict(regions, properties=('label', 'bbox'), separator='-'):
     return out
 
 
-def regionprops_table(label_image, intensity_image=None, cache=True,
-                      properties=('label', 'bbox'), separator='-'):
+def regionprops_table(label_image, intensity_image=None,
+                      properties=('label', 'bbox'),
+                      *,
+                      cache=True, separator='-'):
     """Find image properties and convert them into a dictionary
 
     Parameters
@@ -536,15 +538,15 @@ def regionprops_table(label_image, intensity_image=None, cache=True,
     intensity_image : (N, M) ndarray, optional
         Intensity (i.e., input) image with same size as labeled image.
         Default is None.
-    cache : bool, optional
-        Determine whether to cache calculated properties. The computation is
-        much faster for cached properties, whereas the memory consumption
-        increases.
     properties : tuple or list of str, optional
         Properties that will be included in the resulting dictionary
         For a list of available properties, please see :func:`regionprops`.
         Users should remember to add "label" to keep track of region
         identities.
+    cache : bool, optional
+        Determine whether to cache calculated properties. The computation is
+        much faster for cached properties, whereas the memory consumption
+        increases.
     separator : str, optional
         For non-scalar properties not listed in OBJECT_COLUMNS, each element
         will appear in its own column, with the index of that element separated
