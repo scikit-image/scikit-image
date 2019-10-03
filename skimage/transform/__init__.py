c1075bdcf49862f05610405e67ff4b8d5afd5e0a
Riadh <r.fezzani@vitadx.com>
2019-10-03 14:52:34 +0200

    Add resize_local_mean function

--- skimage/transform/__init__.py
+++ skimage/transform/__init__.py
@@ -13,7 +13,8 @@ from ._geometric import (estimate_transform,
                          EssentialMatrixTransform, PolynomialTransform,
                          PiecewiseAffineTransform)
 from ._warps import (swirl, resize, rotate, rescale,
-                     downscale_local_mean, warp, warp_coords, warp_polar)
+                     downscale_local_mean, warp, warp_coords, warp_polar,
+                     resize_local_mean)
 from .pyramids import (pyramid_reduce, pyramid_expand,
                        pyramid_gaussian, pyramid_laplacian)
 
@@ -48,6 +49,7 @@ __all__ = ['match_histograms',
            'PiecewiseAffineTransform',
            'swirl',
            'resize',
+           'resize_local_mean',
            'rotate',
            'rescale',
            'downscale_local_mean',
