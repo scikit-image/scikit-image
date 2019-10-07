38f287fd8775152e5ca02472b250dfd93eae2329
Dalordish <dalordish@gmail.com>
2019-10-07 16:11:32 +1100

    bugfix for gpu env var flag

--- skimage/_shared/gpu/__init__.py
+++ skimage/_shared/gpu/__init__.py
@@ -1,13 +1,22 @@
-use_gpu = False
-try: 
-    import pyopencl
-    import skimage._vendor.gputools
-    from pyopencl.tools import get_test_platforms_and_devices
-    print("OpenCL devices detected: ",get_test_platforms_and_devices(),"using GPU.")
-    use_gpu = True
+import os
 
-except (ImportError, pyopencl.LogicError) as e: #TODO see what the actual error thrown here is
+
+try:
+    if os.environ['SKIMAGE_USE_GPU'].upper() == 'TRUE':
+        use_gpu = True
+    else:
+        use_gpu = False
+except KeyError:
+    print('SKIMAGE_USE_GPU environt variable not set: defaulting to false')
+    use_gpu = False
+try:
+    if use_gpu == True:
+        import pyopencl
+        import skimage._vendor.gputools
+        from pyopencl.tools import get_test_platforms_and_devices
+        print("OpenCL devices detected: ", get_test_platforms_and_devices(), "using GPU.")
+
+except (ImportError, pyopencl.LogicError) as e:
     print("Error occured with GPU imports, is your system configured correctly?")
     print(e)
     use_gpu = False
-use_gpu = True
