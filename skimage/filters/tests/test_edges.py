930c692c78d5dc51ee548dd4e9db2918ca78f54c
Richard <Dalordish@users.noreply.github.com>
2019-10-09 13:44:42 +1100

    Remove changes used for testing other gpu acceleration options

--- skimage/filters/tests/test_edges.py
+++ skimage/filters/tests/test_edges.py
@@ -491,8 +491,7 @@ def test_vertical_mask_line(grad_func):
     expected[1:-1, 4:7] = 0  # but line and neighbors masked
 
     result = grad_func(hgrad, mask)
-    assert_allclose(result, expected,atol=1e-07,rtol=1e-08)
-
+    assert_allclose(result, expected) 
 
 def test_range():
     """Output of edge detection should be in [0, 1]"""
