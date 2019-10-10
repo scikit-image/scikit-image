f0c7a373ffb7b3ee22683f6b1d60139655ca0b11
Riadh <r.fezzani@vitadx.com>
2019-10-10 15:25:27 +0200

    Fix pep8 errors

--- skimage/feature/corner.py
+++ skimage/feature/corner.py
@@ -917,7 +917,7 @@ def corner_subpix(image, corners, window_size=11, alpha=0.99):
 def corner_peaks(image, min_distance=1, threshold_abs=None, threshold_rel=None,
                  exclude_border=True, indices=True, num_peaks=np.inf,
                  footprint=None, labels=None, *, num_peaks_per_label=np.inf,
-		 metric='chebyshev'):
+                 metric='chebyshev'):
     """Find corners in corner measure response image.
 
     This differs from `skimage.feature.peak_local_max` in that it suppresses
