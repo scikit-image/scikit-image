cd6732e3418aebab757ff88941cb055176c1a5fe
Somkiadcharoen <Robroo.Somkiadcharoen@elektrobit.com>
2019-10-17 18:18:32 +0300

    add test_match_histogram_unichannel
    This commit fixes #4106 by adding a testcase for match_histograms parameter.

--- skimage/exposure/tests/test_histogram_matching.py
+++ skimage/exposure/tests/test_histogram_matching.py
@@ -27,8 +27,6 @@ class TestMatchHistogram:
     image_rgb = data.chelsea()
     template_rgb = data.astronaut()
 
-    # To handle with mutlichannel ==False
-    #(image_rgb[:, :, 0], template_rgb[:, :, 0]),
     @pytest.mark.parametrize('image, reference', [
         (image_rgb, template_rgb)
     ])
@@ -37,7 +35,8 @@ class TestMatchHistogram:
         all channels and all values of matched"""
 
         # when
-        matched = exposure.match_histograms(image, reference, multichannel=True)
+        matched = exposure.match_histograms(image, reference,
+                                            multichannel=True)
 
         matched_pdf = self._calculate_image_empirical_pdf(matched)
         reference_pdf = self._calculate_image_empirical_pdf(reference)
@@ -48,9 +47,39 @@ class TestMatchHistogram:
             matched_values, matched_quantiles = matched_pdf[channel]
 
             for i, matched_value in enumerate(matched_values):
-                closest_id = (np.abs(reference_values - matched_value)).argmin()
+                closest_id = (
+                    np.abs(reference_values - matched_value)
+                ).argmin()
                 assert_almost_equal(matched_quantiles[i],
-                                    reference_quantiles[closest_id], decimal=1)
+                                    reference_quantiles[closest_id],
+                                    decimal=1)
+
+    @pytest.mark.parametrize('image, reference', [
+        (image_rgb[:, :, 0], template_rgb[:, :, 0])
+    ])
+    def test_match_histograms_unichannel(self, image, reference):
+        """Assert that pdf of matched image is close to the reference's pdf for
+        all values of matched"""
+
+        # when
+        matched = exposure.match_histograms(image, reference,
+                                            multichannel=False)
+
+        matched_pdf = self._calculate_image_empirical_pdf(matched)
+        reference_pdf = self._calculate_image_empirical_pdf(reference)
+
+        # then
+        for channel in range(len(matched_pdf)):
+            reference_values, reference_quantiles = reference_pdf[channel]
+            matched_values, matched_quantiles = matched_pdf[channel]
+
+            for i, matched_value in enumerate(matched_values):
+                closest_id = (
+                    np.abs(reference_values - matched_value)
+                ).argmin()
+                assert_almost_equal(matched_quantiles[i],
+                                    reference_quantiles[closest_id],
+                                    decimal=1)
 
     @pytest.mark.parametrize('image, reference', [
         (image_rgb, template_rgb[:, :, 0]),
