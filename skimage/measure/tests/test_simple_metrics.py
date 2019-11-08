91404f930d0d3a0808f1e6b0a592865682084c6e
Ilia Ilmer <i.ilmer@icloud.com>
2019-11-08 13:00:25 -0500

    pep8 fix

--- skimage/measure/tests/test_simple_metrics.py
+++ skimage/measure/tests/test_simple_metrics.py
@@ -69,7 +69,8 @@ def test_NRMSE():
         y, x, normalization='min-max'), 1 / (y.max() - y.min()))
 
     # mixed precision inputs are allowed
-    assert_almost_equal(normalized_root_mse(y, np.float32(x), normalization='min-max'),
+    assert_almost_equal(normalized_root_mse(y, np.float32(x),
+                                            normalization='min-max'),
                         1 / (y.max() - y.min()))
 
 
