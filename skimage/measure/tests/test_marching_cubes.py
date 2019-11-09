96c15a18423d911654eaae3aa9c69896e523147d
Riadh <rfezzani@gmail.com>
2019-11-09 12:08:13 +0100

    Fix pep8

--- skimage/measure/tests/test_marching_cubes.py
+++ skimage/measure/tests/test_marching_cubes.py
@@ -112,18 +112,20 @@ def test_both_algs_same_result_donut():
     a, b = 2.5/n, -1.25
 
     vol = np.empty((n, n, n), 'float32')
-    for iz in range(vol.shape[0]):
-        for iy in range(vol.shape[1]):
-            for ix in range(vol.shape[2]):
-                # Double-torii formula by Thomas Lewiner
-                z, y, x = float(iz)*a+b, float(iy)*a+b, float(ix)*a+b
-                vol[iz,iy,ix] = ( (
-                    (8*x)**2 + (8*y-2)**2 + (8*z)**2 + 16 - 1.85*1.85 ) * ( (8*x)**2 +
-                    (8*y-2)**2 + (8*z)**2 + 16 - 1.85*1.85 ) - 64 * ( (8*x)**2 + (8*y-2)**2 )
-                    ) * ( ( (8*x)**2 + ((8*y-2)+4)*((8*y-2)+4) + (8*z)**2 + 16 - 1.85*1.85 )
-                    * ( (8*x)**2 + ((8*y-2)+4)*((8*y-2)+4) + (8*z)**2 + 16 - 1.85*1.85 ) -
-                    64 * ( ((8*y-2)+4)*((8*y-2)+4) + (8*z)**2
-                    ) ) + 1025
+
+    for idx in range(np.prod(vol.shape)):
+        iz, iy, ix = np.unravel_index(idx, vol.shape)
+        z, y, x = float(iz)*a+b, float(iy)*a+b, float(ix)*a+b
+        vol[iz, iy, ix] = (
+            (((8 * x) ** 2 + (8 * y - 2) ** 2 + (8 * z) ** 2 + 16 - 1.85 * 1.85)
+             * ((8 * x) ** 2 + (8 * y - 2) ** 2 + (8 * z) ** 2
+                + 16 - 1.85 * 1.85) - 64 * ((8 * x) ** 2 + (8 * y - 2) ** 2))
+            * (((8 * x) ** 2 + ((8 * y - 2) + 4) * ((8 * y - 2) + 4)
+                + (8 * z) ** 2 + 16 - 1.85 * 1.85)
+               * ((8 * x) ** 2 + ((8 * y - 2) + 4) * ((8 * y - 2) + 4)
+                  + (8 * z) ** 2 + 16 - 1.85 * 1.85)
+               - 64 * (((8 * y - 2) + 4) * ((8 * y - 2) + 4) + (8 * z) ** 2))
+        ) + 1025
 
     vertices1, faces1 = marching_cubes(vol, 0, use_classic=2)[:2]
     vertices2, faces2 = marching_cubes(vol, 0)[:2]
