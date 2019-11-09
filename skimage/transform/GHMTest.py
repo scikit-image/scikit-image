92680c4a9ccd8e6103d5b32244b304180fe63503
Rocky Mandayam <rocky.mandayam@gmail.com>
2019-11-08 23:29:27 -0800

    remove black.jpg because it is not really all black/all one pix value

--- skimage/transform/GHMTest.py
+++ skimage/transform/GHMTest.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from GHM import GHM
 from GHMHelperFuncs import read_and_check_img
 
@@ -8,6 +10,11 @@ streetlights = read_and_check_img("streetlights.jpg")
 white = read_and_check_img("white.jpg")
 imgs = [black, grayish, house, streetlights, white]
 img_names = ['black', 'grayish', 'house', 'streetlights', 'white']
+
+def is_black(img):
+	return np.all(img == 0)
+def is_white(img):
+	return np.all(img == 255)
 
 def test_all_combinations_pdf(imgs, img_names):
 	for i in range(len(imgs)):
@@ -31,5 +38,11 @@ def test_all_combinations_pdf(imgs, img_names):
 				f.close()
 
 
-matched = GHM(house, streetlights)
+# matched = GHM(house, streetlights)
 # test_all_combinations_pdf(imgs, img_names)
+print("****")
+print(is_black(black))
+print(black.shape)
+print("****")
+print(is_white(white))
+print(white.shape)
