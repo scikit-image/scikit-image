cda74d2fa5e78b06d714d694f855188068e3850a
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2019-12-09 15:06:14 +0100

    Add second region in toy example image

--- doc/examples/segmentation/plot_regionprops.py
+++ doc/examples/segmentation/plot_regionprops.py
@@ -22,6 +22,9 @@ image[rr, cc] = 1
 
 image = rotate(image, angle=15, order=0)
 
+rrr, ccc = ellipse(100, 100, 60, 50)
+image[rrr, ccc] = 1
+
 label_img = label(image)
 regions = regionprops(label_img)
 
