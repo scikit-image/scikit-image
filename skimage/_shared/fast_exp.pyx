cdfe41f3cc2da7710c052e85652881bec051b44e
Riadh <rfezzani@gmail.com>
2019-12-20 15:23:57 +0100

    Fix fast_exp

--- skimage/_shared/fast_exp.pyx
+++ skimage/_shared/fast_exp.pyx
@@ -1,15 +1,8 @@
 import numpy as np
 cimport numpy as cnp
 from .fused_numerics cimport np_floats
-
-cdef extern from "fast_exp.h":
-    double fast_exp(double y) nogil
+from .fast_exp cimport _fast_exp
 
 
 def fast_exp(np_floats x):
-    if np_floats is cnp.float32_t:
-        dtype = np.float32
-    else:
-        dtype = np.float64
-    cdef double x_f64=x
-    return dtype.type(fast_exp(x_f64))
+    return _fast_exp(x)
