df5bcd2fffd51dc30a27932176f2ca7cdec2b601
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2019-12-22 22:42:44 +0100

    Add 3D images to regionprops_table example

--- doc/examples/segmentation/plot_regionprops_table.py
+++ doc/examples/segmentation/plot_regionprops_table.py
@@ -3,12 +3,13 @@
 Explore and visualize region properties with pandas
 ===================================================
 
-This toy example shows how to compute the area of every labelled region in a
-series of 10 images. The blob-like regions are generated synthetically. As the
-fraction of image pixels covered by the blobs increases, the number of blobs
-(regions) decreases, and the size (area) of a single region can get larger and
-larger. The area values are available in a pandas-compatible format, which
-makes for convenient data analysis and visualization.
+This toy example shows how to compute the size of every labelled region in a
+series of 10 images. We use 2D images and then 3D images. The blob-like
+regions are generated synthetically. As the volume fraction (i.e., number of
+pixels or voxels covered by the blobs) increases, the number of blobs
+(regions) decreases, and the size (area or volume) of a single region can get
+larger and larger. The area (size) values are available in a pandas-compatible
+format, which makes for convenient data analysis and visualization.
 
 Beyond area, many other region properties are available.
 
@@ -16,11 +17,17 @@ Beyond area, many other region properties are available.
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 from skimage import data, measure
 
 
 fractions = np.linspace(0.05, 0.5, 10)
+
+"""
+2D images
+---------
+"""
 
 images = [data.binary_blobs(volume_fraction=f) for f in fractions]
 
@@ -54,12 +61,10 @@ plt.show()
 """
 In the scatterplot, many points seem to be overlapping at low area values.
 To get a better sense of the distribution, we may want to add some 'jitter'
-to the visualization. To this end, we import `seaborn`, the Python library
-dedicated to statistical data visualization, and use `stripplot` with
-argument `jitter=True`.
+to the visualization. To this end, we use `stripplot` (from `seaborn`, the
+Python library dedicated to statistical data visualization) with argument
+`jitter=True`.
 """
-
-import seaborn as sns
 
 sns_fig, sns_ax = plt.subplots()
 sns.stripplot(x='volume fraction', y='area', data=areas, jitter=True,
@@ -67,4 +72,34 @@ sns.stripplot(x='volume fraction', y='area', data=areas, jitter=True,
 x_format = sns_ax.xaxis.get_major_formatter()
 x_format.seq = ['{:0.2f}'.format(float(s)) for s in x_format.seq]
 sns_ax.xaxis.set_major_formatter(x_format)
+plt.show()
+
+"""
+3D images
+---------
+"""
+
+images = [data.binary_blobs(length=128, n_dim=3, volume_fraction=f)
+          for f in fractions]
+
+labeled_images = [measure.label(image) for image in images]
+
+properties = ['label', 'area']
+
+tables = [measure.regionprops_table(image, properties=properties)
+          for image in labeled_images]
+tables = [pd.DataFrame(table) for table in tables]
+
+for fraction, table in zip(fractions, tables):
+    table['volume fraction'] = fraction
+
+blob_volumes = pd.concat(tables, axis=0)
+
+d3_fig, d3_ax = plt.subplots()
+sns.stripplot(x='volume fraction', y='area', data=blob_volumes, jitter=True,
+              ax=d3_ax)
+x_format = d3_ax.xaxis.get_major_formatter()
+x_format.seq = ['{:0.2f}'.format(float(s)) for s in x_format.seq]
+d3_ax.xaxis.set_major_formatter(x_format)
+d3_ax.set_ylabel('blob size (3D)')
 plt.show()
