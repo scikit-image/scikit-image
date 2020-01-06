39990aa91b7ff8b5f13ccf4cfd2ad17c36041d3b
Emmanuelle Gouillart <emma@plot.ly>
2020-01-06 11:06:14 -0500

    typo

--- doc/examples/transform/plot_transform_types.py
+++ doc/examples/transform/plot_transform_types.py
@@ -60,9 +60,9 @@ plt.title('Euclidean transformation')
 # For a rotation around the center of the image, one can either 
 # compose a translation to change the origin, a rotation, and finally
 # the inverse of the first translation, or use the utility function
-# :func:`skimage.transform.rotation` (note that its parameter is given)
+# :func:`skimage.transform.rotation` (note that its parameter is given
 # in degrees, while it is given in radians for the different kinds of
-# affine transformations:
+# affine transformations):
 
 plt.figure()
 rotated_img = tf.rotate(img, 45)
