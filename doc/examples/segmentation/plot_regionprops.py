cdf1678503c5dd55172fa322d3458727eb0758a6
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2020-01-12 13:19:31 +0100

    Fix plotting of ellipse axes

--- doc/examples/segmentation/plot_regionprops.py
+++ doc/examples/segmentation/plot_regionprops.py
@@ -41,10 +41,10 @@ ax.imshow(image, cmap=plt.cm.gray)
 for props in regions:
     y0, x0 = props.centroid
     orientation = props.orientation
-    x1 = x0 + math.cos(orientation) * 0.5 * props.major_axis_length
-    y1 = y0 - math.sin(orientation) * 0.5 * props.major_axis_length
-    x2 = x0 - math.sin(orientation) * 0.5 * props.minor_axis_length
-    y2 = y0 - math.cos(orientation) * 0.5 * props.minor_axis_length
+    x1 = x0 + math.cos(orientation) * 0.5 * props.minor_axis_length
+    y1 = y0 - math.sin(orientation) * 0.5 * props.minor_axis_length
+    x2 = x0 - math.sin(orientation) * 0.5 * props.major_axis_length
+    y2 = y0 - math.cos(orientation) * 0.5 * props.major_axis_length
 
     ax.plot((x0, x1), (y0, y1), '-r', linewidth=2.5)
     ax.plot((x0, x2), (y0, y2), '-r', linewidth=2.5)
