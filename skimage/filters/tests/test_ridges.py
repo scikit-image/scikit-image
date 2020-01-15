779b8dde695eae717de80b42c00a76c299ba0c7c
Riadh <r.fezzani@vitadx.com>
2020-01-15 11:00:35 +0100

    Fix tests

--- skimage/filters/tests/test_ridges.py
+++ skimage/filters/tests/test_ridges.py
@@ -18,14 +18,14 @@ def test_2d_null_matrix():
     assert_equal(meijering(a_black, black_ridges=True), zeros)
     assert_equal(meijering(a_white, black_ridges=False), zeros)
 
-    assert_equal(sato(a_black, black_ridges=True), zeros)
-    assert_equal(sato(a_white, black_ridges=False), zeros)
+    assert_equal(sato(a_black, black_ridges=True, mode='reflect'), zeros)
+    assert_equal(sato(a_white, black_ridges=False, mode='reflect'), zeros)
 
     assert_allclose(frangi(a_black, black_ridges=True), zeros, atol=1e-3)
     assert_allclose(frangi(a_white, black_ridges=False), zeros, atol=1e-3)
 
-    assert_equal(hessian(a_black, black_ridges=False), ones)
-    assert_equal(hessian(a_white, black_ridges=True), ones)
+    assert_equal(hessian(a_black, black_ridges=False, mode='reflect'), ones)
+    assert_equal(hessian(a_white, black_ridges=True, mode='reflect'), ones)
 
 
 def test_3d_null_matrix():
@@ -39,14 +39,14 @@ def test_3d_null_matrix():
     assert_allclose(meijering(a_black, black_ridges=True), zeros, atol=1e-1)
     assert_allclose(meijering(a_white, black_ridges=False), zeros, atol=1e-1)
 
-    assert_equal(sato(a_black, black_ridges=True), zeros)
-    assert_equal(sato(a_white, black_ridges=False), zeros)
+    assert_equal(sato(a_black, black_ridges=True, mode='reflect'), zeros)
+    assert_equal(sato(a_white, black_ridges=False, mode='reflect'), zeros)
 
     assert_allclose(frangi(a_black, black_ridges=True), zeros, atol=1e-3)
     assert_allclose(frangi(a_white, black_ridges=False), zeros, atol=1e-3)
 
-    assert_equal(hessian(a_black, black_ridges=False), ones)
-    assert_equal(hessian(a_white, black_ridges=True), ones)
+    assert_equal(hessian(a_black, black_ridges=False, mode='reflect'), ones)
+    assert_equal(hessian(a_white, black_ridges=True, mode='reflect'), ones)
 
 
 def test_2d_energy_decrease():
@@ -60,18 +60,20 @@ def test_2d_energy_decrease():
     assert_array_less(meijering(a_white, black_ridges=False).std(),
                       a_white.std())
 
-    assert_array_less(sato(a_black, black_ridges=True).std(), a_black.std())
-    assert_array_less(sato(a_white, black_ridges=False).std(), a_white.std())
+    assert_array_less(sato(a_black, black_ridges=True, mode='reflect').std(),
+                      a_black.std())
+    assert_array_less(sato(a_white, black_ridges=False, mode='reflect').std(),
+                      a_white.std())
 
     assert_array_less(frangi(a_black, black_ridges=True).std(),
                       a_black.std())
     assert_array_less(frangi(a_white, black_ridges=False).std(),
                       a_white.std())
 
-    assert_array_less(hessian(a_black, black_ridges=True).std(),
-                      a_black.std())
-    assert_array_less(hessian(a_white, black_ridges=False).std(),
-                      a_white.std())
+    assert_array_less(hessian(a_black, black_ridges=True,
+                              mode='reflect').std(), a_black.std())
+    assert_array_less(hessian(a_white, black_ridges=False,
+                              mode='reflect').std(), a_white.std())
 
 
 def test_3d_energy_decrease():
@@ -85,18 +87,20 @@ def test_3d_energy_decrease():
     assert_array_less(meijering(a_white, black_ridges=False).std(),
                       a_white.std())
 
-    assert_array_less(sato(a_black, black_ridges=True).std(), a_black.std())
-    assert_array_less(sato(a_white, black_ridges=False).std(), a_white.std())
+    assert_array_less(sato(a_black, black_ridges=True, mode='reflect').std(),
+                      a_black.std())
+    assert_array_less(sato(a_white, black_ridges=False, mode='reflect').std(),
+                      a_white.std())
 
     assert_array_less(frangi(a_black, black_ridges=True).std(),
                       a_black.std())
     assert_array_less(frangi(a_white, black_ridges=False).std(),
                       a_white.std())
 
-    assert_array_less(hessian(a_black, black_ridges=True).std(),
-                      a_black.std())
-    assert_array_less(hessian(a_white, black_ridges=False).std(),
-                      a_white.std())
+    assert_array_less(hessian(a_black, black_ridges=True,
+                              mode='reflect').std(), a_black.std())
+    assert_array_less(hessian(a_white, black_ridges=False,
+                              mode='reflect').std(), a_white.std())
 
 
 def test_2d_linearity():
@@ -109,20 +113,24 @@ def test_2d_linearity():
     assert_allclose(meijering(1 * a_white, black_ridges=False),
                     meijering(10 * a_white, black_ridges=False), atol=1e-3)
 
-    assert_allclose(sato(1 * a_black, black_ridges=True),
-                    sato(10 * a_black, black_ridges=True), atol=1e-3)
-    assert_allclose(sato(1 * a_white, black_ridges=False),
-                    sato(10 * a_white, black_ridges=False), atol=1e-3)
+    assert_allclose(sato(1 * a_black, black_ridges=True, mode='reflect'),
+                    sato(10 * a_black, black_ridges=True, mode='reflect'),
+                    atol=1e-3)
+    assert_allclose(sato(1 * a_white, black_ridges=False, mode='reflect'),
+                    sato(10 * a_white, black_ridges=False, mode='reflect'),
+                    atol=1e-3)
 
     assert_allclose(frangi(1 * a_black, black_ridges=True),
                     frangi(10 * a_black, black_ridges=True), atol=1e-3)
     assert_allclose(frangi(1 * a_white, black_ridges=False),
                     frangi(10 * a_white, black_ridges=False), atol=1e-3)
 
-    assert_allclose(hessian(1 * a_black, black_ridges=True),
-                    hessian(10 * a_black, black_ridges=True), atol=1e-3)
-    assert_allclose(hessian(1 * a_white, black_ridges=False),
-                    hessian(10 * a_white, black_ridges=False), atol=1e-3)
+    assert_allclose(hessian(1 * a_black, black_ridges=True, mode='reflect'),
+                    hessian(10 * a_black, black_ridges=True, mode='reflect'),
+                    atol=1e-3)
+    assert_allclose(hessian(1 * a_white, black_ridges=False, mode='reflect'),
+                    hessian(10 * a_white, black_ridges=False, mode='reflect'),
+                    atol=1e-3)
 
 
 def test_3d_linearity():
@@ -135,20 +143,24 @@ def test_3d_linearity():
     assert_allclose(meijering(1 * a_white, black_ridges=False),
                     meijering(10 * a_white, black_ridges=False), atol=1e-3)
 
-    assert_allclose(sato(1 * a_black, black_ridges=True),
-                    sato(10 * a_black, black_ridges=True), atol=1e-3)
-    assert_allclose(sato(1 * a_white, black_ridges=False),
-                    sato(10 * a_white, black_ridges=False), atol=1e-3)
+    assert_allclose(sato(1 * a_black, black_ridges=True, mode='reflect'),
+                    sato(10 * a_black, black_ridges=True, mode='reflect'),
+                    atol=1e-3)
+    assert_allclose(sato(1 * a_white, black_ridges=False, mode='reflect'),
+                    sato(10 * a_white, black_ridges=False, mode='reflect'),
+                    atol=1e-3)
 
     assert_allclose(frangi(1 * a_black, black_ridges=True),
                     frangi(10 * a_black, black_ridges=True), atol=1e-3)
     assert_allclose(frangi(1 * a_white, black_ridges=False),
                     frangi(10 * a_white, black_ridges=False), atol=1e-3)
 
-    assert_allclose(hessian(1 * a_black, black_ridges=True),
-                    hessian(10 * a_black, black_ridges=True), atol=1e-3)
-    assert_allclose(hessian(1 * a_white, black_ridges=False),
-                    hessian(10 * a_white, black_ridges=False), atol=1e-3)
+    assert_allclose(hessian(1 * a_black, black_ridges=True, mode='reflect'),
+                    hessian(10 * a_black, black_ridges=True, mode='reflect'),
+                    atol=1e-3)
+    assert_allclose(hessian(1 * a_white, black_ridges=False, mode='reflect'),
+                    hessian(10 * a_white, black_ridges=False, mode='reflect'),
+                    atol=1e-3)
 
 
 def test_2d_cropped_camera_image():
@@ -162,14 +174,16 @@ def test_2d_cropped_camera_image():
     assert_allclose(meijering(a_black, black_ridges=True),
                     meijering(a_white, black_ridges=False))
 
-    assert_allclose(sato(a_black, black_ridges=True),
-                    sato(a_white, black_ridges=False))
+    assert_allclose(sato(a_black, black_ridges=True, mode='reflect'),
+                    sato(a_white, black_ridges=False, mode='reflect'))
 
     assert_allclose(frangi(a_black, black_ridges=True), zeros, atol=1e-3)
     assert_allclose(frangi(a_white, black_ridges=False), zeros, atol=1e-3)
 
-    assert_allclose(hessian(a_black, black_ridges=True), ones, atol=1 - 1e-7)
-    assert_allclose(hessian(a_white, black_ridges=False), ones, atol=1 - 1e-7)
+    assert_allclose(hessian(a_black, black_ridges=True, mode='reflect'),
+                    ones, atol=1 - 1e-7)
+    assert_allclose(hessian(a_white, black_ridges=False, mode='reflect'),
+                    ones, atol=1 - 1e-7)
 
 
 def test_3d_cropped_camera_image():
@@ -184,23 +198,25 @@ def test_3d_cropped_camera_image():
     assert_allclose(meijering(a_black, black_ridges=True),
                     meijering(a_white, black_ridges=False))
 
-    assert_allclose(sato(a_black, black_ridges=True),
-                    sato(a_white, black_ridges=False))
+    assert_allclose(sato(a_black, black_ridges=True, mode='reflect'),
+                    sato(a_white, black_ridges=False, mode='reflect'))
 
     assert_allclose(frangi(a_black, black_ridges=True), zeros, atol=1e-3)
     assert_allclose(frangi(a_white, black_ridges=False), zeros, atol=1e-3)
 
-    assert_allclose(hessian(a_black, black_ridges=True), ones, atol=1 - 1e-7)
-    assert_allclose(hessian(a_white, black_ridges=False), ones, atol=1 - 1e-7)
+    assert_allclose(hessian(a_black, black_ridges=True, mode='reflect'),
+                    ones, atol=1 - 1e-7)
+    assert_allclose(hessian(a_white, black_ridges=False, mode='reflect'),
+                    ones, atol=1 - 1e-7)
 
 
 @pytest.mark.parametrize('func, tol', [(frangi, 1e-7),
                                        (meijering, 1e-2),
-                                       (sato, 1e-3),
-                                       (hessian, 0.55)])
+                                       (sato, 1e-4),
+                                       (hessian, 2e-2)])
 def test_border_management(func, tol):
     img = rgb2gray(retina()[300:500, 700:900])
-    out = func(img, sigmas=[1])
+    out = func(img, sigmas=[1], mode='reflect')
 
     full_std = out.std()
     full_mean = out.mean()
