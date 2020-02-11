799d069b76ed393df7bacef06957bf88c344d5b0
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2020-02-11 17:13:45 -0600

    Implement lazy importerror for future.graph
    In Py3.7+, we can use __getattr__ for this purpose. Otherwise, we use a
    class inspired by nilearn TripWire.

--- skimage/future/__init__.py
+++ skimage/future/__init__.py
@@ -4,6 +4,9 @@ any version update **and will not follow the skimage two-version deprecation
 path**. Therefore, use the functions herein with care, and do not use them in
 production code that will depend on updated skimage versions.
 """
+import sys
+
+
 from .manual_segmentation import manual_polygon_segmentation
 from .manual_segmentation import manual_lasso_segmentation
 
@@ -14,18 +17,32 @@ _remove_error = (
     'https://scikit-image.org/docs/stable/api/skimage.segmentation.html'
 )
 
-class NotImported:
-    def __init__(self, error_message=_remove_error):
-        self.error_message = error_message
 
-    def __getattr__(self, item):
-        raise ImportError(self.error_message)
+if sys.version_info < (3, 7):  # can't use PEP-562
+    class NotImported:
+        def __init__(self, error_message=_remove_error):
+            self.error_message = error_message
+
+        def __getattr__(self, item):
+            raise ImportError(self.error_message)
 
 
-graph = NotImported(_remove_error)
+    graph = NotImported(_remove_error)
+    del NotImported
 
 
-del NotImported
+def __getattr__(name):
+    """PEP-562 implementation: raise ImportError for missing graph module."""
+    if name == 'graph':
+        raise ImportError(_remove_error)
+    else:
+        # note: this is not called for attributes present in the module.
+        raise AttributeError(
+            f"module 'skimage.future' has no attribute '{name}'"
+        )
+
+
+del sys
 
 __all__ = ['manual_lasso_segmentation',
            'manual_polygon_segmentation']
