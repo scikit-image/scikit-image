f7aa611a467f85151b3aebae1dc63ddce6f6e542
Riadh <rfezzani@gmail.com>
2020-02-12 00:07:59 +0100

    Fix pep8

--- benchmarks/benchmark_restoration.py
+++ benchmarks/benchmark_restoration.py
@@ -19,40 +19,40 @@ class RetorationSuite:
 
     def time_denoise_nl_means_f64(self):
         denoise_nl_means(self.volume_f64, patch_size=3, patch_distance=2,
-                         sigma=self.sigma, h=0.7*self.sigma,
+                         sigma=self.sigma, h=0.7 * self.sigma,
                          fast_mode=False)
 
     def time_denoise_nl_means_f32(self):
         denoise_nl_means(self.volume_f32, patch_size=3, patch_distance=2,
-                         sigma=self.sigma, h=0.7*self.sigma,
+                         sigma=self.sigma, h=0.7 * self.sigma,
                          fast_mode=False)
 
     def time_denoise_nl_means_fast_f64(self):
         denoise_nl_means(self.volume_f64, patch_size=3, patch_distance=2,
-                         sigma=self.sigma, h=0.7*self.sigma,
+                         sigma=self.sigma, h=0.7 * self.sigma,
                          fast_mode=True)
 
     def time_denoise_nl_means_fast_f32(self):
         denoise_nl_means(self.volume_f32, patch_size=3, patch_distance=2,
-                         sigma=self.sigma, h=0.7*self.sigma,
+                         sigma=self.sigma, h=0.7 * self.sigma,
                          fast_mode=True)
 
     def peakmem_denoise_nl_means_f64(self):
         denoise_nl_means(self.volume_f64, patch_size=3, patch_distance=2,
-                         sigma=self.sigma, h=0.7*self.sigma,
+                         sigma=self.sigma, h=0.7 * self.sigma,
                          fast_mode=False)
 
     def peakmem_denoise_nl_means_f32(self):
         denoise_nl_means(self.volume_f32, patch_size=3, patch_distance=2,
-                         sigma=self.sigma, h=0.7*self.sigma,
+                         sigma=self.sigma, h=0.7 * self.sigma,
                          fast_mode=False)
 
     def peakmem_denoise_nl_means_fast_f64(self):
         denoise_nl_means(self.volume_f64, patch_size=3, patch_distance=2,
-                         sigma=self.sigma, h=0.7*self.sigma,
+                         sigma=self.sigma, h=0.7 * self.sigma,
                          fast_mode=True)
 
     def peakmem_denoise_nl_means_fast_f32(self):
         denoise_nl_means(self.volume_f32, patch_size=3, patch_distance=2,
-                         sigma=self.sigma, h=0.7*self.sigma,
+                         sigma=self.sigma, h=0.7 * self.sigma,
                          fast_mode=True)
