5378b5ef5e325595d5d68dc4e9d983f5d16f81f7
Riadh <rfezzani@gmail.com>
2020-02-13 00:36:04 +0100

    Fix examples

--- skimage/segmentation/watershed.py
+++ skimage/segmentation/watershed.py
@@ -28,8 +28,9 @@ import numpy as np
 from scipy import ndimage as ndi
 
 from . import _watershed
-from .extrema import local_minima
-from ._util import _validate_connectivity, _offsets_to_raveled_neighbors
+from skimage.morphology.extrema import local_minima
+from skimage.morphology._util import (_validate_connectivity,
+                                      _offsets_to_raveled_neighbors)
 from ..util import crop, regular_seeds
 
 
