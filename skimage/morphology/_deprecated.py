214e99cfe639c6647e1637e6732d2c8523ffa39b
Riadh <r.fezzani@vitadx.com>
2020-02-13 11:22:54 +0100

    Fix doctest

--- skimage/morphology/_deprecated.py
+++ skimage/morphology/_deprecated.py
@@ -4,6 +4,7 @@ from warnings import warn
 def watershed(image, markers=None, connectivity=1, offset=None, mask=None,
               compactness=0, watershed_line=False):
     """Find watershed basins in `image` flooded from given `markers`.
+
     Parameters
     ----------
     image: ndarray (2-D, 3-D, ...) of integers
@@ -29,16 +30,19 @@ def watershed(image, markers=None, connectivity=1, offset=None, mask=None,
     watershed_line : bool, optional
         If watershed_line is True, a one-pixel wide line separates the regions
         obtained by the watershed algorithm. The line has the label 0.
+
     Returns
     -------
     out: ndarray
         A labeled matrix of the same type and shape as markers
+
     See also
     --------
     skimage.segmentation.random_walker: random walker segmentation
         A segmentation algorithm based on anisotropic diffusion, usually
         slower than the watershed but with good results on noisy data and
         boundaries with holes.
+
     Notes
     -----
     This function implements a watershed algorithm [1]_ [2]_ that apportions
@@ -59,6 +63,7 @@ def watershed(image, markers=None, connectivity=1, offset=None, mask=None,
     the local minima of the gradient of the image, or the local maxima of the
     distance function to the background for separating overlapping objects
     (see example).
+
     References
     ----------
     .. [1] https://en.wikipedia.org/wiki/Watershed_%28image_processing%29
@@ -67,10 +72,13 @@ def watershed(image, markers=None, connectivity=1, offset=None, mask=None,
            Preemptive SLIC: On Improving Trade-offs of Superpixel Segmentation
            Algorithms. ICPR 2014, pp 996-1001. :DOI:`10.1109/ICPR.2014.181`
            https://www.tu-chemnitz.de/etit/proaut/forschung/rsrc/cws_pSLIC_ICPR.pdf
+
     Examples
     --------
     The watershed algorithm is useful to separate overlapping objects.
+
     We first generate an initial image with two overlapping circles:
+
     >>> import numpy as np
     >>> x, y = np.indices((80, 80))
     >>> x1, y1, x2, y2 = 28, 28, 44, 52
@@ -78,8 +86,10 @@ def watershed(image, markers=None, connectivity=1, offset=None, mask=None,
     >>> mask_circle1 = (x - x1)**2 + (y - y1)**2 < r1**2
     >>> mask_circle2 = (x - x2)**2 + (y - y2)**2 < r2**2
     >>> image = np.logical_or(mask_circle1, mask_circle2)
+
     Next, we want to separate the two circles. We generate markers at the
     maxima of the distance to the background:
+
     >>> from scipy import ndimage as ndi
     >>> distance = ndi.distance_transform_edt(image)
     >>> from skimage.feature import peak_local_max
@@ -87,8 +97,11 @@ def watershed(image, markers=None, connectivity=1, offset=None, mask=None,
     ...                             footprint=np.ones((3, 3)),
     ...                             indices=False)
     >>> markers = ndi.label(local_maxi)[0]
+
     Finally, we run the watershed on the image and markers:
+
     >>> labels = watershed(-distance, markers, mask=image)
+
     The algorithm works also for 3-D images, and can be used for example to
     separate overlapping spheres.
     """
