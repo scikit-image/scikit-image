df8764eef9a44b69fe40eba97c5c1b9538349758
erjel <36883188+erjel@users.noreply.github.com>
2020-02-15 23:15:25 +0100

    Apply suggestions from code review
    Allocate channel output array prior to loop.

--- skimage/restoration/_denoise.py
+++ skimage/restoration/_denoise.py
@@ -290,13 +290,13 @@ def denoise_tv_bregman(image, weight, max_iter=100, eps=1e-3, isotropic=True,
     shape_ext = (rows + 2, cols + 2, dims)
 
     out = np.zeros(shape_ext, image.dtype)
+    channel_out = np.zeros(shape_ext[:2] + (1,), dtype=out.dtype)
 
     if multichannel:
         for c in range(image.shape[-1]):
             # the algorithm below expects 3 dimensions to always be present.
             # slicing the array in this fashion preserves the channel dimension for us
             channel_in = np.ascontiguousarray(image[..., c:c+1])
-            channel_out = np.zeros(shape_ext[:2] + (1,), dtype=out.dtype)
 
             _denoise_tv_bregman(channel_in, image.dtype.type(weight),
                                 max_iter, eps, isotropic, channel_out)
