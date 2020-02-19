c2e63495fda3fa4a65fe653f2c576b3068161065
Henry Eigen <eigen.henry@gmail.com>
2020-02-19 14:06:49 -0500

    made PEP compliant

--- skimage/transform/ssim_optimize.py
+++ skimage/transform/ssim_optimize.py
@@ -3,8 +3,10 @@ from numpy.linalg import norm
 
 from ..metrics._structural_similarity import structural_similarity as ssim
 
+
 def E(X, Y):
     return (Y - X) / norm(Y - X)
+
 
 def optimize_func(minimize, img, _lambda, iters, sigma):
     if len(img.shape) != 3:
@@ -12,13 +14,13 @@ def optimize_func(minimize, img, _lambda, iters, sigma):
 
     """
     Compute the image with the highest or lowest SSIM to a reference image
-    on the L-2 ball of the reference image. Follows a two step iterative 
-    gradient descent procedure. 
+    on the L-2 ball of the reference image. Follows a two step iterative
+    gradient descent procedure.
 
     Parameters
     ----------
     minimize: bool
-        If True, image with lowest ssim is found. If False, highest ssim is found
+        If True, image with lowest ssim is found. Else, highest ssim is found
     img: ndarray
         The reference image
     _lambda: float
@@ -32,10 +34,10 @@ def optimize_func(minimize, img, _lambda, iters, sigma):
     -------
     adv_x: ndarray
         Optimized image
- 
+
     Notes
     ------
-    First step of algorithm is modified to increase memory efficiency. Behavior 
+    First step of algorithm is modified to increase memory efficiency. Behavior
     of the algorithm is unchanged.
 
     References
@@ -49,40 +51,39 @@ def optimize_func(minimize, img, _lambda, iters, sigma):
 
     """
 
-
     if len(img.shape) != 3:
         raise ValueError(
             "Array should have dimensions (width, height, channel)\n")
 
     if np.max(img) > 1.0:
         raise ValueError(
-            "Array values should me in range [0, 1]")     
-        
-    noise = np.random.randint(0, 255, size = img.shape) / 255.
-    noise = noise * _lambda / norm(noise)
+            "Array values should me in range [0, 1]")
+
+    noise = np.random.randint(0, 255, size = img.shape)/255.
+    noise = noise * _lambda/norm(noise)
     adv_x = np.clip(img + noise, 0, 1)
-    
+
     prev_ssim = ssim(img, adv_x, multichannel=True)
     prev_img = np.copy(adv_x)
-    
+
     for i in range(iters):
         # ----------------- step 1 -----------------
         grad = ssim(img, adv_x, multichannel=True, gradient=True)[1].flatten()
-        grad = np.expand_dims(grad, axis=1) 
+        grad = np.expand_dims(grad, axis=1)
 
         diff = E(img, adv_x).flatten()
-        diff = np.expand_dims(diff, axis=1) 
+        diff = np.expand_dims(diff, axis=1)
 
         diff_t = diff.T
 
-        update = sigma * (grad - diff.dot( (diff_t.dot(grad)) ))
+        update = sigma * (grad - diff.dot((diff_t.dot(grad))))
         update = update.reshape(adv_x.shape)
 
         if minimize:
             adv_x = adv_x - update
         else:
             adv_x = adv_x + update
-            
+
         # ----------------- step 2 -----------------
         adv_x = np.clip(img + _lambda * E(img, adv_x), 0, 1)
 
@@ -103,15 +104,15 @@ def optimize_func(minimize, img, _lambda, iters, sigma):
             else:
                 prev_img = np.copy(adv_x)
                 prev_ssim = ssim_score
-            
+
     return adv_x
-    
+
 
 def minimize_ssim(img, _lambda, iters=50, sigma=30):
     adv_x = optimize_func(True, img, _lambda, iters, sigma)
     return adv_x
-    
+
+
 def maximize_ssim(img, _lambda, iters=50, sigma=30):
     adv_x = optimize_func(False, img, _lambda, iters, sigma)
     return adv_x
-    
