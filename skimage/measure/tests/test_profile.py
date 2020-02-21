6d1cd22eb2b7bce986f24313acfbe04e4313ac14
Riadh <r.fezzani@vitadx.com>
2020-02-21 19:16:37 +0100

    Fix imports in test_profile

--- skimage/measure/tests/test_profile.py
+++ skimage/measure/tests/test_profile.py
@@ -1,7 +1,7 @@
 import numpy as np
-from skimage.measure import profile_line
 
-from skimage._shared.testing import assert_equal, assert_almost_equal
+from ..._shared.testing import assert_equal, assert_almost_equal
+from ..profile import profile_line
 
 
 image = np.arange(100).reshape((10, 10)).astype(np.float)
