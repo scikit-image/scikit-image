85700403afa352a42fae4265f820c1dbe36a4ccc
Riadh <r.fezzani@vitadx.com>
2020-02-25 12:26:35 +0100

    Add test for _set_order function

--- skimage/_shared/tests/test_utils.py
+++ skimage/_shared/tests/test_utils.py
@@ -1,9 +1,10 @@
 import sys
-from skimage._shared.utils import check_nD, deprecate_kwarg
-import numpy.testing as npt
-import numpy as np
-from skimage._shared import testing
 import pytest
+import numpy as np
+import numpy.testing as npt
+from ..utils import check_nD, deprecate_kwarg, _set_order
+from .. import testing
+from .._warnings import expected_warnings
 
 
 def test_deprecated_kwarg():
@@ -57,6 +58,26 @@ def test_check_nD():
     x = z[10:30, 30:10]
     with testing.raises(ValueError):
         check_nD(x, 2)
+
+
+@pytest.mark.parametrize('dtype', [bool, int, np.uint8, np.uint16,
+                                   float, np.float32, np.float64])
+@pytest.mark.parametrize('order', [None, -1, 0, 1, 2, 3, 4, 5, 6])
+def test_set_order(dtype, order):
+    if order is None:
+        # Default order
+        assert _set_order(dtype, None) == 0 if dtype == bool else 1
+    elif order < 0 or order > 5:
+        # Order not in valid range
+        with testing.raises(ValueError):
+            _set_order(dtype, order)
+    elif dtype == bool and order != 0:
+        # Deprecated order for bool array
+        with expected_warnings(["Input image dtype is bool"]):
+            assert _set_order(bool, order) == order
+    else:
+        # Valid use case
+        assert _set_order(dtype, order) == order
 
 
 if __name__ == "__main__":
