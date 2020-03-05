5b80d3f95af125d781ae42000ecb464fbb895e2a
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-03-05 16:44:08 +0100

    added blank line in test random shapes

--- skimage/draw/tests/test_random_shapes.py
+++ skimage/draw/tests/test_random_shapes.py
@@ -106,6 +106,7 @@ def test_generate_circle_throws_when_size_too_small():
         random_shapes(
             (64, 128), max_shapes=1, min_size=1, max_size=1, shape='circle')
 
+
 def test_generate_ellipse_throws_when_size_too_small():
     with testing.raises(ValueError):
         random_shapes(
