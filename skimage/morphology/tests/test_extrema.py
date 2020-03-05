feb4aee2dbc594a15549aee092a599f0db5fa1a6
Hazen Babcock <hbabcock@mac.com>
2020-03-05 15:13:17 -0500

    Fix additional PEP8 style issues.

--- skimage/morphology/tests/test_extrema.py
+++ skimage/morphology/tests/test_extrema.py
@@ -175,7 +175,7 @@ class TestExtrema(unittest.TestCase):
         """specific tests for h-maxima float image type"""
         w = 10
         x, y = np.mgrid[0:w, 0:w]
-        data = 20 - 0.2*((x - w/2)**2 + (y-w/2)**2)
+        data = 20 - 0.2 * ((x - w / 2) ** 2 + (y - w / 2) ** 2)
         data[2:4, 2:4] = 40
         data[2:4, 7:9] = 60
         data[7:9, 2:4] = 80
@@ -232,7 +232,7 @@ class TestExtrema(unittest.TestCase):
         """specific tests for h-minima float image type"""
         w = 10
         x, y = np.mgrid[0:w, 0:w]
-        data = 180 + 0.2*((x - w/2)**2 + (y-w/2)**2)
+        data = 180 + 0.2 * ((x - w / 2) ** 2 + (y - w / 2) ** 2)
         data[2:4, 2:4] = 160
         data[2:4, 7:9] = 140
         data[7:9, 2:4] = 120
