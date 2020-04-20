8a8a0efd34a6c0a6b99d114912c21b42ed245a45
Zhōu Bówēi 周伯威 <github@bowei.me>
2020-04-20 22:35:08 +0900

    Change 3D plot method.
    Modify visulization parameters;
    change 3D plot method;
    apply @rfezzani 's  suggestions.
    
    Co-Authored-By: rfezzani <rfezzani@gmail.com>

--- doc/examples/numpy_operations/plot_structuring_elements.py
+++ doc/examples/numpy_operations/plot_structuring_elements.py
@@ -3,7 +3,7 @@
 Generate structuring elements
 =============================
 
-This example shows how to use functions in :py:module:`skimage.morphology`
+This example shows how to use functions in :py:mod:`skimage.morphology`
 to generate structuring elements.
 The title of each plot indicates the call of the function.
 
@@ -17,18 +17,17 @@ from skimage.morphology import (square, rectangle, diamond, disk, cube,
                                 octahedron, ball, octagon, star)
 
 # Generate 2D and 3D structuring elements.
-# [(title, data)]
 struc_2d = {
-    "square(5)": square(5),
-    "rectangle(5, 3)": rectangle(5, 3),
-    "diamond(5)": diamond(5),
+    "square(15)": square(15),
+    "rectangle(15, 10)": rectangle(15, 10),
+    "diamond(7)": diamond(7),
     "disk(7)": disk(7),
-    "octagon(4, 7)": octagon(4, 7),
-    "star(6)": star(6)
+    "octagon(7, 4)": octagon(7, 4),
+    "star(5)": star(5)
 }
 
 struc_3d = {
-    "cube(5)": cube(5),
+    "cube(11)": cube(11),
     "octahedron(5)": octahedron(5),
     "ball(5)": ball(5)
 }
@@ -37,8 +36,7 @@ struc_3d = {
 fig = plt.figure(figsize=(8, 8))
 
 idx = 1
-for title in struc_2d:
-    struc = struc_2d[title]
+for title, struc in struc_2d.items():
     ax = fig.add_subplot(3, 3, idx)
     ax.imshow(struc, cmap="Paired", vmin=0, vmax=12)
     for i in range(struc.shape[0]):
@@ -48,11 +46,9 @@ for title in struc_2d:
     ax.set_title(title)
     idx += 1
 
-for title in struc_3d:
-    struc = struc_3d[title]
+for title, struc in struc_3d.items():
     ax = fig.add_subplot(3, 3, idx, projection=Axes3D.name)
-    x, y, z = np.where(struc == 1)
-    ax.scatter(x, y, z)
+    ax.voxels(struc)
     ax.set_title(title)
     idx += 1
 
