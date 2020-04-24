43d653f315a64b83ecec8129591b9a84ebc8ea18
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2020-04-24 06:18:57 -0500

    Inverse map dtype should actually map the input dtype

--- skimage/segmentation/tests/test_join.py
+++ skimage/segmentation/tests/test_join.py
@@ -121,7 +121,8 @@ def test_relabel_sequential_int_dtype_overflow():
     offset = 254
     ar_relab, fw, inv = relabel_sequential(ar, offset=offset)
     _check_maps(ar, ar_relab, fw, inv)
-    assert all(a.dtype == np.uint16 for a in (ar_relab, fw, inv))
+    assert all(a.dtype == np.uint16 for a in (ar_relab, fw))
+    assert inv.dtype == ar.dtype
     ar_relab_ref = np.where(ar > 0, ar.astype(np.int) + offset - 1, 0)
     assert_array_equal(ar_relab, ar_relab_ref)
 
