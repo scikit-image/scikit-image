23acc634ba5a6bfe0d9fe426022d78b0d028a3e8
Volker Hilsenstein <volker.hilsenstein@gmail.com>
2020-04-27 11:49:38 +1000

    adjusted sample array to fix doctest
    Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

--- skimage/segmentation/_join.py
+++ skimage/segmentation/_join.py
@@ -202,7 +202,7 @@ class ArrayMap:
     The issue with this indexing is that you need a very large ``values``
     array if the values in the ``indices`` array are large.
 
-    >>> values = np.array([0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0])
+    >>> values = np.array([0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0])
     >>> indices = np.array([[0, 0, 10], [0, 10, 10]])
     >>> values[indices]
     array([[0.25, 0.25, 1.  ],
