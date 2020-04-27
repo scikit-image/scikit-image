bae1eabeb3b26043938a7c97cef6a95006baced1
Volker Hilsenstein <volker.hilsenstein@gmail.com>
2020-04-27 11:35:13 +1000

    added missing paren in docstring

--- skimage/segmentation/_join.py
+++ skimage/segmentation/_join.py
@@ -203,7 +203,7 @@ class ArrayMap:
     array if the values in the ``indices`` array are large.
 
     >>> values = np.array([0.25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0])
-    >>> indices = np.array([[0, 0, 10], [0, 10, 10]]
+    >>> indices = np.array([[0, 0, 10], [0, 10, 10]])
     >>> values[indices]
     array([[0.25, 0.25, 1.  ],
            [0.25, 1.  , 1.  ]])
