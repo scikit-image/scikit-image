7082650a9855671b0ff52c7fe1999bcd585e5c4d
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2020-04-30 06:04:19 -0500

    Test discontiguous output array

--- skimage/segmentation/tests/test_join.py
+++ skimage/segmentation/tests/test_join.py
@@ -189,9 +189,18 @@ def test_incorrect_input_dtype():
         _ = relabel_sequential(labels)
 
 
-def test_incorrect_map_array_output_shape():
+def test_map_array_incorrect_output_shape():
     labels = np.random.randint(0, 5, size=(24, 25))
     out = np.empty((24, 24))
+    in_values = np.unique(labels)
+    out_values = np.random.random(in_values.shape).astype(out.dtype)
+    with testing.raises(ValueError):
+        map_array(labels, in_values, out_values, out=out)
+
+
+def test_map_array_non_contiguous_output_array():
+    labels = np.random.randint(0, 5, size=(24, 25))
+    out = np.empty((24 * 3, 25 * 2))[::3, ::2]
     in_values = np.unique(labels)
     out_values = np.random.random(in_values.shape).astype(out.dtype)
     with testing.raises(ValueError):
