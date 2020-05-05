343f42c66a48c529f5b65a89e78f4e97030231f3
Emmanuelle Gouillart <emma@plot.ly>
2020-05-05 12:23:44 +0200

    suppressed warnings in test

--- skimage/restoration/tests/test_j_invariant.py
+++ skimage/restoration/tests/test_j_invariant.py
@@ -1,3 +1,4 @@
+import functools
 import numpy as np
 
 from skimage._shared.testing import assert_
@@ -16,9 +17,10 @@ noisy_img = random_noise(test_img, mode='gaussian', var=0.01)
 noisy_img_color = random_noise(test_img_color, mode='gaussian', var=0.01)
 noisy_img_3d = random_noise(test_img_3d, mode='gaussian', var=0.1)
 
+_denoise_wavelet = functools.partial(denoise_wavelet, rescale_sigma=True)
 
 def test_invariant_denoise():
-    denoised_img = _invariant_denoise(noisy_img, denoise_wavelet)
+    denoised_img = _invariant_denoise(noisy_img, _denoise_wavelet)
 
     denoised_mse = mse(denoised_img, test_img)
     original_mse = mse(noisy_img, test_img)
@@ -27,7 +29,7 @@ def test_invariant_denoise():
 
 def test_invariant_denoise_color():
     denoised_img_color = _invariant_denoise(
-            noisy_img_color, denoise_wavelet,
+            noisy_img_color, _denoise_wavelet,
             denoiser_kwargs=dict(multichannel=True))
 
     denoised_mse = mse(denoised_img_color, test_img_color)
@@ -36,7 +38,7 @@ def test_invariant_denoise_color():
 
 
 def test_invariant_denoise_3d():
-    denoised_img_3d = _invariant_denoise(noisy_img_3d, denoise_wavelet)
+    denoised_img_3d = _invariant_denoise(noisy_img_3d, _denoise_wavelet)
 
     denoised_mse = mse(denoised_img_3d, test_img_3d)
     original_mse = mse(noisy_img_3d, test_img_3d)
@@ -47,12 +49,12 @@ def test_calibrate_denoiser_full_output():
     parameter_ranges = {'sigma': np.random.random(5) / 2}
     _, (parameters_tested, losses) = calibrate_denoiser(
         noisy_img,
-        denoise_wavelet,
+        _denoise_wavelet,
         denoise_parameters=parameter_ranges,
         full_output=True
     )
 
-    all_denoised = [_invariant_denoise(noisy_img, denoise_wavelet,
+    all_denoised = [_invariant_denoise(noisy_img, _denoise_wavelet,
                                        denoiser_kwargs=denoiser_kwargs)
                     for denoiser_kwargs in parameters_tested]
 
@@ -64,7 +66,7 @@ def test_calibrate_denoiser_full_output():
 def test_calibrate_denoiser():
     parameter_ranges = {'sigma': np.random.random(5) / 2}
 
-    denoiser = calibrate_denoiser(noisy_img, denoise_wavelet,
+    denoiser = calibrate_denoiser(noisy_img, _denoise_wavelet,
                                   denoise_parameters=parameter_ranges)
 
     denoised_mse = mse(denoiser(noisy_img), test_img)
@@ -76,7 +78,7 @@ def test_input_image_not_modified():
     input_image = noisy_img.copy()
 
     parameter_ranges = {'sigma': np.random.random(5) / 2}
-    calibrate_denoiser(input_image, denoise_wavelet,
+    calibrate_denoiser(input_image, _denoise_wavelet,
                        denoise_parameters=parameter_ranges)
 
     assert_(np.all(noisy_img == input_image))
