ad15736162a216e46834cb4722914bfa01aeb3ae
Joshua Batson <joshua.batson@czbiohub.org>
2020-05-06 05:48:31 -0700

    J-invariant denoising (#3824)
    * Initial commit of J-invariant code
    
    * Make interpolate_image n-dimensional.
    
    * Renamed grid_width to stride.
    
    * Remove cowboy option.
    
    * Keep dimensions.
    
    * Add j-invariant to public interface
    
    * Add tests for j-invariant.
    
    * Remove unused dependencies from j-invariant test
    
    * Replace 'reflect' with 'mirror' in convolution to preserve j-invariance.
    
    * Increased variance of 3d denoiser test to ensure default denoising works.
    
    * Refactor calibrate_denoiser to return a function. Add docstrings.
    
    * Add test for calibrate_denoiser
    
    * Add gallery example.
    
    * Make docs PEP8 compliant.
    
    * Speed up gallery example 2.5x by reusing list, trimming search space.
    
    * Use stars instead of underscore for italics in docs.
    
    * Make helper functions private
    
    * Update docstrings to be PEP257 compliant
    
    * Minor API and code tweaks
    
    * Use f-strings and single quotes
    
    * Update doc/examples/filters/plot_j_invariant.py
    
    Co-Authored-By: Emmanuelle Gouillart <emma@plot.ly>
    
    * Update skimage/restoration/j_invariant.py
    
    Co-Authored-By: François Boulogne <fboulogne@sciunto.org>
    
    * Update skimage/restoration/j_invariant.py
    
    Co-Authored-By: Emmanuelle Gouillart <emma@plot.ly>
    
    * removed calibrate_denoiser_search from public API
    
    * removed invariant_denoise from public API
    
    * improved docstring
    
    * updated citation
    
    * returning tuple + simple example
    
    * removed multichannel
    
    * fix docstring warning making CI fail
    
    * full tutorial
    
    * suppressed warnings in test
    
    * suppressed warnings in tutorials
    
    * removed random from test
    
    * full_output --> extra_output
    
    * Update skimage/restoration/j_invariant.py
    
    Co-authored-by: Riadh Fezzani <rfezzani@gmail.com>
    
    * Update skimage/restoration/j_invariant.py
    
    Co-authored-by: Riadh Fezzani <rfezzani@gmail.com>
    
    * Update skimage/restoration/j_invariant.py
    
    Co-authored-by: Riadh Fezzani <rfezzani@gmail.com>
    
    * Update skimage/restoration/j_invariant.py
    
    Co-authored-by: Riadh Fezzani <rfezzani@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Riadh Fezzani <rfezzani@gmail.com>
    
    * kw name
    
    * Update skimage/restoration/j_invariant.py
    
    Co-authored-by: Riadh Fezzani <rfezzani@gmail.com>
    
    * Update skimage/restoration/j_invariant.py
    
    Co-authored-by: Riadh Fezzani <rfezzani@gmail.com>
    
    * changed cython file
    
    * Swap incorrect argument order for psnr
    
    Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
    Co-authored-by: Emmanuelle Gouillart <emma@plot.ly>
    Co-authored-by: François Boulogne <fboulogne@sciunto.org>
    Co-authored-by: Riadh Fezzani <rfezzani@gmail.com>

---
+++ doc/examples/filters/plot_j_invariant.py
@@ -0,0 +1,73 @@
+"""
+========================================
+Calibrating Denoisers Using J-Invariance
+========================================
+
+In this example, we show how to find an optimally calibrated
+version of any denoising algorithm.
+
+The calibration method is based on the `noise2self` algorithm of [1]_.
+
+.. [1] J. Batson & L. Royer. Noise2Self: Blind Denoising by Self-Supervision,
+       International Conference on Machine Learning, p. 524-533 (2019).
+
+.. seealso::
+   More details about the method are given in the full tutorial
+   :ref:`sphx_glr_auto_examples_filters_plot_j_invariant_tutorial.py`.
+"""
+
+#####################################################################
+# Calibrating a wavelet denoiser
+
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import gridspec
+
+from skimage.data import chelsea, hubble_deep_field
+from skimage.metrics import mean_squared_error as mse
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from skimage.restoration import (calibrate_denoiser,
+                                 denoise_wavelet,
+                                 denoise_tv_chambolle, denoise_nl_means,
+                                 estimate_sigma)
+from skimage.util import img_as_float, random_noise
+from skimage.color import rgb2gray
+from functools import partial
+
+_denoise_wavelet = partial(denoise_wavelet, rescale_sigma=True)
+
+image = img_as_float(chelsea())
+sigma = 0.3
+noisy = random_noise(image, var=sigma ** 2)
+
+# Parameters to test when calibrating the denoising algorithm
+sigma_range = np.arange(0.05, 0.5, 0.05)
+
+parameter_ranges = {'sigma': np.arange(0.1, 0.3, 0.02),
+                    'wavelet': ['db1', 'db2'],
+                    'convert2ycbcr': [True, False],
+                    'multichannel': [True]}
+
+# Denoised image using default parameters of `denoise_wavelet`
+default_output = denoise_wavelet(noisy, multichannel=True, rescale_sigma=True)
+
+# Calibrate denoiser
+calibrated_denoiser = calibrate_denoiser(noisy,
+                                         _denoise_wavelet,
+                                         denoise_parameters=parameter_ranges)
+
+# Denoised image using calibrated denoiser
+calibrated_output = calibrated_denoiser(noisy)
+
+fig, axes = plt.subplots(1, 3, sharey=True, figsize=(15, 5))
+
+for ax, img, title in zip(axes,
+                          [noisy, default_output, calibrated_output],
+                          ['Noisy Image', 'Denoised (Default)',
+                           'Denoised (Calibrated)']):
+    ax.imshow(img)
+    ax.set_title(title)
+    ax.set_yticks([])
+    ax.set_xticks([])
+
+plt.show()
