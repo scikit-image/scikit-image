0a4e31d259453146f08aec787195554f6b5f37e7
Riadh Fezzani <rfezzani@gmail.com>
2020-05-07 09:06:52 +0200

    Update skimage/segmentation/slic_superpixels.py
    Co-authored-by: Gregory R. Lee <grlee77@gmail.com>

--- skimage/segmentation/slic_superpixels.py
+++ skimage/segmentation/slic_superpixels.py
@@ -189,7 +189,7 @@ def slic(image, n_segments=100, compactness=10., max_iter=10, sigma=0,
         Pascal Fua, and Sabine Süsstrunk, SLIC Superpixels Compared to
         State-of-the-art Superpixel Methods, TPAMI, May 2012.
         :DOI:`10.1109/TPAMI.2012.120`
-    .. [2] http://ivrg.epfl.ch/research/superpixels#SLICO
+    .. [2] https://www.epfl.ch/labs/ivrl/research/slic-superpixels/#SLICO
     .. [3] Irving, Benjamin. "maskSLIC: regional superpixel generation with
            application to local pathology characterisation in medical images.",
            2016, :arXiv:`1606.09518`
