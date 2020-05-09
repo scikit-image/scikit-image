80f82a63b748a0a1d21f700376afd875844ee6d3
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-09 11:34:48 -0700

    Revert "Forget legacy data dir (#4662)"
    This reverts commit 137f144cc39175a30c60f58c490eeafaf4bdb1a8.

--- skimage/data/__init__.py
+++ skimage/data/__init__.py
@@ -54,7 +54,8 @@ __all__ = ['data_dir',
            'shepp_logan_phantom',
            'stereo_motorcycle']
 
-skimage_distribution_dir = osp.join(osp.abspath(osp.dirname(__file__)), '..')
+legacy_data_dir = osp.abspath(osp.dirname(__file__))
+skimage_distribution_dir = osp.join(legacy_data_dir, '..')
 
 
 import pooch
@@ -167,6 +168,11 @@ def _fetch(data_filename):
 # Fetch all legacy data so that it is available by default
 for filename in legacy_registry:
     _fetch(filename)
+
+else:
+    # Without pooch, fallback on the standard data directory
+    # which for now, includes a few limited data samples
+    data_dir = legacy_data_dir
 
 
 def download_all(directory=None):
