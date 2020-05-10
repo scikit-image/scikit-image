7cc085f91046c7ddf44b685f922a263b71bcb9f9
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2020-05-10 11:16:38 +0200

    Use conventional DOI notation

--- doc/examples/applications/plot_human_mitosis.py
+++ doc/examples/applications/plot_human_mitosis.py
@@ -7,7 +7,8 @@ In this example, we analyze a microscopy image of human cells. We use data
 provided by Jason Moffat [1]_ through
 [CellProfiler](https://cellprofiler.org/examples/#human-cells).
 
-.. [1] Moffat J, Grueneberg DA, Yang X, Kim SY, Kloepfer AM, Hinkle G, Piqani B, Eisenhaure TM, Luo B, Grenier JK, Carpenter AE, Foo SY, Stewart SA, Stockwell BR, Hacohen N, Hahn WC, Lander ES, Sabatini DM, Root DE (2006) "A lentiviral RNAi library for human and mouse genes applied to an arrayed viral high-content screen" Cell, 124(6):1283-98. DOI: [10.1016/j.cell.2006.01.040](https://doi.org/10.1016/j.cell.2006.01.040). PMID: 16564017
+.. [1] Moffat J, Grueneberg DA, Yang X, Kim SY, Kloepfer AM, Hinkle G, Piqani B, Eisenhaure TM, Luo B, Grenier JK, Carpenter AE, Foo SY, Stewart SA, Stockwell BR, Hacohen N, Hahn WC, Lander ES, Sabatini DM, Root DE (2006) "A lentiviral RNAi library for human and mouse genes applied to an arrayed viral high-content screen" Cell, 124(6):1283-98. PMID: 16564017
+       :DOI:`10.1016/j.cell.2006.01.040`
 
 """
 
