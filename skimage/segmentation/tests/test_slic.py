35f8f1dade51c706c8d8bfe1b7ff98b9bafa0f2b
rfezzani <rfezzani@gmail.com>
2020-05-12 09:21:55 +0200

    Fix warning in test_dtype_support

--- skimage/segmentation/tests/test_slic.py
+++ skimage/segmentation/tests/test_slic.py
@@ -470,4 +470,4 @@ def test_gray_3d_mask():
 def test_dtype_support(dtype):
     img = np.random.rand(28, 28).astype(dtype)
 
-    assert slic(img).dtype == np.dtype(int)
+    assert slic(img, start_label=1).dtype == np.dtype(int)
