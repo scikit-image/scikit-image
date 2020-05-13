4ec23b9239fb2f939449f6721f6c89942ad1e101
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2020-05-12 23:23:32 -0700

    Backport PR #4692: suppressing warnings from gallery examples (#4699)
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>

--- doc/examples/features_detection/plot_brief.py
+++ doc/examples/features_detection/plot_brief.py
@@ -24,9 +24,12 @@ tform = transform.AffineTransform(scale=(1.2, 1.2), translation=(0, -100))
 img2 = transform.warp(img1, tform)
 img3 = transform.rotate(img1, 25)
 
-keypoints1 = corner_peaks(corner_harris(img1), min_distance=5)
-keypoints2 = corner_peaks(corner_harris(img2), min_distance=5)
-keypoints3 = corner_peaks(corner_harris(img3), min_distance=5)
+keypoints1 = corner_peaks(corner_harris(img1), min_distance=5,
+                          threshold_rel=0.1)
+keypoints2 = corner_peaks(corner_harris(img2), min_distance=5,
+                          threshold_rel=0.1)
+keypoints3 = corner_peaks(corner_harris(img3), min_distance=5,
+                          threshold_rel=0.1)
 
 extractor = BRIEF()
 
--- doc/examples/segmentation/plot_rag_mean_color.py
+++ doc/examples/segmentation/plot_rag_mean_color.py
@@ -15,12 +15,12 @@ from matplotlib import pyplot as plt
 
 img = data.coffee()
 
-labels1 = segmentation.slic(img, compactness=30, n_segments=400)
-out1 = color.label2rgb(labels1, img, kind='avg')
+labels1 = segmentation.slic(img, compactness=30, n_segments=400, start_label=1)
+out1 = color.label2rgb(labels1, img, kind='avg', bg_label=0)
 
 g = graph.rag_mean_color(img, labels1)
 labels2 = graph.cut_threshold(labels1, g, 29)
-out2 = color.label2rgb(labels2, img, kind='avg')
+out2 = color.label2rgb(labels2, img, kind='avg', bg_label=0)
 
 fig, ax = plt.subplots(nrows=2, sharex=True, sharey=True,
                        figsize=(6, 8))
