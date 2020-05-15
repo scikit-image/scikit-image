718e80453519cf34956d8ec1d62b4a6fa40035b2
François Boulogne <fboulogne@sciunto.org>
2020-05-15 15:40:20 +0200

    Update skimage/segmentation/active_contour_model.py

--- skimage/segmentation/active_contour_model.py
+++ skimage/segmentation/active_contour_model.py
@@ -98,7 +98,7 @@ def active_contour(image, snake, alpha=0.01, beta=0.1,
     """
     if coordinates != 'rc':
         raise ValueError('Coordinate values must be set in a row column '
-                         'format. `coordinates` must be indifferently set '
+                         'format. `coordinates` must be set '
                          'to "rc".')
     max_iterations = int(max_iterations)
     if max_iterations <= 0:
