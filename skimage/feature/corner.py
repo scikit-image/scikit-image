513844349ecaf62a7b0d09c08adad8909a2fde3b
rfezzani <rfezzani@gmail.com>
2020-05-28 09:31:34 +0200

    Fix corner_peaks doctest

--- skimage/feature/corner.py
+++ skimage/feature/corner.py
@@ -970,10 +970,7 @@ def corner_peaks(image, min_distance=1, threshold_abs=None, threshold_rel=None,
            [0., 0., 1., 1., 0.],
            [0., 0., 0., 0., 0.]])
     >>> peak_local_max(response)
-    array([[2, 2],
-           [2, 3],
-           [3, 2],
-           [3, 3]])
+    array([[2, 2]])
     >>> corner_peaks(response, threshold_rel=0)
     array([[2, 2]])
 
