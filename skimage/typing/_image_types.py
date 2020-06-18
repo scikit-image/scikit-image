a53142e8d8f8cade27944a336a49b01dd228fe61
Emmanuelle Gouillart <emma@plot.ly>
2020-06-18 20:51:47 +0200

    debug

--- skimage/typing/_image_types.py
+++ skimage/typing/_image_types.py
@@ -1,19 +1,7 @@
-import sys
 from ._array_like import ArrayLike
 import numpy as np
-from typing import Type, Any
 
-if sys.version_info >= (3, 8):
-    from typing import Annotated
-    HAVE_ANNOTATED = True
-else:
-    try:
-        from typing_extensions import Annotated
-    except ImportError:
-        HAVE_ANNOTATED = False
-    else:
-        HAVE_ANNOTATED = True
-
+from typing_extensions import Annotated
 
 ImageArray = Annotated[np.ndarray, 'image',
                         'a 2D or 3D single or multichannel image']
