bcdc3a43f9020fd37f2385fe3205fc85ad89acd4
Emmanuelle Gouillart <emma@plot.ly>
2020-06-27 17:00:09 +0200

    Update skimage/segmentation/random_walker_segmentation.py
    Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

--- skimage/segmentation/random_walker_segmentation.py
+++ skimage/segmentation/random_walker_segmentation.py
@@ -264,12 +264,19 @@ def _preprocess(labels):
     return labels, nlabels, mask, inds_isolated_seeds, isolated_values
 
 
-def random_walker(data: ImageArray, labels: LabelsArray, beta: float = 130,
-                  mode: Literal['cg', 'cg_j', 'cg_mg', 'bf'] = 'cg_j',
-                  tol: float = 1.e-3, copy: bool = True,
-                  multichannel: bool = False, return_full_prob: bool = False,
-                  spacing: Optional[Sequence] = None, *,
-                  prob_tol: float = 1e-3) -> LabelsArray:
+def random_walker(
+    data: ImageArray,
+    labels: LabelsArray,
+    beta: float = 130,
+    mode: Literal['cg', 'cg_j', 'cg_mg', 'bf'] = 'cg_j',
+    tol: float = 1.e-3,
+    copy: bool = True,
+    multichannel: bool = False,
+    return_full_prob: bool = False,
+    spacing: Optional[Sequence] = None,
+    *,
+    prob_tol: float = 1e-3,
+) -> LabelsArray:
     """Random walker algorithm for segmentation from markers.
 
     Random walker algorithm is implemented for gray-level or multichannel
