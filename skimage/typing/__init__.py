8ec91b1e689fdacec602643f5326d75956886dbb
Emmanuelle Gouillart <emma@plot.ly>
2020-06-29 10:41:22 +0200

    changed all of typing subpackage so that sphinx is not confused

--- skimage/typing/__init__.py
+++ skimage/typing/__init__.py
@@ -4,7 +4,6 @@ from ._shape import _Shape, _ShapeLike
 from ._dtype_like import DtypeLike
 from ._image_types import (Image, Image2D, Image3D, Labels, Mask)
 
-# from typing import Any
 if sys.version_info >= (3, 8):
     from typing import Literal
 else:
@@ -12,10 +11,5 @@ else:
 
 
 __all__ = [
-        'Image',
-        'Image2D',
-        'Image3D',
-        'Labels',
-        'Mask',
         '_SupportsArray'
         ]
