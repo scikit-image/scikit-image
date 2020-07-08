69dbf31ba08ee5a7d784c8911e487d3ca54f9693
Stanley <apple@Apples-MacBook-Pro.local>
2020-07-08 00:58:27 -0400

    finish refactoring core_cy.pyx

--- skimage/filters/rank/core_cy.pyx
+++ skimage/filters/rank/core_cy.pyx
@@ -30,6 +30,120 @@ cdef inline void histogram_decrement(Py_ssize_t[::1] histo, double* pop,
     pop[0] -= 1
 
 
+cdef inline void _count_attack_border_elements(char[:, :, ::1] selem,
+                                               Py_ssize_t [:, :, ::1] se,
+                                               Py_ssize_t [::1] num_se,
+                                               Py_ssize_t splanes,
+                                               Py_ssize_t srows,
+                                               Py_ssize_t scols, 
+                                               Py_ssize_t centre_p,
+                                               Py_ssize_t centre_r,
+                                               Py_ssize_t centre_c):
+
+    # build attack and release borders by using difference along axis
+    t = np.dstack((selem, np.zeros((selem.shape[0], selem.shape[1], 1))))
+    cdef unsigned char[:, :, :] t_e = (np.diff(t, axis=2) < 0).view(np.uint8)
+
+    t = np.dstack((np.zeros((selem.shape[0], selem.shape[1], 1)), selem))
+    cdef unsigned char[:, :, :] t_w = (np.diff(t, axis=2) > 0).view(np.uint8)
+
+    t = np.hstack((selem, np.zeros((selem.shape[0], 1, selem.shape[2]))))
+    cdef unsigned char[:, :, :] t_s = (np.diff(t, axis=1) < 0).view(np.uint8)
+
+    t = np.hstack((np.zeros((selem.shape[0], 1, selem.shape[2])), selem))
+    cdef unsigned char[:, :, :] t_n = (np.diff(t, axis=1) > 0).view(np.uint8)
+
+    for r in range(srows):
+        for c in range(scols):
+            for p in range(splanes):
+                if t_e[p, r, c]:
+                    se[0, 0, num_se[0]] = p - centre_p
+                    se[0, 1, num_se[0]] = r - centre_r
+                    se[0, 2, num_se[0]] = c - centre_c
+                    num_se[0] += 1
+                if t_n[p, r, c]:
+                    se[1, 0, num_se[1]] = p - centre_p
+                    se[1, 1, num_se[1]] = r - centre_r
+                    se[1, 2, num_se[1]] = c - centre_c
+                    num_se[1] += 1
+                if t_w[p, r, c]:
+                    se[2, 0, num_se[2]] = p - centre_p
+                    se[2, 1, num_se[2]] = r - centre_r
+                    se[2, 2, num_se[2]] = c - centre_c
+                    num_se[2] += 1
+                if t_s[p, r, c]:
+                    se[3, 0, num_se[3]] = p - centre_p
+                    se[3, 1, num_se[3]] = r - centre_r
+                    se[3, 2, num_se[3]] = c - centre_c
+                    num_se[3] += 1
+
+
+cdef inline void _build_initial_histogram_from_neighborhood(dtype_t[:, :, ::1] image,
+                                                            char[:, :, ::1] selem,
+                                                            Py_ssize_t [::1] histo,
+                                                            double* pop,
+                                                            char* mask_data,
+                                                            Py_ssize_t p,
+                                                            Py_ssize_t planes,
+                                                            Py_ssize_t rows,
+                                                            Py_ssize_t cols, 
+                                                            Py_ssize_t splanes,
+                                                            Py_ssize_t srows,
+                                                            Py_ssize_t scols, 
+                                                            Py_ssize_t centre_p,
+                                                            Py_ssize_t centre_r,
+                                                            Py_ssize_t centre_c):
+    for r in range(srows):
+        for c in range(scols):
+            for j in range(splanes):
+                pp = j - centre_p + p
+                rr = r - centre_r
+                cc = c - centre_c
+
+                if selem[j, r, c]:
+                    if is_in_mask_3D(rows, cols, planes, rr, cc, pp,
+                                     mask_data):
+                        # histogram_increment(histo, pop, image[pp, rr, cc])
+                        histo[image[pp, rr, cc]] += 1
+                        pop[0] += 1
+
+
+cdef inline void _update_histogram(dtype_t[:, :, ::1] image,
+                                   Py_ssize_t [:, :, ::1] se,
+                                   Py_ssize_t [::1] num_se,
+                                   Py_ssize_t [::1] histo,
+                                   double* pop, char* mask_data,
+                                   Py_ssize_t p, Py_ssize_t r, Py_ssize_t c,
+                                   Py_ssize_t planes, Py_ssize_t rows,
+                                   Py_ssize_t cols,
+                                   Py_ssize_t axis_inc):
+    # Increment histogram
+    for j in range(num_se[axis_inc]):
+        pp = p + se[axis_inc, 0, j]
+        rr = r + se[axis_inc, 1, j]
+        cc = c + se[axis_inc, 2, j]
+        if is_in_mask_3D(rows, cols, planes, rr, cc, pp,
+                         mask_data):
+            histo[image[pp, rr, cc]] += 1
+            pop[0] += 1
+
+    # Decrement histogram
+    axis_dec = (axis_inc + 2) % 4
+    for j in range(num_se[axis_dec]):
+        pp = p + se[axis_dec, 0, j]
+        rr = r + se[axis_dec, 1, j]
+        cc = c + se[axis_dec, 2, j]
+        if axis_dec == 2:
+            cc -= 1
+        elif axis_dec == 1:
+            rr -= 1
+        elif axis_dec == 0:
+            cc += 1
+        if is_in_mask_3D(rows, cols, planes, rr, cc, pp,
+                         mask_data):
+            histo[image[pp, rr, cc]] -= 1
+            pop[0] -= 1
+
 
 cdef inline char is_in_mask(Py_ssize_t rows, Py_ssize_t cols,
                             Py_ssize_t r, Py_ssize_t c,
@@ -296,97 +410,34 @@ cdef void _core_3D(void kernel(dtype_t_out*, Py_ssize_t, Py_ssize_t[::1], double
     cdef char* mask_data = &mask[0, 0, 0]
 
     # define local variable types
-    cdef Py_ssize_t r, c, p, j, rr, cc, pp, value, local_max, i, even_row
+    cdef Py_ssize_t p, r, c, j, rr, cc, pp, value, local_max, i, even_row
 
     # number of pixels actually inside the neighborhood (double)
     cdef double pop = 0
 
-    # build attack and release borders by using difference along axis
-    t = np.dstack((selem, np.zeros((selem.shape[0], selem.shape[1], 1))))
-    cdef unsigned char[:, :, :] t_e = (np.diff(t, axis=2) < 0).view(np.uint8)
-
-    t = np.dstack((np.zeros((selem.shape[0], selem.shape[1], 1)), selem))
-    cdef unsigned char[:, :, :] t_w = (np.diff(t, axis=2) > 0).view(np.uint8)
-
-    t = np.hstack((selem, np.zeros((selem.shape[0], 1, selem.shape[2]))))
-    cdef unsigned char[:, :, :] t_s = (np.diff(t, axis=1) < 0).view(np.uint8)
-
-    t = np.hstack((np.zeros((selem.shape[0], 1, selem.shape[2])), selem))
-    cdef unsigned char[:, :, :] t_n = (np.diff(t, axis=1) > 0).view(np.uint8)
-
     # the current local histogram distribution
-    # cdef Py_ssize_t* histo
     cdef Py_ssize_t [::1] histo = np.zeros(n_bins, dtype=np.intp)
 
-    # these lists contain the relative pixel row and column for each of the 4
-    # attack borders east, west, north and south e.g. se_e_r lists the rows of
-    # the east structuring element border
-    cdef Py_ssize_t se_size = srows * scols * splanes
-    cdef Py_ssize_t [::1] se_e_r = np.empty(se_size, dtype=np.intp)
-    cdef Py_ssize_t [::1] se_e_c = np.empty(se_size, dtype=np.intp)
-    cdef Py_ssize_t [::1] se_e_p = np.empty(se_size, dtype=np.intp)
+    # these lists contain the relative pixel plane, row and column for each of
+    # the 4 attack borders east, north, west and south
+    # e.g. se[0, 0, :] lists the planes of the east structuring element border
+    cdef Py_ssize_t se_size = splanes * srows * scols
+    cdef Py_ssize_t [:, :, ::1] se = np.zeros([4, 3, se_size], dtype=np.intp)
 
-    cdef Py_ssize_t [::1] se_w_p = np.empty(se_size, dtype=np.intp)
-    cdef Py_ssize_t [::1] se_w_r = np.empty(se_size, dtype=np.intp)
-    cdef Py_ssize_t [::1] se_w_c = np.empty(se_size, dtype=np.intp)
-
-    cdef Py_ssize_t [::1] se_n_p = np.empty(se_size, dtype=np.intp)
-    cdef Py_ssize_t [::1] se_n_r = np.empty(se_size, dtype=np.intp)
-    cdef Py_ssize_t [::1] se_n_c = np.empty(se_size, dtype=np.intp)
-
-    cdef Py_ssize_t [::1] se_s_p = np.empty(se_size, dtype=np.intp)
-    cdef Py_ssize_t [::1] se_s_r = np.empty(se_size, dtype=np.intp)
-    cdef Py_ssize_t [::1] se_s_c = np.empty(se_size, dtype=np.intp)
-
-    # number of element in each attack border
-    cdef Py_ssize_t num_se_n, num_se_s, num_se_e, num_se_w, num_se_u, num_se_d
-
-
-    num_se_n = num_se_s = num_se_e = num_se_w = 0
-
-    for r in range(srows):
-        for c in range(scols):
-            for p in range(splanes):
-                if t_e[p, r, c]:
-                    se_e_r[num_se_e] = r - centre_r
-                    se_e_c[num_se_e] = c - centre_c
-                    se_e_p[num_se_e] = p - centre_p
-                    num_se_e += 1
-                if t_w[p, r, c]:
-                    se_w_r[num_se_w] = r - centre_r
-                    se_w_c[num_se_w] = c - centre_c
-                    se_w_p[num_se_w] = p - centre_p
-                    num_se_w += 1
-                if t_n[p, r, c]:
-                    se_n_r[num_se_n] = r - centre_r
-                    se_n_c[num_se_n] = c - centre_c
-                    se_n_p[num_se_n] = p - centre_p
-                    num_se_n += 1
-                if t_s[p, r, c]:
-                    se_s_r[num_se_s] = r - centre_r
-                    se_s_c[num_se_s] = c - centre_c
-                    se_s_p[num_se_s] = p - centre_p
-                    num_se_s += 1
+    # number of element in each attack border in 4 directions
+    cdef Py_ssize_t [::1] num_se = np.zeros(4, dtype=np.intp)
+    
+    _count_attack_border_elements(selem, se, num_se, splanes, srows, scols, 
+                                  centre_p, centre_r, centre_c)
 
     for p in range(planes):
-        # for i in range(n_bins):
-        #     histo[i] = 0
         histo[:] = 0
         pop = 0
-
-        for r in range(srows):
-            for c in range(scols):
-                for j in range(splanes):
-                    rr = r - centre_r
-                    cc = c - centre_c
-                    pp = j - centre_p + p
-
-                    if selem[j, r, c]:
-                        if is_in_mask_3D(rows, cols, planes, rr, cc, pp,
-                                         mask_data):
-                            histogram_increment(histo, &pop,
-                                                image[pp, rr, cc])
-
+        _build_initial_histogram_from_neighborhood(image, selem, histo, &pop,
+                                                   mask_data, p, planes, rows, cols,
+                                                   splanes, srows, scols,
+                                                   centre_p, centre_r,
+                                                   centre_c)
         r = 0
         c = 0
         kernel(&out[p, r, c, 0], odepth, histo, pop, image[p, r, c],
@@ -398,21 +449,8 @@ cdef void _core_3D(void kernel(dtype_t_out*, Py_ssize_t, Py_ssize_t[::1], double
 
             # ---> west to east
             for c in range(1, cols):
-                for j in range(num_se_e):
-                    rr = r + se_e_r[j]
-                    cc = c + se_e_c[j]
-                    pp = p + se_e_p[j]
-                    if is_in_mask_3D(rows, cols, planes, rr, cc, pp,
-                                     mask_data):
-                        histogram_increment(histo, &pop, image[pp, rr, cc])
-
-                for j in range(num_se_w):
-                    rr = r + se_w_r[j]
-                    cc = c + se_w_c[j] - 1
-                    pp = p + se_w_p[j]
-                    if is_in_mask_3D(rows, cols, planes, rr, cc, pp,
-                                     mask_data):
-                        histogram_decrement(histo, &pop, image[pp, rr, cc])
+                _update_histogram(image, se, num_se, histo, &pop, mask_data, p,
+                                  r, c, planes, rows, cols, axis_inc=0)
 
                 kernel(&out[p, r, c, 0], odepth, histo, pop,
                        image[p, r, c], n_bins, mid_bin, p0, p1, s0, s1)
@@ -422,42 +460,16 @@ cdef void _core_3D(void kernel(dtype_t_out*, Py_ssize_t, Py_ssize_t[::1], double
                 break
 
             # ---> north to south
-            for j in range(num_se_s):
-                rr = r + se_s_r[j]
-                cc = c + se_s_c[j]
-                pp = p + se_s_p[j]
-                if is_in_mask_3D(rows, cols, planes, rr, cc, pp,
-                                 mask_data):
-                    histogram_increment(histo, &pop, image[pp, rr, cc])
-
-            for j in range(num_se_n):
-                rr = r + se_n_r[j] - 1
-                cc = c + se_n_c[j]
-                pp = p + se_n_p[j]
-                if is_in_mask_3D(rows, cols, planes, rr, cc, pp,
-                                 mask_data):
-                    histogram_decrement(histo, &pop, image[pp, rr, cc])
+            _update_histogram(image, se, num_se, histo, &pop, mask_data, p,
+                              r, c, planes, rows, cols, axis_inc=3)
 
             kernel(&out[p, r, c, 0], odepth, histo, pop,
                    image[p, r, c], n_bins, mid_bin, p0, p1, s0, s1)
 
             # ---> east to west
             for c in range(cols - 2, -1, -1):
-                for j in range(num_se_w):
-                    rr = r + se_w_r[j]
-                    cc = c + se_w_c[j]
-                    pp = p + se_w_p[j]
-                    if is_in_mask_3D(rows, cols, planes, rr, cc, pp,
-                                     mask_data):
-                        histogram_increment(histo, &pop, image[pp, rr, cc])
-
-                for j in range(num_se_e):
-                    rr = r + se_e_r[j]
-                    cc = c + se_e_c[j] + 1
-                    pp = p + se_e_p[j]
-                    if is_in_mask_3D(rows, cols, planes, rr, cc, pp,
-                                     mask_data):
-                        histogram_decrement(histo, &pop, image[pp, rr, cc])
+                _update_histogram(image, se, num_se, histo, &pop, mask_data, p,
+                                  r, c, planes, rows, cols, axis_inc=2)
 
                 kernel(&out[p, r, c, 0], odepth, histo, pop,
                        image[p, r, c], n_bins, mid_bin, p0, p1, s0, s1)
@@ -467,21 +479,8 @@ cdef void _core_3D(void kernel(dtype_t_out*, Py_ssize_t, Py_ssize_t[::1], double
                 break
 
             # ---> north to south
-            for j in range(num_se_s):
-                rr = r + se_s_r[j]
-                cc = c + se_s_c[j]
-                pp = p + se_s_p[j]
-                if is_in_mask_3D(rows, cols, planes, rr, cc, pp,
-                                 mask_data):
-                    histogram_increment(histo, &pop, image[pp, rr, cc])
-
-            for j in range(num_se_n):
-                rr = r + se_n_r[j] - 1
-                cc = c + se_n_c[j]
-                pp = p + se_n_p[j]
-                if is_in_mask_3D(rows, cols, planes, rr, cc, pp,
-                                 mask_data):
-                    histogram_decrement(histo, &pop, image[pp, rr, cc])
+            _update_histogram(image, se, num_se, histo, &pop, mask_data, p,
+                              r, c, planes, rows, cols, axis_inc=3)
 
             kernel(&out[p, r, c, 0], odepth, histo, pop, image[p, r, c],
                    n_bins, mid_bin, p0, p1, s0, s1)
