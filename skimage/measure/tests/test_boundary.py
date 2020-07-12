de5fb49e0b8a94c5e6b258dac9317154a040198e
Felipe Rodrigues <felipe@felipevr.com>
2020-07-12 16:30:25 -0300

    Add horse test for boundary tracing

--- skimage/measure/tests/test_boundary.py
+++ skimage/measure/tests/test_boundary.py
@@ -1,6 +1,9 @@
 import numpy as np
+from skimage import data
 from skimage.measure import regionprops, trace_boundary
 from skimage._shared.testing import assert_array_equal
+
+from . import boundary_fixtures
 
 def test_simple_example():
     example = np.array(
@@ -16,7 +19,14 @@ def test_simple_example():
     expected_boundary = np.array(
         [[1, 1], [1, 2], [1, 3], [1, 4], [2, 4], [3, 4], [4, 3], [3, 2], [2, 1]]
     )
-    props = regionprops(example)
-    boundary_1 = trace_boundary(props[0].coords)
+    regions = regionprops(example)
+    boundary_1 = trace_boundary(regions[0].coords)
 
     assert_array_equal(boundary_1, expected_boundary)
+
+def test_horse_example():
+    horse = np.logical_not(data.horse()).astype(int)
+    regions = regionprops(horse)
+    boundary = trace_boundary(regions[0].coords)
+
+    assert_array_equal(boundary, boundary_fixtures.HORSE_EXPECTED)
