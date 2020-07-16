b988144ff755299638b5c664398ea5cfe9eb2004
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2020-07-16 16:32:00 +0200

    Port tutorial up to slice explorer

--- doc/examples/applications/plot_3d_image_processing.py
+++ doc/examples/applications/plot_3d_image_processing.py
@@ -37,6 +37,7 @@ information was reported by the microscope used to image the cells.
 """
 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import numpy as np
 from scipy import ndimage as ndi
 
@@ -96,3 +97,108 @@ _, (a, b, c) = plt.subplots(ncols=3, figsize=(15, 5))
 show_plane(a, data[32], title="Plane = 32")
 show_plane(b, data[:, 128, :], title="Row = 128")
 show_plane(c, data[:, :, 128], title="Column = 128")
+
+#####################################################################
+# As hinted before, a three-dimensional image can be viewed as a series of
+# two-dimensional planes. Let us write a helper function, `display`, to
+# display 30 planes of our data. By default, every other plane is displayed.
+
+def display(im3d, cmap="gray", step=2):
+    _, axes = plt.subplots(nrows=5, ncols=6, figsize=(16, 14))
+
+    vmin = im3d.min()
+    vmax = im3d.max()
+
+    for ax, image in zip(axes.flatten(), im3d[::step]):
+        ax.imshow(image, cmap=cmap, vmin=vmin, vmax=vmax)
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+display(data)
+
+#####################################################################
+# Alternatively, we can explore these planes (slices) interactively using
+# Jupyter widgets. Let the user select which slice to display and show the
+# position of this slice in the 3D dataset.
+
+def slice_in_3D(ax, i):
+    # From https://stackoverflow.com/questions/44881885/python-draw-3d-cube
+    Z = np.array([[0, 0, 0],
+                  [1, 0, 0],
+                  [1, 1, 0],
+                  [0, 1, 0],
+                  [0, 0, 1],
+                  [1, 0, 1],
+                  [1, 1, 1],
+                  [0, 1, 1]])
+
+    Z = Z * data.shape
+
+    r = [-1, 1]
+
+    X, Y = np.meshgrid(r, r)
+    # Plot vertices
+    ax.scatter3D(Z[:, 0], Z[:, 1], Z[:, 2])
+
+    # List sides' polygons of figure
+    verts = [[Z[0], Z[1], Z[2], Z[3]],
+             [Z[4], Z[5], Z[6], Z[7]],
+             [Z[0], Z[1], Z[5], Z[4]],
+             [Z[2], Z[3], Z[7], Z[6]],
+             [Z[1], Z[2], Z[6], Z[5]],
+             [Z[4], Z[7], Z[3], Z[0]],
+             [Z[2], Z[3], Z[7], Z[6]]]
+
+    # Plot sides
+    ax.add_collection3d(
+        Poly3DCollection(
+            verts,
+            facecolors=(0, 1, 1, 0.25),
+            linewidths=1,
+            edgecolors="darkblue"
+        )
+    )
+
+    verts = np.array([[[0, 0, 0],
+                       [0, 0, 1],
+                       [0, 1, 1],
+                       [0, 1, 0]]])
+    verts = verts * (60, 256, 256)
+    verts += [i, 0, 0]
+
+    ax.add_collection3d(
+        Poly3DCollection(
+            verts,
+            facecolors="magenta",
+            linewidths=1,
+            edgecolors="black"
+        )
+    )
+
+    ax.set_xlabel("plane")
+    ax.set_ylabel("row")
+    ax.set_zlabel("col")
+
+    # Autoscale plot axes
+    scaling = np.array([getattr(ax,
+        "get_{}lim".format(dim))() for dim in "xyz"])
+    ax.auto_scale_xyz(* [[np.min(scaling), np.max(scaling)]] * 3)
+
+def explore_slices(data, cmap="gray"):
+    from ipywidgets import interact
+    N = len(data)
+
+    @interact(plane=(0, N - 1))
+    def display_slice(plane=34):
+        fig, ax = plt.subplots(figsize=(20, 5))
+
+        ax_3D = fig.add_subplot(133, projection="3d")
+
+        show_plane(ax, data[plane], title="Plane {}".format(plane), cmap=cmap)
+        slice_in_3D(ax_3D, plane)
+
+        plt.show()
+
+    return display_slice
+
+explore_slices(data);
