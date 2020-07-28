3997ec0dbbac38d5a2bec093d2a86e9a851b1286
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2020-07-28 15:45:22 +0200

    Update doc/examples/applications/plot_3d_image_processing.py
    Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

--- doc/examples/applications/plot_3d_image_processing.py
+++ doc/examples/applications/plot_3d_image_processing.py
@@ -100,9 +100,9 @@ def show_plane(ax, plane, cmap="gray", title=None):
 (n_plane, n_row, n_col) = data.shape
 _, (a, b, c) = plt.subplots(ncols=3, figsize=(15, 5))
 
-show_plane(a, data[n_plane // 2], title="Plane = {}".format(n_plane // 2))
-show_plane(b, data[:, n_row // 2, :], title="Row = {}".format(n_row // 2))
-show_plane(c, data[:, :, n_col // 2], title="Column = {}".format(n_col // 2))
+show_plane(a, data[n_plane // 2], title=f'Plane = {n_plane // 2}')
+show_plane(b, data[:, n_row // 2, :], title=f'Row = {n_row // 2}')
+show_plane(c, data[:, :, n_col // 2], title=f'Column = {n_col // 2}')
 
 #####################################################################
 # As hinted before, a three-dimensional image can be viewed as a series of
