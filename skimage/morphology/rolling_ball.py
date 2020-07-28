7884fa7fc699194a1b53bba92fbb5b00f85b13d9
FirefoxMetzger <sebastian@wallkoetter.net>
2020-07-28 20:06:24 +0200

    some type checking + copied docstring

--- skimage/morphology/rolling_ball.py
+++ skimage/morphology/rolling_ball.py
@@ -6,6 +6,103 @@ from skimage.util import invert
 
 
 def rolling_ball(input_img, radius=50, white_background=False):
+    """Perform background subtraction using the rolling ball method.
+
+    The rolling ball filter is a segmentation method that aims to separate the
+    background from a grayscale image in case of uneven exposure. It is
+    frequently used in biomedical image processing and was first proposed by
+    Stanley R. Sternberg (1983) in the paper Biomedical Image Processing [1]_.
+
+    Parameters
+    ----------
+    image : ndarray
+        The image to be filtered.
+    radius : float
+        The radius of the ball that is used as the structuring element.
+    white_background : bool
+        If true, the algorithm separates dark features from a bright
+        background.
+
+    Returns
+    -------
+    filtered_image : ndarray
+        The image with background removes
+    background : ndarray
+        The background that was removed.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from skimage.morphology import reconstruction
+
+    First, we create a sinusoidal mask image with peaks at middle and ends.
+
+    >>> x = np.linspace(0, 4 * np.pi)
+    >>> y_mask = np.cos(x)
+
+    Then, we create a seed image initialized to the minimum mask value (for
+    reconstruction by dilation, min-intensity values don't spread) and add
+    "seeds" to the left and right peak, but at a fraction of peak value (1).
+
+    >>> y_seed = y_mask.min() * np.ones_like(x)
+    >>> y_seed[0] = 0.5
+    >>> y_seed[-1] = 0
+    >>> y_rec = reconstruction(y_seed, y_mask)
+
+    The reconstructed image (or curve, in this case) is exactly the same as the
+    mask image, except that the peaks are truncated to 0.5 and 0. The middle
+    peak disappears completely: Since there were no seed values in this peak
+    region, its reconstructed value is truncated to the surrounding value (-1).
+
+    As a more practical example, we try to extract the bright features of an
+    image by subtracting a background image created by reconstruction.
+
+    >>> y, x = np.mgrid[:20:0.5, :20:0.5]
+    >>> bumps = np.sin(x) + np.sin(y)
+
+    To create the background image, set the mask image to the original image,
+    and the seed image to the original image with an intensity offset, `h`.
+
+    >>> h = 0.3
+    >>> seed = bumps - h
+    >>> background = reconstruction(seed, bumps)
+
+    The resulting reconstructed image looks exactly like the original image,
+    but with the peaks of the bumps cut off. Subtracting this reconstructed
+    image from the original image leaves just the peaks of the bumps
+
+    >>> hdome = bumps - background
+
+    This operation is known as the h-dome of the image and leaves features
+    of height `h` in the subtracted image.
+
+    Notes
+    -----
+    Check out the gallery example of this function (TODO: link)
+
+    References
+    ----------
+    .. [1] Sternberg, Stanley R. "Biomedical image processing." Computer 1
+           (1983): 22-34. :DOI:`10.1109/MC.1983.1654163`
+    """
+
+    if not isinstance(radius, float):
+        try:
+            radius = float(radius)
+        except ValueError:
+            raise ValueError(f"Radius should be float, was {type(radius)}")
+
+    if radius <= 0:
+        raise ValueError(f"Radius must be greater zeros, was {radius}")
+
+    try:
+        white_background = bool(white_background)
+    except ValueError:
+        raise ValueError(
+            f"white_background must be convertible to a boolean, "
+            f"was {type(white_background)}"
+        )
+
     img = input_img.copy()
     if white_background:
         img = invert(img)
