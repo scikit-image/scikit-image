073cb1be2dd413af0257231b2d97599bb892d9d1
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-08-05 17:07:04 +1000

    Add cross_correlate_masked function to registration init file for user import

--- skimage/registration/__init__.py
+++ skimage/registration/__init__.py
@@ -1,8 +1,9 @@
 from ._optical_flow import optical_flow_tvl1
 from ._phase_cross_correlation import phase_cross_correlation
-
+from ._masked_phase_cross_correlation import cross_correlate_masked
 
 __all__ = [
     'optical_flow_tvl1',
-    'phase_cross_correlation'
+    'phase_cross_correlation',
+    'cross_correlate_masked'
     ]
