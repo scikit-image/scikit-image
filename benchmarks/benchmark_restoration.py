2a6250ad2ce67bb8feb722f35ff77b75ea141605
Gregory Lee <grlee77@gmail.com>
2020-08-05 19:16:21 -0400

    fix indentation in existing restoration benchmarks

--- benchmarks/benchmark_restoration.py
+++ benchmarks/benchmark_restoration.py
@@ -8,8 +8,7 @@ class RestorationSuite:
     def setup(self):
         nz = 32
         self.volume_f64 = np.stack([camera()[::2, ::2], ] * nz,
-                                  axis=-1).astype(float) / 255
-
+                                   axis=-1).astype(float) / 255
         self.sigma = .05
         self.volume_f64 += self.sigma * np.random.randn(*self.volume_f64.shape)
         self.volume_f32 = self.volume_f64.astype(np.float32)
