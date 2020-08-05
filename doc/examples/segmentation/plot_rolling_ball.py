467c0b66e1d5d29ba0875b662f64d77ae5faf837
FirefoxMetzger <sebastian@wallkoetter.net>
2020-08-05 05:50:47 +0200

    removed Sternberg's quote

--- doc/examples/segmentation/plot_rolling_ball.py
+++ doc/examples/segmentation/plot_rolling_ball.py
@@ -15,12 +15,7 @@ the intensity of the background at a desired position, we imagine submerging a
 ball into the blocks at the desired pixel position. Once it is completely
 covered by the blocks, the height of the ball determines the intensity of the
 background at that position. We can then *roll* this ball around below the
-surface to get the background values for the entire image. As Sternberg
-puts it:
-
-    "We can visualize a solid sphere that moves freely within the solid
-    volume of the gel image umbra but is constrained by the upper surface of
-    the umbra."
+surface to get the background values for the entire image.
 
 Scikit-image gives you convenient access to this algorithm, and also implements
 a generalized version which allows you to "roll" arbitrary ellipsoids. The
