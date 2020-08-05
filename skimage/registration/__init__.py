8508a95ea6ff67e0ea05ac9ca97eaf85d6915860
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-08-05 17:39:50 +1000

    Oops, masked_cross_correlate is not supposed to be exposed to the user

--- skimage/registration/__init__.py
+++ skimage/registration/__init__.py
@@ -1,9 +1,7 @@
 from ._optical_flow import optical_flow_tvl1
 from ._phase_cross_correlation import phase_cross_correlation
-from ._masked_phase_cross_correlation import cross_correlate_masked
 
 __all__ = [
     'optical_flow_tvl1',
-    'phase_cross_correlation',
-    'cross_correlate_masked'
+    'phase_cross_correlation'
     ]
