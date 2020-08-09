7df0ee5ae07d0c14e2fb2a055a86f272a49ee22a
François Boulogne <devel@sciunto.org>
2020-08-09 07:11:51 +0200

    Enhance titles

--- doc/examples/filters/plot_entropy.py
+++ doc/examples/filters/plot_entropy.py
@@ -20,6 +20,11 @@ level distribution. The second example shows how to detect texture in the camera
 image using a smaller structuring element.
 
 """
+
+##################
+# Object detection
+# ================
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -27,8 +32,6 @@ from skimage import data
 from skimage.util import img_as_ubyte
 from skimage.filters.rank import entropy
 from skimage.morphology import disk
-
-# First example: object detection.
 
 noise_mask = np.full((128, 128), 28, dtype=np.uint8)
 noise_mask[32:-32, 32:-32] = 30
@@ -41,8 +44,8 @@ entr_img = entropy(img, disk(10))
 
 fig, (ax0, ax1, ax2) = plt.subplots(nrows=1, ncols=3, figsize=(10, 4))
 
-ax0.imshow(noise_mask, cmap='gray')
-ax0.set_title("Noise mask")
+img0 = ax0.imshow(noise_mask, cmap='gray')
+ax0.set_title("Object")
 ax1.imshow(img, cmap='gray')
 ax1.set_title("Noisy image")
 ax2.imshow(entr_img, cmap='viridis')
@@ -50,7 +53,9 @@ ax2.set_title("Local entropy")
 
 fig.tight_layout()
 
-# Second example: texture detection.
+################
+# Edge detection
+# ==============
 
 image = img_as_ubyte(data.camera())
 
