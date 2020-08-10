6e7d9334602d715b057d81fb7d3a6b4761c9be63
Gregory Lee <grlee77@gmail.com>
2020-08-09 21:42:27 -0400

    split masked slic tests into a derived class
    this way can mark these cases as n/a on older skimage

--- benchmarks/benchmark_segmentation.py
+++ benchmarks/benchmark_segmentation.py
@@ -5,7 +5,7 @@ import skimage
 from skimage import segmentation
 
 
-class SegmentationSuite:
+class SlicSegmentation:
     """Benchmark for segmentation routines in scikit-image."""
     def setup(self):
         self.image = np.random.random((200, 200, 100))
@@ -27,14 +27,6 @@ class SegmentationSuite:
     def time_slic_basic_multichannel(self):
         segmentation.slic(self.image, enforce_connectivity=False,
                           multichannel=True, **self.slic_kwargs)
-
-    def time_mask_slic(self):
-        segmentation.slic(self.image, enforce_connectivity=False,
-                          mask=self.msk, multichannel=False)
-
-    def time_mask_slic_multichannel(self):
-        segmentation.slic(self.image, enforce_connectivity=False,
-                          mask=self.msk_slice, multichannel=True)
 
     def peakmem_setup(self):
         """peakmem includes the memory used by setup.
@@ -60,3 +52,33 @@ class SegmentationSuite:
     def peakmem_slic_basic_multichannel(self):
         segmentation.slic(self.image, enforce_connectivity=False,
                           multichannel=True, **self.slic_kwargs)
+
+
+class MaskSlicSegmentation(SlicSegmentation):
+    """Benchmark for segmentation routines in scikit-image."""
+    def setup(self):
+        try:
+            mask = np.zeros((64, 64)) > 0
+            mask[10:-10, 10:-10] = 1
+            segmentation.slic(np.ones_like(mask), mask=mask)
+        except TypeError:
+            raise NotImplementedError("masked slic unavailable")
+
+        self.image = np.random.random((200, 200, 100))
+        self.image[:100, :100, :] += 1
+        self.image[150:, 150:, :] += 0.5
+        self.msk = np.zeros((200, 200, 100))
+        self.msk[10:-10, 10:-10, 10:-10] = 1
+        self.msk_slice = self.msk[..., 50]
+        if Version(skimage.__version__) >= Version('0.17.0'):
+            self.slic_kwargs = dict(start_label=1)
+        else:
+            self.slic_kwargs = {}
+
+    def time_mask_slic(self):
+        segmentation.slic(self.image, enforce_connectivity=False,
+                          mask=self.msk, multichannel=False)
+
+    def time_mask_slic_multichannel(self):
+        segmentation.slic(self.image, enforce_connectivity=False,
+                          mask=self.msk_slice, multichannel=True)
