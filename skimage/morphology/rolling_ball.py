bugfix: wrong order for row/col padding + test

skimage/morphology/rolling_ball.py
@@ -91,8 +91,8 @@ def rolling_ellipsoid(image, kernel_size=(100, 100), intensity_vertex=100,
     kernel_size_y, kernel_size_x = np.round(kernel_size).astype(int)
 
     pad_amount = [(0, 0)] * image.ndim
-    pad_amount[-2] = (kernel_size_x, kernel_size_x)
-    pad_amount[-1] = (kernel_size_y, kernel_size_y)
+    pad_amount[-2] = (kernel_size_y, kernel_size_y)
+    pad_amount[-1] = (kernel_size_x, kernel_size_x)
     img = np.pad(img, pad_amount,  constant_values=np.Inf, mode="constant")
 
     tmp_x = np.arange(-kernel_size_x, kernel_size_x + 1)

skimage/morphology/tests/test_rolling_ball.py
@@ -7,7 +7,13 @@ import numpy as np
 import pytest
 
 from skimage import data
-from skimage.morphology import rolling_ball
+from skimage.morphology import rolling_ball, rolling_ellipsoid
+
+
+def test_ellipsoid_const():
+    img = 155 * np.ones((100, 100), dtype=np.uint8)
+    result = rolling_ellipsoid(img, kernel_size=(25, 53))
+    assert np.allclose(result, np.zeros_like(img))
 
 
 @pytest.mark.parametrize("radius", [1, 2.5, 10.346, 50])