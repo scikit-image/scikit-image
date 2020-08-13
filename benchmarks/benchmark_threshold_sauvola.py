694ff86e4a5b463aac67100a1b60ec3d89d0a1ce
Abhishek Patil <abhishek@zeroth.me>
2020-08-13 18:08:37 +1000

    PEP 8 formatting fixed

--- benchmarks/benchmark_threshold_sauvola.py
+++ benchmarks/benchmark_threshold_sauvola.py
@@ -4,19 +4,20 @@ from skimage import filters
 class ThresholdSuite:
     """Benchmark for transform routines in scikit-image."""
 
+
     def setup(self):
         self.image = np.zeros((2000, 2000))
         self.image3D = np.zeros((300, 300, 30))
 
         idx = np.arange(500, 700)
         idx3D = np.arange(100, 100)
-        
+
         self.image[idx[::-1], idx] = 255
         self.image[idx, idx] = 255
 
         self.image3D[idx3D[::-1], idx3D, idx3D] = 255
         self.image3D[idx3D, idx3D, idx3D] = 255
-        
+
     def time_sauvola(self):
         result = filters.threshold_sauvola(self.image, window_size=51)
 
