78982514e5b1923c16e63edcfd630593dacad1d5
Abhishek Patil <abhishek@zeroth.me>
2020-08-13 17:15:10 +1000

    3d image benchmark for threshold_sauvola

--- benchmarks/benchmark_threshold_sauvola.py
+++ benchmarks/benchmark_threshold_sauvola.py
@@ -6,9 +6,19 @@ class ThresholdSuite:
 
     def setup(self):
         self.image = np.zeros((2000, 2000))
-        idx = np.arange(500, 1500)
+        self.image3D = np.zeros((300, 300, 30))
+
+        idx = np.arange(500, 700)
+        idx3D = np.arange(100, 100)
+        
         self.image[idx[::-1], idx] = 255
         self.image[idx, idx] = 255
 
+        self.image3D[idx3D[::-1], idx3D, idx3D] = 255
+        self.image3D[idx3D, idx3D, idx3D] = 255
+        
     def time_sauvola(self):
         result = filters.threshold_sauvola(self.image, window_size=51)
+
+    def time_sauvola_3d(self):
+        result = filters.threshold_sauvola(self.image3D, window_size=51)
