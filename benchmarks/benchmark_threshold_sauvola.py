ffa06ee391b5140c454b00fd80e41b8ccf45ddaf
Abhishek Patil <abhishek@zeroth.me>
2020-08-14 16:31:59 +1000

    Updated ThresholdSuite benchmark to use numpy style array shape and sample image dtype is now np.uint8

--- benchmarks/benchmark_threshold_sauvola.py
+++ benchmarks/benchmark_threshold_sauvola.py
@@ -6,17 +6,17 @@ class ThresholdSuite:
 
 
     def setup(self):
-        self.image = np.zeros((2000, 2000))
-        self.image3D = np.zeros((300, 300, 30))
+        self.image = np.zeros((2000, 2000), dtype=np.uint8)
+        self.image3D = np.zeros((30, 300, 300), dtype=np.uint8)
 
         idx = np.arange(500, 700)
-        idx3D = np.arange(100, 100)
+        idx3D = np.arange(10, 200)
 
         self.image[idx[::-1], idx] = 255
         self.image[idx, idx] = 255
 
-        self.image3D[idx3D[::-1], idx3D, idx3D] = 255
-        self.image3D[idx3D, idx3D, idx3D] = 255
+        self.image3D[:, idx3D[::-1], idx3D] = 255
+        self.image3D[:, idx3D, idx3D] = 255
 
     def time_sauvola(self):
         result = filters.threshold_sauvola(self.image, window_size=51)
