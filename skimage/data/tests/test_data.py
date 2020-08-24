4bdeb75d015eddd7a2808ec497d5922398f3f943
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2020-08-24 18:19:16 +0200

    Test loading of 3D multichannel image via Pooch

--- skimage/data/tests/test_data.py
+++ skimage/data/tests/test_data.py
@@ -153,4 +153,11 @@ def test_cells_3d():
     assert image.shape == (60, 256, 256)
 
 
+def test_kidney_3d_multichannel():
+    """Test that 3D multichannel image of kidney tissue can be loaded.
 
+    Needs internet connection.
+    """
+    path = fetch('data/kidney-tissue-fluorescence.tif')
+    image = io.imread(path)
+    assert image.shape == (16, 512, 512, 3)
