8e8ce83b96213a084e48e5defca95e3366fb98b6
Mark Harfouche <mark.harfouche@gmail.com>
2020-08-31 05:03:10 -0700

    Update __init__.py

--- skimage/data/__init__.py
+++ skimage/data/__init__.py
@@ -634,11 +634,11 @@ def cells3D():
         * After rescaling the voxel size is ``(0.29 0.26 0.26)``.
     """
 
-    return _load("data/cells.tif")
+    return _load("data/cells3D.tif")
 
 
 def human_mitosis():
-    """A dataset containing an image of human cells undergoing mitosis.
+    """Image of human cells undergoing mitosis.
 
     Returns
     -------
@@ -647,20 +647,18 @@ def human_mitosis():
 
     Notes
     -----
-
-    Copyright David Root. Licensed under CC-0[2]_.
+    Copyright David Root. Licensed under CC-0 [2]_.
 
     References
     ----------
-
     .. [1] Moffat J, Grueneberg DA, Yang X, Kim SY, Kloepfer AM, Hinkle G,
            Piqani B, Eisenhaure TM, Luo B, Grenier JK, Carpenter AE, Foo SY,
            Stewart SA, Stockwell BR, Hacohen N, Hahn WC, Lander ES,
            Sabatini DM, Root DE (2006) A lentiviral RNAi library for human and
            mouse genes applied to an arrayed viral high-content screen. Cell,
-           124(6):1283-98 / doi: 10.1016/j.cell.2006.01.040. PMID 16564017
+           124(6):1283-98 / :DOI: `10.1016/j.cell.2006.01.040` PMID 16564017
 
-    .. [2] GitHub Licensing discussion
+    .. [2] GitHub licensing discussion
            https://github.com/CellProfiler/examples/issues/41
 
     """
