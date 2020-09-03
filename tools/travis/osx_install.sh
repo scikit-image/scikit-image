34e6bf68bc2cf80be4273e33027a9d457ebb0b78
François Boulogne <devel@sciunto.org>
2020-09-03 06:55:43 +0200

    Try verbose mode, timeout happens on the absence of log

--- tools/travis/osx_install.sh
+++ tools/travis/osx_install.sh
@@ -7,8 +7,8 @@ brew tap homebrew/homebrew-cask
 brew cask install basictex
 
 export PATH="$PATH:/Library/TeX/texbin"
-travis_wait 20 sudo tlmgr --verify-repo=none update --self
-travis_wait 20 sudo tlmgr --verify-repo=none install ucs dvipng anyfontsize
+sudo tlmgr -v --verify-repo=none update --self
+sudo tlmgr -v --verify-repo=none install ucs dvipng anyfontsize
 
 # Set up virtualenv on OSX
 git clone --depth 1 --branch devel https://github.com/matthew-brett/multibuild ~/multibuild
