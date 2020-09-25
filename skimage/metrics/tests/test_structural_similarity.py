a5645aeb96c31d73f8842568bcb277009ff3a826
alexdesiqueira <alex.desiqueira@igdore.org>
2020-09-24 17:52:19 -0700

    Upd metrics/tests/test_struct_sim::msssim_vs_leg

--- skimage/metrics/tests/test_structural_similarity.py
+++ skimage/metrics/tests/test_structural_similarity.py
@@ -178,8 +178,8 @@ def test_gaussian_structural_similarity_vs_IPOL():
 
 
 def test_mssim_vs_legacy():
-    # check that ssim with default options matches skimage 0.11 result
-    mssim_skimage_0pt17 = 0.31299140502389766
+    # check that ssim with default options matches skimage 0.17 result
+    mssim_skimage_0pt17 = 0.3674518327910367
     mssim = structural_similarity(cam, cam_noisy)
     assert_almost_equal(mssim, mssim_skimage_0pt17)
 
