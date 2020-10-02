8eadc245997c27322493c834f943e6617def8a4c
Alexandre de Siqueira <alex.desiqueira@igdore.org>
2020-10-02 06:46:30 -0700

    Replacing data/camera.png due to copyright (#4913)
    * Removing data/camera.png due to copyright
    
    * Starting to remove data.camera() from our tests
    
    * Replacing camera.png
    
    * Revert "Starting to remove data.camera() from our tests"
    
    This reverts commit c7cab1e22a8cdb33cf2e44b4cd099cc6851fdf45.
    
    * Revert "Removing data/camera.png due to copyright"
    
    This reverts commit 2e5e601475967c0dceb58ea9e55a6d6aae227de0.
    
    * Setting own URL to test new camera.png
    
    * Adding a 2-D, gray version of the new camera.png
    
    * Update skimage/data/__init__.py
    
    * Updating registry for new data/camera
    
    * Fixing tests on filters/tests/test_thresholding
    
    * Adding info on the new camera's license.
    
    * Fixing tests on util/tests/test_random_noise
    
    * Starting to fix tests on skimage/metrics
    
    * Fixing tests on ridges, thresholds
    
    * Fixing test on exposure
    
    * Handling numpy asarray deprecation
    
    * Fixing quantiles output according to new camera
    
    * Improving some imports on feature/tests/test_canny
    
    * Some PEP8 on feature/tests/test_canny
    
    * Fixing SSIM value for new camera
    
    * Fixing MSSIM values; removing MATLAB comp test
    
    * skimage.img -> util.img
    
    * Replacing camera_wiener.npy
    
    * Replacing camera_unsup.py
    
    * Replacing camera_rl
    
    * Fixing test in test_io
    
    * Remove redundant tests in  metrics/test_struct_sim
    
    * Fixing tests in registration
    
    * Replacing camera with brick in test_collection
    
    * Adding new restoration/tests files to registry
    
    * Adding camera_unsup2 and registry info
    
    * Fixing camera_unsup2; adding to registry
    
    * Fixing doc/examples/feat_detection/plot_glcm info
    
    * Fixing some doctests according to new camera
    
    * Fixing viewer/tests/test_plugins — Round 2
    
    * Fixing viewer/tests/test_plugins — Round 3
    
    * Fixing viewer/tests/test_plugins — Round 4
    
    * Fixing viewer/tests/test_plugins — Round 5
    
    * Fixing viewer/tests/test_tools
    
    * Fixing viewer/tests/test_tools — Round 2
    
    * Improving doc/examples/color_exposure/plot_tint
    
    * Fixing doc/exs/registration/plot_masked
    
    * Adding 512x512 camera
    
    * Updating registry
    
    * Fixing filters/tests/test_ridges
    
    * Fixing filters/tests/test_thresholding
    
    * Fixing exposure/tests/test_exposure
    
    * Fixing features/tests/test_canny
    
    * Reverting registration/tests/mask_phase_cross_corr
    
    * Fixing io/tests/test_io.py
    
    * Fixing util/tests/test_random_noise
    
    * Adding new restoration/tests images
    
    * Fixing: camera_unsup2 = np.real(deconvolved)
    
    * Fixing measure/tests/test_structural_sim
    
    * Fixing doctests
    
    * Fixing viewer/tests
    
    * Fixing metrics/test/structural_sim::mssim_vs_leg
    
    * Adding versionchanged info to data/camera
    
    * Fixing some stuff on viewer/tests
    
    * Fixing viewer/tests/plugins — again
    
    * Returning doc/exs/reg/plot_masked_reg_trans
    
    * Getting face right on rest/tests/test_restoration
    
    * Third time is the charm!
    
    * Updating test_exposure
    
    * Updating test_canny
    
    * Last version of the image
    
    * Updating test_thresholding
    
    * Updating test images
    
    * Updating test_struc_sim::mssim_vs_legacy
    
    * Updating test_random_noise
    
    * Upd metrics/tests/test_struct_sim::msssim_vs_leg
    
    * Fixing latest MSSIM, PSNR values
    
    * FIxing doc/viewer errors
    
    * Fixing viewer/tests/test_plugins::prof(max)
    
    * Fixing viewer/tests/test_plugins:prof(mean)
    
    * Update grass and sky locations in GLCM example
    
    * Back to the original URL
    
    Co-authored-by: Emmanuelle Gouillart <emma@plot.ly>
    Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

--- skimage/metrics/tests/test_simple_metrics.py
+++ skimage/metrics/tests/test_simple_metrics.py
@@ -16,9 +16,24 @@ cam_noisy = cam_noisy.astype(cam.dtype)
 
 
 def test_PSNR_vs_IPOL():
-    # Tests vs. imdiff result from the following IPOL article and code:
-    # https://www.ipol.im/pub/art/2011/g_lmii/
-    p_IPOL = 22.4497
+    """ Tests vs. imdiff result from the following IPOL article and code:
+    https://www.ipol.im/pub/art/2011/g_lmii/.
+
+    Notes
+    -----
+    To generate p_IPOL, we need a local copy of cam_noisy:
+
+    >>> from skimage import io
+    >>> io.imsave('/tmp/cam_noisy.png', cam_noisy)
+
+    Then, we use the following command:
+    $ ./imdiff -m psnr <path to camera.png>/camera.png /tmp/cam_noisy.png
+
+    Values for current data.camera() calculated by Gregory Lee on Sep, 2020.
+    Available at:
+    https://github.com/scikit-image/scikit-image/pull/4913#issuecomment-700653165
+    """
+    p_IPOL = 22.409353363576034
     p = peak_signal_noise_ratio(cam, cam_noisy)
     assert_almost_equal(p, p_IPOL, decimal=4)
 
