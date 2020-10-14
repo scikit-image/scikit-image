21046897768fba135378a2bafaf06bbc62ecaa1f
Emmanuelle Gouillart <emma@plot.ly>
2020-10-14 13:46:51 +0200

    remove unused import

--- skimage/data/tests/test_data.py
+++ skimage/data/tests/test_data.py
@@ -2,7 +2,7 @@ import numpy as np
 import skimage.data as data
 from skimage.data import image_fetcher
 from skimage import io
-from skimage._shared.testing import assert_equal, assert_almost_equal, fetch, skipif
+from skimage._shared.testing import assert_equal, assert_almost_equal, fetch
 import os
 import pytest
 
