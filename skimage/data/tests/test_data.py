4cc04ab3ec7d6bcdda4779322694233ef2fd7fbb
Alexander Jarosch <research@alexj.at>
2020-10-21 09:57:54 +0200

    new test for DNS_Gauss_freq() on astronaut, rev. change test_data.py

--- skimage/data/tests/test_data.py
+++ skimage/data/tests/test_data.py
@@ -26,8 +26,7 @@ def test_download_all_with_pooch():
     # The second concern is that this test essentially uses
     # alot of bandwidth, which is not fun for developers on
     # lower speed connections.
-    # https://github.com/scikit-image/scikit-image/pull/4666/files/
-    # 26d5138b25b958da6e97ebf979e9bc36f32c3568#r422604863
+    # https://github.com/scikit-image/scikit-image/pull/4666/files/26d5138b25b958da6e97ebf979e9bc36f32c3568#r422604863
     data_dir = data.data_dir
     if image_fetcher is not None:
         data.download_all()
