9050d77937194d363de9861e5ae9cad78f3b468a
Emmanuelle Gouillart <emma@plot.ly>
2020-10-27 21:19:14 +0100

    Update skimage/_shared/geometry.pyx

--- skimage/_shared/geometry.pyx
+++ skimage/_shared/geometry.pyx
@@ -10,7 +10,7 @@ cnp.import_array()
 
 cdef unsigned char point_in_polygon(np_floats[::1] xp, np_floats[::1] yp,
                                     np_floats x, np_floats y) nogil:
-    """Test relative point position to a polygone.
+    """Test relative point position to a polygon.
 
     Parameters
     ----------
