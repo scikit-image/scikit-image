84c69c0ccb264a120deb9c8b0822068368e362d7
Terence D. Honles <terence@honles.com>
2020-11-05 08:34:31 -0800

    update trove classifiers and CI for Python 3.9

--- setup.py
+++ setup.py
@@ -224,6 +224,7 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3 :: Only',
             'Topic :: Scientific/Engineering',
             'Operating System :: Microsoft :: Windows',
