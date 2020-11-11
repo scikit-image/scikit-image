f8d9a6c144ef84bad2478a4ba4eedb1c559274e0
rfezzani <rfezzani@gmail.com>
2020-11-12 00:31:23 +0100

    Add batch processing strategy to ensure_spacing

--- skimage/_shared/coord.py
+++ skimage/_shared/coord.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.spatial import cKDTree, distance
 
 
-def ensure_spacing(coord, spacing=1, p_norm=np.inf):
+def _ensure_spacing(coord, spacing, p_norm):
     """Returns a subset of coord where a minimum spacing is guaranteed.
 
     Parameters
@@ -24,28 +24,67 @@ def ensure_spacing(coord, spacing=1, p_norm=np.inf):
 
     """
 
-    output = coord
-    if len(coord):
-        # Use KDtree to find the peaks that are too close to each other
-        tree = cKDTree(coord)
+    # Use KDtree to find the peaks that are too close to each other
+    tree = cKDTree(coord)
 
-        indices = tree.query_ball_point(coord, r=spacing, p=p_norm)
-        rejected_peaks_indices = set()
-        for idx, candidates in enumerate(indices):
-            if idx not in rejected_peaks_indices:
-                # keep current point and the points at exactly spacing from it
-                candidates.remove(idx)
-                dist = distance.cdist([coord[idx]],
-                                      coord[candidates],
-                                      distance.minkowski,
-                                      p=p_norm).reshape(-1)
-                candidates = [c for c, d in zip(candidates, dist)
-                              if d < spacing]
+    indices = tree.query_ball_point(coord, r=spacing, p=p_norm)
+    rejected_peaks_indices = set()
+    for idx, candidates in enumerate(indices):
+        if idx not in rejected_peaks_indices:
+            # keep current point and the points at exactly spacing from it
+            candidates.remove(idx)
+            dist = distance.cdist([coord[idx]],
+                                  coord[candidates],
+                                  distance.minkowski,
+                                  p=p_norm).reshape(-1)
+            candidates = [c for c, d in zip(candidates, dist)
+                          if d < spacing]
 
-                # candidates.remove(keep)
-                rejected_peaks_indices.update(candidates)
+            # candidates.remove(keep)
+            rejected_peaks_indices.update(candidates)
 
-        # Remove the peaks that are too close to each other
-        output = np.delete(coord, tuple(rejected_peaks_indices), axis=0)
+    # Remove the peaks that are too close to each other
+    output = np.delete(coord, tuple(rejected_peaks_indices), axis=0)
+
+    return output
+
+
+def ensure_spacing(coords, spacing=1, p_norm=np.inf, batch_size=100):
+    """Returns a subset of coord where a minimum spacing is guaranteed.
+
+    Parameters
+    ----------
+    coord : array_like
+        The coordinates of the considered points.
+    spacing : float
+        the maximum allowed spacing between the points.
+    p_norm : float
+        Which Minkowski p-norm to use. Should be in the range [1, inf].
+        A finite large p may cause a ValueError if overflow can occur.
+        ``inf`` corresponds to the Chebyshev distance and 2 to the
+        Euclidean distance.
+    batch_size : int
+        Batch size used to process ``coord`` by batch to save memory.
+
+    Returns
+    -------
+    output : array_like
+        A subset of coord where a minimum spacing is guaranteed.
+
+    """
+
+    output = coords
+    if len(coords):
+
+        coords = np.atleast_2d(coords)
+        if batch_size is None:
+            batch_list = [coords]
+        else:
+            batch_list = np.array_split(coords, batch_size)
+
+        output = np.zeros((0, coords.shape[1]))
+        for batch in batch_list:
+            output = _ensure_spacing(np.vstack([output, batch]),
+                                     spacing, p_norm)
 
     return output
--- skimage/_shared/tests/test_coord.py
+++ skimage/_shared/tests/test_coord.py
@@ -5,39 +5,65 @@ from skimage._shared.coord import ensure_spacing
 
 
 @pytest.mark.parametrize("p", [1, 2, np.inf])
-def test_ensure_spacing_trivial(p):
+@pytest.mark.parametrize("size", [30, 50, None])
+def test_ensure_spacing_trivial(p, size):
     # --- Empty input
     assert ensure_spacing([], p_norm=p) == []
+
+    # --- A unique point
+    coord = np.random.randn(1, 2)
+    assert np.array_equal(coord, ensure_spacing(coord, p_norm=p,
+                                                batch_size=size))
 
     # --- Verified spacing
     coord = np.random.randn(100, 2)
 
     # --- 0 spacing
-    assert np.array_equal(coord, ensure_spacing(coord, spacing=0, p_norm=p))
+    assert np.array_equal(coord, ensure_spacing(coord, spacing=0, p_norm=p,
+                                                batch_size=size))
 
     # Spacing is chosen to be half the minimum distance
-    spacing = pdist(coord, minkowski, p=p).min() * 0.5
+    spacing = pdist(coord, metric=minkowski, p=p).min() * 0.5
 
-    out = ensure_spacing(coord, spacing=spacing, p_norm=p)
+    out = ensure_spacing(coord, spacing=spacing, p_norm=p,
+                         batch_size=size)
 
     assert np.array_equal(coord, out)
 
 
 @pytest.mark.parametrize("ndim", [1, 2, 3, 4, 5])
-def test_ensure_spacing_nD(ndim):
+@pytest.mark.parametrize("size", [2, 10, None])
+def test_ensure_spacing_nD(ndim, size):
     coord = np.ones((5, ndim))
 
     expected = np.ones((1, ndim))
 
-    assert np.array_equal(ensure_spacing(coord), expected)
+    assert np.array_equal(ensure_spacing(coord, batch_size=size), expected)
 
 
 @pytest.mark.parametrize("p", [1, 2, np.inf])
-def test_ensure_spacing_p_norm(p):
+@pytest.mark.parametrize("size", [50, 100, None])
+def test_ensure_spacing_batch_processing(p, size):
     coord = np.random.randn(100, 2)
 
     # --- Consider the average distance btween the point as spacing
-    spacing = np.median(pdist(coord, minkowski, p))
-    out = ensure_spacing(coord, spacing=spacing, p_norm=p)
+    spacing = np.median(pdist(coord, metric=minkowski, p=p))
 
-    assert pdist(out, minkowski, p).min() > spacing
+    expected = ensure_spacing(coord, spacing=spacing, p_norm=p)
+
+    assert np.array_equal(ensure_spacing(coord, spacing=spacing, p_norm=p,
+                                         batch_size=size),
+                          expected)
+
+
+
+@pytest.mark.parametrize("p", [1, 2, np.inf])
+@pytest.mark.parametrize("size", [30, 50, None])
+def test_ensure_spacing_p_norm(p, size):
+    coord = np.random.randn(100, 2)
+
+    # --- Consider the average distance btween the point as spacing
+    spacing = np.median(pdist(coord, metric=minkowski, p=p))
+    out = ensure_spacing(coord, spacing=spacing, p_norm=p, batch_size=size)
+
+    assert pdist(out, metric=minkowski, p=p).min() > spacing
