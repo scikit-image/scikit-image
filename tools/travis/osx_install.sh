bd8b2a2d830594d34c9be9bf072118274934cac7
Emmanuelle Gouillart <emma@plot.ly>
2020-11-15 15:13:12 +0100

    try to reduce Mac build time

--- tools/travis/osx_install.sh
+++ tools/travis/osx_install.sh
@@ -12,9 +12,6 @@ source ~/multibuild/osx_utils.sh
 get_macpython_environment $MB_PYTHON_VERSION ~/venv
 
 export PATH="$PATH:/Library/TeX/texbin"
-python -m pip install travis-wait-improved;
-travis-wait-improved --timeout 30m sudo tlmgr --verify-repo=none update --self
-travis-wait-improved --timeout 30m sudo tlmgr --verify-repo=none install ucs dvipng anyfontsize
 
 # libpng 1.6.32 has a bug in reading PNG
 # that seems to be the default library that is installed
