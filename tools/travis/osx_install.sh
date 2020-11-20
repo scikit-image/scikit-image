56eff1ba76cd6619d2041992bf953941243adf03
Emmanuelle Gouillart <emma@plot.ly>
2020-11-20 17:38:31 +0100

    remove ccache as well

--- tools/travis/osx_install.sh
+++ tools/travis/osx_install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ex
 
-# brew update
-brew install ccache
-
 # Set up virtualenv on OSX
 git clone --depth 1 --branch devel https://github.com/matthew-brett/multibuild ~/multibuild
 source ~/multibuild/osx_utils.sh
