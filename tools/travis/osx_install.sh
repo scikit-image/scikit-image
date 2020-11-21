0847753d9b468da1c09fb670a41bd8a04ba35011
Emmanuelle Gouillart <emma@plot.ly>
2020-11-21 16:46:33 +0100

    Revert "remove ccache as well"
    This reverts commit 56eff1ba76cd6619d2041992bf953941243adf03.

--- tools/travis/osx_install.sh
+++ tools/travis/osx_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+# brew update
+brew install ccache
+
 # Set up virtualenv on OSX
 git clone --depth 1 --branch devel https://github.com/matthew-brett/multibuild ~/multibuild
 source ~/multibuild/osx_utils.sh
