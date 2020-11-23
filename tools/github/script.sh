cd82595405044bc0c699fcbe7487059142c934f4
Emmanuelle Gouillart <emma@plot.ly>
2020-11-23 15:24:28 +0100

    debug

--- tools/github/script.sh
+++ tools/github/script.sh
@@ -6,20 +6,14 @@ mkdir -p $MPL_DIR
 touch $MPL_DIR/matplotlibrc
 
 
-section "List.installed.dependencies"
 python -m pip list
 tools/build_versions.py
-section_end "List.installed.dependencies"
 
-section "Test"
 # When installing from sdist
 # We can't run it in the git directory since there is a folder called `skimage`
 # in there. pytest will crawl that instead of the module we installed and want to test
 (cd .. && pytest $TEST_ARGS --pyargs skimage)
-section_end "Test"
 
-section "Flake8.test"
 flake8 --exit-zero --exclude=test_* skimage doc/examples viewer_examples
-section_end "Flake8.test"
 
 set +ev
