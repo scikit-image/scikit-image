ee94733b06d46673c7e699b4c23cf2958a9e65c2
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2020-12-07 11:59:09 -0800

    Backport PR #5104: Use v{} for version tags with pooch (#5110)
    Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>

--- skimage/data/__init__.py
+++ skimage/data/__init__.py
@@ -125,7 +125,12 @@ def create_image_fetcher():
     # This helps pooch understand that it should look in master
     # to find the required files
     pooch_version = __version__.replace('.dev', '+')
-    url = "https://github.com/scikit-image/scikit-image/raw/{version}/skimage/"
+    if '+' in pooch_version:
+        url = ("https://github.com/scikit-image/scikit-image/raw/"
+               "{version}/skimage/")
+    else:
+        url = ("https://github.com/scikit-image/scikit-image/raw/"
+               "v{version}/skimage/")
 
     # Create a new friend to manage your sample data storage
     image_fetcher = pooch.create(
