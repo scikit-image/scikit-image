c1d30038c4a45509ecbf58fb5566cdf2612a64ee
Riadh <r.fezzani@vitadx.com>
2020-12-08 17:25:39 +0100

    Fix pep8

--- skimage/_shared/coord.py
+++ skimage/_shared/coord.py
@@ -82,7 +82,7 @@ def ensure_spacing(coords, spacing=1, p_norm=np.inf, min_split_size=50):
             batch_list = [coords]
         else:
             coord_count = len(coords)
-            split_count = int(np.log2(coord_count/min_split_size)) + 1
+            split_count = int(np.log2(coord_count / min_split_size)) + 1
             split_idx = np.cumsum(
                 [coord_count // (2 ** i) for i in range(1, split_count)])
             batch_list = np.array_split(coords, split_idx)
