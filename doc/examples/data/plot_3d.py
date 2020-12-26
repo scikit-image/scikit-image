e86018c893581c18f0c424a526d4a686ef8976ab
Emmanuelle Gouillart <emma@plot.ly>
2020-12-26 16:57:27 +0100

    Apply suggestions from code review
    Co-authored-by: Marianne Corvellec <marianne.corvellec@ens-lyon.org>

--- doc/examples/data/plot_3d.py
+++ doc/examples/data/plot_3d.py
@@ -3,19 +3,20 @@
 Datasets with 3 or more spatial dimensions
 ==========================================
 
-Most scikit-image functions are compatible with 3-D datasets, ie images with
-3 spatial dimensions (as opposed to 2-D multichannel arrays, which also have
-three axes). func:`skimage.data.cells3d` returns a 3D fluorescence microscopy
-image of cells. The returned data is a 3D multichannel array with dimensions
-provided in ``(z, c, y, x)`` order. Channel 0 contains cell membranes, channel
+Most scikit-image functions are compatible with 3D datasets, i.e., images with
+3 spatial dimensions (to be distinguished from to 2D multichannel images, which 
+are also arrays with
+three axes). :func:`skimage.data.cells3d` returns a 3D fluorescence microscopy
+image of cells. The returned dataset is a 3D multichannel image with dimensions
+provided in ``(z, c, y, x)`` order. Channel 0 contains cell membranes, while channel
 1 contains nuclei.
 
-The example below shows how to explore this dataset. This 3-D image can be used
+The example below shows how to explore this dataset. This 3D image can be used
 to test the various functions of scikit-image.
 """
 from skimage import data
-import plotly.express as px
 import plotly
+import plotly.express as px
 
 img = data.cells3d()[20:]
 fig = px.imshow(img, facet_col=1, animation_frame=0,
