2e7617ae30180166be2e64b6ab9be1a5a293c297
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2020-12-31 17:30:53 +0100

    Tell a better story

--- doc/examples/applications/plot_3d_interaction.py
+++ doc/examples/applications/plot_3d_interaction.py
@@ -43,6 +43,48 @@ print(f'dtype: {data.dtype}')
 n_plane, n_Y, n_X, n_chan = data.shape
 
 #####################################################################
+# Let us consider only a slice (2D plane) of the data for now. More
+# specifically, let us consider the slice located halfway in the stack.
+# The `imshow` function can display both grayscale and RGB(A) 2D images.
+
+_, ax = plt.subplots()
+ax.imshow(data[n_plane // 2])
+
+#####################################################################
+# According to the warning message, the range of values is unexpected. The
+# image rendering is clearly not satisfactory colour-wise.
+
+vmin, vmax = data.min(), data.max()
+print(f'range: ({vmin}, {vmax})')
+
+#####################################################################
+# We turn to `plotly`'s implementation of the `imshow` function, for it
+# supports `value ranges
+# <https://plotly.com/python/imshow/#defining-the-data-range-covered-by-the-color-range-with-zmin-and-zmax>`_
+# beyond ``(0.0, 1.0)`` for floats and ``(0, 255)`` for integers.
+
+px.imshow(data[n_plane // 2], zmax=vmax)
+# sphinx_gallery_thumbnail_number = 2
+
+#####################################################################
+# Here you go, *fluorescence* microscopy!
+
+#####################################################################
+# Normalize range for each channel
+# ================================
+# Generally speaking, we may want to normalize the value range on a
+# per-channel basis. Let us facet our data (slice) along the channel axis.
+# This way, we get three single-channel images, where the max value of each
+# image is used:
+
+px.imshow(
+    data[n_plane // 2],
+    facet_col=2,
+    binary_string=True,
+    labels={'facet_col': 'channel'}
+)
+
+#####################################################################
 # What is the range of values for each colour channel?
 
 (vmin_0, vmin_1, vmin_2) = (data[:, :, :, 0].min(),
@@ -56,40 +98,13 @@ print(f'range for channel 1: ({vmin_1}, {vmax_1})')
 print(f'range for channel 2: ({vmin_2}, {vmax_2})')
 
 #####################################################################
-# We notice that the ranges of values is unusual: With images, we
-# typically expect a range of ``(0.0, 1.0)`` for float values and a range of
-# ``(0, 255)`` for integer values.
-# Let us consider only a slice (2D plane) of the data for now. More
-# specifically, let us consider the slice located halfway in the stack.
-# The `imshow` function can display both grayscale and RGB(A) 2D images.
-
-_, ax = plt.subplots()
-ax.imshow(data[n_plane // 2])
-
-#####################################################################
-# The warning message echoes our concern, while the image rendering is clearly
-# not satisfactory colour-wise. Not all multichannel images are RGB(A)!
-# We turn to `plotly`'s implementation of the `imshow` function, for it lets
-# us specify the `value range
-# <https://plotly.com/python/imshow/#defining-the-data-range-covered-by-the-color-range-with-zmin-and-zmax>`_
-# to map to a colour range.
+# We may want to be very specific and pass value ranges on a per-channel
+# basis:
 
 px.imshow(
     data[n_plane // 2],
     zmin=[vmin_0, vmin_1, vmin_2],
     zmax=[vmax_0, vmax_1, vmax_2]
-)
-# sphinx_gallery_thumbnail_number = 2
-
-#####################################################################
-# Normalize range for each channel
-# ================================
-
-px.imshow(
-    data[n_plane // 2],
-    facet_col=2,
-    binary_string=True,
-    labels={'facet_col': 'channel'}
 )
 
 #####################################################################
