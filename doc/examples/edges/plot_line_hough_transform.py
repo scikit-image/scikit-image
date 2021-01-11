c490784f1d92afe388678f6e0e73cd5e65125325
Joseph Fox-Rabinovitz <madphysicist@users.noreply.github.com>
2021-01-11 09:06:47 -0600

    BUG: Fixup to image orientation in example

--- doc/examples/edges/plot_line_hough_transform.py
+++ doc/examples/edges/plot_line_hough_transform.py
@@ -91,7 +91,7 @@ ax[0].set_title('Input image')
 ax[0].set_axis_off()
 
 ax[1].imshow(np.log(1 + h),
-             extent=[np.rad2deg(theta[-1]), np.rad2deg(theta[0]), d[-1], d[0]],
+             extent=[np.rad2deg(theta[0]), np.rad2deg(theta[-1]), d[-1], d[0]],
              cmap=cm.gray, aspect=1/1.5)
 ax[1].set_title('Hough transform')
 ax[1].set_xlabel('Angles (degrees)')
