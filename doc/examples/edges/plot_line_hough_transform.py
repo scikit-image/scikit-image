b9b8465e4b75011a9caac2ff4dbb27e675679d44
Joseph Fox-Rabinovitz <madphysicist@users.noreply.github.com>
2021-01-13 12:47:58 -0600

    Updated example to include three lines
    As requested in review

--- doc/examples/edges/plot_line_hough_transform.py
+++ doc/examples/edges/plot_line_hough_transform.py
@@ -65,6 +65,7 @@ import numpy as np
 
 from skimage.transform import hough_line, hough_line_peaks
 from skimage.feature import canny
+from skimage.draw import line
 from skimage import data
 
 import matplotlib.pyplot as plt
@@ -74,8 +75,9 @@ from matplotlib import cm
 # Constructing test image
 image = np.zeros((200, 200))
 idx = np.arange(25, 175)
-image[idx[::-1], idx] = 255
 image[idx, idx] = 255
+image[line(45, 25, 25, 175)] = 255
+image[line(25, 135, 175, 155)] = 255
 
 # Classic straight-line Hough transform
 # Set a precision of 0.5 degree.
