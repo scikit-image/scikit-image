91327bd1a3b02236b1d734263c00c422846c3859
Riadh <r.fezzani@vitadx.com>
2021-01-27 16:18:59 +0100

    Add float32 support to moments_hu

--- skimage/measure/_moments.py
+++ skimage/measure/_moments.py
@@ -345,7 +345,8 @@ def moments_hu(nu):
     array([7.45370370e-01, 3.51165981e-01, 1.04049179e-01, 4.06442107e-02,
            2.64312299e-03, 2.40854582e-02, 4.33680869e-19])
     """
-    return _moments_cy.moments_hu(nu.astype(np.double))
+    dtype = np.float32 if nu.dtype == 'float32' else np.float64
+    return _moments_cy.moments_hu(nu.astype(dtype, copy=False))
 
 
 def centroid(image):
