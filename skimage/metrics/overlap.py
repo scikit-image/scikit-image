4c1becec6e9a018d9d84866c56f1e9cfc9a5a1bf
Laurent Thomas <laurent132.thomas@laposte.net>
2021-02-08 10:20:12 +0100

    more pep8 formatting

--- skimage/metrics/overlap.py
+++ skimage/metrics/overlap.py
@@ -1,5 +1,6 @@
 """
-Module defining a number of functions to quantify the overlap between shapes, e.g. rectangles representing detections by bounding-boxes.
+Module defining a number of functions to quantify the overlap between shapes.
+for instance rectangles representing detections by bounding-boxes.
 
 """
 
@@ -10,15 +11,15 @@ class Rectangle():
     Utility functions to compute the intersection of 2 rectangles, or the equivalent area for te union of 2 rectangles are also provided.
     """
 
-    def __init__(self, topLeft, bottomRight=None, size=None):
+    def __init__(self, top_left, bottom_right=None, size=None):
         """
         Construct a rectangle using the (r,c) coordinates for the top left corner, and either the coordinates of the botton right corner or the rectangle dimensions (height, width).
 
         Parameters
         ----------
-        topLeft : tuple of 2 ints
+        top_left : tuple of 2 ints
             (r,c)-coordinates for the top left corner of the rectangle.
-        bottomRight : tuple of 2 ints, optional
+        bottom_right : tuple of 2 ints, optional
             (r,c)-coordinates for the bottom right corner of the rectangle. The default is None.
         size : tuple of 2 ints, optional
             Size of the rectangle (height, width). The default is None.
@@ -26,34 +27,34 @@ class Rectangle():
         Raises
         ------
         ValueError
-            If none or both of bottomRight and size are provided.
+            If none or both of bottom_right and size are provided.
 
         Returns
         -------
         Rectangle object.
 
         """
-        self.topLeft = topLeft
-        self.r = topLeft[0]
-        self.c = topLeft[1]
+        self.top_left = top_left
+        self.r = top_left[0]
+        self.c = top_left[1]
 
-        if (bottomRight is None) and (size is None):
-            raise ValueError("One of bottomRight or size argument should be provided.")
+        if (bottom_right is None) and (size is None):
+            raise ValueError("One of bottom_right or size argument should be provided.")
 
-        if (bottomRight is not None) and (size is not None):
-            raise ValueError("Either specify the bottomRight or size, not both.")
+        if (bottom_right is not None) and (size is not None):
+            raise ValueError("Either specify the bottom_right or size, not both.")
 
-        if bottomRight is not None:
-            self.bottomRight = bottomRight
-            self.height = self.bottomRight[0] - self.topLeft[0] +1
-            self.width  = self.bottomRight[1] - self.topLeft[1] +1
+        if bottom_right is not None:
+            self.bottom_right = bottom_right
+            self.height = self.bottom_right[0] - self.top_left[0] +1
+            self.width = self.bottom_right[1] - self.top_left[1] +1
             self.size = (self.height, self.width)
 
         elif size is not None:
             self.height, self.width = size
             self.size = size
-            self.bottomRight = (self.r + self.height-1,
-                                self.c + self.width -1)
+            self.bottom_right = (self.r + self.height-1,
+                                 self.c + self.width -1)
 
         self.area = self.height * self.width
 
@@ -64,13 +65,13 @@ class Rectangle():
         if not isinstance(rectangle2, Rectangle):
             return False
 
-        if self.topLeft == rectangle2.topLeft and self.bottomRight == rectangle2.bottomRight:
+        if self.top_left == rectangle2.top_left and self.bottom_right == rectangle2.bottom_right:
             return True
 
         return False
 
     @staticmethod
-    def isIntersecting(rectangle1, rectangle2):
+    def is_intersecting(rectangle1, rectangle2):
         """
         Check if 2 rectangles are intersecting.
 
@@ -88,11 +89,11 @@ class Rectangle():
         True if the rectangles are intersecting.
         """
         # If one rectangle is on left side of other
-        if rectangle1.topLeft[1] >= rectangle2.bottomRight[1] or rectangle2.topLeft[1] >= rectangle1.bottomRight[1]:
+        if rectangle1.top_left[1] >= rectangle2.bottom_right[1] or rectangle2.top_left[1] >= rectangle1.bottom_right[1]:
             return False
 
         # If one rectangle is above other
-        if rectangle1.topLeft[0] >= rectangle2.bottomRight[0] or rectangle2.topLeft[0] >= rectangle1.bottomRight[0]:
+        if rectangle1.top_left[0] >= rectangle2.bottom_right[0] or rectangle2.top_left[0] >= rectangle1.bottom_right[0]:
             return False
 
         return True
@@ -113,22 +114,22 @@ class Rectangle():
         ------
         ValueError
             If the rectangles are not intersecting.
-            The function isIntersecting can be called to first test if the rectangles are intersecting.
+            The function is_intersecting can be called to first test if the rectangles are intersecting.
 
         Returns
         -------
         Rectangle object representing the intersection.
         """
-        if not Rectangle.isIntersecting(rectangle1, rectangle2):
-            raise ValueError("The rectangles are not intersecting. Use isIntersecting to first test if the rectangles are intersecting.")
+        if not Rectangle.is_intersecting(rectangle1, rectangle2):
+            raise ValueError("The rectangles are not intersecting. Use is_intersecting to first test if the rectangles are intersecting.")
 
         # determine the (x, y)-coordinates of the top left and bottom right points of the intersection rectangle
         r = max(rectangle1.r, rectangle2.r)
         c = max(rectangle1.c, rectangle2.c)
-        bottomRight_r = min(rectangle1.bottomRight[0], rectangle2.bottomRight[0])
-        bottomRight_c = min(rectangle1.bottomRight[1], rectangle2.bottomRight[1])
+        bottom_right_r = min(rectangle1.bottom_right[0], rectangle2.bottom_right[0])
+        bottom_right_c = min(rectangle1.bottom_right[1], rectangle2.bottom_right[1])
 
-        return Rectangle((r,c), (bottomRight_r, bottomRight_c))
+        return Rectangle((r, c), (bottom_right_r, bottom_right_c))
 
     @staticmethod
     def intersection_area(rectangle1, rectangle2):
@@ -145,7 +146,7 @@ class Rectangle():
         Intersection, float
             a float value corresponding to the intersection area.
         """
-        if not Rectangle.isIntersecting(rectangle1, rectangle2):
+        if not Rectangle.is_intersecting(rectangle1, rectangle2):
             return 0
 
         # Compute area of the intersecting box
@@ -154,9 +155,9 @@ class Rectangle():
     @staticmethod
     def union_area(rectangle1, rectangle2):
         """Compute the area for the rectangle corresponding to the union of 2 rectangles."""
-        return (  rectangle1.area
+        return (rectangle1.area
                 + rectangle2.area
-                - Rectangle.intersection_area(rectangle1 , rectangle2) )
+                - Rectangle.intersection_area(rectangle1, rectangle2))
 
     @staticmethod
     def intersection_over_union(rectangle1, rectangle2):
@@ -170,15 +171,15 @@ class Rectangle():
 
 if __name__ == "__main__":
 
-    height1, width1 = 2,4
+    height1, width1 = 2, 4
     rectangle1 = Rectangle((0, 0), size=(height1, width1)) # bottom right in (1,3) thus
 
     height2 = width2 = 3
     rectangle2 = Rectangle((1, 3), size=(height2, width2))
 
-    rectangle3 = Rectangle((0,0), bottomRight=(2,2))
+    rectangle3 = Rectangle((0, 0), bottom_right=(2, 2))
 
-    rectangle4 = Rectangle((10,10), size=(5,5))
+    rectangle4 = Rectangle((10, 10), size=(5, 5))
 
 
     # Check area
@@ -187,16 +188,16 @@ if __name__ == "__main__":
     assert rectangle3.area == 3*3
 
     # Check other dimension (the one not set in constructor)
-    assert rectangle1.bottomRight == (1,3)
-    assert (rectangle3.height, rectangle3.width) == (3,3)
+    assert rectangle1.bottom_right == (1, 3)
+    assert (rectangle3.height, rectangle3.width) == (3, 3)
 
     # Intersections
-    assert Rectangle.isIntersecting(rectangle1, rectangle2) == False
-    assert Rectangle.isIntersecting(rectangle1, rectangle3) == True
+    assert not Rectangle.is_intersecting(rectangle1, rectangle2)
+    assert Rectangle.is_intersecting(rectangle1, rectangle3)
 
     # Intersection rectangle and == comparison
     rect_inter13 = Rectangle.intersection_rectangles(rectangle1, rectangle3)
-    assert  rect_inter13 == Rectangle((0,0), bottomRight=(1,2))
+    assert  rect_inter13 == Rectangle((0, 0), bottom_right=(1, 2))
     assert rect_inter13 != 5
     #intersection_rectangles(rectangle1, rectangle2) # should raise an issue
 
