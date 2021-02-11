dca3e54454390f05f9ab2036173b5909a4160d95
Gregory Lee <grlee77@gmail.com>
2021-02-11 11:17:36 -0500

    update inpainting demo
    extend defects to long+narrow and point-like

--- doc/examples/filters/plot_inpaint.py
+++ doc/examples/filters/plot_inpaint.py
@@ -28,20 +28,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from skimage import data
+from skimage.morphology import disk, binary_dilation
 from skimage.restoration import inpaint
 
-image_orig = data.astronaut()[0:200, 0:200]
+image_orig = data.astronaut()
 
-# Create mask with three defect regions: left, middle, right respectively
-mask = np.zeros(image_orig.shape[:-1])
+# Create mask with six block defect regions
+mask = np.zeros(image_orig.shape[:-1], dtype=bool)
 mask[20:60, 0:20] = 1
 mask[160:180, 70:155] = 1
 mask[30:60, 170:195] = 1
+mask[-60:-30, 170:195] = 1
+mask[-180:-160, 70:155] = 1
+mask[-60:-20, 0:20] = 1
 
-# Defect image over the same region in each color channel
-image_defect = image_orig.copy()
-for layer in range(image_defect.shape[-1]):
-    image_defect[np.where(mask)] = 0
+# add a few long, narrow defects
+mask[200:205, -200:] = 1
+mask[150:255, 20:23] = 1
+mask[365:368, 60:130] = 1
+
+# add randomly positioned small point-like defects
+rstate = np.random.RandomState(0)
+for radius in [0, 2, 4]:
+    # larger defects are less common
+    thresh = 3 + 0.25 * radius # larger defects less commmon
+    tmp_mask = rstate.randn(*image_orig.shape[:-1]) > thresh
+    if radius > 0:
+        tmp_mask = binary_dilation(tmp_mask, disk(radius, dtype=bool))
+    mask[tmp_mask] = 1
+
+# Apply defect mask to the image over the same region in each color channel
+image_defect = image_orig * ~mask[..., np.newaxis]
 
 image_result = inpaint.inpaint_biharmonic(image_defect, mask,
                                           multichannel=True)
