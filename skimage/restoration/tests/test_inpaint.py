0ba67cb7f864ff34a52d50dfb6c4010cbe43cee8
Gregory Lee <grlee77@gmail.com>
2021-02-17 01:14:14 -0500

    Test output dtype
    Test split_into_regions parameter

--- skimage/restoration/tests/test_inpaint.py
+++ skimage/restoration/tests/test_inpaint.py
@@ -6,14 +6,16 @@ from skimage._shared import testing
 from skimage._shared.testing import assert_allclose
 
 
-def test_inpaint_biharmonic_2d():
+@testing.parametrize('split_into_regions', [False, True])
+def test_inpaint_biharmonic_2d(split_into_regions):
     img = np.tile(np.square(np.linspace(0, 1, 5)), (5, 1))
     mask = np.zeros_like(img)
     mask[2, 2:] = 1
     mask[1, 3:] = 1
     mask[0, 4:] = 1
     img[np.where(mask)] = 0
-    out = inpaint.inpaint_biharmonic(img, mask)
+    out = inpaint.inpaint_biharmonic(img, mask,
+                                     split_into_regions=split_into_regions)
     ref = np.array(
         [[0., 0.0625, 0.25000000, 0.5625000, 0.73925058],
          [0., 0.0625, 0.25000000, 0.5478048, 0.76557821],
@@ -24,7 +26,29 @@ def test_inpaint_biharmonic_2d():
     assert_allclose(ref, out)
 
 
-def test_inpaint_biharmonic_3d():
+@testing.parametrize('dtype', [np.float32, np.float64])
+def test_inpaint_biharmonic_2d_float_dtypes(dtype):
+    img = np.tile(np.square(np.linspace(0, 1, 5)), (5, 1))
+    mask = np.zeros_like(img)
+    mask[2, 2:] = 1
+    mask[1, 3:] = 1
+    mask[0, 4:] = 1
+    img[np.where(mask)] = 0
+    img = img.astype(dtype, copy=False)
+    out = inpaint.inpaint_biharmonic(img, mask)
+    assert out.dtype == img.dtype
+    ref = np.array(
+        [[0., 0.0625, 0.25000000, 0.5625000, 0.73925058],
+         [0., 0.0625, 0.25000000, 0.5478048, 0.76557821],
+         [0., 0.0625, 0.25842878, 0.5623079, 0.85927796],
+         [0., 0.0625, 0.25000000, 0.5625000, 1.00000000],
+         [0., 0.0625, 0.25000000, 0.5625000, 1.00000000]]
+    )
+    assert_allclose(ref, out, rtol=1e-5)
+
+
+@testing.parametrize('split_into_regions', [False, True])
+def test_inpaint_biharmonic_3d(split_into_regions):
     img = np.tile(np.square(np.linspace(0, 1, 5)), (5, 1))
     img = np.dstack((img, img.T))
     mask = np.zeros_like(img)
@@ -32,7 +56,8 @@ def test_inpaint_biharmonic_3d():
     mask[1, 3:, :] = 1
     mask[0, 4:, :] = 1
     img[np.where(mask)] = 0
-    out = inpaint.inpaint_biharmonic(img, mask)
+    out = inpaint.inpaint_biharmonic(img, mask,
+                                     split_into_regions=split_into_regions)
     ref = np.dstack((
         np.array(
             [[0.0000, 0.0625, 0.25000000, 0.56250000, 0.53752796],
