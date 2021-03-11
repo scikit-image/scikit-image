96304259ec43b171e0721e7e6d5f34a389922a0c
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-03-11 01:41:31 -0600

    Simplify creation of translation matrix

--- skimage/transform/_geometric.py
+++ skimage/transform/_geometric.py
@@ -71,12 +71,8 @@ def _center_and_normalize_points(points):
 
     norm_factor = np.sqrt(d) / rms
 
-    part_matrix = norm_factor * np.concatenate(
-            (np.eye(d), -centroid[:, np.newaxis]), axis=1
-            )
-    matrix = np.concatenate(
-            (part_matrix, [[0,] * d + [1]]), axis=0
-            )
+    matrix = np.eye(d + 1)
+    matrix[:-1, -1] = -centroid
 
     points_h = np.row_stack([points.T, np.ones(n)])
 
