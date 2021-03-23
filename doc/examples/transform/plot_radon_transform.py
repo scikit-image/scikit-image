af7a08f30073b6ed09cd0f0cd67ba35cbe0bd4c7
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-03-23 16:13:49 +0100

    Update doc/examples/transform/plot_radon_transform.py

--- doc/examples/transform/plot_radon_transform.py
+++ doc/examples/transform/plot_radon_transform.py
@@ -104,12 +104,15 @@ plt.show()
 import matplotlib.pyplot as plt
 from skimage.transform.radon_transform import _get_fourier_filter
 
-filters = ["ramp", "shepp-logan", "cosine", "hamming", "hann"]
-for f in filters:
+filters = ['ramp', 'shepp-logan', 'cosine', 'hamming', 'hann']
+
+for ix, f in enumerate(filters):
     response = _get_fourier_filter(2000, f)
-    plt.plot(response, label=f)
+    plt.plot(response, label= f)
+
+plt.xlim([0, 1000])
+plt.xlabel('frequency')
 plt.legend()
-plt.xlabel("position in pixel")
 plt.show()
 
 ######################################################################
