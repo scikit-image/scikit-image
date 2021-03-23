809e14492712f9bf20800fc04316697c78e2008f
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-03-23 17:22:56 +0100

    Update doc/examples/transform/plot_radon_transform.py
    Co-authored-by: Marianne Corvellec <marianne.corvellec@ens-lyon.org>

--- doc/examples/transform/plot_radon_transform.py
+++ doc/examples/transform/plot_radon_transform.py
@@ -108,7 +108,7 @@ filters = ['ramp', 'shepp-logan', 'cosine', 'hamming', 'hann']
 
 for ix, f in enumerate(filters):
     response = _get_fourier_filter(2000, f)
-    plt.plot(response, label= f)
+    plt.plot(response, label=f)
 
 plt.xlim([0, 1000])
 plt.xlabel('frequency')
