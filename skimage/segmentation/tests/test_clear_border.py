ebe0766c0daf943f5b06fc27fbc8c7556b0ef88a
François Boulogne <devel@sciunto.org>
2021-04-06 09:16:24 +0200

    Replace numpy dtype by builtin dtype

--- skimage/segmentation/tests/test_clear_border.py
+++ skimage/segmentation/tests/test_clear_border.py
@@ -35,7 +35,7 @@ def test_clear_border():
                      [1, 1, 1, 1, 1, 1, 1, 1, 1],
                      [1, 1, 1, 1, 1, 1, 1, 1, 1],
                      [1, 1, 1, 1, 1, 1, 1, 1, 1],
-                     [1, 1, 1, 1, 1, 1, 1, 1, 1]]).astype(np.bool)
+                     [1, 1, 1, 1, 1, 1, 1, 1, 1]]).astype(bool)
     result = clear_border(image.copy(), mask=mask)
     ref = image.copy()
     ref[1:3, 0:2] = 0
