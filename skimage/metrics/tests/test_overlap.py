92be3226fb1095e512f297f48a2ec3237d751c11
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-04-06 08:53:48 -0500

    Fix tests

--- skimage/metrics/tests/test_overlap.py
+++ skimage/metrics/tests/test_overlap.py
@@ -8,10 +8,10 @@ from skimage.metrics.overlap import (
 from skimage._shared import testing
 
 height1, width1 = 2, 4
-rectangle1 = Rectangle((0, 0), dimensions=(height1, width1))  # bottom right in (1,3) thus
+rectangle1 = Rectangle((0, 0), dimensions=(height1, width1))
 
 height2 = width2 = 3
-rectangle2 = Rectangle((1, 3), dimensions=(height2, width2))
+rectangle2 = Rectangle((2, 3), dimensions=(height2, width2))
 
 rectangle3 = Rectangle((0, 0), bottom_right=(2, 2))
 
@@ -25,17 +25,17 @@ union_area13 = union_area(rectangle1, rectangle3)
 def test_area():
     assert rectangle1.area == height1 * width1
     assert rectangle2.area == height2 * width2
-    assert rectangle3.area == 3*3
+    assert rectangle3.area == 2 * 2
 
 
 def test_constructor_size():
     assert tuple(rectangle1.top_left) == (0, 0)
-    assert tuple(rectangle1.bottom_right) == (1, 3)
+    assert tuple(rectangle1.bottom_right) == (2, 4)
 
 
 def test_constructor_bottom_corner():
     assert tuple(rectangle3.top_left) == (0, 0)
-    assert tuple(rectangle3.dimensions) == (3, 3)
+    assert tuple(rectangle3.dimensions) == (2, 2)
 
 
 def test_intersection():
@@ -43,8 +43,8 @@ def test_intersection():
     assert is_intersecting(rectangle1, rectangle3)
 
 
-def test_eq_operator():# Intersection rectangle and == comparison
-    assert rect_inter13 == Rectangle((0, 0), bottom_right=(1, 2))
+def test_eq_operator():  # Intersection rectangle and == comparison
+    assert rect_inter13 == Rectangle((0, 0), bottom_right=(2, 2))
     with testing.raises(ValueError):
         intersection_rectangle(rectangle1, rectangle2)
 
