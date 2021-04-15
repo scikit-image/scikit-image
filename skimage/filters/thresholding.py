fa010b374e472529a3095da9ab9740d87bbff908
divyank <divyank.agarwal1993@gmail.com>
2021-04-15 16:44:49 +0530

    adding generalized historgram thesholding method

--- skimage/filters/thresholding.py
+++ skimage/filters/thresholding.py
@@ -1233,3 +1233,123 @@ def threshold_multiotsu(image, classes=3, nbins=256):
     thresh = bin_centers[thresh_idx]
 
     return thresh
+
+
+
+
+csum = lambda z: np.cumsum(z)[:-1]
+dsum = lambda z: np.cumsum(z[::-1])[-2::-1]
+argmax = lambda x, f: np.mean(x[:-1][f == np.max(f)])  # Use the mean for ties.
+clip = lambda z: np.maximum(1e-30, z)
+
+
+def _preliminaries(n, x= None):
+  """calculate preliminary values to be utlized in the core GHT/TGH algorithm
+  
+  Parameters
+  ----------------------------
+  n : ndarray 
+      Each element is the number of pixels falling in each intensity bin.
+  x : ndaarray, optional
+      Each element is the value corresponding to the center of each intensity bin.
+  
+  Returns
+  ----------------------------
+  x  : ndarray 
+      value corresponding the center of each intensity bin
+  w0 : ndarray 
+      sums of histogram count in n below each split  , size =len(x) -1
+  w1 : ndarray
+      sums of histogram count in n above each split, size = len(x) -1
+  p0 : ndarray
+       weighted mean of all elemnts of x below each split, size = len(x) -1
+  p1 : ndarray
+      weighted mean of all elemnts of x above each split, size = len(x) -1
+  mu0: ndarray
+      weighted distrotion  of all elemnts of x below each split, size = len(x) -1
+  mu1: ndarray
+       weighted distrotion of all elemnts of x above each split, size = len(x) -1
+  d0 : ndarray
+      complex definition
+  d1 : ndarray
+      complex definition
+  
+  References
+  -------------------
+  .. [1] A Generalization of Otsu's Method and Minimum Error Thresholding
+         Jonathan T. Barron, ECCV, 2020
+
+  """
+  assert np.all(n >= 0)
+  x = np.arange(len(n), dtype=n.dtype) if x is None else x
+  assert np.all(x[1:] >= x[:-1])
+    
+  w0 = clip(csum(n))
+  w1 = clip(dsum(n))
+  p0 = w0 / (w0 + w1)
+  p1 = w1 / (w0 + w1)
+  mu0 = csum(n * x) / w0
+  mu1 = dsum(n * x) / w1
+  d0 = csum(n * x**2) - w0 * mu0**2
+  d1 = dsum(n * x**2) - w1 * mu1**2
+  return x, w0, w1, p0, p1, mu0, mu1, d0, d1
+
+
+def theshold_generalized_histogram(n, x=None, nu=0, tau=0, kappa=0, omega=0.5):
+    """Compute the generalized histogram threshold for a histogram
+
+
+    Parameters
+    ----------------------------
+    n : ndarray 
+      Each element is the number of pixels falling in each intensity bin.
+    x : ndaarray, optional
+      Each element is the value corresponding to the center of each intensity bin.
+    nu : Float
+        Degree of freedom , positive integer, scaled inverse chi-squared distribution parameter
+    tau : Float
+        scaling parameter, scaled inverse chi-squared distribution
+    kappa : Float
+        concentration , beta distribution
+    omega : Float
+            mode   , beta distribution ,  0 <= omega <= 1
+
+    Returns
+    ----------------------------
+    t  : float
+        Threshold bin value, corresponding to maximum score
+    score : ndarray 
+        all possible values of score for each possible threshold value
+
+
+    References
+    -------------------
+    .. [1] A Generalization of Otsu's Method and Minimum Error Thresholding
+         Jonathan T. Barron, ECCV, 2020
+    
+    Examples
+    ----------------
+    
+    >>> from skimage.data import camera
+    >>> data = camera()
+    >>> counts, bin_centers = histogram(data.ravel(), 256, source_range = "image" )
+
+    >>> default_nu = np.sum(counts)
+    >>> default_tau = np.sqrt(1/12)
+    >>> default_kappa = np.sum(counts)
+    >>> default_omega = 0.5
+
+    >>> t, score = theshold_generalized_histogram(counts, bin_centers, default_nu, default_tau, default_kappa, default_omega)
+    >>> binary = data<=t
+    """
+    assert nu >= 0
+    assert tau >= 0
+    assert kappa >= 0
+    assert omega >= 0 and omega <= 1
+    x, w0, w1, p0, p1, _, _, d0, d1 = _preliminaries(n, x)
+    v0 = clip((p0 * nu * tau**2 + d0) / (p0 * nu + w0))
+    v1 = clip((p1 * nu * tau**2 + d1) / (p1 * nu + w1))
+    f0 = -d0 / v0 - w0 * np.log(v0) + 2 * (w0 + kappa *      omega)  * np.log(w0)
+    f1 = -d1 / v1 - w1 * np.log(v1) + 2 * (w1 + kappa * (1 - omega)) * np.log(w1)
+    return argmax(x, f0 + f1), f0 + f1
+
