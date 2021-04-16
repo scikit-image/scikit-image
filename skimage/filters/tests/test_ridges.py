75529410b0c213e35c6ea00b5f90e00773922c4d
Gregory Lee <grlee77@gmail.com>
2021-04-16 10:56:42 -0400

    float32 support in filters.ridges

--- skimage/filters/tests/test_ridges.py
+++ skimage/filters/tests/test_ridges.py
@@ -1,11 +1,15 @@
-import pytest
 import numpy as np
+import pytest
 from numpy.testing import assert_allclose, assert_array_less, assert_equal
-from skimage.filters import meijering, sato, frangi, hessian
-from skimage.data import camera, retina
-from skimage.util import crop, invert
-from skimage.color import rgb2gray
+
+from skimage import img_as_float
+from skimage._shared import testing
 from skimage._shared._warnings import expected_warnings
+from skimage._shared.utils import _supported_float_type
+from skimage.color import rgb2gray
+from skimage.data import camera, retina
+from skimage.filters import meijering, sato, frangi, hessian
+from skimage.util import crop, invert
 
 
 def test_2d_null_matrix():
@@ -185,6 +189,13 @@ def test_2d_cropped_camera_image():
                     ones, atol=1 - 1e-7)
     assert_allclose(hessian(a_white, black_ridges=False, mode='reflect'),
                     ones, atol=1 - 1e-7)
+
+
+@testing.parametrize('func', [meijering, sato, frangi, hessian])
+@testing.parametrize('dtype', [np.float16, np.float32, np.float64])
+def test_ridge_output_dtype(func, dtype):
+    img = img_as_float(camera()).astype(dtype, copy=False)
+    assert func(img).dtype == _supported_float_type(img.dtype)
 
 
 def test_3d_cropped_camera_image():
