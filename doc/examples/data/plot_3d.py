7747424f3b57108bc1082e98cd469027bcec66fa
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2021-04-22 14:12:53 +0200

    Favour plotly rendering in sphinx gallery

--- doc/examples/data/plot_3d.py
+++ doc/examples/data/plot_3d.py
@@ -15,7 +15,6 @@ The example below shows how to explore this dataset. This 3D image can be used
 to test the various functions of scikit-image.
 """
 from skimage import data
-import plotly
 import plotly.express as px
 
 img = data.cells3d()[20:]
@@ -23,4 +22,4 @@ fig = px.imshow(img, facet_col=1, animation_frame=0,
                 binary_string=True, binary_format='jpg')
 fig.layout.annotations[0]['text'] = 'Cell membranes'
 fig.layout.annotations[1]['text'] = 'Nuclei'
-plotly.io.show(fig)
+fig
