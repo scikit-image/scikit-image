fe93ff1052b5f2d53e95b6042c568e49c9fc54bb
Gregory Lee <grlee77@gmail.com>
2021-04-28 17:53:58 -0400

    fix copy-paste error in color/setup.py

--- skimage/color/setup.py
+++ skimage/color/setup.py
@@ -10,7 +10,7 @@ base_path = os.path.abspath(os.path.dirname(__file__))
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
 
-    config = Configuration('_shared', parent_package, top_path)
+    config = Configuration('color', parent_package, top_path)
 
     cython(['_colorconv.pyx'], working_path=base_path)
 
