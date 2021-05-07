398c5fdf3900371e54fe30f0059610a4abc6ce01
Niels Cautaerts <nielscautaerts@hotmail.com>
2021-05-07 20:28:43 +0200

    support multidimensional and multichannel images

--- skimage/filters/_fft_based.py
+++ skimage/filters/_fft_based.py
@@ -1,17 +1,17 @@
 import numpy as np
 import scipy.fft as fft
+from skimage.exposure import rescale_intensity
+from skimage import dtype_limits
 
 
-def _get_2D_butterworth_filter(nx, ny, factor, order, high_pass):
+def _get_ND_butterworth_filter(shape, factor, order, high_pass):
     """
-    Get a 2D Butterworth mask centered on the middle of the fft
+    Get a N-dimensional Butterworth mask
 
     Parameters
     ----------
-    nx : int
-        width of filter in pixels
-    ny : int
-        height of filter in pixels
+    shape : tuple of ints
+        dimensions of the n-dimensional image and mask
     factor : float
         fraction of width and height of the filter where the cutoff should be
     order : float
@@ -22,28 +22,33 @@ def _get_2D_butterworth_filter(nx, ny, factor, order, high_pass):
 
     Returns
     -------
-    wfilt : (nx, ny) numpy array
+    wfilt : numpy array of shape
         The filter/fft mask
     """
-    lsize = np.array([nx, ny]) * factor
-    # start and stop is to ensure center of mask aligns with center of fft
-    gx = np.arange(-(nx - 1) // 2, (nx - 1) // 2 + 1) / lsize[0]
-    gy = np.arange(-(ny - 1) // 2, (ny - 1) // 2 + 1) / lsize[1]
-    qxa, qya = np.meshgrid(gx, gy)
-    q2 = np.sqrt(np.square(qxa) + np.square(qya))
-    wfilt = 1 / np.sqrt(1 + np.power(q2, order))
+    ranges = []
+    for d in shape:
+        # start and stop ensures center of mask aligns with center of fft
+        axis = np.arange(-(d - 1) // 2, (d - 1) // 2 + 1) / (d * factor)
+        ranges.append(axis**2)
+    # q2 = euclidian distance squared grid
+    q2 = np.sum(np.meshgrid(*ranges, indexing="ij"), axis=0)
+    # division of order by 2 to avoid additional square root on q2
+    wfilt = 1 / np.sqrt(1 + np.power(q2, order/2))
     if high_pass:
         wfilt = 1 - wfilt
-    return wfilt
+    return fft.ifftshift(wfilt)
 
 
 def butterworth(image,
                 cutoff_frequency_ratio=0.005,
                 high_pass=True,
-                order=2.0):
+                order=2.0,
+                channel_axis=None,
+                preserve_range=False,
+                ):
     """
-    This function applies a Butterworth Fourier filter to a 2D image for
-    enhancing high or low frequency features.
+    This function applies a Butterworth Fourier filter to an N dimensional
+    image for enhancing high or low frequency features.
 
     Parameters
     ----------
@@ -51,12 +56,19 @@ def butterworth(image,
         The 2D image to be filtered
     cutoff_frequency_ratio : float, optional
         Determines the position of the cut-off relative to the
-        width and height of the FFT size
+        shape of the FFT
     high_pass : bool, optional
-        Whether to perform a high pass filter, if not a low pass filter
+        Whether to perform a high pass filter. If False, a low pass filter is
+        performed.
     order : float, optional
         Order of the filter which affects the slope near the cut-off.
-        Higher order means steeper slope.
+        Higher order means steeper slope in frequency space.
+    channel_axis : int, optional
+        If there is a channel dimension, provide the index here. If None
+        (default) then all axes are assumed spatial dimensions.
+    preserve_range : bool, optional
+        Whether to keep the original range of values. Otherwise the input
+        image is returned with dtype float with values scaled between 0 and 1.
 
     Returns
     -------
@@ -76,13 +88,21 @@ def butterworth(image,
     Butterworth, Stephen. "On the theory of filter amplifiers."
     Wireless Engineer 7.6 (1930): 536-541.
     """
-    if image.ndim > 2:
-        raise ValueError("Only single channel images supported")
-    (Ny, Nx) = image.shape
-    wfilt = _get_2D_butterworth_filter(Nx, Ny, cutoff_frequency_ratio,
+    fft_shape = (image.shape if channel_axis is None
+                 else np.delete(image.shape, channel_axis))
+    wfilt = _get_ND_butterworth_filter(fft_shape, cutoff_frequency_ratio,
                                        order, high_pass)
-    im_fft = fft.fftn(image)
-    im_fft = fft.fftshift(im_fft)
-    filtered = fft.ifftshift(wfilt * im_fft)
-    butterfilt = np.real(fft.ifft2(filtered))
+    butterfilt = np.empty(image.shape, dtype=np.float64)
+    if channel_axis is None:
+        im_fft = fft.fftn(image)
+        butterfilt[:] = np.real(fft.ifftn(wfilt * im_fft))
+    else:
+        abs_channel = channel_axis % image.ndim
+        post = image.ndim - abs_channel - 1
+        for i in range(image.shape[channel_axis]):
+            sl = (slice(None),)*abs_channel + (i,) + (slice(None),)*post
+            im_fft = fft.fftn(image[sl])
+            butterfilt[sl] = np.real(fft.ifftn(wfilt * im_fft))
+    out_range = dtype_limits(image) if preserve_range else (0., 1.)
+    butterfilt = rescale_intensity(butterfilt, out_range=out_range)
     return butterfilt
--- skimage/filters/tests/test_fft_based.py
+++ skimage/filters/tests/test_fft_based.py
@@ -3,14 +3,32 @@ import numpy as np
 from skimage.filters import butterworth
 
 
-def test_butterworth():
+def test_butterworth_2D():
     im = np.zeros((4, 4))
     filtered = butterworth(im)
     assert filtered.shape == im.shape
     assert np.all(im == filtered)
 
 
-@pytest.mark.xfail(strict=True, raises=ValueError)
-def test_butterworth_fail():
-    im = np.zeros((4, 4, 2))
-    butterworth(im)
+def test_butterworth_3D():
+    im = np.zeros((3, 4, 5))
+    filtered = butterworth(im)
+    assert filtered.shape == im.shape
+    assert np.all(im == filtered)
+
+
+def test_butterworth_4D():
+    im = np.zeros((3, 4, 5, 6))
+    filtered = butterworth(im, preserve_range=True)
+    assert filtered.shape == im.shape
+    assert np.all(im == filtered)
+
+
+@pytest.mark.parametrize(
+        "chan",
+        [0, 1, 2, 3]
+        )
+def test_butterworth_4D_channel(chan):
+    im = np.zeros((3, 4, 5, 6))
+    filtered = butterworth(im, channel_axis=chan)
+    assert filtered.shape == im.shape
