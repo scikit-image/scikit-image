5a88aeb1043de6d1f6f53d5b243327447c29d783
Gregory Lee <grlee77@gmail.com>
2021-05-11 15:48:52 -0400

    commit missing files

---
+++ skimage/morphology/grey.py
@@ -0,0 +1,9 @@
+import warnings
+
+from .gray import *
+
+warnings.warn(
+    "Importing from skimage.morphology.grey is deprecated. "
+    "Please import from skimage.morphology instead.",
+    FutureWarning, stacklevel=2
+)
