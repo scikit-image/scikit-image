cac06d980e4124921f8839d16e2d9da1d8bcb575
Gregory Lee <grlee77@gmail.com>
2021-05-12 11:28:53 -0400

    keep an __all__ entry to grey.py as before

--- skimage/morphology/grey.py
+++ skimage/morphology/grey.py
@@ -1,6 +1,10 @@
 import warnings
 
-from .gray import *
+from .gray import (black_tophat, closing, dilation, erosion, opening,
+                   white_tophat)
+
+__all__ = ['erosion', 'dilation', 'opening', 'closing', 'white_tophat',
+           'black_tophat']
 
 
 warnings.warn(
