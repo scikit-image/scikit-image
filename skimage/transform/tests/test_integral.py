5bae2acfe0b94a18ebb7b92b7fcae119bff114b7
Gregory R. Lee <grlee77@gmail.com>
2021-06-08 03:32:22 -0400

    improved single precision support in skimage.transform (#5372)
    * expand dtype tests for warp functions
    
    make sure float16 is promoted to float32 when necessary
    
    * ensure pyramid_gaussian promotes float16 to float32
    
    * ensure radon_transform promotes float16 to float32
    
    * ensure pyramid_laplacian promotes float16 to float32
    
    * expand integral_image dtypes tested
    
    make sure floating point integral images are computed in at least double precision
    
    * pep8
    
    * restore old dtype behavior for integral_image
    
    * TST: style: reduce use of skimage._shared.testing
    
    * update test style of remaining files in transform/tests for consistency
    
    * TST: add np.float16 to additional warp tests
    
    * fix import in test_geometric.py

--- skimage/transform/tests/test_integral.py
+++ skimage/transform/tests/test_integral.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
+from numpy.testing import assert_allclose, assert_equal
 
-from skimage._shared.testing import assert_allclose, assert_equal
 from skimage.transform import integral_image, integrate
 
 
@@ -34,14 +34,14 @@ def test_integral_image_validity(dtype, dtype_as_kwarg):
             assert_equal(out[-1, -1], y.sum())
 
 
-def test_basic():
+def test_integrate_basic():
     assert_equal(x[12:24, 10:20].sum(), integrate(s, (12, 10), (23, 19)))
     assert_equal(x[:20, :20].sum(), integrate(s, (0, 0), (19, 19)))
     assert_equal(x[:20, 10:20].sum(), integrate(s, (0, 10), (19, 19)))
     assert_equal(x[10:20, :20].sum(), integrate(s, (10, 0), (19, 19)))
 
 
-def test_single():
+def test_integrate_single():
     assert_equal(x[0, 0], integrate(s, (0, 0), (0, 0)))
     assert_equal(x[10, 10], integrate(s, (10, 10), (10, 10)))
 
