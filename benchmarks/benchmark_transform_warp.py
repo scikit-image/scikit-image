863d78500e9a5c8c3f2ba855720cba37ebd8fa6b
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-06-21 16:05:36 +0200

    satisfy pep8

--- benchmarks/benchmark_transform_warp.py
+++ benchmarks/benchmark_transform_warp.py
@@ -67,9 +67,11 @@ class ResizeLocalMeanSuite:
                    'ndim_out', 'grid_mode']
 
     def setup(self, dtype, shape_in, shape_out, ndim_in, ndim_out, grid_mode):
-        is_slow = any((dtype == np.float64, shape_in == 2048, shape_out == 1024, ndim_in == 3, ndim_out == 3))
+        is_slow = any((dtype == np.float64, shape_in == 2048, shape_out == 1024,
+                       ndim_in == 3, ndim_out == 3))
         if os.environ.get("SKIP_SLOW") == "1" and is_slow:
-            raise NotImplementedError("Skipping in CI because of memory usage and time constraints")
+            m = "Skipping in CI because of memory usage and time constraints"
+            raise NotImplementedError(m)
 
         self.shape_in = ndim_in * (shape_in, )
         self.image = np.zeros(self.shape_in, dtype=dtype)
