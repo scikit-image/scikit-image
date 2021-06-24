765768c47ee54c1b8fe5603e646e8ee571d9658c
ArpithaRaviSeetala <arpitharavi3@gmail.com>
2021-06-24 18:47:23 +0530

    Bounding Box crop module issue 5428 Bounding Box crop module issue 5428 A newfile module - crop_bounding_box.py added. This file contains two methods. One, to locate the users downloads folder and create the Cropped folder if necessary. Two, a method to crop parts of the image in the bounding box. All the cropped images are then saved in the folder created in the download_path() method.

---
+++ skimage/util/crop_bounding_box.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""Bounding boxes cropping module for any image."""
+
+import cv2
+from pathlib import Path
+import os
+
+
+def download_path():
+    """Download folder path along with folder creation if needed.
+
+    Returns
+    -------
+    downloads_path : str
+        Path to the folder in the Downloads
+
+    """
+    downloads_path = str(Path.home() / "Downloads"/"Cropped")
+    if not os.path.exists(downloads_path):
+        os.makedirs(downloads_path)
+    return downloads_path
+
+
+def bounding_box_crop(image_url):
+    """Bounding boxes are found and cropped.
+
+    Bounding boxes saved to "Cropped" folder retrieved from download_path()
+    function.
+
+    Parameters
+    ----------
+    image_url : str
+        URL of the image.
+
+    Returns
+    -------
+    None. "Cropped" Folder in Downloads to be looked for by user.
+
+    """
+    image_name = os.path.basename(image_url).split('.')[0]
+    orig = cv2.imread(image_url)
+    r = 1000.0/orig.shape[1]
+    dim = (1000, int(orig.shape[0] * r))
+    o_resized = cv2.resize(orig, dim, interpolation=cv2.INTER_LINEAR)
+
+    img_number = 0
+
+    img = cv2.imread(image_url, 0)
+    r = 1000.0/img.shape[1]
+    dim = (1000, int(img.shape[0] * r))
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_LINEAR)
+
+    ret, thresh = cv2.threshold(resized, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, 1, 2)
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        if w*h < 5000:
+            continue
+        img = cv2.rectangle(resized, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        crp = o_resized[y:y+h, x:x+w]
+        cv2.imwrite(download_path() + '//' + image_name + '{}.png'.format(
+            img_number), crp)
+        img_number += 1
