e8f54a271140c70f54748878d16091ee5d920d41
ArpithaRaviSeetala <arpitharavi3@gmail.com>
2021-06-25 22:37:41 +0530

    Corrected the PEP8 issues raised.

--- skimage/util/bounding_box_crop.py
+++ skimage/util/bounding_box_crop.py
@@ -20,7 +20,7 @@ def download_path():
         Path to the folder in the Downloads
 
     """
-    downloads_path = str(Path.home() / "Downloads"/"Cropped")
+    downloads_path = str(Path.home() / "Downloads" / "Cropped")
     if not os.path.exists(downloads_path):
         os.makedirs(downloads_path)
     return downloads_path
@@ -43,14 +43,14 @@ def bounding_box_crop(image_url):
     """
     image_name = os.path.basename(image_url).split('.')[0]
     org = cv2.imread(image_url)
-    r = 1000.0/org.shape[1]
+    r = 1000.0 / org.shape[1]
     dim = (1000, int(org.shape[0] * r))
     o_resized = cv2.resize(org, dim, interpolation=cv2.INTER_LINEAR)
 
     img_number = 0
 
     img = cv2.imread(image_url, 0)
-    r = 1000.0/img.shape[1]
+    r = 1000.0 / img.shape[1]
     dim = (1000, int(img.shape[0] * r))
     resized = cv2.resize(img, dim, interpolation=cv2.INTER_LINEAR)
 
@@ -58,10 +58,10 @@ def bounding_box_crop(image_url):
     contours, hierarchy = cv2.findContours(thresh, 1, 2)
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
-        if w*h < 5000:
+        if w * h < 5000:
             continue
-        img = cv2.rectangle(resized, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        crp = o_resized[y:y+h, x:x+w]
+        img = cv2.rectangle(resized, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        crp = o_resized[y:y + h, x:x + w]
         cv2.imwrite(download_path() + '//' + image_name + '{}.png'.format(
             img_number), crp)
         img_number += 1
