c2b9f10342f465f4cecd0778d22f0cc1175c3506
Gregory Lee <grlee77@gmail.com>
2021-06-26 09:49:16 -0400

    keep functions available from selem.py for backwards compatibility

---
+++ skimage/morphology/selem.py
@@ -0,0 +1,12 @@
+import warnings
+
+from .footprint import (
+    square, rectangle, diamond, disk, cube, octahedron, ball, octagon, star
+)
+
+warnings.warn(
+    "The `skimage.morphology.selem` module is deprecated and will be removed "
+    "in scikit-image 1.0. The functions formerly in `skimage.morphology.selem` "
+    "should be imported directly from `skimage.morphology` instead.",
+    FutureWarning,
+)
