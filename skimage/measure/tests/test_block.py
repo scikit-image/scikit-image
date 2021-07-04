53525854da89400d2ae57475584ac78dec04f83f
Risako <lilisako062397@gmail.com>
2021-07-04 13:36:23 +0900

    fix test with simpler case

--- skimage/measure/tests/test_block.py
+++ skimage/measure/tests/test_block.py
@@ -89,9 +89,9 @@ def test_invalid_block_size():
 
 def test_default_block_size():
     image = np.arange(4 * 6).reshape(4, 6)
-    out = block_reduce(image, func=np.mean)
-    expected = np.array([[ 3.5, 5.5, 7.5],
-                          [15.5, 17.5, 19.5]])
+    out = block_reduce(image, func=np.min)
+    expected = np.array([[ 0, 2, 4],
+                          [12, 14, 16]])
     assert_equal(expected, out)
 
 
