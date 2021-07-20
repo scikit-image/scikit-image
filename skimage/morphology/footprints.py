f83dd280adb28bfd5e2f042c0ad1a186e611ad28
Gregory Lee <grlee77@gmail.com>
2021-07-20 13:48:57 -0400

    relax odd size checks
    This check needs to be disable for some cases such as separable decomposition
    of squares with even size along an axis.

--- skimage/morphology/footprints.py
+++ skimage/morphology/footprints.py
@@ -32,25 +32,29 @@ def _footprint_is_sequence(footprint):
     return True
 
 
-def _shape_from_sequence(footprints):
-    """Determine the shape of composite footprint"""
+def _shape_from_sequence(footprints, require_odd_size=False):
+    """Determine the shape of composite footprint
+
+    In the future if we only want to support odd-sized square, we may want to
+    change this to require_odd_size
+    """
     if not _footprint_is_sequence(footprints):
         raise ValueError("expected a sequence of footprints")
     ndim = footprints[0][0].ndim
     shape = [0,] * ndim
 
-    def _odd_size(size):
-        if size % 2 == 0:
+    def _odd_size(size, require_odd_size):
+        if require_odd_size and size % 2 == 0:
             raise ValueError(
                 "expected all footprint elements to have odd size"
             )
 
     for d in range(ndim):
         fp, nreps = footprints[0]
-        _odd_size(fp.shape[d])
+        _odd_size(fp.shape[d], require_odd_size)
         shape[d] = fp.shape[d] + (nreps - 1) * (fp.shape[d] - 1)
         for fp, nreps in footprints[1:]:
-            _odd_size(fp.shape[d])
+            _odd_size(fp.shape[d], require_odd_size)
             shape[d] += nreps * (fp.shape[d] - 1)
     return tuple(shape)
 
