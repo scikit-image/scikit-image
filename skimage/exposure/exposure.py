1c36db7f889edbda2eccc7cd48f1c47b8829fa5d
rfezzani <rfezzani@gmail.com>
2021-08-10 10:28:30 +0200

    Proper Fix

--- skimage/exposure/exposure.py
+++ skimage/exposure/exposure.py
@@ -441,7 +441,7 @@ def _adjust_gamma_u8(image, gamma, gain):
 
     """
     lut = (255 * gain * (np.linspace(0, 1, 256) ** gamma))
-    np.maximum(lut, 255, out=lut).astype('uint8')
+    np.minimum(lut, 255, out=lut).astype('uint8')
     return lut[image]
 
 
