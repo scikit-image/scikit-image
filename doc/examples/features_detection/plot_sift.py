303057f86d178a91b894b0c43c8bd3d15c2167c7
Gregory Lee <grlee77@gmail.com>
2021-08-12 11:44:17 -0400

    Update example
    previous setting of trunacating keypoints can result in zero matches because matches need to
    be across all keypoints.
    Here we instead add a second column of figures where only a subset of the matches are displayed
    for clearer visibility

--- doc/examples/features_detection/plot_sift.py
+++ doc/examples/features_detection/plot_sift.py
@@ -23,31 +23,46 @@ img3 = transform.warp(img1, tform)
 descriptor_extractor = SIFT()
 
 descriptor_extractor.detect_and_extract(img1)
-keypoints1 = descriptor_extractor.keypoints[:100]
-descriptors1 = descriptor_extractor.descriptors[:100]
+keypoints1 = descriptor_extractor.keypoints
+descriptors1 = descriptor_extractor.descriptors
 
 descriptor_extractor.detect_and_extract(img2)
-keypoints2 = descriptor_extractor.keypoints[:100]
-descriptors2 = descriptor_extractor.descriptors[:100]
+keypoints2 = descriptor_extractor.keypoints
+descriptors2 = descriptor_extractor.descriptors
 
 descriptor_extractor.detect_and_extract(img3)
-keypoints3 = descriptor_extractor.keypoints[:100]
-descriptors3 = descriptor_extractor.descriptors[:100]
+keypoints3 = descriptor_extractor.keypoints
+descriptors3 = descriptor_extractor.descriptors
 
 matches12 = match_descriptors(descriptors1, descriptors2, max_ratio=0.6,
                               cross_check=True)
 matches13 = match_descriptors(descriptors1, descriptors3, max_ratio=0.6,
                               cross_check=True)
-fig, ax = plt.subplots(nrows=2, ncols=1)
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(11, 8))
 
 plt.gray()
 
-plot_matches(ax[0], img1, img2, keypoints1, keypoints2, matches12)
-ax[0].axis('off')
-ax[0].set_title("Original Image vs. Transformed Image")
+plot_matches(ax[0, 0], img1, img2, keypoints1, keypoints2, matches12)
+ax[0, 0].axis('off')
+ax[0, 0].set_title("Original Image vs. Flipped Image\n"
+                   "(all keypoints and matches)")
 
-plot_matches(ax[1], img1, img3, keypoints1, keypoints3, matches13)
-ax[1].axis('off')
-ax[1].set_title("Original Image vs. Transformed Image")
+plot_matches(ax[1, 0], img1, img3, keypoints1, keypoints3, matches13)
+ax[1, 0].axis('off')
+ax[1, 0].set_title("Original Image vs. Transformed Image\n"
+                   "(all keypoints and matches)")
 
+plot_matches(ax[0, 1], img1, img2, keypoints1, keypoints2, matches12[::15],
+             only_matches=True)
+ax[0, 1].axis('off')
+ax[0, 1].set_title("Original Image vs. Flipped Image\n"
+                   "(subset of matches for visibility)")
+
+plot_matches(ax[1, 1], img1, img3, keypoints1, keypoints3, matches13[::15],
+             only_matches=True)
+ax[1, 1].axis('off')
+ax[1, 1].set_title("Original Image vs. Transformed Image\n"
+                   "(subset of matches for visibility)")
+
+plt.tight_layout()
 plt.show()
