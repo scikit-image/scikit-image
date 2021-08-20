edb061521978f77074bbc5737cde6c5238afcb03
lucArub <86793289+lucArub@users.noreply.github.com>
2021-08-20 19:25:04 +0200

    Update plot_niblack_sauvola.py

--- doc/examples/segmentation/plot_niblack_sauvola.py
+++ doc/examples/segmentation/plot_niblack_sauvola.py
@@ -51,27 +51,27 @@ binary_sauvola = image > thresh_sauvola
 binary_singh = image > thresh_singh
 
 plt.figure(figsize=(8, 7))
-plt.subplot(2, 2, 1)
+plt.subplot(3, 3, 1)
 plt.imshow(image, cmap=plt.cm.gray)
 plt.title('Original')
 plt.axis('off')
 
-plt.subplot(2, 2, 2)
+plt.subplot(3, 3, 2)
 plt.title('Global Threshold')
 plt.imshow(binary_global, cmap=plt.cm.gray)
 plt.axis('off')
 
-plt.subplot(2, 2, 3)
+plt.subplot(3, 3, 3)
 plt.imshow(binary_niblack, cmap=plt.cm.gray)
 plt.title('Niblack Threshold')
 plt.axis('off')
 
-plt.subplot(2, 2, 4)
+plt.subplot(3, 3, 4)
 plt.imshow(binary_sauvola, cmap=plt.cm.gray)
 plt.title('Sauvola Threshold')
 plt.axis('off')
 
-plt.subplot(2, 2, 5)
+plt.subplot(3, 3, 5)
 plt.imshow(binary_singh, cmap=plt.cm.gray)
 plt.title('Singh Threshold')
 plt.axis('off')
