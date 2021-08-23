3bfd4ca1439e04e3108fe34d7d25835058713ae0
David Manthey <david.manthey@kitware.com>
2021-08-23 08:47:40 -0400

    Handle pooch version 1.5.0
    This is compatible with pooch < 1.5.0, too.  If such older compatibility
    is not desired, the dependency version could be changed and just the new
    code could be kept.

--- skimage/data/__init__.py
+++ skimage/data/__init__.py
@@ -62,7 +62,12 @@ legacy_data_dir = osp.abspath(osp.dirname(__file__))
 skimage_distribution_dir = osp.join(legacy_data_dir, '..')
 
 try:
-    from pooch.utils import file_hash
+    try:
+        # Pooch < 1.5.0
+        from pooch.utils import file_hash
+    except ImportError:
+        # Pooch >= 1.5.0
+        from pooch.hashes import file_hash
 except ModuleNotFoundError:
     # Function taken from
     # https://github.com/fatiando/pooch/blob/master/pooch/utils.py
