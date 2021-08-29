89467f207f92c4151b546b38a4850d825e15083c
SANDEEP <75900310+Sandeep-Kumar-Behura@users.noreply.github.com>
2021-08-30 01:22:11 +0530

    Circle_test.py
    increase accuracy of Circle detection

--- skimage/graph/tests/test_flexible.py
+++ skimage/graph/tests/test_flexible.py
@@ -1,11 +1,39 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import fft
 import skimage.graph.mcp as mcp
+from skimage import data, img_as_float
 
 from skimage._shared.testing import assert_array_equal
 
-
 a = np.ones((8, 8), dtype=np.float32)
 a[1::2] *= 2.0
+
+src = img_as_float(data.camera()[128:256, 128:256])
+src = src + 0.05 * np.random.standard_normal(src.shape)
+target = np.roll(src, (15, -10), axis=(0, 1))
+target = target + 0.05 * np.random.standard_normal(target.shape)
+src_freq = fft.fftn(src)
+target_freq = fft.fftn(target)
+
+# current implementation
+image_product1 = src_freq * target_freq.conj()
+cross_correlation1 = fft.ifftn(image_product1)
+
+# fixed implementation
+image_product = image_product1 / np.abs(image_product1)
+cross_correlation = fft.ifftn(image_product)
+
+fig, axes = plt.subplots(1, 2)
+axes[0].imshow(np.abs(cross_correlation1), cmap=plt.cm.gray)
+axes[0].set_title('Existing Implementation')
+axes[1].imshow(np.abs(cross_correlation), cmap=plt.cm.gray)
+axes[1].set_title('Proposed Implementation')
+for ax in axes:
+    ax.set_axis_off()
+plt.tight_layout()
+plt.show()
 
 
 class FlexibleMCP(mcp.MCP_Flexible):
