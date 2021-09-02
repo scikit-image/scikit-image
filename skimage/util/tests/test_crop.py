ed328f3be3cc1824dfcb3870d3eef9e404dcf394
Gregory Lee <grlee77@gmail.com>
2021-09-01 22:17:58 -0400

    TST: use new-style random generator in test_cropy.py

--- skimage/util/tests/test_crop.py
+++ skimage/util/tests/test_crop.py
@@ -1,17 +1,20 @@
 import numpy as np
 import pytest
+
 from skimage import data
 from skimage.util.crop import bounding_box_crop
 
+rng = np.random.default_rng()
+
 
 def test_2d_crop_1():
-    data = np.random.random((50, 50))
+    data = rng.random((50, 50))
     out_data = bounding_box_crop(data, [(0, 25)])
     np.testing.assert_array_equal(out_data, data[:25, :])
 
 
 def test_2d_crop_2():
-    data = np.random.random((50, 50))
+    data = rng.random((50, 50))
     out_data = bounding_box_crop(data, [(0, 25)], axes=[1])
     np.testing.assert_array_equal(out_data, data[:, :25])
 
@@ -26,24 +29,25 @@ def test_copy():
 
 
 def test_2d_crop_3():
-    data = np.random.random((50, 50))
+    data = rng.random((50, 50))
     out_data = bounding_box_crop(data, [(0, 25), (0, 30)], axes=[1, 0])
     np.testing.assert_array_equal(out_data, data[:30, :25])
 
 
 def test_nd_crop():
-    data = np.random.random((50, 50, 50))
+    data = rng.random((50, 50, 50))
     out_data = bounding_box_crop(data, [(0, 25)])
     np.testing.assert_array_equal(out_data, data[:25, :, :])
 
 
 def test_axes_invalid():
-    data = np.random.random((2, 3))
+    data = np.empty((2, 3))
     with pytest.raises(ValueError):
         bounding_box_crop(data, [(0, 3)], axes=[2])
 
+
 def test_axes_limit_invalid():
-    data = np.random.random((50, 50))
+    data = np.empty((50, 50))
     with pytest.raises(ValueError):
         bounding_box_crop(data, [(0, 51)], axes=[0])
     
