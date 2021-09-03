c8a921db776513d72c640b8f6fafbf9d2d31d487
Gregory R. Lee <grlee77@gmail.com>
2021-09-02 23:07:21 -0400

    single precision support in skimage.filters (#5354)
    * float32 support in edges, gabor, gaussian, lpi_filter
    
    * replace unittest yield tests with pytest parametrization (for test_ip_shape)
    
    * float32 support in ridges, unsharp_mask, correlate_sparse
    
    * TST: style: reduce use of skimage._shared.testing
    
    * FIX: image -> image.dtype as input to _supported_float_type
    
    corrects _supported_float_type calls from previously merged PRs
    
    * omit unnecessary intermediate copy in integral_image
    
    * single-precision support for threshold_local, threshold_sauvola, threshold_niblack
    
    These are the threshold types that return an array
    
    * TST: test additional dtypes for local thresholding methods
    
    * lpi_filter: remove global eps variable and use eps for the proper float precision
    
    * ENH: add float32 support to butterworth
    
    * STYLE: apply isort to modified files
    
    * BUG: fix butterworth dtype behavior with minimum supported SciPy
    
    With SciPy < 1.4, we fallback to numpy.fft for the FFTs which causes undesired dtype promotion

--- skimage/filters/tests/test_ridges.py
+++ skimage/filters/tests/test_ridges.py
@@ -1,11 +1,14 @@
-import pytest
 import numpy as np
+import pytest
 from numpy.testing import assert_allclose, assert_array_less, assert_equal
-from skimage.filters import meijering, sato, frangi, hessian
-from skimage.data import camera, retina
-from skimage.util import crop, invert
-from skimage.color import rgb2gray
+
+from skimage import img_as_float
 from skimage._shared._warnings import expected_warnings
+from skimage._shared.utils import _supported_float_type
+from skimage.color import rgb2gray
+from skimage.data import camera, retina
+from skimage.filters import frangi, hessian, meijering, sato
+from skimage.util import crop, invert
 
 
 def test_2d_null_matrix():
@@ -187,6 +190,13 @@ def test_2d_cropped_camera_image():
                     ones, atol=1 - 1e-7)
 
 
+@pytest.mark.parametrize('func', [meijering, sato, frangi, hessian])
+@pytest.mark.parametrize('dtype', [np.float16, np.float32, np.float64])
+def test_ridge_output_dtype(func, dtype):
+    img = img_as_float(camera()).astype(dtype, copy=False)
+    assert func(img).dtype == _supported_float_type(img.dtype)
+
+
 def test_3d_cropped_camera_image():
 
     a_black = crop(camera(), ((200, 212), (100, 312)))
@@ -238,4 +248,4 @@ def test_border_management(func, tol):
 
 if __name__ == "__main__":
     from numpy import testing
-    testing.run_module_suite()
+    np.testing.run_module_suite()
