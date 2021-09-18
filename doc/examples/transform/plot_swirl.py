fc508b748bba308b514759598d66d904d8680ea7
Juan Antonio Barragan <barragan@purdue.edu>
2021-09-18 01:33:52 -0400

    Fixed math formulas in plot_swirl.py example.

--- doc/examples/transform/plot_swirl.py
+++ doc/examples/transform/plot_swirl.py
@@ -42,7 +42,7 @@ mapping for the swirl transformation first computes, relative to a center
 
 .. math::
 
-    \\theta = \\arctan(y/x)
+    \\theta = \\arctan((y-y0)/(x-x0))
 
     \\rho = \sqrt{(x - x_0)^2 + (y - y_0)^2},
 
@@ -56,7 +56,7 @@ and then transforms them according to
 
     s = \mathtt{strength}
 
-    \\theta' = \phi + s \, e^{-\\rho / r + \\theta}
+    \\theta' = \phi + s \, e^{-\\rho / r} + \\theta
 
 where ``strength`` is a parameter for the amount of swirl, ``radius`` indicates
 the swirl extent in pixels, and ``rotation`` adds a rotation angle.  The
