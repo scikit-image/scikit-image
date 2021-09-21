ce5322a5d7e5b9053dc3ea4c34de45b29badbb1f
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2021-09-21 19:33:37 +0200

    Finish tutorial

--- doc/examples/applications/plot_fluorescence_nuclear_envelope.py
+++ doc/examples/applications/plot_fluorescence_nuclear_envelope.py
@@ -3,14 +3,28 @@
 Measure fluorescence intensity at the nuclear envelope
 ======================================================
 
-online tutorial [1]_ based on publication [2]_
+This example reproduces a well-established workflow in bioimage data analysis
+for measuring the fluorescence intensity localized to the nuclear envelope, in
+a time sequence of cell images (each with two channels and two spatial
+dimensions) which shows a process of protein re-localization from the
+cytoplasmic area to the nuclear envelope. This biological application was
+first presented by Andrea Boni and collaborators in [1]_; it was used in a
+textbook by Kota Miura [2]_ as well as in other works ([3]_, [4]_).
+In other words, we port this workflow from ImageJ Macro to scikit-image.
 
-.. [1] https://doi.org/10.1007/978-3-030-22386-1_2
-.. [2] Boni A, Politi AZ, Strnad P, Xiang W, Hossain MJ, Ellenberg J (2015)
+.. [1] Boni A, Politi AZ, Strnad P, Xiang W, Hossain MJ, Ellenberg J (2015)
        "Live imaging and modeling of inner nuclear membrane targeting reveals
        its molecular requirements in mammalian cells" J Cell Biol
        209(5):705–720. ISSN: 0021-9525.
        :DOI:`10.1083/jcb.201409133`
+.. [2] Miura K (2020) "Measurements of Intensity Dynamics at the Periphery of
+       the Nucleus" in: Miura K, Sladoje N (eds) Bioimage Data Analysis
+       Workflows. Learning Materials in Biosciences. Springer, Cham.
+       :DOI:`10.1007/978-3-030-22386-1_2`
+.. [3] Klemm A (2020) "ImageJ/Fiji Macro Language" NEUBIAS Academy Online
+       Course: https://www.youtube.com/watch?v=o8tfkdcd3DA
+.. [4] Vorkel D and Haase R (2020) "GPU-accelerating ImageJ Macro image
+       processing workflows using CLIJ" https://arxiv.org/abs/2008.11799
 
 """
 
@@ -124,8 +138,32 @@ ax[1].set_title('Selection')
 ax[1].axis('off')
 
 #####################################################################
+# Measure the total intensity
+# ===========================
+# The mean intensity is readily available as a region property in a labeled
+# image.
+
+props = measure.regionprops_table(
+    mask,
+    intensity_image=ch1t0,
+    properties=('label', 'area', 'intensity_mean')
+)
+
+#####################################################################
+# We may want to check that the value for the total intensity
+
+selection.sum()
+
+#####################################################################
+# can be recovered from:
+
+props['area'] * props['intensity_mean']
+
+#####################################################################
 # Iterate the measurement for each time point
 # ===========================================
+# Let us write a function for the first processing step (segmentation of
+# nucleus rim).
 
 
 def get_mask(im, sigma=2, thresh=0.1, thickness=4):
@@ -142,5 +180,35 @@ def get_mask(im, sigma=2, thresh=0.1, thickness=4):
     mask = expand - erode
     return mask
 
+
+#####################################################################
+# Let us now loop through all the image frames.
+
+fluorescence_change = []
+
+for i in range(dat.shape[0]):
+    mask = get_mask(dat[i, 0, :, :])
+    props = measure.regionprops_table(
+        mask,
+        intensity_image=dat[i, 1, :, :],
+        properties=('label', 'area', 'intensity_mean')
+    )
+    assert props['label'] == 1
+    intensity_total = props['area'] * props['intensity_mean']
+    fluorescence_change.append(intensity_total)
+
+fluorescence_change /= fluorescence_change[0]  # normalization
+
+_, ax = plt.subplots()
+ax.plot(fluorescence_change, 'rs')
+ax.grid()
+ax.set_xlabel('Time point')
+ax.set_ylabel('Normalized total intensity')
+ax.set_title('Change in fluorescence intensity at the nuclear envelope')
+
+#####################################################################
+# Reassuringly, we find the expected result: The total fluorescence
+# intensity at the nuclear envelope increases 1.3-fold in the initial five
+# time points, and then becomes roughly constant.
 
 plt.show()
