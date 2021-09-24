68bf0107bfc8822befb108885485b2761caa07e8
Riadh <r.fezzani@vitadx.com>
2021-09-24 10:08:28 +0200

    Add unit tests with image samples provided by @robbibt

--- skimage/measure/tests/test_find_contours.py
+++ skimage/measure/tests/test_find_contours.py
@@ -1,9 +1,8 @@
 import numpy as np
 from skimage.measure import find_contours
 
-from skimage._shared import testing
 from skimage._shared.testing import assert_array_equal
-from pytest import raises
+import pytest
 
 
 a = np.ones((8, 8), dtype=np.float32)
@@ -96,13 +95,13 @@ def test_mask():
 
 def test_mask_shape():
     bad_mask = np.ones((8, 7), dtype=bool)
-    with raises(ValueError, match='shape'):
+    with pytest.raises(ValueError, match='shape'):
         find_contours(a, 0, mask=bad_mask)
 
 
 def test_mask_dtype():
     bad_mask = np.ones((8, 8), dtype=np.uint8)
-    with raises(TypeError, match='binary'):
+    with pytest.raises(TypeError, match='binary'):
         find_contours(a, 0, mask=bad_mask)
 
 
@@ -125,9 +124,9 @@ def test_memory_order():
 
 
 def test_invalid_input():
-    with testing.raises(ValueError):
+    with pytest.raises(ValueError):
         find_contours(r, 0.5, 'foo', 'bar')
-    with testing.raises(ValueError):
+    with pytest.raises(ValueError):
         find_contours(r[..., None], 0.5)
 
 
@@ -150,13 +149,13 @@ def test_mask_levelNone():
 
 def test_mask_shape_levelNone():
     bad_mask = np.ones((8, 7), dtype=bool)
-    with raises(ValueError, match='shape'):
+    with pytest.raises(ValueError, match='shape'):
         find_contours(a, level=None, mask=bad_mask)
 
 
 def test_mask_dtype_levelNone():
     bad_mask = np.ones((8, 8), dtype=np.uint8)
-    with raises(TypeError, match='binary'):
+    with pytest.raises(TypeError, match='binary'):
         find_contours(a, level=None, mask=bad_mask)
 
 
@@ -174,3 +173,35 @@ def test_level_default():
     contours = find_contours(image)  # use default level
     # many contours should be found
     assert len(contours) > 1
+
+
+@pytest.mark.parametrize("image", [
+    [[0.13680, 0.11220, 0.0, 0.0, 0.0, 0.19417, 0.19417, 0.33701],
+     [0.0, 0.15140, 0.10267, 0.0, np.nan, 0.14908, 0.18158, 0.19178],
+     [0.0, 0.06949, 0.0, 0.0, 0.0, 0.0, 0.0, 0.01860],
+     [0.0, 0.06949, 0.0, 0.17852, 0.08469, 0.02135, 0.08198, np.nan],
+     [0.0, 0.08244, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+     [0.12342, 0.21330, 0.0, np.nan, 0.01301, 0.04335, 0.0, 0.0]],
+
+    [[0.08, -0.03, -0.17, -0.08, 0.24, 0.06, 0.17, -0.02],
+     [0.12, 0., np.nan, 0.24, 0., -0.53, 0.26, 0.16],
+     [0.39, 0., 0.,  0., 0., -0.02, -0.3, 0.01],
+     [0.28, -0.04, -0.03,  0.16, 0.12, 0.01, -0.87, 0.16],
+     [0.26, 0.08, 0.08,  0.08, 0.12, 0.13, 0.11, 0.19],
+     [0.27, 0.24, 0.,  0.25, 0.32, 0.19, 0.26, 0.22]],
+
+    [[-0.18, np.nan, np.nan, 0.22, -0.14, -0.23, -0.2, -0.17, -0.19, -0.24],
+     [0., np.nan, np.nan, np.nan, -0.1, -0.24, -0.15, -0.02, -0.09, -0.21],
+     [0.43, 0.19, np.nan, np.nan, -0.01, -0.2, -0.22, -0.18, -0.16, -0.07],
+     [0.23, 0., np.nan, -0.06, -0.07, -0.21, -0.24, -0.25, -0.23, -0.13],
+     [-0.05, -0.11, 0., 0.1, -0.19, -0.23, -0.23, -0.18, -0.19,  -0.16],
+     [-0.19, -0.05, 0.13, -0.08, -0.22, -0.23, -0.26, -0.15, -0.12, -0.13],
+     [-0.2, -0.11, -0.11, -0.24, -0.29, -0.27, -0.35, -0.36, -0.27, -0.13],
+     [-0.28, -0.33, -0.31, -0.36, -0.39, -0.37, -0.38, -0.32, -0.34, -0.2],
+     [-0.28, -0.33, -0.39, -0.4, -0.42, -0.38, -0.35, -0.39, -0.35, -0.34],
+     [-0.38, -0.35, -0.41, -0.42, -0.39, -0.36, -0.34, -0.36, -0.28, -0.34]]
+])
+def test_keyerror_fix(image):
+    """Failing samples from issue #4830
+    """
+    find_contours(np.array(image, np.float32), 0)
