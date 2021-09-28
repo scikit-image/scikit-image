f2709e7121cdf8c62a1adafdf2696d04c8340aeb
Gregory Lee <grlee77@gmail.com>
2021-09-27 21:55:49 -0400

    ENH: remove need for channel_as_last_axis decorator for gaussian
    TST: expand difference_of_gaussian tests to include channel_axis

--- skimage/filters/_gaussian.py
+++ skimage/filters/_gaussian.py
@@ -11,7 +11,6 @@ from ..util import img_as_float
 __all__ = ['gaussian', 'difference_of_gaussians']
 
 
-@utils.channel_as_last_axis()
 @utils.deprecate_multichannel_kwarg(multichannel_position=5)
 def gaussian(image, sigma=1, output=None, mode='nearest', cval=0,
              multichannel=None, preserve_range=False, truncate=4.0, *,
@@ -124,8 +123,9 @@ def gaussian(image, sigma=1, output=None, mode='nearest', cval=0,
         # do not filter across channels
         if not isinstance(sigma, Iterable):
             sigma = [sigma] * (image.ndim - 1)
-        if len(sigma) != image.ndim:
-            sigma = np.concatenate((np.asarray(sigma), [0]))
+        if len(sigma) == image.ndim - 1:
+            sigma = list(sigma)
+            sigma.insert(channel_axis % image.ndim, 0)
     image = convert_to_float(image, preserve_range)
     float_dtype = _supported_float_type(image.dtype)
     image = image.astype(float_dtype, copy=False)
@@ -170,7 +170,6 @@ def _guess_spatial_dimensions(image):
         )
 
 
-@utils.channel_as_last_axis()
 @utils.deprecate_multichannel_kwarg()
 def difference_of_gaussians(image, low_sigma, high_sigma=None, *,
                             mode='nearest', cval=0, channel_axis=None,
