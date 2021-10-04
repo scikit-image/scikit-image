ecabaebf1139a9049a9795052a93f5dd3862afac
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-10-03 22:25:44 -0500

    Add mapping, docstring, and continuous image support

--- skimage/graph/_graph.py
+++ skimage/graph/_graph.py
@@ -6,8 +6,38 @@ from ..morphology._util import _offsets_to_raveled_neighbors
 from ..util._map_array import map_array
 
 
-def pixel_graph(mask):
-    mask = mask.astype(bool)
+def pixel_graph(image=None, *, mask=None, edge_function=None):
+    """Create an adjacency graph of pixels in an image.
+
+    Parameters
+    ----------
+    image : array
+        The input image. If the image is of type bool, it will be used as the
+        mask as well.
+    mask : array of bool
+        Which pixels to use. If None, the graph for the whole image is used.
+        If image is None and mask is not None, the mask is used as the image.
+    edge_function : callable
+        A function taking an array of pixel values, and an array of neighbor
+        pixel values, and returning a value for the edge.
+
+    Returns
+    -------
+    graph : scipy.sparse.csr_matrix
+        A sparse adjacency matrix in which entry (i, j) is 1 if nodes i and j
+        are neighbors, 0 otherwise.
+    nodes : array of int
+        The nodes of the graph. These correspond to the raveled indices of the
+        nonzero pixels in the mask.
+    """
+    if mask is not None and image is None:
+        image = mask
+    if image.dtype == bool and mask is None:
+        mask = image
+    if mask is None:
+        mask = np.ones_like(image, dtype=bool)
+        edge_function = np.subtract
+
     footprint = ndi.generate_binary_structure(mask.ndim, 1)
     padded = np.pad(mask, 1, mode='constant', constant_values=False)
     nodes_padded = np.arange(padded.size).reshape(padded.shape)[padded]
@@ -22,12 +52,17 @@ def pixel_graph(mask):
     neighbors = map_array(neighbors_padded, nodes_padded, nodes)
     neighbors_mask = mask.ravel()[neighbors]
     num_neighbors = np.sum(neighbors_mask, axis=1)
+    indices = np.repeat(nodes, num_neighbors)
     indices_sequential = np.repeat(nodes_sequential, num_neighbors)
     neighbor_indices = neighbors[neighbors_mask]
     neighbor_indices_sequential = map_array(
             neighbor_indices, nodes, nodes_sequential
             )
-    data = np.broadcast_to(1, indices_sequential.shape)
+    if edge_function is None:
+        data = np.broadcast_to(1, indices_sequential.shape)
+    else:
+        image_r = image.ravel()
+        data = edge_function(image_r[indices], image_r[neighbor_indices])
     m = nodes_sequential.size
     mat = sparse.coo_matrix(
             (data, (indices_sequential, neighbor_indices_sequential)),
