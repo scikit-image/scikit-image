5731910b82d64e0941315c816281142e60cddd54
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-10-03 19:17:56 -0500

    Remap to nodes [0..m] for efficiency

--- skimage/graph/_graph.py
+++ skimage/graph/_graph.py
@@ -3,7 +3,7 @@ from scipy import ndimage as ndi
 from scipy import sparse
 from scipy.sparse import csgraph
 from ..morphology._util import _offsets_to_raveled_neighbors
-from ..util import map_array
+from ..util._map_array import map_array
 
 
 def pixel_graph(mask):
@@ -16,24 +16,33 @@ def pixel_graph(mask):
             )
     neighbors_padded = nodes_padded[:, np.newaxis] + neighbor_offsets_padded
     nodes = np.arange(mask.size).reshape(mask.shape)[mask]
+    nodes_sequential = np.arange(nodes.size)
     # neighbors outside the mask get mapped to 0, which is a valid index,
     # BUT, they will be masked out in the next step.
     neighbors = map_array(neighbors_padded, nodes_padded, nodes)
     neighbors_mask = mask.ravel()[neighbors]
     num_neighbors = np.sum(neighbors_mask, axis=1)
-    indices = np.repeat(nodes, num_neighbors)
+    indices_sequential = np.repeat(nodes_sequential, num_neighbors)
     neighbor_indices = neighbors[neighbors_mask]
-    data = np.broadcast_to(1, indices.shape)
-    m = mask.size
-    mat = sparse.coo_matrix((data, (indices, neighbor_indices)), shape=(m, m))
+    neighbor_indices_sequential = map_array(
+            neighbor_indices, nodes, nodes_sequential
+            )
+    data = np.broadcast_to(1, indices_sequential.shape)
+    m = nodes_sequential.size
+    mat = sparse.coo_matrix(
+            (data, (indices_sequential, neighbor_indices_sequential)),
+            shape=(m, m)
+            )
     graph = mat.tocsr()
-    return graph
+    return graph, nodes
 
 
-def central_pixel(graph):
+def central_pixel(graph, nodes=None):
+    if nodes is None:
+        nodes = np.arange(graph.shape[0])
     all_shortest_paths = csgraph.shortest_path(graph, directed=False)
     all_shortest_paths_no_inf = np.nan_to_num(all_shortest_paths, posinf=0)
     total_shortest_path_len = np.sum(all_shortest_paths_no_inf, axis=1)
     nonzero = np.flatnonzero(total_shortest_path_len)
     min_sp = np.argmin(total_shortest_path_len[nonzero])
-    return nonzero[min_sp], total_shortest_path_len
+    return nodes[nonzero[min_sp]], total_shortest_path_len
