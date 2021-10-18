fa10fb8acb0b712d204a138c38edc674528f5155
Gregory Lee <grlee77@gmail.com>
2021-10-18 16:37:33 -0400

    move PytestTester out of testing.py to avoid runtime requirement on pytest
    add git date and hash to __version__ in dev builds

--- skimage/_shared/testing.py
+++ skimage/_shared/testing.py
@@ -5,7 +5,6 @@ Testing utilities.
 import os
 import re
 import struct
-import sys
 import threading
 import functools
 from tempfile import NamedTemporaryFile
@@ -22,12 +21,12 @@ import warnings
 
 from .. import data, io
 from ..util import img_as_uint, img_as_float, img_as_int, img_as_ubyte
-import pytest
 from ._warnings import expected_warnings
 
 
 SKIP_RE = re.compile(r"(\s*>>>.*?)(\s*)#\s*skip\s+if\s+(.*)$")
 
+import pytest
 skipif = pytest.mark.skipif
 xfail = pytest.mark.xfail
 parametrize = pytest.mark.parametrize
@@ -325,133 +324,6 @@ def test_parallel(num_threads=2, warnings_matching=None):
         return inner
 
     return wrapper
-
-
-def _show_skimage_info():
-    import skimage
-    print("skimage version %s" % skimage.__version__)
-
-
-class PytestTester(object):
-    """
-    Pytest test runner.
-
-    This class is made available in ``skimage._shared.testing``, and a test
-    function is typically added to a package's __init__.py like so::
-
-      from skimage._shared.testing import PytestTester
-      test = PytestTester(__name__)
-      del PytestTester
-
-    Calling this test function finds and runs all tests associated with the
-    module and all its sub-modules.
-
-    Attributes
-    ----------
-    module_name : str
-        Full path to the package to test.
-
-    Parameters
-    ----------
-    module_name : module name
-        The name of the module to test.
-
-    """
-    def __init__(self, module_name):
-        self.module_name = module_name
-
-    def __call__(self, label='fast', verbose=1, extra_argv=None,
-                 doctests=False, coverage=False, durations=-1, tests=None):
-        """
-        Run tests for module using pytest.
-
-        Parameters
-        ----------
-        label : {'fast', 'full'}, optional
-            Identifies the tests to run. When set to 'fast', tests decorated
-            with `pytest.mark.slow` are skipped, when 'full', the slow marker
-            is ignored.
-        verbose : int, optional
-            Verbosity value for test outputs, in the range 1-3. Default is 1.
-        extra_argv : list, optional
-            List with any extra arguments to pass to pytests.
-        doctests : bool, optional
-            .. note:: Not supported
-        coverage : bool, optional
-            If True, report coverage of scikit-image code. Default is False.
-            Requires installation of (pip) pytest-cov.
-        durations : int, optional
-            If < 0, do nothing, If 0, report time of all tests, if > 0,
-            report the time of the slowest `timer` tests. Default is -1.
-        tests : test or list of tests
-            Tests to be executed with pytest '--pyargs'
-
-        Returns
-        -------
-        result : bool
-            Return True on success, false otherwise.
-
-        Examples
-        --------
-        >>> result = np.lib.test() #doctest: +SKIP
-        ...
-        1023 passed, 2 skipped, 6 deselected, 1 xfailed in 10.39 seconds
-        >>> result
-        True
-
-        """
-        import pytest
-
-        module = sys.modules[self.module_name]
-        module_path = os.path.abspath(module.__path__[0])
-
-        # setup the pytest arguments
-        pytest_args = ["-l"]
-
-        # offset verbosity. The "-q" cancels a "-v".
-        pytest_args += ["-q"]
-
-        # Filter out annoying import messages. Want these in both develop and
-        # release mode.
-        pytest_args += [
-            "-W ignore:Not importing directory",
-            "-W ignore:numpy.dtype size changed",
-            "-W ignore:numpy.ufunc size changed", ]
-
-        if doctests:
-            raise ValueError("Doctests not supported")
-
-        if extra_argv:
-            pytest_args += list(extra_argv)
-
-        if verbose > 1:
-            pytest_args += ["-" + "v" * (verbose - 1)]
-
-        if coverage:
-            pytest_args += ["--cov=" + module_path]
-
-        if label == "fast":
-            pytest_args += ["-m", "not slow"]
-        elif label != "full":
-            pytest_args += ["-m", label]
-
-        if durations >= 0:
-            pytest_args += ["--durations=%s" % durations]
-
-        if tests is None:
-            tests = [self.module_name]
-
-        pytest_args += ["--pyargs"] + list(tests)
-
-        # run tests.
-        _show_skimage_info()
-
-        try:
-            code = pytest.main(pytest_args)
-        except SystemExit as exc:
-            code = exc.code
-
-        return code == 0
 
 
 if __name__ == '__main__':
