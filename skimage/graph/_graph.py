2ac80cfb341754e4f476eec08e8d6421b2ec618e
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-10-19 01:27:53 -0500

    Add final coverage tests

--- skimage/graph/_graph.py
+++ skimage/graph/_graph.py
@@ -1,9 +1,33 @@
 import numpy as np
-from scipy import ndimage as ndi
 from scipy import sparse
 from scipy.sparse import csgraph
 from ..morphology._util import _raveled_offsets_and_distances
 from ..util._map_array import map_array
+
+
+def _weighted_abs_diff(values0, values1, distances):
+    """A default edge function for complete image graphs.
+
+    A pixel graph on an image with no edge values and no mask is a very
+    boring regular lattice, so we define a default edge weight to be the
+    absolute difference between values *weighted* by the distance
+    between them.
+
+    Parameters
+    ----------
+    values0 : array
+        The pixel values for each node.
+    values1 : array
+        The pixel values for each neighbor.
+    distances : array
+        The distance between each node and its neighbor.
+
+    Returns
+    -------
+    edge_values : array of float
+        The computed values: abs(values0 - values1) * distances.
+    """
+    return np.abs(values0 - values1) * distances
 
 
 def pixel_graph(
@@ -49,9 +73,9 @@ def pixel_graph(
         image = mask
     if image.dtype == bool and mask is None:
         mask = image
-    if mask is None:
+    if mask is None and edge_function is None:
         mask = np.ones_like(image, dtype=bool)
-        edge_function = np.subtract
+        edge_function = _weighted_abs_diff
 
     padded = np.pad(mask, 1, mode='constant', constant_values=False)
     nodes_padded = np.arange(padded.size).reshape(padded.shape)[padded]
--- skimage/graph/tests/test_pixel_graph.py
+++ skimage/graph/tests/test_pixel_graph.py
@@ -12,6 +12,11 @@ def test_small_graph():
     np.testing.assert_allclose(np.unique(g.data), [1, np.sqrt(2)])
     np.testing.assert_array_equal(n, [0, 4, 5, 7])
 
+    # check that using mask=image kwarg gives same result
+    gm, nm = pixel_graph(mask=mask, connectivity=2)
+    np.testing.assert_array_equal(g.todense(), gm.todense())
+    np.testing.assert_array_equal(n, nm)
+
 
 def test_central_pixel():
     g, n = pixel_graph(mask, connectivity=2)
@@ -19,8 +24,14 @@ def test_central_pixel():
     np.testing.assert_array_equal(px, (1, 1))
     s2 = np.sqrt(2)
     np.testing.assert_allclose(ds, [s2*3 + 2, s2 + 2, s2*2 + 2, s2*2 + 2])
+
+    # test raveled coordinate
     px, _ = central_pixel(g, n)
     assert px == 4
+
+    # test no nodes given
+    px, _ = central_pixel(g)
+    assert px == 1
 
 
 def test_edge_function():
@@ -34,6 +45,16 @@ def test_edge_function():
     np.testing.assert_allclose(g[0, 1], np.abs(image[0, 0] - image[1, 1]) + s2)
     np.testing.assert_allclose(g[1, 2], np.abs(image[1, 1] - image[1, 2]) + 1)
     np.testing.assert_array_equal(n, [0, 4, 5, 7])
+
+
+def test_default_edge_func():
+    g, n = pixel_graph(image, spacing=np.array([0.78, 0.78]))
+    num_edges = len(g.data) // 2  # each edge appears in both directions
+    assert num_edges == 12  # lattice in a (3, 3) grid
+    np.testing.assert_almost_equal(
+            g[0, 1], 0.78 * np.abs(image[0, 0] - image[0, 1])
+            )
+    np.testing.assert_array_equal(n, np.arange(image.size))
 
 
 if __name__ == '__main__':
