a052b706c3e1ed5facf543e75676a51a6393c400
rfezzani <rfezzani@gmail.com>
2021-10-28 18:02:15 +0200

    Fix test_ccomp

--- skimage/measure/tests/test_ccomp.py
+++ skimage/measure/tests/test_ccomp.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from skimage.measure import label
-import skimage.measure._ccomp as ccomp
+import skimage._shared.label as ccomp
 
 from skimage._shared import testing
 from skimage._shared.testing import assert_array_equal
