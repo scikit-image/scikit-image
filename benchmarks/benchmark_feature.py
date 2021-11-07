37f168ff4f8634cd85c484b6900070c534cbd823
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-11-08 10:50:34 +1100

    Add BRIEF benchmark

--- benchmarks/benchmark_feature.py
+++ benchmarks/benchmark_feature.py
@@ -12,6 +12,9 @@ class FeatureSuite:
         # get a larger size for the benchmark.
         self.image = np.tile(color.rgb2gray(data.astronaut()), (4, 4))
         self.image_ubyte = util.img_as_ubyte(self.image)
+        self.keypoints = feature.corner_peaks(
+                self.image, min_distance=5, threshold_rel=0.1
+                )
 
     def time_canny(self):
         result = feature.canny(self.image)
@@ -20,3 +23,7 @@ class FeatureSuite:
         pi = np.pi
         result = feature.greycomatrix(self.image_ubyte, distances=[1, 2],
                                       angles=[0, pi/4, pi/2, 3*pi/4])
+
+    def time_brief(self):
+        extractor = feature.BRIEF()
+        extractor.extract(self.image, self.keypoints)
