6c9319f43d35c34e708c959ddee75de81bb469f4
Gregory Lee <grlee77@gmail.com>
2021-11-12 15:47:48 -0500

    Avoid matplotlib import overhead during 'import skimage'
    move has_mpl out of _shared.__init__ because setting its value requires
    importing matplotlib

--- skimage/transform/tests/test_radon_transform.py
+++ skimage/transform/tests/test_radon_transform.py
@@ -3,7 +3,7 @@ import itertools
 import numpy as np
 import pytest
 
-from skimage._shared import has_mpl
+from skimage._shared._dependency_checks import has_mpl
 from skimage._shared._warnings import expected_warnings
 from skimage._shared.testing import test_parallel
 from skimage._shared.utils import _supported_float_type, convert_to_float
