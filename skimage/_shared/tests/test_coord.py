c4e828a0f05ac96a7ec955558e7a9230642901d9
Riadh Fezzani <rfezzani@gmail.com>
2021-11-22 04:14:44 +0100

    Fix peak local max segfault (#6035)
    * Fix batch size
    
    Batch size is now in increasing order instead of decreasing. See https://github.com/scikit-image/scikit-image/issues/6010#issuecomment-966801497
    
    * Add max_split_size argument
    
    * Add link to benchmark results in docstring
    
    * Add run time test for ensure_spacing
    
    * remove specific timeout from test_max_batch_size
    
    instead test that our current max_split_size of 2000 is not slower than a substantially
    larger batch size
    
    Co-authored-by: Riadh <r.fezzani@vitadx.com>
    Co-authored-by: Marianne Corvellec <marianne.corvellec@ens-lyon.org>
    Co-authored-by: Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
    Co-authored-by: Gregory Lee <grlee77@gmail.com>

--- skimage/_shared/tests/test_coord.py
+++ skimage/_shared/tests/test_coord.py
@@ -1,6 +1,9 @@
-import pytest
+import time
+
 import numpy as np
+import pytest
 from scipy.spatial.distance import pdist, minkowski
+
 from skimage._shared.coord import ensure_spacing
 
 
@@ -55,6 +58,24 @@ def test_ensure_spacing_batch_processing(p, size):
                                          min_split_size=size),
                           expected)
 
+
+def test_max_batch_size():
+    """Small batches are slow, large batches -> large allocations -> also slow.
+
+    https://github.com/scikit-image/scikit-image/pull/6035#discussion_r751518691
+    """
+    coords = np.random.randint(low=0, high=1848, size=(40000, 2))
+    tstart = time.time()
+    ensure_spacing(coords, spacing=100, min_split_size=50,
+                   max_split_size=2000)
+    dur1 = time.time() - tstart
+
+    tstart = time.time()
+    ensure_spacing(coords, spacing=100, min_split_size=50,
+                   max_split_size=20000)
+    dur2 = time.time() - tstart
+
+    assert dur1 < dur2
 
 
 @pytest.mark.parametrize("p", [1, 2, np.inf])
