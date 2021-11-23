3d735a2ff21125700c4408e065f7b4cc28376f1e
Gregory Lee <grlee77@gmail.com>
2021-11-23 11:29:36 -0500

    bump 0.18+ to 0.19+ in version error message

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if sys.version_info < (3, 6):
 
     error = """Python {py} detected.
 
-scikit-image 0.18+ supports only Python 3.7 and above.
+scikit-image 0.19+ supports only Python 3.7 and above.
 
 For Python 2.7, please install the 0.14.x Long Term Support release using:
 
