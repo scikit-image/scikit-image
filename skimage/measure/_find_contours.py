185cf8a628763e8e2cb34a6034a7304e411ebca9
Gregory Lee <grlee77@gmail.com>
2022-01-05 17:09:57 -0500

    remove deprecate_kwarg decorator from find_contours

--- skimage/measure/_find_contours.py
+++ skimage/measure/_find_contours.py
@@ -8,9 +8,6 @@ from collections import deque
 _param_options = ('high', 'low')
 
 
-@deprecate_kwarg({'array': 'image'},
-                 deprecated_version='0.18',
-                 removed_version='0.20')
 def find_contours(image, level=None,
                   fully_connected='low', positive_orientation='low',
                   *,
