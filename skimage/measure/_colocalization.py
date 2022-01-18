3a9707a4d86986e957eb5485708b6a56cfbc0662
Thanushi Peiris <54516770+thanushipeiris@users.noreply.github.com>
2022-01-18 23:46:57 +1100

    fix pep8

--- skimage/measure/_colocalization.py
+++ skimage/measure/_colocalization.py
@@ -11,22 +11,30 @@ __all__ = ['pearson_r',
            'average_integrated_density'
            ]
 
-# check that all np.arrays passed to it have the same shape
+
 def check_shape_equality_all(*args):
+    # check that all np.arrays passed to it have the same shape
     im1 = args[0]
     for im in args[1:]:
         check_shape_equality(im1, im)
     return
 
+
 def check_numpy_arr(arr, name, bool_expected=False):
     if type(arr) != np.ndarray:
-        raise ValueError(f"{name} is of type {type(arr)} not nd.array of dtype boolean as expected")
+        raise ValueError(
+            f"{name} is of type {type(arr)} not nd.array of dtype boolean as",
+            "expected")
     if bool_expected:
-        if np.sum(np.where(arr>1,1,0))>0:
-            raise ValueError(f"{name} is ndarray of dtype {arr.dtype} with non-binary values. Check if image mask was passed in as expected.")
+        if np.sum(np.where(arr > 1, 1, 0)) > 0:
+            raise ValueError(
+                f"{name} is ndarray of dtype {arr.dtype} with non-binary",
+                "values.Check if image mask was passed in as expected.")
+
 
 def pcc(imgA, imgB, roi=None):
-    """Calculate Pearson's Correlation Coefficient between pixel intensities in two channels.
+    """Calculate Pearson's Correlation Coefficient between pixel intensities in
+    channels.
 
     Parameters
     ----------
@@ -36,25 +44,29 @@ def pcc(imgA, imgB, roi=None):
         Image of channel 2 to be correlated with channel B.
         Must have same dimensions as `imgA`.
     roi : (M, N) ndarray of dtype bool, optional
-        Only `imgA` and `imgB` pixels within this region of interest mask are included in the calculation.
+        Only `imgA` and `imgB` pixels within this region of interest mask are
+        included in the calculation.
         Must have same dimensions as `imgA`.
 
     Returns
     -------
     pcc : float
-        Pearson's correlation coefficient of the pixel intensities between the two images, within the ROI if provided.
+        Pearson's correlation coefficient of the pixel intensities between the
+        two images, within the ROI if provided.
     p-value : float
         Two-tailed p-value.
 
     Notes
     -------
 
-    Pearson's Correlation Coefficient (PCC) measures the linear correlation between the intensities of the two .
-    Its value ranges from -1 for perfect linear anti-correlation and +1 for perfect linear correlation. The calculation
-    of the p-value assumes that the intensities of pixels in each input image is normally distributed.
+    Pearson's Correlation Coefficient (PCC) measures the linear correlation
+    between the pixel intensities of the two images. Its value ranges from -1
+    for perfect linear anti-correlation and +1 for perfect linear correlation.
+    The calculation of the p-value assumes that the intensities of pixels in
+    each input image are normally distributed.
 
-    Scipy's implementation of Pearson's correlation coefficient is used. Please refer to it for further information
-    and caveats [1]_.
+    Scipy's implementation of Pearson's correlation coefficient is used. Please
+    refer to it for further information and caveats [1]_.
 
     .. math::
         r = \frac{\sum (A_i - m_A_i) (B_i - m_B_i)}
@@ -66,24 +78,31 @@ def pcc(imgA, imgB, roi=None):
         :math:`m_A_i` is the mean of the pixel values in `imgA`
         :math:`m_B_i` is the mean of the pixel values in `imgB`
 
-    A low PCC value does not necessarily mean that there is no correlation between the two channel intensities, just
-    that there is no linear correlation. You may wish to plot the pixel intensities of each of thw two channels in a 2D
-    scatterplot and use Spearman's rank correlation if a non-linear correlation is visually identified [2]_.
-    Also consider if you are interested in correlation or co-occurence, in which case a method involving segmentation
-    masks (e.g. MCC or intersection coefficient) may be more suitable [3]_ [4]_.
+    A low PCC value does not necessarily mean that there is no correlation
+    between the two channel intensities, just that there is no linear
+    correlation. You may wish to plot the pixel intensities of each of thw two
+    channels in a 2D scatterplot and use Spearman's rank correlation if a
+    non-linear correlation is visually identified [2]_. Also consider if you
+    are interested in correlation or co-occurence, in which case a method
+    involving segmentation masks (e.g. MCC or intersection coefficient) may be
+    more suitable [3]_ [4]_.
 
-    Providing the ROI of only relevant sections of the image (e.g. cells, or particular cellular compartments) and
-    removing noise is important as the PCC is sensitive to these measures [3]_ [4]_.
+    Providing the ROI of only relevant sections of the image (e.g. cells, or
+    particular cellular compartments) and removing noise is important as the
+    PCC is sensitive to these measures [3]_ [4]_.
 
     References
     -------
     .. [1] https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.pearsonr.html
     .. [2] https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.spearmanr.html
-    .. [3] Dunn, K. W., Kamocka, M. M., & McDonald, J. H. (2011). A practical guide to evaluating colocalization in
-           biological microscopy. American journal of physiology. Cell physiology, 300(4), C723–C742.
+    .. [3] Dunn, K. W., Kamocka, M. M., & McDonald, J. H. (2011). A practical
+           guide to evaluating colocalization in biological microscopy.
+           American journal of physiology. Cell physiology, 300(4), C723–C742.
            https://doi.org/10.1152/ajpcell.00462.2010
-    .. [4] Bolte, S. and Cordelières, F.P. (2006), A guided tour into subcellular colocalization analysis in light
-           microscopy. Journal of Microscopy, 224: 213-232. https://doi.org/10.1111/j.1365-2818.2006.01706.x
+    .. [4] Bolte, S. and Cordelières, F.P. (2006), A guided tour into
+           subcellular colocalization analysis in light microscopy. Journal of
+           Microscopy, 224: 213-232.
+           https://doi.org/10.1111/j.1365-2818.2006.01706.x
     """
     if roi is None:
         roi = np.ones_like(imgA)
@@ -96,6 +115,7 @@ def pcc(imgA, imgB, roi=None):
     imgB_masked = imgB[roi.astype(bool)]
     return pearsonr(imgA_masked, imgB_masked)
 
+
 def mcc(imgA, imgB_mask, roi=None):
     """Manders' colocalization coefficient between two channels.
 
@@ -107,7 +127,8 @@ def mcc(imgA, imgB_mask, roi=None):
         Binary mask with segmented regions of interest in channel B.
         Must have same dimensions as `imgA`.
     roi : (M, N) ndarray of dtype bool, optional
-        Only `imgA` pixel values within this region of interest mask are included in the calculation.
+        Only `imgA` pixel values within this region of interest mask are
+        included in the calculation.
         Must have same dimensions as `imgA`.
 
     Returns
@@ -117,14 +138,18 @@ def mcc(imgA, imgB_mask, roi=None):
 
     Notes
     -------
-    Manders' Colocalization Coefficient (MCC) is the fraction of total intensity of a certain channel that is within the
-    segmented region of a second channel [1]_. It ranges from 0 for no colocalisation to 1 for complete colocalization.
-    It is also referred to as M1 and M2.
+    Manders' Colocalization Coefficient (MCC) is the fraction of total
+    intensity of a certain channel that is within the segmented region of a
+    second channel [1]_. It ranges from 0 for no colocalisation to 1 for
+    complete colocalization. It is also referred to as M1 and M2.
 
-    MCC is commonly used to measure the colocalization of a particular protein in a subceullar compartment.
-    Typically a threshold on the second channel is set and values above this are used in the MCC calculation.In this
-    implementation, the thresholded image is provided as the argument `imgB_mask`. Alternative segmentation methods on
-    the second channel can also be performed, with the resulting image mask being provided as `imgB_mask`.
+    MCC is commonly used to measure the colocalization of a particular protein
+    in a subceullar compartment. Typically a threshold on the second channel is
+    set and values above this are used in the MCC calculation.In this
+    implementation, the thresholded image is provided as the argument
+    `imgB_mask`. Alternative segmentation methods on the second channel can
+    also be performed, with the resulting image mask being provided as
+    `imgB_mask`.
 
     The implemented equation is:
 
@@ -137,17 +162,20 @@ def mcc(imgA, imgB_mask, roi=None):
         :math:`A_{i,coloc} = A_i` if math:`Bmask_i > 0`
         :math:`Bmask_i` is the value of the :math:`i^{th}` pixel in `imgB_mask`
 
-    MCC is sensitive to noise, with diffuse signal in the first channel inflating its value. Images should be
-    processed to remove out of focus and background light before the MCC is calculated [2]_.
+    MCC is sensitive to noise, with diffuse signal in the first channel
+    inflating its value. Images should be processed to remove out of focus and
+    background light before the MCC is calculated [2]_.
 
     References
     -------
-    .. [1] Manders, E.M.M., Verbeek, F.J. and Aten, J.A. (1993), Measurement of co-localization
-           of objects in dual-colour confocal images. Journal of Microscopy, 169: 375-382.
+    .. [1] Manders, E.M.M., Verbeek, F.J. and Aten, J.A. (1993), Measurement of
+           co-localization of objects in dual-colour confocal images. Journal
+           of Microscopy, 169: 375-382.
            https://doi.org/10.1111/j.1365-2818.1993.tb03313.x
            https://imagej.net/media/manders.pdf
-    .. [2] Dunn, K. W., Kamocka, M. M., & McDonald, J. H. (2011). A practical guide to evaluating colocalization in
-           biological microscopy. American journal of physiology. Cell physiology, 300(4), C723–C742.
+    .. [2] Dunn, K. W., Kamocka, M. M., & McDonald, J. H. (2011). A practical
+           guide to evaluating colocalization in biological microscopy.
+           American journal of physiology. Cell physiology, 300(4), C723–C742.
            https://doi.org/10.1152/ajpcell.00462.2010
 
     """
@@ -160,9 +188,10 @@ def mcc(imgA, imgB_mask, roi=None):
 
     imgA = imgA[roi.astype(bool)]
     imgB_mask = imgB_mask[roi.astype(bool)]
-    if (np.sum(imgA)==0):
+    if (np.sum(imgA) == 0):
         return 0
-    return np.sum(np.multiply(imgA, imgB_mask))/np.sum(imgA)
+    return np.sum(np.multiply(imgA, imgB_mask)) / np.sum(imgA)
+
 
 def moc(imgA, imgB, roi=None):
     """Manders' overlap coefficient
@@ -175,13 +204,15 @@ def moc(imgA, imgB, roi=None):
         Image of channel B.
         Must have same dimensions as `imgA`
     roi : (M, N) ndarray of dtype bool, optional
-        Only `imgA` and `imgB` pixel values within this region of interest mask are included in the calculation.
+        Only `imgA` and `imgB` pixel values within this region of interest mask
+        are included in the calculation.
         Must have same dimensions as `imgA`.
 
     Returns
     -------
     moc: float
-        Manders' Overlap Coefficient of pixel intensities between the two images.
+        Manders' Overlap Coefficient of pixel intensities between the two
+        images.
 
     Notes
     -------
@@ -195,28 +226,37 @@ def moc(imgA, imgB, roi=None):
         :math:`A_i` is the value of the :math:`i^{th}` pixel in `imgA`
         :math:`B_i` is the value of the :math:`i^{th}` pixel in `imgB`
 
-    It ranges between 0 for no colocalization and 1 for complete colocalization of all pixels.
+    It ranges between 0 for no colocalization and 1 for complete colocalization
+    of all pixels.
 
-    MOC does not take into account pixel intensities, just the fraction of pixels that have positive values for both
-    channels[2]_ [3]_. Its usefulness has been criticized as it changes in response to differences in both co-occurence
-    and correlation and so a particular MOC value could indicate a wide range of colocalization patterns [4]_ [5]_.
+    MOC does not take into account pixel intensities, just the fraction of
+    pixels that have positive values for both channels[2]_ [3]_. Its usefulness
+    has been criticized as it changes in response to differences in both
+    co-occurence and correlation and so a particular MOC value could indicate
+    a wide range of colocalization patterns [4]_ [5]_.
 
     References
     ----------
-    .. [1] Manders, E.M.M., Verbeek, F.J. and Aten, J.A. (1993), Measurement of co-localization
-           of objects in dual-colour confocal images. Journal of Microscopy, 169: 375-382.
+    .. [1] Manders, E.M.M., Verbeek, F.J. and Aten, J.A. (1993), Measurement of
+           co-localization of objects in dual-colour confocal images. Journal
+           of Microscopy, 169: 375-382.
            https://doi.org/10.1111/j.1365-2818.1993.tb03313.x
            https://imagej.net/media/manders.pdf
-    .. [2] Dunn, K. W., Kamocka, M. M., & McDonald, J. H. (2011). A practical guide to evaluating colocalization in
-           biological microscopy. American journal of physiology. Cell physiology, 300(4), C723–C742.
+    .. [2] Dunn, K. W., Kamocka, M. M., & McDonald, J. H. (2011). A practical
+           guide to evaluating colocalization in biological microscopy.
+           American journal of physiology. Cell physiology, 300(4), C723–C742.
            https://doi.org/10.1152/ajpcell.00462.2010
-    .. [3] Bolte, S. and Cordelières, F.P. (2006), A guided tour into subcellular colocalization analysis in light
-           microscopy. Journal of Microscopy, 224: 213-232. https://doi.org/10.1111/j.1365-2818.2006.01
-    .. [4] Adler J, Parmryd I. (2010), Quantifying colocalization by correlation: the Pearson correlation coefficient is
-           superior to the Mander's overlap coefficient. Cytometry A.  Aug;77(8):733-42.
-           https://doi.org/10.1002/cyto.a.20896
-    .. [5] Adler, J, Parmryd, I. Quantifying colocalization: The case for discarding the Manders overlap coefficient.
-           Cytometry. 2021; 99: 910– 920. https://doi.org/10.1002/cyto.a.24336
+    .. [3] Bolte, S. and Cordelières, F.P. (2006), A guided tour into
+           subcellular colocalization analysis in light microscopy. Journal of
+           Microscopy, 224: 213-232.
+           https://doi.org/10.1111/j.1365-2818.2006.01
+    .. [4] Adler J, Parmryd I. (2010), Quantifying colocalization by
+           correlation: the Pearson correlation coefficient is
+           superior to the Mander's overlap coefficient. Cytometry A.
+           Aug;77(8):733-42.https://doi.org/10.1002/cyto.a.20896
+    .. [5] Adler, J, Parmryd, I. Quantifying colocalization: The case for
+           discarding the Manders overlap coefficient. Cytometry. 2021; 99:
+           910– 920. https://doi.org/10.1002/cyto.a.24336
 
     """
     if roi is None:
@@ -228,10 +268,14 @@ def moc(imgA, imgB, roi=None):
 
     imgA = imgA[roi.astype(bool)]
     imgB = imgB[roi.astype(bool)]
-    return np.sum(np.multiply(imgA, imgB))/(np.sum(np.square(imgA))*(np.sum(np.square(imgB))))**0.5
+
+    denom = (np.sum(np.square(imgA)) * (np.sum(np.square(imgB)))) ** 0.5
+    return np.sum(np.multiply(imgA, imgB)) / denom
+
 
 def intersection_coefficient(imgA_mask, imgB_mask, roi=None):
-    """Fraction of a channel's segmented binary mask that overlaps with a second channel's segmented binary mask.
+    """Fraction of a channel's segmented binary mask that overlaps with a
+    second channel's segmented binary mask.
 
     Parameters
     ----------
@@ -241,7 +285,8 @@ def intersection_coefficient(imgA_mask, imgB_mask, roi=None):
         Image of channel B.
         Must have same dimensions as `imgA_mask`.
     roi : (M, N) ndarray of dtype bool, optional
-        Only `imgA_mask` and `imgB_mask` pixels within this region of interest mask are included in the calculation.
+        Only `imgA_mask` and `imgB_mask` pixels within this region of interest
+        mask are included in the calculation.
         Must have same dimensions as `imgA_mask`.
 
     Returns
@@ -259,8 +304,11 @@ def intersection_coefficient(imgA_mask, imgB_mask, roi=None):
 
     imgA_mask = imgA_mask[roi.astype(bool)]
     imgB_mask = imgB_mask[roi.astype(bool)]
-    if np.count_nonzero(imgA_mask)==0: return 0
-    return np.count_nonzero(np.logical_and(imgA_mask, imgB_mask))/np.count_nonzero(imgA_mask)
+    if np.count_nonzero(imgA_mask) == 0:
+        return 0
+    denom = np.count_nonzero(imgA_mask)
+    return np.count_nonzero(np.logical_and(imgA_mask, imgB_mask)) / denom
+
 
 def pixel_intensity_sum(img, roi=None):
     """Sum of all intensity values of image within the ROI.
@@ -269,7 +317,8 @@ def pixel_intensity_sum(img, roi=None):
     ----------
     img : (M, N) ndarray
     roi : (M, N) ndarray of dtype bool, optional
-        Only `img` pixel values within this region of interest mask are included in the calculation.
+        Only `img` pixel values within this region of interest mask are
+        included in the calculation.
         Must have same dimensions as `img`.
 
     Returns
@@ -295,6 +344,7 @@ def pixel_intensity_sum(img, roi=None):
     img = img[roi.astype(bool)]
     return np.sum(img)
 
+
 def av_pixel_intensity(img, roi=None):
     """ Average intensity of all pixels within the ROI.
 
@@ -302,7 +352,8 @@ def av_pixel_intensity(img, roi=None):
     ----------
     img : (M, N) ndarray
     roi : (M, N) ndarray of dtype bool, optional
-        Only `img` pixel values within this region of interest mask are included in the calculation.
+        Only `img` pixel values within this region of interest mask are
+        included in the calculation.
         Must have same dimensions as `img`.
 
     Returns
@@ -317,4 +368,4 @@ def av_pixel_intensity(img, roi=None):
     check_shape_equality_all(img, roi)
 
     img = img[roi.astype(bool)]
-    return np.sum(img)/np.size(img)
+    return np.sum(img) / np.size(img)
--- skimage/measure/tests/test_colocalization.py
+++ skimage/measure/tests/test_colocalization.py
@@ -1,14 +1,15 @@
 import numpy as np
 import pytest
 from skimage.measure import (pcc, mcc, moc, intersection_coefficient,
-                              pixel_intensity_sum, av_pixel_intensity)
+                             pixel_intensity_sum, av_pixel_intensity)
+
 
 def test_invalid_input():
     # images are not same size
     img1 = np.array([[i + j for j in range(4)] for i in range(4)])
-    img2 = np.ones((3,5,6))
-    mask = np.array([[i<=1 for j in range(5)]for i in range(5)])
-    non_binary_mask = np.array([[2 for j in range(4)]for i in range(4)])
+    img2 = np.ones((3, 5, 6))
+    mask = np.array([[i <= 1 for j in range(5)] for i in range(5)])
+    non_binary_mask = np.array([[2 for j in range(4)] for i in range(4)])
 
     with pytest.raises(ValueError):
         pcc(img1, img1, mask)
@@ -17,15 +18,15 @@ def test_invalid_input():
     with pytest.raises(ValueError):
         pcc(img1, img1, mask)
     with pytest.raises(ValueError):
-        pcc(img1,img1,non_binary_mask)
+        pcc(img1, img1, non_binary_mask)
     with pytest.raises(ValueError):
         mcc(img1, mask)
     with pytest.raises(ValueError):
         mcc(img1, non_binary_mask)
     with pytest.raises(ValueError):
-        mcc(img1, img1>0, mask)
+        mcc(img1, img1 > 0, mask)
     with pytest.raises(ValueError):
-        mcc(img1, img1>0, non_binary_mask)
+        mcc(img1, img1 > 0, non_binary_mask)
     with pytest.raises(ValueError):
         moc(img1, img1, mask)
     with pytest.raises(ValueError):
@@ -33,15 +34,15 @@ def test_invalid_input():
     with pytest.raises(ValueError):
         moc(img1, img1, mask)
     with pytest.raises(ValueError):
-        moc(img1,img1,non_binary_mask)
+        moc(img1, img1, non_binary_mask)
     with pytest.raises(ValueError):
-        intersection_coefficient(img1>2, img2>1, mask)
+        intersection_coefficient(img1 > 2, img2 > 1, mask)
     with pytest.raises(ValueError):
         intersection_coefficient(img1, img2)
     with pytest.raises(ValueError):
         intersection_coefficient(img1, img1, mask)
     with pytest.raises(ValueError):
-        intersection_coefficient(img1>2,img1>1,non_binary_mask)
+        intersection_coefficient(img1 > 2, img1 > 1, non_binary_mask)
     with pytest.raises(ValueError):
         pixel_intensity_sum(img1, mask)
     with pytest.raises(ValueError):
@@ -50,6 +51,7 @@ def test_invalid_input():
         av_pixel_intensity(img1, mask)
     with pytest.raises(ValueError):
         av_pixel_intensity(img1, non_binary_mask)
+
 
 def test_pcc():
     # simple example
@@ -60,31 +62,35 @@ def test_pcc():
     assert pcc(img1, img2) == (0.944911182523068, 3.5667540654536515e-08)
 
     # change background of roi and see if values are same
-    roi  = np.where(img1 <= 2, 0, 1)
+    roi = np.where(img1 <= 2, 0, 1)
     assert pcc(img1, img1, roi=roi) == pcc(img1, img2, roi=roi)
+
 
 def test_mcc():
     img1 = np.array([[j for j in range(4)] for i in range(4)])
-    mask = np.array([[i<=1 for j in range(4)]for i in range(4)])
+    mask = np.array([[i <= 1 for j in range(4)]for i in range(4)])
     assert mcc(img1, imgB_mask=mask) == 0.5
 
+
 def test_moc():
-    img1 = np.ones((4,4))
-    img2 = 2*np.ones((4,4))
+    img1 = np.ones((4, 4))
+    img2 = 2*np.ones((4, 4))
     assert moc(img1, img2) == 1
 
+
 def test_intersection_coefficient():
-    img1_mask = np.array([[j<=1 for j in range(4)] for i in range(4)])
-    img2_mask = np.array([[i<=1 for j in range(4)] for i in range(4)])
+    img1_mask = np.array([[j <= 1 for j in range(4)] for i in range(4)])
+    img2_mask = np.array([[i <= 1 for j in range(4)] for i in range(4)])
     img3_mask = np.array([[1 for j in range(4)] for i in range(4)])
     assert intersection_coefficient(img1_mask, img2_mask) == 0.5
     assert intersection_coefficient(img1_mask, img3_mask) == 1
+
 
 def test_pixel_intensity_sum():
     img1 = np.array([[i + j for j in range(4)] for i in range(4)])
     assert pixel_intensity_sum(img1) == 48
 
+
 def test_av_pixel_intensity():
     img1 = np.array([[i + j for j in range(4)] for i in range(4)])
     assert av_pixel_intensity(img1) == 3
-
