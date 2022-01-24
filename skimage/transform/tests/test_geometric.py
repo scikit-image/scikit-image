5244bc7f1262d858f6373fa8d108b111387bc93b
Chris Roat <1053153+chrisroat@users.noreply.github.com>
2022-01-23 20:41:26 -0800

    Always set params to nan when ProjectiveTransform.estimate fails
    Checks .estimate return value in all tests.

--- skimage/transform/tests/test_geometric.py
+++ skimage/transform/tests/test_geometric.py
@@ -67,7 +67,7 @@ def test_euclidean_estimation():
 
     # via estimate method
     tform3 = EuclideanTransform()
-    tform3.estimate(SRC, DST)
+    assert tform3.estimate(SRC, DST)
     assert_almost_equal(tform3.params, tform2.params)
 
 
@@ -114,7 +114,7 @@ def test_similarity_estimation():
 
     # via estimate method
     tform3 = SimilarityTransform()
-    tform3.estimate(SRC, DST)
+    assert tform3.estimate(SRC, DST)
     assert_almost_equal(tform3.params, tform2.params)
 
 
@@ -180,7 +180,7 @@ def test_affine_estimation():
 
     # via estimate method
     tform3 = AffineTransform()
-    tform3.estimate(SRC, DST)
+    assert tform3.estimate(SRC, DST)
     assert_almost_equal(tform3.params, tform2.params)
 
 
@@ -210,7 +210,7 @@ def test_affine_init():
 
 def test_piecewise_affine():
     tform = PiecewiseAffineTransform()
-    tform.estimate(SRC, DST)
+    assert tform.estimate(SRC, DST)
     # make sure each single affine transform is exactly estimated
     assert_almost_equal(tform(SRC), DST)
     assert_almost_equal(tform.inverse(DST), SRC)
@@ -324,7 +324,7 @@ def test_projective_estimation():
 
     # via estimate method
     tform3 = ProjectiveTransform()
-    tform3.estimate(SRC, DST)
+    assert tform3.estimate(SRC, DST)
     assert_almost_equal(tform3.params, tform2.params)
 
 
@@ -368,7 +368,7 @@ def test_polynomial_estimation():
 
     # via estimate method
     tform2 = PolynomialTransform()
-    tform2.estimate(SRC, DST, order=10)
+    assert tform2.estimate(SRC, DST, order=10)
     assert_almost_equal(tform2.params, tform.params)
 
 
@@ -519,15 +519,15 @@ def test_degenerate():
     src = dst = np.zeros((10, 2))
 
     tform = SimilarityTransform()
-    tform.estimate(src, dst)
+    assert tform.estimate(src, dst)  # NOTE: Always returns true.
     assert np.all(np.isnan(tform.params))
 
     tform = AffineTransform()
-    tform.estimate(src, dst)
+    assert not tform.estimate(src, dst)
     assert np.all(np.isnan(tform.params))
 
     tform = ProjectiveTransform()
-    tform.estimate(src, dst)
+    assert not tform.estimate(src, dst)
     assert np.all(np.isnan(tform.params))
 
     # See gh-3926 for discussion details
@@ -542,6 +542,13 @@ def test_degenerate():
         # Prior to gh-3926, under the above circumstances,
         # a transform could be returned with nan values.
         assert(not tform.estimate(src, dst) or np.isfinite(tform.params).all())
+
+    src = np.array([[0, 2, 0], [0, 2, 0], [0, 4, 0]])
+    dst = np.array([[0, 1, 0], [0, 1, 0], [0, 3, 0]])
+    tform = AffineTransform()
+    assert not tform.estimate(src, dst)
+    # Prior to gh-xxxx, the above would set the parameters as the identity.
+    assert np.all(np.isnan(tform.params))
 
 
 def test_normalize_degenerate_points():
@@ -612,7 +619,7 @@ def test_estimate_affine_3d():
     dst = tf(src)
     dst_noisy = dst + np.random.random((25, ndim))
     tf2 = AffineTransform(dimensionality=ndim)
-    tf2.estimate(src, dst_noisy)
+    assert tf2.estimate(src, dst_noisy)
     # we check rot/scale/etc more tightly than translation because translation
     # estimation is on the 1 pixel scale
     assert_almost_equal(tf2.params[:, :-1], matrix[:, :-1], decimal=2)
