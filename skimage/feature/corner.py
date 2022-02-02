6146b5a5b4de378d5ca6a03eb7dbd2dffb844153
Gregory Lee <grlee77@gmail.com>
2022-02-02 14:53:21 -0500

    fix missing import of warn

--- skimage/feature/corner.py
+++ skimage/feature/corner.py
@@ -6,7 +6,7 @@ from scipy import ndimage as ndi
 from scipy import spatial, stats
 
 from .._shared.filters import gaussian
-from .._shared.utils import _supported_float_type, safe_as_int
+from .._shared.utils import _supported_float_type, safe_as_int, warn
 from ..transform import integral_image
 from ..util import img_as_float
 from ._hessian_det_appx import _hessian_matrix_det
