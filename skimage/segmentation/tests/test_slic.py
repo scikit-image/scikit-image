beaba6b524e01a6f1e6331a5af3bbf089df6a1cd
Gregory Lee <grlee77@gmail.com>
2022-02-13 12:13:58 -0500

    add test case

--- skimage/segmentation/tests/test_slic.py
+++ skimage/segmentation/tests/test_slic.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 from numpy.testing import assert_equal
 
-from skimage import data, img_as_float
+from skimage import data, filters, img_as_float
 from skimage._shared.testing import test_parallel, expected_warnings
 from skimage.segmentation import slic
 
@@ -538,3 +538,23 @@ def test_dtype_support(dtype):
 
     # Simply run the function to assert that it runs without error
     slic(img, start_label=1)
+
+
+def test_start_label_fix():
+    """Tests the fix for a bug producing a label < start_label (gh-6240).
+
+    For the v0.19.1 release, the `img` and `slic` call as below result in two
+    non-contiguous regions with value 0 despite `start_label=1`. We verify that
+    the minimum label is now `start_label` as expected.
+    """
+
+    # generate bumpy data that gives unexpected label prior to bug fix
+    rng = np.random.default_rng(9)
+    img = rng.standard_normal((8, 13)) > 0
+    img = filters.gaussian(img, sigma=1)
+
+    start_label = 1
+    superp = slic(img, start_label=start_label, multichannel=False,
+                  n_segments=6, compactness=0.01, enforce_connectivity=True,
+                  max_num_iter=10)
+    assert superp.min() == start_label
