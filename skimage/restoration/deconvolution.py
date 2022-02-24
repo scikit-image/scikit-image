3f9e23d9ab91659b00aff78e68cd214a42c6d3dc
Elena <elenadpascal@gmail.com>
2022-02-24 17:21:29 +0000

    Fix for #6018

--- skimage/restoration/deconvolution.py
+++ skimage/restoration/deconvolution.py
@@ -60,7 +60,7 @@ def wiener(image, psf, balance, reg=None, is_real=True, clip=True):
     >>> img = convolve2d(img, psf, 'same')
     >>> rng = np.random.default_rng()
     >>> img += 0.1 * img.std() * rng.standard_normal(img.shape)
-    >>> deconvolved_img = restoration.wiener(img, psf, 1100)
+    >>> deconvolved_img = restoration.wiener(img, psf, 0.1)
 
     Notes
     -----
