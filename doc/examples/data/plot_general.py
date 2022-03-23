100428919077b78f24dbe3325f5189bbe6efdb6d
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2022-03-23 07:53:50 +0100

    change to 3x3 example

--- doc/examples/data/plot_general.py
+++ doc/examples/data/plot_general.py
@@ -48,20 +48,22 @@ plt.show()
 
 
 ############################################################################
-# A thumbnail image for the gallery
+# Thumbnail image for the gallery
 
-fig, axs = plt.subplots(nrows=3, ncols=2)
-# The next line sets the thumbnail for the last figure in the gallery
 # sphinx_gallery_thumbnail_number = -1
+fig, axs = plt.subplots(nrows=3, ncols=3)
 for ax in axs.flat:
     ax.axis("off")
 axs[0, 0].imshow(data.astronaut())
 axs[0, 1].imshow(data.binary_blobs(), cmap=plt.cm.gray)
-axs[1, 0].imshow(data.brick(), cmap=plt.cm.gray)
-axs[1, 1].imshow(data.colorwheel())
-axs[2, 0].imshow(data.camera(), cmap=plt.cm.gray)
+axs[0, 2].imshow(data.brick(), cmap=plt.cm.gray)
+axs[1, 0].imshow(data.colorwheel())
+axs[1, 1].imshow(data.camera(), cmap=plt.cm.gray)
+axs[1, 2].imshow(data.cat())
+axs[2, 0].imshow(data.checkerboard(), cmap=plt.cm.gray)
+axs[2, 1].imshow(data.clock(), cmap=plt.cm.gray)
 further_img = np.full((300, 300), 255)
 for xpos in [100, 150, 200]:
     further_img[150 - 10 : 150 + 10, xpos - 10 : xpos + 10] = 0
-axs[2, 1].imshow(further_img, cmap=plt.cm.gray)
-plt.subplots_adjust(wspace=-0.7, hspace=0.1)
+axs[2, 2].imshow(further_img, cmap=plt.cm.gray)
+plt.subplots_adjust(wspace=0.1, hspace=0.1)
