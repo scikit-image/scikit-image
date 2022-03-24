403cd417dcf5668c27744385157b1db29e32a8c9
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2022-03-24 13:08:54 +0100

    invert colors

--- doc/examples/data/plot_3d.py
+++ doc/examples/data/plot_3d.py
@@ -19,7 +19,7 @@ import plotly
 import plotly.express as px
 
 img = data.cells3d()[20:]
-fig = px.imshow(img, facet_col=1, animation_frame=0,
+fig = px.imshow(-img, facet_col=1, animation_frame=0,
                 binary_string=True, binary_format='jpg')
 fig.layout.annotations[0]['text'] = 'Cell membranes'
 fig.layout.annotations[1]['text'] = 'Nuclei'
