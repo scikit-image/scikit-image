e2af9cc8324806b49aef24b865c608035ee0acdf
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2022-03-25 11:45:31 +0100

    run pep8

--- doc/examples/data/plot_specific.py
+++ doc/examples/data/plot_specific.py
@@ -57,15 +57,13 @@ fig, axes = plt.subplots(4, 5, figsize=(20, 20))
 ax = axes.ravel()
 lfs_images = data.lfw_subset()
 for i in range(20):
-    ax[i].imshow(lfs_images[90+i], cmap=plt.cm.gray)
-    ax[i].axis('off')
+    ax[i].imshow(lfs_images[90 + i], cmap=plt.cm.gray)
+    ax[i].axis("off")
 fig.tight_layout()
 plt.show()
 
 
-
-
-############################################################################
+######################################################################
 # Thumbnail image for the gallery
 
 # sphinx_gallery_thumbnail_number = -1
@@ -75,23 +73,34 @@ ax1 = plt.subplot(2, 2, 1)
 ax1.imshow(cylce_images[0])
 ax1.add_artist(
     AnchoredText(
-        "Stereo", prop=dict(size=20), frameon=True, borderpad=0, loc="upper left"
+        "Stereo",
+        prop=dict(size=20),
+        frameon=True,
+        borderpad=0,
+        loc="upper left",
     )
 )
 ax2 = plt.subplot(2, 2, 2)
 ax2.imshow(vortex_images[0])
 ax2.add_artist(
-    AnchoredText("PIV", prop=dict(size=20), frameon=True, borderpad=0, loc="upper left")
+    AnchoredText(
+        "PIV",
+        prop=dict(size=20),
+        frameon=True,
+        borderpad=0,
+        loc="upper left",
+    )
 )
 
 ax3 = plt.subplot(2, 4, 5)
-ax3.imshow(lfs_images[90+1], cmap="gray")
+ax3.imshow(lfs_images[90 + 1], cmap="gray")
 ax4 = plt.subplot(2, 4, 6)
-ax4.imshow(lfs_images[90+2], cmap="gray")
+ax4.imshow(lfs_images[90 + 2], cmap="gray")
 ax5 = plt.subplot(2, 4, 7)
-ax5.imshow(lfs_images[90+3], cmap="gray")
+ax5.imshow(lfs_images[90 + 3], cmap="gray")
 ax6 = plt.subplot(2, 4, 8)
-ax6.imshow(lfs_images[90+4], cmap="gray")
+ax6.imshow(lfs_images[90 + 4], cmap="gray")
 for ax in [ax1, ax2, ax3, ax4, ax5, ax6]:
     ax.axis("off")
 plt.tight_layout()
+plt.show()
