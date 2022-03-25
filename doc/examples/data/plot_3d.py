8832dcf939440054a396ee7815be3464b48a9aa0
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2022-03-25 11:59:15 +0100

    update colorrange with percentile

--- doc/examples/data/plot_3d.py
+++ doc/examples/data/plot_3d.py
@@ -17,10 +17,23 @@ to test the various functions of scikit-image.
 from skimage import data
 import plotly
 import plotly.express as px
+import numpy as np
 
 img = data.cells3d()[20:]
-fig = px.imshow(-img, facet_col=1, animation_frame=0,
-                binary_string=True, binary_format='jpg')
-fig.layout.annotations[0]['text'] = 'Cell membranes'
-fig.layout.annotations[1]['text'] = 'Nuclei'
+
+upper_limit = np.percentile(img, q=99)
+img = np.clip(img, 0, 1.2 * upper_limit)
+
+# omit some slices that are partially empty
+img = img[5:26]
+
+fig = px.imshow(
+    img,
+    facet_col=1,
+    animation_frame=0,
+    binary_string=True,
+    binary_format="jpg",
+)
+fig.layout.annotations[0]["text"] = "Cell membranes"
+fig.layout.annotations[1]["text"] = "Nuclei"
 plotly.io.show(fig)
