3acdb2d88670f3e5a46480791d9b119c9bb78d1d
alexdesiqueira <alexdesiqueira@igdore.org>
2022-06-15 18:19:06 -0700

    Remove license info from _canny.py

--- skimage/feature/_canny.py
+++ skimage/feature/_canny.py
@@ -3,13 +3,6 @@ canny.py - Canny Edge detector
 
 Reference: Canny, J., A Computational Approach To Edge Detection, IEEE Trans.
     Pattern Analysis and Machine Intelligence, 8:679-714, 1986
-
-Originally part of CellProfiler, code licensed under both GPL and BSD licenses.
-Website: http://www.cellprofiler.org
-Copyright (c) 2003-2009 Massachusetts Institute of Technology
-Copyright (c) 2009-2011 Broad Institute
-All rights reserved.
-Original author: Lee Kamentsky
 """
 
 import numpy as np
