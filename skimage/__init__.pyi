3e55f981a148ec4d800cd2957b82c518c25dda9c
Talley Lambert <talley.lambert@gmail.com>
2022-07-12 09:18:22 -0400

    fix: add __all__ for rexport

--- skimage/__init__.pyi
+++ skimage/__init__.pyi
@@ -1,19 +1,27 @@
-from . import color as color
-from . import data as data
-from . import draw as draw
-from . import exposure as exposure
-from . import feature as feature
-from . import filters as filters
-from . import future as future
-from . import graph as graph
-from . import io as io
-from . import measure as measure
-from . import metrics as metrics
-from . import morphology as morphology
-from . import registration as registration
-from . import restoration as restoration
-from . import segmentation as segmentation
-from . import transform as transform
-from . import util as util
-
+from . import (color, data, draw, exposure, feature, filters, future, graph,
+               io, measure, metrics, morphology, registration, restoration,
+               segmentation, transform, util)
 from .data import data_dir
+
+# required because re-exports must be explicit in stubs:
+# https://mypy.readthedocs.io/en/stable/config_file.html#confval-implicit_reexport
+__all__ = [
+    'color',
+    'data',
+    'draw',
+    'exposure',
+    'feature',
+    'filters',
+    'future',
+    'graph',
+    'io',
+    'measure',
+    'metrics',
+    'morphology',
+    'registration',
+    'restoration',
+    'segmentation',
+    'transform',
+    'util',
+    'data_dir',
+]
