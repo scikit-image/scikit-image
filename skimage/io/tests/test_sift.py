6eb548114be4d205f760bd7c146a1bd5c3dcbcec
Malinda <malinda.dilhara@gmail.com>
2022-08-07 23:01:54 -0700

    Fixing test failure

--- skimage/io/tests/test_sift.py
+++ skimage/io/tests/test_sift.py
@@ -58,8 +58,8 @@ def test_load_surf():
 
     with open(fname, 'r') as f:
         features = load_surf(f)
-        os.remove(fname)
         assert_equal(len(features), 2)
         assert_equal(len(features['data'][0]), 64)
         assert_equal(features['column'][1], 68.5773)
         assert_equal(features['row'][0], 62.0491)
+    os.remove(fname)
