ec84988c70f601123351855ea3594b0c46251ae4
Lars Grüter <lagru@mailbox.org>
2022-08-23 17:51:19 +0200

    Use colormaps instead of deprecated get_cmap
    Top-level functions in mpl.cm are deprecated since maptlotlib version
    3.6. The replacement .ColormapRegistry class is exposed in
    matplotlib.colormaps since version 3.5 [1].
    
    [1] https://github.com/matplotlib/matplotlib/blob/main/doc/api/next_api_changes/deprecations/23668-TC.rst

--- skimage/future/graph/rag.py
+++ skimage/future/graph/rag.py
@@ -448,7 +448,7 @@ def rag_boundary(labels, edge_map, connectivity=2):
     return rag
 
 
-@require("matplotlib", ">=3.3")
+@require("matplotlib", ">=3.5")
 def show_rag(labels, rag, image, border_color='black', edge_width=1.5,
              edge_cmap='magma', img_cmap='bone', in_place=True, ax=None):
     """Show a Region Adjacency Graph on an image.
@@ -500,7 +500,7 @@ def show_rag(labels, rag, image, border_color='black', edge_width=1.5,
     >>> lc = graph.show_rag(labels, g, img)
     >>> cbar = plt.colorbar(lc)
     """
-    from matplotlib import colors, cm
+    from matplotlib import colors, colormaps
     from matplotlib import pyplot as plt
     from matplotlib.collections import LineCollection
 
@@ -518,12 +518,12 @@ def show_rag(labels, rag, image, border_color='black', edge_width=1.5,
         # Ignore the alpha channel
         out = image[:, :, :3]
     else:
-        img_cmap = cm.get_cmap(img_cmap)
+        img_cmap = colormaps[img_cmap]
         out = color.rgb2gray(image)
         # Ignore the alpha channel
         out = img_cmap(out)[:, :, :3]
 
-    edge_cmap = cm.get_cmap(edge_cmap)
+    edge_cmap = colormaps[edge_cmap]
 
     # Handling the case where one node has multiple labels
     # offset is 1 so that regionprops does not ignore 0
