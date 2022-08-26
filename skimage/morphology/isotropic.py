5f3004e926edfcbd96f288277c541a4334e46d58
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2022-08-26 17:29:42 +0200

    Implement isotropic versions of binary morphologic operators
    Closes #6491

---
+++ skimage/morphology/isotropic.py
@@ -0,0 +1,129 @@
+"""
+Binary morphological operations
+"""
+import numpy as np
+from scipy import ndimage as ndi
+
+
+def isotropic_erosion(image, radius, out=None):
+    """Return binary morphological erosion of an image.
+
+    This function returns the same result as binary erosion but performs
+    faster for large circular structuring elements.
+
+    Morphological erosion sets a pixel at ``(i,j)`` to the minimum over all
+    pixels in the neighborhood centered at ``(i,j)``. Erosion shrinks bright
+    regions and enlarges dark regions.
+
+    Parameters
+    ----------
+    image : ndarray
+        Binary input image.
+    radius : float
+        The radius by which regions should be eroded.
+    out : ndarray of bool, optional
+        The array to store the result of the morphology. If None is
+        passed, a new array will be allocated.
+
+    Returns
+    -------
+    eroded : ndarray of bool
+        The result of the morphological erosion taking values in
+        ``[False, True]``.
+    """
+
+    dist = ndi.distance_transform_edt(image)
+    return np.greater(dist, radius, out=out)
+
+
+def isotropic_dilation(image, radius, out=None):
+    """Return binary morphological dilation of an image.
+
+    This function returns the same result as binary dilation but performs
+    faster for large circular structuring elements.
+
+    Morphological dilation sets a pixel at ``(i,j)`` to the maximum over all
+    pixels in the neighborhood centered at ``(i,j)``. Dilation enlarges bright
+    regions and shrinks dark regions.
+
+    Parameters
+    ----------
+    image : ndarray
+        Binary input image.
+    radius : float
+        The radius by which regions should be dilated.
+    out : ndarray of bool, optional
+        The array to store the result of the morphology. If None is
+        passed, a new array will be allocated.
+
+    Returns
+    -------
+    dilated : ndarray of bool
+        The result of the morphological dilation with values in
+        ``[False, True]``.
+    """
+    
+    dist = ndi.distance_transform_edt(image==0)
+    return np.less(dist, radius, out=out)
+
+
+def isotropic_opening(image, radius, out=None):
+    """Return binary morphological opening of an image.
+
+    This function returns the same result as binary opening but performs
+    faster for large circular structuring elements.
+
+    The morphological opening on an image is defined as an erosion followed by
+    a dilation. Opening can remove small bright spots (i.e. "salt") and connect
+    small dark cracks. This tends to "open" up (dark) gaps between (bright)
+    features.
+
+    Parameters
+    ----------
+    image : ndarray
+        Binary input image.
+    radius : float
+        The radius with which the regions should be opened.
+    out : ndarray of bool, optional
+        The array to store the result of the morphology. If None
+        is passed, a new array will be allocated.
+
+    Returns
+    -------
+    opening : ndarray of bool
+        The result of the morphological opening.
+    """
+
+    eroded = isotropic_erosion(image, radius, out=out)
+    return isotropic_dilation(eroded, radius, out=out)
+
+
+def isotropic_closing(image, radius, out=None):
+    """Return binary morphological closing of an image.
+
+    This function returns the same result as binary closing but performs
+    faster for large circular structuring elements.
+
+    The morphological closing on an image is defined as a dilation followed by
+    an erosion. Closing can remove small dark spots (i.e. "pepper") and connect
+    small bright cracks. This tends to "close" up (dark) gaps between (bright)
+    features.
+
+    Parameters
+    ----------
+    image : ndarray
+        Binary input image.
+    radius : float
+        The radius with which the regions should be closed.
+    out : ndarray of bool, optional
+        The array to store the result of the morphology. If None,
+        is passed, a new array will be allocated.
+
+    Returns
+    -------
+    closing : ndarray of bool
+        The result of the morphological closing.
+    """
+
+    dilated = isotropic_dilation(image, radius, out=out)
+    return isotropic_erosion(dilated, radius, out=out)
---
+++ skimage/morphology/tests/test_isotropic.py
@@ -0,0 +1,65 @@
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from skimage import color, data, morphology
+from skimage.morphology import binary, isotropic
+from skimage.util import img_as_bool
+
+img = color.rgb2gray(data.astronaut())
+bw_img = img > 100 / 255.
+
+def iou(a,b):
+    denom = (a | b).sum()
+    if denom == 0:
+        return 1
+
+    return (a & b).sum() / denom
+
+
+def test_non_square_image():
+    isotropic_res = isotropic.isotropic_erosion(bw_img[:100, :200], 3)
+    binary_res = img_as_bool(binary.binary_erosion(bw_img[:100, :200], morphology.disk(3)))
+    assert iou(isotropic_res, binary_res) > .95
+
+
+def test_isotropic_erosion():
+    isotropic_res = isotropic.isotropic_erosion(bw_img, 3)
+    binary_res = img_as_bool(binary.binary_erosion(bw_img, morphology.disk(3)))
+    assert iou(isotropic_res, binary_res) > .95
+
+
+def test_isotropic_dilation():
+    isotropic_res = isotropic.isotropic_dilation(bw_img, 3)
+    binary_res = img_as_bool(binary.binary_dilation(bw_img, morphology.disk(3)))
+    assert iou(isotropic_res, binary_res) > .95
+
+
+def test_isotropic_closing():
+    isotropic_res = isotropic.isotropic_closing(bw_img, 3)
+    binary_res = img_as_bool(binary.binary_closing(bw_img, morphology.disk(3)))
+    assert iou(isotropic_res, binary_res) > .95
+
+
+def test_isotropic_opening():
+    isotropic_res = isotropic.isotropic_opening(bw_img, 3)
+    binary_res = img_as_bool(binary.binary_opening(bw_img, morphology.disk(3)))
+    assert iou(isotropic_res, binary_res) > .95
+
+
+def test_footprint_overflow():
+    img = np.zeros((20, 20), dtype=bool)
+    img[2:19, 2:19] = True
+    isotropic_res = isotropic.isotropic_erosion(img, 9)
+    binary_res = img_as_bool(binary.binary_erosion(img, morphology.disk(9)))
+    assert iou(isotropic_res, binary_res) > .95
+
+
+def test_out_argument():
+    for func in (isotropic.isotropic_erosion, isotropic.isotropic_dilation):
+        radius = 3
+        img = np.ones((10, 10))
+        out = np.zeros_like(img)
+        out_saved = out.copy()
+        func(img, radius, out=out)
+        assert np.any(out != out_saved)
+        assert_array_equal(out, func(img, radius))
