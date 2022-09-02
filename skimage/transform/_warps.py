b77ce3c31871154762f1e28a51d9fdb85e2481e6
Riadh <r.fezzani@vitadx.com>
2022-09-02 15:01:39 +0200

    Add doc

--- skimage/transform/_warps.py
+++ skimage/transform/_warps.py
@@ -118,6 +118,8 @@ def resize(image, output_shape, order=None, mode='reflect', cval=0, clip=True,
         to downsampling. It is crucial to filter when downsampling
         the image to avoid aliasing artifacts. If not specified, it is set to
         True when downsampling an image whose data type is not bool.
+        It is also set to False when using nearest neighbor interpolation
+        (``order`` == 0) with integer input data type.
     anti_aliasing_sigma : {float, tuple of floats}, optional
         Standard deviation for Gaussian filtering used when anti-aliasing.
         By default, this value is chosen as (s - 1) / 2 where s is the
