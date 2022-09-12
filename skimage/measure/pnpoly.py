e8e71fbaa2cfd4d182782c7db9d676ac848e78b4
Dudu Lasry <dudu.lasry@viz.ai>
2022-09-12 08:31:16 +0000

    Add docstrings to convex_hull_image, grid_points_in_poly and _grid_points_in_poly

--- skimage/measure/pnpoly.py
+++ skimage/measure/pnpoly.py
@@ -15,6 +15,10 @@ def grid_points_in_poly(shape, verts, return_labels=False):
         Specify the V vertices of the polygon, sorted either clockwise
         or anti-clockwise. The first point may (but does not need to be)
         duplicated.
+    return_labels: bool, optional
+        If ``True``, a label mask will also be returned along with the default
+        boolean mask. The possible labels are: O - outside, 1 - inside,
+        2 - vertex, 3 - edge.
 
     See Also
     --------
@@ -24,6 +28,9 @@ def grid_points_in_poly(shape, verts, return_labels=False):
     -------
     mask : (M, N) ndarray of bool
         True where the grid falls inside the polygon.
+    labels: (M, N) ndarray of labels (integers)
+        Labels array, with pixels having a label between 0 and 3.
+        This is only returned if `return_labels` is set to True.
 
     """
     return _grid_points_in_poly(shape, verts, return_labels=return_labels)
