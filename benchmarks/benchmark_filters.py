d549c794a0f6a2e9983dd057d4743b57a3270b55
Teemu Kumpumäki <teemu.kumpumaki@deltacygnilabs.com>
2022-09-20 14:46:25 +0300

    Reduce ridge filters memory footprints (#6509)
    * Reduce ridge filters memory footprints by not storing intermediate result and taking max between iterations.
    
    * Clean PEP8 whitespace issue.
    
    * Add memory benchmarks for ridge filters
    
    * Reformat indent in math expression
    
    Co-authored-by: Riadh Fezzani <rfezzani@gmail.com>
    
    * Add CPU benchmarks for ridge filters
    
    * Remake memory footprint fix.
    
    * Use zeros_like where possible
    
    Co-authored-by: Riadh Fezzani <rfezzani@gmail.com>
    
    Co-authored-by: Lars Grüter <lagru@mailbox.org>
    Co-authored-by: Lars Grüter <lagru+github@mailbox.org>
    Co-authored-by: Riadh Fezzani <rfezzani@gmail.com>

--- benchmarks/benchmark_filters.py
+++ benchmarks/benchmark_filters.py
@@ -2,7 +2,7 @@
 # https://asv.readthedocs.io/en/latest/writing_benchmarks.html
 import numpy as np
 
-from skimage import data, filters
+from skimage import data, filters, color
 from skimage.filters.thresholding import threshold_li
 
 
@@ -103,3 +103,50 @@ class ThresholdLi:
 
     def time_float32_image(self):
         result1 = threshold_li(self.image_float32)
+
+
+class RidgeFilters:
+    """Benchmark ridge filters in scikit-image."""
+
+    def setup(self):
+        # Ensure memory footprint of lazy import is included in reference
+        self._ = filters.meijering, filters.sato, filters.frangi, filters.hessian
+        self.image = color.rgb2gray(data.retina())
+
+    def peakmem_setup(self):
+        """peakmem includes the memory used by setup.
+        Peakmem benchmarks measure the maximum amount of RAM used by a
+        function. However, this maximum also includes the memory used
+        by ``setup`` (as of asv 0.2.1; see [1]_)
+        Measuring an empty peakmem function might allow us to disambiguate
+        between the memory used by setup and the memory used by slic (see
+        ``peakmem_slic_basic``, below).
+        References
+        ----------
+        .. [1]: https://asv.readthedocs.io/en/stable/writing_benchmarks.html#peak-memory
+        """
+        pass
+
+    def time_meijering(self):
+        filters.meijering(self.image)
+
+    def peakmem_meijering(self):
+        filters.meijering(self.image)
+
+    def time_sato(self):
+        filters.sato(self.image)
+
+    def peakmem_sato(self):
+        filters.sato(self.image)
+
+    def time_frangi(self):
+        filters.frangi(self.image)
+
+    def peakmem_frangi(self):
+        filters.frangi(self.image)
+
+    def time_hessian(self):
+        filters.hessian(self.image)
+
+    def peakmem_hessian(self):
+        filters.hessian(self.image)
