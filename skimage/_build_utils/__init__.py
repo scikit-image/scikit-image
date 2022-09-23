14dbede2dfb728eab088f1c366ad108194567083
Matthew Brett <matthew.brett@gmail.com>
2022-09-23 01:55:29 +0100

    Clean out __init__.py for _build_utils

--- skimage/_build_utils/__init__.py
+++ skimage/_build_utils/__init__.py
@@ -1,10 +1,3 @@
-import os
-
-import numpy as np
-from ._fortran import *
-from .system_info import combine_dict
-
-
 # Don't use the deprecated NumPy C API. Define this to a fixed version instead of
 # NPY_API_VERSION in order not to break compilation for released SciPy versions
 # when NumPy introduces a new deprecation. Use in setup.py::
@@ -14,9 +7,6 @@ from .system_info import combine_dict
 numpy_nodepr_api = dict(define_macros=[("NPY_NO_DEPRECATED_API",
                                         "NPY_1_9_API_VERSION")])
 
-
-def uses_blas64():
-    return (os.environ.get("NPY_USE_BLAS_ILP64", "0") != "0")
 
 def import_file(folder, module_name):
     """Import a file directly, avoiding importing scipy"""
@@ -28,8 +18,3 @@ def import_file(folder, module_name):
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
-
-
-from scipy._lib._testutils import PytestTester
-test = PytestTester(__name__)
-del PytestTester
