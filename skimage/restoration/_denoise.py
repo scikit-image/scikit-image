340b7e93dea186af5d59227c5ded5a26a36b053e
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2022-09-26 21:25:28 +0200

    Rewrite docstring for denoise_tv_chambolle

--- skimage/restoration/_denoise.py
+++ skimage/restoration/_denoise.py
@@ -437,32 +437,41 @@ def _denoise_tv_chambolle_nd(image, weight=0.1, eps=2.e-4, max_num_iter=200):
 @utils.deprecate_multichannel_kwarg(multichannel_position=4)
 def denoise_tv_chambolle(image, weight=0.1, eps=2.e-4, max_num_iter=200,
                          multichannel=False, *, channel_axis=None):
-    """Perform total-variation denoising on n-dimensional images.
+    r"""Perform total variation denoising in nD.
+
+    This function is an implementation of the algorithm proposed by Chambolle
+    in [1]_ to solve the Rudin--Osher--Fatemi minimization problem:
+    :math:`\min_{u} \sum_{i=0}^{N-1} \left( \left| \nabla{u_i} \right| + \frac{\lambda}{2}(f_i - u_i)^2 \right)`
+    where :math:`f` denotes the input data (noisy image) and :math:`\lambda`
+    is a positive parameter. The first term of this cost function is the total
+    variation; the second term represents data fidelity. As :math:`\lambda \to 0`,
+    the total variation term dominates, forcing the solution to have smaller
+    total variation, at the expense of looking less like the input data.
 
     Parameters
     ----------
-    image : ndarray of ints, uints or floats
-        Input data to be denoised. `image` can be of any numeric type,
-        but it is cast into an ndarray of floats for the computation
-        of the denoised image.
+    image : ndarray
+        Input image to be denoised. If its dtype is not float, it gets
+        converted with ``img_as_float``.
     weight : float, optional
-        Denoising weight. The greater `weight`, the more denoising (at
-        the expense of fidelity to `input`).
+        Denoising weight. It is equal to :math:`\frac{1}{\lambda}`. Therefore,
+        the greater `weight`, the more denoising (at
+        the expense of fidelity to `image`).
     eps : float, optional
-        Relative difference of the value of the cost function that
+        Absolute value of relative difference of the cost function that
         determines the stop criterion. The algorithm stops when:
 
-            (E_(n-1) - E_n) < eps * E_0
+            |E_(n-1) - E_n| < eps * |E_0|
 
     max_num_iter : int, optional
         Maximal number of iterations used for the optimization.
     multichannel : bool, optional
-        Apply total-variation denoising separately for each channel. This
-        option should be true for color images, otherwise the denoising is
+        Whether to apply total-variation denoising separately to each channel.
+        Should be True for color images, otherwise the denoising is
         also applied in the channels dimension. This argument is deprecated:
         specify `channel_axis` instead.
     channel_axis : int or None, optional
-        If None, the image is assumed to be a grayscale (single channel) image.
+        If None, the image is assumed to be grayscale (single-channel).
         Otherwise, this parameter indicates which axis of the array corresponds
         to channels.
 
@@ -476,25 +485,22 @@ def denoise_tv_chambolle(image, weight=0.1, eps=2.e-4, max_num_iter=200,
 
     Notes
     -----
-    Make sure to set the multichannel parameter appropriately for color images.
+    Make sure to set the `channel_axis` parameter appropriately for color
+    images.
 
-    The principle of total variation denoising is explained in
-    https://en.wikipedia.org/wiki/Total_variation_denoising
-
-    The principle of total variation denoising is to minimize the
-    total variation of the image, which can be roughly described as
-    the integral of the norm of the image gradient. Total variation
-    denoising tends to produce "cartoon-like" images, that is,
+    The principle of total-variation denoising is explained in [2]_.
+    It is about minimizing the total variation of an image,
+    which can be roughly described as
+    the integral of the norm of the image gradient. Total-variation
+    denoising tends to produce cartoon-like images, that is,
     piecewise-constant images.
-
-    This code is an implementation of the algorithm of Rudin, Fatemi and Osher
-    that was proposed by Chambolle in [1]_.
 
     References
     ----------
     .. [1] A. Chambolle, An algorithm for total variation minimization and
            applications, Journal of Mathematical Imaging and Vision,
            Springer, 2004, 20, 89-97.
+    .. [2] https://en.wikipedia.org/wiki/Total_variation_denoising
 
     Examples
     --------
