9955259b8592cf2d8d7580994d63306cede006b3
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2022-09-27 11:37:04 +0200

    Test spacing parameter

--- skimage/morphology/tests/test_isotropic.py
+++ skimage/morphology/tests/test_isotropic.py
@@ -21,6 +21,25 @@ def test_isotropic_erosion():
     binary_res = img_as_bool(binary.binary_erosion(bw_img, morphology.disk(3)))
     assert_array_equal(isotropic_res, binary_res)
 
+def _disk_with_spacing(radius, dtype=np.uint8, *, strict_radius=True, spacing=None):
+    # Identical to morphology.disk, but with a spacing parameter and without decomposition.
+    # This is different from morphology.ellipse which produces a slightly different footprint.
+    L = np.arange(-radius, radius + 1)
+    X, Y = np.meshgrid(L, L)
+
+    if spacing is not None:
+        X *= spacing[1]
+        Y *= spacing[0]
+
+    if not strict_radius:
+        radius += 0.5
+    return np.array((X ** 2 + Y ** 2) <= radius ** 2, dtype=dtype)
+
+def test_isotropic_erosion_spacing():
+    isotropic_res = isotropic.isotropic_dilation(bw_img, 6, spacing=(1,2))
+    binary_res = img_as_bool(binary.binary_dilation(bw_img, _disk_with_spacing(6, spacing=(1,2))))
+    assert_array_equal(isotropic_res, binary_res)
+
 
 def test_isotropic_dilation():
     isotropic_res = isotropic.isotropic_dilation(bw_img, 3)
