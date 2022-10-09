1ca971ae9cb20d6b51726771375d2c149b767c1f
Julio Faracco <jcfaracco@gmail.com>
2022-10-09 03:46:20 +0000

    tests: Add test for new 'mean' GLCM graycoprops
    This commit just enable a test for new 'mean' graycopropos value.
    
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- skimage/feature/tests/test_texture.py
+++ skimage/feature/tests/test_texture.py
@@ -210,6 +210,12 @@ class TestGLCM():
         np.testing.assert_almost_equal(energy[0, 0], 0.71953255)
         np.testing.assert_almost_equal(energy[1, 0], 0.41176470)
 
+    def test_mean(self):
+        result = graycomatrix(self.image, [1, 2], [0], 4, normed=True,
+                              symmetric=True)
+        mean = graycoprops(result, 'mean')
+        np.testing.assert_almost_equal(mean[0, 0], 1.2916666)
+
     def test_uniform_properties(self):
         im = np.ones((4, 4), dtype=np.uint8)
         result = graycomatrix(im, [1, 2, 8], [0, np.pi / 2], 4, normed=True,
