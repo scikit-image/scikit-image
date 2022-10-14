df0e54d88ec91901d59ea4f041a13da4dbfa7757
Lars Grüter <lagru@mailbox.org>
2022-10-14 13:31:35 +0200

    Format imports consistently
    Format the PYI files consistently with filters/__init__.pyi by applying
    black -S and isort --profile black.

--- skimage/__init__.pyi
+++ skimage/__init__.pyi
@@ -15,13 +15,27 @@ submodules = [
     'restoration',
     'segmentation',
     'transform',
-    'util'
+    'util',
 ]
 
-__all__ = submodules + [
-    '__version__'
-]
+__all__ = submodules + ['__version__']
 
-from . import (color, data, draw, exposure, feature, filters, future,
-               graph, io, measure, metrics, morphology, registration,
-               restoration, segmentation, transform, util)
+from . import (
+    color,
+    data,
+    draw,
+    exposure,
+    feature,
+    filters,
+    future,
+    graph,
+    io,
+    measure,
+    metrics,
+    morphology,
+    registration,
+    restoration,
+    segmentation,
+    transform,
+    util,
+)
