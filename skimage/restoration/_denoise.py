c8adb416c10abf0e9378d57bd089497ca9097ba6
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2022-10-19 17:41:40 +0200

    Update description for deprecated multichannel argument

--- skimage/restoration/_denoise.py
+++ skimage/restoration/_denoise.py
@@ -310,9 +310,9 @@ def denoise_tv_bregman(image, weight=5.0, max_num_iter=100, eps=1e-3,
            ``channel_axis`` was added in 0.19.
     multichannel : bool, optional
         Whether or not to apply denoising separately to each channel.
-        Should always be set to ``True`` for color images, otherwise the denoising is
-        also applied in the channels dimension. This argument is deprecated:
-        specify `channel_axis` instead.
+        Should always be set to ``True`` for color images, otherwise the
+        denoising is also applied in the channels dimension.
+        This argument is deprecated: specify `channel_axis` instead.
 
     Returns
     -------
@@ -503,10 +503,10 @@ def denoise_tv_chambolle(image, weight=0.1, eps=2.e-4, max_num_iter=200,
     max_num_iter : int, optional
         Maximal number of iterations used for the optimization.
     multichannel : bool, optional
-        Whether to apply total variation denoising separately to each channel.
-        Should be ``True`` for color images, otherwise the denoising is
-        also applied in the channels dimension. This argument is deprecated:
-        specify `channel_axis` instead.
+        Whether or not to apply denoising separately to each channel.
+        Should always be set to ``True`` for color images, otherwise the
+        denoising is also applied in the channels dimension.
+        This argument is deprecated: specify `channel_axis` instead.
     channel_axis : int or None, optional
         If ``None``, the image is assumed to be grayscale (single-channel).
         Otherwise, this parameter indicates which axis of the array corresponds
