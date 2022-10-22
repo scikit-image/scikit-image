a2534d51f57b3e88b8eda56a3c5f9bddba0cdfd9
Gregory Lee <grlee77@gmail.com>
2022-10-22 10:40:46 -0400

    apply flake8 and isort fixes to plot_colocalization_metrics

--- doc/examples/applications/plot_colocalization_metrics.py
+++ doc/examples/applications/plot_colocalization_metrics.py
@@ -7,8 +7,9 @@ In this example, we demonstrate the use of different metrics to assess the
 colocalization of two different image channels.
 
 Colocalization can be split into two different concepts:
-1. Co-occurence: what proportion of a substance is localised to a particular area
-2. Correlation: what is the relationship in intensity between two substances
+1. Co-occurence: What proportion of a substance is localised to a particular
+area?
+2. Correlation: What is the relationship in intensity between two substances?
 
 """
 
@@ -25,17 +26,18 @@ Colocalization can be split into two different concepts:
 # and assume that whatever is not in the nucleus is in the cytoplasm.
 # The protein, "Protein A", will be simulated as blobs and segmented.
 
+import random
+
 import matplotlib.pyplot as plt
 import numpy as np
-import random
-from skimage import data, measure, filters, segmentation
-from scipy import ndimage as ndi
 from matplotlib.colors import LinearSegmentedColormap
+from scipy import ndimage as ndi
+from skimage import data, filters, measure, segmentation
 
 random.seed(36)
 
 # segment nucleus
-nucleus = data.protein_transport()[0,0,:,:180]
+nucleus = data.protein_transport()[0, 0, :, :180]
 smooth = filters.gaussian(nucleus, sigma=1.5)
 thresh = smooth > filters.threshold_otsu(smooth)
 fill = ndi.binary_fill_holes(thresh)
@@ -50,7 +52,7 @@ for blob_seed in range(10):
                               blob_size_fraction=0.5,
                               volume_fraction=(50/(180**2)),
                               seed=blob_seed)
-    blobs_image = filters.gaussian(blobs, sigma=1.5)*random.randint(50,256)
+    blobs_image = filters.gaussian(blobs, sigma=1.5)*random.randint(50, 256)
     proteinA += blobs_image
     proteinA_seg += blobs
 
@@ -62,17 +64,18 @@ ax[0, 0].set_title('Nucleus')
 ax[0, 1].imshow(nucleus_seg, cmap=plt.cm.gray)
 ax[0, 1].set_title('Nucleus segmentation')
 
-ax[1, 0].imshow(proteinA, cmap=LinearSegmentedColormap.from_list("", ["black", "magenta"]))
+black_magenta = LinearSegmentedColormap.from_list("", ["black", "magenta"])
+ax[1, 0].imshow(proteinA, cmap=black_magenta)
 ax[1, 0].set_title('Protein A')
 
-ax[1, 1].imshow(proteinA_seg, cmap=LinearSegmentedColormap.from_list("", ["black", "magenta"]))
+ax[1, 1].imshow(proteinA_seg, cmap=black_magenta)
 ax[1, 1].set_title('Protein A segmentation')
 
-ax[2, 0].imshow(proteinA, cmap=LinearSegmentedColormap.from_list("", ["black", "magenta"]))
+ax[2, 0].imshow(proteinA, cmap=black_magenta)
 ax[2, 0].imshow(nucleus_seg, cmap=plt.cm.gray, alpha=0.2)
 ax[2, 0].set_title('Protein A with nucleus overlaid')
 
-ax[2, 1].imshow(proteinA_seg, cmap=LinearSegmentedColormap.from_list("", ["black", "magenta"]))
+ax[2, 1].imshow(proteinA_seg, cmap=black_magenta)
 ax[2, 1].imshow(nucleus_seg, cmap=plt.cm.gray, alpha=0.2)
 ax[2, 1].set_title('Protein A segmentation with nucleus overlaid')
 
@@ -96,11 +99,11 @@ measure.intersection_coeff(proteinA_seg, nucleus_seg)
 # The overlap coefficient assumes that the area of protein segmentation
 # corresponds to the concentration of that protein - with larger areas
 # indicating more protein. As the resolution of images are usually too small to
-# make out individual proteins, they can clump together within one pixel, making
-# the intensity of that pixel brighter. So to better capture the the protein
-# concentration, we may choose to determine what proportion of the *intensity*
-# of the protein channel is inside the nucleus. This metric is known as Manders'
-# Colocalization Coefficient.
+# make out individual proteins, they can clump together within one pixel,
+# making the intensity of that pixel brighter. So to better capture the the
+# protein concentration, we may choose to determine what proportion of the
+# *intensity* of the protein channel is inside the nucleus. This metric is
+# known as Manders' Colocalization Coefficient.
 #
 # In this image, while there are a lot of Protein A spots within the nucleus
 # they are dim compared to some of the spots outside the nucleus, so the MCC is
@@ -108,10 +111,10 @@ measure.intersection_coeff(proteinA_seg, nucleus_seg)
 
 measure.manders_coloc_coeff(proteinA, nucleus_seg)
 
-# After choosing a co-occurence metric, we can apply the same process to control
-# images. If no control images are available, the Costes method could be used to
-# compare the MCC value of the original image with that of the randomly
-# scrambled image. Information about this method is given in [1]_.
+# After choosing a co-occurence metric, we can apply the same process to
+# control images. If no control images are available, the Costes method could
+# be used to compare the MCC value of the original image with that of the
+# randomly scrambled image. Information about this method is given in [1]_.
 
 # .. [1] J. S. Aaron, A. B. Taylor and T.-L. Chew, Image co-localization –
 #        co-occurrence versus correlation. J Cell Sci 1 February 2018
@@ -133,10 +136,11 @@ proteinB = proteinA + rng.normal(loc=100, scale=10, size=proteinA.shape)
 # plot images
 fig, ax = plt.subplots(1, 2, figsize=(8, 8), sharey=True)
 
-ax[0].imshow(proteinA, cmap=LinearSegmentedColormap.from_list("", ["black", "magenta"]))
+ax[0].imshow(proteinA, cmap=black_magenta)
 ax[0].set_title('Protein A')
 
-ax[1].imshow(proteinB, cmap=LinearSegmentedColormap.from_list("", ["black", "cyan"]))
+black_cyan = LinearSegmentedColormap.from_list("", ["black", "cyan"])
+ax[1].imshow(proteinB, cmap=black_cyan)
 ax[1].set_title('Protein B')
 
 for a in ax.ravel():
