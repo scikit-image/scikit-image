f25ae8557b1d68fb3093dc7bdb2bb7fd5cc7fe0f
Gregory Lee <grlee77@gmail.com>
2022-11-28 12:36:18 -0500

    ignore DeprecationWarning from Cascade class

--- skimage/feature/tests/test_cascade.py
+++ skimage/feature/tests/test_cascade.py
@@ -1,5 +1,8 @@
+import warnings
+
 import skimage.data as data
 from skimage.feature import Cascade
+
 
 
 def test_detector_astronaut():
@@ -8,7 +11,13 @@ def test_detector_astronaut():
     trained_file = data.lbp_frontal_face_cascade_filename()
 
     # Initialize the detector cascade.
-    detector = Cascade(trained_file)
+    with warnings.catch_warnings():
+        # TODO: Ignore warning on overflow of negative values to positive in
+        #       cast to uint32. Should determine the cause of negative values
+        #       in trained_file
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+        detector = Cascade(trained_file)
 
     img = data.astronaut()
 
