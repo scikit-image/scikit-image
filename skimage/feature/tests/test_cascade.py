4b21720bd34b163d78b22dc66679619112c27992
Gregory Lee <grlee77@gmail.com>
2022-11-28 22:00:50 -0500

    use explicit astype call to avoid warning in Cascade
    Co-authored-by: Brett M. Morris <morrisbrettm@gmail.com>

--- skimage/feature/tests/test_cascade.py
+++ skimage/feature/tests/test_cascade.py
@@ -1,5 +1,3 @@
-import warnings
-
 import skimage.data as data
 from skimage.feature import Cascade
 
@@ -11,13 +9,7 @@ def test_detector_astronaut():
     trained_file = data.lbp_frontal_face_cascade_filename()
 
     # Initialize the detector cascade.
-    with warnings.catch_warnings():
-        # TODO: Ignore warning on overflow of negative values to positive in
-        #       cast to uint32. Should determine the cause of negative values
-        #       in trained_file
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-        detector = Cascade(trained_file)
+    detector = Cascade(trained_file)
 
     img = data.astronaut()
 
