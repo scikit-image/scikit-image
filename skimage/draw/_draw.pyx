f0f2f2ffa8a01ebb3c4285b351f0a9c97c7c869f
Henry Pinkard <7969470+henrypinkard@users.noreply.github.com>
2022-12-18 12:42:48 -0800

    remove redundant code

--- skimage/draw/_draw.pyx
+++ skimage/draw/_draw.pyx
@@ -230,9 +230,9 @@ def _polygon(r, c, shape, offset):
         offset = (max(0, offset[0]), max(0, offset[1]))
 
     cdef Py_ssize_t nr_verts = c.shape[0]
-    cdef Py_ssize_t minr = int(max(offset[0] if offset is not None else 0, max(0, r.min())))
+    cdef Py_ssize_t minr = int(max(offset[0], max(0, r.min())))
     cdef Py_ssize_t maxr = int(ceil(r.max()))
-    cdef Py_ssize_t minc = int(max(offset[1] if offset is not None else 0, max(0, c.min())))
+    cdef Py_ssize_t minc = int(max(offset[1], max(0, c.min())))
     cdef Py_ssize_t maxc = int(ceil(c.max()))
 
     # make sure output coordinates do not exceed image size
