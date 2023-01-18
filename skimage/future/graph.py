9cd6389d670ca5b9a1245ed6197b9f69b40cd273
Lars Grüter <lagru@mailbox.org>
2023-01-18 11:46:39 +0100

    Reword error message as suggested by Stéfan
    Co-authored-by: Stefan van der Walt <sjvdwalt@gmail.com>

--- skimage/future/graph.py
+++ skimage/future/graph.py
@@ -1,6 +1,6 @@
 # Remove this file in the release after v0.20
 
 raise ModuleNotFoundError(
-    "The API in skimage.future.graph was moved into skimage.graph in v0.20. "
-    "Please update your import paths accordingly."
+    "The `skimage.future.graph` submodule was moved to `skimage.graph` in "
+    "v0.20. Please update your import paths accordingly."
 )
--- skimage/future/tests/test_graph.py
+++ skimage/future/tests/test_graph.py
@@ -2,7 +2,10 @@ import pytest
 
 
 def test_future_graph_import_error():
-    error_msg = "The API in skimage.future.graph was moved into skimage.graph"
+    error_msg = (
+        "The `skimage.future.graph` submodule was moved to `skimage.graph` in "
+        "v0.20. Please update your import paths accordingly."
+    )
     with pytest.raises(ModuleNotFoundError, match=error_msg):
         import skimage.future.graph
 
