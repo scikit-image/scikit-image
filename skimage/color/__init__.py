21ec0ff227ae2c96d3886769d94136afd900f509
Lars Grüter <lagru@mailbox.org>
2023-01-21 12:22:49 +0100

    Expose color.get_xyz_coords in public API
    For now it's mentioned once in the docstring of `skimage.color.lab2rgb`
    but it's probably intended to be part of our public API. So it should
    be part of our public API documentation too!
    
    See also gh-6688 which directly references this function.

--- skimage/color/__init__.py
+++ skimage/color/__init__.py
@@ -1,4 +1,5 @@
 from .colorconv import (convert_colorspace,
+                        get_xyz_coords,
                         rgba2rgb,
                         rgb2hsv,
                         hsv2rgb,
@@ -66,6 +67,7 @@ from .delta_e import (deltaE_cie76,
 
 
 __all__ = ['convert_colorspace',
+           'get_xyz_coords',
            'rgba2rgb',
            'rgb2hsv',
            'hsv2rgb',
