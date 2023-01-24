6131bbb41810d26b59826594d7e31ffcaf244303
Lars Grüter <lagru@mailbox.org>
2023-01-24 22:51:28 +0100

    Re-add deprecated function to __init__.py
    While it is deprecated, removing it from the module level would break
    code for users who use the import path skimage.filters.DEPRECATED_FUNC.
    The whole purpose of a deprecation cycle is to give users the chance to
    avoid broken code.
    
    I think it's fine to keep them out of __all__ so that users no longer
    discover them through our documentation.

--- skimage/filters/__init__.pyi
+++ skimage/filters/__init__.pyi
@@ -80,8 +80,20 @@ from ._edges import (
     sobel_h,
     sobel_v,
 )
-from ._lpi_filter import LPIFilter2D, filter_forward, filter_inverse, wiener
-from ._ridges import frangi, hessian, meijering, sato
+from ._lpi_filter import (
+    filter_forward,
+    filter_inverse,
+    inverse,
+    LPIFilter2D,
+    wiener,
+)
+from ._ridges import (
+    compute_hessian_eigenvalues,
+    frangi,
+    hessian,
+    meijering,
+    sato,
+)
 from ._thresholding import (
     apply_hysteresis_threshold,
     threshold_isodata,
