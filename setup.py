c26bd6c35771b7e1d3f3b99ad6b99ac1f42ea71a
Jarrod Millman <jarrod.millman@gmail.com>
2023-01-27 15:51:52 -0800

    Python 3.11 released (#6709)

--- setup.py
+++ setup.py
@@ -232,6 +232,7 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
             'Programming Language :: Python :: 3 :: Only',
             'Topic :: Scientific/Engineering',
             'Operating System :: Microsoft :: Windows',
