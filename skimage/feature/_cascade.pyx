4bd4f7d44e0940e5aa07ae17b1571f4fe7fc0fb7
Lars Grüter <lagru@mailbox.org>
2023-02-03 21:09:46 +0100

    Fix struct warning in detect_multi_scale (#6728)
    Fixes the warnings:
    
    warning: skimage/feature/_cascade.pyx:753:49: Not all members given for struct 'Detection'
    warning: skimage/feature/_cascade.pyx:753:49: Not all members given for struct 'Detection'
    
    The memory for the Detection struct was already allocated on the stack
    of this function, so we can just directly assign to its members. I also
    tried the syntax
    
        new_detection = Detection(
            r=current_row
            c=current_col
            width=current_width
            height=current_height
        )
    
    with and without parameter names. However, that leads to the following
    compilation error:
    
    /home/lg/Res/scikit-image/skimage/feature/_cascade.pyx:753:50: Operation not allowed without gil

--- skimage/feature/_cascade.pyx
+++ skimage/feature/_cascade.pyx
@@ -750,7 +750,6 @@ cdef class Cascade:
 
                     if result:
 
-                        new_detection = Detection()
                         new_detection.r = current_row
                         new_detection.c = current_col
                         new_detection.width = current_width
