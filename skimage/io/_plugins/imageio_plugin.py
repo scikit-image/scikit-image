c9ca7c026e66eeaaf0f62bc7be3f00b0484824ce
Stefan van der Walt <stefanv@berkeley.edu>
2023-02-16 16:04:33 -0800

    Use imageio v3 API
    In response to
    https://discuss.scientific-python.org/t/full-support-for-4-channel-images-in-imsave/651/9
    
    Thanks for the prod, @FirefoxMetzger

--- skimage/io/_plugins/imageio_plugin.py
+++ skimage/io/_plugins/imageio_plugin.py
@@ -3,11 +3,7 @@ __all__ = ['imread', 'imsave']
 from functools import wraps
 import numpy as np
 
-try:
-    # Try using the v2 API directly to avoid a warning from imageio >= 2.16.2
-    from imageio.v2 import imread as imageio_imread, imsave
-except ImportError:
-    from imageio import imread as imageio_imread, imsave
+from imageio.v3 import imread as imageio_imread, imsave
 
 
 @wraps(imageio_imread)
