9ef5415dd1914c5bd819126189235b2f548faf25
Lars Grüter <lagru@mailbox.org>
2023-02-19 16:20:52 +0100

    Ignore labels which aren't surface

--- skimage/morphology/misc.py
+++ skimage/morphology/misc.py
@@ -391,13 +391,16 @@ def remove_near_objects(
         # Priority value for the background (ID 0) is not expected
         priority = priority
 
-    # TODO Safely ignore points that don't lie on an object's surface
+    # Safely ignore points that don't lie on an object's surface
     # This reduces the size of the KDTree and the number of points that
     # need to be evaluated
-    # out[ndi.binary_erosion(out, structure=footprint)] = 0
+    footprint = np.ones((3,) * out.ndim, dtype=out.dtype)
+    surfaces = out * footprint.sum()
+    surfaces -= ndi.convolve(out, footprint)  # edges are non-zero
 
-    # Create index, that will define the iteration order of pixels later
-    indices = np.nonzero(out.ravel())[0]
+    # Create index, that will define the iteration order of pixels later.
+    indices = np.nonzero(surfaces.ravel())[0]
+    del surfaces
     # Sort by label ID first, so that IDs of the same object are contiguous
     # in the sorted index. This allows fast discovery of the whole object by
     # simple iteration up or down the index!
