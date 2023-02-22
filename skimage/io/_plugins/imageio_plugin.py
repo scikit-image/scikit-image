cade22be7e9580e1edfccce59831e20db09a29a9
Stefan van der Walt <stefanv@berkeley.edu>
2023-02-22 12:41:44 -0800

    imageio: use imwrite instead of imsave

--- skimage/io/_plugins/imageio_plugin.py
+++ skimage/io/_plugins/imageio_plugin.py
@@ -3,7 +3,7 @@ __all__ = ['imread', 'imsave']
 from functools import wraps
 import numpy as np
 
-from imageio.v3 import imread as imageio_imread, imsave
+from imageio.v3 import imread as imageio_imread, imwrite as imsave
 
 
 @wraps(imageio_imread)
