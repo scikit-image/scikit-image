32a5b5373288db13abb673c8e7541fd7c23fde19
ana42742 <amit.ritu.ananya@gmail.com>
2023-03-08 11:31:02 +0530

    Update docstring of skimage.future

--- skimage/future/__init__.py
+++ skimage/future/__init__.py
@@ -1,11 +1,11 @@
 """Functionality with an experimental API.
 
 .. warning::
-Although you can count on the functions in this package being
-around in the future, the API may change with any version update
-**and will not follow the skimage two-version deprecation path**.
-Therefore, use the functions herein with care, and do not use them
-in production code that will depend on updated skimage versions.
+    Although you can count on the functions in this package being
+    around in the future, the API may change with any version update
+    **and will not follow the skimage two-version deprecation path**.
+    Therefore, use the functions herein with care, and do not use them
+    in production code that will depend on updated skimage versions.
 """
 
 from .manual_segmentation import manual_polygon_segmentation
