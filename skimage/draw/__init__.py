2b8b5ce1719c477eb454f828f7c41be66571a9eb
anand_pb <apb.softdev@gmail.com>
2023-03-09 21:49:26 +0530

    Added detailed description docstring to skimage.draw module

--- skimage/draw/__init__.py
+++ skimage/draw/__init__.py
@@ -1,4 +1,50 @@
-"""Functionality for generating coordinates of different shapes and curves.
+"""Utilities for generating coordinates of different shapes and curves.
+
+Use this module for generating coordinates of a bezier curve, circle, ellipse, rectangle, 
+generating ellipsoid, calculating surface-area and volume of ellipsoid, 
+drawing lines, setting pixel color at a coordinate.
+
+...
+
+Using this module following functionalities can be accessed
+
+
+bezier_curve(r0, c0, r1, c1, ...)     Generate Bezier curve coordinates.
+
+circle_perimeter(r, c, radius)        Generate circle perimeter coordinates.
+
+circle_perimeter_aa(r, c, radius)     Generate anti-aliased circle perimeter coordinates.
+
+disk(center, radius, *[, shape])      Generate coordinates of pixels within circle.
+
+ellipse(r, c, r_radius, c_radius)     Generate coordinates of pixels within ellipse.
+
+ellipse_perimeter(r, c, ...[, ...])   Generate ellipse perimeter coordinates.
+
+ellipsoid(a, b, c[, spacing, ...])    Generates ellipsoid with semimajor axes aligned with grid dimensions on grid with specified spacing.
+
+ellipsoid_stats(a, b, c)              Calculates analytical surface area and volume for ellipsoid with semimajor axes aligned with grid dimensions of specified spacing.
+
+line(r0, c0, r1, c1)                  Generate line pixel coordinates.
+
+line_aa(r0, c0, r1, c1)               Generate anti-aliased line pixel coordinates.
+
+line_nd(start, stop, *[, ...])        Draw a single-pixel thick line in n dimensions.
+
+polygon(r, c[, shape])                Generate coordinates of pixels within polygon.
+
+polygon2mask(image_shape, polygon)    Compute a mask from polygon.
+
+polygon_perimeter(r, c[, ...])        Generate polygon perimeter coordinates.
+
+random_shapes(image_shape, ...)       Generate an image with random shapes, labeled with bounding boxes.
+
+rectangle(start[, end, extent, ...])  Generate coordinates of pixels within a rectangle.
+
+rectangle_perimeter(start[, ...])     Generate coordinates of pixels that are exactly around a rectangle.
+
+set_color(image, coords, color)       Set pixel color in the image at the given coordinates.
+
 """
 from .draw import (ellipse, set_color, polygon_perimeter,
                    line, line_aa, polygon, ellipse_perimeter,
