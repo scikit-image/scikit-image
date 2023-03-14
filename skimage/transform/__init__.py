987544538e9c599755d6ce669522715572b246f4
Ramya <ramyapgk1887@gmail.com>
2023-03-14 13:39:56 +0530

    rendered the doc locally and checked for errors before pushing

--- skimage/transform/__init__.py
+++ skimage/transform/__init__.py
@@ -1,35 +1,33 @@
 """This module includes tools to transform images and volumetric data.
 
 - Geometric Transformations:
- These transforms change the shape or position of an image.
- They are useful for tasks such as image registration,
- alignment, and geometric correction.
- Examples: `~skimage.transform.AffineTransform`,
- `~skimage.transform.ProjectiveTransform`,
- `~skimage.transform.EuclideanTransform`.
+  These transforms change the shape or position of an image.
+  They are useful for tasks such as image registration,
+  alignment, and geometric correction.
+  Examples: :class:`~skimage.transform.AffineTransform`,
+  :class:`~skimage.transform.ProjectiveTransform`,
+  :class:`~skimage.transform.EuclideanTransform`.
 
 - Image Resizing and Rescaling:
- These transforms change the size or resolution of an image.
- They are useful for tasks such as down-sampling an image to
- reduce its size or up-sampling an image to increase its resolution.
- Examples: :func:`~skimage.transform.resize`,
- :func:`~skimage.transform.rescale`.
+  These transforms change the size or resolution of an image.
+  They are useful for tasks such as down-sampling an image to
+  reduce its size or up-sampling an image to increase its resolution.
+  Examples: :func:`~skimage.transform.resize`,
+  :func:`~skimage.transform.rescale`.
 
 - Feature Detection and Extraction:
- These transforms identify and extract specific features or
- patterns in an image. They are useful for tasks s object detection,
- image segmentation, and  feature matching.
- Examples: `~skimage.transform.hough_circle` and other Hough transforms,
- `~skimage.transform.pyramid_expand` and other Pyramid Transforms,
- `~skimage.transform.radon` and other Radon transforms.
+  These transforms identify and extract specific features or
+  patterns in an image. They are useful for tasks s object detection,
+  image segmentation, and  feature matching.
+  Examples: :func:`~skimage.transform.hough_circle` and other Hough transforms,
+  :func:`~skimage.transform.pyramid_expand` and other Pyramid Transforms,
+  :func:`~skimage.transform.radon` and other Radon transforms.
 
 - Image Transformation:
- These transforms change the appearance of an image without changing its
- content. They are useful for tasks such a creating image mosaics,
- applying artistic effects, and visualizing image data.
- Examples:`~skimage.transform.warp`, Inverse transforms.
-
-The following transforms are available:
+  These transforms change the appearance of an image without changing its
+  content. They are useful for tasks such a creating image mosaics,
+  applying artistic effects, and visualizing image data.
+  Examples::func:`~skimage.transform.warp`, Inverse transforms.
 
 """
 
