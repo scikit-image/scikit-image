2bac2919303bc295d08f8e4d7cdd872a227b1575
Br-Al <kenfack_bruno@outlook.fr>
2023-03-15 13:42:54 -0400

    resolved comments from code review

--- skimage/measure/__init__.py
+++ skimage/measure/__init__.py
@@ -1,42 +1,5 @@
 """
-Image measurement tools. A collection of functions for image measurement.
-
-Functions
----------
-find_contours : Find iso-valued contours in a 2D array for a given level value.
-regionprops : Calculate various properties of labeled image regions.
-regionprops_table: Return measurements for image region properties in tabular form.
-perimeter : Calculate perimeter of objects in the label image.
-perimeter_crofton : Approximate perimeter of objects in the label image using Crofton formula.
-euler_number : Calculate Euler characteristic of labeled image regions.
-approximate_polygon : Approximate a polygonal curve(s) for a given contour or binary image.
-subdivide_polygon : Subdivide a polygon into smaller chunks.
-LineModelND : A class to fit a straight n-dimensional line to a set of data points.
-CircleModel : A class to fit a circular model to a set of data points.
-EllipseModel : A class to fit an elliptical model to a set of data points.
-ransac : An implementation of the RANSAC algorithm for robust linear estimation.
-block_reduce : Reduce the image size by block averaging.
-moments : Calculate raw (uncentered) moments of the distribution.
-moments_central : Calculate central moments of the distribution.
-moments_coords : Calculate moments about the origin of the image.
-moments_coords_central : Calculate central moments about the center of mass of the image.
-moments_normalized : Calculate normalized central moments of the distribution.
-moments_hu : Calculate Hu moments of the distribution.
-inertia_tensor : Calculate inertia tensor of the input image.
-inertia_tensor_eigvals : Calculate eigenvalues of the inertia tensor.
-marching_cubes : Generate a triangular mesh for a given isosurface using the marching cubes algorithm.
-mesh_surface_area : Compute total surface area of the triangular mesh.
-profile_line : Profile intensity along a directed line segment inside the image.
-label : Label connected regions of an integer array.
-points_in_poly : Check if points fall within a polygon.
-grid_points_in_poly : Find grid points falling within a polygon.
-shannon_entropy : Calculate the Shannon entropy of an array-like sequence.
-blur_effect : Apply a blur effect to an image.
-pearson_corr_coeff : Compute Pearson's correlation coefficient between two channels.
-manders_coloc_coeff : Compute Mander's coefficients for colocalization analysis.
-manders_overlap_coeff : Compute Mander's overlap coefficients for colocalization analysis.
-intersection_coeff : Compute intersection coefficient for colocalization analysis.
-
+Image measurement tool is collection of functions for image measurement.
 """
 
 
