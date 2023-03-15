afed6dff5fb877b06c3075e201fcac622a33020b
Jake Martin <jakemartin@berkeley.edu>
2023-03-15 13:28:27 -0700

    Updated pyramid_gaussian function demo documentation

--- doc/examples/transform/plot_pyramid.py
+++ doc/examples/transform/plot_pyramid.py
@@ -3,10 +3,10 @@
 Build image pyramids
 ====================
 
-The ``pyramid_gaussian`` function takes an image and yields successive images
-shrunk by a constant scale factor. Image pyramids are often used, e.g., to
-implement algorithms for denoising, texture discrimination, and scale-invariant
-detection.
+The ``pyramid_gaussian`` function takes an image and returns a generator that
+yields successive images shrunk by a constant scale factor. Image pyramids are
+often used, e.g., to implement algorithms for denoising, texture discrimination,
+and scale-invariant detection.
 
 """
 
