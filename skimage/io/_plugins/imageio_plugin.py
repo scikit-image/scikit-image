90335cde4a5a48c755e71660f6e1a164dea24042
Stefan van der Walt <stefanv@berkeley.edu>
2023-04-05 15:56:53 -0700

    Always return a writable array

--- skimage/io/_plugins/imageio_plugin.py
+++ skimage/io/_plugins/imageio_plugin.py
@@ -8,4 +8,7 @@ from imageio.v3 import imread as imageio_imread, imwrite as imsave
 
 @wraps(imageio_imread)
 def imread(*args, **kwargs):
-    return np.asarray(imageio_imread(*args, **kwargs))
+    out = np.asarray(imageio_imread(*args, **kwargs))
+    if not out.flags['WRITEABLE']:
+        out = out.copy()
+    return out
