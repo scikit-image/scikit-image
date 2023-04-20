ec870e05521651f263ef4a8c5e22219401fa9cf4
Riadh <r.fezzani@vitadx.com>
2023-04-20 13:55:49 +0200

    Remove future.graph artifacts after v0.20 release

--- doc/tools/build_modref_templates.py
+++ doc/tools/build_modref_templates.py
@@ -54,7 +54,6 @@ if __name__ == '__main__':
         r'\.fixes$',
         r'\.externals$',
         r'filter$',
-        r'\.future.graph$',  # Remove after v0.20 release
     ]
     docwriter.write_api_docs(outdir)
     docwriter.write_index(outdir, 'api', relative_to='source/api')
--- skimage/future/graph/__init__.py
+++
@@ -1,7 +0,0 @@
-# Remove this package in the release after v0.20
-
-raise ModuleNotFoundError(
-    "The `skimage.future.graph` submodule was moved to `skimage.graph` in "
-    "v0.20. `ncut` was removed in favor of the identical function "
-    "`cut_normalized`. Please update your import paths accordingly."
-)
