88c3a90b206278ddb1b29294127721a1b36b8c15
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2023-04-28 12:27:07 +0200

    Fix trailing whitespaces

--- skimage/transform/hough_transform.py
+++ skimage/transform/hough_transform.py
@@ -122,14 +122,14 @@ def hough_ellipse(image, threshold=4, accuracy=1, min_size=4, max_size=None):
     threshold : int, optional
         Accumulator threshold value. A lower value will return more ellipses.
     accuracy : double, optional
-        Bin size on the minor axis used in the accumulator. A higher value 
-        will return more ellipses, but lead to a less precise estimation of 
+        Bin size on the minor axis used in the accumulator. A higher value
+        will return more ellipses, but lead to a less precise estimation of
         the minor axis lengths.
     min_size : int, optional
         Minimal major axis length.
     max_size : int, optional
         Maximal minor axis length.
-        If None, the value is set to the half of the smaller
+        If None, the value is set to half of the smaller
         image dimension.
 
     Examples
@@ -146,16 +146,16 @@ def hough_ellipse(image, threshold=4, accuracy=1, min_size=4, max_size=None):
     Notes
     -----
 
-    Potential ellipses in the image are characterized by their major & 
-    minor axis length. For any pair of nonzero pixels in the image that 
-    are at least half of `min_size` apart, an accumulator keeps track of 
-    the minor axis lengths of potential ellipses formed with all the 
-    other nonzero pixels. If any bin (with `bin_size = accuracy * accuracy`) 
-    in the histogram of those accumulated minor axis lengths is above 
+    Potential ellipses in the image are characterized by their major and
+    minor axis lengths. For any pair of nonzero pixels in the image that
+    are at least half of `min_size` apart, an accumulator keeps track of
+    the minor axis lengths of potential ellipses formed with all the
+    other nonzero pixels. If any bin (with `bin_size = accuracy * accuracy`)
+    in the histogram of those accumulated minor axis lengths is above
     `threshold`, the corresponding ellipse is added to the results.
 
-    A higher `accuracy` will therefore lead to more ellipses being found 
-    in the image, at the cost of a less precise estimation of the minor 
+    A higher `accuracy` will therefore lead to more ellipses being found
+    in the image, at the cost of a less precise estimation of the minor
     axis length.
 
     References
