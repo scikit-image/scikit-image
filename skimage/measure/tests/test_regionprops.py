162f8158c667098dedfc42e85263e04a58ff523a
Tony Reina <tony.reina@resilience.com>
2023-05-16 09:38:55 -0700

    Adding back spacing tests for moment

--- skimage/measure/tests/test_regionprops.py
+++ skimage/measure/tests/test_regionprops.py
@@ -341,6 +341,14 @@ def test_spacing_parameter_complex_input(spacing):
     with pytest.raises(TypeError, match="spacing isn't of float or integer type"):
         regionprops(SAMPLE, spacing=spacing)[0].centroid
 
+@pytest.mark.parametrize(
+    "spacing",
+    [np.nan, np.inf, -np.inf],
+)
+def test_spacing_parameter_nan_inf(spacing):
+    """Test the _normalize_spacing code."""
+    with pytest.raises(ValueError):
+        regionprops(SAMPLE, spacing=spacing)[0].centroid
 
 @pytest.mark.parametrize(
     "spacing", ([1], [[1, 1]], (1, 1, 1))
