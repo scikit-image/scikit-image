d42f89d943c30063ff19969d6ecfaaeb7e430bf0
Biola <52200708+decorouz@users.noreply.github.com>
2023-06-01 10:58:03 +0100

    implement lazy loading

--- skimage/exposure/__init__.pyi
+++ skimage/exposure/__init__.pyi
@@ -15,10 +15,15 @@ __all__ = [
     'match_histograms'
 ]
 
-from .exposure import histogram, equalize_hist, \
-                      rescale_intensity, cumulative_distribution, \
-                      adjust_gamma, adjust_sigmoid, adjust_log, \
-                      is_low_contrast
-
 from ._adapthist import equalize_adapthist
 from .histogram_matching import match_histograms
+from .exposure import (
+    histogram,
+    equalize_hist,
+    rescale_intensity,
+    cumulative_distribution,
+    adjust_gamma,
+    adjust_sigmoid,
+    adjust_log,
+    is_low_contrast
+)
