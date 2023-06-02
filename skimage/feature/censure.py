7c604024b55a511e5b207c721b0358c7da9a2a69
Stefan van der Walt <sjvdwalt@gmail.com>
2023-06-02 08:32:18 -0700

    Fix circular import
    Co-authored-by: Adeyemi Biola  <52200708+decorouz@users.noreply.github.com>

--- skimage/feature/censure.py
+++ skimage/feature/censure.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.ndimage import maximum_filter, minimum_filter, convolve
 
 from ..transform import integral_image
-from ..feature import structure_tensor
+from .corner import structure_tensor
 from ..morphology import octagon, star
 from .censure_cy import _censure_dob_loop
 from ..feature.util import (FeatureDetector, _prepare_grayscale_input_2D,
