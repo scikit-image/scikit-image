6844eaf331a1a9157ef5f2e2b3b0d4dfa55e6c9e
ana42742 <amit.ritu.ananya@gmail.com>
2023-06-03 23:37:24 +0530

    Add lazy loading to skimage.metrics module

--- skimage/metrics/__init__.py
+++ skimage/metrics/__init__.py
@@ -1,20 +1,3 @@
-from ._adapted_rand_error import adapted_rand_error
-from ._contingency_table import contingency_table
-from ._structural_similarity import structural_similarity
-from ._variation_of_information import variation_of_information
-from .set_metrics import hausdorff_distance, hausdorff_pair
-from .simple_metrics import (mean_squared_error, normalized_mutual_information,
-                             normalized_root_mse, peak_signal_noise_ratio)
+import lazy_loader as lazy
 
-__all__ = [
-    "adapted_rand_error",
-    "variation_of_information",
-    "contingency_table",
-    "mean_squared_error",
-    "normalized_mutual_information",
-    "normalized_root_mse",
-    "peak_signal_noise_ratio",
-    "structural_similarity",
-    "hausdorff_distance",
-    "hausdorff_pair",
-]
+__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
