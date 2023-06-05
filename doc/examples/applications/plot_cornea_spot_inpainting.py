90b97d1742aa083786d3ad57eb67e7c8fd9a3dcf
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2023-06-05 18:35:25 +0200

    Apply suggestions from code review
    Co-authored-by: Ananya Srivastava <95476298+ana42742@users.noreply.github.com>
    Co-authored-by: Adeyemi Biola  <52200708+decorouz@users.noreply.github.com>

--- doc/examples/applications/plot_cornea_spot_inpainting.py
+++ doc/examples/applications/plot_cornea_spot_inpainting.py
@@ -25,7 +25,10 @@ The images were acquired by Viacheslav Mazlin.
 import numpy as np
 import plotly.io
 import plotly.express as px
+import matplotlib.pyplot as plt
 
+from skimage.morphology import opening, erosion, diamond
+from skimage.restoration import inpaint
 from skimage import filters
 from skimage.data import palisades_of_vogt
 
@@ -80,21 +83,84 @@ plotly.io.show(fig)
 # Use local thresholding
 # ======================
 
-spot_size = 17
+#####################################################################
+# Let's compare the visibility of the hidden data in two different
+# thresholding masks, one of which has the `block_size` set to 21, while
+# the other has it set to 35. For this, we start by defining a convenience
+# function to create a mask: 
 
-thresh_value = filters.threshold_local(
-    image_seq_mean,
-    block_size=spot_size
-)
+def create_mask(image_seq_mean, spot_size):
+    thresh_value = filters.threshold_local(
+        image_seq_mean,
+        block_size=spot_size,
+        offset=10
+    )
+    mask = (image_seq_mean > thresh_value)
+    return mask
+
+#####################################################################
+# Let's also define a function to display two plots side-by-side so
+# that it is easier for us to draw comparisons between plots:
+
+def plot_comparison(plot1, plot2, title1, title2):
+    fig, (ax1, ax2) = plt.subplots(
+        ncols=2,
+        figsize=(12, 8),
+        sharex=True,
+        sharey=True
+    )
+    ax1.imshow(plot1)
+    ax1.set_title(title1)
+    ax1.axis('off')
+    ax2.imshow(plot2)
+    ax2.set_title(title2)
+    ax2.axis('off')
+
+#####################################################################
+# Now, we plot the two masks. It seems that the dust-covered
+# spots appear more distinct in the second mask!
+
+mask1 = create_mask(image_seq_mean, 21)
+mask2 = create_mask(image_seq_mean, 35)
+
+plot_comparison(mask1, mask2, "spot_size = 21", "spot_size = 35")
 
 #####################################################################
 # Remove fine-grained features
 # ============================
+# We use diamond to create a diamond structuring element ``footprint`` for this example.
+# Morphological ``opening`` on an image is defined as an *erosion followed by
+# a dilation*. Opening can remove small bright spots (i.e. "dust") and
+# connect small dark cracks.
+
+footprint = diamond(1)
+mask = opening(mask, footprint)
+plot_comparison(image_seq_mean, mask, 'erosion')
+
+# Since ``opening`` an image starts with an erosion operation, light regions that are
+# smaller than the structuring element are removed.
+
+# Let's make the detected areas wider
+mask = dilation(mask, diamond(3))
+plot_comparison(image_seq_mean, mask, 'dilation')
+
+# Dilation enlarges bright regions and shrinks dark regions.
+# Notice how the white spots of the image thickens, or gets dilated, as we increase the 
+# size of the diamond.
+
 
 #####################################################################
 # Apply mask across frames
 # ========================
+# Although masks are binary, they can be applied to images to filter out 
+# pixels where the mask is ``False``.
+# Numpy's ``where()`` is a flexible way of applying masks.
+# The application of a mask to the input image produces an output image of
+# the same size as the input.
+# Let's apply the mask across frame
 
+image_masked = np.where(image_seq_mean, mask, 0)
+plot_comparison(image_seq_mean, image_masked, 'Masked across frames')
 #####################################################################
 # Inpaint each frame separately
 # =============================
