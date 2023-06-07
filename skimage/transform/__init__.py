fe86ac1252855285bbabdf8c66cac5d8ad0cc3b8
Ananya Srivastava <95476298+ana42742@users.noreply.github.com>
2023-06-07 19:53:14 +0530

    Add lazy loading to skimage.transform module. (#7009)

--- skimage/transform/__init__.py
+++ skimage/transform/__init__.py
@@ -32,60 +32,6 @@
 
 """
 
-from .hough_transform import (hough_line, hough_line_peaks,
-                              probabilistic_hough_line, hough_circle,
-                              hough_circle_peaks, hough_ellipse)
-from .radon_transform import (radon, iradon, iradon_sart,
-                              order_angles_golden_ratio)
-from .finite_radon_transform import frt2, ifrt2
-from .integral import integral_image, integrate
-from ._geometric import (estimate_transform,
-                         matrix_transform, EuclideanTransform,
-                         SimilarityTransform, AffineTransform,
-                         ProjectiveTransform, FundamentalMatrixTransform,
-                         EssentialMatrixTransform, PolynomialTransform,
-                         PiecewiseAffineTransform)
-from ._warps import (swirl, resize, rotate, rescale,
-                     downscale_local_mean, warp, warp_coords, warp_polar,
-                     resize_local_mean)
-from .pyramids import (pyramid_reduce, pyramid_expand,
-                       pyramid_gaussian, pyramid_laplacian)
+import lazy_loader as lazy
 
-
-__all__ = ['hough_circle',
-           'hough_ellipse',
-           'hough_line',
-           'probabilistic_hough_line',
-           'hough_circle_peaks',
-           'hough_line_peaks',
-           'radon',
-           'iradon',
-           'iradon_sart',
-           'order_angles_golden_ratio',
-           'frt2',
-           'ifrt2',
-           'integral_image',
-           'integrate',
-           'warp',
-           'warp_coords',
-           'warp_polar',
-           'estimate_transform',
-           'matrix_transform',
-           'EuclideanTransform',
-           'SimilarityTransform',
-           'AffineTransform',
-           'ProjectiveTransform',
-           'EssentialMatrixTransform',
-           'FundamentalMatrixTransform',
-           'PolynomialTransform',
-           'PiecewiseAffineTransform',
-           'swirl',
-           'resize',
-           'resize_local_mean',
-           'rotate',
-           'rescale',
-           'downscale_local_mean',
-           'pyramid_reduce',
-           'pyramid_expand',
-           'pyramid_gaussian',
-           'pyramid_laplacian']
+__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
