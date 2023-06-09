6903d45cf1a8750fad99ab5bce19abbcc21e22b6
Lars Grüter <lagru@mailbox.org>
2023-06-09 10:18:08 +0200

    Ensure existing target directory for random.js
    An issue was reported on Zulip when running `spin docs` which resulted
    in the following error:
    ```
    Extension error (skimage_extensions):
    Handler <function write_random_js at 0x120763f70> for event 'build-finished' threw an exception (exception: [Errno 2] No such file or directory: '~/scikit-image/doc/build/html/_static/random.js')
    make: *** [html] Error 2
    ```
    A cause might be the missing target directory
    `~/scikit-image/doc/build/html/_static`, though it's curious that this
    hasn't come up for us or in our CI.
    
    This should make the function a bit more robust and independent of
    sphinx creating the target directory in time. Might save us some future
    pain if we ever update or change how this extension works.

--- doc/ext/skimage_extensions.py
+++ doc/ext/skimage_extensions.py
@@ -122,6 +122,7 @@ def write_random_js(app, exception):
     content = content.replace('{{LINKS}}', str(tutorial_urls))
     content = content.replace('{{GALLERY_DIV}}', ''.join(GALLERY_DIV.split('\n')))
 
+    random_js_path.parent.mkdir(parents=True, exist_ok=True)
     with open(random_js_path, 'w') as file:
         file.write(content)
     logger.info(
