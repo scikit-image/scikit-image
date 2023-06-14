2473ff76c0ddce28adddaa9e5c329d78726c6541
ana42742 <amit.ritu.ananya@gmail.com>
2023-06-15 00:20:59 +0530

    Refactor test_rolling_ball.py

--- skimage/restoration/tests/test_rolling_ball.py
+++ skimage/restoration/tests/test_rolling_ball.py
@@ -7,7 +7,7 @@ import numpy as np
 import pytest
 
 from skimage import data
-from skimage.restoration import rolling_ball
+from skimage.restoration._rolling_ball import rolling_ball
 from skimage.restoration._rolling_ball import ellipsoid_kernel
 
 
