04b945bf723a7dbc7e86fa696c2b99e56458366a
Loic Musy <loic@famille-musy.com>
2023-07-06 13:11:02 +0200

    feat: Change the init to allows for the import of find_transform_ECC

--- skimage/registration/__init__.py
+++ skimage/registration/__init__.py
@@ -1,8 +1,5 @@
-from ._optical_flow import optical_flow_tvl1, optical_flow_ilk
+from ._optical_flow import optical_flow_ilk, optical_flow_tvl1
 from ._phase_cross_correlation import phase_cross_correlation
+from .enhanced_correlation_coef import find_transform_ECC
 
-__all__ = [
-    'optical_flow_ilk',
-    'optical_flow_tvl1',
-    'phase_cross_correlation'
-    ]
+__all__ = ["optical_flow_ilk", "optical_flow_tvl1", "phase_cross_correlation", "find_transform_ECC"]
