ac91cede6a73d0460e3d27c1712c26aa831bb011
Adeyemi Biola <52200708+decorouz@users.noreply.github.com>
2023-07-08 20:48:33 +0100

    Fix abs value function warnings (#7010)

--- skimage/transform/_hough_transform.pyx
+++ skimage/transform/_hough_transform.pyx
@@ -7,7 +7,7 @@ import numpy as np
 cimport numpy as cnp
 
 from cpython.mem cimport PyMem_Malloc, PyMem_Free
-from libc.stdlib cimport abs
+from libc.stdlib cimport labs
 from libc.math cimport fabs, sqrt, ceil, atan2, M_PI
 
 from ..draw import circle_perimeter
@@ -495,8 +495,8 @@ def _probabilistic_hough_line(cnp.ndarray img, Py_ssize_t threshold,
                     py += dy
 
             # confirm line length is sufficient
-            good_line = (abs(line_end[3] - line_end[1]) >= line_length or
-                         abs(line_end[2] - line_end[0]) >= line_length)
+            good_line = (labs(line_end[3] - line_end[1]) >= line_length or
+                         labs(line_end[2] - line_end[0]) >= line_length)
 
             # pass 2: walk the line again and reset accumulator and mask
             for k in range(2):
