f0950a63a860a35f12259169f7754e9e68e51703
Biola <52200708+decorouz@users.noreply.github.com>
2023-08-08 12:56:06 +0100

    Update example

--- doc/examples/transform/plot_tps_deformation.py
+++ doc/examples/transform/plot_tps_deformation.py
@@ -18,8 +18,8 @@ image warping.
 
 For further information on TPS Transformation, see:
 
-.. [1] Wikipedia, Cubic Spline interpolation
-       https://en.wikiversity.org/wiki/Cubic_Spline_Interpolation
+.. [1] Wikipedia, Thin plate spline
+       https://en.wikipedia.org/wiki/Thin_plate_spline
 
 .. [2] Weisstein, Eric W. "Biharmonic Equation." From MathWorld--A Wolfram Web Resource
        https://mathworld.wolfram.com/BiharmonicEquation.html
@@ -36,56 +36,56 @@ Image Interpolation
 ===================
 
 In this example we will see how to use thin plate spline interpolation in the
-context of image interpolation based on landmarks or control points.
-
+context of interpolating the correspondence of five pairs of landmarks.
+The examples shows landmarks before and after being bent using the TPS algorithm,
+and the control points (X) used as input the the algorithm.
 """
 import matplotlib.pyplot as plt
 import numpy as np
 
 import skimage as ski
 
-src = np.array([[0.25, 0.25],
-                [0.25, 0.75],
-                [0.75, 0.25],
-                [0.75, 0.75]])
+chess = ski.data.checkerboard()
 
-dst = np.array([[0.35, 0.35],
-                [0.35, 0.65],
-                [0.65, 0.35],
-                [0.65, 0.65]])
+src = np.array([[3.6929, 10.3819],[6.5827, 8.8386], [6.7766, 12.0866], [4.8189, 11.2047], [5.6969, 10.0748]])
+dst = np.array([[3.9724, 6.5354], [6.6969, 4.1181], [6.5394, 7.2362], [5.4016, 6.4528], [5.7756, 5.1142]])
 
-astronaut = ski.data.astronaut()
-width, height, _ = astronaut.shape
-start = (50, 150)
-end = (250, 350)
-rr, cc = ski.draw.rectangle_perimeter(start=start, end=end, shape=astronaut.shape, clip=True)
-astronaut[rr, cc] = 1
-
-src *= [height, width]
-dst *= [height, width]
 
 # Fit the thin plate spline from output to input
 tps = ski.transform.TpsTransform()
+warped_img = ski.transform.tps_warp(chess, src, dst, grid_scaling=1)
 
-warped = ski.transform.tps_warp(astronaut, src, dst)
 
-fig, ax = plt.subplots(ncols=2)
-ax[0].imshow(astronaut)
-ax[1].imshow(warped)
+fig, axs = plt.subplots(1, 2, figsize=(16, 8))
+axs[0].axis('off')
+axs[1].axis('off')
+
+labels = ['1', '2', '3', '4', '5']  # Adjust the number of labels to match the number of points
+
+# axs[0].imshow(img[..., ::-1], origin='upper')
+axs[0].scatter(src[:, 0] * chess.shape[0], src[:, 1] * chess.shape[1], marker='x', color='black')
+
+for i, label in enumerate(labels):
+    axs[0].annotate(label, (src[:, 0][i] * chess.shape[0], src[:, 1][i] * chess.shape[1]),
+                    textcoords="offset points", xytext=(0, 10), ha='center', color='red')
+
+# axs[1].imshow(warped[..., ::-1], origin='upper')
+axs[1].scatter(dst[:, 0] * warped_img.shape[0], dst[:, 1] * warped_img.shape[1], marker='x', color='black')
+
+for i, label in enumerate(labels):
+    axs[1].annotate(label, (dst[:, 0][i] * warped_img.shape[0], dst[:, 1][i] * warped_img.shape[1]),
+                    textcoords="offset points", xytext=(0, 10), ha='center', color='red')
+
 plt.show()
-
-
 
 ######################################################################
 #
 # Deformation
 # ===========
-# In 2D cases, given a set of K corresponding points, the TPS warp is described by
-# `2(K+3)`` parameters which include 6 global affine motion parameters and
-# `2K` coefficients for correspondences of the control points.
-#
-# In this example we compute the coefficients, and then transform an arbitrary
-# point from source surface to the deformed surface.
+# In this example thin-plate spline is applied to source coordinates and to
+# each target coordinates to derive an interpolation function and coefficients for
+# each target points. These coefficients is then used to transforms an arbitrary
+# point associated with the reference to an interpolated location on the target.
 
 import matplotlib.pyplot as plt
 
@@ -93,6 +93,7 @@ import skimage as ski
 
 samp = np.linspace(-2, 2, 4)
 xx, yy = np.meshgrid(samp, samp)
+coords= np.meshgrid(samp, samp)
 
 # Make source points
 source_xy = np.column_stack((xx.ravel(), yy.ravel()))
@@ -101,17 +102,18 @@ source_xy = np.column_stack((xx.ravel(), yy.ravel()))
 yy[:, [0, 3]] *=2
 target_xy = np.column_stack((xx.ravel(), yy.ravel()))
 
-# Get the coeefficient
+
+# Compute the coefficient
 trans = ski.transform.TpsTransform()
 trans.estimate(source_xy, target_xy)
 
-# Make an arbitiary point
+
+# Make another arbitiary point
 samp2 = np.linspace(-1.8, 1.8, 10)
 test_xy = np.tile(samp2, [2, 1]).T
 
 # Estimate transformed points from given sets of source and targets points.
 transformed_xy = trans(test_xy)
-
 
 fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, figsize=(8, 3))
 
@@ -121,7 +123,7 @@ ax0.legend(loc="upper center")
 ax0.set_title('Source and Test Coordinates')
 
 ax1.scatter(target_xy[:, 0], target_xy[:, 1], label='Target Points')
-ax1.scatter(transformed_xy[0], transformed_xy[1], c="orange", label='Transformed Test Points')
+ax1.scatter(transformed_xy[:,0], transformed_xy[:,1], c="orange", label='Transformed Test Points')
 ax1.legend(loc="upper center")
 ax1.set_title("Target and Transformed Coordinates")
 plt.show()
