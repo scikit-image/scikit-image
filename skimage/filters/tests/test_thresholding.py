b9b837bf7093375ddc056adecc33201efb063097
Adeyemi Biola <52200708+decorouz@users.noreply.github.com>
2023-08-26 08:13:56 +0100

    Update skimage/filters/tests/test_thresholding.py
    Co-authored-by: Lars Grüter <lagru+github@mailbox.org>

--- skimage/filters/tests/test_thresholding.py
+++ skimage/filters/tests/test_thresholding.py
@@ -570,9 +570,9 @@ def test_mean():
 
 @pytest.mark.parametrize("dtype", [np.uint8, np.int16, np.float16, np.float32])
 def test_traingle_uniform_images(dtype):
-    assert(threshold_triangle(np.zeros((10, 10), dtype=dtype)) == 0)
-    assert(threshold_triangle(np.ones((10, 10), dtype=dtype)) == 1)
-    assert(threshold_triangle(np.full((10, 10), 2, dtype=dtype)) == 2)
+    assert threshold_triangle(np.zeros((10, 10), dtype=dtype)) == 0
+    assert threshold_triangle(np.ones((10, 10), dtype=dtype)) == 1
+    assert threshold_triangle(np.full((10, 10), 2, dtype=dtype)) == 2
 
 def test_triangle_uint_images():
     assert(threshold_triangle(np.invert(data.text())) == 151)
