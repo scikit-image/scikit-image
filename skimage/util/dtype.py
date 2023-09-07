aa40a883a948bee05182f8bd88446eba1623f900
Jarrod Millman <jarrod.millman@gmail.com>
2023-09-07 10:09:14 -0700

    Update for NumPy 2 namespace cleanup (#7119)

--- skimage/util/dtype.py
+++ skimage/util/dtype.py
@@ -254,7 +254,7 @@ def _convert(image, dtype, force_copy=False, uniform=False):
     #   is a subclass of that type (e.g. `np.floating` will allow
     #   `float32` and `float64` arrays through)
 
-    if np.issubdtype(dtype_in, np.obj2sctype(dtype)):
+    if np.issubdtype(dtype_in, np.core.numerictypes.obj2sctype(dtype)):
         if force_copy:
             image = image.copy()
         return image
