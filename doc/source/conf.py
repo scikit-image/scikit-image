7aca0236c3bf960dfe47fd2095ba9717068ebd43
Lars Grüter <lagru@mailbox.org>
2023-09-16 11:22:43 +0200

    Enable version warning banners for docs
    If the doc version isn't the preferred one, a warning banner is shown
    at the top. This also accounts for dev builds.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -156,18 +156,24 @@ html_theme_options = {
     "header_links_before_dropdown": 6,
     "icon_links": [
         {
+            "name": "GitHub",
+            "url": "https://github.com/scikit-image/scikit-image",
+            "icon": "fa-brands fa-github",
+        },
+        {
             "name": "PyPI",
             "url": "https://pypi.org/project/scikit-image/",
             "icon": "fa-solid fa-box",
         },
     ],
+    "navbar_align": "left",
     "navbar_end": ["version-switcher", "navbar-icon-links"],
     "show_prev_next": False,
     "switcher": {
-        "json_url": "https://scikit-image.org/docs/dev/_static/version_switcher.json",
+        "json_url": "https://gist.githubusercontent.com/lagru/8bce79c510b9026dab455c0a96a2c0b3/raw/d7d6159090de17cf54b55b457b198284c4078da0/version_switcher.json",
         "version_match": "dev" if "dev" in version else version,
     },
-    "github_url": "https://github.com/scikit-image/scikit-image",
+    "show_version_warning_banner": True,
     # Footer
     "footer_start": ["copyright"],
     "footer_end": ["sphinx-version", "theme-version"],
