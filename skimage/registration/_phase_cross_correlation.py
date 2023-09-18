c94079576d6d9beed4e0a7992ce6dc3858c5761d
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2023-09-18 21:33:59 +0200

    Update skimage/registration/_phase_cross_correlation.py
    Co-authored-by: Lars Grüter <lagru+github@mailbox.org>

--- skimage/registration/_phase_cross_correlation.py
+++ skimage/registration/_phase_cross_correlation.py
@@ -142,7 +142,7 @@ def _disambiguate_shift(reference_image, moving_image, shift):
 
     Returns
     -------
-    real_shift : sequence of floats
+    real_shift : ndarray
         The shift disambiguated in real space.
     """
     shape = reference_image.shape
