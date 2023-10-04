89d24d4f05340b317495b25a3ac08e4e65ec4860
Anam Fatima <anam.fatima1304@gmail.com>
2023-10-05 03:11:35 +0500

    Added Docstring to module skimage.graph

--- skimage/graph/__init__.py
+++ skimage/graph/__init__.py
@@ -1,3 +1,7 @@
+"""
+Utilities for Image Segmentation, Object detection and Network Analysis.
+This module provides functions to work with graphs including finding the shortest path,closest centrality and merging.
+"""
 import lazy_loader as lazy
 
 __getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
