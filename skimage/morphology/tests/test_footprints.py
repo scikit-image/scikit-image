1a403b7457ec48c93185a5a6246a4c7ffbbe6f9a
Lars Grüter <lagru@mailbox.org>
2023-10-10 17:48:32 +0200

    Extend tests of pad_footprint and mirror_footprint
    so that more of the happy path and all parameters are covered.

--- skimage/morphology/tests/test_footprints.py
+++ skimage/morphology/tests/test_footprints.py
@@ -238,15 +238,29 @@ def test_ball_series_approximation_unavailable():
         footprints.ball(radius=10000, decomposition="sequence")
 
 
-def test_mirror_footprint():
+@pytest.mark.parametrize("as_sequence", [tuple, None])
+def test_mirror_footprint(as_sequence):
     footprint = np.array([[0, 0, 0], [0, 1, 1], [0, 1, 1]], np.uint8)
     expected_res = np.array([[1, 1, 0], [1, 1, 0], [0, 0, 0]], dtype=np.uint8)
+    if as_sequence is not None:
+        footprint = as_sequence([(footprint, 2), (footprint.T, 3)])
+        expected_res = as_sequence([(expected_res, 2), (expected_res.T, 3)])
+
     actual_res = footprints.mirror_footprint(footprint)
+    assert type(expected_res) is type(actual_res)
     assert_equal(expected_res, actual_res)
 
 
-def test_pad_footprint():
-    footprint = np.array([[0, 0], [1, 1], [1, 1]], np.uint8)
-    expected_res = np.array([[0, 0, 0], [1, 1, 0], [1, 1, 0]], dtype=np.uint8)
-    actual_res = footprints.pad_footprint(footprint, right=True)
+@pytest.mark.parametrize("as_sequence", [tuple, None])
+@pytest.mark.parametrize("pad_end", [True, False])
+def test_pad_footprint(as_sequence, pad_end):
+    footprint = np.array([[0, 0], [1, 0], [1, 1]], np.uint8)
+    pad_width = [(0, 0), (0, 1)] if pad_end is True else [(0, 0), (1, 0)]
+    expected_res = np.pad(footprint, pad_width)
+    if as_sequence is not None:
+        footprint = as_sequence([(footprint, 2), (footprint.T, 3)])
+        expected_res = as_sequence([(expected_res, 2), (expected_res.T, 3)])
+
+    actual_res = footprints.pad_footprint(footprint, pad_end=pad_end)
+    assert type(expected_res) is type(actual_res)
     assert_equal(expected_res, actual_res)
