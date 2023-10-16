5f6ec5f06ae058d1f32908b1f5981bd7608c42ba
Lars Grüter <lagru+github@mailbox.org>
2023-10-16 16:16:18 +0200

    Add missing newline

--- skimage/graph/__init__.py
+++ skimage/graph/__init__.py
@@ -1,5 +1,6 @@
 """
 This moddule provides utilities for graph-based image processing.
+
 This includes creating adjacency graphs of pixels in an image, finding the
 central pixel in an image, finding (minimum-cost) paths across pixels, merging
 and cutting of graphs, etc.
