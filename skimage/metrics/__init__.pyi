0d66ed44137420e10fc3774e9ee8ad2400a6aff3
Anam Fatima <123370498+anamfatima1304@users.noreply.github.com>
2023-10-19 08:49:42 +0500

    Update skimage/metrics/__init__.pyi
    Co-authored-by: Marianne Corvellec <marianne.corvellec@ens-lyon.org>

--- skimage/metrics/__init__.pyi
+++ skimage/metrics/__init__.pyi
@@ -1,3 +1,7 @@
+# Explicitly setting `__all__` is necessary for type inference engines
+# to know which symbols are exported. See
+# https://peps.python.org/pep-0484/#stub-files
+
 __all__ = [
     "adapted_rand_error",
     "variation_of_information",
