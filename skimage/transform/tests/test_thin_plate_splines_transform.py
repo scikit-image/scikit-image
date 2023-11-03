0ebd61ebff361cad4d9f1b36b9cec412ecb6b88c
Biola <52200708+decorouz@users.noreply.github.com>
2023-11-03 09:17:42 +0100

    update variable name from parameters to coefficients

--- skimage/transform/tests/test_thin_plate_splines_transform.py
+++ skimage/transform/tests/test_thin_plate_splines_transform.py
@@ -2,20 +2,20 @@ import numpy as np
 import pytest
 from numpy.testing import assert_array_equal
 
-from skimage.transform._thin_plate_splines import (TpsTransform, _ensure_2d,
-                                                   tps_warp)
+from skimage.transform._thin_plate_splines import TpsTransform, _ensure_2d, tps_warp
 
 SRC = np.array([[0, 0], [0, 5], [5, 5], [5, 0]])
 
 DST = np.array([[5, 0], [0, 0], [0, 5], [5, 5]])
 
-class TestTpsTransform():
+
+class TestTpsTransform:
     def test_tps_transform_init(self):
         tform = TpsTransform()
 
         # Test that _estimated is initialized to False
         assert tform._estimated is False
-        assert tform.parameters is None
+        assert tform.coefficients is None
         assert tform.src is None
 
     def test_tps_transform_inverse(self):
@@ -32,9 +32,7 @@ class TestTpsTransform():
         assert_array_equal(_ensure_2d(array_1d), expected)
 
         empty_array = np.array([])
-        with pytest.raises(
-            ValueError, match="Array of points can not be empty."
-        ):
+        with pytest.raises(ValueError, match="Array of points can not be empty."):
             _ensure_2d(empty_array)
 
         scalar = 5
@@ -46,38 +44,34 @@ class TestTpsTransform():
             _ensure_2d(array_3d)
 
         control_pts_less_than_3 = np.array([[0, 0], [0, 0]])
-        with pytest.raises(
-            ValueError, match="Array points less than 3 is undefined."
-        ):
+        with pytest.raises(ValueError, match="Array points less than 3 is undefined."):
             _ensure_2d(control_pts_less_than_3)
 
     def test_tps_transform_estimation(self):
         src = np.array([[0, 1], [-1, 0], [0, -1], [1, 0]])
-        dst = np.array(
-            [[0, 0.75], [-1, 0.25], [0, -1.25], [1, 0.25]], dtype=np.float32
-        )
+        dst = np.array([[0, 0.75], [-1, 0.25], [0, -1.25], [1, 0.25]], dtype=np.float32)
         tform = TpsTransform()
 
         # Ensure that the initial state is as expected
-        assert tform.parameters is None
+        assert tform.coefficients is None
         assert tform.src is None
 
         # Perform estimation
         tform.estimate(src, dst)
         assert len(tform.src) > 0
 
-        assert len(tform.parameters) > 0
-        assert tform.parameters.shape[0] == src.shape[0] + 3
+        assert len(tform.coefficients) > 0
+        assert tform.coefficients.shape[0] == src.shape[0] + 3
 
         np.testing.assert_array_equal(tform.src, src)
         np.testing.assert_allclose(
-            tform.parameters[:, 0],
+            tform.coefficients[:, 0],
             np.array([0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0]),
             rtol=0.1,
             atol=1e-16,
         )
         np.testing.assert_allclose(
-            tform.parameters[:, 1],
+            tform.coefficients[:, 1],
             np.array([-0.0902, 0.0902, -0.0902, 0.0902, 0.0, 0.0, 1.0]),
             rtol=0.1,
             atol=1e-16,
@@ -91,7 +85,7 @@ class TestTpsTransform():
 
         # Ensure that the initial state is as expected
         assert tform._estimated is False
-        assert tform.parameters is None
+        assert tform.coefficients is None
         assert tform.src is None
 
         # Perform the estimation, which should fail due to the mismatched number of points
@@ -100,19 +94,26 @@ class TestTpsTransform():
 
         # Check if the estimation failed and the instance attributes remain unchanged
         assert tform._estimated is False
-        assert tform.parameters is None
+        assert tform.coefficients is None
         assert tform.src is None
 
-class TestTpsWarp():
+
+class TestTpsWarp:
     @pytest.mark.parametrize("image_shape", [0, (0, 10), (10, 0)])
     def test_tps_warp_invalid_image_shape(self, image_shape):
         img = np.zeros(image_shape)
 
-        with pytest.raises(ValueError, match="Cannot warp empty image with dimensions."):
+        with pytest.raises(
+            ValueError, match="Cannot warp empty image with dimensions."
+        ):
             tps_warp(img, SRC, DST)
-        with pytest.raises(ValueError, match="Cannot warp empty image with dimensions."):
+        with pytest.raises(
+            ValueError, match="Cannot warp empty image with dimensions."
+        ):
             tps_warp(img, SRC, DST)
-        with pytest.raises(ValueError, match="Cannot warp empty image with dimensions."):
+        with pytest.raises(
+            ValueError, match="Cannot warp empty image with dimensions."
+        ):
             tps_warp(img, SRC, DST)
 
     def test_tps_warp_valid_output_region(self):
@@ -144,13 +145,14 @@ class TestTpsWarp():
         trans_coord = tform(coords)
         yy_trans = trans_coord[:, 1]
 
+        # fmt: off
         expected_yy = np.array([0, 1.0, 2.0, 3.0, 4.0,
                                 0, 1.0, 2.0, 3.0, 4.0,
                                 0, 1.0, 2.0, 3.0, 4.0,
                                 0, 1.0, 2.0, 3.0, 4.0,
                                 0, 1.0, 2.0, 3.0, 4.0], dtype=np.float32)
+        # fmt: on
         np.testing.assert_allclose(yy_trans, expected_yy)
-
 
     def test_tps_warp_resizing(self):
         pass
