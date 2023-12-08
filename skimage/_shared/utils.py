2d9ff7bd3116874c00110817abc49db54b784766
Lars Grüter <lagru+github@mailbox.org>
2023-12-08 11:44:28 +0100

    Apply suggestions from code review
    Co-authored-by: Marianne Corvellec <marianne.corvellec@ens-lyon.org>

--- skimage/_shared/utils.py
+++ skimage/_shared/utils.py
@@ -155,7 +155,7 @@ class PatchClassRepr(type):
 class DEPRECATED(metaclass=PatchClassRepr):
     """Signal value to help with deprecating parameters that use None.
 
-    This is a proxy object, used to signal that a parameter has not been set,
+    This is a proxy object, used to signal that a parameter has not been set.
     This is useful if ``None`` is already used for a different purpose or just
     to highlight a deprecated parameter in the signature.
     """
@@ -176,9 +176,9 @@ class deprecate_parameter:
     template : str, optional
         If given, this message template is used instead of the default one.
     new_name : str, optional
-        If given, the default message will recommend a new parameter and an
-        error will be raised if the user uses the old and new parameter at the
-        same time.
+        If given, the default message will recommend the new parameter name and an
+        error will be raised if the user uses both old and new names for the
+        same parameter.
     modify_docstring : bool, optional
         If the wrapped function has a docstring, add the deprecated parameters
         to the "Other Parameters" section.
