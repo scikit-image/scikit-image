fdc03bddb2d4aea4b42b2ff0fdc90b37ce9ec241
Grzegorz Bokota <bokota+github@gmail.com>
2023-12-13 14:00:16 +0100

    use prange in map_appray implementation

--- skimage/util/_remap.pyx
+++ skimage/util/_remap.pyx
@@ -9,6 +9,7 @@ def _map_array(np_anyint[:] inarr, np_numeric[:] outarr,
     # build the map from the input and output vectors
     cdef size_t i, n_map, n_array
     cdef unordered_map[np_anyint, np_numeric] lut
+    cdef unordered_map[np_anyint, np_numeric].iterator it
     n_map = inval.shape[0]
     for i in range(n_map):
         lut[inval[i]] = outval[i]
@@ -18,5 +19,7 @@ def _map_array(np_anyint[:] inarr, np_numeric[:] outarr,
     #  "Unsigned index type not allowed before OpenMP 3.0"
     # and didn't seem to be any faster
     # for i in prange(n_array, nogil=True): #
-    for i in range(n_array):
-        outarr[i] = lut[inarr[i]]
+    for i in prange(n_array):
+        it = lut.find(inarr[i])
+        if it != lut.end():
+            outarr[i] = dereference(it).second 
