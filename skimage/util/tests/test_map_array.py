aecf6f5f6200474225e0f7de6fcf41f6bc73b386
Lars Grüter <lagru@mailbox.org>
2023-12-15 14:46:20 +0100

    Test supported in & out dtypes separately
    Co-authored-by: Juan Nunez-Iglesias <jni@fastmail.com>

--- skimage/util/tests/test_map_array.py
+++ skimage/util/tests/test_map_array.py
@@ -5,20 +5,32 @@ from skimage.util._map_array import map_array, ArrayMap
 from skimage._shared import testing
 
 
-@pytest.mark.parametrize(
-    "dtype",
-    [np.uint8, np.uint16, np.uint32, np.uint64, np.int8, np.int16, np.int32, np.int64],
-)
-def test_map_array_simple(dtype):
-    input_arr = np.array([0, 2, 0, 3, 4, 5, 0], dtype=dtype)
-    input_vals = np.array([1, 2, 3, 4, 6], dtype=dtype)[::-1]
-    output_vals = np.array([6, 7, 8, 9, 10], dtype=dtype)[::-1]
-    desired = np.array([0, 7, 0, 8, 9, 0, 0], dtype=dtype)
+_map_array_dtypes_in = [
+    np.uint8,
+    np.uint16,
+    np.uint32,
+    np.uint64,
+    np.int8,
+    np.int16,
+    np.int32,
+    np.int64,
+]
+
+_map_array_dtypes_out = _map_array_dtypes_in + [np.float32, np.float64]
+
+
+@pytest.mark.parametrize("dtype_in", _map_array_dtypes_in)
+@pytest.mark.parametrize("dtype_out", _map_array_dtypes_out)
+def test_map_array_simple(dtype_in, dtype_out):
+    input_arr = np.array([0, 2, 0, 3, 4, 5, 0], dtype=dtype_in)
+    input_vals = np.array([1, 2, 3, 4, 6], dtype=dtype_in)[::-1]
+    output_vals = np.array([6, 7, 8, 9, 10], dtype=dtype_out)[::-1]
+    desired = np.array([0, 7, 0, 8, 9, 0, 0], dtype=dtype_out)
     result = map_array(
         input_arr=input_arr, input_vals=input_vals, output_vals=output_vals
     )
     np.testing.assert_array_equal(result, desired)
-    assert result.dtype == dtype
+    assert result.dtype == dtype_out
 
 
 def test_map_array_incorrect_output_shape():
