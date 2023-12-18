1860d4e30d51cccf2e8df6906418d1e639916680
Juan Nunez-Iglesias <jni@fastmail.com>
2023-12-18 20:26:59 +1100

    Add test for out_array

--- skimage/util/tests/test_map_array.py
+++ skimage/util/tests/test_map_array.py
@@ -21,13 +21,17 @@ _map_array_dtypes_out = _map_array_dtypes_in + [np.float32, np.float64]
 
 @pytest.mark.parametrize("dtype_in", _map_array_dtypes_in)
 @pytest.mark.parametrize("dtype_out", _map_array_dtypes_out)
-def test_map_array_simple(dtype_in, dtype_out):
+@pytest.mark.parametrize("out_array", [True, False])
+def test_map_array_simple(dtype_in, dtype_out, out_array):
     input_arr = np.array([0, 2, 0, 3, 4, 5, 0], dtype=dtype_in)
     input_vals = np.array([1, 2, 3, 4, 6], dtype=dtype_in)[::-1]
     output_vals = np.array([6, 7, 8, 9, 10], dtype=dtype_out)[::-1]
     desired = np.array([0, 7, 0, 8, 9, 0, 0], dtype=dtype_out)
+    out = None
+    if out_array:
+        out = np.random.randint(2**23, size=desired.shape).astype(dtype_out)
     result = map_array(
-        input_arr=input_arr, input_vals=input_vals, output_vals=output_vals
+        input_arr=input_arr, input_vals=input_vals, output_vals=output_vals, out=out
     )
     np.testing.assert_array_equal(result, desired)
     assert result.dtype == dtype_out
