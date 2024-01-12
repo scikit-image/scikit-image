f2e3c037b24daeeeb787255d73119555a338a2c7
Lars Grüter <lagru@mailbox.org>
2024-01-12 23:51:54 +0100

    Deprecate and vendor lookfor
    Unfortunately we relied on NumPy's lookfor which is promptly removed in
    NumPy 2.0. Technically, if we don't want to make a breaking release we
    need to deprecate the function in a deprecation cycle for which we need
    to vendor NumPy's lookfor.
    
    Though, skimage.lookfor is an interactive function, so we might get away
    with not vendoring NumPy's lookfor, and just making it return a
    deprecation warning...

--- skimage/util/lookfor.py
+++ skimage/util/lookfor.py
@@ -1,7 +1,10 @@
-import numpy as np
 import sys
 
+from .._shared.utils import deprecate_func
+from .._vendored.numpy_lookfor import lookfor as _lookfor
 
+
+@deprecate_func(deprecated_version="0.23", removed_version="0.26")
 def lookfor(what):
     """Do a keyword search on scikit-image docstrings.
 
@@ -21,4 +24,4 @@ def lookfor(what):
     skimage.util.regular_grid
         Find `n_points` regularly spaced along `ar_shape`.
     """
-    return np.lookfor(what, sys.modules[__name__.split('.')[0]])
+    return _lookfor(what, sys.modules[__name__.split('.')[0]])
