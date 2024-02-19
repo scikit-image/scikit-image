70b4c404774b635ab99a71ccee1712cf75d695dd
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2024-02-19 19:31:43 +0100

    Use pytest.mark.parametrize in old test

--- skimage/feature/tests/test_util.py
+++ skimage/feature/tests/test_util.py
@@ -54,12 +54,9 @@ def test_mask_border_keypoints():
 
 
 @pytest.mark.skipif(not has_mpl, reason="Matplotlib not installed")
-def test_plot_matches():
-    from matplotlib import pyplot as plt
-
-    fig, ax = plt.subplots(nrows=1, ncols=1)
-
-    shapes = (
+@pytest.mark.parametrize(
+    "shapes",
+    [
         ((10, 10), (10, 10)),
         ((10, 10), (12, 10)),
         ((10, 10), (10, 12)),
@@ -67,7 +64,12 @@ def test_plot_matches():
         ((12, 10), (10, 10)),
         ((10, 12), (10, 10)),
         ((12, 12), (10, 10)),
-    )
+    ],
+)
+def test_plot_matches(shapes):
+    from matplotlib import pyplot as plt
+
+    fig, ax = plt.subplots(nrows=1, ncols=1)
 
     keypoints1 = 10 * np.random.rand(10, 2)
     keypoints2 = 10 * np.random.rand(10, 2)
@@ -75,18 +77,14 @@ def test_plot_matches():
     idxs2 = np.random.randint(10, size=10)
     matches = np.column_stack((idxs1, idxs2))
 
-    for shape1, shape2 in shapes:
-        img1 = np.zeros(shape1)
-        img2 = np.zeros(shape2)
-        plot_matches(ax, img1, img2, keypoints1, keypoints2, matches)
-        plot_matches(ax, img1, img2, keypoints1, keypoints2, matches, only_matches=True)
-        plot_matches(
-            ax, img1, img2, keypoints1, keypoints2, matches, keypoints_color='r'
-        )
-        plot_matches(ax, img1, img2, keypoints1, keypoints2, matches, matches_color='r')
-        plot_matches(
-            ax, img1, img2, keypoints1, keypoints2, matches, alignment='vertical'
-        )
+    shape1, shape2 = shapes
+    img1 = np.zeros(shape1)
+    img2 = np.zeros(shape2)
+    plot_matches(ax, img1, img2, keypoints1, keypoints2, matches)
+    plot_matches(ax, img1, img2, keypoints1, keypoints2, matches, only_matches=True)
+    plot_matches(ax, img1, img2, keypoints1, keypoints2, matches, keypoints_color='r')
+    plot_matches(ax, img1, img2, keypoints1, keypoints2, matches, matches_color='r')
+    plot_matches(ax, img1, img2, keypoints1, keypoints2, matches, alignment='vertical')
 
 
 @pytest.mark.skipif(not has_mpl, reason="Matplotlib not installed")
