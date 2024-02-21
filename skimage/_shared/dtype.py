6e39a2e0037df17029bb8ab99e32b7611ea75431
Lars Grüter <lagru@mailbox.org>
2024-02-21 13:41:16 +0100

    Remove dtype lists
    While these might be useful to define in one place to reuse in tests,
    keep the PR focused for now.

--- skimage/_shared/dtype.py
+++ skimage/_shared/dtype.py
@@ -1,30 +1,5 @@
 import numpy as np
 
-# Define classes of supported dtypes and Python scalar types
-# Variables ending in `_dtypes` only contain numpy.dtypes of the respective
-# class; variables ending in `_types` additionally include Python scalar types.
-signed_integer_dtypes = {np.int8, np.int16, np.int32, np.int64}
-signed_integer_types = signed_integer_dtypes | {int}
-
-unsigned_integer_dtypes = {np.uint8, np.uint16, np.uint32, np.uint64}
-
-integer_dtypes = signed_integer_dtypes | unsigned_integer_dtypes
-integer_types = signed_integer_types | unsigned_integer_dtypes
-
-floating_dtypes = {np.float16, np.float32, np.float64}
-floating_types = floating_dtypes | {float}
-
-complex_dtypes = {np.complex64, np.complex128}
-complex_types = complex_dtypes | {complex}
-
-inexact_dtypes = floating_dtypes | complex_dtypes
-inexact_types = floating_types | complex_types
-
-bool_types = {np.dtype(bool), bool}
-
-numeric_dtypes = integer_dtypes | inexact_dtypes | {np.bool_}
-numeric_types = integer_types | inexact_types | bool_types
-
 
 def numeric_dtype_min_max(dtype):
     """Return minimum and maximum representable value for a given dtype.
