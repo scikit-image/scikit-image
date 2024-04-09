9149b19895b9d8fd0cedff6b81587e9c5e93a231
aldehyde <aldehyde@hdu.edu.cn>
2024-04-09 15:24:02 +0800

    Fix a typo in code comment #7380

--- skimage/transform/_geometric.py
+++ skimage/transform/_geometric.py
@@ -1264,7 +1264,7 @@ class EuclideanTransform(ProjectiveTransform):
 
     where the homogeneous transformation matrix is::
 
-        [[a0  b0  a1]
+        [[a0  -b0  a1]
          [b0  a0  b1]
          [0   0    1]]
 
