58a68364040b85a639969a4531ae345714902894
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-06-05 20:03:22 +0530

    Revert "Remove `__init__` dunder from mocked thread exec"
    This reverts commit 0bcf3c091c22715becb429b2e23af27c51c5d66f.

--- skimage/feature/_basic_features.py
+++ skimage/feature/_basic_features.py
@@ -12,6 +12,8 @@ else:
 
     # Threading isn't supported on WASM, mock ThreadPoolExecutor as a fallback
     class PoolExecutor(AbstractContextManager):
+        def __init__(self, *_, **__):
+            pass
 
         def __exit__(self, exc_type, exc_val, exc_tb):
             pass
