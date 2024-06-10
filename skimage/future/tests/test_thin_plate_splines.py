756433896badacbed006a84e63ad8058a68f27ed
Lars Grüter <lagru@mailbox.org>
2024-06-10 10:56:50 -0700

    Overhaul TPS test suite
    Remove tests that rely on internal implementation details and add one
    that tests ThinPlateSplineTransform together with warp.

--- skimage/future/tests/test_thin_plate_splines.py
+++ skimage/future/tests/test_thin_plate_splines.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
-from scipy.spatial import distance_matrix
 
+import skimage as ski
 from skimage.future import ThinPlateSplineTransform
 
 SRC = np.array([[0, 0], [0, 5], [5, 5], [5, 0]])
@@ -10,109 +10,50 @@ DST = np.array([[5, 0], [0, 0], [0, 5], [5, 5]])
 
 
 class TestThinPlateSplineTransform:
-    def test_tps_transform_init(self):
-        tps = ThinPlateSplineTransform()
-
-        # Test that _estimated is initialized to False
-        assert tps._estimated is False
-        assert tps._spline_mappings is None
-        assert tps.src is None
-
     def test_call_before_estimation(self):
         tps = ThinPlateSplineTransform()
-        coords = np.array([[0, 0], [0, 5]])
+        assert tps.src is None
         with pytest.raises(ValueError, match="Transformation is undefined"):
-            tps(coords)
+            tps(SRC)
 
     def test_call_invalid_coords_shape(self):
         tps = ThinPlateSplineTransform()
+        tps.estimate(SRC, DST)
         coords = np.array([1, 2, 3])
-        with pytest.raises(ValueError):
+        with pytest.raises(
+            ValueError, match=r"Input `coords` must have shape \(N, 2\)"
+        ):
             tps(coords)
 
-    def test_call_valid_transformation(self):
+    def test_call_on_SRC(self):
         tps = ThinPlateSplineTransform()
         tps.estimate(SRC, DST)
-        coords = np.array([[2.5, 2.5], [1, 1]])
-        result = tps(coords)
-        assert result.shape == (2, 2)
+        result = tps(SRC)
+        np.testing.assert_allclose(result, DST, atol=1e-15)
 
     def test_tps_transform_inverse(self):
         tps = ThinPlateSplineTransform()
+        tps.estimate(SRC, DST)
         with pytest.raises(NotImplementedError):
             tps.inverse()
 
-    def test_radial_distance_basic(self):
-        tps = ThinPlateSplineTransform()
-        src = np.array([[0, 0], [0, 5], [5, 5]])
-        tps.src = src
-        coords = np.array([[0, 0], [0, 5]])
-        expected_dists = distance_matrix(coords, src)
-        expected_kernel = tps._radial_basis_kernel(expected_dists)
-        result = tps._radial_distance(coords)
-        np.testing.assert_array_equal(result, expected_kernel)
-
-    def test_radial_basis_small_values(self):
-        r = np.array([[1e-10, 1e-8], [1e-6, 1e-4]])
-        expected = np.array([[0.0, 0.0], [0.0, 0.0]])
-        result = ThinPlateSplineTransform._radial_basis_kernel(r)
-        np.testing.assert_almost_equal(result, expected, decimal=6)
-
-    def test_tps_transform_estimation(self):
-        src = np.array([[0, 1], [-1, 0], [0, -1], [1, 0]])
-        dst = np.array([[0, 0.75], [-1, 0.25], [0, -1.25], [1, 0.25]], dtype=np.float32)
-        tps = ThinPlateSplineTransform()
-        desired_spline_mappings = np.array(
-            [
-                [0.0, -0.0902],
-                [0.0, 0.0902],
-                [0.0, -0.0902],
-                [0.0, 0.0902],
-                [0.0, 0.0],
-                [1.0, 0.0],
-                [0.0, 1.0],
-            ]
-        )
-
-        # Ensure that the initial state is as expected
-        assert tps._spline_mappings is None
-        assert tps.src is None
-
-        # Perform estimation
-        assert tps.estimate(src, dst) is True
-        np.testing.assert_array_equal(tps.src, src)
-
-        assert tps._spline_mappings.shape == (src.shape[0] + 3, 2)
-
-        np.testing.assert_allclose(
-            tps._spline_mappings,
-            desired_spline_mappings,
-            rtol=0.1,
-            atol=1e-16,
-        )
-
-    def test_tps_transform_estimation_failure(self):
-        # Test the estimate method when the estimation fails
-        tps = ThinPlateSplineTransform()
+    def test_tps_estimation_faulty_input(self):
         src = np.array([[0, 0], [0, 5], [5, 5], [5, 0]])
         dst = np.array([[5, 0], [0, 0], [0, 5]])
 
-        # Ensure that the initial state is as expected
-        assert tps._estimated is False
-        assert tps._spline_mappings is None
+        tps = ThinPlateSplineTransform()
         assert tps.src is None
 
-        # Perform the estimation, which should fail due to the mismatched number of points
         with pytest.raises(ValueError, match="Shape of `src` and `dst` didn't match"):
             tps.estimate(src, dst)
 
-        # # Check if src and dst have fewer than 3 points
+        less_than_3pts = np.array([[0, 0], [0, 5]])
         with pytest.raises(ValueError, match="Need at least 3 points"):
-            src_less_than_3pts = np.array([[0, 0], [0, 5]])
-            tps.estimate(src_less_than_3pts, dst)
-
-            dst_less_than_3pts = np.array([[0, 0], [0, 5]])
-            tps.estimate(src, dst_less_than_3pts)
+            tps.estimate(less_than_3pts, dst)
+        with pytest.raises(ValueError, match="Need at least 3 points"):
+            tps.estimate(src, less_than_3pts)
+        with pytest.raises(ValueError, match="Need at least 3 points"):
+            tps.estimate(less_than_3pts, less_than_3pts)
 
         not_2d = np.array([0, 1, 2, 3])
         with pytest.raises(ValueError, match=".*`src` must be a 2-dimensional array"):
@@ -120,21 +61,17 @@ class TestThinPlateSplineTransform:
         with pytest.raises(ValueError, match=".*`dst` must be a 2-dimensional array"):
             tps.estimate(src, not_2d)
 
-        # Check that src or dst not being (N, 2) does error
-        with pytest.raises(ValueError):
-            src_not_2d = np.array([0, 1, 2, 3])
-            tps.estimate(src_not_2d, dst)
-
-            dst_not_2d = np.array([[1, 2, 3], [4, 5, 6]])
-            tps.estimate(src, dst_not_2d)
-
-        # Check that, when the estimation fails, the instance attributes remain unchanged
-        assert tps._estimated is False
-        assert tps._spline_mappings is None
+        # When the estimation fails, the instance attributes remain unchanged
         assert tps.src is None
 
-    def test_estimate_spline_mappings(self):
-        tps = ThinPlateSplineTransform()
-        tps.estimate(SRC, DST)
-        assert tps._spline_mappings is not None
-        assert tps._spline_mappings.shape == (len(SRC) + 3, 2)
+    def test_rotate(self):
+        image = ski.data.astronaut()
+        desired = ski.transform.rotate(image, angle=90)
+
+        src = np.array([[0, 0], [0, 511], [511, 511], [511, 0]])
+        dst = np.array([[511, 0], [0, 0], [0, 511], [511, 511]])
+        tps = ski.future.ThinPlateSplineTransform()
+        tps.estimate(src, dst)
+        result = ski.transform.warp(image, tps)
+
+        np.testing.assert_allclose(result, desired, atol=1e-13)
