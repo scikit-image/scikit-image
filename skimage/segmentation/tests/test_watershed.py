2be64418f330a35309d098f593faad7bffc3d362
Lars Grüter <lagru@mailbox.org>
2024-07-13 11:35:39 -0700

    Add bug report based test on off-center markers
    Taken from the bug report at
    https://www.github.com/scikit-image/scikit-image/issues/6632

--- skimage/segmentation/tests/test_watershed.py
+++ skimage/segmentation/tests/test_watershed.py
@@ -8,6 +8,7 @@ import numpy as np
 import pytest
 from scipy import ndimage as ndi
 
+import skimage.measure
 from skimage._shared.filters import gaussian
 from skimage.feature import peak_local_max
 from skimage.measure import label
@@ -965,3 +966,35 @@ def test_connectivity():
 
     for lab, area in zip(range(5), [61824, 3653, 20466, 12385, 11292]):
         assert np.sum(labels_c2 == lab) == area
+
+
+def test_offcenter_markers():
+    # Example taken from bug report
+    # https://github.com/scikit-image/scikit-image/issues/6632
+
+    # Generate an initial image with two overlapping circles
+    x, y = np.indices((80, 80))
+    x1, y1, x2, y2 = 28, 28, 44, 52
+    r1, r2 = 16, 20
+    mask_circle1 = (x - x1) ** 2 + (y - y1) ** 2 < r1**2
+    mask_circle2 = (x - x2) ** 2 + (y - y2) ** 2 < r2**2
+    image = np.logical_or(mask_circle1, mask_circle2)
+
+    # Now we want to separate the two objects in image
+    # Generate the markers as local maxima of the distance to the background
+    distance = ndi.distance_transform_edt(image)
+
+    coords = peak_local_max(distance, footprint=np.ones((3, 3)), labels=image)
+    coords[:, 0] += 6
+    mask = np.zeros(distance.shape, dtype=bool)
+    mask[tuple(coords.T)] = True
+    markers, _ = ndi.label(mask)
+    labels = watershed(-distance, markers, mask=image)
+
+    # Assert pixel count from reviewed reproducing example in bug report
+    # Generally, assert both objects have covered their basin
+    props = skimage.measure.regionprops(labels)
+    assert props[0].eccentricity <= 0.5
+    assert props[1].eccentricity <= 0.5
+    assert props[0].num_pixels == 732
+    assert props[1].num_pixels == 1206
