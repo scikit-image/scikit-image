0995f325230cdf9af5a052145bef9c40422abc86
Piyush Amitabh <pamitabh@uoregon.edu>
2024-07-28 18:02:00 -0700

    Add rotation feature to draw.ellipsoid function

--- skimage/draw/draw3d.py
+++ skimage/draw/draw3d.py
@@ -2,22 +2,26 @@ import numpy as np
 from scipy.special import elliprg
 
 
-def ellipsoid(a, b, c, spacing=(1.0, 1.0, 1.0), levelset=False):
-    """Generate ellipsoid for given semi-axis lengths.
-
-    The respective semi-axis lengths are given along three dimensions in
-    Cartesian coordinates. Each dimension may use a different grid spacing.
+# works! using the explicit formula from https://mathworld.wolfram.com/Ellipsoid.html
+def ellipsoid(a, b, c, spacing=(1.0, 1.0, 1.0), rotation=(0.0, 0.0, 0.0), levelset=False):
+    """
+    Generates ellipsoid with semimajor axes aligned with grid dimensions
+    on grid with specified `spacing`.
 
     Parameters
     ----------
     a : float
-        Length of semi-axis along x-axis.
+        Length of semimajor axis aligned with plane-axis, coming out of plane.
     b : float
-        Length of semi-axis along y-axis.
+        Length of semimajor axis aligned with row-axis, going vertically down.
     c : float
-        Length of semi-axis along z-axis.
+        Length of semimajor axis aligned with column-axis, going horizontally right.
     spacing : 3-tuple of floats
-        Grid spacing in three spatial dimensions.
+        Spacing in three spatial dimensions.
+    rotation : 3-tuple of floats, optional (default (0.0, 0.0, 0.0))
+        Set the ellipsoid rotation in Euler angles (in radians) in the order (phi, theta, psi),
+        where rotation angles are defined in counter-clockwise direction using the right-hand rule.
+        `phi` about plane-axis, `theta` about intermediate row-axis, and `psi` about the final plane-axis.
     levelset : bool
         If True, returns the level set for this ellipsoid (signed level
         set about zero, with positive denoting interior) as np.float64.
@@ -30,15 +34,56 @@ def ellipsoid(a, b, c, spacing=(1.0, 1.0, 1.0), levelset=False):
         Boolean dtype unless `levelset=True`, in which case a float array is
         returned with the level set above 0.0 representing the ellipsoid.
 
+    Notes
+    -----
+    The returned ellipsoid satisfies the following equation::
+        ((plane_rot / a) ** 2 + (row_rot / float(b)) ** 2 + (col_rot / float(c)) ** 2) = 1
+
+    where plane_rot, row_rot, and col_rot are the passive rotated coordinates generated using the Rotation matrix `R`::
+        [plane_rot, row_rot, col_rot].T = R[2,0,1].T @ [plane, row, col].T
+
+    where R is the standard active rotation matrix for the given Euler angles (theta, phi, psi) and coordinates (x, y, z) [1]_::
+        R = [[cos(psi)*cos(phi) - cos(theta)*sin(phi)*sin(psi), -sin(psi)*cos(phi) - cos(theta)*sin(phi)*cos(psi), sin(theta)*sin(phi)],
+             [cos(psi)*sin(phi) + cos(theta)*cos(phi)*sin(psi), -sin(psi)*sin(phi) + cos(theta)*cos(phi)*cos(psi), -sin(theta)*cos(phi)],
+             [sin(psi)*sin(theta), cos(psi)*sin(theta), cos(theta)]]
+
+    References
+    ----------
+    .. [1] Weisstein, Eric W. "Euler Angles." From MathWorld--A Wolfram Web Resource.
+            https://mathworld.wolfram.com/EulerAngles.html
+
     """
     if (a <= 0) or (b <= 0) or (c <= 0):
         raise ValueError('Parameters a, b, and c must all be > 0')
 
     offset = np.r_[1, 1, 1] * np.r_[spacing]
+    phi, theta, psi = rotation
+    # Define the standard rotation matrix for the given Euler angles defined for the coordinate system (x,y,z)
+    standard_rotation_matrix = np.array(
+        [
+            [
+                np.cos(psi) * np.cos(phi) - np.cos(theta) * np.sin(phi) * np.sin(psi),
+                np.cos(psi) * np.sin(phi) + np.cos(theta) * np.cos(phi) * np.sin(psi),
+                np.sin(psi) * np.sin(theta),
+            ],
+            [
+                -np.sin(psi) * np.cos(phi) - np.cos(theta) * np.sin(phi) * np.cos(psi),
+                -np.sin(psi) * np.sin(phi) + np.cos(theta) * np.cos(phi) * np.cos(psi),
+                np.cos(psi) * np.sin(theta),
+            ],
+            [np.sin(theta) * np.sin(phi), -np.sin(theta) * np.cos(phi), np.cos(theta)],
+        ]
+    )
+    # Switch the elemets of the rotation matrix to match our convention of (plane, row, col)-axis <-> (z, x, y)-axis
+    permutation = [2, 0, 1]
+    rotation_matrix = standard_rotation_matrix[permutation, :][:, permutation]
 
+    # Rotate the ellipsoid axes i.e. active rotation
+    abc_rot = rotation_matrix @ np.r_[a, b, c]
+    a_rot, b_rot, c_rot = np.abs(abc_rot)
     # Calculate limits, and ensure output volume is odd & symmetric
-    low = np.ceil(-np.r_[a, b, c] - offset)
-    high = np.floor(np.r_[a, b, c] + offset + 1)
+    low = np.ceil(-np.r_[a_rot, b_rot, c_rot] - offset)
+    high = np.floor(np.r_[a_rot, b_rot, c_rot] + offset + 1)
 
     for dim in range(3):
         if (high[dim] - low[dim]) % 2 == 0:
@@ -48,16 +93,24 @@ def ellipsoid(a, b, c, spacing=(1.0, 1.0, 1.0), levelset=False):
             low[dim] -= np.max(num[num < 0])
 
     # Generate (anisotropic) spatial grid
-    x, y, z = np.mgrid[
+    plane_grid, row_grid, col_grid = np.mgrid[
         low[0] : high[0] : spacing[0],
         low[1] : high[1] : spacing[1],
         low[2] : high[2] : spacing[2],
     ]
+    prc_grid = np.vtack((plane_grid.flatten(), row_grid.flatten(), col_grid.flatten()))
+    # Rotate the grid i.e. passive rotation
+    prc_grid_rot = rotation_matrix.T @ prc_grid
+
+    # Extract the rotated coordinates
+    plane_grid_rot = prc_grid_rot[0, :].reshape(plane_grid.shape)
+    row_grid_rot = prc_grid_rot[1, :].reshape(row_grid.shape)
+    col_grid_rot = prc_grid_rot[2, :].reshape(col_grid.shape)
 
     if not levelset:
-        arr = ((x / float(a)) ** 2 + (y / float(b)) ** 2 + (z / float(c)) ** 2) <= 1
+        arr = ((plane_grid_rot / float(a)) ** 2 + (row_grid_rot / float(b)) ** 2 + (col_grid_rot / float(c)) ** 2) <= 1
     else:
-        arr = ((x / float(a)) ** 2 + (y / float(b)) ** 2 + (z / float(c)) ** 2) - 1
+        arr = ((plane_grid_rot / float(a)) ** 2 + (row_grid_rot / float(b)) ** 2 + (col_grid_rot / float(c)) ** 2) - 1
 
     return arr
 
