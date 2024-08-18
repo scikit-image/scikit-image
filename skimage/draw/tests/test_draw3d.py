79eb0911701128233a5ba4f149082b164af811b8
Piyush Amitabh <pamitabh@uoregon.edu>
2024-08-17 18:38:28 -0700

    added tests for the new rotation parameter

--- skimage/draw/tests/test_draw3d.py
+++ skimage/draw/tests/test_draw3d.py
@@ -1,8 +1,8 @@
 import numpy as np
-from skimage._shared.testing import assert_array_equal, assert_allclose
 
-from skimage.draw import ellipsoid, ellipsoid_stats, rectangle
 from skimage._shared import testing
+from skimage._shared.testing import assert_allclose, assert_array_equal
+from skimage.draw import ellipsoid, ellipsoid_stats, rectangle
 
 
 def test_ellipsoid_sign_parameters1():
@@ -22,6 +22,8 @@ def test_ellipsoid_sign_parameters3():
 
 def test_ellipsoid_bool():
     test = ellipsoid(2, 2, 2)[1:-1, 1:-1, 1:-1]
+    test_rotation = ellipsoid(2, 2, 2, rotation=(np.pi / 2, np.pi / 2, np.pi / 2))
+    test_rotation = test_rotation[1:-1, 1:-1, 1:-1]
     test_anisotropic = ellipsoid(2, 2, 4, spacing=(1.0, 1.0, 2.0))
     test_anisotropic = test_anisotropic[1:-1, 1:-1, 1:-1]
 
@@ -66,11 +68,16 @@ def test_ellipsoid_bool():
     )
 
     assert_array_equal(test, expected.astype(bool))
+    assert_array_equal(test_rotation, expected.astype(bool))
     assert_array_equal(test_anisotropic, expected.astype(bool))
 
 
 def test_ellipsoid_levelset():
     test = ellipsoid(2, 2, 2, levelset=True)[1:-1, 1:-1, 1:-1]
+    test_rotation = ellipsoid(
+        2, 2, 2, rotation=(np.pi / 2, np.pi / 2, np.pi / 2), levelset=True
+    )
+    test_rotation = test_rotation[1:-1, 1:-1, 1:-1]
     test_anisotropic = ellipsoid(2, 2, 4, spacing=(1.0, 1.0, 2.0), levelset=True)
     test_anisotropic = test_anisotropic[1:-1, 1:-1, 1:-1]
 
@@ -110,6 +117,94 @@ def test_ellipsoid_levelset():
                 [1.0, 0.25, 0.0, 0.25, 1.0],
                 [1.25, 0.5, 0.25, 0.5, 1.25],
                 [2.0, 1.25, 1.0, 1.25, 2.0],
+            ],
+        ]
+    )
+
+    assert_allclose(test, expected)
+    assert_allclose(test_rotation, expected)
+    assert_allclose(test_anisotropic, expected)
+
+
+def test_ellipsoid_rotation_bool():
+    test = ellipsoid(a=2, b=1, c=1, rotation=(np.pi / 2, np.pi / 2, np.pi / 2))
+    test = test[1:-1, 1:-1, 1:-1]
+    test_anisotropic = ellipsoid(
+        a=2,
+        b=1,
+        c=2,
+        rotation=(np.pi / 2, np.pi / 2, np.pi / 2),
+        spacing=(1.0, 1.0, 2.0),
+    )
+    test_anisotropic = test_anisotropic[1:-1, 1:-1, 1:-1]
+
+    expected = np.array(
+        [
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 1, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ],
+            [
+                [0, 1, 0],
+                [0, 1, 0],
+                [1, 1, 1],
+                [0, 1, 0],
+                [0, 1, 0],
+            ],
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 1, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ],
+        ]
+    )
+
+    assert_array_equal(test, expected.astype(bool))
+    assert_array_equal(test_anisotropic, expected.astype(bool))
+
+
+def test_ellipsoid_rotation_levelset():
+    test = ellipsoid(
+        a=2, b=1, c=1, rotation=(np.pi / 2, np.pi / 2, np.pi / 2), levelset=True
+    )
+    test = test[1:-1, 1:-1, 1:-1]
+    test_anisotropic = ellipsoid(
+        a=2,
+        b=1,
+        c=2,
+        rotation=(np.pi / 2, np.pi / 2, np.pi / 2),
+        spacing=(1.0, 1.0, 2.0),
+        levelset=True,
+    )
+    test_anisotropic = test_anisotropic[1:-1, 1:-1, 1:-1]
+
+    expected = np.array(
+        [
+            [
+                [2.0, 1.0, 2.0],
+                [1.25, 0.25, 1.25],
+                [1.0, 0.0, 1.0],
+                [1.25, 0.25, 1.25],
+                [2.0, 1.0, 2.0],
+            ],
+            [
+                [1.0, 0.0, 1.0],
+                [0.25, -0.75, 0.25],
+                [0.0, -1.0, 0.0],
+                [0.25, -0.75, 0.25],
+                [1.0, 0.0, 1.0],
+            ],
+            [
+                [2.0, 1.0, 2.0],
+                [1.25, 0.25, 1.25],
+                [1.0, 0.0, 1.0],
+                [1.25, 0.25, 1.25],
+                [2.0, 1.0, 2.0],
             ],
         ]
     )
