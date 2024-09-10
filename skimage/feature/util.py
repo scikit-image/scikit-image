19fda51ea07a6a1f4e32ae61352f3893facc5f96
Michael Bratsch <michael.bratsch83@gmail.com>
2024-09-10 22:24:08 +0200

    Fix seed for deterministic colors

--- skimage/feature/util.py
+++ skimage/feature/util.py
@@ -145,7 +145,7 @@ def plot_matched_features(
     ax.imshow(image, cmap='gray')
     ax.axis((0, image0.shape[1] + offset[1], image0.shape[0] + offset[0], 0))
 
-    rng = np.random.default_rng()
+    rng = np.random.default_rng(seed=0)
 
     for i in range(matches.shape[0]):
         idx0 = matches[i, 0]
