befc125f2456ab19ac49064d7c3139ebbeeda464
Lars Grüter <lagru@mailbox.org>
2024-09-26 17:42:23 +0200

    Deprecate plugin infrastructure in skimage.io
    but keep imread, imsave and imread_collection around to work as simple
    wrappers for imageio down the line.

--- skimage/io/tests/test_io.py
+++ skimage/io/tests/test_io.py
@@ -6,7 +6,7 @@ import numpy as np
 import pytest
 
 from skimage import io
-from skimage._shared.testing import assert_array_equal, fetch
+from skimage._shared.testing import assert_array_equal, fetch, assert_stacklevel
 from skimage._shared._dependency_checks import is_wasm
 from skimage.data import data_dir
 
@@ -120,3 +120,42 @@ def test_failed_temporary_file(monkeypatch, error_class):
         )
         with pytest.raises(error_class):
             io.imread(image_url)
+
+
+@pytest.mark.parametrize(
+    # Test `**plugin_args` with `mode`
+    "kwarg",
+    [{"plugin": None}, {"plugin": "imageio"}, {"mode": "r"}],
+)
+def test_plugin_deprecation_on_imread(kwarg):
+    path = fetch("data/multipage.tif")
+    regex = ".*Use imageio or a similar package instead.*"
+    with pytest.warns(FutureWarning, match=regex) as record:
+        io.imread(path, **kwarg)
+    assert len(record) == 1
+    assert_stacklevel(record, offset=-2)
+
+
+@pytest.mark.parametrize(
+    # Test `**plugin_args` with `mode`
+    "kwarg",
+    [{"plugin": None}, {"plugin": "imageio"}, {"mode": "w"}],
+)
+def test_plugin_deprecation_on_imsave(kwarg, tmp_path):
+    path = tmp_path / "test.tif"
+    array = np.array([0, 1], dtype=float)
+    regex = ".*Use imageio or a similar package instead.*"
+    with pytest.warns(FutureWarning, match=regex) as record:
+        io.imsave(path, array, **kwarg)
+    assert len(record) == 1
+    assert_stacklevel(record, offset=-2)
+
+
+@pytest.mark.parametrize("kwarg", [{"plugin": None}, {"plugin": "imageio"}])
+def test_plugin_deprecation_on_imread_collection(kwarg):
+    pattern = data_dir + "*.png"
+    regex = ".*Use imageio or a similar package instead.*"
+    with pytest.warns(FutureWarning, match=regex) as record:
+        io.imread_collection(pattern, **kwarg)
+    assert len(record) == 1
+    assert_stacklevel(record, offset=-2)
