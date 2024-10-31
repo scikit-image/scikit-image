42ddee38c5bbdc32549c6bd2bf1ba8192d8ac0ba
scrimpys <scrimpy.sailor@gmail.com>
2024-10-31 15:23:16 +0800

    Update _blur_effect.py
    Apologies. I hope I got it right this time.  This is all new for me.  I certainly see the reasoning for moving it up a line. Why waste time doing a calculation if the value of M1 is zero.  As for the rest of the process in committing a change and other processes involved please let me know what I need to do.  As I said this is all new to me.

--- skimage/measure/_blur_effect.py
+++ skimage/measure/_blur_effect.py
@@ -84,9 +84,9 @@ def blur_effect(image, h_size=11, channel_axis=None, reduce_func=np.max):
         im_blur = np.abs(sobel(filt_im, axis=ax))
         T = np.maximum(0, im_sharp - im_blur)
         M1 = np.sum(im_sharp[slices])
-        M2 = np.sum(T[slices])
         if M1 == 0:
             raise ZeroDivisionError
+        M2 = np.sum(T[slices])
         B.append(np.abs(M1 - M2) / M1)
 
     return B if reduce_func is None else reduce_func(B)
