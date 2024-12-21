54f778438bf59e5a4f6073bbd21caa6ec1dd3de5
Michael Bratsch <michael.bratsch83@gmail.com>
2024-12-21 14:22:13 +0100

    Add blur test for constant images

--- skimage/measure/tests/test_blur_effect.py
+++ skimage/measure/tests/test_blur_effect.py
@@ -1,4 +1,5 @@
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_almost_equal, assert_equal
+import numpy as np
 
 from skimage.color import rgb2gray
 from skimage.data import astronaut, cells3d
@@ -50,3 +51,25 @@ def test_blur_effect_3d():
     B2 = blur_effect(gaussian(image_3d, sigma=4))
     assert 0 <= B0 < 1
     assert B0 < B1 < B2
+
+
+def test_blur_constant_image():
+    """Test that the blur metric works for a constant image."""
+    image = np.zeros((1080, 1920, 3), dtype=int)
+    B0 = blur_effect(image)
+    assert_equal(B0, 1.0)
+
+
+def test_blur_single_axis_constant_image():
+    """Test that the blur metric work for an image that is constant in one axis."""
+    image = np.array(
+        [
+            [0, 0.025, 0.05, 0.075, 0.1],
+            [0, 0.025, 0.05, 0.075, 0.1],
+            [0, 0.025, 0.05, 0.075, 0.1],
+            [0, 0.025, 0.05, 0.075, 0.1],
+            [0, 0.025, 0.05, 0.075, 0.1],
+        ]
+    )
+    B0 = blur_effect(image)
+    assert_almost_equal(B0, 1.0, decimal=5)
