9eee7192f61fb85de146fa7999c2733b2c7e1e99
Stefan van der Walt <stefanv@berkeley.edu>
2025-01-24 15:21:46 -0800

    Refactor github before_install and script

--- tools/github/before_install.sh
+++ tools/github/before_install.sh
@@ -1,4 +1,28 @@
 #!/usr/bin/env bash
+
+# Options:
+#
+# MINIMUM_REQUIREMENTS:
+#   Install the minimum versions of all requirements, instead of the latest.
+#
+# FREE_THREADED_BUILD:
+#   Indicates that we are running on the free-threaded build.
+#   This option disables the GIL and installs free-threaded builds of
+#   dependencies. (TODO: remove nightly-wheels install once those builds are
+#   available on pypi.)
+#
+# PIP_FLAGS:
+#   These options are passed to pip.
+#
+# BUILD_DOCS / TEST_EXAMPLES:
+#   Install documentation dependencies, and set up headless Matplotlib backend.
+#
+# OPTIONAL_DEPS:
+#   Install optional requirements.
+#
+# WITHOUT_POOCH:
+#   Remove pooch from environment.
+
 set -ex
 
 export PIP_DEFAULT_TIMEOUT=60
@@ -11,6 +35,16 @@ fi
 
 python -m pip install --upgrade pip
 
+# Combine requirement files for a more robust pip solve
+# installing successively may update previously constrained dependencies
+REQUIREMENT_FILES="-r requirements/default.txt -r requirements/test.txt"
+if [[ "${OPTIONAL_DEPS}" == "1" ]]; then
+    REQUIREMENT_FILES="${REQUIREMENT_FILES} -r requirements/optional.txt"
+fi
+
+python -m pip install $PIP_FLAGS $REQUIREMENT_FILES
+
+
 # TODO: delete when scipy, numpy, cython and pywavelets free-threaded wheels are available on PyPi
 FREE_THREADED_BUILD="$(python -c"import sysconfig; print(bool(sysconfig.get_config_var('Py_GIL_DISABLED')))")"
 if [[ $FREE_THREADED_BUILD == "True" ]]; then
@@ -19,6 +53,28 @@ fi
 
 # Install build time requirements
 python -m pip install $PIP_FLAGS -r requirements/build.txt
+
+# Prepare for building the docs (and test examples)
+if [[ "${BUILD_DOCS}" == "1" ]] || [[ "${TEST_EXAMPLES}" == "1" ]]; then
+  echo "Build or run examples"
+  # Use previous installed requirements as well, otherwise installing
+  # successively may update previously constraint dependencies
+  python -m pip install $PIP_FLAGS $REQUIREMENT_FILES -r ./requirements/docs.txt
+  python -m pip list
+
+  export MPL_DIR
+  MPL_DIR=$(python -c 'import matplotlib; print(matplotlib.get_configdir())')
+  if [[ -n "${MPL_DIR}" ]]; then
+    mkdir -p "${MPL_DIR}"
+    touch "${MPL_DIR}/matplotlibrc"
+    echo 'backend : Template' > "${MPL_DIR}/matplotlibrc"
+  fi
+fi
+
+if [[ ${WITHOUT_POOCH} == "1" ]]; then
+  # remove pooch (previously installed via requirements/test.txt)
+  python -m pip uninstall pooch -y
+fi
 
 # Show what's installed
 python -m pip list
--- tools/github/script.sh
+++ tools/github/script.sh
@@ -1,47 +1,21 @@
 #!/usr/bin/env bash
+
+# Options:
+#
+# BUILD_DOCS:
+#   Whether or not to build the docs.
+# TEST_EXAMPLES:
+#   Whether or not to run the examples.
+#   This is mutually exclusive with BUILD_DOCS=1, which
+#   already runs the examples.
+
 # Fail on non-zero exit and echo the commands
 set -evx
 
 TEST_ARGS="--doctest-plus --showlocals"
 
-
-# Combine requirement files for a more robust pip solve
-# installing successively may update previously constrained dependencies
-REQUIREMENT_FILES="-r requirements/default.txt -r requirements/test.txt"
-if [[ "${OPTIONAL_DEPS}" == "1" ]]; then
-    REQUIREMENT_FILES="${REQUIREMENT_FILES} -r requirements/optional.txt"
-fi
-
-python -m pip install $PIP_FLAGS $REQUIREMENT_FILES
-
-if [[ ${WITHOUT_POOCH} == "1" ]]; then
-  # remove pooch (previously installed via requirements/test.txt)
-  python -m pip uninstall pooch -y
-fi
-
-python -m pip list
-
-
 # Run the tests
 (cd .. && pytest $TEST_ARGS --pyargs skimage)
-
-
-# Optionally, prepare building the docs (and test examples)
-if [[ "${BUILD_DOCS}" == "1" ]] || [[ "${TEST_EXAMPLES}" == "1" ]]; then
-  echo "Build or run examples"
-  # Use previous installed requirements as well, otherwise installing
-  # successively may update previously constraint dependencies
-  python -m pip install $PIP_FLAGS $REQUIREMENT_FILES -r ./requirements/docs.txt
-  python -m pip list
-
-  export MPL_DIR
-  MPL_DIR=$(python -c 'import matplotlib; print(matplotlib.get_configdir())')
-  if [[ -n "${MPL_DIR}" ]]; then
-    mkdir -p "${MPL_DIR}"
-    touch "${MPL_DIR}/matplotlibrc"
-    echo 'backend : Template' > "${MPL_DIR}/matplotlibrc"
-  fi
-fi
 
 # Optionally, build the docs or test examples
 if [[ "${BUILD_DOCS}" == "1" ]]; then
