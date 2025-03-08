2419c84835f878549e90da8bc7744cb0a28769fa
Lars Grüter <lagru@mailbox.org>
2025-03-08 18:40:28 +0100

    Remove obsolete doctest_skip_parser
    This decorator isn't used anywhere right now. Since we use docstest-plus
    an alternative to this exists, e.g. prepending
    
        .. testsetup::
            >>> import pytest; _ = pytest.importorskip('matplotlib')
    
    to a docstring example.

--- skimage/_shared/testing.py
+++ skimage/_shared/testing.py
@@ -64,53 +64,6 @@ def assert_greater(a, b, msg=None):
     assert a > b, message
 
 
-def doctest_skip_parser(func):
-    """Decorator replaces custom skip test markup in doctests
-
-    Say a function has a docstring::
-
-        >>> something, HAVE_AMODULE, HAVE_BMODULE = 0, False, False
-        >>> something # skip if not HAVE_AMODULE
-        0
-        >>> something # skip if HAVE_BMODULE
-        0
-
-    This decorator will evaluate the expression after ``skip if``.  If this
-    evaluates to True, then the comment is replaced by ``# doctest: +SKIP``. If
-    False, then the comment is just removed. The expression is evaluated in the
-    ``globals`` scope of `func`.
-
-    For example, if the module global ``HAVE_AMODULE`` is False, and module
-    global ``HAVE_BMODULE`` is False, the returned function will have docstring::
-
-        >>> something # doctest: +SKIP
-        >>> something + else # doctest: +SKIP
-        >>> something # doctest: +SKIP
-
-    """
-    lines = func.__doc__.split('\n')
-    new_lines = []
-    for line in lines:
-        match = SKIP_RE.match(line)
-        if match is None:
-            new_lines.append(line)
-            continue
-        code, space, expr = match.groups()
-
-        try:
-            # Works as a function decorator
-            if eval(expr, func.__globals__):
-                code = code + space + "# doctest: +SKIP"
-        except AttributeError:
-            # Works as a class decorator
-            if eval(expr, func.__init__.__globals__):
-                code = code + space + "# doctest: +SKIP"
-
-        new_lines.append(code)
-    func.__doc__ = "\n".join(new_lines)
-    return func
-
-
 def roundtrip(image, plugin, suffix):
     """Save and read an image using a specified plugin"""
     if '.' not in suffix:
