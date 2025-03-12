bc5cf0334ad7313430a7625e00e029e5c6d6ce94
Stefan van der Walt <stefanv@berkeley.edu>
2025-03-12 14:15:55 -0700

    Add TOML export of dependencies
    Internal refactoring:
    
    - Single function to provide coherent modules and modules index list
    - Sort modules list internally, always
    - Correct type annotations

--- tools/module_dependencies.py
+++ tools/module_dependencies.py
@@ -18,30 +18,38 @@ excluded_mods = [
 
 
 @functools.cache
-def _pkg_modules() -> list[str]:
+def _pkg_modules() -> tuple[list[str], dict[str, int]]:
+    """List all package submodules.
+
+    Returns
+    -------
+    submodules : list
+        Sorted list of package submodules.
+    submodule_idx : dict
+        Mapping of submodule to integer, its index
+        in `submodules`.
+    """
     pkg = importlib.import_module(package)
 
     members = {f'{package}.{attr}' for attr in dir(pkg)}
     included_mods_full = {f'{package}.{mod}' for mod in included_mods}
     excluded_mods_full = {f'{package}.{mod}' for mod in excluded_mods}
 
-    return (members | set(included_mods_full)) - set(excluded_mods_full)
+    # Sort entries, so that all adjacency matrix calculations are stable
+    submodules = sorted((members | included_mods_full) - excluded_mods_full)
+    submodule_idx = {mod: index for index, mod in enumerate(submodules)}
+
+    return submodules, submodule_idx
 
 
-@functools.cache
-def _pkg_modules_index():
-    mods = sorted(_pkg_modules())
-    return {mod: index for index, mod in enumerate(mods)}
-
-
-def _import_dependencies(module: str | list[str]) -> set[str]:
+def _import_dependencies(module: str) -> set[str]:
     importlib.import_module(module)
     importlib.import_module(f"{module}.tests")
 
-    pkg_modules = _pkg_modules()
-    pkg_sys_modules = {mod for mod in sys.modules if package in mod}
+    pkg_modules, _ = _pkg_modules()
+    pkg_sys_modules = {mod for mod in sys.modules if f"{package}." in mod}
 
-    return pkg_modules & pkg_sys_modules
+    return set(pkg_modules) & pkg_sys_modules
 
 
 def dependency_graph():
@@ -50,19 +58,33 @@ def dependency_graph():
     Each row represents the dependencies of one subpackage.
 
     """
-    mods = sorted(_pkg_modules())  # sort for stable matrix
-    mods_idx = _pkg_modules_index()
+    mods, mods_idx = _pkg_modules()
 
     n = len(mods)
-    A = np.zeros((n, n), dtype=int)
+    A = np.zeros((n, n), dtype=bool)
 
     with multiprocessing.Pool() as p:
         mod_deps = p.map(_import_dependencies, mods)
         for k, dependencies in enumerate(mod_deps):
             for mod in dependencies:
-                A[k, mods_idx[mod]] = 1
+                A[k, mods_idx[mod]] = True
 
     return A
+
+
+def dependency_toml():
+    mods, mods_idx = _pkg_modules()
+    mods_arr = np.array(mods, dtype=object)
+
+    A = dependency_graph()
+
+    toml = []
+    for i, mod in enumerate(mods):
+        dependency_names = mods_arr[A[:, i]].tolist()
+
+        toml.append({"modules": {"path": mod, "depends_on": dependency_names}})
+
+    return toml
 
 
 def modules_dependent_on(modules: set[str] | list[str]) -> set[str]:
@@ -71,12 +93,24 @@ def modules_dependent_on(modules: set[str] | list[str]) -> set[str]:
 
     A = dependency_graph().astype(bool)
 
-    pkg_mods = np.array(sorted(_pkg_modules_index()), dtype=object)
-    pkg_mods_idx = _pkg_modules_index()
+    pkg_mods, pkg_mods_idx = _pkg_modules()
+    pkg_mods_arr: np.typing.NDArray = np.array(pkg_mods, dtype=object)
 
     all_dependent_mods = []
     for changed_mod in changed_modules:
-        dependent_mods = pkg_mods[A[:, pkg_mods_idx[changed_mod]]]
+        dependent_mods = pkg_mods_arr[A[:, pkg_mods_idx[changed_mod]]]
         all_dependent_mods.extend(dependent_mods.tolist())
 
     return set(all_dependent_mods)
+
+
+if __name__ == "__main__":
+    toml = dependency_toml()
+    for i, section in enumerate(toml):
+        if i != 0:
+            print()
+
+        for key in section:
+            print(f"[[{key}]]")
+            for name, value in section[key].items():
+                print(f"{name} = {repr(value)}")
