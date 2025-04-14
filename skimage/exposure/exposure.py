afd20217e50b02efd2864ac2d3bf8c8766cfe1d9
Hemanth Kumar <2300030439@kluniversity.in>
2025-04-15 01:07:58 +0530

    DOC: Improve docstring for `rescale_intensity` in exposure.py
    This PR improves the clarity and formatting of the `rescale_intensity` docstring in `skimage/exposure/exposure.py`.
    
    - Updated parameter descriptions to be more explicit.
    - Improved examples for better readability.
    - Clarified `in_range` and `out_range` behavior.
    - Followed NumPy/SciPy docstring standards.
    
    This helps address issue #7769.
    
    Let me know if any additional changes are needed!

--- skimage/exposure/exposure.py
+++ skimage/exposure/exposure.py
@@ -489,41 +489,40 @@ def _output_dtype(dtype_or_range, image_dtype):
 
 
 def rescale_intensity(image, in_range='image', out_range='dtype'):
-    """Return image after stretching or shrinking its intensity levels.
+    """
+    Rescale the intensity values of an image.
 
-    The desired intensity range of the input and output, `in_range` and
-    `out_range` respectively, are used to stretch or shrink the intensity range
-    of the input image. See examples below.
+    This function adjusts the intensity values in `image` from the specified
+    input range (`in_range`) to the desired output range (`out_range`).
+    It performs linear scaling of the values and clips those outside the
+    input range.
 
     Parameters
     ----------
     image : array
-        Image array.
-    in_range, out_range : str or 2-tuple, optional
-        Min and max intensity values of input and output image.
-        The possible values for this parameter are enumerated below.
+        Input image array.
+    in_range : {'image', 'dtype', dtype-name} or 2-tuple of scalars, optional
+        Intensity range of the input image:
+        - 'image': Use min and max of the input image.
+        - 'dtype': Use full intensity range of the image's dtype.
+        - dtype-name: Use intensity limits of the specified dtype (e.g., 'uint8').
+        - tuple (min, max): Specify explicit min and max values.
 
-        'image'
-            Use image min/max as the intensity range.
-        'dtype'
-            Use min/max of the image's dtype as the intensity range.
-        dtype-name
-            Use intensity range based on desired `dtype`. Must be valid key
-            in `DTYPE_RANGE`.
-        2-tuple
-            Use `range_values` as explicit min/max intensities.
+    out_range : {'dtype'} or 2-tuple of scalars, optional
+        Desired intensity range of the output image:
+        - 'dtype': Use full range of the image's dtype.
+        - tuple (min, max): Specify explicit output intensity range.
 
     Returns
     -------
     out : array
-        Image array after rescaling its intensity. This image is the same dtype
-        as the input image.
+        Image array with rescaled intensity values. Output dtype matches the input
+        unless a custom range requires float.
 
     Notes
     -----
     .. versionchanged:: 0.17
-        The dtype of the output array has changed to match the input dtype, or
-        float if the output range is specified by a pair of values.
+        The output array's dtype now matches the input dtype unless a float range is used.
 
     See Also
     --------
@@ -531,51 +530,27 @@ def rescale_intensity(image, in_range='image', out_range='dtype'):
 
     Examples
     --------
-    By default, the min/max intensities of the input image are stretched to
-    the limits allowed by the image's dtype, since `in_range` defaults to
-    'image' and `out_range` defaults to 'dtype':
-
     >>> image = np.array([51, 102, 153], dtype=np.uint8)
     >>> rescale_intensity(image)
     array([  0, 127, 255], dtype=uint8)
-
-    It's easy to accidentally convert an image dtype from uint8 to float:
-
-    >>> 1.0 * image
-    array([ 51., 102., 153.])
-
-    Use `rescale_intensity` to rescale to the proper range for float dtypes:
 
     >>> image_float = 1.0 * image
     >>> rescale_intensity(image_float)
     array([0. , 0.5, 1. ])
 
-    To maintain the low contrast of the original, use the `in_range` parameter:
-
     >>> rescale_intensity(image_float, in_range=(0, 255))
     array([0.2, 0.4, 0.6])
 
-    If the min/max value of `in_range` is more/less than the min/max image
-    intensity, then the intensity levels are clipped:
-
     >>> rescale_intensity(image_float, in_range=(0, 102))
     array([0.5, 1. , 1. ])
-
-    If you have an image with signed integers but want to rescale the image to
-    just the positive range, use the `out_range` parameter. In that case, the
-    output dtype will be float:
 
     >>> image = np.array([-10, 0, 10], dtype=np.int8)
     >>> rescale_intensity(image, out_range=(0, 127))
     array([  0. ,  63.5, 127. ])
 
-    To get the desired range with a specific dtype, use ``.astype()``:
-
     >>> rescale_intensity(image, out_range=(0, 127)).astype(np.int8)
     array([  0,  63, 127], dtype=int8)
 
-    If the input image is constant, the output will be clipped directly to the
-    output range:
     >>> image = np.array([130, 130, 130], dtype=np.int32)
     >>> rescale_intensity(image, out_range=(0, 127)).astype(np.int32)
     array([127, 127, 127], dtype=int32)
@@ -605,6 +580,7 @@ def rescale_intensity(image, in_range='image', out_range='dtype'):
         return (image * (omax - omin) + omin).astype(out_dtype)
     else:
         return np.clip(image, omin, omax).astype(out_dtype)
+
 
 
 def _assert_non_negative(image):
