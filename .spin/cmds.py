9f7d74f2443bfff2229636a92cadd639c04b379b
Stefan van der Walt <stefanv@berkeley.edu>
2025-04-30 16:07:58 -0700

    Tweak comment

--- .spin/cmds.py
+++ .spin/cmds.py
@@ -97,8 +97,8 @@ def test(*, parent_callback, doctest=False, **kwargs):
         if '--doctest-plus' not in pytest_args:
             pytest_args = ('--doctest-plus',) + pytest_args
 
-    # `--import-mode="importlib"` is necessary to successfully collect doctests
-    # in editable installs.
+    # `--import-mode="importlib"` is necessary to collect doctests
+    # for editable installs.
     if any('--doctest' in arg for arg in pytest_args) and not any(
         '--import-mode' in arg for arg in pytest_args
     ):
