320f7993a05aa49db6422522a564630165817fc0
Michael Bratsch <michael.bratsch83@gmail.com>
2025-07-06 22:24:14 +0200

    Forward  in _generic_edge_filter

--- skimage/filters/edges.py
+++ skimage/filters/edges.py
@@ -187,7 +187,7 @@ def _generic_edge_filter(
         smooth_axes = list(set(range(ndim)) - {edge_dim})
         for smooth_dim in smooth_axes:
             kernel = kernel * _reshape_nd(smooth_weights, ndim, smooth_dim)
-        ax_output = ndi.convolve(image, kernel, mode=mode)
+        ax_output = ndi.convolve(image, kernel, mode=mode, cval=cval)
         if return_magnitude:
             ax_output *= ax_output
         output += ax_output
