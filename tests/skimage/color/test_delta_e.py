574c9a7f48bc5e9e8c8ffd9d7ee06ad8190d1d8e
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2025-08-15 18:16:39 +0200

    Use fetcher

--- tests/skimage/color/test_delta_e.py
+++ tests/skimage/color/test_delta_e.py
@@ -4,6 +4,7 @@ import numpy as np
 import pytest
 from numpy.testing import assert_allclose, assert_almost_equal, assert_equal
 
+from skimage._shared.testing import fetch
 from skimage._shared.utils import _supported_float_type
 from skimage.color.delta_e import (
     deltaE_cie76,
@@ -64,7 +65,7 @@ def load_ciede2000_data():
     ]
 
     # note: ciede_test_data.txt contains several intermediate quantities
-    path = 'ciede2000_test_data.txt'
+    path = fetch('ciede2000_test_data.txt', prefix='tests')
     return np.loadtxt(path, dtype=dtype)
 
 
