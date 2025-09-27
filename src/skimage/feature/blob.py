56c5c4d2b387b70536c8aaaf9958e993e69f9877
Jamal Mustafa <jimustafa@users.noreply.github.com>
2025-09-26 22:20:26 -0700

    Apply suggestions from code review
    Co-authored-by: Marianne Corvellec <marianne.corvellec@ens-lyon.org>

--- src/skimage/feature/blob.py
+++ src/skimage/feature/blob.py
@@ -433,9 +433,9 @@ def blob_log(
         Input grayscale image, blobs are assumed to be light on dark
         background (white on black).
     min_sigma : scalar or sequence of scalars, optional
-        The minimum standard deviation for Gaussian kernel. Keep this low to
-        detect smaller blobs. The standard deviations of the Gaussian filter
-        are given for each axis as a sequence, or as a single number, in
+        Minimum standard deviation for Gaussian kernel. Keep this value low to
+        detect smaller blobs. The standard deviation of the Gaussian kernel
+        is given either as a sequence for each axis, or as a single number, in
         which case it is equal for all axes.
     max_sigma : scalar or sequence of scalars, optional
         The maximum standard deviation for Gaussian kernel. Keep this high to
@@ -443,7 +443,7 @@ def blob_log(
         are given for each axis as a sequence, or as a single number, in
         which case it is equal for all axes.
     num_sigma : int, optional
-        The number of evenly spaced values of standard deviations to consider
+        The number of evenly spaced values for standard deviation of the Gaussian kernel to consider
         on the closed interval ``[min_sigma, max_sigma]``.
     threshold : float or None, optional
         The absolute lower bound for scale space maxima. Local maxima smaller
@@ -610,7 +610,7 @@ def blob_doh(
         The maximum standard deviation for Gaussian Kernel used to compute
         Hessian matrix. Keep this high to detect larger blobs.
     num_sigma : int, optional
-        The number of evenly spaced values of standard deviations to consider
+        The number of evenly spaced values for standard deviation of the Gaussian kernel to consider
         on the closed interval ``[min_sigma, max_sigma]``.
     threshold : float or None, optional
         The absolute lower bound for scale space maxima. Local maxima smaller
