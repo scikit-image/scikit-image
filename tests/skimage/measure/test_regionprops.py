Fix negative axis length due to numerical error (#7916)
Putting guard on potential numerical errors that causes negative values
of length (which causes error in the sqrt function)

Closes #6630
Closes #7812

---------

Co-authored-by: EdytaRz <edytarzonc@gmail.com>
Co-authored-by: Pawel Rzonca <pawel.rzonca@contractors.roche.com>

tests/skimage/measure/test_regionprops.py
@@ -1,7 +1,7 @@
 import math
 import pickle
-
 import re
+
 import numpy as np
 import pytest
 import scipy.ndimage as ndi
@@ -744,6 +744,28 @@ def test_axis_minor_length():
     assert abs(length_wo_spacing - target_length) > 0.1
     length = regionprops(img[::2], spacing=(2, 1))[0].axis_minor_length
     assert_almost_equal(length, target_length, decimal=1)
+
+    # this input can produce small value that can be negative due to numerical errors
+    img_negative_length = np.array(
+        [
+            [
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 0],
+                [0, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+                [0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+                [0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+                [0, 1, 1, 0, 1, 1, 1, 1, 0, 0],
+                [0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+            ]
+        ],
+        dtype=np.uint8,
+    )
+
+    length = regionprops(img_negative_length)[0].axis_minor_length
+    target_length = 0.0
+    assert_almost_equal(length, target_length)
 
 
 def test_moments():