91b4dc6f526d50dc0749971262b59447fb76caad
Matthew Brett <matthew.brett@gmail.com>
2025-11-17 10:47:32 +0000

    Further tighten tolerance.
    It appears that MacOS Intel, `moments_weighted_central` is an outlier
    for tolerance.

--- tests/skimage/measure/test_regionprops.py
+++ tests/skimage/measure/test_regionprops.py
@@ -1543,18 +1543,17 @@ def test_multichannel(prop_name):
     if np.shape(p) == np.shape(p_multi):
         # property does not depend on multiple channels
         assert_array_equal(p, p_multi)
-    else:
-        atol = 1e-12
+        return
+    atol = 1e-15
+    # moments_weighted_central can differ in multichannel case due to
+    # differences in np.dot implementation of not-contiguous arrays.
+    if PROPS.get(prop_name) == 'moments_weighted_central':
         # MacOS Intel Accelerate has higher error in non-contiguous case:
         # https://github.com/scikit-image/scikit-image/issues/7941
-        if (
-            is_macos_intel_accelerate()
-            and PROPS[prop_name] == 'moments_weighted_central'
-        ):
-            atol = 1e-11
-        # property uses multiple channels, returns props stacked along
-        # final axis
-        assert_allclose(p, np.asarray(p_multi)[..., 1], rtol=1e-12, atol=atol)
+        atol = 1e-11 if is_macos_intel_accelerate() else 1e-12
+    # property uses multiple channels, returns props stacked along
+    # final axis
+    assert_allclose(p, np.asarray(p_multi)[..., 1], rtol=1e-15, atol=atol)
 
 
 def test_3d_ellipsoid_axis_lengths():
