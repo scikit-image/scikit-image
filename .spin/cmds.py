65b5f509d64c7cb6f5128fd06e092b4fe3e8185d
Lars Grüter <lagru@mailbox.org>
2026-01-28 21:35:01 +0100

    Offer example in warning

--- .spin/cmds.py
+++ .spin/cmds.py
@@ -84,6 +84,17 @@ def sdist(pyproject_build_args):
     spin.util.run(["tools/check_sdist.py", sdist])
 
 
+_SRC_IN_TEST_ARGS_WARNING_MESSAGE = """\
+WARNING: Found 'src' in test arguments and using out-of-tree build.
+For out-of-tree builds, selecting `src/` as a doctest path may fail because
+Pytest doesn't expect source and installation to live in different places.
+Use an editable install (`spin install`) which supports this or avoid passing
+`src/`. For example:
+    spin test -- src/skimage/io  # NO!
+    spin test -- skimage.io      # YES!
+"""
+
+
 @click.option("--doctest/--no-doctest", default=True, help="Whether to run doctests.")
 @spin.util.extend_command(spin.cmds.meson.test)
 def test(*, parent_callback, doctest=False, **kwargs):
@@ -91,15 +102,7 @@ def test(*, parent_callback, doctest=False, **kwargs):
 
     is_out_of_tree_build = not _is_editable_install_of_same_source("scikit-image")
     if is_out_of_tree_build and "src" in str(pytest_args):
-        click.secho(
-            "WARN: Found 'src' in test arguments and using out-of-tree build. "
-            "For out-of-tree builds, selecting `src/` as a test path fails. "
-            "Pytest doesn't expect test sources and installation to be different. "
-            "Avoid passing `src/` or use an editable install (`spin install`) "
-            "to avoid this.",
-            fg="yellow",
-            bold=True,
-        )
+        click.secho(_SRC_IN_TEST_ARGS_WARNING_MESSAGE, fg="yellow", bold=True)
 
     if doctest:
         if '--doctest-plus' not in pytest_args:
