a793fc81a7ac0ae5d8cc28a18784399307eb8671
Lars Grüter <lagru@mailbox.org>
2026-01-29 11:03:58 +0100

    Pin randomness of tests in test_synthetic.py

--- tests/skimage2/data/test_synthetic.py
+++ tests/skimage2/data/test_synthetic.py
@@ -6,19 +6,26 @@ from skimage2.data import binary_blobs
 from skimage._shared.testing import assert_stacklevel
 
 
+SEED = 20260129  # Pin randomness of tests
+
+
 class Test_binary_blobs:
     def test_volume_fraction(self):
-        blobs = binary_blobs(shape=(128, 128), blob_size=13)
+        blobs = binary_blobs(shape=(128, 128), blob_size=13, rng=SEED)
         assert_almost_equal(blobs.mean(), 0.5, decimal=1)
 
-        blobs = binary_blobs(shape=(128, 128), blob_size=13, volume_fraction=0.25)
+        blobs = binary_blobs(
+            shape=(128, 128), blob_size=13, volume_fraction=0.25, rng=SEED
+        )
         assert_almost_equal(blobs.mean(), 0.25, decimal=1)
 
-        blobs = binary_blobs(shape=(32, 32, 32), blob_size=3, volume_fraction=0.25)
+        blobs = binary_blobs(
+            shape=(32, 32, 32), blob_size=3, volume_fraction=0.25, rng=SEED
+        )
         assert_almost_equal(blobs.mean(), 0.25, decimal=1)
 
         other_realization = binary_blobs(
-            shape=(32, 32, 32), blob_size=3, volume_fraction=0.25
+            shape=(32, 32, 32), blob_size=3, volume_fraction=0.25, rng=SEED
         )
         assert not np.all(blobs == other_realization)
 
@@ -26,10 +33,10 @@ class Test_binary_blobs:
         # Assert that `boundary_mode="wrap"` decreases the pixel difference on
         # opposing borders compared to `boundary_mode="nearest"`
         blobs_near = binary_blobs(
-            shape=(300, 300), blob_size=30, boundary_mode="nearest", rng=3
+            shape=(300, 300), blob_size=30, boundary_mode="nearest", rng=SEED
         )
         blobs_wrap = binary_blobs(
-            shape=(300, 300), blob_size=30, boundary_mode="wrap", rng=3
+            shape=(300, 300), blob_size=30, boundary_mode="wrap", rng=SEED
         )
 
         diff_near_ax0 = blobs_near[0, :] ^ blobs_near[-1, :]
@@ -47,9 +54,9 @@ class Test_binary_blobs:
         with pytest.warns(RuntimeWarning, match=regex) as record:
             result = binary_blobs(
                 shape=(100, 100),
-                rng=3,
                 blob_size=blob_size,
                 volume_fraction=volume_fraction,
+                rng=SEED,
             )
         assert_stacklevel(record, offset=-6)
         if blob_size >= 0.4:
@@ -63,6 +70,6 @@ class Test_binary_blobs:
         # Check that this is gracefully handled.
         regex = r"Clamping to `blob_size=0.1`"
         with pytest.warns(RuntimeWarning, match=regex) as record:
-            result = binary_blobs(shape=(100, 100), rng=3, blob_size=0.09)
+            result = binary_blobs(shape=(100, 100), blob_size=0.09, rng=SEED)
         assert_stacklevel(record)
         np.testing.assert_equal(result, 1)
