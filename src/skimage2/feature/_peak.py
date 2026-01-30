Fix wrong shape for empty results with labels set

src/skimage2/feature/_peak.py
@@ -176,8 +176,9 @@ def peak_local_max(
 
     Returns
     -------
-    output : ndarray
-        The coordinates of the peaks.
+    output : ndarray of shape (N, D)
+        The coordinates of the peaks. ``N`` denotes the number of peaks and
+        ``D`` corresponds to the number of dimensions in `image`.
 
     Notes
     -----
@@ -294,7 +295,7 @@ def peak_local_max(
         if labels_peak_coord:
             coordinates = np.vstack(labels_peak_coord)
         else:
-            coordinates = np.empty((0, 2), dtype=int)
+            coordinates = np.empty((0, image.ndim), dtype=int)
 
         if len(coordinates) > num_peaks:
             out = np.zeros_like(image, dtype=bool)

tests/skimage2/feature/test_peak.py
@@ -229,13 +229,14 @@ class Test_peak_local_max:
         result[tuple(peak_idx.T)] = True
         assert_equal(result, nd_image.astype(bool))
 
-    def test_empty(self):
-        image = np.zeros((10, 20))
-        labels = np.zeros((10, 20), int)
+    @pytest.mark.parametrize("ndim", [1, 2, 3, 4])
+    def test_empty(self, ndim):
+        image = np.zeros((10,) * ndim)
+        labels = np.zeros_like(image, dtype=int)
         result = peak_local_max(
             image,
             labels=labels,
-            footprint=np.ones((3, 3), bool),
+            footprint=np.ones((3,) * ndim, dtype=bool),
             min_distance=1,
             threshold_rel=0,
             exclude_border=False,