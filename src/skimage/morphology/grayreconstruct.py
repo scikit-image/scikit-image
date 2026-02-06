ba5595fa6eb29654e5cef53235abbded8db63fbf
Stefan van der Walt <stefanv@berkeley.edu>
2026-02-05 17:14:21 -0800

    Satisfy linter

--- src/skimage/morphology/grayreconstruct.py
+++ src/skimage/morphology/grayreconstruct.py
@@ -214,8 +214,6 @@ def reconstruction(seed, mask, method='dilation', footprint=None, offset=None):
     # Reshape reconstructed image to original image shape and remove padding.
     rec_img = value_map[value_rank[:image_stride]]
     np.reshape(
-        rec_img,
-        np.array(seed.shape) + (np.array(footprint.shape) - 1),
-        copy=False
+        rec_img, np.array(seed.shape) + (np.array(footprint.shape) - 1), copy=False
     )
     return rec_img[inside_slices]
