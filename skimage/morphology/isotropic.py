b2d0945481f6bd74e32d0e617422fed9129772df
Aditi Juneja <91629733+Schefflera-Arboricola@users.noreply.github.com>
2026-02-14 11:19:30 +0530

    Update error message

--- skimage/morphology/isotropic.py
+++ skimage/morphology/isotropic.py
@@ -70,7 +70,7 @@ def isotropic_erosion(image, radius, out=None, spacing=None):
            [0, 0, 0, 0, 0]], dtype=uint8)
     """
     if image.dtype != np.bool:
-        raise TypeError("Input image must be a binary image")
+        raise TypeError("dtype of 'image' must be boolean")
     dist = ndi.distance_transform_edt(image, sampling=spacing)
     return np.greater(dist, radius, out=out)
 
@@ -139,7 +139,7 @@ def isotropic_dilation(image, radius, out=None, spacing=None):
            [0, 0, 1, 1, 0]], dtype=uint8)
     """
     if image.dtype != np.bool:
-        raise TypeError("Input image must be a binary image")
+        raise TypeError("dtype of 'image' must be boolean")
     dist = ndi.distance_transform_edt(np.logical_not(image), sampling=spacing)
     return np.less_equal(dist, radius, out=out)
 
