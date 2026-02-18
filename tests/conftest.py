d522ed082d064911bfb42288b97f5a78346ac842
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2026-02-18 14:01:39 -0700

    fix ruff

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ from _pytest.pathlib import bestrelpath
 
 
 FREE_THREADED_BUILD = bool(sysconfig.get_config_var("Py_GIL_DISABLED"))
-GIL_ENABLED_AT_START=True
+GIL_ENABLED_AT_START = True
 
 if FREE_THREADED_BUILD:
     GIL_ENABLED_AT_START = sys._is_gil_enabled()
