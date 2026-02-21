9e1fcae59874dbf4233f1dc1e55f682a91a62cee
devendrakushwah80 <dk3313250@gmail.com>
2026-02-21 22:15:16 +0530

    Remove venv and add to gitignore

--- venv/Lib/site-packages/sphinx/__main__.py
+++
@@ -1,9 +0,0 @@
-"""The Sphinx documentation toolchain."""
-
-from __future__ import annotations
-
-import sys
-
-from sphinx.cmd.build import main
-
-raise SystemExit(main(sys.argv[1:]))
--- venv/Lib/site-packages/sphinx/ext/autodoc/_legacy_class_based/__init__.py
+++
@@ -1,1 +0,0 @@
-"""The legacy (class-based) implementation of autodoc."""
