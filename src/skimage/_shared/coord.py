f1550000be5a8c36d7a0b1d6e19c42250bb48c94
Lars Grüter <lagru@mailbox.org>
2026-02-26 14:47:06 +0100

    Wrap `skimage2.feature.ensure_spacing` in `skimage`

--- src/skimage/_shared/coord.py
+++ src/skimage/_shared/coord.py
@@ -1,59 +1,11 @@
+from textwrap import dedent
+
 import numpy as np
-from scipy.spatial import cKDTree, distance
 
+from ..util import PendingSkimage2Change
+from .._shared._warnings import warn_external
 
-def _ensure_spacing(coord, spacing, p_norm, max_out):
-    """Returns a subset of coord where a minimum spacing is guaranteed.
-
-    Parameters
-    ----------
-    coord : ndarray
-        The coordinates of the considered points.
-    spacing : float
-        the maximum allowed spacing between the points.
-    p_norm : float
-        Which Minkowski p-norm to use. Should be in the range [1, inf].
-        A finite large p may cause a ValueError if overflow can occur.
-        ``inf`` corresponds to the Chebyshev distance and 2 to the
-        Euclidean distance.
-    max_out : int
-        If not None, at most the first ``max_out`` candidates are
-        returned.
-
-    Returns
-    -------
-    output : ndarray
-        A subset of coord where a minimum spacing is guaranteed.
-
-    """
-
-    # Use KDtree to find the peaks that are too close to each other
-    tree = cKDTree(coord)
-
-    indices = tree.query_ball_point(coord, r=spacing, p=p_norm)
-    rejected_peaks_indices = set()
-    naccepted = 0
-    for idx, candidates in enumerate(indices):
-        if idx not in rejected_peaks_indices:
-            # keep current point and the points at exactly spacing from it
-            candidates.remove(idx)
-            dist = distance.cdist(
-                [coord[idx]], coord[candidates], "minkowski", p=p_norm
-            ).reshape(-1)
-            candidates = [c for c, d in zip(candidates, dist) if d < spacing]
-
-            # candidates.remove(keep)
-            rejected_peaks_indices.update(candidates)
-            naccepted += 1
-            if max_out is not None and naccepted >= max_out:
-                break
-
-    # Remove the peaks that are too close to each other
-    output = np.delete(coord, tuple(rejected_peaks_indices), axis=0)
-    if max_out is not None:
-        output = output[:max_out]
-
-    return output
+import skimage2 as ski2
 
 
 def ensure_spacing(
@@ -99,26 +51,28 @@ def ensure_spacing(
         A subset of coord where a minimum spacing is guaranteed.
 
     """
-    output = coords
-    if len(coords):
-        coords = np.atleast_2d(coords)
-        if min_split_size is None:
-            batch_list = [coords]
-        else:
-            coord_count = len(coords)
-            split_idx = [min_split_size]
-            split_size = min_split_size
-            while coord_count - split_idx[-1] > max_split_size:
-                split_size *= 2
-                split_idx.append(split_idx[-1] + min(split_size, max_split_size))
-            batch_list = np.array_split(coords, split_idx)
+    warn_external(
+        dedent("""\
+        `skimage.feature.ensure_spacing` is deprecated in favor of
+        `skimage2.feature.ensure_spacing` with new behavior:
 
-        output = np.zeros((0, coords.shape[1]), dtype=coords.dtype)
-        for batch in batch_list:
-            output = _ensure_spacing(
-                np.vstack([output, batch]), spacing, p_norm, max_out
-            )
-            if max_out is not None and len(output) >= max_out:
-                break
+        * ...
 
+        To keep the old behavior when switching to `skimage2`, update your call
+        according to the following cases:
+
+        * ...
+
+        Other keyword parameters can be left unchanged.
+        """),
+        category=PendingSkimage2Change,
+    )
+    output = ski2.feature.ensure_spacing(
+        coords=coords,
+        spacing=spacing,
+        p_norm=p_norm,
+        min_split_size=min_split_size,
+        max_out=max_out,
+        max_split_size=max_split_size,
+    )
     return output
