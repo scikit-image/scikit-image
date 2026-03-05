3823c15914efa4fc9423141e8b36072e529d5b02
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2026-03-05 13:32:08 -0700

    simplify conftest changes

--- src/skimage/conftest.py
+++ src/skimage/conftest.py
@@ -17,18 +17,10 @@ import pytest
 def pytest_configure(config):
     if not PARALLEL_RUN_AVAILABLE:
         config.addinivalue_line(
-            'markers',
-            'parallel_threads(n): run the given test function in parallel '
-            'using `n` threads.',
-        )
-        config.addinivalue_line(
             "markers",
             "thread_unsafe: mark the test function as single-threaded",
         )
-        config.addinivalue_line(
-            "markers",
-            "iterations(n): run the given test function `n` times in each thread",
-        )
+
 
 
 @pytest.fixture(autouse=True)
@@ -40,10 +32,3 @@ def handle_np2():
         np.set_printoptions(legacy="1.21")
     except ImportError:
         pass
-
-
-if not PARALLEL_RUN_AVAILABLE:
-
-    @pytest.fixture
-    def num_parallel_threads():
-        return 1
