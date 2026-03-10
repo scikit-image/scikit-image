665c70e47c9dba4ed14998e12789a82e70f49058
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2026-03-10 10:39:38 -0600

    fix logic error

--- tests/dependency_checks.py
+++ tests/dependency_checks.py
@@ -4,7 +4,7 @@ from skimage._shared.version_requirements import is_installed
 
 # pyodide maintainers suggest not running tests that use matplotlib,
 # https://github.com/pyodide/pyodide-recipes/issues/475#issuecomment-4020982042
-is_pyodide = "pyodide" not in sys.modules
+is_pyodide = "pyodide" in sys.modules
 has_mpl = is_installed("matplotlib", ">=3.3")
 
 uses_matplotlib = pytest.mark.skipif(not has_mpl, reason="matplotlib not installed")(
