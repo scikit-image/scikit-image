4c14a41c7d2cc7382e336cd0caf4519556e179ab
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2026-03-10 11:51:10 -0600

    fix ruff

--- tests/dependency_checks.py
+++ tests/dependency_checks.py
@@ -10,10 +10,12 @@ has_mpl = is_installed("matplotlib", ">=3.3")
 
 def uses_matplotlib(test_func):
     if is_pyodide:
-        return pytest.mark.skip(reason=(
-            "importing matplotlib fails randomly. "
-            "See https://github.com/pyodide/pyodide-recipes/issues/475"
-        ))(test_func)
+        return pytest.mark.skip(
+            reason=(
+                "importing matplotlib fails randomly. "
+                "See https://github.com/pyodide/pyodide-recipes/issues/475"
+            )
+        )(test_func)
     if not has_mpl:
         return pytest.mark.skip(reason="matplotlib not installed")(test_func)
     return test_func
