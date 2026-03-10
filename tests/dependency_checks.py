82a5da6128b4d20522d4aa9a4585cbfd10e3de0f
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2026-03-10 10:57:29 -0600

    simplify implementation of uses_matplotlib

--- tests/dependency_checks.py
+++ tests/dependency_checks.py
@@ -7,12 +7,13 @@ from skimage._shared.version_requirements import is_installed
 is_pyodide = "pyodide" in sys.modules
 has_mpl = is_installed("matplotlib", ">=3.3")
 
-uses_matplotlib = pytest.mark.skipif(not has_mpl, reason="matplotlib not installed")(
-    pytest.mark.skipif(
-        is_pyodide,
-        reason=(
+
+def uses_matplotlib(test_func):
+    if is_pyodide:
+        return pytest.mark.skip(reason=(
             "importing matplotlib fails randomly. "
             "See https://github.com/pyodide/pyodide-recipes/issues/475"
-        ),
-    )
-)
+        ))(test_func)
+    if not has_mpl:
+        return pytest.mark.skip(reason="matplotlib not installed")(test_func)
+    return test_func
