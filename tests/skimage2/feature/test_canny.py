9970e1aa5523832abaeff58e87ecf95fade57e1c
Michael Bratsch <michael.bratsch83@gmail.com>
2026-03-16 21:59:31 +0100

    Check default argument mode is nearest

---
+++ tests/skimage2/feature/test_canny.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pytest
+
+from skimage2 import feature
+
+
+@pytest.mark.parametrize(
+    'mode, should_match',
+    [
+        ('nearest', True),
+        ('reflect', False),
+        ('constant', False),
+        ('mirror', False),
+        ('wrap', False),
+    ],
+)
+def test_default_mode_is_nearest(mode, should_match):
+    rng = np.random.default_rng(0)
+    image = rng.random((64, 64))
+    result_default = feature.canny(image)
+    result_other = feature.canny(image, mode=mode)
+    if should_match:
+        np.testing.assert_array_equal(result_default, result_other)
+    else:
+        assert not np.array_equal(result_default, result_other)
