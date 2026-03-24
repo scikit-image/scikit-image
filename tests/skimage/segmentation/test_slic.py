0b64dc68b518d87b1ce89bac398d0c51eca04858
Lars Grüter <lagru@mailbox.org>
2026-03-24 12:04:17 +0100

    Fix warning sensitive test_list_sigma

--- tests/skimage/segmentation/test_slic.py
+++ tests/skimage/segmentation/test_slic.py
@@ -5,7 +5,7 @@ import pytest
 from numpy.testing import assert_equal
 
 from skimage import data, filters, img_as_float
-from skimage._shared.testing import run_in_parallel, expected_warnings
+from skimage._shared.testing import run_in_parallel
 from skimage.segmentation import slic
 
 
@@ -174,9 +174,11 @@ def test_list_sigma():
     img = np.array([[1, 1, 1, 0, 0, 0], [0, 0, 0, 1, 1, 1]], float)
     img += 0.1 * rng.normal(size=img.shape)
     result_sigma = np.array([[0, 0, 0, 1, 1, 1], [0, 0, 0, 1, 1, 1]], int)
-    with expected_warnings(
-        ["Input image is 2D: sigma number of " "elements must be 2"]
-    ):
+    regex = (
+        r"Input image is 2D: sigma number of elements must be 2. "
+        r"In the future, a ValueError will be raised."
+    )
+    with pytest.warns(FutureWarning, match=regex):
         seg_sigma = slic(
             img, n_segments=2, sigma=[1, 50, 1], channel_axis=None, start_label=0
         )
