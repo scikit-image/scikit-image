f37f876a2c493dd20757a48de0537ce3d3b0fb9d
Lars Grüter <lagru@mailbox.org>
2026-03-27 12:18:19 +0100

    Setup _skimage2/metrics subpackage

--- src/_skimage2/__init__.pyi
+++ src/_skimage2/__init__.pyi
@@ -1,6 +1,7 @@
 _submodules = [
     "data",
     "feature",
+    "metrics",
     "morphology",
     "util",
 ]
@@ -10,6 +11,7 @@ __all__ = _submodules + ["__version__", "ExperimentalAPIWarning"]  # noqa: F822
 from . import (
     data,
     feature,
+    metrics,
     morphology,
     util,
 )
