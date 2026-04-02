e3f7ab42bb58cc789d63c09705e336a33e79df7c
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2026-04-02 14:35:21 +0200

    Add apply_parallel to skimage2

--- src/_skimage2/util/__init__.pyi
+++ src/_skimage2/util/__init__.pyi
@@ -1,3 +1,9 @@
 from ._value_rescaling import rescale_minmax, rescale_legacy, _prescale_value_range
+from .apply_parallel import apply_parallel
 
-__all__ = ["rescale_minmax", "rescale_legacy", "_prescale_value_range"]
+__all__ = [
+    "apply_parallel",
+    "rescale_minmax",
+    "rescale_legacy",
+    "_prescale_value_range",
+]
