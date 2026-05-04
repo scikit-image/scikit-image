b7193df185a30747affcc3b1c70f7449bef209aa
Matthew Brett <matthew.brett@gmail.com>
2026-05-04 15:06:42 +0100

    Add suggestions from code review at #8099

--- src/skimage/restoration/unwrap.py
+++ src/skimage/restoration/unwrap.py
@@ -28,11 +28,13 @@ def unwrap_phase(image, wrap_around=False, rng=None):
         process. If only a single boolean is given, it will apply to all axes.
         Wrap around is not supported for 1D arrays.
     rng : {`numpy.random.Generator`, int}, optional
-        Seed for the random number generator used during unwrapping.
-        If an int, it is used directly as the seed. If a
-        `numpy.random.Generator`, a seed is drawn from it. If `None`,
-        unwrapping relies on a random initialization and results may vary
-        between runs.
+        Unwrapping relies on a random initialization.  `rng` supplies the
+        source of a seed for the C-level random number generator used during
+        unwrapping. If an int, it is used directly as the seed; in that case,
+        it should be in the range of a C unsigned integer. If a
+        `numpy.random.Generator`, we draw a suitable integer from the generator
+        to use as the seed. If ``None``, create a new (unseeded)
+        `numpy.random.Generator` from which to draw the seed.
 
     Returns
     -------
@@ -94,12 +96,20 @@ def unwrap_phase(image, wrap_around=False, rng=None):
             'algorithm'
         )
 
+    if rng is None:  # Use new generator to make seed.
+        rng = np.random.default_rng()
+    # Algorithms use C `srand(seed)` to initialize random stream from
+    # subsequent calls to `rand()`.  `srand` expects an unsigned C int.
+    uintc_max = np.iinfo(np.uintc).max
     if isinstance(rng, np.random.Generator):
-        seed = int(rng.integers(np.iinfo(np.uint32).max, endpoint=True))
-    elif rng is None:
-        seed = None
+        seed = rng.integers(uintc_max, endpoint=True)
     else:
-        seed = int(rng)
+        try:
+            seed = np.uintc(rng)
+        except OverflowError:
+            raise ValueError(
+                f"Integer seed must be in [0, {uintc_max}], got {rng}"
+            )
 
     if np.ma.isMaskedArray(image):
         mask = np.require(np.ma.getmaskarray(image), np.uint8, ['C'])
