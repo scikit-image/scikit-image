02b32dc5639a655d801ef297a89ce671a452afc4
Matthew Brett <matthew.brett@gmail.com>
2026-05-20 23:18:53 +0100

    Refactor, document migration utilities (#8184)

--- doc/tools/migration_utils.py
+++ doc/tools/migration_utils.py
@@ -1,4 +1,11 @@
-"""Utilities for migration CLI tools"""
+"""Utilities for migration command line tools
+
+Routines that support tools for:
+
+* Collecting migration docstrings from scikit-image source, and
+* Filling the migration guide from collected docstrings, and
+* Executing examples from collected docstrings.
+"""
 
 import doctest
 from functools import partial
@@ -10,6 +17,18 @@ RUNNER = doctest.DocTestRunner()
 
 def _append_msgs(msg_str, messages=[]):
     messages.append(msg_str)
+
+
+class TrackerDict(dict):
+    """Dict that keeps check on keys that have been accessed."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.not_accessed_keys = set(self)
+
+    def __getitem__(self, key):
+        self.not_accessed_keys.discard(key)
+        return super().__getitem__(key)
 
 
 def run_doctest(func_name, doc):
