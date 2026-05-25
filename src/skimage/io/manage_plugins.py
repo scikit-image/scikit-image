b05df5627c756386fa295d84e78852a2f2e09504
Matthew Brett <matthew.brett@gmail.com>
2026-05-25 21:44:42 +0100

    Add missing underscore imports

--- src/skimage/io/manage_plugins.py
+++ src/skimage/io/manage_plugins.py
@@ -1,2 +1,4 @@
 from _skimage2.io.manage_plugins import *  # noqa: F403
 from _skimage2.io.manage_plugins import __doc__  # noqa: F401
+
+from _skimage2.io.manage_plugins import _hide_plugin_deprecation_warnings  # noqa: F401
--- src/skimage/util/_value_rescaling.py
+++ src/skimage/util/_value_rescaling.py
@@ -1,2 +1,4 @@
 from _skimage2.util._value_rescaling import *  # noqa: F403
 from _skimage2.util._value_rescaling import __doc__  # noqa: F401
+
+from _skimage2.util._value_rescaling import _prescale_value_range  # noqa: F401
