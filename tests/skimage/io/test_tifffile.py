96f96786da1b3c894205b56f2b44a56298d87eaf
Lars Grüter <lagru@mailbox.org>
2026-05-29 15:00:28 +0200

    Remove tifffile tests that use deprecated args

--- tests/skimage/io/test_tifffile.py
+++ tests/skimage/io/test_tifffile.py
@@ -5,15 +5,7 @@ import numpy as np
 import pytest
 from numpy.testing import assert_array_almost_equal, assert_array_equal
 from _skimage2._shared.testing import fetch
-from skimage.io import imread, imsave, reset_plugins, use_plugin
-
-
-@pytest.fixture(autouse=True)
-def _use_tifffile_plugin():
-    """Ensure that PIL plugin is used in tests here."""
-    use_plugin('tifffile')
-    yield
-    reset_plugins()
+from skimage.io import imread, imsave
 
 
 def test_imread_uint16():
@@ -33,11 +25,6 @@ def test_imread_uint16_big_endian():
 def test_imread_multipage_rgb_tif():
     img = imread(fetch('data/multipage_rgb.tif'))
     assert img.shape == (2, 10, 10, 3), img.shape
-
-
-def test_tifffile_kwarg_passthrough():
-    img = imread(fetch('data/multipage.tif'), key=[1], is_ome=True)
-    assert img.shape == (15, 10), img.shape
 
 
 def test_imread_handle():
@@ -69,9 +56,8 @@ class TestSave:
     @pytest.mark.parametrize("shape,seed", shapes_seeds)
     @pytest.mark.parametrize("dtype", dtypes)
     @pytest.mark.parametrize("use_pathlib", [False, True])
-    @pytest.mark.parametrize('explicit_photometric_kwarg', [False, True])
     def test_imsave_roundtrip(
-        self, shape, seed, dtype, use_pathlib, explicit_photometric_kwarg
+        self, shape, seed, dtype, use_pathlib
     ):
         rng = np.random.RandomState(seed)
         x = rng.rand(*shape)
@@ -80,8 +66,4 @@ class TestSave:
             x = (x * np.iinfo(dtype).max).astype(dtype)
         else:
             x = x.astype(dtype)
-        if explicit_photometric_kwarg and x.shape[-1] in [3, 4]:
-            kwargs = {'photometric': 'rgb'}
-        else:
-            kwargs = {}
-        self.roundtrip(dtype, x, use_pathlib, **kwargs)
+        self.roundtrip(dtype, x, use_pathlib)
