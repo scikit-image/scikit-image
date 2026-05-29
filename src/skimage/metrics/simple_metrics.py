dc065af71f4ee739ffc6325ab4b0fb396f5bbd9d
Matthew Brett <matthew.brett@gmail.com>
2026-05-29 10:19:21 +0100

    Move backends back into skimage tree.

--- src/skimage/metrics/simple_metrics.py
+++ src/skimage/metrics/simple_metrics.py
@@ -1,2 +1,6 @@
 from _skimage2.metrics.simple_metrics import *  # noqa: F403
 from _skimage2.metrics.simple_metrics import __doc__  # noqa: F401
+from ..util._backends import dispatchable
+
+mean_squared_error = dispatchable(mean_squared_error)
+normalized_root_mse = dispatchable(normalized_root_mse)
