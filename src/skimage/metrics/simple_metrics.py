c2287082b81a3d862eea0a0d1969033e6c67628f
Matthew Brett <matthew.brett@gmail.com>
2026-05-29 15:26:57 +0100

    Enforce `skimage` for dispatching backends.
    Code partly suggested by Cursor, edited heavily by MB.

--- src/skimage/metrics/simple_metrics.py
+++ src/skimage/metrics/simple_metrics.py
@@ -1,6 +1,8 @@
 from _skimage2.metrics.simple_metrics import *  # noqa: F403
 from _skimage2.metrics.simple_metrics import __doc__  # noqa: F401
-from ..util._backends import dispatchable
+import _skimage2.metrics.simple_metrics as ski2_sm
 
-mean_squared_error = dispatchable(mean_squared_error)
-normalized_root_mse = dispatchable(normalized_root_mse)
+from ..util._backends import dispatchable_shim as _dshim
+
+mean_squared_error = _dshim(ski2_sm.mean_squared_error, module=__name__)
+normalized_root_mse = _dshim(ski2_sm.normalized_root_mse, module=__name__)
