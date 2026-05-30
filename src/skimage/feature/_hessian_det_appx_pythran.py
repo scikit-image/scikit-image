7488a980e09145d44ecdbd8079f0df852e7de21c
Matthew Brett <matthew.brett@gmail.com>
2026-05-30 21:54:24 +0100

    Add init.pyi to wheel, remove unneeded stubs.

--- src/skimage/feature/_hessian_det_appx_pythran.py
+++
@@ -1,2 +0,0 @@
-from _skimage2.feature._hessian_det_appx_pythran import *  # noqa: F403
-from _skimage2.feature._hessian_det_appx_pythran import __doc__  # noqa: F401
--- src/skimage/feature/brief_pythran.py
+++
@@ -1,2 +0,0 @@
-from _skimage2.feature.brief_pythran import *  # noqa: F403
-from _skimage2.feature.brief_pythran import __doc__  # noqa: F401
