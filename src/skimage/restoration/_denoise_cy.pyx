b76a4998180e79c4289ff7aeb4e58a14819f0d85
Apaar Raina <apaarraina@gmail.com>
2026-06-01 02:31:29 +0530

    added the _denoise_bilateral3d function here

--- src/skimage/restoration/_denoise_cy.pyx
+++ src/skimage/restoration/_denoise_cy.pyx
@@ -7,7 +7,7 @@ cimport numpy as cnp
 import numpy as np
 from libc.math cimport sqrt
 from libc.float cimport DBL_MAX
-from _skimage2._shared.interpolation cimport get_pixel3d
+from _skimage2._shared.interpolation cimport get_pixel3d, get_pixel4d
 from _skimage2._shared.fused_numerics cimport np_floats
 
 cnp.import_array()
@@ -92,6 +92,88 @@ def _denoise_bilateral(np_floats[:, :, ::1] image, cnp.float64_t max_value,
                         total_weight += weight
                 for d in range(dims):
                     out[r, c, d] = total_values[d] / total_weight
+
+    return np.squeeze(np.asarray(out))
+
+def _denoise_bilateral3d(np_floats[:, :, :, ::1] image, cnp.float64_t max_value,
+                       Py_ssize_t win_size, cnp.float64_t sigma_color,
+                       cnp.float64_t sigma_spatial, Py_ssize_t bins, mode,
+                       cnp.float64_t cval, np_floats[::1] color_lut,
+                       np_floats[::1] range_lut, np_floats[::1] empty_dims,
+                       np_floats[:, :, :, ::1] out):
+    cdef:
+        Py_ssize_t rows = image.shape[0]
+        Py_ssize_t cols = image.shape[1]
+        Py_ssize_t depth = image.shape[2]
+        Py_ssize_t dims = image.shape[3]
+        Py_ssize_t window_ext = (win_size - 1) / 2
+        Py_ssize_t max_color_lut_bin = bins - 1
+
+        Py_ssize_t r, c, z, d, wr, wc, wz, kr, kc, kz, rr, cc, zz, color_lut_bin
+        np_floats value, weight, dist, total_weight, csigma_color, color_weight, \
+               range_weight, t
+        np_floats dist_scale
+        np_floats[:] values
+        np_floats[:] centres
+        np_floats[:] total_values
+
+    if sigma_color is None:
+        csigma_color = image.std()
+    else:
+        csigma_color = sigma_color
+
+    if mode not in ('constant', 'wrap', 'symmetric', 'reflect', 'edge'):
+        raise ValueError("Invalid mode specified.  Please use `constant`, "
+                         "`edge`, `wrap`, `symmetric` or `reflect`.")
+    cdef char cmode = ord(mode[0].upper())
+
+    dist_scale = bins / dims / max_value
+    values = empty_dims.copy()
+    centres = empty_dims.copy()
+    total_values = empty_dims.copy()
+
+    with nogil:
+        for r in range(rows):
+            for c in range(cols):
+                for z in range(depth):
+                    total_weight = 0
+                    for d in range(dims):
+                        total_values[d] = 0
+                        centres[d] = image[r, c, z, d]
+
+                    for wz in range(-window_ext, window_ext + 1):
+                        zz = wz + z
+                        kz = wz + window_ext
+                        for wr in range(-window_ext, window_ext + 1):
+                            rr = wr + r
+                            kr = wr + window_ext
+                            for wc in range(-window_ext, window_ext + 1):
+                                cc = wc + c
+                                kc = wc + window_ext
+
+                                # save pixel values for all dims and compute euclidean
+                                # distance between centre stack and current position
+                                dist = 0
+                                for d in range(dims):
+                                    value = get_pixel4d(&image[0, 0, 0, 0], rows, cols, depth,
+                                                        dims, rr, cc, zz, d, cmode, cval)
+                                    values[d] = value
+                                    t = centres[d] - value
+                                    dist += t * t
+                                dist = sqrt(dist)
+
+                                range_weight = range_lut[kz * win_size * win_size + kr * win_size + kc]
+
+                                color_lut_bin = Py_ssize_t_min(
+                                    <Py_ssize_t>(dist * dist_scale), max_color_lut_bin)
+                                color_weight = color_lut[color_lut_bin]
+
+                                weight = range_weight * color_weight
+                                for d in range(dims):
+                                    total_values[d] += values[d] * weight
+                                total_weight += weight
+                        for d in range(dims):
+                            out[r, c, z, d] = total_values[d] / total_weight
 
     return np.squeeze(np.asarray(out))
 
