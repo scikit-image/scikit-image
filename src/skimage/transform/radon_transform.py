588444a8a72e28d0eb178a8d2669577e1dde8223
Matthew Brett <matthew.brett@gmail.com>
2026-05-31 10:25:48 +0100

    Add missing private import for doc build

--- src/skimage/transform/radon_transform.py
+++ src/skimage/transform/radon_transform.py
@@ -1,3 +1,6 @@
 from _skimage2.transform.radon_transform import *  # noqa: F403
 from _skimage2.transform.radon_transform import __doc__  # noqa: F401
-from _skimage2.transform.radon_transform import _sinogram_circle_to_square  # noqa: F401
+from _skimage2.transform.radon_transform import (  # noqa: F401
+    _sinogram_circle_to_square,
+    _get_fourier_filter,
+)
