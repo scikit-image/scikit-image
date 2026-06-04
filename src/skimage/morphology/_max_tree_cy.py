52d6a8f03802748ad603e12573007d9822f5d2e6
Matthew Brett <matthew.brett@gmail.com>
2026-06-04 21:24:28 +0100

    Follow _skimage2 pattern for imports

---
+++ src/skimage/morphology/_max_tree_cy.py
@@ -0,0 +1,2 @@
+from _skimage2.morphology._max_tree_cy import *  # noqa: F403
+from _skimage2.morphology._max_tree_cy import __doc__  # noqa: F401
--- src/skimage/morphology/max_tree.py
+++
@@ -1,2 +0,0 @@
-from _skimage2.morphology._max_tree import *  # noqa: F403
-from _skimage2.morphology._max_tree import __doc__  # noqa: F401
