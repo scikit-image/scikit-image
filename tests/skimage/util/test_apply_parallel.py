f0be6ef7cd3cc6afc232249fe78d3b6ba270cca5
Lars Grüter <lagru@mailbox.org>
2026-06-09 19:16:26 +0200

    Avoid import confusion for `apply_parallel` in test suite

--- tests/skimage/util/test_apply_parallel.py
+++ tests/skimage/util/test_apply_parallel.py
@@ -4,7 +4,12 @@ import platform
 from _skimage2._shared.testing import assert_array_almost_equal, assert_equal
 from skimage import color, data, img_as_float
 from skimage.filters import threshold_local, gaussian
-from skimage.util.apply_parallel import apply_parallel
+
+# Don't use `from skimage.util.apply_parallel import apply_parallel` as this
+# may bypass lazy_loader. This causes problems in
+# `test_migration::test_public_skimage_api` because skimage.util.apply_parallel`
+# then refers to the module instead of the function.
+from skimage.util import apply_parallel
 
 import pytest
 
