8dc2a211a83460506c30583343d4968378dab7a7
Lars Grüter <lagru@mailbox.org>
2026-06-11 14:51:26 +0200

    Add missing docstring to `skimage.util:lookfor`

--- src/skimage/util/lookfor.py
+++ src/skimage/util/lookfor.py
@@ -5,6 +5,29 @@ from _skimage2.util._lookfor import __doc__  # noqa: F401
 
 
 def lookfor(what):
+    """Do a keyword search on scikit-image docstrings and print results.
+
+    .. warning::
+
+        This function may also print results that are not part of
+        scikit-image's public API.
+
+    Parameters
+    ----------
+    what : str
+        Words to look for.
+
+    Examples
+    --------
+    >>> import skimage as ski
+    >>> ski.util.lookfor('regular_grid')
+    Search results for 'regular_grid'
+    ---------------------------------
+    skimage.util.regular_grid
+        Find `n_points` regularly spaced along `ar_shape`.
+    skimage.util.lookfor
+        Do a keyword search on scikit-image docstrings and print results.
+    """
     # Walk skimage public namespace; follow _skimage2 implementations only.
     return _lookfor(
         what,
