4a853d3ec147b5d3876efc1cd051ef8e997ae40d
Ravi Kumar <ravishani98765432@gmail.com>
2026-06-13 02:20:48 +0530

    Refine internal plugin package docstring

--- src/skimage/io/_plugins/__init__.py
+++ src/skimage/io/_plugins/__init__.py
@@ -1,7 +1,7 @@
 """
 Image I/O plugin backends used by skimage.io.
 
-This package contains backend-specific plugins for reading and
-writing images through external libraries such as ImageIO,
-Pillow, tifffile, GDAL, and SimpleITK.
+This package contains backend-specific plugins that provide
+image reading and writing support through a variety of
+external libraries.
 """
