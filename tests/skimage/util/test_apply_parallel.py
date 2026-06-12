af18840ae6330e2411e9e9c1aac52ec0c5cd4636
Lars Grüter <lagru@mailbox.org>
2026-06-12 12:28:47 +0200

    Remove outdated comment
    Imports should work as usual with
    https://github.com/matthew-brett/scikit-image/blob/5a2abd41d4869e6a135bc2f57c9068bc9e329c3d/src/skimage/util/__init__.py#L70.

--- tests/skimage/util/test_apply_parallel.py
+++ tests/skimage/util/test_apply_parallel.py
@@ -4,11 +4,6 @@ import platform
 from _skimage2._shared.testing import assert_array_almost_equal, assert_equal
 from skimage import color, data, img_as_float
 from skimage.filters import threshold_local, gaussian
-
-# Don't use `from skimage.util.apply_parallel import apply_parallel` as this
-# may bypass lazy_loader. This causes problems in
-# `test_migration::test_public_skimage_api` because skimage.util.apply_parallel`
-# then refers to the module instead of the function.
 from skimage.util import apply_parallel
 
 import pytest
