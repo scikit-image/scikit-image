009207d03a35fcbc4151d184be700edbe0bbf668
Ravi Kumar <ravishani98765432@gmail.com>
2026-06-13 01:59:44 +0530

    Add package docstrings for internal packages

---
+++ src/skimage/io/_plugins/__init__.py
@@ -0,0 +1,7 @@
+"""
+Image I/O plugin backends used by skimage.io.
+
+This package contains backend-specific plugins for reading and
+writing images through external libraries such as ImageIO,
+Pillow, tifffile, GDAL, and SimpleITK.
+"""
