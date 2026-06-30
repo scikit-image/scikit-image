6704f79d7e18933a406cc7889d5fcd8673fe2167
somnus-J-307 <212805647+somnus-J-307@users.noreply.github.com>
2026-06-30 10:37:19 +0800

    DOC Clarify top-hat morphology example

--- doc/examples/filters/plot_tophat.py
+++ doc/examples/filters/plot_tophat.py
@@ -4,10 +4,10 @@ Removing small objects in grayscale images with a top hat filter
 ================================================================
 
 This example shows how to remove small objects from grayscale images.
-The top-hat transform [1]_ is an operation that extracts small elements and
-details from given images. Here we use a white top-hat transform, which is
-defined as the difference between the input image and its
-(mathematical morphology) opening.
+The top-hat transform [1]_ extracts small elements and details from an image.
+Here, the white top-hat transform extracts bright structures smaller than the
+footprint. Morphological opening produces the complementary result, with these
+bright structures removed.
 
 .. [1] https://en.wikipedia.org/wiki/Top-hat_transform
 
@@ -21,14 +21,15 @@ from skimage import color, morphology
 image = color.rgb2gray(data.hubble_deep_field())[:500, :500]
 
 footprint = morphology.disk(1)
-res = morphology.white_tophat(image, footprint)
+white_tophat = morphology.white_tophat(image, footprint)
+opened = morphology.opening(image, footprint)
 
 fig, ax = plt.subplots(ncols=3, figsize=(20, 8))
 ax[0].set_title('Original')
 ax[0].imshow(image, cmap='gray')
-ax[1].set_title('White tophat')
-ax[1].imshow(res, cmap='gray')
-ax[2].set_title('Complementary')
-ax[2].imshow(image - res, cmap='gray')
+ax[1].set_title('White top-hat')
+ax[1].imshow(white_tophat, cmap='gray')
+ax[2].set_title('Morphological opening')
+ax[2].imshow(opened, cmap='gray')
 
 plt.show()
