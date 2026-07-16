c17c42f6c6d15a1eb984b391deb336d16a7c8bae
sici17 <francescosiciliani624@gmail.com>
2026-07-16 16:18:28 +0200

    doc indentation

--- src/_skimage2/measure/_regionprops.py
+++ src/_skimage2/measure/_regionprops.py
@@ -1288,8 +1288,8 @@ def regionprops(
     **image_filled** : (H, J) ndarray
         Binary region image with filled holes sliced by bounding box.
     **image_intensity** : (H, J) ndarray
-         Intensity image sliced by the bounding box and masked by the region image.
-         Pixels outside the region are set to 0.
+        Intensity image sliced by the bounding box and masked by the region image.
+        Pixels outside the region are set to 0.
     **inertia_tensor** : ndarray
         Inertia tensor of the region for the rotation around its mass.
     **inertia_tensor_eigvals** : tuple
