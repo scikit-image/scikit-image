9b6df22bef6a2767798c0d8bc5d6f4bf3620956f
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2026-07-24 23:53:13 +0200

    Proofread gallery example

--- doc/examples/registration/plot_register_affine.py
+++ doc/examples/registration/plot_register_affine.py
@@ -3,16 +3,17 @@
 Estimate affine transformation to register images
 =================================================
 
-In this example, we use image registration to find an affine transformation
-which can be used to align a moving image to a reference image.
+In this example, we compute an affine transformation
+which may be used to align (register) a moving image to a reference image.
 
-The :func:`skimage.measure.estimate_affine` function uses a Gaussian pyramid,
-and a solver to estimate the parameter of an affine transformation model that
-best aligns the moving image to a reference image. This transformation (which
-is expressed as a (ndim+1, ndim+1) matrix) can be used by
-:func:`scipy.ndimage.affine_transform` to convert the target image to the
-reference space. This approach is explained in detail in Chapter 7 of Elegant
-SciPy [1]_.
+The :func:`skimage.measure.estimate_affine` function uses a Gaussian pyramid
+and a solver to estimate the parameters of an affine transformation model that
+best aligns (registers) the moving image to the reference image. This
+transformation, which is expressed as a (ndim+1, ndim+1) matrix, may be used by
+:func:`scipy.ndimage.affine_transform` to convert the moving image to the
+reference space. This approach is explained in detail in Chapter 7 of *Elegant
+SciPy* [1]_. Note that the moving image is denoted by ``target_image`` or
+``target`` in [1]_.
 
 .. [1] Juan Nunez-Iglesias, Stefan van der Walt, and Harriet Dashnow. Elegant
        SciPy: The Art of Scientific Python. 1st. O'Reilly Media, Inc.,
@@ -27,8 +28,8 @@ from matplotlib import pyplot as plt
 import skimage as ski
 
 ###############################################################################
-# First, we make a toy example with an image and a shifted and rotated version
-# of the same image, using a transformation matrix. See the Wikipedia page on
+# First, we create a toy example with an image, and a shifted and rotated version
+# of the same image, by using a transformation matrix. See the Wikipedia page on
 # `homogeneous coordinates`_ for information on this step.
 #
 # .. _homogeneous coordinates: https://en.wikipedia.org/wiki/Homogeneous_coordinates
@@ -36,7 +37,7 @@ import skimage as ski
 
 reference = ski.data.camera()
 
-# Define a rotation around the center of the image
+# rotation around the center of the image
 r = -np.pi / 4  # rotation angle in radians
 c, s = np.cos(r), np.sin(r)
 R = np.array([[c, -s, 0], [s, c, 0], [0, 0, 1]])
@@ -49,47 +50,51 @@ matrix = np.linalg.inv(T) @ R @ T
 moving = ndi.affine_transform(reference, matrix)
 
 ###############################################################################
-# Next, we are going to see how ``ski.registration.estimate_affine`` can recover
+# Next, we are going to see how :func:`skimage.measure.estimate_affine` can recover
 # that transformation starting from only the two images. It does this initially on a
 # much blurrier and smaller version of the two images, then progressively
-# refines the alignment with sharper, full-resolution versions. This is called
-# a Gaussian pyramid. This function can take different solvers for estimating
-# the transformation at each pyramid level. Solvers are "lukas-kanade", "studholme" and "ecc"
+# refines the alignment with sharper, full-resolution versions. That is called
+# a Gaussian pyramid. The function may use different kinds of solvers for estimating
+# the transformation at each pyramid level. Available solvers are
+# "lukas-kanade", "studholme", and "ecc".
 
 
 transform = ski.registration.estimate_affine(
     reference, moving, transform_type="affine", solver_config="lukas-kanade"
 )
 
-################################################################################
-# To align the moving image, we use ``ndi.affine_transform``
+###############################################################################
+# To register the moving image, we use :func:`scipy.ndimage.affine_transform`:
 
 registered = ndi.affine_transform(moving, transform.params)
 
 
 ###############################################################################
-# Because we know the original transform, we can also compute the target
-# registration error map:
+# Since we know the original transform, we can also compute the target
+# registration error (TRE):
 
 tre = ski.registration.target_registration_error(
     reference.shape, transform.params @ matrix
 )
 
 ###############################################################################
-# Let's have a look at the results. We display here images pairs as
+# Let us have a look at the results. Below we display image pairs as
 # magenta-green color images.
 
-plt.subplot(131)
-plt.imshow(np.stack((reference, moving, reference), -1))
-plt.axis('off')
-plt.title('Before registration')
-plt.subplot(132)
-plt.imshow(np.stack((reference, registered, reference), -1))
-plt.axis('off')
-plt.title('After registration')
-plt.subplot(133)
-plt.imshow(tre)
-plt.axis('off')
-plt.title(f'TRE max {tre.max():.2g} pixels')
-plt.tight_layout()
+fig, ax = plt.subplots(ncols=3)
+
+ax[0].imshow(np.stack((reference, moving, reference), -1))
+ax[0].set_title('Before registration')
+ax[0].axis('off')
+
+ax[1].imshow(np.stack((reference, registered, reference), -1))
+ax[1].set_title('After registration')
+ax[1].axis('off')
+
+ax[2].imshow(tre)
+ax[2].set_title(f'TRE; max = {tre.max():.2g}')
+ax[2].axis('off')
+
+fig.tight_layout()
+
 plt.show()
