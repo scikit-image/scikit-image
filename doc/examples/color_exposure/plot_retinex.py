3d1dafdc33b3bcb930b2e4e14d299d30f8600b95
qbarthelemy <q.barthelemy@gmail.com>
2026-09-21 18:46:33 +0200

    ruff format

--- doc/examples/color_exposure/plot_retinex.py
+++ doc/examples/color_exposure/plot_retinex.py
@@ -23,7 +23,7 @@ from skimage import data, img_as_float
 from skimage import exposure
 from skimage.filters import gaussian
 
-matplotlib.rcParams['font.size'] = 8
+matplotlib.rcParams["font.size"] = 8
 
 
 def ssr(img, sigma):
@@ -69,8 +69,11 @@ def msrcr(img, sigmas, G=192, b=-30, alpha=125, beta=46, low_clip=None, high_cli
     out = G * (C * R + b)  # Eq(6) of [1]
 
     for i in range(out.shape[2]):
-        out[:, :, i] = 255 * (out[:, :, i] - np.min(out[:, :, i])) / \
-            (np.max(out[:, :, i]) - np.min(out[:, :, i]))
+        out[:, :, i] = (
+            255
+            * (out[:, :, i] - np.min(out[:, :, i]))
+            / (np.max(out[:, :, i]) - np.min(out[:, :, i]))
+        )
     out = np.uint8(np.minimum(np.maximum(out, 0), 255))
     if low_clip and high_clip:
         out = scb(out, low_clip, high_clip)
@@ -88,15 +91,15 @@ def plot_img_and_hist(image, axes, bins=256):
     ax_img.set_axis_off()
 
     # Display histogram
-    ax_hist.hist(image.ravel(), bins=bins, histtype='step', color='black')
-    ax_hist.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
-    ax_hist.set_xlabel('Pixel intensity')
+    ax_hist.hist(image.ravel(), bins=bins, histtype="step", color="black")
+    ax_hist.ticklabel_format(axis="y", style="scientific", scilimits=(0, 0))
+    ax_hist.set_xlabel("Pixel intensity")
     ax_hist.set_xlim(0, 1)
     ax_hist.set_yticks([])
 
     # Display cumulative distribution
     img_cdf, bins = exposure.cumulative_distribution(image, bins)
-    ax_cdf.plot(bins, img_cdf, 'r')
+    ax_cdf.plot(bins, img_cdf, "r")
     ax_cdf.set_yticks([])
 
     return ax_img, ax_hist, ax_cdf
@@ -124,19 +127,19 @@ axes[1, 1] = plt.subplot(2, 3, 5)
 axes[1, 2] = plt.subplot(2, 3, 6)
 
 ax_img, ax_hist, ax_cdf = plot_img_and_hist(img, axes[:, 0])
-ax_img.set_title('Low contrast image')
+ax_img.set_title("Low contrast image")
 
 y_min, y_max = ax_hist.get_ylim()
-ax_hist.set_ylabel('Number of pixels')
+ax_hist.set_ylabel("Number of pixels")
 ax_hist.set_yticks(np.linspace(0, y_max, 5))
 
 ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_msr, axes[:, 1])
-ax_img.set_title('MSR enhancement')
+ax_img.set_title("MSR enhancement")
 
 ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_msrscb, axes[:, 2])
-ax_img.set_title('MSR+SCB enhancement')
+ax_img.set_title("MSR+SCB enhancement")
 
-ax_cdf.set_ylabel('Fraction of total intensity')
+ax_cdf.set_ylabel("Fraction of total intensity")
 ax_cdf.set_yticks(np.linspace(0, 1, 5))
 
 # prevent overlap of y-axis labels
